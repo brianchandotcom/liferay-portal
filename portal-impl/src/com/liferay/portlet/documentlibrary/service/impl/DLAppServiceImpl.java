097b49c109ca4a4a6f1d26e86e99fd04c9bde430
Alexander Chow <alexander.chow@liferay.com>
2011-06-22 16:37:51 +0100

    LPS-18197 Fix deadlock

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLAppServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLAppServiceImpl.java
@@ -35,6 +35,7 @@ import com.liferay.portlet.documentlibrary.NoSuchFileException;
 import com.liferay.portlet.documentlibrary.model.DLFileShortcut;
 import com.liferay.portlet.documentlibrary.model.DLFolderConstants;
 import com.liferay.portlet.documentlibrary.service.base.DLAppServiceBaseImpl;
+import com.liferay.portlet.documentlibrary.util.DLProcessor;
 import com.liferay.portlet.documentlibrary.util.comparator.RepositoryModelModifiedDateComparator;
 
 import java.io.File;
@@ -122,7 +123,7 @@ public class DLAppServiceImpl extends DLAppServiceBaseImpl {
 			folderId, mimeType, title, description, changeLog, is, size,
 			serviceContext);
 
-		dlAppHelperLocalService.triggerProcesses(fileEntry);
+		DLProcessor.triggerAll(fileEntry);
 
 		return fileEntry;
 	}
@@ -711,7 +712,7 @@ public class DLAppServiceImpl extends DLAppServiceBaseImpl {
 			fileEntryId, sourceFileName, mimeType, title, description,
 			changeLog, majorVersion, is, size, serviceContext);
 
-		dlAppHelperLocalService.triggerProcesses(fileEntry);
+		DLProcessor.triggerAll(fileEntry);
 
 		return fileEntry;
 	}
@@ -766,9 +767,11 @@ public class DLAppServiceImpl extends DLAppServiceBaseImpl {
 
 		for (FileEntry srcFileEntry : srcFileEntries) {
 			try {
-				repository.copyFileEntry(
+				FileEntry fileEntry = repository.copyFileEntry(
 					destFolder.getGroupId(), srcFileEntry.getFileEntryId(),
 					destFolder.getFolderId(), serviceContext);
+
+				DLProcessor.triggerAll(fileEntry);
 			}
 			catch (Exception e) {
 				_log.error(e, e);

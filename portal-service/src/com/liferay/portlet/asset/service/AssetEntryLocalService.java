02694c067fe65e8bf5c469c5f3d1cffdcac6b2ec
Juan Fernández <juan.fernandez@liferay.com>
2011-06-22 12:46:21 +0200

    LPS-17179 Autogenerated

--- portal-service/src/com/liferay/portlet/asset/service/AssetEntryLocalService.java
+++ portal-service/src/com/liferay/portlet/asset/service/AssetEntryLocalService.java
@@ -381,6 +381,12 @@ public interface AssetEntryLocalService {
 		throws com.liferay.portal.kernel.exception.PortalException,
 			com.liferay.portal.kernel.exception.SystemException;
 
+	public com.liferay.portlet.asset.model.AssetEntry updateVisible(
+		java.lang.String className, long classPK, boolean visible,
+		com.liferay.portal.service.ServiceContext serviceContext)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException;
+
 	public void validate(long groupId, java.lang.String className,
 		long[] categoryIds, java.lang.String[] tagNames)
 		throws com.liferay.portal.kernel.exception.PortalException,

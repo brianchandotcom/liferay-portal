55d8f67470be83b152bad4d4b19650a370396480
Connor McKay <connor@homeschooldebate.net>
2011-07-07 09:23:50 -0700

    Adding system for assigning resource block IDS

--- portal-impl/src/com/liferay/portal/service/impl/ResourceBlockLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/ResourceBlockLocalServiceImpl.java
@@ -14,20 +14,55 @@
 
 package com.liferay.portal.service.impl;
 
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.util.DigesterUtil;
+import com.liferay.portal.model.PermissionedModel;
+import com.liferay.portal.model.ResourceBlock;
 import com.liferay.portal.model.ResourcePermission;
 import com.liferay.portal.service.base.ResourceBlockLocalServiceBaseImpl;
 
 import java.nio.ByteBuffer;
-
 import java.util.List;
 
 /**
+ * Manages the creation and upkeep of resource blocks and the resources they
+ * contain.
+ *
  * @author Connor McKay
  */
 public class ResourceBlockLocalServiceImpl
 	extends ResourceBlockLocalServiceBaseImpl {
+
+	/**
+	 * Adds a resource block for the resource and creates associations between
+	 * it and the roles with permission to access the resource.
+	 *
+	 * @param  permissionsHash the resource's permissions hash
+	 * @param  name the resource's name, which can be either a class name or a
+	 *         portlet ID
+	 * @param  primKey the primary key of the resource
+	 * @return the new resource block
+	 * @throws SystemException if a system exception occurred
+	 */
+	public ResourceBlock addResourceBlock(
+			String permissionsHash, String name, String primKey)
+		throws SystemException {
+
+		long resourceBlockId = counterLocalService.increment();
+
+		ResourceBlock resourceBlock =
+			resourceBlockPersistence.create(resourceBlockId);
+
+		resourceBlock.setPermissionsHash(permissionsHash);
+		resourceBlock.setReferenceCount(1);
+
+		resourceBlockPersistence.update(resourceBlock, false);
+
+		resourceBlockRoleActionLocalService.addResourceBlockRoleActions(resourceBlock, name, primKey);
+
+		return resourceBlock;
+	}
 
 	/**
 	 * Returns the permissions hash for the resource. The permissions hash is a
@@ -47,6 +82,12 @@ public class ResourceBlockLocalServiceImpl
 			resourcePermissionLocalService.getResourceResourcePermissions(
 			name, primKey);
 
+		return getPermissionsHash(resourcePermissions);
+	}
+
+	public String getPermissionsHash(
+		List<ResourcePermission> resourcePermissions) {
+
 		ByteBuffer buffer =
 			ByteBuffer.allocate(resourcePermissions.size() * 16);
 
@@ -58,6 +99,91 @@ public class ResourceBlockLocalServiceImpl
 		buffer.flip();
 
 		return DigesterUtil.digestHex("SHA-1", buffer);
+	}
+
+	/**
+	 * Increments the reference count of the resource block and updates it in
+	 * the database.
+	 *
+	 * @param  resourceBlock the resource block
+	 * @throws SystemException if a system exception occurred
+	 */
+	public void retain(ResourceBlock resourceBlock) throws SystemException {
+		resourceBlock.setReferenceCount(resourceBlock.getReferenceCount() + 1);
+		updateResourceBlock(resourceBlock);
+	}
+
+	/**
+	 * Decrements the reference count of the resource block and updates it in
+	 * the database or deletes the resource block if the reference count reaches
+	 * zero.
+	 *
+	 * @param  resourceBlock the resource block
+	 * @throws SystemException if a system exception occurred
+	 */
+	public void release(ResourceBlock resourceBlock) throws SystemException {
+		long referenceCount = resourceBlock.getReferenceCount() - 1;
+
+		if (referenceCount <= 0) {
+			deleteResourceBlock(resourceBlock);
+			return;
+		}
+
+		resourceBlock.setReferenceCount(referenceCount);
+		updateResourceBlock(resourceBlock);
+	}
+
+	/**
+	 * Updates which resource block the resource is a member of, and updates it
+	 * in the database. Automatically retains, releases, and creates resource
+	 * blocks as necessary.
+	 *
+	 * @param  resource the resource block
+	 * @param  name the resource's name, which can be either a class name or a
+	 *         portlet ID
+	 * @param  primKey the primary key of the resource
+	 * @throws SystemException if a system exception occurred
+	 */
+	public void updateResourceBlockId(
+			PermissionedModel resource, String name, String primKey)
+		throws SystemException {
+
+		ResourceBlock resourceBlock;
+
+		try {
+			resourceBlock =
+				getResourceBlock(resource.getResourceBlockId());
+		}
+		catch (PortalException e) {
+			throw new SystemException(e);
+		}
+
+		String newPermissionsHash = getPermissionsHash(name, primKey);
+		String currentPermissionsHash =
+			resourceBlock.getPermissionsHash();
+
+		// If the permissions hash is still the same, nothing needs to be done
+
+		if (currentPermissionsHash.equals(newPermissionsHash)) {
+			return;
+		}
+
+		// Otherwise, release the old resource block
+
+		release(resourceBlock);
+
+		resourceBlock =
+			resourceBlockPersistence.fetchByPermissionsHash(newPermissionsHash);
+
+		if (resourceBlock == null) {
+			resourceBlock = addResourceBlock(newPermissionsHash, name, primKey);
+		}
+		else {
+			retain(resourceBlock);
+		}
+
+		resource.setResourceBlockId(resourceBlock.getResourceBlockId());
+		resource.save();
 	}
 
 }

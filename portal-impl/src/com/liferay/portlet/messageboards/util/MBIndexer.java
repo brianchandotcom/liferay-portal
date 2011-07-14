a4a30df7395381493d9ac2fe362bb8c8e7f320b3
Michael Han <michael.han@liferay.com>
2011-07-14 16:53:20 -0500

    LPS-18875 -add multi-destination proxy mechanism -refactoring query factories to allow lucene and generic to existing simultaneously -refactoring to add for auto wiring support when new search engines are added -refactoring to introduce searchContext in searchWriter interface

--- portal-impl/src/com/liferay/portlet/messageboards/util/MBIndexer.java
+++ portal-impl/src/com/liferay/portlet/messageboards/util/MBIndexer.java
@@ -115,7 +115,8 @@ public class MBIndexer extends BaseIndexer {
 				return;
 			}
 
-			BooleanQuery categoriesQuery = BooleanQueryFactoryUtil.create();
+			BooleanQuery categoriesQuery = BooleanQueryFactoryUtil.create(
+				searchContext);
 
 			for (long categoryId : categoryIds) {
 				try {
@@ -134,10 +135,14 @@ public class MBIndexer extends BaseIndexer {
 
 	@Override
 	protected void doDelete(Object obj) throws Exception {
+		SearchContext searchContext = new SearchContext();
+		searchContext.setSearchEngineId(SearchEngineUtil.SYSTEM_ENGINE_ID);
+
 		if (obj instanceof MBCategory) {
 			MBCategory category = (MBCategory)obj;
 
-			BooleanQuery booleanQuery = BooleanQueryFactoryUtil.create();
+			BooleanQuery booleanQuery = BooleanQueryFactoryUtil.create(
+				searchContext);
 
 			booleanQuery.addRequiredTerm(Field.PORTLET_ID, PORTLET_ID);
 
@@ -171,7 +176,8 @@ public class MBIndexer extends BaseIndexer {
 			MBMessage message = MBMessageLocalServiceUtil.getMessage(
 				thread.getRootMessageId());
 
-			BooleanQuery booleanQuery = BooleanQueryFactoryUtil.create();
+			BooleanQuery booleanQuery = BooleanQueryFactoryUtil.create(
+				searchContext);
 
 			booleanQuery.addRequiredTerm(Field.PORTLET_ID, PORTLET_ID);
 

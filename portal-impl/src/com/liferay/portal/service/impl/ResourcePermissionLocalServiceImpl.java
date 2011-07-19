ae8988dd0a46ab1f5100cecee4c325226e1e6d0e
Alexander Chow <alexander.chow@liferay.com>
2011-07-19 01:54:45 +0100

    LPS-17841 Fix source formatting error

--- portal-impl/src/com/liferay/portal/service/impl/ResourcePermissionLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/ResourcePermissionLocalServiceImpl.java
@@ -584,6 +584,9 @@ public class ResourcePermissionLocalServiceImpl
 				}
 			}
 		}
+		else {
+			actionIdsLong |= resourcePermission.getActionIds();
+		}
 
 		resourcePermission.setActionIds(actionIdsLong);
 

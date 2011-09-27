a739b7932136ce43595f4e8ed9a07861c0472a74
Jim Hinkey <james.hinkey@liferay.com>
2011-09-27 11:56:03 -0400

    LRDOCS-138-Javadoc-Company: fd043b1 Merge remote-tracking branch 'gabriel-lee/javadoc' into LRDOCS-138-Javadoc-Company (cherry-picking from commit 3c64273ddb3dd4be435ae03d5cba0d910dec970c)

--- portal-impl/src/com/liferay/portal/service/impl/CompanyLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/CompanyLocalServiceImpl.java
@@ -79,11 +79,30 @@ import javax.portlet.PortletException;
 import javax.portlet.PortletPreferences;
 
 /**
+ * The implementation of the company local service. Each company refers to a
+ * separate portal instance.
+ *
  * @author Brian Wing Shun Chan
  * @author Julio Camarero
  */
 public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 
+	/**
+	 * Adds a company.
+	 *
+	 * @param  webId the the company's web domain
+	 * @param  virtualHostname the company's virtual host name
+	 * @param  mx the company's mail domain
+	 * @param  shardName the company's shard
+	 * @param  system whether the company is used by WSRP
+	 * @param  maxUsers the max number of company users (optionally
+	 *         <code>0</code>)
+	 * @param  active whether the company is active
+	 * @return the company
+	 * @throws PortalException if the web domain, virtual host name, or mail
+	 *         domain was invalid
+	 * @throws SystemException if a system exception occurred
+	 */
 	public Company addCompany(
 			String webId, String virtualHostname, String mx, String shardName,
 			boolean system, int maxUsers, boolean active)
@@ -118,6 +137,17 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 		return company;
 	}
 
+	/**
+	 * Returns the company with the web domain.
+	 *
+	 * The method sets mail domain to the web domain, and the shard name to
+	 * the default name set in portal.properties
+	 *
+	 * @param  webId the company's web domain
+	 * @return the company with the web domain
+	 * @throws PortalException if a portal exception occurred
+	 * @throws SystemException if a system exception occurred
+	 */
 	public Company checkCompany(String webId)
 		throws PortalException, SystemException {
 
@@ -127,6 +157,20 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 			webId, mx, PropsValues.SHARD_DEFAULT_NAME);
 	}
 
+	/**
+	 * Returns the company with the web domain, mail domain, and shard. If no
+	 * such company exits, the method will create a new company.
+	 *
+	 * The method goes through a series of checks to ensure that the company
+	 * contains default users, groups, etc.
+	 *
+	 * @param  webId the company's web domain
+	 * @param  mx the company's mail domain
+	 * @param  shardName the company's shard
+	 * @return the company with the web domain, mail domain, and shard
+	 * @throws PortalException if a portal exception occurred
+	 * @throws SystemException if a system exception occurred
+	 */
 	public Company checkCompany(String webId, String mx, String shardName)
 		throws PortalException, SystemException {
 
@@ -400,6 +444,15 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 		return company;
 	}
 
+	/**
+	 * Checks if the company has an encryption key. It will create a key if one
+	 * does not exist.
+	 *
+	 * @param  companyId the primary key of the company
+	 * @throws PortalException if a company with the primary key could not be
+	 *         found
+	 * @throws SystemException if a system exception occurred
+	 */
 	public void checkCompanyKey(long companyId)
 		throws PortalException, SystemException {
 
@@ -419,6 +472,14 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 		}
 	}
 
+	/**
+	 * Deletes the company's logo.
+	 *
+	 * @param  companyId the primary key of the company
+	 * @throws PortalException if the company with the primary key could not be
+	 *         found or if the company's logo could not be found
+	 * @throws SystemException if a system exception occurred
+	 */
 	public void deleteLogo(long companyId)
 		throws PortalException, SystemException {
 
@@ -435,10 +496,26 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 		}
 	}
 
+	/**
+	 * Returns the company with the primary key.
+	 *
+	 * @param  companyId the primary key of the company
+	 * @return the company with the primary key, <code>null</code> if a company
+	 *         with the primary key could not be found
+	 * @throws SystemException if a system exception occurred
+	 */
 	public Company fetchCompany(long companyId) throws SystemException {
 		return companyPersistence.fetchByPrimaryKey(companyId);
 	}
 
+	/**
+	 * Returns the company with the virtual host name.
+	 *
+	 * @param  virtualHostname the virtual host name
+	 * @return the company with the virtual host name, <code>null</code> if a
+	 *         company with the virtual host could not be found
+	 * @throws SystemException if a system exception occurred
+	 */
 	public Company fetchCompanyByVirtualHost(String virtualHostname)
 		throws SystemException {
 
@@ -455,36 +532,92 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 			virtualHost.getCompanyId());
 	}
 
+	/**
+	 * Returns all the companies.
+	 *
+	 * @return the companies
+	 * @throws SystemException if a system exception occurred
+	 */
 	public List<Company> getCompanies() throws SystemException {
 		return companyPersistence.findAll();
 	}
 
+	/**
+	 * Returns all the companies used by WSRP.
+	 *
+	 * @param  system whether the company is used by WSRP
+	 * @return the companies used by WSRP
+	 * @throws SystemException if a system exception occurred
+	 */
 	public List<Company> getCompanies(boolean system) throws SystemException {
 		return companyPersistence.findBySystem(system);
 	}
 
+	/**
+	 * Returns the number of companies used by WSRP.
+	 *
+	 * @param  system whether the company is used by WSRP
+	 * @return the number of companies used by WSRP
+	 * @throws SystemException if a system exception occurred
+	 */
 	public int getCompaniesCount(boolean system) throws SystemException {
 		return companyPersistence.countBySystem(system);
 	}
 
+	/**
+	 * Returns the company with the primary key.
+	 *
+	 * @param  companyId the primary key of the company
+	 * @return the company with the primary key
+	 * @throws PortalException if a company with the primary key could not be
+	 *         found
+	 * @throws SystemException if a system exception occurred
+	 */
 	public Company getCompanyById(long companyId)
 		throws PortalException, SystemException {
 
 		return companyPersistence.findByPrimaryKey(companyId);
 	}
 
+	/**
+	 * Returns the company with the logo.
+	 *
+	 * @param  logoId the ID of the company's logo
+	 * @return the company with the logo
+	 * @throws PortalException if the company with the logo could not be found
+	 * @throws SystemException if a system exception occurred
+	 */
 	public Company getCompanyByLogoId(long logoId)
 		throws PortalException, SystemException {
 
 		return companyPersistence.findByLogoId(logoId);
 	}
 
+	/**
+	 * Returns the company with the mail domain.
+	 *
+	 * @param  mx the company's mail domain
+	 * @return the company with the mail domain
+	 * @throws PortalException if the company with the mail domain could not be
+	 *         found
+	 * @throws SystemException if a system exception occurred
+	 */
 	public Company getCompanyByMx(String mx)
 		throws PortalException, SystemException {
 
 		return companyPersistence.findByMx(mx);
 	}
 
+	/**
+	 * Returns the company with the virtual host name.
+	 *
+	 * @param  virtualHostname the company's virtual host name
+	 * @return the company with the virtual host name
+	 * @throws PortalException if the company with the virtual host name could
+	 *         not be found or if the virtual host was not associated with a
+	 *         company
+	 * @throws SystemException if a system exception occurred
+	 */
 	public Company getCompanyByVirtualHost(String virtualHostname)
 		throws PortalException, SystemException {
 
@@ -508,6 +641,15 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 		}
 	}
 
+	/**
+	 * Returns the company with the web domain.
+	 *
+	 * @param  webId the company's web domain
+	 * @return the company with the web domain
+	 * @throws PortalException if the company with the web domain could not be
+	 *         found
+	 * @throws SystemException if a system exception occurred
+	 */
 	public Company getCompanyByWebId(String webId)
 		throws PortalException, SystemException {
 
@@ -539,6 +681,17 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 		return companyId;
 	}
 
+	/**
+	 * Removes the values that match the keys of the company's preferences.
+	 *
+	 * This method is called by {@link
+	 * com.liferay.portlet.portalsettings.action.EditLDAPServerAction} remotely
+	 * through {@link com.liferay.portal.service.CompanyService}.
+	 *
+	 * @param  companyId the primary key of the company
+	 * @param  keys the company's preferences keys to be remove
+	 * @throws SystemException if a system exception occurred
+	 */
 	public void removePreferences(long companyId, String[] keys)
 		throws SystemException {
 
@@ -557,6 +710,24 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 		}
 	}
 
+	/**
+	 * Returns an ordered range of all assets that match the keywords in the
+	 * company.
+	 *
+	 * The method is called in {@link
+	 * com.liferay.portal.search.PortalOpenSearchImpl} which is not longer used
+	 * by the Search portlet.
+	 *
+	 * @param  companyId the primary key of the company
+	 * @param  userId the primary key of the user
+	 * @param  keywords the keywords (space separated),which may occur in
+	 *         assets in the company (optionally <code>null</code>)
+	 * @param  start the lower bound of the range of assets to return
+	 * @param  end the upper bound of the range of assets to return (not
+	 *         inclusive)
+	 * @return the matching assets in the company
+	 * @throws SystemException if a system exception occurred
+	 */
 	public Hits search(
 			long companyId, long userId, String keywords, int start, int end)
 		throws SystemException {
@@ -564,6 +735,25 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 		return search(companyId, userId, null, 0, null, keywords, start, end);
 	}
 
+	/**
+	 * Returns an ordered range of all assets that match the keywords in the
+	 * portlet within the company.
+	 *
+	 * @param  companyId the primary key of the company
+	 * @param  userId the primary key of the user
+	 * @param  portletId the primary key of the portlet (optionally
+	 *         <code>null</code>)
+	 * @param  groupId the primary key of the group (optionally <code>0</code>)
+	 * @param  type the mime type of assets to return(optionally
+	 *         <code>null</code>)
+	 * @param  keywords the keywords (space separated), which may occur in any
+	 *         assets in the portlet (optionally <code>null</code>)
+	 * @param  start the lower bound of the range of assets to return
+	 * @param  end the upper bound of the range of assets to return (not
+	 *         inclusive)
+	 * @return the matching assets in the portlet within the company
+	 * @throws SystemException if a system exception occurred
+	 */
 	public Hits search(
 			long companyId, long userId, String portletId, long groupId,
 			String type, String keywords, int start, int end)
@@ -614,6 +804,20 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 		}
 	}
 
+	/**
+	 * Updates the company.
+	 *
+	 * @param  companyId the primary key of the company
+	 * @param  virtualHostname the company's virtual host name
+	 * @param  mx the company's mail domain
+	 * @param  maxUsers the max number of company users (optionally
+	 *         <code>0</code>)
+	 * @param  active whether the company is active
+	 * @return the company with the primary key
+	 * @throws PortalException if a company with primary key could not be found
+	 *         or if the new information was invalid
+	 * @throws SystemException if a system exception occurred
+	 */
 	public Company updateCompany(
 			long companyId, String virtualHostname, String mx, int maxUsers,
 			boolean active)
@@ -649,6 +853,33 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 		return company;
 	}
 
+	/**
+	 * Update the company with additional account information.
+	 *
+	 * @param  companyId the primary key of the company
+	 * @param  virtualHostname the company's virtual host name
+	 * @param  mx the company's mail domain
+	 * @param  homeURL the company's home URL (optionally <code>null</code>)
+	 * @param  name the company's account name(optionally <code>null</code>)
+	 * @param  legalName the company's account legal name (optionally
+	 *         <code>null</code>)
+	 * @param  legalId the company's account legal ID (optionally
+	 *         <code>null</code>)
+	 * @param  legalType the company's account legal type (optionally
+	 *         <code>null</code>)
+	 * @param  sicCode the company's account SIC code (optionally
+	 *         <code>null</code>)
+	 * @param  tickerSymbol the company's account ticker symbol (optionally
+	 *         <code>null</code>)
+	 * @param  industry the company's account industry (optionally
+	 *         <code>null</code>)
+	 * @param  type the company's account type (optionally <code>null</code>)
+	 * @param  size the company's account size (optionally <code>null</code>)
+	 * @return the company with the primary key
+	 * @throws PortalException if a company with the primary key could not be
+	 *         found or if the new information was invalid
+	 * @throws SystemException if a system exception occurred
+	 */
 	public Company updateCompany(
 			long companyId, String virtualHostname, String mx, String homeURL,
 			String name, String legalName, String legalId, String legalType,
@@ -714,6 +945,15 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 		return company;
 	}
 
+	/**
+	 * Update the company's display.
+	 *
+	 * @param  companyId the primary key of the company
+	 * @param  languageId the ID of the company's default user's language
+	 * @param  timeZoneId the ID of the company's default user's time zone
+	 * @throws PortalException if the company's default user could not be found
+	 * @throws SystemException if a system exception occurred
+	 */
 	public void updateDisplay(
 			long companyId, String languageId, String timeZoneId)
 		throws PortalException, SystemException {
@@ -726,6 +966,15 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 		userPersistence.update(user, false);
 	}
 
+	/**
+	 * Updates the company's logo.
+	 *
+	 * @param  companyId the primary key of the company
+	 * @param  bytes the bytes of the company's logo image
+	 * @throws PortalException if the company's logo ID could not be found or
+	 *         if the logo's image was corrupted
+	 * @throws SystemException if a system exception occurred
+	 */
 	public void updateLogo(long companyId, byte[] bytes)
 		throws PortalException, SystemException {
 
@@ -734,6 +983,15 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 		imageLocalService.updateImage(logoId, bytes);
 	}
 
+	/**
+	 * Updates the company's logo.
+	 *
+	 * @param  companyId the primary key of the company
+	 * @param  file the file of the company's logo image
+	 * @throws PortalException the company's logo ID could not be found or if
+	 *         the logo's image was corrupted
+	 * @throws SystemException if a system exception occurred
+	 */
 	public void updateLogo(long companyId, File file)
 		throws PortalException, SystemException {
 
@@ -742,6 +1000,15 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 		imageLocalService.updateImage(logoId, file);
 	}
 
+	/**
+	 * Update the company's logo.
+	 *
+	 * @param  companyId the primary key of the company
+	 * @param  is the input stream of the company's logo image
+	 * @throws PortalException if the company's logo ID could not be found or
+	 *         if the company's logo image was corrupted
+	 * @throws SystemException if a system exception occurred
+	 */
 	public void updateLogo(long companyId, InputStream is)
 		throws PortalException, SystemException {
 
@@ -750,6 +1017,15 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 		imageLocalService.updateImage(logoId, is);
 	}
 
+	/**
+	 * Updates the company's preferences. The company's default properties are
+	 * found in portal.properties.
+	 *
+	 * @param  companyId the primary key of the company
+	 * @param  properties the company's properties. See {@link
+	 *         com.liferay.portal.kernel.util.UnicodeProperties}
+	 * @throws SystemException if a system exception occurred
+	 */
 	public void updatePreferences(long companyId, UnicodeProperties properties)
 		throws PortalException, SystemException {
 
@@ -808,6 +1084,25 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 		}
 	}
 
+	/**
+	 * Updates the company's security properties.
+	 *
+	 * @param  companyId the primary key of the company
+	 * @param  authType the company's method of authenticating users
+	 * @param  autoLogin whether to allow users to select the "remember me"
+	 *         feature
+	 * @param  sendPassword whether to allow users to ask the company to send
+	 *         their password
+	 * @param  strangers whether to allow strangers to create accounts register
+	 *         themselves in the company
+	 * @param  strangersWithMx whether to allow strangers to create accounts
+	 *         with email addresses that match the company mail suffix
+	 * @param  strangersVerify whether to require strangers who create accounts
+	 *         to be verified via email
+	 * @param  siteLogo whether to allow site administrators to use their own
+	 *         logo instead of the enterprise logo
+	 * @throws SystemException if a system exception occurred
+	 */
 	public void updateSecurity(
 			long companyId, String authType, boolean autoLogin,
 			boolean sendPassword, boolean strangers, boolean strangersWithMx,

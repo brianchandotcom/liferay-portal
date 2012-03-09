brianchandotcom/liferay-portal

commit fae7b2523dec5a63c6d6e6ebedc0f87d84815b6a

LPS-25345

portal-impl/src/com/liferay/portal/events/ServicePreAction.java
@@ -399,8 +399,7 @@ public class ServicePreAction extends Action {
 				Group sourceGroup = GroupLocalServiceUtil.getGroup(
 					sourceGroupId);
 
-				if (layout.isPublicLayout() ||
-					SitesUtil.isUserGroupLayoutSetViewable(
+				if (SitesUtil.isUserGroupLayoutSetViewable(
 						permissionChecker, layout.getGroup())) {
 
 					layout = new VirtualLayout(layout, sourceGroup);
@@ -664,7 +663,8 @@ public class ServicePreAction extends Action {
 			request.setAttribute(WebKeys.LAYOUT, layout);
 			request.setAttribute(WebKeys.LAYOUTS, layouts);
 
-			if (layout.isPrivateLayout() ||
+			if ((layout.isPrivateLayout() &&
+				 !PropsValues.PERMISSIONS_LAYOUT_PRIVATE_CHECK_GUEST_ENABLED)||
 				!PropsValues.PERMISSIONS_LAYOUT_PUBLIC_CHECK_GUEST_ENABLED) {
 
 				permissionChecker.setCheckGuest(false);

portal-impl/src/com/liferay/portal/util/PropsValues.java
@@ -1112,6 +1112,8 @@ public class PropsValues {
 
 	public static final boolean PERMISSIONS_INLINE_SQL_CHECK_ENABLED = GetterUtil.getBoolean(PropsUtil.get(PropsKeys.PERMISSIONS_INLINE_SQL_CHECK_ENABLED));
 
+	public static final boolean PERMISSIONS_LAYOUT_PRIVATE_CHECK_GUEST_ENABLED = GetterUtil.getBoolean(PropsUtil.get(PropsKeys.PERMISSIONS_LAYOUT_PRIVATE_CHECK_GUEST_ENABLED));
+
 	public static final boolean PERMISSIONS_LAYOUT_PUBLIC_CHECK_GUEST_ENABLED = GetterUtil.getBoolean(PropsUtil.get(PropsKeys.PERMISSIONS_LAYOUT_PUBLIC_CHECK_GUEST_ENABLED));
 
 	public static final String PERMISSIONS_LIST_FILTER = PropsUtil.get(PropsKeys.PERMISSIONS_LIST_FILTER);

portal-impl/src/portal.properties
@@ -3598,11 +3598,26 @@
     #
     # Set this to true if resources that belong to a public layout should assume
     # that all users have the Guest role. Set this to false if resources that
-    # belong to a public layout should behave like they belong to a private
-    # layout, that is, users do not automatically inherit permissions that
-    # belong to the Guest role.
+    # belong to a public layout will not assume that all users have the Guest
+	# role and thus do not automatically inherit permissions that belong to the
+	# Guest role.
+	#
+	# Note: setting this property to false may require users to grant
+	# permissions to roles like Site Member and User.
     #
     permissions.layout.public.check.guest.enabled=true
+
+    #
+    # Set this to true if resources that belong to a private layout should
+	# assume that all users have the Guest role. Set this to false if resources
+	# that belong to a private layout will not assume that all users have the
+	# Guest role and thus do not automatically inherit permissions that belong
+	# to the Guest role.
+	#
+	# Note: setting this property to false may require users to grant
+	# permissions to roles like Site Member and User.
+    #
+    permissions.layout.private.check.guest.enabled=true
 
 ##
 ## Captcha

portal-service/src/com/liferay/portal/kernel/util/PropsKeys.java
@@ -1555,6 +1555,8 @@ public interface PropsKeys {
 
 	public static final String PERMISSIONS_INLINE_SQL_CHECK_ENABLED = "permissions.inline.sql.check.enabled";
 
+	public static final String PERMISSIONS_LAYOUT_PRIVATE_CHECK_GUEST_ENABLED = "permissions.layout.private.check.guest.enabled";
+
 	public static final String PERMISSIONS_LAYOUT_PUBLIC_CHECK_GUEST_ENABLED = "permissions.layout.public.check.guest.enabled";
 
 	public static final String PERMISSIONS_LIST_FILTER = "permissions.list.filter";

commit 3e16a2c2459462f667c38ffbad04fb0b0069c6b5

LPS-26002 caused by bad removal from fae7b2523d

portal-impl/src/com/liferay/portal/events/ServicePreAction.java
@@ -399,7 +399,8 @@ public class ServicePreAction extends Action {
 				Group sourceGroup = GroupLocalServiceUtil.getGroup(
 					sourceGroupId);
 
-				if (SitesUtil.isUserGroupLayoutSetViewable(
+				if (layout.isPublicLayout() ||
+					SitesUtil.isUserGroupLayoutSetViewable(
 						permissionChecker, layout.getGroup())) {
 
 					layout = new VirtualLayout(layout, sourceGroup);
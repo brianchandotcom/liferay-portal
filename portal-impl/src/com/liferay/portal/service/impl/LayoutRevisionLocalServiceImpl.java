6cf0cf72358734abfc610fd844fe97d8167873d9
shuyangzhou <shuyang.zhou@liferay.com>
2012-03-19 16:19:44 +0800

    LPS-26149 Optimize Preferences API

--- portal-impl/src/com/liferay/portal/service/impl/LayoutRevisionLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/LayoutRevisionLocalServiceImpl.java
@@ -530,7 +530,7 @@ public class LayoutRevisionLocalServiceImpl
 
 		for (PortletPreferences portletPreferences : portletPreferencesList) {
 			portletPreferencesLocalService.addPortletPreferences(
-				layoutRevision.getCompanyId(), portletPreferences.getOwnerId(),
+				portletPreferences.getOwnerId(),
 				portletPreferences.getOwnerType(),
 				layoutRevision.getLayoutRevisionId(),
 				portletPreferences.getPortletId(), null,

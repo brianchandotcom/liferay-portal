b447d7f005d4cae0df050cd8266194a5c9b37969
Sergio González <sergio.gonzalez@liferay.com>
2012-05-04 15:13:18 +0200

    LPS-27010

---
+++ portal-impl/src/com/liferay/portlet/trash/util/TrashUtil.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.trash.util;
+
+import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portlet.trash.util.comparator.EntryDeletedByUserNameComparator;
+import com.liferay.portlet.trash.util.comparator.EntryDeletedDateComparator;
+import com.liferay.portlet.trash.util.comparator.EntryTypeComparator;
+
+/**
+ * @author Sergio González
+ */
+public class TrashUtil {
+
+	public static OrderByComparator getEntryOrderByComparator(
+		String orderByCol, String orderByType) {
+
+		boolean orderByAsc = false;
+
+		if (orderByType.equals("asc")) {
+			orderByAsc = true;
+		}
+
+		OrderByComparator orderByComparator = null;
+
+		if (orderByCol.equals("removed-by")) {
+			orderByComparator =
+				new EntryDeletedByUserNameComparator(orderByAsc);
+		}
+		else if (orderByCol.equals("removed-date")) {
+			orderByComparator = new EntryDeletedDateComparator(orderByAsc);
+		}
+		else if (orderByCol.equals("type")) {
+			orderByComparator = new EntryTypeComparator(orderByAsc);
+		}
+
+		return orderByComparator;
+	}
+
+}

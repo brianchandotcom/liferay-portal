8330a61d35080e3d870809a11c45588b4fd7b898
Hugo Huijser <hugo.huijser@liferay.com>
2012-05-24 12:35:01 -0700

    SourceFormatter - rename

--- portal-impl/src/com/liferay/portal/tools/SourceFormatter.java
+++ portal-impl/src/com/liferay/portal/tools/SourceFormatter.java
@@ -1390,7 +1390,7 @@ public class SourceFormatter {
 						}
 
 						if (_isInJavaTermTypeGroup(
-								javaTermType, _TYPE_VARIABLE_NOT_STATIC)) {
+								javaTermType, _TYPE_VARIABLE_NOT_FINAL)) {
 
 							char firstChar = javaTermName.charAt(0);
 
@@ -3468,7 +3468,7 @@ public class SourceFormatter {
 
 	private static final int _TYPE_METHOD_PUBLIC_STATIC = 4;
 
-	private static final int[] _TYPE_VARIABLE_NOT_STATIC = {
+	private static final int[] _TYPE_VARIABLE_NOT_FINAL = {
 		SourceFormatter._TYPE_VARIABLE_PRIVATE,
 		SourceFormatter._TYPE_VARIABLE_PRIVATE_STATIC,
 		SourceFormatter._TYPE_VARIABLE_PROTECTED,

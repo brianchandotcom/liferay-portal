9a423c4a6adbf4d88084a7a3ed162ec16fd9eae9
Hugo Huijser <hugo.huijser@liferay.com>
2012-05-25 12:21:26 -0700

    LPS-27407 Source formatting

--- portal-impl/src/com/liferay/portal/security/auth/LDAPAuth.java
+++ portal-impl/src/com/liferay/portal/security/auth/LDAPAuth.java
@@ -443,8 +443,10 @@ public class LDAPAuth implements Authenticator {
 			return SUCCESS;
 		}
 
-		if (PrefsPropsUtil.getBoolean(companyId, PropsKeys.LDAP_AUTH_REQUIRED) || 
-				!PrefsPropsUtil.getBoolean(companyId, PropsKeys.LDAP_IMPORT_USER_PASSWORD_DEFAULT)) {
+		if (PrefsPropsUtil.getBoolean(
+				companyId, PropsKeys.LDAP_AUTH_REQUIRED) ||
+			!PrefsPropsUtil.getBoolean(
+				companyId, PropsKeys.LDAP_IMPORT_USER_PASSWORD_DEFAULT)) {
 
 			return failureCode;
 		}

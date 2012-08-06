c157f1fe3df3bff79a4071defc67412f7a3b0cbb
Julio Camarero <julio.camarero@liferay.com>
2012-08-06 16:53:08 +0200

    LPS-29025 Integration Tests

--- portal-impl/test/integration/com/liferay/portal/staging/StagingImplTest.java
+++ portal-impl/test/integration/com/liferay/portal/staging/StagingImplTest.java
@@ -17,9 +17,6 @@ package com.liferay.portal.staging;
 import com.liferay.portal.kernel.lar.PortletDataHandlerKeys;
 import com.liferay.portal.kernel.staging.StagingConstants;
 import com.liferay.portal.kernel.staging.StagingUtil;
-import com.liferay.portal.kernel.util.LocaleUtil;
-import com.liferay.portal.kernel.util.StringBundler;
-import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.service.LayoutLocalServiceUtil;
 import com.liferay.portal.service.ServiceContext;
@@ -31,16 +28,10 @@ import com.liferay.portal.test.TransactionalExecutionTestListener;
 import com.liferay.portal.util.PortletKeys;
 import com.liferay.portal.util.TestPropsValues;
 import com.liferay.portlet.journal.model.JournalArticle;
-import com.liferay.portlet.journal.model.JournalFolderConstants;
 import com.liferay.portlet.journal.service.JournalArticleLocalServiceUtil;
-import com.liferay.portlet.polls.model.PollsChoice;
 import com.liferay.portlet.polls.model.PollsQuestion;
 import com.liferay.portlet.polls.service.PollsQuestionLocalServiceUtil;
-import com.liferay.portlet.polls.service.persistence.PollsChoiceUtil;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
@@ -57,7 +48,7 @@ import org.junit.runner.RunWith;
 	TransactionalExecutionTestListener.class
 })
 @RunWith(LiferayIntegrationJUnitTestRunner.class)
-public class StagingImplTest {
+public class StagingImplTest extends BaseStagingTestCase {
 
 	@Test
 	public void testLocalStagingJournal() throws Exception {
@@ -67,88 +58,6 @@ public class StagingImplTest {
 	@Test
 	public void testLocalStagingPolls() throws Exception {
 		enableLocalStaging(false, true);
-	}
-
-	protected JournalArticle addJournalArticle(
-			long groupId, String name, String content)
-		throws Exception {
-
-		Map<Locale, String> titleMap = new HashMap<Locale, String>();
-
-		for (Locale locale : _locales) {
-			titleMap.put(locale, name.concat(LocaleUtil.toLanguageId(locale)));
-		}
-
-		Map<Locale, String> descriptionMap = new HashMap<Locale, String>();
-
-		ServiceContext serviceContext = ServiceTestUtil.getServiceContext();
-
-		StringBundler sb = new StringBundler(3 + 6 * _locales.length);
-
-		sb.append("<?xml version=\"1.0\"?><root available-locales=");
-		sb.append("\"en_US,es_ES,de_DE\" default-locale=\"en_US\">");
-
-		for (Locale locale : _locales) {
-			sb.append("<static-content language-id=\"");
-			sb.append(LocaleUtil.toLanguageId(locale));
-			sb.append("\"><![CDATA[<p>");
-			sb.append(content);
-			sb.append(LocaleUtil.toLanguageId(locale));
-			sb.append("</p>]]></static-content>");
-		}
-
-		sb.append("</root>");
-
-		return JournalArticleLocalServiceUtil.addArticle(
-			TestPropsValues.getUserId(), groupId,
-			JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID, 0, 0,
-			StringPool.BLANK, true, 1, titleMap, descriptionMap, sb.toString(),
-			"general", null, null, null, 1, 1, 1965, 0, 0, 0, 0, 0, 0, 0, true,
-			0, 0, 0, 0, 0, true, false, false, null, null, null, null,
-			serviceContext);
-	}
-
-	protected PollsChoice addPollsChoice(String name, String description) {
-		Map<Locale, String> descriptionMap = new HashMap<Locale, String>();
-
-		for (Locale locale : _locales) {
-			descriptionMap.put(
-				locale, description.concat(LocaleUtil.toLanguageId(locale)));
-		}
-
-		PollsChoice pollsChoice = PollsChoiceUtil.create(0);
-
-		pollsChoice.setName(name);
-		pollsChoice.setDescriptionMap(descriptionMap);
-
-		return pollsChoice;
-	}
-
-	protected PollsQuestion addPollsQuestion(
-			long groupId, String title, String description)
-		throws Exception {
-
-		Map<Locale, String> titleMap = new HashMap<Locale, String>();
-		Map<Locale, String> descriptionMap = new HashMap<Locale, String>();
-
-		for (Locale locale : _locales) {
-			titleMap.put(locale, title.concat(LocaleUtil.toLanguageId(locale)));
-			descriptionMap.put(
-				locale, description.concat(LocaleUtil.toLanguageId(locale)));
-		}
-
-		List<PollsChoice> pollsChoices = new ArrayList<PollsChoice>();
-
-		pollsChoices.add(addPollsChoice("optionA", "descriptionA"));
-		pollsChoices.add(addPollsChoice("optionB", "descriptionB"));
-
-		ServiceContext serviceContext = ServiceTestUtil.getServiceContext();
-
-		serviceContext.setScopeGroupId(groupId);
-
-		return PollsQuestionLocalServiceUtil.addQuestion(
-			TestPropsValues.getUserId(), titleMap, descriptionMap, 0, 0, 0, 0,
-			0, true, pollsChoices, serviceContext);
 	}
 
 	protected void enableLocalStaging(boolean stageJournal, boolean stagePolls)
@@ -165,7 +74,7 @@ public class StagingImplTest {
 		// Create content
 
 		JournalArticle journalArticle = addJournalArticle(
-			group.getGroupId(), "Title", "content");
+			group.getGroupId(), "Title", "content", "en_US");
 
 		PollsQuestion pollsQuestion = addPollsQuestion(
 			group.getGroupId(), "Question", "Description");
@@ -276,48 +185,5 @@ public class StagingImplTest {
 			}
 		}
 	}
-
-	protected JournalArticle updateJournalArticle(
-			JournalArticle journalArticle, String name, String content)
-		throws Exception {
-
-		Map<Locale, String> titleMap = new HashMap<Locale, String>();
-
-		for (Locale locale : _locales) {
-			titleMap.put(locale, name.concat(LocaleUtil.toLanguageId(locale)));
-		}
-
-		return JournalArticleLocalServiceUtil.updateArticle(
-			journalArticle.getUserId(), journalArticle.getGroupId(),
-			journalArticle.getFolderId(), journalArticle.getArticleId(),
-			journalArticle.getVersion(), titleMap,
-			journalArticle.getDescriptionMap(), content,
-			journalArticle.getLayoutUuid(),
-			ServiceTestUtil.getServiceContext());
-	}
-
-	protected PollsQuestion updatePollsQuestion(
-			PollsQuestion pollsQuestion, String title, String description)
-		throws Exception {
-
-		Map<Locale, String> titleMap = new HashMap<Locale, String>();
-		Map<Locale, String> descriptionMap = new HashMap<Locale, String>();
-
-		for (Locale locale : _locales) {
-			titleMap.put(locale, title.concat(LocaleUtil.toLanguageId(locale)));
-			descriptionMap.put(
-				locale, description.concat(LocaleUtil.toLanguageId(locale)));
-		}
-
-		return PollsQuestionLocalServiceUtil.updateQuestion(
-			pollsQuestion.getUserId(), pollsQuestion.getQuestionId(), titleMap,
-			descriptionMap, 0, 0, 0, 0, 0, true, pollsQuestion.getChoices(),
-			ServiceTestUtil.getServiceContext());
-	}
-
-	private static Locale[] _locales = {
-		new Locale("en", "US"), new Locale("es", "ES"),
-		new Locale("de", "DE")
-	};
 
 }

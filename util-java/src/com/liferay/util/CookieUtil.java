82b8c3974375a9647a21eab7c3abac799b9a499e
Julio Camarero <julio.camarero@liferay.com>
2012-10-04 17:14:22 -0700

    LPS-30192 Moving method from portal-impl to class in portal-service. Deprecating class in portal-util to the one in portal-service

--- util-java/src/com/liferay/util/CookieUtil.java
+++ util-java/src/com/liferay/util/CookieUtil.java
@@ -14,18 +14,15 @@
 
 package com.liferay.util;
 
-import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.CookieKeys;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Brian Wing Shun Chan
  * @author Shuyang Zhou
+ * @deprecated {@link com.liferay.portal.kernel.util.CookieKeys}
+
  */
 public class CookieUtil {
 
@@ -36,53 +33,7 @@ public class CookieUtil {
 	public static String get(
 		HttpServletRequest request, String name, boolean toUpperCase) {
 
-		Map<String, Cookie> cookieMap = _getCookieMap(request);
-
-		if (toUpperCase) {
-			name = name.toUpperCase();
-		}
-
-		Cookie cookie = cookieMap.get(name);
-
-		if (cookie == null) {
-			return null;
-		}
-		else {
-			return cookie.getValue();
-		}
-	}
-
-	private static Map<String, Cookie> _getCookieMap(
-		HttpServletRequest request) {
-
-		Map<String, Cookie> cookieMap =
-			(Map<String, Cookie>)request.getAttribute(
-				CookieUtil.class.getName());
-
-		if (cookieMap != null) {
-			return cookieMap;
-		}
-
-		Cookie[] cookies = request.getCookies();
-
-		if (cookies == null) {
-			cookieMap = Collections.emptyMap();
-		}
-		else {
-			cookieMap = new HashMap<String, Cookie>(cookies.length * 4 / 3);
-
-			for (Cookie cookie : cookies) {
-				String cookieName = GetterUtil.getString(cookie.getName());
-
-				cookieName = cookieName.toUpperCase();
-
-				cookieMap.put(cookieName, cookie);
-			}
-		}
-
-		request.setAttribute(CookieUtil.class.getName(), cookieMap);
-
-		return cookieMap;
+		return CookieKeys.get(request, name, toUpperCase);
 	}
 
 }

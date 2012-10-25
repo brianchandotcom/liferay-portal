07b92d183c3cfe1d3897a45dc94f14e3651ca605
shuyangzhou <shuyang.zhou@liferay.com>
2012-10-25 15:21:02 +0800

    LPS-30234 IntraBand Core

---
+++ portal-service/src/com/liferay/portal/kernel/nio/intraband/nonblocking/SelectionKeyRegistrationReference.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.nio.intraband.nonblocking;
+
+import com.liferay.portal.kernel.nio.intraband.IntraBand;
+import com.liferay.portal.kernel.nio.intraband.RegistrationReference;
+
+import java.io.IOException;
+
+import java.nio.channels.SelectionKey;
+
+/**
+ * @author Shuyang Zhou
+ */
+class SelectionKeyRegistrationReference implements RegistrationReference {
+
+	public void cancelRegistration() {
+		readSelectionKey.cancel();
+		writeSelectionKey.cancel();
+	}
+
+	public void closeChannels() throws IOException {
+		writeSelectionKey.channel().close();
+		readSelectionKey.channel().close();
+	}
+
+	public IntraBand getIntraBand() {
+		return intraBand;
+	}
+
+	public boolean isValid() {
+		return writeSelectionKey.isValid();
+	}
+
+	// Package level
+
+	SelectionKeyRegistrationReference(
+		IntraBand intraBand, SelectionKey readSelectionKey,
+		SelectionKey writeSelectionKey) {
+
+		this.intraBand = intraBand;
+		this.readSelectionKey = readSelectionKey;
+		this.writeSelectionKey = writeSelectionKey;
+	}
+
+	final IntraBand intraBand;
+	final SelectionKey readSelectionKey;
+	final SelectionKey writeSelectionKey;
+
+}

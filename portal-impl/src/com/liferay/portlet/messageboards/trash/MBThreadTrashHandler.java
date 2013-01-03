425e6d48d2b130b1876f3b084536ec65580ddd13
Eduardo Garcia <eduardo.garcia@liferay.com>
2013-01-03 12:11:04 +0100

    LPS-32060 Cannot restore a Thread from a trashed Category

--- portal-impl/src/com/liferay/portlet/messageboards/trash/MBThreadTrashHandler.java
+++ portal-impl/src/com/liferay/portlet/messageboards/trash/MBThreadTrashHandler.java
@@ -67,6 +67,11 @@ public class MBThreadTrashHandler extends BaseTrashHandler {
 	}
 
 	@Override
+	public String getContainerModelClassName() {
+		return MBCategory.class.getName();
+	}
+
+	@Override
 	public List<ContainerModel> getContainerModels(
 			long classPK, long parentContainerModelId, int start, int end)
 		throws PortalException, SystemException {

7b0f15950b0f32940d564491eb9c1d0992203813
James Lefeu <James.Lefeu@liferay.com>
2013-01-04 17:02:45 -0600

    LPS-31315 Apply changed method names

--- portal-impl/src/com/liferay/portal/staging/StagingImpl.java
+++ portal-impl/src/com/liferay/portal/staging/StagingImpl.java
@@ -178,8 +178,10 @@ public class StagingImpl implements Staging {
 		Group stagingGroup = targetLayout.getGroup();
 		Group liveGroup = stagingGroup.getLiveGroup();
 
-		Layout sourceLayout = LayoutLocalServiceUtil.getLayoutByUuidAndGroupId(
-			targetLayout.getUuid(), liveGroup.getGroupId());
+		Layout sourceLayout =
+			LayoutLocalServiceUtil.getLayoutByUuidAndGroupId(
+				targetLayout.getUuid(), liveGroup.getGroupId(),
+				targetLayout.isPrivateLayout());
 
 		copyPortlet(
 			portletRequest, liveGroup.getGroupId(), stagingGroup.getGroupId(),
@@ -626,7 +628,8 @@ public class StagingImpl implements Staging {
 
 			try {
 				LayoutLocalServiceUtil.getLayoutByUuidAndGroupId(
-					parentLayout.getUuid(), liveGroupId);
+					parentLayout.getUuid(), liveGroupId,
+					parentLayout.isPrivateLayout());
 
 				// If one parent is found all others are assumed to exist
 
@@ -1090,8 +1093,10 @@ public class StagingImpl implements Staging {
 			stagingGroup = sourceLayout.getGroup();
 			liveGroup = stagingGroup.getLiveGroup();
 
-			targetLayout = LayoutLocalServiceUtil.getLayoutByUuidAndGroupId(
-				sourceLayout.getUuid(), liveGroup.getGroupId());
+			targetLayout =
+				LayoutLocalServiceUtil.getLayoutByUuidAndGroupId(
+					sourceLayout.getUuid(), liveGroup.getGroupId(),
+					sourceLayout.isPrivateLayout());
 		}
 
 		copyPortlet(

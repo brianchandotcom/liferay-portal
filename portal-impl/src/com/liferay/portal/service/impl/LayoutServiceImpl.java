90c5652c525ff05615d4a0e4c865e2e53e178d68
Jim Hinkey <james.hinkey@liferay.com>
2013-01-08 19:14:41 -0500

    LPS-31815 re-add Javadoc

--- portal-impl/src/com/liferay/portal/service/impl/LayoutServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/LayoutServiceImpl.java
@@ -519,6 +519,18 @@ public class LayoutServiceImpl extends LayoutServiceBaseImpl {
 		return LayoutConstants.DEFAULT_PLID;
 	}
 
+	/**
+	 * Returns the layout matching the universally unique identifier (UUID) and
+	 * group.
+	 *
+	 * @param  uuid the universally unique identifier of the scope layout
+	 * @param  groupId the primary key of the group
+	 * @return the matching layout
+	 * @throws PortalException if a matching layout could not be found, if the
+	 *         user did not have permission to view the layout, or if some other
+	 *         portal exception occurred
+	 * @throws SystemException if a system exception occurred
+	 */
 	public Layout getLayoutByUuidAndGroupId(String uuid, long groupId)
 		throws PortalException, SystemException {
 

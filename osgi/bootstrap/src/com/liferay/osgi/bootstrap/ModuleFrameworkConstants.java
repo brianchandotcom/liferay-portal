brandonyee333/test

LPS-32743 Some constants required for the module framework. Some of them are not used right now but they will be used in the next modules I will push shortly

osgi/bootstrap/src/com/liferay/osgi/bootstrap/ModuleFrameworkConstants.java
@@ -18,6 +18,39 @@ package com.liferay.osgi.bootstrap;
  * @author Raymond Augé
  */
 public interface ModuleFrameworkConstants {
+	public static enum Action {
+		ADDING, REMOVED, MODIFIED
+	}
+
+	public static final String ALIAS = "alias";
+
+	public static final String BUNDLE = "bundle";
+
+	public static final String BUNDLE_ID = "bundle.id";
+
+	public static final String BUNDLE_SYMBOLICNAME = "bundle.symbolicName";
+
+	public static final String BUNDLE_VERSION = "bundle.version";
+
+	public static final String COLLISION = "collision";
+
+	public static final String COLLISION_BUNDLES = "collision.bundles";
+
+	public static final String CONTEXT_ID = "contextId";
+
+	public static final String CONTEXT_PATH = "context.path";
+
+	public static final String EXCEPTION = "exception";
+
+	public static final String EXTENDER_BUNDLE = "extender.bundle";
+
+	public static final String EXTENDER_BUNDLE_ID = "extender.bundle.id";
+
+	public static final String EXTENDER_BUNDLE_SYMBOLICNAME =
+		"extender.bundle.symbolicName";
+
+	public static final String EXTENDER_BUNDLE_VERSION =
+		"extender.bundle.version";
 
 	public static final String FELIX_FILEINSTALL_DIR = "felix.fileinstall.dir";
 
@@ -30,14 +63,39 @@ public interface ModuleFrameworkConstants {
 	public static final String FELIX_FILEINSTALL_TMPDIR =
 		"felix.fileinstall.tmpdir";
 
+	public static final String INIT_PREFIX = "init.";
+
+	public static final String INVOKER_PATH = "/invoke";
+
 	public static final String MANIFEST_PATH = "META-INF/MANIFEST.MF";
+
+	public static final String MODULE = "o";
+
+	public static final String NAME = "name";
+
+	public static final String OSGI_BUNDLE = "osgi-bundle";
+
+	public static final String OSGI_BUNDLECONTEXT = "osgi-bundlecontext";
+
+	public static final String PATTERN = "pattern";
 
 	public static final String SERVICE_PROPERTY_KEY_BEAN_ID = "bean.id";
 
 	public static final String SERVICE_PROPERTY_KEY_ORIGINAL_BEAN =
 		"original.bean";
 
+	public static final String SERVICE_PROPERTY_KEY_SERVICE_RANKING =
+		"service.ranking";
+
 	public static final String SERVICE_PROPERTY_KEY_SERVICE_VENDOR =
 		"service.vendor";
+
+	public static final String SERVLET_CONTEXT_NAME = "servlet.context.name";
+
+	public static final String SYSTEM_GENERATED_FRAGMENT_PREFIX = "sfg_";
+
+	public static final String TIMESTAMP = "timestamp";
+
+	public static final String WEB_CONTEXTPATH = "Web-ContextPath";
 
 }

brianchandotcom/liferay-portal

osgi/bootstrap/src/com/liferay/osgi/bootstrap/ModuleFrameworkConstants.java
@@ -18,6 +18,24 @@ package com.liferay.osgi.bootstrap;
  * @author Raymond Augé
  */
 public interface ModuleFrameworkConstants {
+	public static enum Action {
+		ADDING, REMOVED, MODIFIED
+	}
+
+	public static final String EXTENDER_BUNDLE = "extender.bundle";
+
+	public static final String EXTENDER_BUNDLE_ID = "extender.bundle.id";
+
+	public static final String EXTENDER_BUNDLE_SYMBOLICNAME =
+		"extender.bundle.symbolicName";
+
+	public static final String EXTENDER_BUNDLE_VERSION =
+		"extender.bundle.version";
+
+	public static final String EXTENDER_KEY_MANIFEST_PATH =
+		"META-INF/MANIFEST.MF";
+
+	public static final String EXTNDER_KEY_MODULE = "o";
 
 	public static final String FELIX_FILEINSTALL_DIR = "felix.fileinstall.dir";
 
@@ -30,14 +48,60 @@ public interface ModuleFrameworkConstants {
 	public static final String FELIX_FILEINSTALL_TMPDIR =
 		"felix.fileinstall.tmpdir";
 
-	public static final String MANIFEST_PATH = "META-INF/MANIFEST.MF";
+	public static final String HTTP_SERVICE_KEY_ALIAS = "alias";
 
-	public static final String SERVICE_PROPERTY_KEY_BEAN_ID = "bean.id";
+	public static final String HTTP_SERVICE_KEY_BEAN_ID = "bean.id";
 
-	public static final String SERVICE_PROPERTY_KEY_ORIGINAL_BEAN =
-		"original.bean";
+	public static final String HTTP_SERVICE_KEY_BUNDLE = "bundle";
 
-	public static final String SERVICE_PROPERTY_KEY_SERVICE_VENDOR =
+	public static final String HTTP_SERVICE_KEY_BUNDLE_ID = "bundle.id";
+
+	public static final String HTTP_SERVICE_KEY_BUNDLE_SYMBOLICNAME =
+		"bundle.symbolicName";
+
+	public static final String HTTP_SERVICE_KEY_BUNDLE_VERSION =
+		"bundle.version";
+
+	public static final String HTTP_SERVICE_KEY_COLLISION = "collision";
+
+	public static final String HTTP_SERVICE_KEY_COLLISION_BUNDLES =
+		"collision.bundles";
+
+	public static final String HTTP_SERVICE_KEY_CONTEXT_ID = "contextId";
+
+	public static final String HTTP_SERVICE_KEY_CONTEXT_PATH = "context.path";
+
+	public static final String HTTP_SERVICE_KEY_EXCEPTION = "exception";
+
+	public static final String HTTP_SERVICE_KEY_INIT_PREFIX = "init.";
+
+	public static final String HTTP_SERVICE_KEY_INVOKER_PATH = "/invoke";
+
+	public static final String HTTP_SERVICE_KEY_NAME = "name";
+
+	public static final String HTTP_SERVICE_KEY_ORIGINAL_BEAN = "original.bean";
+
+	public static final String HTTP_SERVICE_KEY_OSGI_BUNDLE = "osgi-bundle";
+
+	public static final String HTTP_SERVICE_KEY_OSGI_BUNDLECONTEXT =
+		"osgi-bundlecontext";
+
+	public static final String HTTP_SERVICE_KEY_PATTERN = "pattern";
+
+	public static final String HTTP_SERVICE_KEY_SERVICE_RANKING =
+		"service.ranking";
+
+	public static final String HTTP_SERVICE_KEY_SERVICE_VENDOR =
 		"service.vendor";
+
+	public static final String HTTP_SERVICE_KEY_SERVLET_CONTEXT_NAME =
+		"servlet.context.name";
+
+	public static final String HTTP_SERVICE_KEY_TIMESTAMP = "timestamp";
+
+	public static final String HTTP_SERVICE_KEY_WEB_CONTEXTPATH =
+		"Web-ContextPath";
+
+	public static final String SYSTEM_GENERATED_FRAGMENT_PREFIX = "sfg_";
 
 }
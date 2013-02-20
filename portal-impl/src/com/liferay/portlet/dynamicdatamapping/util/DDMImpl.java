brianchandotcom/liferay-portal

commit e4614c8fb43e44e21efd4897c55d934865e8b8f7

LPS-17862 Updating DDMUtil API to accept localized content

portal-impl/src/com/liferay/portlet/dynamicdatamapping/util/DDMImpl.java
@@ -22,14 +22,17 @@ import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.servlet.ServletResponseUtil;
 import com.liferay.portal.kernel.upload.UploadRequest;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.MimeTypesUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.StreamUtil;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.model.BaseModel;
 import com.liferay.portal.model.CompanyConstants;
 import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portal.util.PortalUtil;
 import com.liferay.portlet.documentlibrary.DuplicateDirectoryException;
 import com.liferay.portlet.documentlibrary.DuplicateFileException;
@@ -59,7 +62,9 @@ import java.io.Serializable;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
@@ -97,6 +102,17 @@ public class DDMImpl implements DDM {
 			ServiceContext serviceContext)
 		throws PortalException, SystemException {
 
+		String defaultLanguageId = GetterUtil.getString(
+			serviceContext.getAttribute("defaultLanguageId"));
+
+		Locale defaultLocale = LocaleUtil.fromLanguageId(defaultLanguageId);
+
+		String languageId = GetterUtil.getString(
+			serviceContext.getAttribute("languageId"),
+			serviceContext.getLanguageId());
+
+		Locale locale = LocaleUtil.fromLanguageId(languageId);
+
 		DDMStructure ddmStructure = getDDMStructure(
 			ddmStructureId, ddmTemplateId);
 
@@ -116,7 +132,10 @@ public class DDMImpl implements DDM {
 				continue;
 			}
 
-			Field field = new Field(ddmStructureId, fieldName, fieldValues);
+			Field field = new Field(
+				ddmStructureId, fieldName, fieldValues, locale);
+
+			field.setDefaultLocale(defaultLocale);
 
 			fields.put(field);
 		}
@@ -223,6 +242,30 @@ public class DDMImpl implements DDM {
 		return orderByComparator;
 	}
 
+	public Fields mergeFields(Fields newFields, Fields existingFields) {
+		Iterator<Field> it = newFields.iterator();
+
+		while (it.hasNext()) {
+			Field newField = it.next();
+
+			Field existingField = existingFields.get(newField.getName());
+
+			if (existingField == null) {
+				existingFields.put(newField);
+			}
+			else {
+				for (Locale locale : newField.getAvailableLocales()) {
+					existingField.setValues(locale, newField.getValues(locale));
+				}
+
+				existingField.setDefaultLocale(newField.getDefaultLocale());
+			}
+
+		}
+
+		return existingFields;
+	}
+
 	public void sendFieldFile(
 			HttpServletRequest request, HttpServletResponse response,
 			Field field, int valueIndex)
@@ -232,9 +275,13 @@ public class DDMImpl implements DDM {
 			return;
 		}
 
+		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
 		DDMStructure structure = field.getDDMStructure();
 
-		Serializable fieldValue = field.getValue(valueIndex);
+		Serializable fieldValue = field.getValue(
+			themeDisplay.getLocale(), valueIndex);
 
 		JSONObject fileJSONObject = JSONFactoryUtil.createJSONObject(
 			String.valueOf(fieldValue));
@@ -320,7 +367,8 @@ public class DDMImpl implements DDM {
 			}
 		}
 
-		Field field = new Field(structureId, fieldName, fieldValues);
+		Field field = new Field(
+			structureId, fieldName, fieldValues, serviceContext.getLocale());
 
 		fields.put(field);
 

portal-service/src/com/liferay/portlet/dynamicdatamapping/util/DDM.java
@@ -56,6 +56,8 @@ public interface DDM {
 	public OrderByComparator getTemplateOrderByComparator(
 		String orderByCol, String orderByType);
 
+	public Fields mergeFields(Fields newFields, Fields existingFields);
+
 	public void sendFieldFile(
 			HttpServletRequest request, HttpServletResponse response,
 			Field field, int valueIndex)

portal-service/src/com/liferay/portlet/dynamicdatamapping/util/DDMUtil.java
@@ -87,6 +87,10 @@ public class DDMUtil {
 		return getDDM().getTemplateOrderByComparator(orderByCol, orderByType);
 	}
 
+	public static Fields mergeFields(Fields newFields, Fields existingFields) {
+		return getDDM().mergeFields(newFields, existingFields);
+	}
+
 	public static void sendFieldFile(
 			HttpServletRequest request, HttpServletResponse response,
 			Field field, int valueIndex)

commit dc9ddc5dd15c11ed578d0154b0268ed294acd35a

LPS-32983 Prevent NPE when a file is being downloaded from a DDL record (caused by e4614c8f); The locale should be retrieved from the user locale;

portal-impl/src/com/liferay/portlet/dynamicdatamapping/util/DDMImpl.java
@@ -32,11 +32,9 @@ import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.UnicodeFormatter;
 import com.liferay.portal.kernel.util.Validator;
-import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.model.BaseModel;
 import com.liferay.portal.model.CompanyConstants;
 import com.liferay.portal.service.ServiceContext;
-import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portal.util.PortalUtil;
 import com.liferay.portal.util.PropsValues;
 import com.liferay.portlet.documentlibrary.model.DLFileEntryMetadata;
@@ -316,13 +314,11 @@ public class DDMImpl implements DDM {
 			return;
 		}
 
-		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
-			WebKeys.THEME_DISPLAY);
+		Locale locale = PortalUtil.getLocale(request);
 
 		DDMStructure structure = field.getDDMStructure();
 
-		Serializable fieldValue = field.getValue(
-			themeDisplay.getLocale(), valueIndex);
+		Serializable fieldValue = field.getValue(locale, valueIndex);
 
 		JSONObject fileJSONObject = JSONFactoryUtil.createJSONObject(
 			String.valueOf(fieldValue));
@@ -568,7 +564,7 @@ public class DDMImpl implements DDM {
 		String dirName = getFileUploadPath(baseModel);
 
 		if (!DLStoreUtil.hasDirectory(
-			companyId, CompanyConstants.SYSTEM, dirName)) {
+				companyId, CompanyConstants.SYSTEM, dirName)) {
 
 			DLStoreUtil.addDirectory(
 				companyId, CompanyConstants.SYSTEM, dirName);
@@ -579,8 +575,8 @@ public class DDMImpl implements DDM {
 		String fileName = dirName + StringPool.SLASH + fieldName;
 
 		if (DLStoreUtil.hasFile(
-			companyId, CompanyConstants.SYSTEM, fileName,
-			Store.VERSION_DEFAULT)) {
+				companyId, CompanyConstants.SYSTEM, fileName,
+				Store.VERSION_DEFAULT)) {
 
 			DLStoreUtil.deleteFile(
 				companyId, CompanyConstants.SYSTEM, fileName,
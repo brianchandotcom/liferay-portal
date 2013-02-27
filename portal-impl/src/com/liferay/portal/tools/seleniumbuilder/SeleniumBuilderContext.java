7163b3c815071e3ad4ef7170f896a51666044b38
Michael Hashimoto <michael.hashimoto@liferay.com>
2013-02-26 16:57:22 -0800

    LPS-32400 - Add params for functions to a HashMap

--- portal-impl/src/com/liferay/portal/tools/seleniumbuilder/SeleniumBuilderContext.java
+++ portal-impl/src/com/liferay/portal/tools/seleniumbuilder/SeleniumBuilderContext.java
@@ -79,6 +79,8 @@ public class SeleniumBuilderContext {
 
 				Element rootElement = _getRootElement(fileName);
 
+				_functionParams.put(functionName, _getParams(rootElement));
+
 				_functionReturnTypes.put(
 					functionName, _getReturnType(rootElement));
 
@@ -210,6 +212,10 @@ public class SeleniumBuilderContext {
 
 	public Set<String> getFunctionNames() {
 		return _functionNames;
+	}
+
+	public Integer getFunctionParam(String functionName) {
+		return _functionParams.get(functionName);
 	}
 
 	public String getFunctionReturnType(String functionName) {
@@ -348,6 +354,10 @@ public class SeleniumBuilderContext {
 		return _seleniumBuilderFileUtil.getName(fileName);
 	}
 
+	private int _getParams(Element rootElement) throws Exception {
+		return _seleniumBuilderFileUtil.getParams(rootElement);
+	}
+
 	private String _getReturnType(Element rootElement) throws Exception {
 		return _seleniumBuilderFileUtil.getReturnType(rootElement);
 	}
@@ -373,6 +383,8 @@ public class SeleniumBuilderContext {
 	private Map<String, String> _functionFileNames =
 		new HashMap<String, String>();
 	private Set<String> _functionNames = new HashSet<String>();
+	private Map<String, Integer> _functionParams =
+		new HashMap<String, Integer>();
 	private Map<String, String> _functionReturnTypes =
 		new HashMap<String, String>();
 	private Map<String, Element> _functionRootElements =

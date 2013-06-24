f76b6583e8cf15cc14d5ec9ba1319f8436cdd5f0
Mika Koivisto <mika.koivisto@liferay.com>
2013-06-24 11:39:10 -0700

    LPS-35742 build-service

--- portal-service/src/com/liferay/portal/service/GroupLocalServiceUtil.java
+++ portal-service/src/com/liferay/portal/service/GroupLocalServiceUtil.java
@@ -1185,6 +1185,23 @@ public class GroupLocalServiceUtil {
 	}
 
 	/**
+	* Returns all the groups that are direct children of the parent group.
+	*
+	* @param companyId the primary key of the company
+	* @param parentGroupId the primary key of the parent group
+	* @param site whether the group is to be associated with a main site
+	* @param start the lower bound of the range of results
+	* @param end the upper bound of the range of results (not inclusive)
+	* @return the range of matching groups
+	* @throws SystemException if a system exception occurred
+	*/
+	public static java.util.List<com.liferay.portal.model.Group> getGroups(
+		long companyId, long parentGroupId, boolean site, int start, int end)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return getService().getGroups(companyId, parentGroupId, site, start, end);
+	}
+
+	/**
 	* Returns all the groups that are direct children of the parent group with
 	* the matching className.
 	*
--- portal-service/src/com/liferay/portal/service/GroupLocalServiceWrapper.java
+++ portal-service/src/com/liferay/portal/service/GroupLocalServiceWrapper.java
@@ -1267,6 +1267,25 @@ public class GroupLocalServiceWrapper implements GroupLocalService,
 	}
 
 	/**
+	* Returns all the groups that are direct children of the parent group.
+	*
+	* @param companyId the primary key of the company
+	* @param parentGroupId the primary key of the parent group
+	* @param site whether the group is to be associated with a main site
+	* @param start the lower bound of the range of results
+	* @param end the upper bound of the range of results (not inclusive)
+	* @return the range of matching groups
+	* @throws SystemException if a system exception occurred
+	*/
+	@Override
+	public java.util.List<com.liferay.portal.model.Group> getGroups(
+		long companyId, long parentGroupId, boolean site, int start, int end)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return _groupLocalService.getGroups(companyId, parentGroupId, site,
+			start, end);
+	}
+
+	/**
 	* Returns all the groups that are direct children of the parent group with
 	* the matching className.
 	*

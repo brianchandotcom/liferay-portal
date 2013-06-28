e2ab2b09661f1297435ccc5c083954705bafcf57
Zsolt Berentey <zsolt.berentey@liferay.com>
2013-06-28 11:15:39 +0200

    LPS-36407 Generated

--- portal-service/src/com/liferay/portlet/polls/model/PollsQuestionWrapper.java
+++ portal-service/src/com/liferay/portlet/polls/model/PollsQuestionWrapper.java
@@ -14,6 +14,7 @@
 
 package com.liferay.portlet.polls.model;
 
+import com.liferay.portal.kernel.lar.StagedModelType;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.model.ModelWrapper;
 
@@ -831,6 +832,11 @@ public class PollsQuestionWrapper implements PollsQuestion,
 		}
 
 		return false;
+	}
+
+	@Override
+	public StagedModelType getStagedModelType() {
+		return _pollsQuestion.getStagedModelType();
 	}
 
 	/**

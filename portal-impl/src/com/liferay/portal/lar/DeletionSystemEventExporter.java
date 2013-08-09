d586529aff317d7be804e9a5bc80a6e6e975c043
Zsolt Berentey <zsolt.berentey@liferay.com>
2013-08-09 10:36:48 +0200

    LPS-36216 Force creation order for exporting system events

--- portal-impl/src/com/liferay/portal/lar/DeletionSystemEventExporter.java
+++ portal-impl/src/com/liferay/portal/lar/DeletionSystemEventExporter.java
@@ -18,6 +18,7 @@ import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
 import com.liferay.portal.kernel.dao.orm.Conjunction;
 import com.liferay.portal.kernel.dao.orm.Disjunction;
 import com.liferay.portal.kernel.dao.orm.DynamicQuery;
+import com.liferay.portal.kernel.dao.orm.OrderFactoryUtil;
 import com.liferay.portal.kernel.dao.orm.Property;
 import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
 import com.liferay.portal.kernel.dao.orm.RestrictionsFactoryUtil;
@@ -130,6 +131,8 @@ public class DeletionSystemEventExporter {
 					typeProperty.eq(SystemEventConstants.TYPE_DELETE));
 
 				_addCreateDateProperty(dynamicQuery);
+
+				dynamicQuery.addOrder(OrderFactoryUtil.asc("createDate"));
 			}
 
 			@Override

875215e148950d1c567dfa711d02470c662f75ef
Hugo Huijser <hugo.huijser@liferay.com>
2013-10-22 09:08:08 +0800

    LPS-39724 Remove imports

--- portal-impl/src/com/liferay/portal/servlet/filters/cache/CacheFileHelper.java
+++ portal-impl/src/com/liferay/portal/servlet/filters/cache/CacheFileHelper.java
@@ -21,8 +21,6 @@ import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.portal.kernel.util.Validator;
-import com.liferay.portal.util.PortalUtil;
 
 import java.io.File;
 

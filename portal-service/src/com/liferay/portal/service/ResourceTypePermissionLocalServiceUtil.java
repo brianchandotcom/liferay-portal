bb8bf82f7779ed320980df0a42722f08490ab8b7
Roberto Díaz <roberto.diaz@liferay.com>
2013-10-24 10:41:17 +0200

    LPS-41495 Regen to include the javadoc

--- portal-service/src/com/liferay/portal/service/ResourceTypePermissionLocalServiceUtil.java
+++ portal-service/src/com/liferay/portal/service/ResourceTypePermissionLocalServiceUtil.java
@@ -168,7 +168,9 @@ public class ResourceTypePermissionLocalServiceUtil {
 	}
 
 	/**
-	* Returns the number of rows that match the dynamic query.
+	* Returns the number of rows that match the dynamic query. This method
+	* modifies the dynamic query so that reusing it to obtain results that
+	* are not a number could produce unexpected results.
 	*
 	* @param dynamicQuery the dynamic query
 	* @param projection the projection to apply to the query
--- portal-service/src/com/liferay/portlet/journal/service/JournalArticleResourceLocalServiceWrapper.java
+++ portal-service/src/com/liferay/portlet/journal/service/JournalArticleResourceLocalServiceWrapper.java
@@ -169,7 +169,9 @@ public class JournalArticleResourceLocalServiceWrapper
 	}
 
 	/**
-	* Returns the number of rows that match the dynamic query.
+	* Returns the number of rows that match the dynamic query. This method
+	* modifies the dynamic query so that reusing it to obtain results that
+	* are not a number could produce unexpected results.
 	*
 	* @param dynamicQuery the dynamic query
 	* @param projection the projection to apply to the query

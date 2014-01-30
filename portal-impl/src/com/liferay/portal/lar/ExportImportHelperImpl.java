d3fb582840122096afe32d5b4cc76a814e834fe6
Tina Tian <tina.tian@liferay.com>
2014-01-30 08:49:58 +0800

    LPS-43608 Apply usage of PortalUtil.getPortalServerPort/getPortalLocalPort

--- portal-impl/src/com/liferay/portal/lar/ExportImportHelperImpl.java
+++ portal-impl/src/com/liferay/portal/lar/ExportImportHelperImpl.java
@@ -1157,7 +1157,7 @@ public class ExportImportHelperImpl implements ExportImportHelper {
 		LayoutSet privateLayoutSet = group.getPrivateLayoutSet();
 		LayoutSet publicLayoutSet = group.getPublicLayoutSet();
 
-		int portalPort = PortalUtil.getPortalPort(false);
+		int portalPort = PortalUtil.getPortalServerPort(false);
 
 		if (portalPort != -1) {
 			if (Validator.isNotNull(company.getVirtualHostname())) {
@@ -1176,7 +1176,7 @@ public class ExportImportHelperImpl implements ExportImportHelper {
 			}
 		}
 
-		int securePortalPort = PortalUtil.getPortalPort(true);
+		int securePortalPort = PortalUtil.getPortalServerPort(true);
 
 		String companySecurePortalURL = StringPool.BLANK;
 		String privateLayoutSetSecurePortalURL = StringPool.BLANK;
@@ -1880,7 +1880,7 @@ public class ExportImportHelperImpl implements ExportImportHelper {
 
 		boolean secure = HttpUtil.isSecure(url);
 
-		int portalPort = PortalUtil.getPortalPort(secure);
+		int portalPort = PortalUtil.getPortalServerPort(secure);
 
 		if (portalPort == -1) {
 			return url;

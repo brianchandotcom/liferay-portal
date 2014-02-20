c95c3a273816c106995c067c4e4c2146f71cf9d5
Miguel Angel Pastor Olivar <miguel.pastor@liferay.com>
2014-02-20 15:01:43 +0100

    LPS-44431 Apply this new interface to the checkers used in the permissions service. Note this is only breaking the high coupling between the permissions service and external apps (like polls). This could serve as the basics for a huge refactor in the permissions system, but this is a completely different story. This commit include those beans following the *Permission model; next commit will include those beans which follow the Util and Impl pattern

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/permission/DLFolderPermission.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/permission/DLFolderPermission.java
@@ -20,6 +20,7 @@ import com.liferay.portal.kernel.repository.model.Folder;
 import com.liferay.portal.kernel.staging.permission.StagingPermissionUtil;
 import com.liferay.portal.security.auth.PrincipalException;
 import com.liferay.portal.security.permission.ActionKeys;
+import com.liferay.portal.security.permission.EntityPermissionChecker;
 import com.liferay.portal.security.permission.PermissionChecker;
 import com.liferay.portal.util.PortletKeys;
 import com.liferay.portal.util.PropsValues;
@@ -32,7 +33,7 @@ import com.liferay.portlet.documentlibrary.service.DLFolderLocalServiceUtil;
 /**
  * @author Brian Wing Shun Chan
  */
-public class DLFolderPermission {
+public class DLFolderPermission implements EntityPermissionChecker {
 
 	public static void check(
 			PermissionChecker permissionChecker, DLFolder dlFolder,
@@ -143,6 +144,15 @@ public class DLFolderPermission {
 		Folder folder = DLAppLocalServiceUtil.getFolder(folderId);
 
 		return folder.containsPermission(permissionChecker, actionId);
+	}
+
+	@Override
+	public void checkPermission(
+			PermissionChecker permissionChecker, long groupId, long entryId,
+			String actionId)
+		throws PortalException, SystemException {
+
+		DLFolderPermission.check(permissionChecker, groupId, entryId, actionId);
 	}
 
 	private static boolean _hasPermission(

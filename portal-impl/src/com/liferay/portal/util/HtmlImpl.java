7c2138a7cfe852128bba4c2f712e3369276592b6
Hugo Huijser <hugo.huijser@liferay.com>
2014-04-14 14:09:51 -0700

    LPS-43171 Documentation was removed from original method by accident

--- portal-impl/src/com/liferay/portal/util/HtmlImpl.java
+++ portal-impl/src/com/liferay/portal/util/HtmlImpl.java
@@ -57,6 +57,13 @@ public class HtmlImpl implements Html {
 
 	public static final int ESCAPE_MODE_URL = 5;
 
+	/**
+	 * Escapes the text so that it is safe to use in an HTML context.
+	 *
+	 * @param  text the text to escape
+	 * @return the escaped HTML text, or <code>null</code> if the text is
+	 *         <code>null</code>
+	 */
 	@Override
 	public String escape(String text) {
 		if (text == null) {
@@ -388,13 +395,6 @@ public class HtmlImpl implements Html {
 		return StringUtil.replace(text, "&amp;", "&");
 	}
 
-	/**
-	 * Escapes the text so that it is safe to use in an HTML context.
-	 *
-	 * @param  text the text to escape
-	 * @return the escaped HTML text, or <code>null</code> if the text is
-	 *         <code>null</code>
-	 */
 	@Override
 	public String getAUICompatibleId(String text) {
 		if (Validator.isNull(text)) {

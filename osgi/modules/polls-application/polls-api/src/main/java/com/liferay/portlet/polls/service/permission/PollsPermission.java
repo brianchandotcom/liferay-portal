0567e38a34c73c7840a09cf941c9064eb51c5b2e
Miguel Angel Pastor Olivar <miguel.pastor@liferay.com>
2014-04-24 11:23:59 -0700

    LPS-46191 Remove the keys and create them in the new modules

--- osgi/modules/polls-application/polls-api/src/main/java/com/liferay/portlet/polls/service/permission/PollsPermission.java
+++ osgi/modules/polls-application/polls-api/src/main/java/com/liferay/portlet/polls/service/permission/PollsPermission.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
@@ -18,7 +18,7 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.staging.permission.StagingPermissionUtil;
 import com.liferay.portal.security.auth.PrincipalException;
 import com.liferay.portal.security.permission.PermissionChecker;
-import com.liferay.portal.util.PortletKeys;
+import com.liferay.portlet.polls.constants.PollsPortletKeys;
 
 /**
  * @author Mika Koivisto
@@ -41,7 +41,7 @@ public class PollsPermission {
 
 		Boolean hasPermission = StagingPermissionUtil.hasPermission(
 			permissionChecker, groupId, RESOURCE_NAME, groupId,
-			PortletKeys.POLLS, actionId);
+			PollsPortletKeys.POLLS, actionId);
 
 		if (hasPermission != null) {
 			return hasPermission.booleanValue();

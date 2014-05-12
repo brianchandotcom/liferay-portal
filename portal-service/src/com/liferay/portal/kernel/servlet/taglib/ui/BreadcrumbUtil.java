33c92a368a36698f3d76d411f895921678344b3a
JM <jose.navarro@liferay.com>
2014-05-12 15:41:30 +0200

    LPS-44643 Removed provider annotation

--- portal-service/src/com/liferay/portal/kernel/servlet/taglib/ui/BreadcrumbUtil.java
+++ portal-service/src/com/liferay/portal/kernel/servlet/taglib/ui/BreadcrumbUtil.java
@@ -14,8 +14,6 @@
 
 package com.liferay.portal.kernel.servlet.taglib.ui;
 
-import aQute.bnd.annotation.ProviderType;
-
 import com.liferay.portal.theme.ThemeDisplay;
 
 import java.util.List;
@@ -25,7 +23,6 @@ import javax.servlet.http.HttpServletRequest;
 /**
  * @author José Manuel Navarro
  */
-@ProviderType
 public class BreadcrumbUtil {
 
 	public static Breadcrumb getBreadcrumb() {

39c982b74a217af7d7fde5bc7821ee3e7c8b4e82
peterfellwock <peter.fellwock@liferay.com>
2014-06-25 13:57:40 -0700

    LPS-47688 As a developer I would like to register EmailAddressGenerator/Validator from OSGi. Relying on registry centralization from 65fe160b7fcc5be759894cca8097b94cb7513203

--- portal-impl/src/com/liferay/portal/deploy/hot/HookHotDeployListener.java
+++ portal-impl/src/com/liferay/portal/deploy/hot/HookHotDeployListener.java
@@ -577,18 +577,6 @@ public class HookHotDeployListener
 			}
 		}
 
-		if (portalProperties.containsKey(
-				PropsKeys.USERS_EMAIL_ADDRESS_GENERATOR)) {
-
-			EmailAddressGeneratorFactory.setInstance(null);
-		}
-
-		if (portalProperties.containsKey(
-				PropsKeys.USERS_EMAIL_ADDRESS_VALIDATOR)) {
-
-			EmailAddressValidatorFactory.setInstance(null);
-		}
-
 		if (portalProperties.containsKey(PropsKeys.USERS_FULL_NAME_GENERATOR)) {
 			FullNameGeneratorFactory.setInstance(null);
 		}
@@ -2073,7 +2061,12 @@ public class HookHotDeployListener
 					portletClassLoader, EmailAddressGenerator.class,
 					emailAddressGeneratorClassName);
 
-			EmailAddressGeneratorFactory.setInstance(emailAddressGenerator);
+			ServiceRegistration<EmailAddressGenerator> serviceRegistration =
+	                registry.registerService(
+	                    EmailAddressGenerator.class, emailAddressGenerator);
+
+	            serviceRegistrations.put(
+	                emailAddressGeneratorClassName, serviceRegistration);
 		}
 
 		if (portalProperties.containsKey(
@@ -2088,7 +2081,12 @@ public class HookHotDeployListener
 					portletClassLoader, EmailAddressValidator.class,
 					emailAddressValidatorClassName);
 
-			EmailAddressValidatorFactory.setInstance(emailAddressValidator);
+			ServiceRegistration<EmailAddressValidator> serviceRegistration =
+	                registry.registerService(
+	                    EmailAddressValidator.class, emailAddressValidator);
+
+	            serviceRegistrations.put(
+	            		emailAddressValidatorClassName, serviceRegistration);
 		}
 
 		if (portalProperties.containsKey(PropsKeys.USERS_FULL_NAME_GENERATOR)) {

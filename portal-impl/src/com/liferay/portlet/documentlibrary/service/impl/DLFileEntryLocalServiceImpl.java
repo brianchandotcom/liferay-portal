8e1f9b9b76efa475ba8bf4eaa22f7e5b970d3f52
Hugo Huijser <hugo.huijser@liferay.com>
2014-08-04 12:11:02 +0800

    LPS-48992 Source formatting

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryLocalServiceImpl.java
@@ -673,9 +673,10 @@ public class DLFileEntryLocalServiceImpl
 		int pages = count / _DELETE_INTERVAL;
 
 		int start = 0;
-		int end = _DELETE_INTERVAL;
 
 		for (int i = 0; i <= pages; i++) {
+			int end = start + _DELETE_INTERVAL;
+
 			List<DLFileEntry> dlFileEntries = dlFileEntryPersistence.findByG_F(
 				groupId, folderId, start, end);
 
@@ -687,7 +688,6 @@ public class DLFileEntryLocalServiceImpl
 				}
 				else {
 					start++;
-					end++;
 				}
 			}
 		}

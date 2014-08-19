41509e596814fd5ef98d5a200f25f94a52406b63
arbetts <andrew.betts@liferay.com>
2014-08-19 15:12:36 -0700

    LPS-47199 owner's are unable to view their journalArticle drafts from the home tab in the control panel

--- modules/apps/journal/journal-service/src/com/liferay/journal/service/impl/JournalFolderLocalServiceImpl.java
+++ modules/apps/journal/journal-service/src/com/liferay/journal/service/impl/JournalFolderLocalServiceImpl.java
@@ -363,8 +363,9 @@ public class JournalFolderLocalServiceImpl
 		QueryDefinition<?> queryDefinition = new QueryDefinition<>(
 			WorkflowConstants.STATUS_ANY);
 
-		return journalFolderFinder.findF_A_ByG_F(
-			groupId, folderId, queryDefinition);
+		return journalFolderFinder.findF_A_ByG_U_F(
+			groupId, JournalFolderConstants.DEFAULT_USER_ID, folderId,
+			queryDefinition);
 	}
 
 	@Override
@@ -373,8 +374,9 @@ public class JournalFolderLocalServiceImpl
 
 		QueryDefinition<?> queryDefinition = new QueryDefinition<>(status);
 
-		return journalFolderFinder.findF_A_ByG_F(
-			groupId, folderId, queryDefinition);
+		return journalFolderFinder.findF_A_ByG_U_F(
+			groupId, JournalFolderConstants.DEFAULT_USER_ID, folderId,
+			queryDefinition);
 	}
 
 	@Override
@@ -386,8 +388,9 @@ public class JournalFolderLocalServiceImpl
 			WorkflowConstants.STATUS_ANY, start, end,
 			(OrderByComparator<Object>)obc);
 
-		return journalFolderFinder.findF_A_ByG_F(
-			groupId, folderId, queryDefinition);
+		return journalFolderFinder.findF_A_ByG_U_F(
+			groupId, JournalFolderConstants.DEFAULT_USER_ID, folderId,
+			queryDefinition);
 	}
 
 	@Override
@@ -419,8 +422,8 @@ public class JournalFolderLocalServiceImpl
 
 	@Override
 	public int getFoldersAndArticlesCount(long groupId, long folderId) {
-		return journalFolderFinder.countF_A_ByG_F(
-			groupId, folderId,
+		return journalFolderFinder.countF_A_ByG_U_F(
+			groupId, JournalFolderConstants.DEFAULT_USER_ID, folderId,
 			new QueryDefinition<Object>(WorkflowConstants.STATUS_ANY));
 	}
 
@@ -428,8 +431,9 @@ public class JournalFolderLocalServiceImpl
 	public int getFoldersAndArticlesCount(
 		long groupId, long folderId, int status) {
 
-		return journalFolderFinder.countF_A_ByG_F(
-			groupId, folderId, new QueryDefinition<Object>(status));
+		return journalFolderFinder.countF_A_ByG_U_F(
+			groupId, JournalFolderConstants.DEFAULT_USER_ID, folderId,
+			new QueryDefinition<Object>(status));
 	}
 
 	@Override

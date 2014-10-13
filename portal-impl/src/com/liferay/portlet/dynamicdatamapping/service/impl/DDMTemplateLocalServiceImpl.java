2eb259a8ee9d008519dd5a6cc1fbf8964be23794
Michael Han <michael.han@liferay.com>
2014-10-13 14:01:37 -0500

    LPS-50800 add ability to delete template by classNameId and groupId

--- portal-impl/src/com/liferay/portlet/dynamicdatamapping/service/impl/DDMTemplateLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/dynamicdatamapping/service/impl/DDMTemplateLocalServiceImpl.java
@@ -464,6 +464,8 @@ public class DDMTemplateLocalServiceImpl
 	 * Deletes the matching templates and its resources.
 	 *
 	 * @param  groupId the primary key of the group
+	 * @param  classNameId the primary key of the class name for the template's
+	 *         related model
 	 * @throws PortalException if a portal exception occurred
 	 */
 	@Override

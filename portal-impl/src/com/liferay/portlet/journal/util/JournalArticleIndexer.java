a808713b2a9ddc95468af2a9b0713b4ae1a707d9
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2014-12-10 15:07:32 +0100

    LPS-52014 We should delete the article from index if status is distinct to trash and status is distinct to approve, when journal.articles.index.all.versions = false

--- portal-impl/src/com/liferay/portlet/journal/util/JournalArticleIndexer.java
+++ portal-impl/src/com/liferay/portlet/journal/util/JournalArticleIndexer.java
@@ -488,6 +488,22 @@ public class JournalArticleIndexer extends BaseIndexer {
 			return;
 		}
 
+		if (!PropsValues.JOURNAL_ARTICLE_INDEX_ALL_VERSIONS) {
+			int status = article.getStatus();
+
+			if ((status != WorkflowConstants.STATUS_APPROVED) &&
+				(status != WorkflowConstants.STATUS_IN_TRASH)) {
+
+				Document document = getDocument(article);
+
+				SearchEngineUtil.deleteDocument(
+					getSearchEngineId(), article.getCompanyId(),
+					document.get(Field.UID), isCommitImmediately());
+
+				return;
+			}
+		}
+
 		if (allVersions) {
 			reindexArticleVersions(article);
 		}
@@ -737,17 +753,9 @@ public class JournalArticleIndexer extends BaseIndexer {
 	protected void reindexArticleVersions(JournalArticle article)
 		throws Exception {
 
-		Collection<Document> documents = getArticleVersions(article);
-
-		if(documents.isEmpty() && 
-			!PropsValues.JOURNAL_ARTICLE_INDEX_ALL_VERSIONS) {
-
-			doDelete(article);
-		}
-
 		SearchEngineUtil.updateDocuments(
-			getSearchEngineId(), article.getCompanyId(), documents,
-			isCommitImmediately());
+			getSearchEngineId(), article.getCompanyId(),
+			getArticleVersions(article), isCommitImmediately());
 	}
 
 	private static Log _log = LogFactoryUtil.getLog(

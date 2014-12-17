cae15a5490a0b45b62f57c231b4b5a3c7bd87441
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2014-12-16 16:54:45 -0800

    LPS-52061 SF

--- portal-service/src/com/liferay/portal/kernel/search/BaseIndexer.java
+++ portal-service/src/com/liferay/portal/kernel/search/BaseIndexer.java
@@ -1065,12 +1065,12 @@ public abstract class BaseIndexer implements Indexer {
 			BooleanQuery contextQuery, SearchContext searchContext)
 		throws Exception {
 
-		long[] folderIds = searchContext.getFolderIds();
-
 		MultiValueFacet multiValueFacet = new MultiValueFacet(searchContext);
 
 		multiValueFacet.setFieldName(Field.TREE_PATH);
 		multiValueFacet.setStatic(true);
+
+		long[] folderIds = searchContext.getFolderIds();
 
 		folderIds = ArrayUtil.remove(folderIds, 0);
 

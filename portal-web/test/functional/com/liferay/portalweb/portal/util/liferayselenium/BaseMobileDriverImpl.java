5c30974b6ef2730a3ec525064ab4bf24d0f758b8
Kenji Heigel <kenji.heigel@liferay.com>
2015-01-14 08:56:37 -0800

    LPS-52620 SF

--- portal-web/test/functional/com/liferay/portalweb/portal/util/liferayselenium/BaseMobileDriverImpl.java
+++ portal-web/test/functional/com/liferay/portalweb/portal/util/liferayselenium/BaseMobileDriverImpl.java
@@ -272,7 +272,7 @@ public abstract class BaseMobileDriverImpl
 
 	@Override
 	public String getDependenciesDirName() {
-		return _dependenciesDirName;
+		return _DEPENDENCIES_DIR_NAME;
 	}
 
 	@Override
@@ -307,7 +307,7 @@ public abstract class BaseMobileDriverImpl
 
 	@Override
 	public String getOutputDirName() {
-		return _outputDirName;
+		return _OUTPUT_DIR_NAME;
 	}
 
 	@Override
@@ -322,7 +322,7 @@ public abstract class BaseMobileDriverImpl
 
 	@Override
 	public String getSikuliImagesDirName() {
-		return _sikuliImagesDirName;
+		return _SIKULI_IMAGES_DIR_NAME;
 	}
 
 	@Override
@@ -766,12 +766,16 @@ public abstract class BaseMobileDriverImpl
 		throw new UnsupportedOperationException();
 	}
 
-	private String _dependenciesDirName =
+	private static final String _DEPENDENCIES_DIR_NAME =
 		"portal-web//test//functional//com//liferay//portalweb//dependencies//";
-	private String _outputDirName = TestPropsValues.OUTPUT_DIR_NAME;
+
+	private static final String _OUTPUT_DIR_NAME =
+		TestPropsValues.OUTPUT_DIR_NAME;
+
+	private static final String _SIKULI_IMAGES_DIR_NAME =
+		_DEPENDENCIES_DIR_NAME + "sikuli//linux//";
+
 	private String _primaryTestSuiteName;
-	private String _projectDirName;
-	private String _sikuliImagesDirName =
-		_dependenciesDirName + "sikuli//linux//";
+	private final String _projectDirName;
 
 }

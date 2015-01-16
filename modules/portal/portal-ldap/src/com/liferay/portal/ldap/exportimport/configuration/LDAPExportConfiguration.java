a1651c64cdb9c8d11d7f3e8ab08c1a53952eef47
Michael Han <michael.han@liferay.com>
2015-01-16 12:04:01 -0600

    LPS-51916 add configuration interfaces

---
+++ modules/portal/portal-ldap/src/com/liferay/portal/ldap/exportimport/configuration/LDAPExportConfiguration.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.ldap.exportimport.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+/**
+ * @author Michael C. Han
+ */
+@Meta.OCD(
+	id = "com.liferay.portal.ldap.export", localization = "content.Language"
+)
+public interface LDAPExportConfiguration {
+
+	@Meta.AD(deflt = "false", id = "ldap.export.enabled")
+	public boolean isLDAPExportEnabled();
+
+	@Meta.AD(deflt = "true", id = "ldap.export.group.enabled")
+	public boolean isLDAPExportGroupEnabled();
+
+}

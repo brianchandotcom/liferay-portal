b3b7d47d22d99009464af386a9f8c6b24d6241ef
shuyangzhou <shuyang.zhou@liferay.com>
2015-01-22 11:07:07 -0800

    LPS-52574 SF

--- portal-impl/src/com/liferay/portal/servlet/filters/weblogic/WeblogicIncludeFilter.java
+++ portal-impl/src/com/liferay/portal/servlet/filters/weblogic/WeblogicIncludeFilter.java
@@ -11,6 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
+
 package com.liferay.portal.servlet.filters.weblogic;
 
 import com.liferay.portal.kernel.servlet.MetaInfoCacheServletResponse;

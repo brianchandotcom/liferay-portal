b10d481b012fa840b93172260fa72974133b8634
Gregory Amerson <gregory.amerson@liferay.com>
2015-01-26 18:05:56 +0800

    LPS-52961 recompiled libraries from ubuntu10.04

--- modules/util/java-sass-compiler/src/com/liferay/tools/jsc/SassCompiler.java
+++ modules/util/java-sass-compiler/src/com/liferay/tools/jsc/SassCompiler.java
@@ -14,19 +14,53 @@
 
 package com.liferay.tools.jsc;
 
+import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.tools.jsc.libsass.SassLibrary;
 import com.liferay.tools.jsc.libsass.SassLibrary.Sass_Context;
 import com.liferay.tools.jsc.libsass.SassLibrary.Sass_File_Context;
 import com.liferay.tools.jsc.libsass.SassLibrary.Sass_Options;
-
 import com.sun.jna.Pointer;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 /**
  * @author Gregory Amerson
  */
 public class SassCompiler {
+
+	public static void main(String[] args) {
+
+		try {
+			new SassCompiler(args);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public SassCompiler() {
+	}
+
+	public SassCompiler(String[] args)
+		throws Exception {
+
+		final SassCompiler compiler = new SassCompiler();
+
+		for (String arg : args) {
+			final File file = new File(arg);
+
+			if (isValidFile(file)) {
+				final String output = compiler.compileFile(arg, "", "");
+
+				final File outputFile = getOutputFile(file);
+				write(outputFile, output);
+			}
+		}
+	}
 
 	public String compileFile(
 			String inputFile, String includePath, String imgPath)
@@ -47,6 +81,7 @@ public class SassCompiler {
 
 		try {
 			final Sass_Options opt = _libsass.sass_make_options();
+			_libsass.sass_option_set_input_path(opt, inputFile);
 			_libsass.sass_option_set_output_path(opt, "");
 			_libsass.sass_option_set_image_path(opt, imgPath);
 			_libsass.sass_option_set_output_style(opt, outputstyle);
@@ -87,6 +122,31 @@ public class SassCompiler {
 			catch (Throwable t) {
 				throw new SassCompilationException(t);
 			}
+		}
+	}
+
+	private File getOutputFile(File file) {
+
+		return new File(file.getParentFile(), getOutputFileName(file));
+	}
+
+	private String getOutputFileName(File file) {
+
+		return file.getName().replaceAll("scss$", "css");
+	}
+
+	private boolean isValidFile(File file) {
+
+		return file != null && file.exists() &&
+			file.getName().endsWith(".scss");
+	}
+
+	private void write(File outputFile, String output) throws IOException {
+
+		try (Writer writer = new OutputStreamWriter(
+			new FileOutputStream(outputFile, false), StringPool.UTF8)) {
+
+			writer.write(output);
 		}
 	}
 

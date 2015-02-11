brandonyee333/test

LPS-52822 Add OSGi service locator class to be able to maintain model adapters

portal-impl/src/com/liferay/portal/model/adapter/ServiceTrackerMapModelAdapterBuilderLocator.java
@@ -1,0 +1,112 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.model.adapter;
+
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.ReflectionUtil;
+import com.liferay.registry.Registry;
+import com.liferay.registry.RegistryUtil;
+import com.liferay.registry.ServiceReference;
+import com.liferay.registry.collections.ServiceReferenceMapper;
+import com.liferay.registry.collections.ServiceTrackerCollections;
+import com.liferay.registry.collections.ServiceTrackerMap;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+public class ServiceTrackerMapModelAdapterBuilderLocator<T, V>
+	implements ModelAdapterBuilderLocator<T, V>, Closeable {
+
+	public ServiceTrackerMapModelAdapterBuilderLocator() {
+		_modelAdaptersMap.open();
+	}
+
+	@Override
+	public void close() throws IOException {
+		_modelAdaptersMap.close();
+	}
+
+	@Override
+	public ModelAdapterBuilder<T, V> locate(
+		Class<T> adapteeModelClass, Class<V> adaptedModelClass) {
+
+		return _modelAdaptersMap.getService(
+			getKey(adapteeModelClass, adaptedModelClass));
+	}
+
+	private String getKey(
+		Class<T> adapteeModelClass, Class<V> adaptedModelClass) {
+
+		return adapteeModelClass.getName() + "->" + adaptedModelClass.getName();
+	}
+
+	private final ServiceTrackerMap<String, ModelAdapterBuilder>
+		_modelAdaptersMap =
+			ServiceTrackerCollections.singleValueMap(
+				ModelAdapterBuilder.class, null,
+				new ServiceReferenceMapper<String, ModelAdapterBuilder>() {
+
+					@Override
+					public void map(
+						ServiceReference<ModelAdapterBuilder> serviceReference,
+						Emitter<String> emitter) {
+
+						Registry registry = RegistryUtil.getRegistry();
+
+						ModelAdapterBuilder modelAdapterBuilder =
+							registry.getService(serviceReference);
+
+						Type genericInterface =
+							ReflectionUtil.getGenericInterface(
+								modelAdapterBuilder, ModelAdapterBuilder.class);
+
+						if ((genericInterface == null) ||
+							!(genericInterface instanceof ParameterizedType)) {
+
+							return;
+						}
+
+						ParameterizedType parameterizedType =
+							(ParameterizedType)genericInterface;
+
+						Type[] typeArguments =
+							parameterizedType.getActualTypeArguments();
+
+						if (ArrayUtil.isEmpty(typeArguments) ||
+							(typeArguments.length != 2)) {
+
+							return;
+						}
+
+						try {
+							Class adapteeModelClass = (Class)typeArguments[0];
+							Class adaptedModelClass = (Class)typeArguments[1];
+
+							emitter.emit(
+								getKey(adapteeModelClass, adaptedModelClass));
+						}
+						catch (ClassCastException cce) {
+							return;
+						}
+					}
+				});
+
+}

brianchandotcom/liferay-portal

portal-impl/src/com/liferay/portal/model/adapter/builder/ServiceTrackerMapModelAdapterBuilderLocator.java
@@ -1,0 +1,112 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.model.adapter.builder;
+
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.ReflectionUtil;
+import com.liferay.registry.Registry;
+import com.liferay.registry.RegistryUtil;
+import com.liferay.registry.ServiceReference;
+import com.liferay.registry.collections.ServiceReferenceMapper;
+import com.liferay.registry.collections.ServiceTrackerCollections;
+import com.liferay.registry.collections.ServiceTrackerMap;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+public class ServiceTrackerMapModelAdapterBuilderLocator<T, V>
+	implements ModelAdapterBuilderLocator<T, V>, Closeable {
+
+	public ServiceTrackerMapModelAdapterBuilderLocator() {
+		_modelAdapterBuildersMap.open();
+	}
+
+	@Override
+	public void close() throws IOException {
+		_modelAdapterBuildersMap.close();
+	}
+
+	@Override
+	public ModelAdapterBuilder<T, V> locate(
+		Class<T> adapteeModelClass, Class<V> adaptedModelClass) {
+
+		return _modelAdapterBuildersMap.getService(
+			getKey(adapteeModelClass, adaptedModelClass));
+	}
+
+	private String getKey(
+		Class<T> adapteeModelClass, Class<V> adaptedModelClass) {
+
+		return adapteeModelClass.getName() + "->" + adaptedModelClass.getName();
+	}
+
+	private final ServiceTrackerMap<String, ModelAdapterBuilder>
+		_modelAdapterBuildersMap =
+			ServiceTrackerCollections.singleValueMap(
+				ModelAdapterBuilder.class, null,
+				new ServiceReferenceMapper<String, ModelAdapterBuilder>() {
+
+					@Override
+					public void map(
+						ServiceReference<ModelAdapterBuilder> serviceReference,
+						Emitter<String> emitter) {
+
+						Registry registry = RegistryUtil.getRegistry();
+
+						ModelAdapterBuilder modelAdapterBuilder =
+							registry.getService(serviceReference);
+
+						Type genericInterface =
+							ReflectionUtil.getGenericInterface(
+								modelAdapterBuilder, ModelAdapterBuilder.class);
+
+						if ((genericInterface == null) ||
+							!(genericInterface instanceof ParameterizedType)) {
+
+							return;
+						}
+
+						ParameterizedType parameterizedType =
+							(ParameterizedType)genericInterface;
+
+						Type[] typeArguments =
+							parameterizedType.getActualTypeArguments();
+
+						if (ArrayUtil.isEmpty(typeArguments) ||
+							(typeArguments.length != 2)) {
+
+							return;
+						}
+
+						try {
+							Class adapteeModelClass = (Class)typeArguments[0];
+							Class adaptedModelClass = (Class)typeArguments[1];
+
+							emitter.emit(
+								getKey(adapteeModelClass, adaptedModelClass));
+						}
+						catch (ClassCastException cce) {
+							return;
+						}
+					}
+				});
+
+}
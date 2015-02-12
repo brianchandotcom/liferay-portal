4d47d1c86f82a236df9cefac32cfea18794914b9
adolfo.perez <adolfo.perez@liferay.com>
2015-02-12 16:16:04 +0100

    LPS-53401 rename to match interface
    It is not a provider, but a thin wrapper over the OSGi configuration.

--- modules/apps/wiki/wiki-api/src/com/liferay/wiki/settings/provider/WikiConfigurationImpl.java
+++ modules/apps/wiki/wiki-api/src/com/liferay/wiki/settings/provider/WikiConfigurationImpl.java
@@ -31,7 +31,7 @@ import org.osgi.service.component.annotations.Modified;
 	configurationPid = "com.liferay.wiki", immediate = true,
 	service = WikiConfiguration.class
 )
-public class WikiConfigurationProvider implements WikiConfiguration {
+public class WikiConfigurationImpl implements WikiConfiguration {
 
 	public String defaultFormat() {
 		return _wikiConfiguration.defaultFormat();

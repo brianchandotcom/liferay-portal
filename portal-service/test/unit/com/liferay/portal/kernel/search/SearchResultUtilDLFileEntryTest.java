cc5b416ad1d84f654437085d698c4db1af6991ba
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2015-02-24 13:37:26 +0100

    LPS-53730 remove PortletURL from the indexers in order to decouple the portlets from the indexer

--- portal-service/test/unit/com/liferay/portal/kernel/search/SearchResultUtilDLFileEntryTest.java
+++ portal-service/test/unit/com/liferay/portal/kernel/search/SearchResultUtilDLFileEntryTest.java
@@ -175,7 +175,7 @@ public class SearchResultUtilDLFileEntryTest
 		String title = RandomTestUtil.randomString();
 		String content = RandomTestUtil.randomString();
 
-		Summary summary = new Summary(null, title, content, null);
+		Summary summary = new Summary(null, title, content);
 
 		doReturn(
 			summary
@@ -183,7 +183,7 @@ public class SearchResultUtilDLFileEntryTest
 			indexer
 		).getSummary(
 			(Document)Matchers.any(), Matchers.anyString(),
-			(PortletURL)Matchers.any(), (PortletRequest)Matchers.isNull(),
+			(PortletRequest)Matchers.isNull(),
 			(PortletResponse)Matchers.isNull());
 
 		SearchResult searchResult = assertOneSearchResult(
@@ -282,8 +282,7 @@ public class SearchResultUtilDLFileEntryTest
 			indexer
 		).getSummary(
 			(Document)Matchers.any(), Matchers.anyString(),
-			(PortletURL)Matchers.any(), (PortletRequest)Matchers.any(),
-			(PortletResponse)Matchers.any());
+			(PortletRequest)Matchers.any(), (PortletResponse)Matchers.any());
 
 		stub(
 			method(IndexerRegistryUtil.class, "getIndexer", String.class)
@@ -315,7 +314,7 @@ public class SearchResultUtilDLFileEntryTest
 		Mockito.verify(
 			indexer
 		).getSummary(
-			document, snippet, portletURL, null, null
+			document, snippet, null, null
 		);
 
 		assertEmptyFileEntryTuples(searchResult);

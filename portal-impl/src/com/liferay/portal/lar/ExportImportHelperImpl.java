620a085b81f4b6afd10a702bd3b67e4923a06eac
Gergely Mathe <gergely.mathe@liferay.com>
2015-02-25 14:22:14 +0100

    LPS-53885 Don't remove undeployed portlets automatically from the page

--- portal-impl/src/com/liferay/portal/lar/ExportImportHelperImpl.java
+++ portal-impl/src/com/liferay/portal/lar/ExportImportHelperImpl.java
@@ -1925,7 +1925,7 @@ public class ExportImportHelperImpl implements ExportImportHelper {
 		Portlet portlet = PortletLocalServiceUtil.getPortletById(
 			companyId, portletId);
 
-		if (portlet == null) {
+		if ((portlet == null) || portlet.isUndeployedPortlet()) {
 			return false;
 		}
 

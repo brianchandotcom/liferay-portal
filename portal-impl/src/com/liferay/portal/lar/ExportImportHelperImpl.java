3047c84c37b5f1e9d3c8ae5e38ca3d781c66a9c7
Máté Thurzó <mate.thurzo@liferay.com>
2015-04-02 15:02:29 +0200

    LPS-53885 One more check

--- portal-impl/src/com/liferay/portal/lar/ExportImportHelperImpl.java
+++ portal-impl/src/com/liferay/portal/lar/ExportImportHelperImpl.java
@@ -237,7 +237,7 @@ public class ExportImportHelperImpl implements ExportImportHelper {
 		Portlet portlet = PortletLocalServiceUtil.getPortletById(
 			companyId, portletId);
 
-		if (portlet == null) {
+		if ((portlet == null) || portlet.isUndeployedPortlet()) {
 			return null;
 		}
 

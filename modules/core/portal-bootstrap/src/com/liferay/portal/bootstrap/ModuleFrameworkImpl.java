brianchandotcom/liferay-portal

commit 55f59fc14485563d4510709d0d994d41e5dafba7

LPS-54635 Apply proper interface handling to spring beans published as osgi services

modules/core/portal-bootstrap/src/com/liferay/portal/bootstrap/ModuleFrameworkImpl.java
@@ -59,7 +59,6 @@ import java.security.ProtectionDomain;
 import java.util.ArrayList;
 import java.util.Dictionary;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -589,30 +588,6 @@ public class ModuleFrameworkImpl implements ModuleFramework {
 		return String.valueOf(level);
 	}
 
-	private Set<Class<?>> _getInterfaces(Object bean) {
-		Set<Class<?>> interfaces = new HashSet<>();
-
-		Class<?> beanClass = bean.getClass();
-
-		interfaces.add(beanClass);
-
-		for (Class<?> interfaceClass : beanClass.getInterfaces()) {
-			interfaces.add(interfaceClass);
-		}
-
-		while ((beanClass = beanClass.getSuperclass()) != null) {
-			interfaces.add(beanClass);
-
-			for (Class<?> interfaceClass : beanClass.getInterfaces()) {
-				if (!interfaces.contains(interfaceClass)) {
-					interfaces.add(interfaceClass);
-				}
-			}
-		}
-
-		return interfaces;
-	}
-
 	private String _getSystemPackagesExtra() {
 		String[] systemPackagesExtra =
 			PropsValues.MODULE_FRAMEWORK_SYSTEM_PACKAGES_EXTRA;
@@ -813,7 +788,7 @@ public class ModuleFrameworkImpl implements ModuleFramework {
 	private void _registerService(
 		BundleContext bundleContext, String beanName, Object bean) {
 
-		Set<Class<?>> interfaces = _getInterfaces(bean);
+		Set<Class<?>> interfaces = OSGiBeanProperties.Service.interfaces(bean);
 
 		if (interfaces.isEmpty()) {
 			return;

commit 88d21a23bd75b34a3690b69664e0227eca0be7ab

LPS-54635 Allow Spring bean to be registered under its class name, not all @Reference are on interface types, this is causing PortalExecutorManagerImpl failed to @Reference all ThreadPoolExecutors created in executor-spring.xml (Regression caused by 55f59fc14485563d4510709d0d994d41e5dafba7)

modules/core/portal-bootstrap/src/com/liferay/portal/bootstrap/ModuleFrameworkImpl.java
@@ -813,9 +813,7 @@ public class ModuleFrameworkImpl implements ModuleFramework {
 
 		Set<Class<?>> interfaces = OSGiBeanProperties.Service.interfaces(bean);
 
-		if (interfaces.isEmpty()) {
-			return;
-		}
+		interfaces.add(bean.getClass());
 
 		List<String> names = new ArrayList<>(interfaces.size());
 
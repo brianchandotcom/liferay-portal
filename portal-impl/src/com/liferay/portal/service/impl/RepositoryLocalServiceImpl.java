c3ae8f2e14558b11fe4279a8dd018ca2a56dc50d
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2015-05-14 11:42:21 +0200

    LPS-55566 Remove unused method

--- portal-impl/src/com/liferay/portal/service/impl/RepositoryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/RepositoryLocalServiceImpl.java
@@ -224,10 +224,6 @@ public class RepositoryLocalServiceImpl extends RepositoryLocalServiceBaseImpl {
 	}
 
 	@Override
-	public void invalidate() {
-	}
-
-	@Override
 	public void updateRepository(
 			long repositoryId, String name, String description)
 		throws PortalException {

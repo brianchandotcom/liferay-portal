eb8230a5b6a4b252729fcc8cb1a90384b7252a3c
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-05-30 18:29:57 -0300

    LPS-54704 SF

--- modules/apps/dynamic-data-lists/dynamic-data-lists-service/src/com/liferay/dynamic/data/lists/indexer/DDLIndexer.java
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-service/src/com/liferay/dynamic/data/lists/indexer/DDLIndexer.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.dynamic.data.lists.util;
+package com.liferay.dynamic.data.lists.indexer;
 
 import com.liferay.dynamic.data.lists.model.DDLRecord;
 import com.liferay.dynamic.data.lists.model.DDLRecordConstants;

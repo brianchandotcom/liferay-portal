brianchandotcom/liferay-portal

commit 77cbf161d1f01ee722649168ba86adae631da932

LPS-56383 Properties: set custom default value

portal-impl/src/com/liferay/portal/util/LocalizationImpl.java
@@ -368,6 +368,17 @@ public class LocalizationImpl implements Localization {
 		PortletPreferences preferences, String preferenceName,
 		String propertyName) {
 
+		return getLocalizationMap(
+			preferences, preferenceName, propertyName,
+			PropsUtil.get(propertyName), getClass().getClassLoader());
+	}
+
+	@Override
+	public Map<Locale, String> getLocalizationMap(
+		PortletPreferences preferences, String preferenceName,
+		String propertyName, String defaultPropertyValue,
+		ClassLoader classLoader) {
+
 		Map<Locale, String> map = new HashMap<>();
 
 		for (Locale locale : LanguageUtil.getAvailableLocales()) {
@@ -391,7 +402,8 @@ public class LocalizationImpl implements Localization {
 			return map;
 		}
 
-		map.put(defaultLocale, ContentUtil.get(PropsUtil.get(propertyName)));
+		map.put(
+			defaultLocale, ContentUtil.get(classLoader, defaultPropertyValue));
 
 		return map;
 	}

portal-service/src/com/liferay/portal/kernel/util/Localization.java
@@ -187,6 +187,11 @@ public interface Localization {
 		PortletPreferences preferences, String preferenceName,
 		String propertyName);
 
+	public Map<Locale, String> getLocalizationMap(
+		PortletPreferences preferences, String preferenceName,
+		String propertyName, String defaultPropertyValue,
+		ClassLoader classLoader);
+
 	/**
 	 * Returns a map of locales and localized strings for the parameter in the
 	 * portlet request.

portal-service/src/com/liferay/portal/kernel/util/LocalizationUtil.java
@@ -149,6 +149,16 @@ public class LocalizationUtil {
 	}
 
 	public static Map<Locale, String> getLocalizationMap(
+		PortletPreferences preferences, String preferenceName,
+		String propertyName, String defaultPropertyValue,
+		ClassLoader classLoader) {
+
+		return getLocalization().getLocalizationMap(
+			preferences, preferenceName, propertyName, defaultPropertyValue,
+			classLoader);
+	}
+
+	public static Map<Locale, String> getLocalizationMap(
 		PortletRequest portletRequest, String parameter) {
 
 		return getLocalization().getLocalizationMap(portletRequest, parameter);

commit 1d22d01d7d4a19720c0dd5535cd9f6830eb52557

LPS-56383 PropsUtil.get() can not take null as input, caused by 77cbf161d1f01ee722649168ba86adae631da932.

portal-impl/src/com/liferay/portal/util/LocalizationImpl.java
@@ -368,9 +368,15 @@ public class LocalizationImpl implements Localization {
 		PortletPreferences preferences, String preferenceName,
 		String propertyName) {
 
+		String defaultPropertyValue = null;
+
+		if (propertyName != null) {
+			defaultPropertyValue = PropsUtil.get(propertyName);
+		}
+
 		return getLocalizationMap(
-			preferences, preferenceName, propertyName,
-			PropsUtil.get(propertyName), getClass().getClassLoader());
+			preferences, preferenceName, propertyName, defaultPropertyValue,
+			getClass().getClassLoader());
 	}
 
 	@Override
a2bdd53bede76dae9f084790361762bad37639ef
Raymond Auge <raymond.auge@liferay.com>
2015-07-06 11:56:16 -0400

    LPS-55355 SF

--- portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
@@ -924,7 +924,6 @@ public class GroupLocalServiceImpl extends GroupLocalServiceBaseImpl {
 
 			expandoRowLocalService.deleteRows(group.getGroupId());
 
-
 			// Social
 
 			socialActivityLocalService.deleteActivities(group.getGroupId());

6e60bb113839158e58d0f99af5c7fbe5ed3b3525
tinatian <tina.tian@liferay.com>
2015-07-10 09:32:29 +0800

    Revert "LPS-56171 Rename CacheReplicator to PortalCacheReplicator"

--- portal-service/src/com/liferay/portal/kernel/cache/AggregatedPortalCacheListener.java
+++ portal-service/src/com/liferay/portal/kernel/cache/AggregatedPortalCacheListener.java
@@ -180,7 +180,7 @@ public class AggregatedPortalCacheListener<K extends Serializable, V>
 		CacheListenerScope cacheListenerScope) {
 
 		if (_remoteInvokeThreadLocal.get()) {
-			if (cacheListener instanceof PortalCacheReplicator) {
+			if (cacheListener instanceof CacheReplicator) {
 				return false;
 			}
 

abbf173467e22f9705a42f5af5b4f334235d91c5
Adolfo Pérez <adolfo.perez@liferay.com>
2015-07-13 10:21:24 +0200

    LPS-56933 Automatic rename of portal pkgs from `comment` to `comments`

--- modules/apps/blogs/blogs-test/test/unit/com/liferay/blogs/util/PingbackMethodImplTest.java
+++ modules/apps/blogs/blogs-test/test/unit/com/liferay/blogs/util/PingbackMethodImplTest.java
@@ -15,8 +15,8 @@
 package com.liferay.blogs.util;
 
 import com.liferay.blogs.web.constants.BlogsPortletKeys;
-import com.liferay.portal.kernel.comment.CommentManager;
-import com.liferay.portal.kernel.comment.DuplicateCommentException;
+import com.liferay.portal.kernel.comments.CommentManager;
+import com.liferay.portal.kernel.comments.DuplicateCommentException;
 import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.portlet.FriendlyURLMapper;

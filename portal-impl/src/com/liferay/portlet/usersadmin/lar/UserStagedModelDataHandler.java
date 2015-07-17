0f38968130b051e022f9728fd0113e48cc025485
akosthurzo <akos.thurzo@liferay.com>
2015-07-17 16:36:37 +0200

    LPS-57211 Add doImportMissingReference() implementation to avoid UnsupportedOperationException

--- portal-impl/src/com/liferay/portlet/usersadmin/lar/UserStagedModelDataHandler.java
+++ portal-impl/src/com/liferay/portlet/usersadmin/lar/UserStagedModelDataHandler.java
@@ -81,6 +81,13 @@ public class UserStagedModelDataHandler
 	}
 
 	@Override
+	protected void doImportMissingReference(
+			PortletDataContext portletDataContext, String uuid, long groupId,
+			long classPK)
+		throws Exception {
+	}
+
+	@Override
 	protected void doImportStagedModel(
 		PortletDataContext portletDataContext, User user) {
 	}

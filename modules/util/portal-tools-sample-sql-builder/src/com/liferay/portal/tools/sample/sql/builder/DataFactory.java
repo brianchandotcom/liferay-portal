a7adb83ddeed75483ac33a7d7cd4140fe974c1d3
Máté Thurzó <mate.thurzo@liferay.com>
2015-07-22 13:07:06 +0200

    LPS-57310 Source format

--- modules/util/portal-tools-sample-sql-builder/src/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -1293,13 +1293,13 @@ public class DataFactory {
 		ddlRecordModel.setVersionUserId(_sampleUserId);
 		ddlRecordModel.setVersionUserName(_SAMPLE_USER_NAME);
 		ddlRecordModel.setCreateDate(new Date());
-		ddlRecordModel.setLastPublishDate(new Date());
 		ddlRecordModel.setModifiedDate(new Date());
 		ddlRecordModel.setDDMStorageId(_counter.get());
 		ddlRecordModel.setRecordSetId(dDLRecordSetModel.getRecordSetId());
 		ddlRecordModel.setVersion(DDLRecordConstants.VERSION_DEFAULT);
 		ddlRecordModel.setDisplayIndex(
 			DDLRecordConstants.DISPLAY_INDEX_DEFAULT);
+		ddlRecordModel.setLastPublishDate(new Date());
 
 		return ddlRecordModel;
 	}
@@ -1316,7 +1316,6 @@ public class DataFactory {
 		ddlRecordSetModel.setUserId(_sampleUserId);
 		ddlRecordSetModel.setUserName(_SAMPLE_USER_NAME);
 		ddlRecordSetModel.setCreateDate(new Date());
-		ddlRecordSetModel.setLastPublishDate(new Date());
 		ddlRecordSetModel.setModifiedDate(new Date());
 		ddlRecordSetModel.setDDMStructureId(ddmStructureModel.getStructureId());
 		ddlRecordSetModel.setRecordSetKey(String.valueOf(_counter.get()));
@@ -1335,6 +1334,7 @@ public class DataFactory {
 			DDLRecordSetConstants.MIN_DISPLAY_ROWS_DEFAULT);
 		ddlRecordSetModel.setScope(
 			DDLRecordSetConstants.SCOPE_DYNAMIC_DATA_LISTS);
+		ddlRecordSetModel.setLastPublishDate(new Date());
 
 		return ddlRecordSetModel;
 	}

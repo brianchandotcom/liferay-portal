ffeaa6ac827181ca8abd3344662196c74729013d
Tamas Molnar <tamas.molnar@liferay.com>
2015-07-28 10:07:19 +0200

    LPS-56418 Add implementations

--- modules/apps/document-library/document-library-web/src/com/liferay/document/library/web/asset/DLFileEntryAssetRenderer.java
+++ modules/apps/document-library/document-library-web/src/com/liferay/document/library/web/asset/DLFileEntryAssetRenderer.java
@@ -61,7 +61,7 @@ import javax.servlet.http.HttpServletResponse;
  * @author Zsolt Berentey
  */
 public class DLFileEntryAssetRenderer
-	extends BaseJSPAssetRenderer implements TrashRenderer {
+	extends BaseJSPAssetRenderer<FileEntry> implements TrashRenderer {
 
 	public DLFileEntryAssetRenderer(
 		FileEntry fileEntry, FileVersion fileVersion) {
@@ -71,7 +71,7 @@ public class DLFileEntryAssetRenderer
 	}
 
 	@Override
-	public Object getAsset() {
+	public FileEntry getAsset() {
 		return _fileEntry;
 	}
 

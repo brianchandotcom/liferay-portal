dc7432bfd669b8076ea0b538a28f4a53bb8e2b00
Juergen Kappler <jurgen.kappler@liferay.com>
2015-07-30 10:27:39 +0200

    LPS-57379 Change interface name from SettingsIdMapping to ConfigurationPidMapping and propagate changes

--- modules/apps/site-navigation/site-navigation-menu-web/src/com/liferay/site/navigation/menu/web/settings/internal/NavigationMenuPortletInstanceSettingsIdMapping.java
+++ modules/apps/site-navigation/site-navigation-menu-web/src/com/liferay/site/navigation/menu/web/settings/internal/NavigationMenuPortletInstanceSettingsIdMapping.java
@@ -14,7 +14,7 @@
 
 package com.liferay.site.navigation.menu.web.settings.internal;
 
-import com.liferay.portal.kernel.settings.definition.SettingsIdMapping;
+import com.liferay.portal.kernel.settings.definition.ConfigurationPidMapping;
 import com.liferay.site.navigation.menu.web.configuration.NavigationMenuPortletInstanceConfiguration;
 import com.liferay.site.navigation.menu.web.constants.NavigationMenuPortletKeys;
 
@@ -25,7 +25,7 @@ import org.osgi.service.component.annotations.Component;
  */
 @Component
 public class NavigationMenuPortletInstanceSettingsIdMapping
-	implements SettingsIdMapping {
+	implements ConfigurationPidMapping {
 
 	@Override
 	public Class<?> getConfigurationBeanClass() {

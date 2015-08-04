ffc343c3ebaeca674c0e7f3fae307a3498c02974
Juergen Kappler <jurgen.kappler@liferay.com>
2015-08-04 10:12:43 +0200

    LPS-57379 Move from settings to configuration package (and standardize internal package) to conform with naming of API. No logical changes

--- modules/apps/wiki/wiki-service/src/com/liferay/wiki/asset/WikiPageAssetRenderer.java
+++ modules/apps/wiki/wiki-service/src/com/liferay/wiki/asset/WikiPageAssetRenderer.java
@@ -29,6 +29,7 @@ import com.liferay.portlet.asset.model.AssetRendererFactory;
 import com.liferay.portlet.asset.model.BaseJSPAssetRenderer;
 import com.liferay.portlet.trash.util.TrashUtil;
 import com.liferay.wiki.configuration.WikiGroupServiceConfiguration;
+import com.liferay.wiki.configuration.WikiGroupServiceOverriddenConfiguration;
 import com.liferay.wiki.constants.WikiConstants;
 import com.liferay.wiki.constants.WikiPortletKeys;
 import com.liferay.wiki.constants.WikiWebKeys;
@@ -36,7 +37,6 @@ import com.liferay.wiki.model.WikiPage;
 import com.liferay.wiki.model.WikiPageConstants;
 import com.liferay.wiki.service.WikiPageLocalServiceUtil;
 import com.liferay.wiki.service.permission.WikiPagePermissionChecker;
-import com.liferay.wiki.settings.WikiGroupServiceOverriddenConfiguration;
 import com.liferay.wiki.util.WikiUtil;
 
 import java.util.Date;

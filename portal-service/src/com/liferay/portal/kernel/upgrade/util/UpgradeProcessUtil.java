33bd79ee7481b43f897e3400d9741783dabbd831
Miguel Angel Pastor Olivar <miguel.pastor@liferay.com>
2015-08-06 06:27:51 +0200

    LPS-57681 Let's add this  method so we can easily have backwards compatibility (we still need to support the old legacy upgrade stuff when doing traditional deployments)

--- portal-service/src/com/liferay/portal/kernel/upgrade/util/UpgradeProcessUtil.java
+++ portal-service/src/com/liferay/portal/kernel/upgrade/util/UpgradeProcessUtil.java
@@ -19,6 +19,7 @@ import aQute.bnd.annotation.ProviderType;
 import com.liferay.portal.kernel.dao.jdbc.DataAccess;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.plugin.Version;
 import com.liferay.portal.kernel.search.SearchEngineUtil;
 import com.liferay.portal.kernel.upgrade.UpgradeException;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
@@ -127,10 +128,17 @@ public class UpgradeProcessUtil {
 	}
 
 	public static boolean upgradeProcess(
-			int buildNumber, List<UpgradeProcess> upgradeProcesses)
+		int buildNumber, List<UpgradeProcess> upgradeProcesses)
 		throws UpgradeException {
 
 		return upgradeProcess(buildNumber, upgradeProcesses, _INDEX_ON_UPGRADE);
+	}
+
+	public static boolean upgradeProcess(
+			Version version, List<UpgradeProcess> upgradeProcesses)
+		throws UpgradeException {
+
+		return upgradeProcess(version.toInt(), upgradeProcesses, _INDEX_ON_UPGRADE);
 	}
 
 	public static boolean upgradeProcess(

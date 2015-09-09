d9efda040044aa3280c994e85d8bd904bd1fbab6
Juergen Kappler <jurgen.kappler@liferay.com>
2015-09-09 08:51:34 +0200

    LPS-54585 Avoid caching embedded portlets since it fails when creating preferences on the first run for embedded portlets

--- portal-impl/src/com/liferay/portal/model/impl/LayoutTypePortletImpl.java
+++ portal-impl/src/com/liferay/portal/model/impl/LayoutTypePortletImpl.java
@@ -1442,10 +1442,6 @@ public class LayoutTypePortletImpl
 	protected List<Portlet> getEmbeddedPortlets(
 		List<Portlet> columnPortlets, List<Portlet> staticPortlets) {
 
-		if (_embeddedPortlets != null) {
-			return _embeddedPortlets;
-		}
-
 		List<Portlet> portlets = new ArrayList<>();
 
 		Layout layout = getLayout();

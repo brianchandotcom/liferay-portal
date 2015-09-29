d6e5092577521c874fdd61743352f852c7e869a6
Máté Thurzó <mate.thurzo@liferay.com>
2015-09-29 13:03:43 +0200

    LPS-58992 Implement XStreamConfigurator for Asset Tags Admin

---
+++ modules/apps/asset/asset-tags-admin-web/src/com/liferay/asset/tags/admin/web/exportimport/xstream/configurator/AssetTagsPortletXStreamConfigurator.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.tags.admin.web.exportimport.xstream.configurator;
+
+import com.liferay.exportimport.xstream.configurator.XStreamConfigurator;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portlet.asset.model.impl.AssetTagImpl;
+import com.liferay.portlet.exportimport.xstream.XStreamAlias;
+import com.liferay.portlet.exportimport.xstream.XStreamConverter;
+
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Mate Thurzo
+ */
+@Component(immediate = true, service = XStreamConfigurator.class)
+public class AssetTagsPortletXStreamConfigurator
+	implements XStreamConfigurator {
+
+	@Override
+	public List<XStreamAlias> getAliases() {
+		return ListUtil.toList(
+			new XStreamAlias[] {
+				new XStreamAlias(AssetTagImpl.class, "AssetTag")
+			});
+	}
+
+	@Override
+	public List<XStreamConverter> getConverters() {
+		return null;
+	}
+
+}

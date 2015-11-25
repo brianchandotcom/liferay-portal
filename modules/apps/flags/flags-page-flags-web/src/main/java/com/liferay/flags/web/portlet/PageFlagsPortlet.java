278687f6d515a10bd6807773e902fd957761f0f0
peterfellwock <peter.fellwock@liferay.com>
2015-11-25 13:06:17 -0800

    LPS-60633 (WEB) Move PageFlagsPortlet to just flags package

--- modules/apps/flags/flags-page-flags-web/src/main/java/com/liferay/flags/web/portlet/PageFlagsPortlet.java
+++ modules/apps/flags/flags-page-flags-web/src/main/java/com/liferay/flags/web/portlet/PageFlagsPortlet.java
@@ -14,7 +14,7 @@
 
 package com.liferay.flags.page.flags.web.portlet;
 
-import com.liferay.flags.page.flags.web.constants.PageFlagsPortletKeys;
+import com.liferay.flags.web.constants.FlagsPortletKeys;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import com.liferay.portal.model.Release;
 
@@ -40,7 +40,7 @@ import org.osgi.service.component.annotations.Reference;
 		"javax.portlet.expiration-cache=0",
 		"javax.portlet.init-param.template-path=/",
 		"javax.portlet.init-param.view-template=/view.jsp",
-		"javax.portlet.name=" + PageFlagsPortletKeys.PAGE_FLAGS,
+		"javax.portlet.name=" + FlagsPortletKeys.PAGE_FLAGS,
 		"javax.portlet.resource-bundle=content.Language",
 		"javax.portlet.security-role-ref=guest,power-user,user",
 		"javax.portlet.supports.mime-type=text/html"

40be9238f0f2d7b8f824c66f75e11a4a907fc42b
Julio Camarero <julio.camarero@liferay.com>
2015-12-17 12:31:38 +0100

    LPS-60158 SF

--- modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/constants/DLWebKeys.java
+++ modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/constants/DLWebKeys.java
@@ -22,10 +22,10 @@ public class DLWebKeys {
 	public static final String DOCUMENT_LIBRARY_PORTLET_TOOLBAR_CONTRIBUTOR =
 		"DOCUMENT_LIBRARY_PORTLET_TOOLBAR_CONTRIBUTOR";
 
-	private static final String DYNAMIC_INCLUDE_VIEW_FILE_ENTRY_POST =
+	private static final String _DYNAMIC_INCLUDE_VIEW_FILE_ENTRY_POST =
 		"com.liferay.document.library#VIEW_FILE_ENTRY#post";
 
-	private static final String DYNAMIC_INCLUDE_VIEW_FILE_ENTRY_PRE =
+	private static final String _DYNAMIC_INCLUDE_VIEW_FILE_ENTRY_PRE =
 		"com.liferay.document.library#VIEW_FILE_ENTRY#pre";
 
 }

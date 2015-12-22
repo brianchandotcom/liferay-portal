2a8a7cacff048aebe7a2ce78e07b75d29df574ab
Pavel Savinov <pavel.savinov@liferay.com>
2015-12-22 13:10:02 +0100

    LPS-60158 add dynamic include keys

---
+++ portal-service/src/com/liferay/portal/util/DynamicIncludeKeys.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.util;
+
+/**
+ * @author Pavel Savinov
+ */
+public class DynamicIncludeKeys {
+
+	public static final String ASSET_PUBLISHER_VIEW_CONTENT_POST =
+		"com.liferay.asset.publisher#VIEW_CONTENT#post";
+
+	public static final String ASSET_PUBLISHER_VIEW_CONTENT_PRE =
+		"com.liferay.asset.publisher#VIEW_CONTENT#pre";
+
+	public static final String BLOGS_VIEW_ENTRY_POST =
+		"com.liferay.blogs#VIEW_ENTRY#post";
+
+	public static final String BLOGS_VIEW_ENTRY_PRE =
+		"com.liferay.blogs#VIEW_ENTRY#pre";
+
+	public static final String BOOKMARKS_VIEW_ENTRY_POST =
+		"com.liferay.bookmarks#VIEW_ENTRY#post";
+
+	public static final String BOOKMARKS_VIEW_ENTRY_PRE =
+		"com.liferay.bookmarks#VIEW_ENTRY#pre";
+
+	public static final String CALENDAR_VIEW_CALENDAR_BOOKING_POST =
+		"com.liferay.calendar#VIEW_CALENDAR_BOOKING#post";
+
+	public static final String CALENDAR_VIEW_CALENDAR_BOOKING_PRE =
+		"com.liferay.calendar#VIEW_CALENDAR_BOOKING#pre";
+
+	public static final String DDL_VIEW_RECORD_POST =
+		"com.liferay.dynamic.data.lists#VIEW_RECORD#post";
+
+	public static final String DDL_VIEW_RECORD_PRE =
+		"com.liferay.dynamic.data.lists#VIEW_RECORD#pre";
+
+	public static final String DOCUMENT_LIBRARY_VIEW_FILE_ENTRY_POST =
+		"com.liferay.document.library#VIEW_FILE_ENTRY#post";
+
+	public static final String DOCUMENT_LIBRARY_VIEW_FILE_ENTRY_PRE =
+		"com.liferay.document.library#VIEW_FILE_ENTRY#pre";
+
+	public static final String JOURNAL_VIEW_POST =
+		"com.liferay.journal.content#VIEW#post";
+
+	public static final String JOURNAL_VIEW_PRE =
+		"com.liferay.journal.content#VIEW#pre";
+
+	public static final String MESSAGE_BOARDS_VIEW_THREAD_MESSAGE_POST =
+		"com.liferay.message.boards#VIEW_THREAD_MESSAGE#post";
+
+	public static final String MESSAGE_BOARDS_VIEW_THREAD_MESSAGE_PRE =
+		"com.liferay.message.boards#VIEW_THREAD_MESSAGE#pre";
+
+	public static final String WIKI_VIEW_POST = "com.liferay.wiki#VIEW#post";
+
+	public static final String WIKI_VIEW_PRE = "com.liferay.wiki#VIEW#pre";
+
+}

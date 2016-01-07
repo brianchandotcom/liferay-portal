5113c015570791c389ef090c5b8c531ac25d2d24
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2016-01-07 21:46:55 +0100

    LPS-61795 Register Spring Advices onto corresponding Spring services

--- modules/apps/staging/staging-security/src/main/java/com/liferay/staging/security/spring/OrganizationLocalServiceStagingAdvice.java
+++ modules/apps/staging/staging-security/src/main/java/com/liferay/staging/security/spring/OrganizationLocalServiceStagingAdvice.java
@@ -15,13 +15,18 @@
 package com.liferay.staging.security.spring;
 
 import com.liferay.portal.service.OrganizationLocalService;
+import com.liferay.portlet.exportimport.staging.Staging;
 
 import java.util.Arrays;
 import java.util.List;
 
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
 /**
  * @author Tomas Polesovsky
  */
+@Component(immediate = true)
 public class OrganizationLocalServiceStagingAdvice
 	extends LiveGroupStagingAdvice {
 
@@ -36,6 +41,19 @@ public class OrganizationLocalServiceStagingAdvice
 			"getGroupUserOrganizations", 0, long.class, long.class);
 
 		checkCoverage(_GROUP_METHODS_WHITELIST);
+	}
+
+	@Reference
+	protected void setService(OrganizationLocalService service) {
+		registerAdvice(service);
+	}
+
+	@Reference(unbind = "-")
+	protected void setStaging(Staging staging) {
+	}
+
+	protected void unsetService(OrganizationLocalService service) {
+		unregisterAdvice(service);
 	}
 
 	private static final List<String> _GROUP_METHODS_WHITELIST = Arrays.asList(

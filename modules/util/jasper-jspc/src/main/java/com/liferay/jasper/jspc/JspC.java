brianchandotcom/liferay-portal

commit 0f5669a9123508dc5cc8644c4c0436ad6dc0f511

LPS-61813 SF

modules/util/jasper-jspc/src/main/java/com/liferay/jasper/jspc/JspC.java
@@ -59,8 +59,8 @@ public class JspC extends org.apache.jasper.JspC {
 
 			jspc.execute();
 		}
-		catch (Exception e) {
-			e.printStackTrace();
+		catch (Exception e1) {
+			e1.printStackTrace();
 
 			try {
 				Field noDieLevelField =

commit fdb3371ebe1142704fa8208cc55ecbdebc6ac194

LPS-60355 Fix compilation error caused by 0f5669a9123508dc5cc8644c4c0436ad6dc0f511

modules/util/jasper-jspc/src/main/java/com/liferay/jasper/jspc/JspC.java
@@ -76,7 +76,7 @@ public class JspC extends org.apache.jasper.JspC {
 				}
 			}
 			catch (Exception e2) {
-				e2.addSuppressed(e);
+				e2.addSuppressed(e1);
 
 				throw new RuntimeException(e2);
 			}
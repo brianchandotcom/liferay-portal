c3b20aeba530f9d07e55c5e4e8a572983dec0921
Julio Camarero <julio.camarero@liferay.com>
2016-02-15 13:46:24 +0100

    LPS-63194 apply to all portlet configuration icons

--- modules/apps/staging/staging-processes-web/src/main/java/com/liferay/staging/processes/web/portlet/configuration/icon/StagingConfigurationPortletConfigurationIcon.java
+++ modules/apps/staging/staging-processes-web/src/main/java/com/liferay/staging/processes/web/portlet/configuration/icon/StagingConfigurationPortletConfigurationIcon.java
@@ -36,7 +36,7 @@ public class StagingConfigurationPortletConfigurationIcon
 	}
 
 	@Override
-	public String getMessage() {
+	public String getMessage(PortletRequest portletRequest) {
 		return "staging-configuration";
 	}
 

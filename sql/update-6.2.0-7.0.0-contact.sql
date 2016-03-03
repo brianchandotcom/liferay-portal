7c922919818787f73c921a5957c86391be408e15
Miguel Angel Pastor Olivar <miguel.pastor@liferay.com>
2016-03-03 13:38:39 +0100

    LPS-63792 Split the contacts drop statements (Contacts table potentially can be quite big)

---
+++ sql/update-6.2.0-7.0.0-contact.sql
@@ -0,0 +1,5 @@
+alter table Contact_ drop column aimSn;
+alter table Contact_ drop column icqSn;
+alter table Contact_ drop column msnSn;
+alter table Contact_ drop column mySpaceSn;
+alter table Contact_ drop column ymSn;

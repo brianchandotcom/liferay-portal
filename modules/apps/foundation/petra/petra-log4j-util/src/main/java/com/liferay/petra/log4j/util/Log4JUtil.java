badeeb53ad56562dbb6f3f31a569da1cbe57ddfd
peterfellwock <peter.fellwock@liferay.com>
2016-04-05 15:10:19 -0700

    LPS-64799 Remove unnecessary code that uses reflection to load custom settings that no longer applies post migration to util-java

--- modules/apps/foundation/petra/petra-log4j-util/src/main/java/com/liferay/petra/log4j/util/Log4JUtil.java
+++ modules/apps/foundation/petra/petra-log4j-util/src/main/java/com/liferay/petra/log4j/util/Log4JUtil.java
@@ -18,10 +18,8 @@ import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayOutputStream;
 import com.liferay.portal.kernel.io.unsync.UnsyncStringReader;
 import com.liferay.portal.kernel.log.LogFactory;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.util.PortalClassLoaderUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.PropsUtil;
-import com.liferay.portal.kernel.util.ReflectionUtil;
 import com.liferay.portal.kernel.util.ServerDetector;
 import com.liferay.portal.kernel.util.StreamUtil;
 import com.liferay.portal.kernel.util.StringPool;
@@ -30,15 +28,12 @@ import com.liferay.portal.kernel.util.StringUtil;
 import java.io.IOException;
 import java.io.InputStream;
 
-import java.lang.reflect.Field;
-
 import java.net.URL;
 
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.LogManager;
@@ -145,10 +140,6 @@ public class Log4JUtil {
 		}
 	}
 
-	public static Map<String, String> getCustomLogSettings() {
-		return new HashMap<>(_getCustomLogSettings());
-	}
-
 	public static String getOriginalLevel(String className) {
 		Level level = Level.ALL;
 
@@ -200,12 +191,6 @@ public class Log4JUtil {
 			name);
 
 		jdkLogger.setLevel(_getJdkLevel(priority));
-
-		if (custom) {
-			Map<String, String> customLogSettings = _getCustomLogSettings();
-
-			customLogSettings.put(name, priority);
-		}
 	}
 
 	/**
@@ -220,26 +205,6 @@ public class Log4JUtil {
 		StreamUtil.transfer(inputStream, unsyncByteArrayOutputStream, -1, true);
 
 		return unsyncByteArrayOutputStream.toByteArray();
-	}
-
-	private static Map<String, String> _getCustomLogSettings() {
-		ClassLoader classLoader = PortalClassLoaderUtil.getClassLoader();
-
-		if (Log4JUtil.class.getClassLoader() == classLoader) {
-			return _customLogSettings;
-		}
-
-		try {
-			Class<?> clazz = classLoader.loadClass(Log4JUtil.class.getName());
-
-			Field field = ReflectionUtil.getDeclaredField(
-				clazz, "_customLogSettings");
-
-			return (Map<String, String>)field.get(null);
-		}
-		catch (Exception e) {
-			return ReflectionUtil.throwException(e);
-		}
 	}
 
 	private static java.util.logging.Level _getJdkLevel(String priority) {
@@ -334,8 +299,6 @@ public class Log4JUtil {
 
 	private static final Logger _logger = Logger.getRootLogger();
 
-	private static final Map<String, String> _customLogSettings =
-		new ConcurrentHashMap<>();
 	private static String _liferayHome;
 
 }

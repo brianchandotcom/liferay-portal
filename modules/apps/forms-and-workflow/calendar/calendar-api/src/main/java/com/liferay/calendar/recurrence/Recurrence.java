115e24cca3d7f4c28ec85049d045dfd06060e06e
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2016-08-01 11:24:41 -0300

    LPS-62735 Rename methods to follow jCalendar convention.

--- modules/apps/forms-and-workflow/calendar/calendar-api/src/main/java/com/liferay/calendar/recurrence/Recurrence.java
+++ modules/apps/forms-and-workflow/calendar/calendar-api/src/main/java/com/liferay/calendar/recurrence/Recurrence.java
@@ -25,7 +25,7 @@ import java.util.TimeZone;
  */
 public class Recurrence {
 
-	public void addExceptionDate(Calendar calendar) {
+	public void addExceptionJCalendar(Calendar calendar) {
 		_exceptionJCalendars.add(calendar);
 	}
 
@@ -103,7 +103,7 @@ public class Recurrence {
 		return weekdays;
 	}
 
-	public void removeExceptionDate(Calendar calendar) {
+	public void removeExceptionJCalendar(Calendar calendar) {
 		_exceptionJCalendars.remove(calendar);
 	}
 

d804ce5424d7c7d42ceceb6aa09722d961a4b598
Matthew Tambara <matthew.tambara@liferay.com>
2016-08-16 16:37:36 -0700

    LPS-67611 Regen

--- modules/apps/opensocial/opensocial-portlet/docroot/WEB-INF/sql/indexes.sql
+++ modules/apps/opensocial/opensocial-portlet/docroot/WEB-INF/sql/indexes.sql
@@ -1,4 +1,4 @@
-create unique index IX_A6A89EB1 on OpenSocial_Gadget (companyId, url[$COLUMN_LENGTH:4000$]);
+create index IX_729869EE on OpenSocial_Gadget (companyId);
 create index IX_3C79316E on OpenSocial_Gadget (uuid_[$COLUMN_LENGTH:75$], companyId);
 
 create index IX_8E715BF8 on OpenSocial_OAuthConsumer (gadgetKey[$COLUMN_LENGTH:75$], serviceName[$COLUMN_LENGTH:75$]);

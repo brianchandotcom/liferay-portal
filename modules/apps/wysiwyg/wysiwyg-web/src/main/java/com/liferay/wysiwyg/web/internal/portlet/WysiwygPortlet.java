67a0ea0a3e0b16091eab63235e45bf88b0d9c53a
peterfellwock <peter.fellwock@liferay.com>
2016-11-09 15:33:19 -0800

    LPS-68072 Portlet update

--- modules/apps/wysiwyg/wysiwyg-web/src/main/java/com/liferay/wysiwyg/web/internal/portlet/WysiwygPortlet.java
+++ modules/apps/wysiwyg/wysiwyg-web/src/main/java/com/liferay/wysiwyg/web/internal/portlet/WysiwygPortlet.java
@@ -31,6 +31,7 @@ import org.osgi.service.component.annotations.Reference;
 	property = {
 		"com.liferay.portlet.css-class-wrapper=wysiwyg-portlet",
 		"com.liferay.portlet.display-category=category.cms",
+		"com.liferay.portlet.instanceable=true",
 		"com.liferay.portlet.use-default-template=true",
 		"javax.portlet.display-name=WYSIWYG",
 		"javax.portlet.expiration-cache=0",

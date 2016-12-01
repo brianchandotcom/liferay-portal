54c71eb96062b07379c2805c9206f9b8c936ae84
Hugo Huijser <hugo.huijser@liferay.com>
2016-12-01 16:01:28 +0800

    LPS-66853 Fix logic

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/JavaSourceProcessor.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/JavaSourceProcessor.java
@@ -3152,10 +3152,10 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 		String strippedQuotesPreviousLine = stripQuotes(previousLine);
 
 		if (strippedQuotesLine.contains("// ") ||
-			strippedQuotesLine.contains("*/") ||
+			strippedQuotesLine.contains("/*") ||
 			strippedQuotesLine.contains("*/") ||
 			strippedQuotesPreviousLine.contains("// ") ||
-			strippedQuotesPreviousLine.contains("*/") ||
+			strippedQuotesPreviousLine.contains("/*") ||
 			strippedQuotesPreviousLine.contains("*/")) {
 
 			return null;

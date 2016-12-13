d27d87c9f223ee412e7f7470847cdc81bd0fdf27
Hugo Huijser <hugo.huijser@liferay.com>
2016-12-13 17:59:39 +0800

    LPS-69673 Temporarily disable for modules

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/JavaSourceProcessor.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/JavaSourceProcessor.java
@@ -2519,7 +2519,9 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 
 				checkResourceUtil(line, fileName, absolutePath, lineCount);
 
-				if (_addMissingDeprecationReleaseVersion) {
+				if (_addMissingDeprecationReleaseVersion &&
+					!isModulesFile(absolutePath)) {
+
 					line = formatDeprecatedJavadoc(fileName, line);
 				}
 

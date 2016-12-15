2f96b560a80f6b1844766f691c511540e40e03b6
Tibor Lipusz <tibor.lipusz@liferay.com>
2016-12-15 14:46:29 +0100

    LPS-69480 Sort

--- portal-impl/test/integration/com/liferay/portlet/service/persistence/GroupFinderTest.java
+++ portal-impl/test/integration/com/liferay/portlet/service/persistence/GroupFinderTest.java
@@ -78,8 +78,6 @@ public class GroupFinderTest {
 	public static void setUpClass() throws Exception {
 		_group = GroupTestUtil.addGroup();
 
-		_organization = OrganizationTestUtil.addOrganization(true);
-
 		List<ResourceAction> resourceActions =
 			ResourceActionLocalServiceUtil.getResourceActions(0, 1);
 
@@ -103,24 +101,26 @@ public class GroupFinderTest {
 			_modelResourceAction.getName(),
 			StringUtil.valueOf(_group.getGroupId()),
 			_resourceTypePermission.getRoleId(), ResourceConstants.SCOPE_GROUP);
+
+		_organization = OrganizationTestUtil.addOrganization(true);
 	}
 
 	@AfterClass
 	public static void tearDownClass() throws Exception {
+		GroupLocalServiceUtil.deleteGroup(_group);
+		GroupLocalServiceUtil.deleteGroup(_userGroupGroup);
+
 		ResourcePermissionLocalServiceUtil.deleteResourcePermission(
 			_resourcePermission);
 
 		ResourceTypePermissionLocalServiceUtil.deleteResourceTypePermission(
 			_resourceTypePermission);
 
+		OrganizationLocalServiceUtil.deleteOrganization(_organization);
+
 		UserLocalServiceUtil.deleteUser(_userGroupUser);
 
 		UserGroupLocalServiceUtil.deleteUserGroup(_userGroup);
-
-		OrganizationLocalServiceUtil.deleteOrganization(_organization);
-
-		GroupLocalServiceUtil.deleteGroup(_group);
-		GroupLocalServiceUtil.deleteGroup(_userGroupGroup);
 	}
 
 	@Test

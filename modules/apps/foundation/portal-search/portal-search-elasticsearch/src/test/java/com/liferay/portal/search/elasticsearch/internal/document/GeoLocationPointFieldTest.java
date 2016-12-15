037e85112d887fe7d353be51062fef40a82a74e6
André de Oliveira <andre.oliveira@liferay.com>
2016-12-14 21:55:52 -0800

    LPS-69726 portal-search-elasticsearch: use Search unit test classes from portal-search-test-unit-util
    https://issues.liferay.com/browse/LPS-69726

--- modules/apps/foundation/portal-search/portal-search-elasticsearch/src/test/java/com/liferay/portal/search/elasticsearch/internal/document/GeoLocationPointFieldTest.java
+++ modules/apps/foundation/portal-search/portal-search-elasticsearch/src/test/java/com/liferay/portal/search/elasticsearch/internal/document/GeoLocationPointFieldTest.java
@@ -26,8 +26,8 @@ import com.liferay.portal.search.elasticsearch.internal.connection.IndexCreator;
 import com.liferay.portal.search.elasticsearch.internal.connection.IndicesAdminClientSupplier;
 import com.liferay.portal.search.elasticsearch.internal.connection.LiferayIndexCreationHelper;
 import com.liferay.portal.search.elasticsearch.internal.index.LiferayDocumentTypeFactory;
-import com.liferay.portal.search.unit.test.BaseIndexingTestCase;
-import com.liferay.portal.search.unit.test.IndexingFixture;
+import com.liferay.portal.search.unit.test.base.indexing.BaseIndexingTestCase;
+import com.liferay.portal.search.unit.test.base.indexing.IndexingFixture;
 
 import java.util.Arrays;
 import java.util.concurrent.Callable;

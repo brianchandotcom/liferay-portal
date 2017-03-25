253fb2e80982d3987e5eb3cea47d867bd15f2a1a
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-03-26 00:55:32 +0100

    LPS-71500 Moves methods as default to the interface

--- portal-kernel/src/com/liferay/portal/kernel/model/TrashedModel.java
+++ portal-kernel/src/com/liferay/portal/kernel/model/TrashedModel.java
@@ -16,9 +16,14 @@ package com.liferay.portal.kernel.model;
 
 import aQute.bnd.annotation.ProviderType;
 
+import com.liferay.portal.kernel.exception.NoSuchModelException;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.trash.TrashHandler;
+import com.liferay.portal.kernel.trash.TrashHandlerRegistryUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.trash.kernel.model.TrashEntry;
+import com.liferay.trash.kernel.service.TrashEntryLocalServiceUtil;
 
 /**
  * @author Zsolt Berentey
@@ -26,20 +31,136 @@ import com.liferay.trash.kernel.model.TrashEntry;
 @ProviderType
 public interface TrashedModel {
 
-	public int getStatus();
+	public String getModelClassName();
 
-	public TrashEntry getTrashEntry() throws PortalException;
+	public long getPrimaryKey();
 
-	public long getTrashEntryClassPK();
+	public default int getStatus() {
+		return 0;
+	}
 
-	public TrashHandler getTrashHandler();
+	public default TrashEntry getTrashEntry() throws PortalException {
+		if (!isInTrash()) {
+			return null;
+		}
 
-	public boolean isInTrash();
+		TrashEntry trashEntry = TrashEntryLocalServiceUtil.fetchEntry(
+			getModelClassName(), getTrashEntryClassPK());
 
-	public boolean isInTrashContainer();
+		if (trashEntry != null) {
+			return trashEntry;
+		}
 
-	public boolean isInTrashExplicitly();
+		TrashHandler trashHandler = getTrashHandler();
 
-	public boolean isInTrashImplicitly();
+		if (Validator.isNotNull(
+				trashHandler.getContainerModelClassName(getPrimaryKey()))) {
+
+			ContainerModel containerModel = null;
+
+			try {
+				containerModel = trashHandler.getParentContainerModel(this);
+			}
+			catch (NoSuchModelException nsme) {
+				return null;
+			}
+
+			while (containerModel != null) {
+				if (containerModel instanceof TrashedModel) {
+					TrashedModel trashedModel = (TrashedModel)containerModel;
+
+					return trashedModel.getTrashEntry();
+				}
+
+				trashHandler = TrashHandlerRegistryUtil.getTrashHandler(
+					trashHandler.getContainerModelClassName(
+						containerModel.getContainerModelId()));
+
+				if (trashHandler == null) {
+					return null;
+				}
+
+				containerModel = trashHandler.getContainerModel(
+					containerModel.getParentContainerModelId());
+			}
+		}
+
+		return null;
+	}
+
+	public default long getTrashEntryClassPK() {
+		return getPrimaryKey();
+	}
+
+	public default TrashHandler getTrashHandler() {
+		return TrashHandlerRegistryUtil.getTrashHandler(getModelClassName());
+	}
+
+	public default boolean isInTrash() {
+		if (getStatus() == WorkflowConstants.STATUS_IN_TRASH) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	public default boolean isInTrashContainer() {
+		TrashHandler trashHandler = getTrashHandler();
+
+		if ((trashHandler == null) ||
+			Validator.isNull(
+				trashHandler.getContainerModelClassName(getPrimaryKey()))) {
+
+			return false;
+		}
+
+		try {
+			ContainerModel containerModel =
+				trashHandler.getParentContainerModel(this);
+
+			if (containerModel == null) {
+				return false;
+			}
+
+			if (containerModel instanceof TrashedModel) {
+				return ((TrashedModel)containerModel).isInTrash();
+			}
+		}
+		catch (Exception e) {
+		}
+
+		return false;
+	}
+
+	public default boolean isInTrashExplicitly() {
+		if (!isInTrash()) {
+			return false;
+		}
+
+		TrashEntry trashEntry = TrashEntryLocalServiceUtil.fetchEntry(
+			getModelClassName(), getTrashEntryClassPK());
+
+		if (trashEntry != null) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public default boolean isInTrashImplicitly() {
+		if (!isInTrash()) {
+			return false;
+		}
+
+		TrashEntry trashEntry = TrashEntryLocalServiceUtil.fetchEntry(
+			getModelClassName(), getTrashEntryClassPK());
+
+		if (trashEntry != null) {
+			return false;
+		}
+
+		return true;
+	}
 
 }

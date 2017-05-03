cbb7092e5c60f5fcf2ebe30952c1aa5bd43c56e4
Hugo Huijser <hugo.huijser@liferay.com>
2017-05-03 14:52:15 +0800

    LPS-67352 SF not catching chaining when chains have lambdas inside

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/checks/ChainingCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/checks/ChainingCheck.java
@@ -64,14 +64,18 @@ public class ChainingCheck extends AbstractCheck {
 			detailAST, true, TokenTypes.METHOD_CALL);
 
 		for (DetailAST methodCallAST : methodCallASTList) {
-			List<DetailAST> childMethodCallASTList =
-				DetailASTUtil.getAllChildTokens(
-					methodCallAST, true, TokenTypes.METHOD_CALL);
+			DetailAST dotAST = methodCallAST.findFirstToken(TokenTypes.DOT);
 
-			// Only check the method that is first in the chain
+			if (dotAST != null) {
+				List<DetailAST> childMethodCallASTList =
+					DetailASTUtil.getAllChildTokens(
+						dotAST, false, TokenTypes.METHOD_CALL);
 
-			if (!childMethodCallASTList.isEmpty()) {
-				continue;
+				// Only check the method that is first in the chain
+
+				if (!childMethodCallASTList.isEmpty()) {
+					continue;
+				}
 			}
 
 			List<String> chainedMethodNames = _getChainedMethodNames(

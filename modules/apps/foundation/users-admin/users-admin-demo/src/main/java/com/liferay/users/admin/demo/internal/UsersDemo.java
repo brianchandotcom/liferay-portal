7f8cb3b7d7c3753f51924b7bdc3e4a752b819bf7
Pei-Jung Lan <pei-jung.lan@liferay.com>
2017-05-19 11:55:14 -0700

    LPS-72477 Add web content author with a sample web content layout

--- modules/apps/foundation/users-admin/users-admin-demo/src/main/java/com/liferay/users/admin/demo/internal/UsersDemo.java
+++ modules/apps/foundation/users-admin/users-admin-demo/src/main/java/com/liferay/users/admin/demo/internal/UsersDemo.java
@@ -14,14 +14,31 @@
 
 package com.liferay.users.admin.demo.internal;
 
+import com.liferay.journal.content.web.constants.JournalContentPortletKeys;
+import com.liferay.journal.demo.data.creator.JournalArticleDemoDataCreator;
+import com.liferay.journal.model.JournalArticle;
 import com.liferay.portal.instance.lifecycle.BasePortalInstanceLifecycleListener;
 import com.liferay.portal.instance.lifecycle.PortalInstanceLifecycleListener;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.LayoutConstants;
+import com.liferay.portal.kernel.model.LayoutTypePortlet;
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.DefaultLayoutPrototypesUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.roles.admin.demo.data.creator.RoleDemoDataCreator;
 import com.liferay.site.demo.data.creator.SiteDemoDataCreator;
 import com.liferay.users.admin.demo.data.creator.CompanyAdminUserDemoDataCreator;
 import com.liferay.users.admin.demo.data.creator.SiteAdminUserDemoDataCreator;
+import com.liferay.users.admin.demo.data.creator.SiteMemberUserDemoDataCreator;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
@@ -35,30 +52,96 @@ public class UsersDemo extends BasePortalInstanceLifecycleListener {
 
 	@Override
 	public void portalInstanceRegistered(Company company) throws Exception {
+		long companyId = company.getCompanyId();
+
 		_companyAdminUserDemoDataCreator.create(
 			company.getCompanyId(), "bruno@liferay.com");
 
 		Group acmeCorpGroup = _siteDemoDataCreator.create(
-			company.getCompanyId(), "Acme’s Corporation");
+			companyId, "Acme’s Corporation");
 
-		_siteAdminUserDemoDataCreator.create(
+		User siteAdmin = _siteAdminUserDemoDataCreator.create(
 			acmeCorpGroup.getGroupId(), "helen@liferay.com");
+
+		// Web content author with a sample web content layout
+
+		String webContentAuthorXml = StringUtil.read(
+			UsersDemo.class, "dependencies/web-content-author.xml");
+
+		Role webContentAuthor = _siteRoleDemoDataCreator.create(
+			companyId, "Web Content Author", webContentAuthorXml);
+
+		_siteMemberUserDemoDataCreator.create(
+			acmeCorpGroup.getGroupId(), "joe@liferay.com",
+			new long[] {webContentAuthor.getRoleId()});
+
+		Layout webContentLayout = addLayout(
+			siteAdmin.getUserId(), acmeCorpGroup.getGroupId(), "Web Contents",
+			"/web-contents", "1_column");
+
+		String webContentPortletId = DefaultLayoutPrototypesUtil.addPortletId(
+			webContentLayout, JournalContentPortletKeys.JOURNAL_CONTENT,
+			"column-1");
+
+		JournalArticle article = _journalArticleDemoDataCreator.create(
+			siteAdmin.getUserId(), acmeCorpGroup.getGroupId());
+
+		Map<String, String> preferences = new HashMap<>();
+
+		preferences.put("articleId", article.getArticleId());
+		preferences.put("groupId", String.valueOf(acmeCorpGroup.getGroupId()));
+
+		DefaultLayoutPrototypesUtil.updatePortletSetup(
+			webContentLayout, webContentPortletId, preferences);
+	}
+
+	protected Layout addLayout(
+			Long userId, Long groupId, String name, String friendlyURL,
+			String layoutTemplateId)
+		throws Exception {
+
+		Layout layout = _layoutLocalService.addLayout(
+			userId, groupId, false, LayoutConstants.DEFAULT_PARENT_LAYOUT_ID,
+			name, null, null, LayoutConstants.TYPE_PORTLET, false, friendlyURL,
+			new ServiceContext());
+
+		LayoutTypePortlet layoutTypePortlet =
+			(LayoutTypePortlet)layout.getLayoutType();
+
+		layoutTypePortlet.setLayoutTemplateId(userId, layoutTemplateId);
+
+		return layout;
 	}
 
 	@Deactivate
 	protected void deactivate() throws PortalException {
 		_companyAdminUserDemoDataCreator.delete();
 		_siteAdminUserDemoDataCreator.delete();
+		_siteMemberUserDemoDataCreator.delete();
+
 		_siteDemoDataCreator.delete();
+		_siteRoleDemoDataCreator.delete();
 	}
 
 	@Reference
 	private CompanyAdminUserDemoDataCreator _companyAdminUserDemoDataCreator;
 
 	@Reference
+	private JournalArticleDemoDataCreator _journalArticleDemoDataCreator;
+
+	@Reference
+	private LayoutLocalService _layoutLocalService;
+
+	@Reference
 	private SiteAdminUserDemoDataCreator _siteAdminUserDemoDataCreator;
 
 	@Reference
 	private SiteDemoDataCreator _siteDemoDataCreator;
+
+	@Reference
+	private SiteMemberUserDemoDataCreator _siteMemberUserDemoDataCreator;
+
+	@Reference(target = "(role.type=site)")
+	private RoleDemoDataCreator _siteRoleDemoDataCreator;
 
 }

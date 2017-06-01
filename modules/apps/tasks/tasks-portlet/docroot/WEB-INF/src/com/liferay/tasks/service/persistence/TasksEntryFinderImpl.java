1665db0be9d76a78bf86a2b722186d67278c12a5
Brent Krone-Schmidt <brent.krone-schmidt@liferay.com>
2017-06-01 10:03:00 -0700

    LPS-72918 manual sf

--- modules/apps/tasks/tasks-portlet/docroot/WEB-INF/src/com/liferay/tasks/service/persistence/TasksEntryFinderImpl.java
+++ modules/apps/tasks/tasks-portlet/docroot/WEB-INF/src/com/liferay/tasks/service/persistence/TasksEntryFinderImpl.java
@@ -15,7 +15,7 @@
  * Liferay Social Office. If not, see http://www.gnu.org/licenses/agpl-3.0.html.
  */
 
-package com.liferay.tasks.service.persistence;
+package com.liferay.tasks.service.persistence.impl;
 
 import com.liferay.portal.kernel.dao.orm.QueryPos;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;

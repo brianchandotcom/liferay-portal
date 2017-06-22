a0d499cc49daad5a7f924f547067cffa25f36c34
shuyangzhou <shuyang.zhou@liferay.com>
2017-06-22 09:38:11 -0700

    LPS-69008 Since dfc7794b4350fe64f5e7a00b69fbab556f2fa939 moved it to a new interface, we'd better make it type safe. Using generic in service interface is a very bad practice, because we don't really know what would be published into the registry, but when we retrieve them, just blindly cast them to the expected generic type, which is risking to see a runtime cast failure for parameters on invoking those service.

--- portal-impl/src/com/liferay/portal/servlet/filters/util/CacheFileNameContributor.java
+++ portal-impl/src/com/liferay/portal/servlet/filters/util/CacheFileNameContributor.java
@@ -14,20 +14,15 @@
 
 package com.liferay.portal.servlet.filters.util;
 
-import com.liferay.portal.kernel.language.LanguageUtil;
-import com.liferay.portal.kernel.spring.osgi.OSGiBeanProperties;
-import com.liferay.portal.kernel.util.Function;
-import com.liferay.portal.kernel.util.KeyValuePair;
-import com.liferay.portal.kernel.util.LocaleUtil;
-
-import java.util.Locale;
-import java.util.Set;
-
 import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Carlos Sierra Andrés
  */
-public interface CacheFileNameContributor
-	extends Function<HttpServletRequest, KeyValuePair> {
+public interface CacheFileNameContributor {
+
+	public String getParameterName();
+
+	public String getParameterValue(HttpServletRequest request);
+
 }

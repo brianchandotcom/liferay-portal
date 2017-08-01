brianchandotcom/liferay-portal

commit e9eb6dfd0e39be6a0a8c53e4a54344c475b9145d

LPS-47334 Ignore invalid parameters, don't let them abort page execution

portal-impl/src/com/liferay/portal/util/HttpImpl.java
@@ -746,7 +746,19 @@ public class HttpImpl implements Http {
 				String value = StringPool.BLANK;
 
 				if (kvp.length > 1) {
-					value = decodeURL(kvp[1]);
+					try {
+						value = decodeURL(kvp[1]);
+					}
+					catch (IllegalArgumentException e) {
+						if (_log.isInfoEnabled()) {
+							_log.info(
+								"Skipping parameter " + key +
+									", it has invalid value " + kvp[1] + " .",
+								e);
+						}
+
+						continue;
+					}
 				}
 
 				List<String> values = tempParameterMap.get(key);
@@ -1088,7 +1100,18 @@ public class HttpImpl implements Http {
 
 				String redirect = param.substring(pos + 1);
 
-				redirect = decodeURL(redirect);
+				try {
+					redirect = decodeURL(redirect);
+				}
+				catch (IllegalArgumentException e) {
+					if (_log.isDebugEnabled()) {
+						_log.debug(
+							"Unable to decode parameter " + param +
+								". Skipping.", e);
+					}
+
+					continue;
+				}
 
 				String newURL = shortenURL(redirect, count - 1);
 

commit b00495578a34f232e5e5243a5c7a5f331ea6a0c7

LPS-73606 decodeURL() never throws IllegalArgumentException, therefore the catch block is a dead code. This is really supposed to call URLCodec.decodeURL() directly, and skip the undecodeable parameter. Caused by e9eb6dfd0e39be6a0a8c53e4a54344c475b9145d

portal-impl/src/com/liferay/portal/util/HttpImpl.java
@@ -1252,7 +1252,7 @@ public class HttpImpl implements Http {
 				String redirect = param.substring(pos + 1);
 
 				try {
-					redirect = decodeURL(redirect);
+					redirect = URLCodec.decodeURL(redirect, StringPool.UTF8);
 				}
 				catch (IllegalArgumentException iae) {
 					if (_log.isDebugEnabled()) {
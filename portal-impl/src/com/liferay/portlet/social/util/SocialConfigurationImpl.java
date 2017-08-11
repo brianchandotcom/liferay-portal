862b80c9f3f1f60619d35aafd0e1f1fe4ff9bc88
Alberto Chaparro <alberto.chaparro@liferay.com>
2017-08-11 12:08:18 +0200

    LPS-73481 Creating DTDs for 7.1.0 and apply them in XML files. Apply this new version to the rest of configuration files

--- portal-impl/src/com/liferay/portlet/social/util/SocialConfigurationImpl.java
+++ portal-impl/src/com/liferay/portlet/social/util/SocialConfigurationImpl.java
@@ -175,7 +175,8 @@ public class SocialConfigurationImpl implements SocialConfiguration {
 
 		if (!publicId.equals("-//Liferay//DTD Social 6.1.0//EN") &&
 			!publicId.equals("-//Liferay//DTD Social 6.2.0//EN") &&
-			!publicId.equals("-//Liferay//DTD Social 7.0.0//EN")) {
+			!publicId.equals("-//Liferay//DTD Social 7.0.0//EN") &&
+			!publicId.equals("-//Liferay//DTD Social 7.1.0//EN")) {
 
 			throw new DocumentException(
 				"Unsupported document type " + publicId);

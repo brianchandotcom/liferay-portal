brianchandotcom/liferay-portal

commit 4eaac551866d8c33ebb295b501106a6d953af969

LPS-74139 Allow SourceFormatterExcludes to accept both regex and glob patterns

modules/util/source-formatter/src/main/java/com/liferay/source/formatter/util/SourceFormatterUtil.java
@@ -19,6 +19,8 @@ import com.liferay.portal.kernel.util.CharPool;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.source.formatter.ExcludeSyntax;
+import com.liferay.source.formatter.ExcludeSyntaxPattern;
 import com.liferay.source.formatter.SourceFormatterExcludes;
 import com.liferay.source.formatter.checks.util.SourceUtil;
 
@@ -61,22 +63,45 @@ public class SourceFormatterUtil {
 			}
 		}
 
-		for (String exclude : sourceFormatterExcludes.getDefaultExcludes()) {
-			if (!exclude.contains(StringPool.DOLLAR)) {
-				excludeRegexList.add(_createRegex(exclude));
+		for (ExcludeSyntaxPattern excludeSyntaxPattern :
+				sourceFormatterExcludes.getDefaultExcludeSyntaxPatterns()) {
+
+			String excludePattern = excludeSyntaxPattern.getExcludePattern();
+			ExcludeSyntax excludeSyntax =
+				excludeSyntaxPattern.getExcludeSyntax();
+
+			if (excludeSyntax.equals(ExcludeSyntax.REGEX)) {
+				excludeRegexList.add(excludePattern);
+			}
+			else if (!excludePattern.contains(StringPool.DOLLAR)) {
+				excludeRegexList.add(_createRegex(excludePattern));
 			}
 		}
 
-		Map<String, List<String>> excludesMap =
-			sourceFormatterExcludes.getExcludesMap();
+		Map<String, List<ExcludeSyntaxPattern>> excludeSyntaxPatternsMap =
+			sourceFormatterExcludes.getExcludeSyntaxPatternsMap();
 
-		for (Map.Entry<String, List<String>> entry : excludesMap.entrySet()) {
-			List<String> excludesList = entry.getValue();
+		for (Map.Entry<String, List<ExcludeSyntaxPattern>> entry :
+				excludeSyntaxPatternsMap.entrySet()) {
+
+			List<ExcludeSyntaxPattern> excludeSyntaxPatterns = entry.getValue();
 
 			List<String> regexList = new ArrayList<>();
 
-			for (String exclude : excludesList) {
-				regexList.add(_createRegex(exclude));
+			for (ExcludeSyntaxPattern excludeSyntaxPattern :
+					excludeSyntaxPatterns) {
+
+				String excludePattern =
+					excludeSyntaxPattern.getExcludePattern();
+				ExcludeSyntax excludeSyntax =
+					excludeSyntaxPattern.getExcludeSyntax();
+
+				if (excludeSyntax.equals(ExcludeSyntax.REGEX)) {
+					regexList.add(excludePattern);
+				}
+				else {
+					regexList.add(_createRegex(excludePattern));
+				}
 			}
 
 			excludeRegexMap.put(entry.getKey(), regexList);
@@ -350,18 +375,24 @@ public class SourceFormatterUtil {
 		PathMatchers pathMatchers = new PathMatchers(FileSystems.getDefault());
 
 		for (String exclude : excludes) {
-			pathMatchers.addExclude(exclude);
+			pathMatchers.addExcludeSyntaxPattern(
+				new ExcludeSyntaxPattern(ExcludeSyntax.GLOB, exclude));
 		}
 
-		for (String exclude : sourceFormatterExcludes.getDefaultExcludes()) {
-			pathMatchers.addExclude(exclude);
+		for (ExcludeSyntaxPattern excludeSyntaxPattern :
+				sourceFormatterExcludes.getDefaultExcludeSyntaxPatterns()) {
+
+			pathMatchers.addExcludeSyntaxPattern(excludeSyntaxPattern);
 		}
 
-		Map<String, List<String>> excludesMap =
-			sourceFormatterExcludes.getExcludesMap();
+		Map<String, List<ExcludeSyntaxPattern>> excludeSyntaxPatternsMap =
+			sourceFormatterExcludes.getExcludeSyntaxPatternsMap();
 
-		for (Map.Entry<String, List<String>> entry : excludesMap.entrySet()) {
-			pathMatchers.addExcludes(entry.getKey(), entry.getValue());
+		for (Map.Entry<String, List<ExcludeSyntaxPattern>> entry :
+				excludeSyntaxPatternsMap.entrySet()) {
+
+			pathMatchers.addExcludeSyntaxPatterns(
+				entry.getKey(), entry.getValue());
 		}
 
 		for (String include : includes) {
@@ -508,43 +539,71 @@ public class SourceFormatterUtil {
 			_fileSystem = fileSystem;
 		}
 
-		public void addExclude(String exclude) {
-			if (!exclude.startsWith("**/")) {
-				exclude = "**/" + exclude;
+		public void addExcludeSyntaxPattern(
+			ExcludeSyntaxPattern excludeSyntaxPattern) {
+
+			String excludePattern = excludeSyntaxPattern.getExcludePattern();
+			ExcludeSyntax excludeSyntax =
+				excludeSyntaxPattern.getExcludeSyntax();
+
+			if (excludeSyntax.equals(ExcludeSyntax.GLOB) &&
+				!excludePattern.startsWith("**/")) {
+
+				excludePattern = "**/" + excludePattern;
 			}
 
-			if (exclude.endsWith("/**")) {
-				exclude = exclude.substring(0, exclude.length() - 3);
+			if (excludeSyntax.equals(ExcludeSyntax.GLOB) &&
+				excludePattern.endsWith("/**")) {
+
+				excludePattern = excludePattern.substring(
+					0, excludePattern.length() - 3);
 
 				_excludeDirPathMatchers.add(
-					_fileSystem.getPathMatcher("glob:" + exclude));
+					_fileSystem.getPathMatcher(
+						excludeSyntax.getValue() + ":" + excludePattern));
 			}
 			else {
 				_excludeFilePathMatchers.add(
-					_fileSystem.getPathMatcher("glob:" + exclude));
+					_fileSystem.getPathMatcher(
+						excludeSyntax.getValue() + ":" + excludePattern));
 			}
 		}
 
-		public void addExcludes(
-			String propertiesFileLocation, List<String> excludes) {
+		public void addExcludeSyntaxPatterns(
+			String propertiesFileLocation,
+			List<ExcludeSyntaxPattern> excludeSyntaxPatterns) {
 
 			List<PathMatcher> excludeDirPathMatcherList = new ArrayList<>();
 			List<PathMatcher> excludeFilePathMatcherList = new ArrayList<>();
 
-			for (String exclude : excludes) {
-				if (!exclude.startsWith("**/")) {
-					exclude = "**/" + exclude;
+			for (ExcludeSyntaxPattern excludeSyntaxPattern :
+					excludeSyntaxPatterns) {
+
+				String excludePattern =
+					excludeSyntaxPattern.getExcludePattern();
+				ExcludeSyntax excludeSyntax =
+					excludeSyntaxPattern.getExcludeSyntax();
+
+				if (excludeSyntax.equals(ExcludeSyntax.GLOB) &&
+					!excludePattern.startsWith("**/")) {
+
+					excludePattern = "**/" + excludePattern;
 				}
 
-				if (exclude.endsWith("/**")) {
-					exclude = exclude.substring(0, exclude.length() - 3);
+				if (excludeSyntax.equals(ExcludeSyntax.GLOB) &&
+					excludePattern.endsWith("/**")) {
 
-					excludeDirPathMatcherList.add(
-						_fileSystem.getPathMatcher("glob:" + exclude));
+					excludePattern = excludePattern.substring(
+						0, excludePattern.length() - 3);
+
+					_excludeDirPathMatchers.add(
+						_fileSystem.getPathMatcher(
+							excludeSyntax.getValue() + ":" + excludePattern));
 				}
 				else {
-					excludeFilePathMatcherList.add(
-						_fileSystem.getPathMatcher("glob:" + exclude));
+					_excludeFilePathMatchers.add(
+						_fileSystem.getPathMatcher(
+							excludeSyntax.getValue() + ":" + excludePattern));
 				}
 			}
 

modules/util/source-formatter/src/main/java/com/liferay/source/formatter/ExcludeSyntax.java
@@ -1,0 +1,53 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter;
+
+/**
+ * @author Peter Shin
+ */
+public enum ExcludeSyntax {
+
+	GLOB("glob"), REGEX("regex");
+
+	public static ExcludeSyntax parse(String value) {
+		String globValue = GLOB.getValue();
+		String regexValue = REGEX.getValue();
+
+		if (globValue.equals(value)) {
+			return GLOB;
+		}
+		else if (regexValue.equals(value)) {
+			return REGEX;
+		}
+
+		throw new IllegalArgumentException("Invalid value " + value);
+	}
+
+	public String getValue() {
+		return _value;
+	}
+
+	@Override
+	public String toString() {
+		return _value;
+	}
+
+	private ExcludeSyntax(String value) {
+		_value = value;
+	}
+
+	private final String _value;
+
+}

modules/util/source-formatter/src/main/java/com/liferay/source/formatter/ExcludeSyntaxPattern.java
@@ -1,0 +1,67 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter;
+
+/**
+ * @author Peter Shin
+ */
+public class ExcludeSyntaxPattern {
+
+	public ExcludeSyntaxPattern(
+		ExcludeSyntax excludeSyntax, String excludePattern) {
+
+		_excludeSyntax = excludeSyntax;
+		_excludePattern = excludePattern;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+
+		if (!(obj instanceof ExcludeSyntaxPattern)) {
+			return false;
+		}
+
+		ExcludeSyntaxPattern excludeSyntaxPattern = (ExcludeSyntaxPattern)obj;
+
+		ExcludeSyntax excludeSyntax = excludeSyntaxPattern.getExcludeSyntax();
+
+		if (!excludeSyntax.equals(_excludeSyntax)) {
+			return false;
+		}
+
+		String excludePattern = excludeSyntaxPattern.getExcludePattern();
+
+		if (!excludePattern.equals(_excludePattern)) {
+			return false;
+		}
+
+		return true;
+	}
+
+	public String getExcludePattern() {
+		return _excludePattern;
+	}
+
+	public ExcludeSyntax getExcludeSyntax() {
+		return _excludeSyntax;
+	}
+
+	private final String _excludePattern;
+	private final ExcludeSyntax _excludeSyntax;
+
+}

modules/util/source-formatter/src/main/java/com/liferay/source/formatter/SourceFormatter.java
@@ -51,6 +51,24 @@ import java.util.concurrent.Future;
  */
 public class SourceFormatter {
 
+	public static final ExcludeSyntaxPattern[]
+		DEFAULT_EXCLUDE_SYNTAX_PATTERNS = {
+			new ExcludeSyntaxPattern(ExcludeSyntax.GLOB, "**/.git/**"),
+			new ExcludeSyntaxPattern(ExcludeSyntax.GLOB, "**/.gradle/**"),
+			new ExcludeSyntaxPattern(ExcludeSyntax.GLOB, "**/bin/**"),
+			new ExcludeSyntaxPattern(ExcludeSyntax.GLOB, "**/build/**"),
+			new ExcludeSyntaxPattern(ExcludeSyntax.GLOB, "**/classes/**"),
+			new ExcludeSyntaxPattern(ExcludeSyntax.GLOB, "**/node_modules/**"),
+			new ExcludeSyntaxPattern(
+				ExcludeSyntax.GLOB, "**/npm-shrinkwrap.json"),
+			new ExcludeSyntaxPattern(
+				ExcludeSyntax.GLOB, "**/package-lock.json"),
+			new ExcludeSyntaxPattern(ExcludeSyntax.GLOB, "**/test-classes/**"),
+			new ExcludeSyntaxPattern(ExcludeSyntax.GLOB, "**/test-coverage/**"),
+			new ExcludeSyntaxPattern(ExcludeSyntax.GLOB, "**/test-results/**"),
+			new ExcludeSyntaxPattern(ExcludeSyntax.GLOB, "**/tmp/**")
+	};
+
 	public static void main(String[] args) throws Exception {
 		Map<String, String> arguments = ArgumentsUtil.parseArguments(args);
 
@@ -300,6 +318,23 @@ public class SourceFormatter {
 		return _firstSourceMismatchException;
 	}
 
+	private List<ExcludeSyntaxPattern> _getExcludeSyntaxPatterns(
+		String sourceFormatterExcludes) {
+
+		List<String> excludes = ListUtil.fromString(
+			sourceFormatterExcludes, StringPool.COMMA);
+
+		List<ExcludeSyntaxPattern> excludeSyntaxPatterns =
+			new ArrayList<ExcludeSyntaxPattern>();
+
+		for (String exclude : excludes) {
+			excludeSyntaxPatterns.add(
+				new ExcludeSyntaxPattern(ExcludeSyntax.GLOB, exclude));
+		}
+
+		return excludeSyntaxPatterns;
+	}
+
 	private int _getMaxDirLevel() {
 		File portalImplDir = SourceFormatterUtil.getFile(
 			_sourceFormatterArgs.getBaseDirName(), "portal-impl",
@@ -324,7 +359,7 @@ public class SourceFormatter {
 
 	private void _init() throws Exception {
 		_sourceFormatterExcludes = new SourceFormatterExcludes(
-			SetUtil.fromArray(_DEFAULT_EXCLUDES));
+			SetUtil.fromArray(DEFAULT_EXCLUDE_SYNTAX_PATTERNS));
 
 		// Find properties file in any parent directory
 
@@ -378,13 +413,12 @@ public class SourceFormatter {
 		}
 
 		if (FileUtil.exists(propertiesFileLocation + "portal-impl")) {
-			_sourceFormatterExcludes.addDefaultExcludes(
-				ListUtil.fromString(value, StringPool.COMMA));
+			_sourceFormatterExcludes.addDefaultExcludeSyntaxPatterns(
+				_getExcludeSyntaxPatterns(value));
 		}
 		else {
-			_sourceFormatterExcludes.addExcludes(
-				propertiesFileLocation,
-				ListUtil.fromString(value, StringPool.COMMA));
+			_sourceFormatterExcludes.addExcludeSyntaxPatterns(
+				propertiesFileLocation, _getExcludeSyntaxPatterns(value));
 		}
 
 		properties.remove("source.formatter.excludes");
@@ -411,13 +445,6 @@ public class SourceFormatter {
 				sourceProcessor.getFirstSourceMismatchException();
 		}
 	}
-
-	private static final String[] _DEFAULT_EXCLUDES = {
-		"**/.git/**", "**/.gradle/**", "**/bin/**", "**/build/**",
-		"**/classes/**", "**/node_modules/**", "**/npm-shrinkwrap.json",
-		"**/package-lock.json", "**/test-classes/**", "**/test-coverage/**",
-		"**/test-results/**", "**/tmp/**"
-	};
 
 	private static final String _PROPERTIES_FILE_NAME =
 		"source-formatter.properties";

modules/util/source-formatter/src/main/java/com/liferay/source/formatter/SourceFormatterExcludes.java
@@ -24,29 +24,38 @@ import java.util.Set;
  */
 public class SourceFormatterExcludes {
 
-	public SourceFormatterExcludes(Set<String> defaultExcludes) {
-		_defaultExcludes = defaultExcludes;
+	public SourceFormatterExcludes(
+		Set<ExcludeSyntaxPattern> defaultExcludeSyntaxPatterns) {
+
+		_defaultExcludeSyntaxPatterns = defaultExcludeSyntaxPatterns;
 	}
 
-	public void addDefaultExcludes(List<String> defaultExcludes) {
-		_defaultExcludes.addAll(defaultExcludes);
+	public void addDefaultExcludeSyntaxPatterns(
+		List<ExcludeSyntaxPattern> defaultExcludeSyntaxPatterns) {
+
+		_defaultExcludeSyntaxPatterns.addAll(defaultExcludeSyntaxPatterns);
 	}
 
-	public void addExcludes(
-		String propertiesFileLocation, List<String> exludes) {
+	public void addExcludeSyntaxPatterns(
+		String propertiesFileLocation,
+		List<ExcludeSyntaxPattern> excludeSyntaxPatterns) {
 
-		_excludesMap.put(propertiesFileLocation, exludes);
+		_excludeSyntaxPatternsMap.put(
+			propertiesFileLocation, excludeSyntaxPatterns);
 	}
 
-	public Set<String> getDefaultExcludes() {
-		return _defaultExcludes;
+	public Set<ExcludeSyntaxPattern> getDefaultExcludeSyntaxPatterns() {
+		return _defaultExcludeSyntaxPatterns;
 	}
 
-	public Map<String, List<String>> getExcludesMap() {
-		return _excludesMap;
+	public Map<String, List<ExcludeSyntaxPattern>>
+		getExcludeSyntaxPatternsMap() {
+
+		return _excludeSyntaxPatternsMap;
 	}
 
-	private final Set<String> _defaultExcludes;
-	private Map<String, List<String>> _excludesMap = new HashMap<>();
+	private final Set<ExcludeSyntaxPattern> _defaultExcludeSyntaxPatterns;
+	private Map<String, List<ExcludeSyntaxPattern>> _excludeSyntaxPatternsMap =
+		new HashMap<>();
 
 }

modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/checks/MissingOverrideCheck.java
@@ -19,6 +19,8 @@ import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.SetUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Tuple;
+import com.liferay.source.formatter.ExcludeSyntaxPattern;
+import com.liferay.source.formatter.SourceFormatter;
 import com.liferay.source.formatter.SourceFormatterExcludes;
 import com.liferay.source.formatter.checks.util.SourceUtil;
 import com.liferay.source.formatter.util.SourceFormatterUtil;
@@ -44,6 +46,7 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author Hugo Huijser
@@ -169,9 +172,12 @@ public class MissingOverrideCheck extends AbstractCheck {
 			}
 		}
 
+		Set<ExcludeSyntaxPattern> defaultExcludeSyntaxPatterns =
+			SetUtil.fromArray(SourceFormatter.DEFAULT_EXCLUDE_SYNTAX_PATTERNS);
+
 		List<String> fileNames = SourceFormatterUtil.scanForFiles(
 			absolutePath + "/", new String[0], new String[] {"**/*.java"},
-			new SourceFormatterExcludes(SetUtil.fromArray(_EXCLUDES)), true);
+			new SourceFormatterExcludes(defaultExcludeSyntaxPatterns), true);
 
 		for (String curFileName : fileNames) {
 			curFileName = StringUtil.replace(
@@ -350,13 +356,6 @@ public class MissingOverrideCheck extends AbstractCheck {
 
 		return false;
 	}
-
-	private static final String[] _EXCLUDES = {
-		"**/.git/**", "**/.gradle/**", "**/bin/**", "**/build/**",
-		"**/classes/**", "**/node_modules/**", "**/npm-shrinkwrap.json",
-		"**/package-lock.json", "**/test-classes/**", "**/test-coverage/**",
-		"**/test-results/**", "**/tmp/**"
-	};
 
 	private static final double _LOWEST_SUPPORTED_JAVA_VERSION = 1.7;
 

commit 0e85c2972cee5c54b61be03d5cbf3f89cd13c915

LPS-74139 Fix logic caused by 4eaac551866d8c33ebb295b501106a6d953af969

modules/util/source-formatter/src/main/java/com/liferay/source/formatter/util/SourceFormatterUtil.java
@@ -598,12 +598,12 @@ public class SourceFormatterUtil {
 					excludePattern = excludePattern.substring(
 						0, excludePattern.length() - 3);
 
-					_excludeDirPathMatchers.add(
+					excludeDirPathMatcherList.add(
 						_fileSystem.getPathMatcher(
 							excludeSyntax.getValue() + ":" + excludePattern));
 				}
 				else {
-					_excludeFilePathMatchers.add(
+					excludeFilePathMatcherList.add(
 						_fileSystem.getPathMatcher(
 							excludeSyntax.getValue() + ":" + excludePattern));
 				}
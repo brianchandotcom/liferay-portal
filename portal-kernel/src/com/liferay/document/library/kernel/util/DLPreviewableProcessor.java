46e61b839bda1aa6ff8aa5964f0b391dc491d7af
Preston-Crary <preston.crary@liferay.com>
2017-09-27 14:01:42 -0700

    LPS-74772 SF.

--- portal-kernel/src/com/liferay/document/library/kernel/util/DLPreviewableProcessor.java
+++ portal-kernel/src/com/liferay/document/library/kernel/util/DLPreviewableProcessor.java
@@ -530,10 +530,15 @@ public abstract class DLPreviewableProcessor implements DLProcessor {
 
 		fileEntryElement.addAttribute(binPathName, binPath);
 
-		int realFileIndex = fileIndex < 0 ? 0 : fileIndex + 1;
+		if (fileIndex < 0) {
+			fileIndex = 0;
+		}
+		else {
+			fileIndex++;
+		}
 
-		try (InputStream is =
-				doGetPreviewAsStream(fileVersion, realFileIndex, previewType)) {
+		try (InputStream is = doGetPreviewAsStream(
+				fileVersion, fileIndex, previewType)) {
 
 			exportBinary(
 				portletDataContext, fileEntryElement, fileVersion, is, binPath,
@@ -994,10 +999,15 @@ public abstract class DLPreviewableProcessor implements DLProcessor {
 				return;
 			}
 
-			int realFileIndex = fileIndex < 0 ? 0 : fileIndex + 1;
+			if (fileIndex < 0) {
+				fileIndex = 0;
+			}
+			else {
+				fileIndex++;
+			}
 
 			try (InputStream is = doGetPreviewAsStream(
-					fileVersion, realFileIndex, previewType)) {
+					fileVersion, fileIndex, previewType)) {
 
 				addFileToStore(
 					portletDataContext.getCompanyId(), PREVIEW_PATH,

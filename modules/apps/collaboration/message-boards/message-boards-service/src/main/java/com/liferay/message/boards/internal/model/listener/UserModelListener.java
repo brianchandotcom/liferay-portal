6759bbf29092d8cdb7d0696deb32d7793da9647e
Adolfo Pérez <adolfo.perez@liferay.com>
2017-11-06 10:32:52 +0100

    LPS-75319 Remove references in User services

--- modules/apps/collaboration/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/internal/model/listener/UserModelListener.java
+++ modules/apps/collaboration/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/internal/model/listener/UserModelListener.java
@@ -15,6 +15,7 @@
 package com.liferay.message.boards.internal.model.listener;
 
 import com.liferay.message.boards.service.MBStatsUserLocalService;
+import com.liferay.message.boards.service.MBThreadFlagLocalService;
 import com.liferay.portal.kernel.model.BaseModelListener;
 import com.liferay.portal.kernel.model.ModelListener;
 import com.liferay.portal.kernel.model.User;
@@ -31,9 +32,13 @@ public class UserModelListener extends BaseModelListener<User> {
 	@Override
 	public void onBeforeRemove(User user) {
 		_mbStatsUserLocalService.deleteStatsUsersByUserId(user.getUserId());
+		_mbThreadFlagLocalService.deleteThreadFlagsByUserId(user.getUserId());
 	}
 
 	@Reference
 	private MBStatsUserLocalService _mbStatsUserLocalService;
+
+	@Reference
+	private MBThreadFlagLocalService _mbThreadFlagLocalService;
 
 }

f1586285b02daa5ae3ad590fde473b7b2f866053
Máté Thurzó <mate.thurzo@liferay.com>
2017-11-20 17:03:14 +0100

    LPS-75933 Fix test rules

--- modules/apps/foundation/user-groups-admin/user-groups-admin-test/src/testIntegration/java/com/liferay/user/groups/admin/lar/test/UserGroupsAdminPortletDataHandlerTest.java
+++ modules/apps/foundation/user-groups-admin/user-groups-admin-test/src/testIntegration/java/com/liferay/user/groups/admin/lar/test/UserGroupsAdminPortletDataHandlerTest.java
@@ -17,6 +17,7 @@ package com.liferay.user.groups.admin.lar.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.exportimport.kernel.lar.DataLevel;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
 import com.liferay.portal.lar.test.BasePortletDataHandlerTestCase;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.user.groups.admin.constants.UserGroupsAdminPortletKeys;
@@ -35,7 +36,9 @@ public class UserGroupsAdminPortletDataHandlerTest
 	@ClassRule
 	@Rule
 	public static final AggregateTestRule aggregateTestRule =
-		new LiferayIntegrationTestRule();
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			SynchronousDestinationTestRule.INSTANCE);
 
 	@Override
 	protected void addStagedModels() {
--- modules/apps/foundation/users-admin/users-admin-test/src/testIntegration/java/com/liferay/users/admin/internal/exportimport/data/handler/test/UsersAdminPortletDataHandlerTest.java
+++ modules/apps/foundation/users-admin/users-admin-test/src/testIntegration/java/com/liferay/users/admin/internal/exportimport/data/handler/test/UsersAdminPortletDataHandlerTest.java
@@ -19,6 +19,7 @@ import com.liferay.exportimport.kernel.lar.DataLevel;
 import com.liferay.portal.kernel.model.Organization;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
 import com.liferay.portal.kernel.test.util.OrganizationTestUtil;
 import com.liferay.portal.lar.test.BasePortletDataHandlerTestCase;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
@@ -38,7 +39,9 @@ public class UsersAdminPortletDataHandlerTest
 	@ClassRule
 	@Rule
 	public static final AggregateTestRule aggregateTestRule =
-		new LiferayIntegrationTestRule();
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			SynchronousDestinationTestRule.INSTANCE);
 
 	@Override
 	protected void addStagedModels() throws Exception {

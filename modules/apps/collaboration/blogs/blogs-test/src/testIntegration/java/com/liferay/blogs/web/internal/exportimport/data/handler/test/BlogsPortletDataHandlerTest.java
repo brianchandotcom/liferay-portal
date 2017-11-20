b8eb8e37f7d8de475e64e09856a394c91613db32
Máté Thurzó <mate.thurzo@liferay.com>
2017-11-20 16:52:50 +0100

    LPS-75933 Merge the two BlogsPDH test class

--- modules/apps/collaboration/blogs/blogs-test/src/testIntegration/java/com/liferay/blogs/web/internal/exportimport/data/handler/test/BlogsPortletDataHandlerTest.java
+++ modules/apps/collaboration/blogs/blogs-test/src/testIntegration/java/com/liferay/blogs/web/internal/exportimport/data/handler/test/BlogsPortletDataHandlerTest.java
@@ -17,6 +17,7 @@ package com.liferay.blogs.web.internal.exportimport.data.handler.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.blogs.constants.BlogsPortletKeys;
 import com.liferay.blogs.service.BlogsEntryLocalServiceUtil;
+import com.liferay.exportimport.kernel.lar.DataLevel;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
@@ -25,6 +26,7 @@ import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.lar.test.BasePortletDataHandlerTestCase;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
+import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
@@ -55,6 +57,26 @@ public class BlogsPortletDataHandlerTest
 	@Override
 	protected String getPortletId() {
 		return BlogsPortletKeys.BLOGS;
+	}
+
+	@Override
+	protected DataLevel getDataLevel() {
+		return DataLevel.SITE;
+	}
+
+	@Override
+	protected boolean isDataPortalLevel() {
+		return false;
+	}
+
+	@Override
+	protected boolean isDataPortletInstanceLevel() {
+		return false;
+	}
+
+	@Override
+	protected boolean isDataSiteLevel() {
+		return true;
 	}
 
 }

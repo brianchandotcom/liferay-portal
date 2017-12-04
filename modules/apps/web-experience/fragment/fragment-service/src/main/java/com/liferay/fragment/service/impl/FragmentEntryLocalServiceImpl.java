d63cb369340228546e5a9e9d4a4fec12cb5ebfb1
pavel-savinov <pavel.savinov@liferay.com>
2017-12-04 16:49:25 +0100

    LPS-76269 Use fragment entry processor util to validate fragment entry HTML

--- modules/apps/web-experience/fragment/fragment-service/src/main/java/com/liferay/fragment/service/impl/FragmentEntryLocalServiceImpl.java
+++ modules/apps/web-experience/fragment/fragment-service/src/main/java/com/liferay/fragment/service/impl/FragmentEntryLocalServiceImpl.java
@@ -15,8 +15,10 @@
 package com.liferay.fragment.service.impl;
 
 import com.liferay.fragment.exception.DuplicateFragmentEntryException;
+import com.liferay.fragment.exception.FragmentEntryContentException;
 import com.liferay.fragment.exception.FragmentEntryNameException;
 import com.liferay.fragment.model.FragmentEntry;
+import com.liferay.fragment.processor.FragmentEntryProcessorUtil;
 import com.liferay.fragment.service.base.FragmentEntryLocalServiceBaseImpl;
 import com.liferay.html.preview.model.HtmlPreviewEntry;
 import com.liferay.html.preview.service.HtmlPreviewEntryLocalService;
@@ -31,7 +33,6 @@ import com.liferay.portal.spring.extender.service.ServiceReference;
 
 import java.util.Date;
 import java.util.List;
-import java.util.Objects;
 
 /**
  * @author Jürgen Kappler
@@ -49,7 +50,7 @@ public class FragmentEntryLocalServiceImpl
 
 		User user = userLocalService.getUser(userId);
 
-		validate(groupId, name);
+		validate(groupId, 0, name, html);
 
 		long fragmentEntryId = counterLocalService.increment();
 
@@ -189,9 +190,7 @@ public class FragmentEntryLocalServiceImpl
 		FragmentEntry fragmentEntry = fragmentEntryPersistence.findByPrimaryKey(
 			fragmentEntryId);
 
-		if (!Objects.equals(fragmentEntry.getName(), name)) {
-			validate(fragmentEntry.getGroupId(), name);
-		}
+		validate(fragmentEntry.getGroupId(), fragmentEntryId, name, html);
 
 		fragmentEntry.setModifiedDate(new Date());
 		fragmentEntry.setName(name);
@@ -206,7 +205,10 @@ public class FragmentEntryLocalServiceImpl
 		return fragmentEntry;
 	}
 
-	protected void validate(long groupId, String name) throws PortalException {
+	protected void validate(
+			long groupId, long fragmentEntryId, String name, String html)
+		throws PortalException {
+
 		if (Validator.isNull(name)) {
 			throw new FragmentEntryNameException(
 				"Name must not be null for group " + groupId);
@@ -215,10 +217,19 @@ public class FragmentEntryLocalServiceImpl
 		FragmentEntry fragmentEntry = fragmentEntryPersistence.fetchByG_N(
 			groupId, name);
 
-		if (fragmentEntry != null) {
+		if ((fragmentEntry != null) &&
+			(fragmentEntry.getFragmentEntryId() != fragmentEntryId)) {
+
 			throw new DuplicateFragmentEntryException(name);
 		}
+
+		if (!_fragmentEntryProcessorUtil.validateFragmentEntryHtml(html)) {
+			throw new FragmentEntryContentException("invalid-fragment-html");
+		}
 	}
+
+	@ServiceReference(type = FragmentEntryProcessorUtil.class)
+	private FragmentEntryProcessorUtil _fragmentEntryProcessorUtil;
 
 	private HtmlPreviewEntry _updateHtmlPreviewEntry(
 			FragmentEntry fragmentEntry, ServiceContext serviceContext)

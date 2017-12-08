88974b3e1d787ae9a064878a2a5ff8a847ed4a82
Máté Thurzó <mate.thurzo@liferay.com>
2017-12-08 17:01:39 +0100

    LPS-76372 Autogenerated

--- portal-kernel/src/com/liferay/asset/kernel/service/AssetEntryLocalService.java
+++ portal-kernel/src/com/liferay/asset/kernel/service/AssetEntryLocalService.java
@@ -359,6 +359,12 @@ public interface AssetEntryLocalService extends BaseLocalService,
 		throws PortalException;
 
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public long getEntryPriority(long classNameId, long classPK);
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public long getEntryPriority(java.lang.String className, long classPK);
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public List<AssetEntry> getGroupEntries(long groupId);
 
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)

3836b186e605ceef33705e3fc94dc43cee42f3dd
drewbrokke <drew.brokke@liferay.com>
2018-01-02 16:19:49 -0800

    LPS-75598 user-associated-data-impl - updates the package path of UADRegistryImpl.java to match the interface

--- modules/apps/foundation/user-associated-data/user-associated-data-impl/src/main/java/com/liferay/user/associated/data/internal/registry/UADRegistryImpl.java
+++ modules/apps/foundation/user-associated-data/user-associated-data-impl/src/main/java/com/liferay/user/associated/data/internal/registry/UADRegistryImpl.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.user.associated.data.internal;
+package com.liferay.user.associated.data.internal.registry;
 
 import com.liferay.osgi.service.tracker.collections.map.ServiceReferenceMapper;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;

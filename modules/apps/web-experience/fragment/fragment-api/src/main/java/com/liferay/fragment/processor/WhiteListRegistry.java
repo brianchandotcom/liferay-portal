f75601b51adb6a2cdb075dab2c35349f67872061
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2018-02-04 21:00:19 +0100

    LPS-77338 Adds white list registry class to track new white list entries

---
+++ modules/apps/web-experience/fragment/fragment-api/src/main/java/com/liferay/fragment/processor/WhiteListRegistry.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.processor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(immediate = true, service = WhiteListRegistry.class)
+public class WhiteListRegistry {
+
+	public String[] getWhiteListEntriesAttributes(String tagName) {
+		return _whiteListEntries.get(tagName);
+	}
+
+	public List<String> getWhiteListEntriesTagNames() {
+		return new ArrayList<>(_whiteListEntries.keySet());
+	}
+
+	@Reference(
+		cardinality = ReferenceCardinality.MULTIPLE,
+		policy = ReferencePolicy.DYNAMIC, unbind = "unsetWhiteListEntry"
+	)
+	protected void setWhiteListEntry(WhiteListEntry whiteListEntry) {
+		_whiteListEntries.put(
+			whiteListEntry.getTagName(), whiteListEntry.getAttributes());
+	}
+
+	protected void unsetWhiteListEntry(WhiteListEntry whiteListEntry) {
+		_whiteListEntries.remove(whiteListEntry.getTagName());
+	}
+
+	private final Map<String, String[]> _whiteListEntries =
+		new ConcurrentHashMap<>();
+
+}

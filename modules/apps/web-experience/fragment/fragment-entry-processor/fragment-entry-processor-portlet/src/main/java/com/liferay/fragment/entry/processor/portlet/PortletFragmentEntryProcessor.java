fedff695f2bd107ddd2e095f8cc2bc937efec65c
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2018-02-08 16:18:59 +0100

    LPS-76925 Update usages

--- modules/apps/web-experience/fragment/fragment-entry-processor/fragment-entry-processor-portlet/src/main/java/com/liferay/fragment/entry/processor/portlet/PortletFragmentEntryProcessor.java
+++ modules/apps/web-experience/fragment/fragment-entry-processor/fragment-entry-processor-portlet/src/main/java/com/liferay/fragment/entry/processor/portlet/PortletFragmentEntryProcessor.java
@@ -20,14 +20,12 @@ import com.liferay.fragment.processor.PortletRegistry;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.language.LanguageUtil;
-import com.liferay.portal.kernel.service.PortletLocalServiceUtil;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
 
 import java.util.ResourceBundle;
-
-import javax.portlet.Portlet;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -62,18 +60,19 @@ public class PortletFragmentEntryProcessor implements FragmentEntryProcessor {
 			String alias = StringUtil.replace(
 				tagName, "lfr-app-", StringPool.BLANK);
 
-			Portlet portlet = _portletRegistry.getPortlet(alias);
+			String portletName = _portletRegistry.getPortletName(alias);
 
-			if (portlet == null) {
+			if (Validator.isNull(portletName)) {
 				throw new FragmentEntryContentException(
 					LanguageUtil.format(
 						_resourceBundle,
 						"there-is-no-portlet-available-for-alias-x", alias));
 			}
 
+
 			Element runtimeHTMLTag = new Element("@liferay_portlet.runtime");
 
-			runtimeHTMLTag.attr("portletName", portlet);
+			runtimeHTMLTag.attr("portletName", portletName);
 
 			element.replaceWith(runtimeHTMLTag);
 		}
@@ -97,9 +96,7 @@ public class PortletFragmentEntryProcessor implements FragmentEntryProcessor {
 			String alias = StringUtil.replace(
 				htmlTagName, "lfr-app-", StringPool.BLANK);
 
-			Portlet portlet = _portletRegistry.getPortlet(alias);
-
-			if (portlet == null) {
+			if (Validator.isNull(_portletRegistry.getPortletName(alias))) {
 				throw new FragmentEntryContentException(
 					LanguageUtil.format(
 						_resourceBundle,

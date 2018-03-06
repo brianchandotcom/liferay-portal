brianchandotcom/liferay-portal

commit 8353f148910fb1b0057287462ff35e5079654b76

LPS-73338 Rename attribute to showDisableCheckbox so it’s purpose is clear

portal-web/docroot/html/taglib/ui/input_date/page.jsp
@@ -26,7 +26,6 @@ if (GetterUtil.getBoolean((String)request.getAttribute("liferay-ui:input-date:di
 String cssClass = GetterUtil.getString((String)request.getAttribute("liferay-ui:input-date:cssClass"));
 String dateTogglerCheckboxLabel = GetterUtil.getString((String)request.getAttribute("liferay-ui:input-date:dateTogglerCheckboxLabel"), "disable");
 boolean disabled = GetterUtil.getBoolean((String)request.getAttribute("liferay-ui:input-date:disabled"));
-boolean disableCheckbox = GetterUtil.getBoolean((String)request.getAttribute("liferay-ui:input-date:disableCheckbox"));
 String dayParam = GetterUtil.getString((String)request.getAttribute("liferay-ui:input-date:dayParam"));
 int dayValue = GetterUtil.getInteger((String)request.getAttribute("liferay-ui:input-date:dayValue"));
 int firstDayOfWeek = GetterUtil.getInteger((String)request.getAttribute("liferay-ui:input-date:firstDayOfWeek"));
@@ -38,6 +37,7 @@ int monthValue = GetterUtil.getInteger((String)request.getAttribute("liferay-ui:
 String name = GetterUtil.getString((String)request.getAttribute("liferay-ui:input-date:name"));
 boolean nullable = GetterUtil.getBoolean((String)request.getAttribute("liferay-ui:input-date:nullable"));
 boolean required = GetterUtil.getBoolean((String)request.getAttribute("liferay-ui:input-date:required"));
+boolean showDisableCheckbox = GetterUtil.getBoolean((String)request.getAttribute("liferay-ui:input-date:showDisableCheckbox"));
 String yearParam = GetterUtil.getString((String)request.getAttribute("liferay-ui:input-date:yearParam"));
 int yearValue = GetterUtil.getInteger((String)request.getAttribute("liferay-ui:input-date:yearValue"));
 
@@ -119,7 +119,7 @@ else {
 	<input <%= disabled ? "disabled=\"disabled\"" : "" %> id="<%= yearParamId %>" name="<%= namespace + HtmlUtil.escapeAttribute(yearParam) %>" type="hidden" value="<%= yearValue %>" />
 </span>
 
-<c:if test="<%= nullable && !required && !disableCheckbox %>">
+<c:if test="<%= nullable && !required && !showDisableCheckbox %>">
 
 	<%
 	String dateTogglerCheckboxName = TextFormatter.format(dateTogglerCheckboxLabel, TextFormatter.M);

util-taglib/src/META-INF/liferay-ui.tld
@@ -1698,12 +1698,6 @@
 			<type>int</type>
 		</attribute>
 		<attribute>
-			<description>Whether to disable the checkbox that disables input field.</description>
-			<name>disableCheckbox</name>
-			<required>false</required>
-			<rtexprvalue>true</rtexprvalue>
-		</attribute>
-		<attribute>
 			<description>Whether to disable the input field. The default value is <![CDATA[<code>false</code>]]>.</description>
 			<name>disabled</name>
 			<required>false</required>
@@ -1779,6 +1773,12 @@
 			<required>false</required>
 			<rtexprvalue>true</rtexprvalue>
 			<type>boolean</type>
+		</attribute>
+		<attribute>
+			<description>Whether to display the checkbox that disables input field.</description>
+			<name>showDisableCheckbox</name>
+			<required>false</required>
+			<rtexprvalue>true</rtexprvalue>
 		</attribute>
 		<attribute>
 			<description>A variable name to refer to the year of the component.</description>

util-taglib/src/com/liferay/taglib/ui/InputDateTag.java
@@ -52,10 +52,6 @@ public class InputDateTag extends BaseValidatorTagSupport {
 		_dayValue = dayValue;
 	}
 
-	public void setDisableCheckbox(boolean disableCheckbox) {
-		_disableCheckbox = disableCheckbox;
-	}
-
 	public void setDisabled(boolean disabled) {
 		_disabled = disabled;
 	}
@@ -104,6 +100,10 @@ public class InputDateTag extends BaseValidatorTagSupport {
 		_required = required;
 	}
 
+	public void setShowDisableCheckbox(boolean showDisableCheckbox) {
+		_showDisableCheckbox = showDisableCheckbox;
+	}
+
 	public void setYearParam(String yearParam) {
 		_yearParam = yearParam;
 	}
@@ -120,7 +120,6 @@ public class InputDateTag extends BaseValidatorTagSupport {
 		_dayParam = null;
 		_dayValue = 0;
 		_disabled = false;
-		_disableCheckbox = false;
 		_disableNamespace = false;
 		_firstDayOfWeek = Calendar.SUNDAY - 2;
 		_firstEnabledDate = null;
@@ -131,6 +130,7 @@ public class InputDateTag extends BaseValidatorTagSupport {
 		_monthValue = -1;
 		_nullable = false;
 		_required = false;
+		_showDisableCheckbox = false;
 		_yearParam = null;
 		_yearValue = 0;
 	}
@@ -151,9 +151,6 @@ public class InputDateTag extends BaseValidatorTagSupport {
 		request.setAttribute("liferay-ui:input-date:dayParam", _dayParam);
 		request.setAttribute(
 			"liferay-ui:input-date:dayValue", String.valueOf(_dayValue));
-		request.setAttribute(
-			"liferay-ui:input-date:disableCheckbox",
-			String.valueOf(_disableCheckbox));
 		request.setAttribute(
 			"liferay-ui:input-date:disabled", String.valueOf(_disabled));
 		request.setAttribute(
@@ -177,6 +174,9 @@ public class InputDateTag extends BaseValidatorTagSupport {
 			"liferay-ui:input-date:nullable", String.valueOf(_nullable));
 		request.setAttribute(
 			"liferay-ui:input-date:required", String.valueOf(_required));
+		request.setAttribute(
+			"liferay-ui:input-date:showDisableCheckbox",
+			String.valueOf(_showDisableCheckbox));
 		request.setAttribute("liferay-ui:input-date:yearParam", _yearParam);
 		request.setAttribute(
 			"liferay-ui:input-date:yearValue", String.valueOf(_yearValue));
@@ -189,7 +189,6 @@ public class InputDateTag extends BaseValidatorTagSupport {
 	private String _dateTogglerCheckboxLabel;
 	private String _dayParam;
 	private int _dayValue;
-	private boolean _disableCheckbox;
 	private boolean _disabled;
 	private boolean _disableNamespace;
 	private int _firstDayOfWeek = Calendar.SUNDAY - 2;
@@ -202,6 +201,7 @@ public class InputDateTag extends BaseValidatorTagSupport {
 	private String _name;
 	private boolean _nullable;
 	private boolean _required;
+	private boolean _showDisableCheckbox;
 	private String _yearParam;
 	private int _yearValue;
 

commit 0a1c715960b9157b23a9cbd7ef1b7d54f820aa49

LPS-78475 Fix error caused by 8353f148910fb1b0057287462ff35e5079654b76

portal-web/docroot/html/taglib/ui/input_date/page.jsp
@@ -128,7 +128,7 @@ else {
 
 </span>
 
-<c:if test="<%= nullable && !required && !showDisableCheckbox %>">
+<c:if test="<%= nullable && !required && showDisableCheckbox %>">
 
 	<%
 	String dateTogglerCheckboxName = TextFormatter.format(dateTogglerCheckboxLabel, TextFormatter.M);
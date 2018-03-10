brianchandotcom/liferay-portal

commit 78f016d93b0ce45b06e1ce76cfc6189f969af138

LPS-78451 Rename

modules/apps/foundation/frontend-editor/frontend-editor-taglib/src/main/java/com/liferay/frontend/editor/taglib/internal/activator/EditorRendererBundleActivator.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.frontend.editor.taglib.internal;
+package com.liferay.frontend.editor.taglib.internal.activator;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
@@ -20,7 +20,7 @@ import org.osgi.framework.BundleContext;
 /**
  * @author Iván Zaera Avellón
  */
-public class Activator implements BundleActivator {
+public class EditorRendererBundleActivator implements BundleActivator {
 
 	@Override
 	public void start(BundleContext bundleContext) throws Exception {

modules/apps/foundation/frontend-editor/frontend-editor-taglib/bnd.bnd
@@ -1,4 +1,4 @@
-Bundle-Activator: com.liferay.frontend.editor.taglib.internal.Activator
+Bundle-Activator: com.liferay.frontend.editor.taglib.internal.activator.EditorRendererBundleActivator
 Bundle-Name: Liferay Frontend Editor Taglib
 Bundle-SymbolicName: com.liferay.frontend.editor.taglib
 Bundle-Version: 1.0.0

commit c7ece693a125adf967bb97b28e8c072bd589db0a

LPS-78451 Fix compile error caused by 78f016d93b0ce45b06e1ce76cfc6189f969af138

modules/apps/foundation/frontend-editor/frontend-editor-taglib/src/main/java/com/liferay/frontend/editor/taglib/internal/EditorRendererUtil.java
@@ -39,7 +39,7 @@ public class EditorRendererUtil {
 		return _editorRendererMap.get(name);
 	}
 
-	protected static void start(final BundleContext bundleContext) {
+	public static void start(final BundleContext bundleContext) {
 		_serviceTracker = new ServiceTracker<EditorRenderer, EditorRenderer>(
 			bundleContext, EditorRenderer.class,
 			new ServiceTrackerCustomizer<EditorRenderer, EditorRenderer>() {
@@ -92,7 +92,7 @@ public class EditorRendererUtil {
 		_serviceTracker.open();
 	}
 
-	protected static void stop() {
+	public static void stop() {
 		_serviceTracker.close();
 
 		_serviceTracker = null;

modules/apps/foundation/frontend-editor/frontend-editor-taglib/src/main/java/com/liferay/frontend/editor/taglib/internal/activator/EditorRendererBundleActivator.java
@@ -14,6 +14,8 @@
 
 package com.liferay.frontend.editor.taglib.internal.activator;
 
+import com.liferay.frontend.editor.taglib.internal.EditorRendererUtil;
+
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
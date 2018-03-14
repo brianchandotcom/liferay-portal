636b4e57be51ff9204c4c96e20d4f4d5defbec30
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2018-03-14 15:54:03 +0100

    OAuth2Authorization minimal classes used by Finder

---
+++ modules/apps/oauth2-provider/oauth2-provider-api/src/main/java/com/liferay/oauth2/provider/model/OAuth2Authorization.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.oauth2.provider.model;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author Tomas Polesovsky
+ */
+public interface OAuth2Authorization extends OAuth2AuthorizationModel {
+
+	public Date getAccessTokenExpirationDate();
+
+	public long getCompanyId();
+
+	public Date getCreateDate();
+
+	public long getOAuth2ApplicationId();
+
+	public long getOAuth2RefreshTokenId();
+
+	public long getOAuth2TokenId();
+
+	public Date getRefreshTokenExpirationDate();
+
+	public String getRemoteIPInfo();
+
+	public String getScopes();
+
+	public List<String> getScopesList();
+
+	public long getUserId();
+
+	public String getUserName();
+
+	public void setAccessTokenExpirationDate(Date accessTokenExpirationDate);
+
+	public void setCompanyId(long companyId);
+
+	public void setCreateDate(Date createDate);
+
+	public void setOAuth2ApplicationId(long oAuth2ApplicationId);
+
+	public void setOAuth2RefreshTokenId(long oAuth2RefreshTokenId);
+
+	public void setOAuth2TokenId(long oAuth2TokenId);
+
+	public void setRefreshTokenExpirationDate(Date refreshTokenExpirationDate);
+
+	public void setRemoteIPInfo(String remoteIPInfo);
+
+	public void setScopes(String scopes);
+
+	public void setScopesList(List<String> scopesList);
+
+	public void setUserId(long userId);
+
+	public void setUserName(String userName);
+
+}

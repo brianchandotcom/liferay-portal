7903b202508f6a1cf5a8cd62cb16c6fe804b13eb
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2018-03-15 16:12:20 +0100

    OAUTH2-119 SF auto

--- modules/apps/oauth2-provider/oauth2-provider-service/src/main/java/com/liferay/oauth2/provider/service/impl/OAuth2RefreshTokenServiceImpl.java
+++ modules/apps/oauth2-provider/oauth2-provider-service/src/main/java/com/liferay/oauth2/provider/service/impl/OAuth2RefreshTokenServiceImpl.java
@@ -32,9 +32,11 @@ import com.liferay.oauth2.provider.service.base.OAuth2RefreshTokenServiceBaseImp
  */
 public class OAuth2RefreshTokenServiceImpl
 	extends OAuth2RefreshTokenServiceBaseImpl {
-	/*
+
+	/**
 	 * NOTE FOR DEVELOPERS:
 	 *
 	 * Never reference this class directly. Always use {@link com.liferay.oauth2.provider.service.OAuth2RefreshTokenServiceUtil} to access the o auth2 refresh token remote service.
 	 */
+
 }

4e8b40b8a6341ff9f8b202d406eaf7d5a459c822
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2018-04-15 21:53:53 +0200

    LPS-79631 SF

--- modules/apps/foundation/frontend-taglib/frontend-taglib-clay/src/main/java/com/liferay/frontend/taglib/clay/servlet/taglib/soy/ManagementToolbarTag.java
+++ modules/apps/foundation/frontend-taglib/frontend-taglib-clay/src/main/java/com/liferay/frontend/taglib/clay/servlet/taglib/soy/ManagementToolbarTag.java
@@ -74,13 +74,8 @@ public class ManagementToolbarTag extends BaseClayTag {
 
 		CreationMenu creationMenu = (CreationMenu)context.get("creationMenu");
 
-		if (Validator.isNotNull(creationMenu) && (showCreationMenu == null)) {
-			showCreationMenu = true;
-
-			setShowCreationMenu(showCreationMenu);
-		}
-		else if (Validator.isNull(creationMenu) && (showCreationMenu == null)) {
-			setShowCreationMenu(false);
+		if (showCreationMenu == null) {
+			setShowCreationMenu(Validator.isNotNull(creationMenu));
 		}
 
 		String infoPanelId = (String)context.get("infoPanelId");

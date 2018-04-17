brianchandotcom/liferay-portal

commit 19b309273982b73ed4ae45675a25aafbf902acf3

OAUTH2-124 Use Dictionary

modules/apps/oauth2-provider/oauth2-provider-rest/src/main/java/com/liferay/oauth2/provider/rest/internal/endpoint/access/token/LiferayAccessTokenServiceRegistrator.java
@@ -19,6 +19,7 @@ import com.liferay.oauth2.provider.rest.internal.endpoint.liferay.LiferayOAuthDa
 import com.liferay.portal.kernel.util.MapUtil;
 
 import java.util.ArrayList;
+import java.util.Dictionary;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
@@ -64,9 +65,9 @@ public class LiferayAccessTokenServiceRegistrator {
 		liferayAccessTokenService.setDataProvider(_liferayOAuthDataProvider);
 		liferayAccessTokenService.setGrantHandlers(_accessTokenGrantHandlers);
 
-		Hashtable<String, Object> endpointProperties = new Hashtable<>();
+		Dictionary<String, Object> endpointProperties = new Hashtable<>();
 
-		endpointProperties.put(
+		properties.put(
 			OAuth2ProviderRestEndpointConstants.
 				LIFERAY_OAUTH2_ENDPOINT_RESOURCE,
 			true);

commit bcb1128c1c515ee43296830636fa6a29c43b057c

OAUTH2-124 Regression by 19b30927

modules/apps/oauth2-provider/oauth2-provider-rest/src/main/java/com/liferay/oauth2/provider/rest/internal/endpoint/access/token/LiferayAccessTokenServiceRegistrator.java
@@ -67,7 +67,7 @@ public class LiferayAccessTokenServiceRegistrator {
 
 		Dictionary<String, Object> endpointProperties = new Hashtable<>();
 
-		properties.put(
+		endpointProperties.put(
 			OAuth2ProviderRestEndpointConstants.
 				LIFERAY_OAUTH2_ENDPOINT_RESOURCE,
 			true);
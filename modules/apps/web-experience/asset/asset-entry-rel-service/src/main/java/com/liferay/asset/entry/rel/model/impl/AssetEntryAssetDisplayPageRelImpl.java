8851a0f5c3ab8a9f0a03137cde18fc16553679c6
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2018-05-04 08:42:49 +0100

    LPS-79703 SF

--- modules/apps/web-experience/asset/asset-entry-rel-service/src/main/java/com/liferay/asset/entry/rel/model/impl/AssetEntryAssetDisplayPageRelImpl.java
+++ modules/apps/web-experience/asset/asset-entry-rel-service/src/main/java/com/liferay/asset/entry/rel/model/impl/AssetEntryAssetDisplayPageRelImpl.java
@@ -17,24 +17,9 @@ package com.liferay.asset.entry.rel.model.impl;
 import aQute.bnd.annotation.ProviderType;
 
 /**
- * The extended model implementation for the AssetEntryAssetDisplayPageRel service. Represents a row in the &quot;AssetEntryAssetDisplayPageRel&quot; database table, with each column mapped to a property of this class.
- *
- * <p>
- * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.asset.entry.rel.model.AssetEntryAssetDisplayPageRel} interface.
- * </p>
- *
- * @author Brian Wing Shun Chan
+ * @author Jürgen Kappler
  */
 @ProviderType
 public class AssetEntryAssetDisplayPageRelImpl
 	extends AssetEntryAssetDisplayPageRelBaseImpl {
-
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. All methods that expect a asset entry asset display page rel model instance should use the {@link com.liferay.asset.entry.rel.model.AssetEntryAssetDisplayPageRel} interface instead.
-	 */
-	public AssetEntryAssetDisplayPageRelImpl() {
-	}
-
 }

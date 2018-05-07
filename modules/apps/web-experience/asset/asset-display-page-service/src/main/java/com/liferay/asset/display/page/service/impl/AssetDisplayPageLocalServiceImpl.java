816f364b1693362b382e62563ec94d9e48d3fd0f
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2018-05-07 11:53:20 +0100

    LPS-79703 Methods to fetch, add and remove an asset display page

--- modules/apps/web-experience/asset/asset-display-page-service/src/main/java/com/liferay/asset/display/page/service/impl/AssetDisplayPageLocalServiceImpl.java
+++ modules/apps/web-experience/asset/asset-display-page-service/src/main/java/com/liferay/asset/display/page/service/impl/AssetDisplayPageLocalServiceImpl.java
@@ -14,11 +14,50 @@
 
 package com.liferay.asset.display.page.service.impl;
 
+import com.liferay.asset.display.page.model.AssetDisplayPage;
 import com.liferay.asset.display.page.service.base.AssetDisplayPageLocalServiceBaseImpl;
+import com.liferay.portal.kernel.exception.PortalException;
 
 /**
  * @author Eudaldo Alonso
  */
 public class AssetDisplayPageLocalServiceImpl
 	extends AssetDisplayPageLocalServiceBaseImpl {
+
+	@Override
+	public AssetDisplayPage addAssetDisplayPage(
+		long assetEntryId, long layoutId) {
+
+		long assetDisplayPageId = counterLocalService.increment();
+
+		AssetDisplayPage assetDisplayPage = assetDisplayPagePersistence.create(
+			assetDisplayPageId);
+
+		assetDisplayPage.setAssetEntryId(assetEntryId);
+		assetDisplayPage.setLayoutId(layoutId);
+
+		assetDisplayPagePersistence.update(assetDisplayPage);
+
+		return assetDisplayPage;
+	}
+
+	@Override
+	public void deleteAssetDisplayPage(long assetEntryId, long layoutId)
+		throws PortalException {
+
+		assetDisplayPagePersistence.removeByA_L(assetEntryId, layoutId);
+	}
+
+	@Override
+	public void deleteAssetDisplayPageByAssetEntryId(long assetEntryId) {
+		assetDisplayPagePersistence.removeByAssetEntryId(assetEntryId);
+	}
+
+	@Override
+	public AssetDisplayPage fetchAssetDisplayPage(
+		long assetEntryId, long layoutId) {
+
+		return assetDisplayPagePersistence.fetchByA_L(assetEntryId, layoutId);
+	}
+
 }

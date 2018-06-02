5c82d926ecb2f01836420cadd5186e9c6b039f8a
Alejandro Hernández <info@alejandrohdezma.com>
2018-06-02 10:11:40 +0200

    LPS-81769 Add keywords to both form and representor

--- modules/apps/headless-apio/blog/blog-apio-impl/src/main/java/com/liferay/blog/apio/internal/architect/resource/BlogPostingNestedCollectionResource.java
+++ modules/apps/headless-apio/blog/blog-apio-impl/src/main/java/com/liferay/blog/apio/internal/architect/resource/BlogPostingNestedCollectionResource.java
@@ -23,6 +23,9 @@ import com.liferay.apio.architect.representor.Representor;
 import com.liferay.apio.architect.resource.NestedCollectionResource;
 import com.liferay.apio.architect.routes.ItemRoutes;
 import com.liferay.apio.architect.routes.NestedCollectionRoutes;
+import com.liferay.asset.kernel.model.AssetTag;
+import com.liferay.asset.kernel.model.AssetTagModel;
+import com.liferay.asset.kernel.service.AssetTagLocalService;
 import com.liferay.blog.apio.architect.identifier.BlogPostingIdentifier;
 import com.liferay.blog.apio.internal.architect.form.BlogPostingForm;
 import com.liferay.blogs.model.BlogsEntry;
@@ -36,6 +39,7 @@ import com.liferay.portal.apio.permission.HasPermission;
 import com.liferay.portal.apio.user.CurrentUser;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.site.apio.architect.identifier.WebSiteIdentifier;
 
@@ -134,6 +138,8 @@ public class BlogPostingNestedCollectionResource
 			"fileFormat", blogsEntry -> "text/html"
 		).addString(
 			"headline", BlogsEntry::getTitle
+		).addStringList(
+			"keywords", this::_getBlogsEntryTags
 		).build();
 	}
 
@@ -149,6 +155,13 @@ public class BlogPostingNestedCollectionResource
 			blogPostingForm.getSemanticUrl(), blogPostingForm.getDescription(),
 			blogPostingForm.getArticleBody(), blogPostingForm.getDisplayDate(),
 			null, null, null, blogPostingForm.getServiceContext(groupId));
+	}
+
+	private List<String> _getBlogsEntryTags(BlogsEntry blogsEntry) {
+		List<AssetTag> tags = _assetTagLocalService.getTags(
+			BlogsEntry.class.getName(), blogsEntry.getEntryId());
+
+		return ListUtil.toList(tags, AssetTagModel::getName);
 	}
 
 	private PageItems<BlogsEntry> _getPageItems(
@@ -181,6 +194,9 @@ public class BlogPostingNestedCollectionResource
 			blogPostingForm.getArticleBody(), blogPostingForm.getDisplayDate(),
 			null, null, null, serviceContext);
 	}
+
+	@Reference
+	private AssetTagLocalService _assetTagLocalService;
 
 	@Reference
 	private BlogsEntryService _blogsEntryService;

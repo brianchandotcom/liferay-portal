77fa895a8475f47b904e12a5c9952432b4e1ada7
Alejandro Hernández <info@alejandrohdezma.com>
2018-06-02 09:51:13 +0200

    LPS-81769 Add new method to BlogsEntryServiceImpl

--- modules/apps/blogs/blogs-service/src/main/java/com/liferay/blogs/service/impl/BlogsEntryServiceImpl.java
+++ modules/apps/blogs/blogs-service/src/main/java/com/liferay/blogs/service/impl/BlogsEntryServiceImpl.java
@@ -145,6 +145,44 @@ public class BlogsEntryServiceImpl extends BlogsEntryServiceBaseImpl {
 			serviceContext);
 	}
 
+	/**
+	 * Creates a new blogs entry
+	 *
+	 * @param  title the blogs entry's title
+	 * @param  subtitle the blogs entry's subtitle
+	 * @param  urlTitle the blogs entry's urlTitle
+	 * @param  description the blogs entry's description
+	 * @param  content the blogs entry's content
+	 * @param  displayDate the blogs entry's displayDate
+	 * @param  coverImageCaption the blogs entry's cover image caption
+	 * @param  coverImageImageSelector an object containing the data of the
+	 *         blogs's entry cover image, can be {@code null}
+	 * @param  smallImageImageSelector an object containing the data of the
+	 *         blogs's entry small cover image, can be {@code null}
+	 * @param  serviceContext the blogs entry's serviceContext; at least it must
+	 *         contain the {@code groupId}
+	 * @return the created blogs entry
+	 * @review
+	 */
+	@Override
+	public BlogsEntry addEntry(
+			String title, String subtitle, String urlTitle, String description,
+			String content, Date displayDate, String coverImageCaption,
+			ImageSelector coverImageImageSelector,
+			ImageSelector smallImageImageSelector,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		_portletResourcePermission.check(
+			getPermissionChecker(), serviceContext.getScopeGroupId(),
+			ActionKeys.ADD_ENTRY);
+
+		return blogsEntryLocalService.addEntry(
+			getUserId(), title, subtitle, urlTitle, description, content,
+			displayDate, true, true, new String[0], coverImageCaption,
+			coverImageImageSelector, smallImageImageSelector, serviceContext);
+	}
+
 	@Override
 	public BlogsEntry addEntry(
 			String title, String subtitle, String urlTitle, String description,
@@ -661,6 +699,44 @@ public class BlogsEntryServiceImpl extends BlogsEntryServiceBaseImpl {
 			displayDateMinute, allowPingbacks, allowTrackbacks, trackbacks,
 			coverImageCaption, coverImageImageSelector, smallImageImageSelector,
 			serviceContext);
+	}
+
+	/**
+	 * Updates a blogs entry
+	 *
+	 * @param  entryId the blogs entry's ID
+	 * @param  title the blogs entry's title
+	 * @param  subtitle the blogs entry's subtitle
+	 * @param  urlTitle the blogs entry's urlTitle
+	 * @param  description the blogs entry's description
+	 * @param  content the blogs entry's content
+	 * @param  displayDate the blogs entry's displayDate
+	 * @param  coverImageCaption the blogs entry's cover image caption
+	 * @param  coverImageImageSelector an object containing the data of the
+	 *         blogs's entry cover image, can be {@code null}
+	 * @param  smallImageImageSelector an object containing the data of the
+	 *         blogs's entry small cover image, can be {@code null}
+	 * @param  serviceContext the blogs entry's serviceContext; at least it must
+	 *         contain the {@code groupId}
+	 * @return the updated blogs entry
+	 * @review
+	 */
+	@Override
+	public BlogsEntry updateEntry(
+			long entryId, String title, String subtitle, String urlTitle,
+			String description, String content, Date displayDate,
+			String coverImageCaption, ImageSelector coverImageImageSelector,
+			ImageSelector smallImageImageSelector,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		_blogsEntryFolderModelResourcePermission.check(
+			getPermissionChecker(), entryId, ActionKeys.UPDATE);
+
+		return blogsEntryLocalService.updateEntry(
+			getUserId(), entryId, title, subtitle, urlTitle, description,
+			content, displayDate, true, true, new String[0], coverImageCaption,
+			coverImageImageSelector, smallImageImageSelector, serviceContext);
 	}
 
 	@Override

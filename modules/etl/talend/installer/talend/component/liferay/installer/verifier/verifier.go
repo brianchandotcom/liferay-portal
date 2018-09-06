2214493fdaeae4879437890fdb29ea2b720396de
Zoltán Takács <zoltan.takacs@liferay.com>
2018-09-06 17:53:11 +0200

    COMMERCE-618 Move installer to the liferay-portal ETL directory

---
+++ modules/etl/talend/installer/talend/component/liferay/installer/verifier/verifier.go
@@ -0,0 +1,60 @@
+package verifier
+
+import (
+	"fmt"
+	"log"
+	"path/filepath"
+	"strings"
+
+	"github.com/ZoltanTakacs/golang/talend/component/liferay/installer/constants"
+	"github.com/ZoltanTakacs/golang/utils"
+	"github.com/go-ini/ini"
+)
+
+func VerifyInstallerIntegrity() {
+	if !utils.Exists(constants.ComponentArtifactsDir) {
+		log.Println("Liferay component artifacts are missing from the installer: " + constants.ComponentArtifactsDir)
+		log.Fatal("Installation failed")
+	}
+
+	if !utils.Exists(constants.ComponentDefinitionDir) {
+		log.Println("Liferay component definition is missing from the installer: " + constants.ComponentDefinitionDir)
+		log.Fatal("Installation failed")
+	}
+
+	if !utils.Exists(constants.ThirdPartyDependenciesDir) {
+		log.Println("3rd party dependencies are missing from the installer: " + constants.ThirdPartyDependenciesDir)
+		log.Fatal("Installation failed")
+	}
+}
+
+func VerifyTOSLocation() {
+	if !utils.Exists(constants.TosHomeDir) {
+		log.Println("TOS Home directory does not exist")
+		log.Fatal("Installation failed")
+	}
+	if !utils.Exists(constants.TosM2Dir) {
+		log.Println("TOS Maven repository's directory does not exist")
+		log.Fatal("Installation failed")
+	}
+
+	fmt.Println("Talend Open Studio's Maven repository: \t" + constants.TosM2Dir)
+}
+
+func VerifyTOSVersion() {
+	tosConfigIniPath := filepath.Join(constants.TosHomeDir, "configuration", "config.ini")
+	cfg, err := ini.Load(tosConfigIniPath)
+
+	if err != nil {
+		log.Printf("Fail to read TOS's config file: %v \n", err)
+		log.Fatal("Installation failed")
+	}
+
+	tosVersion := cfg.Section("").Key("talend.studio.version").String()
+
+	if !strings.HasPrefix(tosVersion, constants.SUPPORTED_TOS_VERSION) {
+		log.Println("Unsupported Talend Version: " + tosVersion)
+		log.Println("The installer is only compatible with " + constants.SUPPORTED_TOS_VERSION)
+		log.Fatal("Installation failed")
+	}
+}

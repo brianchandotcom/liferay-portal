59d70e6aec662d0f167699f2de035675422db21c
Peter Shin <peter.shin@liferay.com>
2018-10-17 12:00:23 -0700

    LPS-84825 Append Properties for consistency

--- modules/sdk/project-templates/project-templates/src/test/java/com/liferay/project/templates/ProjectTemplatesTest.java
+++ modules/sdk/project-templates/project-templates/src/test/java/com/liferay/project/templates/ProjectTemplatesTest.java
@@ -3592,10 +3592,10 @@ public class ProjectTemplatesTest {
 
 	@Test
 	public void testListTemplatesWithCustomArchetypesDir() throws Exception {
-		Properties archetypes =
-			ProjectTemplatesUtil.getProjectTemplateJarVersions();
+		Properties archetypesProperties =
+			ProjectTemplatesUtil.getProjectTemplateJarVersionsProperties();
 
-		Set<String> artifactIds = archetypes.stringPropertyNames();
+		Set<String> artifactIds = archetypesProperties.stringPropertyNames();
 
 		Iterator<String> artifactIdIterator = artifactIds.iterator();
 

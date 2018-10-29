2beb6a9c4a9f01a8398063c04a5014d1fa449742
Iván Zaera Avellón <ivan.zaera@liferay.com>
2018-10-29 10:44:27 +0100

    LPS-86630 Soy portlets using HTML parameters fail rendering with a SoyTofuException

--- modules/apps/portal-template/portal-template-soy-api/src/main/java/com/liferay/portal/template/soy/utils/SoyHTMLSanitizer.java
+++ modules/apps/portal-template/portal-template-soy-api/src/main/java/com/liferay/portal/template/soy/utils/SoyHTMLSanitizer.java
@@ -16,7 +16,11 @@ package com.liferay.portal.template.soy.utils;
 
 /**
  * @author Shuyang Zhou
+ * @deprecated Use {@link com.liferay.portal.template.soy.data.SoyHTMLData} to
+ * 				wrap HTML values
+ * @review
  */
+@Deprecated
 public interface SoyHTMLSanitizer {
 
 	public Object sanitize(String value);

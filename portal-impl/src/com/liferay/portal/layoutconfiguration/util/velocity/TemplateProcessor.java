1b99108dd1fbb7c2682ad56c835e4a8e0ad21d06
shuyangzhou <shuyang.zhou@liferay.com>
2018-11-13 14:30:16 -0800

    Revert "LPS-87152 SF"
    This reverts commit 239275d10121fcedce23fd47bb0024552813ca5d.

--- portal-impl/src/com/liferay/portal/layoutconfiguration/util/velocity/TemplateProcessor.java
+++ portal-impl/src/com/liferay/portal/layoutconfiguration/util/velocity/TemplateProcessor.java
@@ -38,6 +38,7 @@ import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.layoutconfiguration.util.PortletRenderer;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;

3ccfdc624cd1677148e73786df6c5fc7a445f939
Javier Gamarra <nhpatt@gmail.com>
2018-11-29 02:33:24 +0100

    LPS-85828 small rename

--- modules/apps/headless-apio/workflow/workflow-apio-impl/src/main/java/com/liferay/workflow/apio/internal/architect/resource/WorkflowTaskItemResource.java
+++ modules/apps/headless-apio/workflow/workflow-apio-impl/src/main/java/com/liferay/workflow/apio/internal/architect/resource/WorkflowTaskItemResource.java
@@ -155,7 +155,7 @@ public class WorkflowTaskItemResource
 				_workflowTaskManager.getWorkflowTask(
 					currentUser.getCompanyId(), workflowTaskId))
 		).filter(
-			l -> l.contains(transition)
+			transitionName -> transitionName.contains(transition)
 		).orElseThrow(
 			() -> new ValidationException("Invalid transition: " + transition)
 		);

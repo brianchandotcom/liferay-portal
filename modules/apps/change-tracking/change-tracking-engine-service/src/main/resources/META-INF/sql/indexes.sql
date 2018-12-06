brianchandotcom/liferay-portal

commit 9466c909a465acf2341d013ad476cd42f851ae72

LPS-87540 Regen

modules/apps/change-tracking/change-tracking-engine-service/src/main/resources/META-INF/sql/indexes.sql
@@ -2,4 +2,6 @@ create index IX_264F41B7 on CTEEntry (resourcePrimKey);
 
 create index IX_9F3670F9 on Collections_Entries (collectionId);
 create index IX_8745ABC on Collections_Entries (companyId);
+create index IX_D43D5A11 on Collections_Entries (cteCollectionId);
+create index IX_DF400899 on Collections_Entries (cteEntryId);
 create index IX_2F5E40B1 on Collections_Entries (entryId);

modules/apps/change-tracking/change-tracking-engine-api/src/main/java/com/liferay/change/tracking/engine/model/CTECollection.java
@@ -37,10 +37,11 @@ public interface CTECollection extends CTECollectionModel, PersistedModel {
 	 *
 	 * Never modify this interface directly. Add methods to {@link com.liferay.change.tracking.engine.model.impl.CTECollectionImpl} and rerun ServiceBuilder to automatically copy the method declarations to this interface.
 	 */
-	public static final Accessor<CTECollection, Long> COLLECTION_ID_ACCESSOR = new Accessor<CTECollection, Long>() {
+	public static final Accessor<CTECollection, Long> CTE_COLLECTION_ID_ACCESSOR =
+		new Accessor<CTECollection, Long>() {
 			@Override
 			public Long get(CTECollection cteCollection) {
-				return cteCollection.getCollectionId();
+				return cteCollection.getCteCollectionId();
 			}
 
 			@Override

modules/apps/change-tracking/change-tracking-engine-api/src/main/java/com/liferay/change/tracking/engine/model/CTECollectionModel.java
@@ -67,18 +67,18 @@ public interface CTECollectionModel extends AuditedModel,
 	public void setPrimaryKey(long primaryKey);
 
 	/**
-	 * Returns the collection ID of this cte collection.
+	 * Returns the cte collection ID of this cte collection.
 	 *
-	 * @return the collection ID of this cte collection
+	 * @return the cte collection ID of this cte collection
 	 */
-	public long getCollectionId();
+	public long getCteCollectionId();
 
 	/**
-	 * Sets the collection ID of this cte collection.
+	 * Sets the cte collection ID of this cte collection.
 	 *
-	 * @param collectionId the collection ID of this cte collection
+	 * @param cteCollectionId the cte collection ID of this cte collection
 	 */
-	public void setCollectionId(long collectionId);
+	public void setCteCollectionId(long cteCollectionId);
 
 	/**
 	 * Returns the company ID of this cte collection.

modules/apps/change-tracking/change-tracking-engine-api/src/main/java/com/liferay/change/tracking/engine/model/CTECollectionSoap.java
@@ -33,7 +33,7 @@ public class CTECollectionSoap implements Serializable {
 	public static CTECollectionSoap toSoapModel(CTECollection model) {
 		CTECollectionSoap soapModel = new CTECollectionSoap();
 
-		soapModel.setCollectionId(model.getCollectionId());
+		soapModel.setCteCollectionId(model.getCteCollectionId());
 		soapModel.setCompanyId(model.getCompanyId());
 		soapModel.setUserId(model.getUserId());
 		soapModel.setUserName(model.getUserName());
@@ -90,19 +90,19 @@ public class CTECollectionSoap implements Serializable {
 	}
 
 	public long getPrimaryKey() {
-		return _collectionId;
+		return _cteCollectionId;
 	}
 
 	public void setPrimaryKey(long pk) {
-		setCollectionId(pk);
+		setCteCollectionId(pk);
 	}
 
-	public long getCollectionId() {
-		return _collectionId;
+	public long getCteCollectionId() {
+		return _cteCollectionId;
 	}
 
-	public void setCollectionId(long collectionId) {
-		_collectionId = collectionId;
+	public void setCteCollectionId(long cteCollectionId) {
+		_cteCollectionId = cteCollectionId;
 	}
 
 	public long getCompanyId() {
@@ -193,7 +193,7 @@ public class CTECollectionSoap implements Serializable {
 		_statusDate = statusDate;
 	}
 
-	private long _collectionId;
+	private long _cteCollectionId;
 	private long _companyId;
 	private long _userId;
 	private String _userName;

modules/apps/change-tracking/change-tracking-engine-api/src/main/java/com/liferay/change/tracking/engine/model/CTECollectionWrapper.java
@@ -58,7 +58,7 @@ public class CTECollectionWrapper implements CTECollection,
 	public Map<String, Object> getModelAttributes() {
 		Map<String, Object> attributes = new HashMap<String, Object>();
 
-		attributes.put("collectionId", getCollectionId());
+		attributes.put("cteCollectionId", getCteCollectionId());
 		attributes.put("companyId", getCompanyId());
 		attributes.put("userId", getUserId());
 		attributes.put("userName", getUserName());
@@ -76,10 +76,10 @@ public class CTECollectionWrapper implements CTECollection,
 
 	@Override
 	public void setModelAttributes(Map<String, Object> attributes) {
-		Long collectionId = (Long)attributes.get("collectionId");
+		Long cteCollectionId = (Long)attributes.get("cteCollectionId");
 
-		if (collectionId != null) {
-			setCollectionId(collectionId);
+		if (cteCollectionId != null) {
+			setCteCollectionId(cteCollectionId);
 		}
 
 		Long companyId = (Long)attributes.get("companyId");
@@ -160,16 +160,6 @@ public class CTECollectionWrapper implements CTECollection,
 	}
 
 	/**
-	* Returns the collection ID of this cte collection.
-	*
-	* @return the collection ID of this cte collection
-	*/
-	@Override
-	public long getCollectionId() {
-		return _cteCollection.getCollectionId();
-	}
-
-	/**
 	* Returns the company ID of this cte collection.
 	*
 	* @return the company ID of this cte collection
@@ -187,6 +177,16 @@ public class CTECollectionWrapper implements CTECollection,
 	@Override
 	public Date getCreateDate() {
 		return _cteCollection.getCreateDate();
+	}
+
+	/**
+	* Returns the cte collection ID of this cte collection.
+	*
+	* @return the cte collection ID of this cte collection
+	*/
+	@Override
+	public long getCteCollectionId() {
+		return _cteCollection.getCteCollectionId();
 	}
 
 	/**
@@ -430,16 +430,6 @@ public class CTECollectionWrapper implements CTECollection,
 	}
 
 	/**
-	* Sets the collection ID of this cte collection.
-	*
-	* @param collectionId the collection ID of this cte collection
-	*/
-	@Override
-	public void setCollectionId(long collectionId) {
-		_cteCollection.setCollectionId(collectionId);
-	}
-
-	/**
 	* Sets the company ID of this cte collection.
 	*
 	* @param companyId the company ID of this cte collection
@@ -457,6 +447,16 @@ public class CTECollectionWrapper implements CTECollection,
 	@Override
 	public void setCreateDate(Date createDate) {
 		_cteCollection.setCreateDate(createDate);
+	}
+
+	/**
+	* Sets the cte collection ID of this cte collection.
+	*
+	* @param cteCollectionId the cte collection ID of this cte collection
+	*/
+	@Override
+	public void setCteCollectionId(long cteCollectionId) {
+		_cteCollection.setCteCollectionId(cteCollectionId);
 	}
 
 	/**

modules/apps/change-tracking/change-tracking-engine-api/src/main/java/com/liferay/change/tracking/engine/model/CTEEntry.java
@@ -37,10 +37,10 @@ public interface CTEEntry extends CTEEntryModel, PersistedModel {
 	 *
 	 * Never modify this interface directly. Add methods to {@link com.liferay.change.tracking.engine.model.impl.CTEEntryImpl} and rerun ServiceBuilder to automatically copy the method declarations to this interface.
 	 */
-	public static final Accessor<CTEEntry, Long> ENTRY_ID_ACCESSOR = new Accessor<CTEEntry, Long>() {
+	public static final Accessor<CTEEntry, Long> CTE_ENTRY_ID_ACCESSOR = new Accessor<CTEEntry, Long>() {
 			@Override
 			public Long get(CTEEntry cteEntry) {
-				return cteEntry.getEntryId();
+				return cteEntry.getCteEntryId();
 			}
 
 			@Override

modules/apps/change-tracking/change-tracking-engine-api/src/main/java/com/liferay/change/tracking/engine/model/CTEEntryModel.java
@@ -68,18 +68,18 @@ public interface CTEEntryModel extends AttachedModel, AuditedModel,
 	public void setPrimaryKey(long primaryKey);
 
 	/**
-	 * Returns the entry ID of this cte entry.
+	 * Returns the cte entry ID of this cte entry.
 	 *
-	 * @return the entry ID of this cte entry
+	 * @return the cte entry ID of this cte entry
 	 */
-	public long getEntryId();
+	public long getCteEntryId();
 
 	/**
-	 * Sets the entry ID of this cte entry.
+	 * Sets the cte entry ID of this cte entry.
 	 *
-	 * @param entryId the entry ID of this cte entry
+	 * @param cteEntryId the cte entry ID of this cte entry
 	 */
-	public void setEntryId(long entryId);
+	public void setCteEntryId(long cteEntryId);
 
 	/**
 	 * Returns the company ID of this cte entry.

modules/apps/change-tracking/change-tracking-engine-api/src/main/java/com/liferay/change/tracking/engine/model/CTEEntrySoap.java
@@ -33,7 +33,7 @@ public class CTEEntrySoap implements Serializable {
 	public static CTEEntrySoap toSoapModel(CTEEntry model) {
 		CTEEntrySoap soapModel = new CTEEntrySoap();
 
-		soapModel.setEntryId(model.getEntryId());
+		soapModel.setCteEntryId(model.getCteEntryId());
 		soapModel.setCompanyId(model.getCompanyId());
 		soapModel.setUserId(model.getUserId());
 		soapModel.setUserName(model.getUserName());
@@ -87,19 +87,19 @@ public class CTEEntrySoap implements Serializable {
 	}
 
 	public long getPrimaryKey() {
-		return _entryId;
+		return _cteEntryId;
 	}
 
 	public void setPrimaryKey(long pk) {
-		setEntryId(pk);
+		setCteEntryId(pk);
 	}
 
-	public long getEntryId() {
-		return _entryId;
+	public long getCteEntryId() {
+		return _cteEntryId;
 	}
 
-	public void setEntryId(long entryId) {
-		_entryId = entryId;
+	public void setCteEntryId(long cteEntryId) {
+		_cteEntryId = cteEntryId;
 	}
 
 	public long getCompanyId() {
@@ -166,7 +166,7 @@ public class CTEEntrySoap implements Serializable {
 		_resourcePrimKey = resourcePrimKey;
 	}
 
-	private long _entryId;
+	private long _cteEntryId;
 	private long _companyId;
 	private long _userId;
 	private String _userName;

commit 6a6db19e49a78372221986eb5414b624e619c553

LPS-87540 Proper regen caused by 9466c909a465acf2341d013ad476cd42f851ae72

modules/apps/change-tracking/change-tracking-engine-service/src/main/resources/META-INF/sql/indexes.sql
@@ -1,7 +1,5 @@
 create index IX_264F41B7 on CTEEntry (resourcePrimKey);
 
-create index IX_9F3670F9 on Collections_Entries (collectionId);
 create index IX_8745ABC on Collections_Entries (companyId);
 create index IX_D43D5A11 on Collections_Entries (cteCollectionId);
 create index IX_DF400899 on Collections_Entries (cteEntryId);
-create index IX_2F5E40B1 on Collections_Entries (entryId);
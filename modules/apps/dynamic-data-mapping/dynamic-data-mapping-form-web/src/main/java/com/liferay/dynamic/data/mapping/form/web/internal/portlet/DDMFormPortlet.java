b45aa7b30f3e7f4c2d54c39a7348caa38e47c51a
Alberto Chaparro <alberto.chaparro@liferay.com>
2018-12-19 12:31:28 +0100

    LPS-86461 Portlet classes have to wait until major and minor changes in the schemaVersion during the upgrade have been completed

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/portlet/DDMFormPortlet.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/portlet/DDMFormPortlet.java
@@ -31,6 +31,7 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.Release;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
 import com.liferay.portal.kernel.service.GroupLocalService;
@@ -212,6 +213,13 @@ public class DDMFormPortlet extends MVCPortlet {
 			portletSession.setAttribute("formInstanceId", formInstanceId);
 			portletSession.setAttribute("shared", Boolean.TRUE);
 		}
+	}
+
+	@Reference(
+		target = "(&(release.bundle.symbolic.name=com.liferay.dynamic.data.mapping.form.web)(&(release.schema.version>=1.0.0)(!(release.schema.version>=1.1.0))))",
+		unbind = "-"
+	)
+	protected void setRelease(Release release) {
 	}
 
 	protected void setRenderRequestAttributes(

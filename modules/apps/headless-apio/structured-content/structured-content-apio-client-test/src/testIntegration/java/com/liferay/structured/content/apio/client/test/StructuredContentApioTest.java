7efd781d28bd8bd7991b2ca67e772aff6a66c686
Cristina González Castellano <cristina.gonzalez@liferay.com>
2018-12-19 16:20:01 +0100

    LPS-88562 Use JSON files, instead strings.

--- modules/apps/headless-apio/structured-content/structured-content-apio-client-test/src/testIntegration/java/com/liferay/structured/content/apio/client/test/StructuredContentApioTest.java
+++ modules/apps/headless-apio/structured-content/structured-content-apio-client-test/src/testIntegration/java/com/liferay/structured/content/apio/client/test/StructuredContentApioTest.java
@@ -18,10 +18,16 @@ import com.liferay.content.space.apio.client.test.util.ContentSpaceApioTestUtil;
 import com.liferay.oauth2.provider.test.util.OAuth2ProviderTestUtil;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.apio.test.util.ApioClientBuilder;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.structured.content.apio.client.test.activator.StructuredContentApioTestBundleActivator;
+
+import java.io.InputStream;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+
+import java.util.Collections;
+import java.util.List;
 
 import org.hamcrest.Matchers;
 import org.hamcrest.core.IsEqual;
@@ -91,7 +97,7 @@ public class StructuredContentApioTest {
 	}
 
 	@Test
-	public void testCreateStructuredContent() {
+	public void testCreateStructuredContent() throws Exception {
 		String contentStructureId = ApioClientBuilder.given(
 		).basicAuth(
 			"test@liferay.com", "test"
@@ -108,6 +114,8 @@ public class StructuredContentApioTest {
 					"'}._links.self.href"
 		);
 
+		System.out.println("--- " + contentStructureId);
+
 		ApioClientBuilder.given(
 		).basicAuth(
 			"test@liferay.com", "test"
@@ -116,9 +124,9 @@ public class StructuredContentApioTest {
 		).header(
 			"Content-Type", "application/json"
 		).body(
-			"{\"contentStructure\": \"" + contentStructureId +
-				"\", \"title\": \"Example Structured Content\", \"values\": " +
-					"[{\"name\": \"MyDecimal\", \"value\": \"1.0\"}]}"
+			_read(
+				"test-create-structured-content.json",
+				Collections.singletonList(contentStructureId))
 		).when(
 		).post(
 			_structuredContentEndpointURL.toExternalForm()
@@ -134,7 +142,7 @@ public class StructuredContentApioTest {
 	}
 
 	@Test
-	public void testDeleteStructuredContent() {
+	public void testDeleteStructuredContent() throws Exception {
 		String contentStructureId = ApioClientBuilder.given(
 		).basicAuth(
 			"test@liferay.com", "test"
@@ -159,9 +167,9 @@ public class StructuredContentApioTest {
 		).header(
 			"Content-Type", "application/json"
 		).body(
-			"{\"contentStructure\": \"" + contentStructureId +
-				"\", \"title\": \"Example Structured Content\", \"values\": " +
-					"[{\"name\": \"MyDecimal\", \"value\": \"1.0\"}]}"
+			_read(
+				"test-create-structured-content.json",
+				Collections.singletonList(contentStructureId))
 		).when(
 		).post(
 			_structuredContentEndpointURL.toExternalForm()
@@ -425,7 +433,7 @@ public class StructuredContentApioTest {
 	}
 
 	@Test
-	public void testUpdateStructuredContent() {
+	public void testUpdateStructuredContent() throws Exception {
 		String contentStructureId = ApioClientBuilder.given(
 		).basicAuth(
 			"test@liferay.com", "test"
@@ -442,6 +450,8 @@ public class StructuredContentApioTest {
 					"'}._links.self.href"
 		);
 
+		System.out.println("-->> " + contentStructureId);
+
 		String structuredContentHref = ApioClientBuilder.given(
 		).basicAuth(
 			"test@liferay.com", "test"
@@ -450,9 +460,9 @@ public class StructuredContentApioTest {
 		).header(
 			"Content-Type", "application/json"
 		).body(
-			"{\"contentStructure\": \"" + contentStructureId +
-				"\", \"title\": \"Example Structured Content\", \"values\": " +
-					"[{\"name\": \"MyDecimal\", \"value\": \"1.0\"}]}"
+			_read(
+				"test-create-structured-content.json",
+				Collections.singletonList(contentStructureId))
 		).when(
 		).post(
 			_structuredContentEndpointURL.toExternalForm()
@@ -470,10 +480,9 @@ public class StructuredContentApioTest {
 		).header(
 			"Content-Type", "application/json"
 		).body(
-			"{\"contentStructure\": \"" + contentStructureId +
-				"\", \"title\": \"Example Structured Content Modified\", " +
-					"\"values\": [{\"name\": \"MyDecimal\", \"value\": " +
-						"\"2.0\"}]}"
+			_read(
+				"test-update-structured-content.json",
+				Collections.singletonList(contentStructureId))
 		).when(
 		).put(
 			structuredContentHref
@@ -484,6 +493,13 @@ public class StructuredContentApioTest {
 			"_embedded.values._embedded.find {it.name == 'MyDecimal'}.value",
 			Matchers.equalTo("2.0")
 		);
+	}
+
+	private String _read(String fileName, List<String> vars) throws Exception {
+		URL url = getClass().getResource(fileName);
+
+		return String.format(
+			StringUtil.read(url.openStream()), vars.toArray());
 	}
 
 	private URL _contentStructuresEndpointURL;

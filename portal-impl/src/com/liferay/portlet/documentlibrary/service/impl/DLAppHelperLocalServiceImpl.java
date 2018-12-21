43f7487ccc2ad967a8e81006031c359a10e9a502
Adolfo Pérez <adolfo.perez@liferay.com>
2018-12-21 12:44:14 +0100

    LPS-88484 SF - Move variable declaration up

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLAppHelperLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLAppHelperLocalServiceImpl.java
@@ -1772,11 +1772,11 @@ public class DLAppHelperLocalServiceImpl
 			TrashEntry trashEntry)
 		throws PortalException {
 
+		List<Long> dlFileEntryIds = new ArrayList<>();
+
 		List<DLFileEntry> dlFileEntries =
 			dlFileEntryLocalService.getFileEntries(
 				childDLFolder.getGroupId(), childDLFolder.getFolderId());
-
-		List<Long> dlFileEntryIds = new ArrayList<>();
 
 		for (DLFileEntry dlFileEntry : dlFileEntries) {
 			if (moveToTrash) {

5e9bed64eae38e8618df5bad50084b6ffb55f8e2
Bryan Engler <bryan.engler@liferay.com>
2019-01-07 16:20:40 -0800

    LPS-88736 portal-search-test: Add more integration tests
    https://issues.liferay.com/browse/LPS-88736

--- modules/apps/portal-search/portal-search-test/src/testIntegration/java/com/liferay/portal/search/internal/test/SearchLoggingEventsTest.java
+++ modules/apps/portal-search/portal-search-test/src/testIntegration/java/com/liferay/portal/search/internal/test/SearchLoggingEventsTest.java
@@ -17,32 +17,32 @@ package com.liferay.portal.search.internal.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.search.Field;
-import com.liferay.portal.kernel.search.Hits;
 import com.liferay.portal.kernel.search.Indexer;
 import com.liferay.portal.kernel.search.IndexerRegistry;
-import com.liferay.portal.kernel.search.QueryConfig;
-import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.SearchEngine;
+import com.liferay.portal.kernel.search.SearchEngineHelperUtil;
+import com.liferay.portal.kernel.search.generic.MatchAllQuery;
 import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
 import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
-import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.SearchContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
-import com.liferay.portal.kernel.test.util.UserTestUtil;
-import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.search.engine.adapter.SearchEngineAdapter;
+import com.liferay.portal.search.engine.adapter.search.CountSearchRequest;
+import com.liferay.portal.search.engine.adapter.search.MultisearchSearchRequest;
+import com.liferay.portal.search.engine.adapter.search.SearchSearchRequest;
 import com.liferay.portal.test.log.CaptureAppender;
 import com.liferay.portal.test.log.Log4JLoggerTestUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.spi.LoggingEvent;
 
 import org.junit.Assert;
+import org.junit.Assume;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
@@ -50,6 +50,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
+ * @author Bryan Engler
  * @author Wade Cao
  */
 @RunWith(Arquillian.class)
@@ -62,138 +63,169 @@ public class SearchLoggingEventsTest {
 
 	@Before
 	public void setUp() throws Exception {
+		Assume.assumeTrue(_isSearchEngineElasticsearch());
+
+		PermissionThreadLocal.setPermissionChecker(
+			PermissionCheckerFactoryUtil.create(TestPropsValues.getUser()));
+
 		_indexer = _indexerRegistry.getIndexer(User.class);
 	}
 
 	@Test
-	public void testCountSearchEngineAdapterLogs() throws Exception {
-		String keyword = "countSearchAdapterTestUser";
-
-		SearchContext searchContext = _addUser(keyword);
-
-		try (CaptureAppender captureAppender =
-				Log4JLoggerTestUtil.configureLog4JLogger(
-					"com.liferay.portal.search.elasticsearch6.internal." +
-						"search.engine.adapter.search." +
-							"CountSearchRequestExecutorImpl",
-					Level.DEBUG))
-		{
-
-			long cnt = _indexer.searchCount(searchContext);
-
-			_assertLogDebug(captureAppender, "searchAdapterCount", (int)cnt);
-		}
+	public void testCountSearchRequestExecutorLogs() throws Exception {
+		testLoggingEvents(
+			_CLASS_NAME_COUNT_SEARCH_REQUEST_EXECUTOR_IMPL, Level.DEBUG, 1,
+			() -> _searchEngineApapter.execute(createCountSearchRequest()));
 	}
 
 	@Test
-	public void testSearchCountEngineLogs() throws Exception {
-		String keyword = "searchCountTestUser";
+	public void testCountSearchRequestExecutorLogsViaIndexer()
+		throws Exception {
 
-		SearchContext searchContext = _addUser(keyword);
-
-		try (CaptureAppender captureAppender =
-				Log4JLoggerTestUtil.configureLog4JLogger(
-					"com.liferay.portal.search.elasticsearch6.internal." +
-						"ElasticsearchIndexSearcher",
-					Level.INFO))
-		{
-
-			long cnt = _indexer.searchCount(searchContext);
-
-			_assertLogInfo(captureAppender, "searchCount", (int)cnt);
-		}
+		testLoggingEvents(
+			_CLASS_NAME_COUNT_SEARCH_REQUEST_EXECUTOR_IMPL, Level.DEBUG, 1,
+			() -> _indexer.searchCount(
+				SearchContextTestUtil.getSearchContext()));
 	}
 
 	@Test
-	public void testSearchEngineAdapterLogs() throws Exception {
-		String keyword = "searchAdapterTestUser";
-
-		SearchContext searchContext = _addUser(keyword);
-
-		try (CaptureAppender captureAppender =
-				Log4JLoggerTestUtil.configureLog4JLogger(
-					"com.liferay.portal.search.elasticsearch6.internal." +
-						"search.engine.adapter.search." +
-							"SearchSearchRequestExecutorImpl",
-					Level.DEBUG))
-		{
-
-			Hits hits = _indexer.search(searchContext);
-
-			_assertLogDebug(
-				captureAppender, hits.toString(), hits.getDocs().length);
-		}
+	public void testIndexerSearchCountLogs() throws Exception {
+		testLoggingEvents(
+			_CLASS_NAME_ELASTICSEARCH_INDEX_SEARCHER, Level.INFO, 2,
+			() -> _indexer.searchCount(
+				SearchContextTestUtil.getSearchContext()));
 	}
 
 	@Test
-	public void testSearchEngineLogs() throws Exception {
-		String keyword = "searchTestUser";
+	public void testIndexerSearchLogs() throws Exception {
+		testLoggingEvents(
+			_CLASS_NAME_ELASTICSEARCH_INDEX_SEARCHER, Level.INFO, 2,
+			() -> _indexer.search(SearchContextTestUtil.getSearchContext()));
+	}
 
-		SearchContext searchContext = _addUser(keyword);
+	@Test
+	public void testMultisearchSearchRequestExecutorLogs() throws Exception {
+		MultisearchSearchRequest multisearchSearchRequest =
+			new MultisearchSearchRequest();
+
+		multisearchSearchRequest.addSearchSearchRequest(
+			createSearchSearchRequest());
+		multisearchSearchRequest.addSearchSearchRequest(
+			createSearchSearchRequest());
+
+		testLoggingEvents(
+			_CLASS_NAME_MULTISEARCH_SEARCH_REQUEST_EXECUTOR_IMPL, Level.DEBUG,
+			2, () -> _searchEngineApapter.execute(multisearchSearchRequest));
+	}
+
+	@Test
+	public void testSearchSearchRequestExecutorLogs() throws Exception {
+		testLoggingEvents(
+			_CLASS_NAME_SEARCH_SEARCH_REQUEST_EXECUTOR_IMPL, Level.DEBUG, 1,
+			() -> _searchEngineApapter.execute(createSearchSearchRequest()));
+	}
+
+	@Test
+	public void testSearchSearchRequestExecutorLogsViaIndexer()
+		throws Exception {
+
+		testLoggingEvents(
+			_CLASS_NAME_SEARCH_SEARCH_REQUEST_EXECUTOR_IMPL, Level.DEBUG, 1,
+			() -> _indexer.search(SearchContextTestUtil.getSearchContext()));
+	}
+
+	protected CountSearchRequest createCountSearchRequest() throws Exception {
+		CountSearchRequest countSearchRequest = new CountSearchRequest();
+
+		countSearchRequest.setIndexNames(getIndexNames());
+		countSearchRequest.setQuery(new MatchAllQuery());
+
+		return countSearchRequest;
+	}
+
+	protected SearchSearchRequest createSearchSearchRequest() throws Exception {
+		SearchSearchRequest searchSearchRequest = new SearchSearchRequest();
+
+		searchSearchRequest.setIndexNames(getIndexNames());
+		searchSearchRequest.setQuery(new MatchAllQuery());
+
+		return searchSearchRequest;
+	}
+
+	protected String[] getIndexNames() throws PortalException {
+		return new String[] {"liferay-" + TestPropsValues.getCompanyId()};
+	}
+
+	protected void testLoggingEvents(
+			String name, Level level, long numberOfEvents,
+			SearchFunction searchFunction)
+		throws Exception {
 
 		try (CaptureAppender captureAppender =
-				Log4JLoggerTestUtil.configureLog4JLogger(
-					"com.liferay.portal.search.elasticsearch6.internal." +
-						"ElasticsearchIndexSearcher",
-					Level.INFO))
-		{
+				Log4JLoggerTestUtil.configureLog4JLogger(name, level)) {
 
-			Hits hits = _indexer.search(searchContext);
+			searchFunction.execute();
 
-			_assertLogInfo(
-				captureAppender, hits.toString(), hits.getDocs().length);
+			_assertLoggingEvents(captureAppender, numberOfEvents);
 		}
 	}
 
-	private SearchContext _addUser(String keyword) throws Exception,
-		PortalException {
-
-		SearchContext searchContext = SearchContextTestUtil.getSearchContext();
-
-		searchContext.setKeywords(keyword);
-
-		QueryConfig queryConfig = searchContext.getQueryConfig();
-
-		queryConfig.addSelectedFieldNames(Field.ANY);
-
-		User user = UserTestUtil.addUser(
-			keyword, LocaleUtil.getDefault(), keyword, keyword,
-			new long[] {TestPropsValues.getGroupId()});
-
-		PermissionThreadLocal.setPermissionChecker(
-			PermissionCheckerFactoryUtil.create(user));
-
-		_users.add(user);
-
-		return searchContext;
-	}
-
-	private void _assertLogDebug(
-		CaptureAppender captureAppender, String describ, int count) {
-
-		Assert.assertEquals(describ, 1, count);
+	private void _assertLoggingEvents(
+		CaptureAppender captureAppender, long numberOfEvents) {
 
 		List<LoggingEvent> loggingEvents = captureAppender.getLoggingEvents();
 
-		Assert.assertEquals(loggingEvents.toString(), 1, loggingEvents.size());
+		Assert.assertEquals(
+			loggingEvents.toString(), numberOfEvents, loggingEvents.size());
+
+		for (LoggingEvent loggingEvent : loggingEvents) {
+			String renderedMessage = loggingEvent.getRenderedMessage();
+
+			Assert.assertTrue(
+				renderedMessage.startsWith("The search engine processed ") ||
+				renderedMessage.startsWith("Searching "));
+		}
 	}
 
-	private void _assertLogInfo(
-		CaptureAppender captureAppender, String describ, int count) {
+	private boolean _isSearchEngineElasticsearch() {
+		SearchEngine searchEngine = SearchEngineHelperUtil.getSearchEngine(
+			SearchEngineHelperUtil.getDefaultSearchEngineId());
 
-		Assert.assertEquals(describ, 1, count);
+		String vendor = searchEngine.getVendor();
 
-		List<LoggingEvent> loggingEvents = captureAppender.getLoggingEvents();
-
-		Assert.assertEquals(loggingEvents.toString(), 2, loggingEvents.size());
+		return vendor.equals("Elasticsearch");
 	}
+
+	private static final String _CLASS_NAME_COUNT_SEARCH_REQUEST_EXECUTOR_IMPL =
+		"com.liferay.portal.search.elasticsearch6.internal.search.engine." +
+			"adapter.search.CountSearchRequestExecutorImpl";
+
+	private static final String _CLASS_NAME_ELASTICSEARCH_INDEX_SEARCHER =
+		"com.liferay.portal.search.elasticsearch6.internal." +
+			"ElasticsearchIndexSearcher";
+
+	private static final String
+		_CLASS_NAME_MULTISEARCH_SEARCH_REQUEST_EXECUTOR_IMPL =
+			"com.liferay.portal.search.elasticsearch6.internal.search.engine." +
+				"adapter.search.MultisearchSearchRequestExecutorImpl";
+
+	private static final String
+		_CLASS_NAME_SEARCH_SEARCH_REQUEST_EXECUTOR_IMPL =
+			"com.liferay.portal.search.elasticsearch6.internal.search.engine." +
+				"adapter.search.SearchSearchRequestExecutorImpl";
 
 	@Inject
 	private static IndexerRegistry _indexerRegistry;
 
+	@Inject(blocking = false, filter = "search.engine.impl=Elasticsearch")
+	private static SearchEngineAdapter _searchEngineApapter;
+
 	private Indexer<User> _indexer;
 
-	@DeleteAfterTestRun
-	private final List<User> _users = new ArrayList<>();
+	private interface SearchFunction {
+
+		public void execute() throws Exception;
+
+	}
 
 }

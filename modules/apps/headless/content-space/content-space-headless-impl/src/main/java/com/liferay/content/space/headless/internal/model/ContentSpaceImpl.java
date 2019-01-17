c78f5619fba56518b295d7128e05bbcd6a2436bf
Javier Gamarra <nhpatt@gmail.com>
2019-01-17 14:28:09 +0100

    LPS-89235 sf

--- modules/apps/headless/content-space/content-space-headless-impl/src/main/java/com/liferay/content/space/headless/internal/model/ContentSpaceImpl.java
+++ modules/apps/headless/content-space/content-space-headless-impl/src/main/java/com/liferay/content/space/headless/internal/model/ContentSpaceImpl.java
@@ -1,11 +1,11 @@
 /**
  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- * <p>
+ *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 2.1 of the License, or (at your option)
  * any later version.
- * <p>
+ *
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
@@ -23,11 +23,9 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 
-
 /**
- * @author Ruben Pulido
+ * @author Rubén Pulido
  */
-
 public class ContentSpaceImpl implements ContentSpace {
 
 	public ContentSpaceImpl(Group group) {

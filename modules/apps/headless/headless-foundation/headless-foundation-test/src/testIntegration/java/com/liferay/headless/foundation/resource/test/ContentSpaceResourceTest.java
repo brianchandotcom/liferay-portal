d68ed71e800f12b08c888a773187028b76494185
Sarai Díaz García <dgarciasarai@gmail.com>
2019-01-29 15:39:23 +0100

    LPS-89625 Update tests

--- modules/apps/headless/headless-foundation/headless-foundation-test/src/testIntegration/java/com/liferay/headless/foundation/resource/test/ContentSpaceResourceTest.java
+++ modules/apps/headless/headless-foundation/headless-foundation-test/src/testIntegration/java/com/liferay/headless/foundation/resource/test/ContentSpaceResourceTest.java
@@ -15,14 +15,23 @@
 package com.liferay.headless.foundation.resource.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
-
 import com.liferay.headless.foundation.dto.ContentSpace;
+import com.liferay.headless.foundation.dto.ContentSpaceCollection;
 import com.liferay.headless.foundation.resource.ContentSpaceResource;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerTestRule;
-import org.junit.runner.RunWith;
+import com.liferay.portal.vulcan.context.Pagination;
+
+import java.util.Collection;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -30,17 +39,11 @@ import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
-
-
-import com.liferay.portal.kernel.service.CompanyLocalService;
-import com.liferay.portal.kernel.service.GroupLocalService;
-import com.liferay.portal.kernel.service.ServiceContext;
-import com.liferay.portal.kernel.test.rule.AggregateTestRule;
-import com.liferay.portal.kernel.test.util.GroupTestUtil;
-import com.liferay.portal.kernel.util.LocaleUtil;
+import org.junit.runner.RunWith;
 
 /**
  * @author Cristina González
+ * @author Sarai Díaz
  */
 @RunWith(Arquillian.class)
 public class ContentSpaceResourceTest {
@@ -63,21 +66,63 @@ public class ContentSpaceResourceTest {
 	}
 
 	@Test
-	public void testGetContentSpace() {
-		ContentSpace contentSpace =
-			_contentSpaceResource.getContentSpace(_group.getGroupId());
+	public void testGetContentSpaceCollectionWithActiveGroup()
+		throws Exception {
 
-		Assert.assertEquals(
-			Long.valueOf(_group.getGroupId()), contentSpace.getId());
+		ContentSpaceCollection<ContentSpace> contentSpaceCollection =
+			_contentSpaceResource.getContentSpaceCollection(
+				new Pagination(20, 1), "");
+
+		ContentSpace contentSpace = _getContentSpace(
+			contentSpaceCollection.getItems(), _group.getGroupId());
+
+		Assert.assertNotNull(contentSpace);
 	}
+
+	@Test
+	public void testGetContentSpaceCollectionWithInactiveGroup()
+		throws Exception {
+
+		_deactivateGroup(_group);
+
+		ContentSpaceCollection<ContentSpace> contentSpaceCollection =
+			_contentSpaceResource.getContentSpaceCollection(
+				new Pagination(20, 1), "");
+
+		ContentSpace contentSpace = _getContentSpace(
+			contentSpaceCollection.getItems(), _group.getGroupId());
+
+		Assert.assertNull(contentSpace);
+	}
+
+	private Group _deactivateGroup(Group group) throws PortalException {
+		return _groupLocalService.updateGroup(
+			group.getGroupId(), group.getParentGroupId(), group.getNameMap(),
+			group.getDescriptionMap(), group.getType(),
+			group.isManualMembership(), group.getMembershipRestriction(),
+			group.getFriendlyURL(), group.isInheritContent(), false,
+			new ServiceContext());
+	}
+
+	private ContentSpace _getContentSpace(
+		Collection<ContentSpace> contentSpaces, long groupId) {
+
+		Stream<ContentSpace> stream = contentSpaces.stream();
+
+		return stream.filter(
+			contentSpace -> Objects.equals(contentSpace.getId(), groupId)
+		).findAny(
+		).orElse(
+			null
+		);
+	}
+
+	@Inject
+	private ContentSpaceResource _contentSpaceResource;
 
 	private Group _group;
 
 	@Inject
 	private GroupLocalService _groupLocalService;
-
-	@Inject
-	private ContentSpaceResource _contentSpaceResource;
-
 
 }

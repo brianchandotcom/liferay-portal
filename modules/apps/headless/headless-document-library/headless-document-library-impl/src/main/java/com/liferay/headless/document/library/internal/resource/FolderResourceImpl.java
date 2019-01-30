befc5778060f4724ffd2592407c8c392fe73d807
rubenpulido <ruben.pulido@liferay.com>
2019-01-30 13:33:35 +0100

    LPS-89711 Implement getFolderCollection

--- modules/apps/headless/headless-document-library/headless-document-library-impl/src/main/java/com/liferay/headless/document/library/internal/resource/FolderResourceImpl.java
+++ modules/apps/headless/headless-document-library/headless-document-library-impl/src/main/java/com/liferay/headless/document/library/internal/resource/FolderResourceImpl.java
@@ -14,16 +14,25 @@
 
 package com.liferay.headless.document.library.internal.resource;
 
+import com.liferay.document.library.kernel.service.DLAppService;
 import com.liferay.headless.document.library.dto.Folder;
 import com.liferay.headless.document.library.dto.FolderCollection;
 import com.liferay.headless.document.library.resource.FolderResource;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.CompanyService;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.vulcan.context.Pagination;
 
-import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.annotation.Generated;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 import org.osgi.service.jaxrs.whiteboard.JaxrsWhiteboardConstants;
 
@@ -46,7 +55,40 @@ public class FolderResourceImpl implements FolderResource {
 			Pagination pagination, String size)
 		throws Exception {
 
-		return new FolderCollection(Collections.emptyList(), 0);
+		Company company = _companyService.getCompanyByWebId(
+			PropsUtil.get(PropsKeys.COMPANY_DEFAULT_WEB_ID));
+
+		Group group = company.getGroup();
+
+		List<com.liferay.portal.kernel.repository.model.Folder> folders =
+			_dlAppService.getFolders(
+				group.getGroupId(), 0, pagination.getStartPosition(),
+				pagination.getEndPosition(), null);
+
+		Stream<com.liferay.portal.kernel.repository.model.Folder> stream =
+			folders.stream();
+
+		List<Folder> folders1 = stream.map(
+			folder -> {
+				Folder folder1 = new Folder();
+
+				folder1.setId(folder.getFolderId());
+
+				return folder1;
+			}
+		).collect(
+			Collectors.toList()
+		);
+
+		int count = _dlAppService.getFoldersCount(group.getGroupId(), 0);
+
+		return new FolderCollection(folders1, count);
 	}
+
+	@Reference
+	private CompanyService _companyService;
+
+	@Reference
+	private DLAppService _dlAppService;
 
 }

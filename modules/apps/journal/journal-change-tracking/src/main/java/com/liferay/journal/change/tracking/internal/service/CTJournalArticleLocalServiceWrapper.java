a4783d66d4fb0239c108e8a149885842f14d07b7
Daniel Kocsis <daniel.kocsis@liferay.com>
2019-01-30 17:04:21 +0100

    LPS-89624 Adding a more obvious syntax to synchronize the wrapper with the service class

--- modules/apps/journal/journal-change-tracking/src/main/java/com/liferay/journal/change/tracking/internal/service/CTJournalArticleLocalServiceWrapper.java
+++ modules/apps/journal/journal-change-tracking/src/main/java/com/liferay/journal/change/tracking/internal/service/CTJournalArticleLocalServiceWrapper.java
@@ -508,7 +508,16 @@ public class CTJournalArticleLocalServiceWrapper
 		return journalArticle;
 	}
 
-	private void _registerChange(JournalArticle journalArticle, int changeType)
+	@Reference(unbind = "-")
+	protected void setJournalArticleLocalService(
+		JournalArticleLocalService journalArticleLocalService) {
+
+		// this is needed because of synchronisation
+
+	}
+
+	private void _registerChange(
+			JournalArticle journalArticle, int changeType)
 		throws CTException {
 
 		try {

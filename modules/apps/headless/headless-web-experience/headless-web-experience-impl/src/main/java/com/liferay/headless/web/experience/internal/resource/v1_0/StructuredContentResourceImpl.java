360d54cc79aa2537444ebf12d5b0e9cf56702a57
Cristina González Castellano <cristina.gonzalez@liferay.com>
2019-02-06 17:51:09 +0100

    LPS-89658 Support filtering and sorting with Odata in structured content.

--- modules/apps/headless/headless-web-experience/headless-web-experience-impl/src/main/java/com/liferay/headless/web/experience/internal/resource/v1_0/StructuredContentResourceImpl.java
+++ modules/apps/headless/headless-web-experience/headless-web-experience-impl/src/main/java/com/liferay/headless/web/experience/internal/resource/v1_0/StructuredContentResourceImpl.java
@@ -14,29 +14,15 @@
 
 package com.liferay.headless.web.experience.internal.resource.v1_0;
 
+import com.liferay.headless.web.experience.internal.odata.ODataHelper;
 import com.liferay.headless.web.experience.dto.v1_0.StructuredContent;
 import com.liferay.headless.web.experience.resource.v1_0.StructuredContentResource;
 import com.liferay.journal.model.JournalArticle;
-import com.liferay.journal.model.JournalArticleConstants;
-import com.liferay.journal.util.JournalHelper;
-import com.liferay.portal.kernel.model.Group;
-import com.liferay.portal.kernel.search.Field;
-import com.liferay.portal.kernel.search.Hits;
-import com.liferay.portal.kernel.search.IndexSearcherHelperUtil;
-import com.liferay.portal.kernel.search.Indexer;
-import com.liferay.portal.kernel.search.IndexerRegistry;
-import com.liferay.portal.kernel.search.Query;
-import com.liferay.portal.kernel.search.QueryConfig;
-import com.liferay.portal.kernel.search.SearchContext;
-import com.liferay.portal.kernel.search.SearchException;
-import com.liferay.portal.kernel.search.SearchResultPermissionFilter;
-import com.liferay.portal.kernel.search.SearchResultPermissionFilterFactory;
-import com.liferay.portal.kernel.search.SearchResultPermissionFilterSearcher;
-import com.liferay.portal.kernel.security.permission.PermissionChecker;
-import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
-import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.kernel.service.GroupService;
 import com.liferay.portal.vulcan.context.Pagination;
 import com.liferay.portal.vulcan.dto.Page;
+
+import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -54,81 +40,15 @@ public class StructuredContentResourceImpl
 
 	@Override
 	public Page<StructuredContent> getContentSpaceStructuredContentsPage(
-			Long parentId, String filter, String sort, Pagination pagination)
+			Long contentSpaceId, String filter, String sort,
+			Pagination pagination)
 		throws Exception {
 
-		Hits hits = _getHits(pagination);
+		List<JournalArticle> journalArticles = _oDataHelper.getJournalArticles(
+			_groupService.getGroup(contentSpaceId),
+			acceptLanguage.getPreferredLocale(), pagination, filter, sort);
 
-		return Page.of(
-			transform(
-				_journalHelper.getArticles(hits), this::_toStructuredContent),
-			pagination, hits.getLength());
-	}
-
-	private SearchContext _createSearchContext(
-		Group group, Pagination pagination) {
-
-		SearchContext searchContext = new SearchContext();
-
-		searchContext.setAttribute(
-			Field.CLASS_NAME_ID, JournalArticleConstants.CLASSNAME_ID_DEFAULT);
-		searchContext.setAttribute(
-			Field.STATUS, WorkflowConstants.STATUS_APPROVED);
-		searchContext.setAttribute("head", Boolean.TRUE);
-		searchContext.setCompanyId(company.getCompanyId());
-		searchContext.setEnd(pagination.getEndPosition());
-		searchContext.setGroupIds(new long[] {group.getGroupId()});
-		searchContext.setStart(pagination.getStartPosition());
-
-		QueryConfig queryConfig = searchContext.getQueryConfig();
-
-		queryConfig.setHighlightEnabled(false);
-		queryConfig.setScoreEnabled(false);
-		queryConfig.setSelectedFieldNames(
-			Field.ARTICLE_ID, Field.SCOPE_GROUP_ID);
-
-		return searchContext;
-	}
-
-	private Hits _getHits(Pagination pagination) throws Exception {
-		SearchContext searchContext = _createSearchContext(
-			company.getGroup(), pagination);
-
-		Query query = _getQuery(searchContext);
-
-		PermissionChecker permissionChecker =
-			PermissionThreadLocal.getPermissionChecker();
-
-		if (permissionChecker == null) {
-			return IndexSearcherHelperUtil.search(searchContext, query);
-		}
-
-		if (searchContext.getUserId() == 0) {
-			searchContext.setUserId(permissionChecker.getUserId());
-		}
-
-		SearchResultPermissionFilter searchResultPermissionFilter =
-			_searchResultPermissionFilterFactory.create(
-				new SearchResultPermissionFilterSearcher() {
-
-					public Hits search(SearchContext searchContext)
-						throws SearchException {
-
-						return IndexSearcherHelperUtil.search(
-							searchContext, query);
-					}
-
-				},
-				permissionChecker);
-
-		return searchResultPermissionFilter.search(searchContext);
-	}
-
-	private Query _getQuery(SearchContext searchContext) throws Exception {
-		Indexer<JournalArticle> indexer = _indexerRegistry.nullSafeGetIndexer(
-			JournalArticle.class);
-
-		return indexer.getFullQuery(searchContext);
+		return Page.of(transform(journalArticles, this::_toStructuredContent));
 	}
 
 	private StructuredContent _toStructuredContent(
@@ -151,13 +71,9 @@ public class StructuredContentResourceImpl
 	}
 
 	@Reference
-	private IndexerRegistry _indexerRegistry;
+	private GroupService _groupService;
 
 	@Reference
-	private JournalHelper _journalHelper;
-
-	@Reference
-	private SearchResultPermissionFilterFactory
-		_searchResultPermissionFilterFactory;
+	private ODataHelper _oDataHelper;
 
 }

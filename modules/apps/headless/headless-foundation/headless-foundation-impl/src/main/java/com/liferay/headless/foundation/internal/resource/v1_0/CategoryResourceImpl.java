24fce330afce6d11deb6d12e1e2efc4be024b3f1
Peter Shin <peter.shin@liferay.com>
2019-02-22 09:47:29 -0800

    LPS-90074 Auto SF

--- modules/apps/headless/headless-foundation/headless-foundation-impl/src/main/java/com/liferay/headless/foundation/internal/resource/v1_0/CategoryResourceImpl.java
+++ modules/apps/headless/headless-foundation/headless-foundation-impl/src/main/java/com/liferay/headless/foundation/internal/resource/v1_0/CategoryResourceImpl.java
@@ -242,7 +242,7 @@ public class CategoryResourceImpl
 					_assetCategoryService.getChildCategoriesCount(
 						assetCategory.getCategoryId());
 
-				hasCategories = (childAssetCategoriesCount > 0);
+				hasCategories = childAssetCategoriesCount > 0;
 
 				id = assetCategory.getCategoryId();
 				name = assetCategory.getTitle(

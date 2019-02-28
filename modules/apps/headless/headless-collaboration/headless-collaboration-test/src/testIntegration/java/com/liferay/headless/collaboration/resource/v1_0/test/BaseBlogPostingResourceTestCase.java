d5f39960ac4cf3741ff43a4297067661b9193529
rubenpulido <ruben.pulido@liferay.com>
2019-02-28 13:33:16 +0100

    LPS-91304 Initialize dateFormat during setUpClass to avoid NPE

--- modules/apps/headless/headless-collaboration/headless-collaboration-test/src/testIntegration/java/com/liferay/headless/collaboration/resource/v1_0/test/BaseBlogPostingResourceTestCase.java
+++ modules/apps/headless/headless-collaboration/headless-collaboration-test/src/testIntegration/java/com/liferay/headless/collaboration/resource/v1_0/test/BaseBlogPostingResourceTestCase.java
@@ -56,6 +56,7 @@ import javax.annotation.Generated;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -71,6 +72,12 @@ public abstract class BaseBlogPostingResourceTestCase {
 	@Rule
 	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
 		new LiferayIntegrationTestRule();
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		_dateFormat = DateFormatFactoryUtil.getSimpleDateFormat(
+			"yyyy-MM-dd'T'HH:mm:ss'Z'");
+	}
 
 	@Before
 	public void setUp() throws Exception {
@@ -696,8 +703,8 @@ public abstract class BaseBlogPostingResourceTestCase {
 	@Inject
 	private BlogPostingResource _blogPostingResource;
 
-	private final DateFormat _dateFormat =
-		DateFormatFactoryUtil.getSimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+	private static DateFormat _dateFormat;
+
 	private URL _resourceURL;
 
 }

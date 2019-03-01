0c7d372dc3f2b9308aac8bd665d0bba7c52a6979
Peter Shin <peter.shin@liferay.com>
2019-03-01 15:59:49 -0600

    LPS-89415 Fix array data types

--- modules/util/portal-tools-rest-builder/src/main/java/com/liferay/portal/tools/rest/builder/internal/freemarker/tool/java/parser/ResourceOpenAPIParser.java
+++ modules/util/portal-tools-rest-builder/src/main/java/com/liferay/portal/tools/rest/builder/internal/freemarker/tool/java/parser/ResourceOpenAPIParser.java
@@ -227,7 +227,7 @@ public class ResourceOpenAPIParser {
 
 		if (parameterNames.contains("sort")) {
 			JavaMethodParameter javaMethodParameter = new JavaMethodParameter(
-				"sorts", Sort.class.getName() + "[]");
+				"sorts", Sort[].class.getName());
 
 			javaMethodParameters.add(javaMethodParameter);
 		}
@@ -469,7 +469,7 @@ public class ResourceOpenAPIParser {
 			return "@Context";
 		}
 
-		if (Objects.equals(parameterType, Sort.class.getName() + "[]") &&
+		if (Objects.equals(parameterType, Sort[].class.getName()) &&
 			parameterNames.contains("sort")) {
 
 			return "@Context";
@@ -529,12 +529,13 @@ public class ResourceOpenAPIParser {
 				String returnType = OpenAPIParserUtil.getJavaDataType(
 					javaDataTypeMap, schema);
 
-				if (returnType.endsWith("[]")) {
+				if (returnType.startsWith("[")) {
 					StringBuilder sb = new StringBuilder();
 
 					sb.append(Page.class.getName());
 					sb.append("<");
-					sb.append(returnType.substring(0, returnType.length() - 2));
+					sb.append(
+						OpenAPIParserUtil.getElementClassName(returnType));
 					sb.append(">");
 
 					return sb.toString();

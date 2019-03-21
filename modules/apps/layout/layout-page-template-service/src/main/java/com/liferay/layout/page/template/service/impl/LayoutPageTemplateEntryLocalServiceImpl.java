7b140998cab73a6ac0b7578b9e72be1ffbb93140
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2019-03-21 17:15:59 +0100

    LPS-92447 Uses name with a count instead a random string for draft names

--- modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/service/impl/LayoutPageTemplateEntryLocalServiceImpl.java
+++ modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/service/impl/LayoutPageTemplateEntryLocalServiceImpl.java
@@ -41,7 +41,6 @@ import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.LocalizationUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
-import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.spring.extender.service.ServiceReference;
@@ -169,23 +168,26 @@ public class LayoutPageTemplateEntryLocalServiceImpl
 		if (status == WorkflowConstants.STATUS_APPROVED) {
 			serviceContext.setModifiedDate(now);
 
-			String draftName = StringUtil.randomString(64);
+			int count = 0;
 
-			LayoutPageTemplateEntry draftLayoutPageTemplateEntry =
-				fetchLayoutPageTemplateEntry(groupId, draftName);
+			while (true) {
+				LayoutPageTemplateEntry draftLayoutPageTemplateEntry =
+					fetchLayoutPageTemplateEntry(
+						groupId, name + "_draft_" + count);
 
-			while (draftLayoutPageTemplateEntry != null) {
-				draftName = StringUtil.randomString(64);
+				if (draftLayoutPageTemplateEntry == null) {
+					break;
+				}
 
-				draftLayoutPageTemplateEntry = fetchLayoutPageTemplateEntry(
-					groupId, draftName);
+				count++;
 			}
 
-			draftLayoutPageTemplateEntry = addLayoutPageTemplateEntry(
-				userId, groupId, layoutPageTemplateCollectionId, classNameId,
-				classTypeId, draftName, type, defaultTemplate,
-				layoutPrototypeId, previewFileEntryId, 0,
-				WorkflowConstants.STATUS_DRAFT, serviceContext);
+			LayoutPageTemplateEntry draftLayoutPageTemplateEntry =
+				addLayoutPageTemplateEntry(
+					userId, groupId, layoutPageTemplateCollectionId,
+					classNameId, classTypeId, name + "_draft_" + count, type,
+					defaultTemplate, layoutPrototypeId, previewFileEntryId, 0,
+					WorkflowConstants.STATUS_DRAFT, serviceContext);
 
 			layoutPageTemplateEntry.setDraftLayoutPageTemplateEntryId(
 				draftLayoutPageTemplateEntry.getLayoutPageTemplateEntryId());

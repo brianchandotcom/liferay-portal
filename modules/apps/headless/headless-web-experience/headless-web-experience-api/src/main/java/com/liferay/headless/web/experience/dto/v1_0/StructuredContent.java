6ac7d75bd3b6038dbfeab4ba4916185304a8e19d
Javier Gamarra <nhpatt@gmail.com>
2019-03-24 11:07:55 +0100

    LPS-92454 regenerate

--- modules/apps/headless/headless-web-experience/headless-web-experience-api/src/main/java/com/liferay/headless/web/experience/dto/v1_0/StructuredContent.java
+++ modules/apps/headless/headless-web-experience/headless-web-experience-api/src/main/java/com/liferay/headless/web/experience/dto/v1_0/StructuredContent.java
@@ -341,6 +341,28 @@ public class StructuredContent {
 	@JsonProperty(access = JsonProperty.Access.READ_WRITE)
 	protected Long id;
 
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	@JsonIgnore
+	public void setKey(UnsafeSupplier<String, Exception> keyUnsafeSupplier) {
+		try {
+			key = keyUnsafeSupplier.get();
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@GraphQLField
+	@JsonProperty(access = JsonProperty.Access.READ_WRITE)
+	protected String key;
+
 	public String[] getKeywords() {
 		return keywords;
 	}
@@ -512,6 +534,28 @@ public class StructuredContent {
 	@JsonProperty(access = JsonProperty.Access.READ_WRITE)
 	protected String title;
 
+	public String getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+
+	@JsonIgnore
+	public void setUuid(UnsafeSupplier<String, Exception> uuidUnsafeSupplier) {
+		try {
+			uuid = uuidUnsafeSupplier.get();
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@GraphQLField
+	@JsonProperty(access = JsonProperty.Access.READ_WRITE)
+	protected String uuid;
+
 	public ViewableBy getViewableBy() {
 		return viewableBy;
 	}
@@ -647,6 +691,13 @@ public class StructuredContent {
 		sb.append(id);
 		sb.append(", ");
 
+		sb.append("\"key\": ");
+
+		sb.append("\"");
+		sb.append(key);
+		sb.append("\"");
+		sb.append(", ");
+
 		sb.append("\"keywords\": ");
 
 		if (keywords == null) {
@@ -749,6 +800,13 @@ public class StructuredContent {
 
 		sb.append("\"");
 		sb.append(title);
+		sb.append("\"");
+		sb.append(", ");
+
+		sb.append("\"uuid\": ");
+
+		sb.append("\"");
+		sb.append(uuid);
 		sb.append("\"");
 		sb.append(", ");
 

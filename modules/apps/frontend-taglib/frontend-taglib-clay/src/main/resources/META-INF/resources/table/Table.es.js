22bf58286a0758b9a1a54158671a85a57b2c506d
Chema Balsas <jose.balsas@liferay.com>
2019-03-29 21:48:41 +0100

    LPS-91785 Matches openAPI page and pageSize common args more closely

--- modules/apps/frontend-taglib/frontend-taglib-clay/src/main/resources/META-INF/resources/table/Table.es.js
+++ modules/apps/frontend-taglib/frontend-taglib-clay/src/main/resources/META-INF/resources/table/Table.es.js
@@ -1,63 +1,46 @@
-import 'clay-table';
-import 'clay-pagination-bar';
+import {Config} from 'metal-state';
 import ClayComponent from 'clay-component';
 import Soy from 'metal-soy';
-import {Config} from 'metal-state';
+
+import 'clay-table';
+import 'clay-pagination-bar';
 
 import templates from './Table.soy';
 
 class Table extends ClayComponent {
-
-	_handleItemToggled(event) {
-	}
-
-	_handleItemsPerPageClicked(event) {
+	_handlepageSizeClicked(event) {
 		event.preventDefault();
 
-		if (this.itemsPerPage == event.data.item.label) {
-			return;
-		}
+		this.pageSize = event.data.item.label;
 
-		this.itemsPerPage = event.data.item.label;
-
-		this.paginationSelectedEntry = this.paginationEntries.map((x) => x.label).indexOf(this.itemsPerPage);
+		this.paginationSelectedEntry = this.paginationEntries.map(
+			x => x.label
+		).indexOf(this.pageSize);
 
 		this._loadData();
 	}
 
 	_handlePageClicked(event) {
-		const newPageNumber = parseInt(event.data.page, 10);
+		event.preventDefault();
 
-		if (this.pageNumber == newPageNumber) {
-			return;
-		}
-
-		this.pageNumber = newPageNumber;
+		this.page = parseInt(event.data.page, 10);
 
 		this._loadData();
 	}
 
 	_loadData() {
-		fetch(
-			this._getDataSourceURL()
-		).then(
-			response =>
-				response.json()
-		).then(
-			result => {
-				this.items = result.items;
-			}
-		).catch(
-			() => {}
-		);
+		fetch(this._getDataSourceURL())
+			.then(response => response.json())
+			.then(
+				result => {
+					this.items = result.items;
+				}
+			);
 	}
 
 	_getDataSourceURL() {
-		return `${this.dataSourceURL}?` +
-			`itemsPerPage=${this.itemsPerPage}` +
-			`&pageNumber=${this.pageNumber}`;
+		return `${this.dataSourceURL}?pageSize=${this.pageSize}&page=${this.page}`;
 	}
-
 }
 
 Soy.register(Table, templates);
@@ -65,8 +48,8 @@ Soy.register(Table, templates);
 Table.STATE = {
 	dataSourceURL: Config.string(),
 	items: Config.array(),
-	itemsPerPage: Config.number(),
-	pageNumber: Config.number(),
+	page: Config.number(),
+	pageSize: Config.number(),
 	paginationEntries: Config.array(),
 	paginationSelectedEntry: Config.number()
 };

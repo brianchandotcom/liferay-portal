brianchandotcom/liferay-portal

commit 05e8915a82493429715f7e97efe34690bb8abf95

LPS-91855 rename

modules/apps/asset/asset-service/src/main/java/com/liferay/asset/internal/info/renderer/BaseAssetEntryInfoItemRenderer.java
@@ -30,7 +30,7 @@ import org.osgi.service.component.annotations.Component;
  * @author Jorge Ferrer
  */
 @Component(service = InfoItemRenderer.class)
-public abstract class AssetEntryInfoItemRenderer
+public abstract class BaseAssetEntryInfoItemRenderer
 	implements InfoItemRenderer<AssetEntry> {
 
 	@Override

modules/apps/asset/asset-service/src/main/java/com/liferay/asset/internal/info/renderer/AbstractAssetEntryInfoItemRenderer.java
@@ -23,12 +23,12 @@ import org.osgi.service.component.annotations.Component;
  * @author Jorge Ferrer
  */
 @Component(service = InfoItemRenderer.class)
-public class AssetEntryFullContentInfoItemRenderer
-	extends AssetEntryInfoItemRenderer {
+public class AbstractAssetEntryInfoItemRenderer
+	extends BaseAssetEntryInfoItemRenderer {
 
 	@Override
 	protected String getTemplate() {
-		return AssetRenderer.TEMPLATE_FULL_CONTENT;
+		return AssetRenderer.TEMPLATE_ABSTRACT;
 	}
 
 }

modules/apps/asset/asset-service/src/main/java/com/liferay/asset/internal/info/renderer/FullContentAssetEntryInfoItemRenderer.java
@@ -23,12 +23,12 @@ import org.osgi.service.component.annotations.Component;
  * @author Jorge Ferrer
  */
 @Component(service = InfoItemRenderer.class)
-public class AssetEntryAbstractInfoItemRenderer
-	extends AssetEntryInfoItemRenderer {
+public class FullContentAssetEntryInfoItemRenderer
+	extends BaseAssetEntryInfoItemRenderer {
 
 	@Override
 	protected String getTemplate() {
-		return AssetRenderer.TEMPLATE_ABSTRACT;
+		return AssetRenderer.TEMPLATE_FULL_CONTENT;
 	}
 
 }

commit 3ae3f15d06da333c5fb57af3f8ae0f3e09a525f4

LPS-91855 You can not have abstract Component, caused by 05e8915a82493429715f7e97efe34690bb8abf95

modules/apps/asset/asset-service/src/main/java/com/liferay/asset/internal/info/renderer/BaseAssetEntryInfoItemRenderer.java
@@ -24,12 +24,10 @@ import com.liferay.portal.kernel.util.WebKeys;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.osgi.service.component.annotations.Component;
 
 /**
  * @author Jorge Ferrer
  */
-@Component(service = InfoItemRenderer.class)
 public abstract class BaseAssetEntryInfoItemRenderer
 	implements InfoItemRenderer<AssetEntry> {
 
d261c6fe26839122a1d551b6a841990ef35f13b2
Javier Gamarra <nhpatt@gmail.com>
2019-04-09 07:35:08 +0000

    LPS-91222 organization test

--- modules/apps/headless/headless-foundation/headless-foundation-test/src/testIntegration/java/com/liferay/headless/foundation/resource/v1_0/test/OrganizationResourceTest.java
+++ modules/apps/headless/headless-foundation/headless-foundation-test/src/testIntegration/java/com/liferay/headless/foundation/resource/v1_0/test/OrganizationResourceTest.java
@@ -15,14 +15,186 @@
 package com.liferay.headless.foundation.resource.v1_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.headless.foundation.dto.v1_0.Organization;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.OrganizationLocalServiceUtil;
+import com.liferay.portal.kernel.service.UserLocalServiceUtil;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
 
-import org.junit.Ignore;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 
 /**
  * @author Javier Gamarra
  */
-@Ignore
 @RunWith(Arquillian.class)
 public class OrganizationResourceTest extends BaseOrganizationResourceTestCase {
+
+	@Before
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+
+		_user = UserTestUtil.addGroupAdminUser(testGroup);
+	}
+
+	@Override
+	protected void assertValid(Organization organization) {
+		boolean valid = false;
+
+		if ((organization.getId() != null) &&
+			(organization.getName() != null)) {
+
+			valid = true;
+		}
+
+		Assert.assertTrue(valid);
+	}
+
+	@Override
+	protected boolean equals(
+		Organization organization1, Organization organization2) {
+
+		if (Objects.equals(organization1.getName(), organization2.getName())) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	protected Organization
+			testGetMyUserAccountOrganizationsPage_addOrganization(
+				Long userAccountId, Organization organization)
+		throws Exception {
+
+		return _addUserOrganization(userAccountId, organization);
+	}
+
+	@Override
+	protected Long testGetMyUserAccountOrganizationsPage_getUserAccountId()
+		throws Exception {
+
+		return _user.getUserId();
+	}
+
+	@Override
+	protected Organization testGetOrganization_addOrganization()
+		throws Exception {
+
+		return _addUserOrganization(_user.getUserId(), randomOrganization());
+	}
+
+	@Override
+	protected Organization testGetOrganizationOrganizationsPage_addOrganization(
+			Long parentOrganizationId, Organization organization)
+		throws Exception {
+
+		com.liferay.portal.kernel.model.Organization
+			serviceBuilderOrganization = _addOrganization(
+				organization, parentOrganizationId);
+
+		return _toOrganization(serviceBuilderOrganization);
+	}
+
+	@Override
+	protected Long
+			testGetOrganizationOrganizationsPage_getParentOrganizationId()
+		throws Exception {
+
+		com.liferay.portal.kernel.model.Organization organization =
+			_addOrganization(randomOrganization(), 0);
+
+		return organization.getOrganizationId();
+	}
+
+	@Override
+	protected Organization testGetOrganizationsPage_addOrganization(
+			Organization organization)
+		throws Exception {
+
+		return _addUserOrganization(_user.getUserId(), organization);
+	}
+
+	@Override
+	protected Organization testGetUserAccountOrganizationsPage_addOrganization(
+			Long userAccountId, Organization organization)
+		throws Exception {
+
+		return _addUserOrganization(userAccountId, organization);
+	}
+
+	@Override
+	protected Long testGetUserAccountOrganizationsPage_getUserAccountId()
+		throws Exception {
+
+		return _user.getUserId();
+	}
+
+	private com.liferay.portal.kernel.model.Organization _addOrganization(
+			Organization organization, long parentOrganizationId)
+		throws PortalException {
+
+		com.liferay.portal.kernel.model.Organization
+			serviceBuilderOrganization =
+				OrganizationLocalServiceUtil.addOrganization(
+					_user.getUserId(), parentOrganizationId,
+					organization.getName(), true);
+
+		if (parentOrganizationId == 0) {
+			_organizations.add(serviceBuilderOrganization);
+		}
+		else {
+			_childOrganizations.add(serviceBuilderOrganization);
+		}
+
+		return serviceBuilderOrganization;
+	}
+
+	private Organization _addUserOrganization(
+			Long userAccountId, Organization organization)
+		throws Exception {
+
+		Organization parentOrganization = _toOrganization(
+			_addOrganization(organization, 0));
+
+		if (userAccountId != null) {
+			UserLocalServiceUtil.addOrganizationUser(
+				parentOrganization.getId(), userAccountId);
+		}
+
+		return parentOrganization;
+	}
+
+	private Organization _toOrganization(
+		com.liferay.portal.kernel.model.Organization organization) {
+
+		return new Organization() {
+			{
+				id = organization.getOrganizationId();
+				dateCreated = organization.getCreateDate();
+				dateModified = organization.getModifiedDate();
+				name = organization.getName();
+			}
+		};
+	}
+
+	@DeleteAfterTestRun
+	private final List<com.liferay.portal.kernel.model.Organization>
+		_childOrganizations = new ArrayList<>();
+
+	@DeleteAfterTestRun
+	private final List<com.liferay.portal.kernel.model.Organization>
+		_organizations = new ArrayList<>();
+
+	@DeleteAfterTestRun
+	private User _user;
+
 }

brianchandotcom/liferay-portal

commit 9d25b27518038fb9cedc2b1e9f87ae8d1a4c3dd1

LPS-82138 rename

modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/upgrade/JournalServiceUpgrade.java
@@ -54,7 +54,7 @@ import com.liferay.journal.internal.upgrade.v1_1_6.UpgradeAssetDisplayPageEntry;
 import com.liferay.journal.internal.upgrade.v2_0_0.util.JournalArticleTable;
 import com.liferay.journal.internal.upgrade.v2_0_0.util.JournalFeedTable;
 import com.liferay.journal.internal.upgrade.v2_0_0.util.JournalFolderTable;
-import com.liferay.journal.internal.upgrade.v3_0_0.UpgradeJournalImages;
+import com.liferay.journal.internal.upgrade.v3_0_0.UpgradeJournalArticleImage;
 import com.liferay.journal.model.JournalArticle;
 import com.liferay.portal.configuration.upgrade.PrefsPropsToConfigurationUpgradeHelper;
 import com.liferay.portal.kernel.dao.db.DB;
@@ -202,7 +202,7 @@ public class JournalServiceUpgrade implements UpgradeStepRegistrator {
 				}));
 
 		registry.register(
-			"2.0.0", "3.0.0", new UpgradeJournalImages(_imageLocalService));
+			"2.0.0", "3.0.0", new UpgradeJournalArticleImage(_imageLocalService));
 	}
 
 	protected void deleteTempImages() throws Exception {

modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/upgrade/v3_0_0/UpgradeJournalArticleImage.java
@@ -26,9 +26,9 @@ import java.sql.Statement;
 /**
  * @author Pavel Savinov
  */
-public class UpgradeJournalImages extends UpgradeProcess {
+public class UpgradeJournalArticleImage extends UpgradeProcess {
 
-	public UpgradeJournalImages(ImageLocalService imageLocalService) {
+	public UpgradeJournalArticleImage(ImageLocalService imageLocalService) {
 		_imageLocalService = imageLocalService;
 	}
 
@@ -58,7 +58,7 @@ public class UpgradeJournalImages extends UpgradeProcess {
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
-		UpgradeJournalImages.class);
+		UpgradeJournalArticleImage.class);
 
 	private final ImageLocalService _imageLocalService;
 

modules/apps/journal/journal-service/src/main/resources/META-INF/module-log4j.xml
@@ -26,7 +26,7 @@
 		<priority value="WARN" />
 	</category>
 
-	<category name="com.liferay.journal.internal.upgrade.v3_0_0.UpgradeJournalImages">
+	<category name="com.liferay.journal.internal.upgrade.v3_0_0.UpgradeJournalArticleImage">
 		<priority value="INFO" />
 	</category>
 

commit 73416c3747070671ba2ddfb6bc5c6412f8db3313

LPS-82138 Fix compile, caused by 9d25b27518038fb9cedc2b1e9f87ae8d1a4c3dd1

modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/upgrade/JournalServiceUpgrade.java
@@ -54,7 +54,6 @@ import com.liferay.journal.internal.upgrade.v1_1_6.UpgradeAssetDisplayPageEntry;
 import com.liferay.journal.internal.upgrade.v2_0_0.util.JournalArticleTable;
 import com.liferay.journal.internal.upgrade.v2_0_0.util.JournalFeedTable;
 import com.liferay.journal.internal.upgrade.v2_0_0.util.JournalFolderTable;
-import com.liferay.journal.internal.upgrade.v3_0_0.UpgradeJournalArticleImage;
 import com.liferay.journal.model.JournalArticle;
 import com.liferay.portal.configuration.upgrade.PrefsPropsToConfigurationUpgradeHelper;
 import com.liferay.portal.kernel.dao.db.DB;
@@ -202,7 +201,9 @@ public class JournalServiceUpgrade implements UpgradeStepRegistrator {
 				}));
 
 		registry.register(
-			"2.0.0", "3.0.0", new UpgradeJournalArticleImage(_imageLocalService));
+			"2.0.0", "3.0.0",
+			new com.liferay.journal.internal.upgrade.v3_0_0.
+				UpgradeJournalArticleImage(_imageLocalService));
 	}
 
 	protected void deleteTempImages() throws Exception {
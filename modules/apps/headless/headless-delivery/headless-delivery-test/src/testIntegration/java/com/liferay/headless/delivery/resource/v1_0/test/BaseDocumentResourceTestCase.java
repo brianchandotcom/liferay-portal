70d0b1723d2a768a1bc0f57ca8285f725a869346
rubenpulido <ruben.pulido@liferay.com>
2019-05-23 15:53:42 +0200

    LPS-96050 Implement testPostSiteDocument comparing document JSON and content

--- modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/BaseDocumentResourceTestCase.java
+++ modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/BaseDocumentResourceTestCase.java
@@ -35,6 +35,7 @@ import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.util.DateFormatFactoryUtil;
+import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.odata.entity.EntityField;
 import com.liferay.portal.odata.entity.EntityModel;
@@ -781,15 +782,38 @@ public abstract class BaseDocumentResourceTestCase {
 
 	@Test
 	public void testPostSiteDocument() throws Exception {
-		Assert.assertTrue(true);
+		Document randomDocument = randomDocument();
+
+		Map<String, File> multipartFiles = getMultipartFiles();
+
+		Document postDocument =
+			testPostSiteDocument_addDocument(randomDocument, multipartFiles);
+
+		assertEquals(randomDocument, postDocument);
+		assertValid(postDocument);
+
+		String content = testPostSiteDocument_getContent(
+			postDocument.getContentUrl());
+
+		String expectedContent = new String(
+			FileUtil.getBytes(multipartFiles.get("file")));
+
+		Assert.assertEquals(content, expectedContent);
 	}
 
-	protected Document testPostSiteDocument_addDocument(Document document)
+	protected Document testPostSiteDocument_addDocument(
+		Document document, Map<String, File> multipartFiles)
 		throws Exception {
 
 		return DocumentResource.postSiteDocument(
 			testGetSiteDocumentsPage_getSiteId(), document,
-			getMultipartFiles());
+			multipartFiles);
+	}
+
+	protected String testPostSiteDocument_getContent(String contentURL)
+		throws Exception {
+
+		return DocumentResource.getContent(contentURL);
 	}
 
 	protected void assertHttpResponseStatusCode(
@@ -1493,6 +1517,7 @@ public abstract class BaseDocumentResourceTestCase {
 				id = RandomTestUtil.randomLong();
 				sizeInBytes = RandomTestUtil.randomLong();
 				title = RandomTestUtil.randomString();
+				viewableBy = ViewableBy.ANYONE;
 			}
 		};
 	}

996609c0349389f37b836b4987d2d919ff959821
Pavel Savinov <pavel.savinov@liferay.com>
2019-06-03 10:40:44 +0200

    LPS-96354 Add submit form listeners only for portlet configuration windows

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/utils/FragmentsEditorDialogUtils.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/utils/FragmentsEditorDialogUtils.js
@@ -140,17 +140,22 @@ function startListeningWidgetConfigurationChange(store) {
 				submitFormHandler = null;
 			}
 
-			submitFormHandler = event.win.Liferay.on(
-				'submitForm',
-				() => {
-					store.dispatch(
-						{
-							lastSaveDate: new Date(),
-							type: UPDATE_LAST_SAVE_DATE
-						}
-					);
-				}
-			);
+			const configurationForm = event.win.document.querySelector(
+				'.portlet-configuration-setup');
+
+			if (configurationForm) {
+				submitFormHandler = event.win.Liferay.on(
+					'submitForm',
+					() => {
+						store.dispatch(
+							{
+								lastSaveDate: new Date(),
+								type: UPDATE_LAST_SAVE_DATE
+							}
+						);
+					}
+				);
+			}
 		}
 	);
 }

40cc659165a7525afce5727f7aadde35f49776a1
mateomustapic <mateo.mustapic@liferay.com>
2019-06-05 09:50:53 +0200

    LPS-94542 fragment-web - Replace Tooltip with Clay Tooltip in fragment-web module

--- modules/apps/fragment/fragment-web/src/main/resources/META-INF/resources/js/SourceEditor.es.js
+++ modules/apps/fragment/fragment-web/src/main/resources/META-INF/resources/js/SourceEditor.es.js
@@ -1,4 +1,4 @@
-import 'frontend-js-web/liferay/compat/tooltip/Tooltip.es';
+import ClayTooltip from 'clay-tooltip';
 import Component from 'metal-component';
 import Soy from 'metal-soy';
 import {Config} from 'metal-state';
@@ -11,6 +11,25 @@ import './SourceEditorToolbar.es';
  * Creates a Source Editor component to use for source code editing.
  */
 class SourceEditor extends Component {
+	
+	/**
+	 * @inheritDoc
+	 */
+	attached() {
+		this._instanceClayTooltip = ClayTooltip.init({});
+
+		this._instanceClayTooltip.elementClasses = 'source-editor__fixed-text__help__tooltip';
+		this._instanceClayTooltip.position = 'BottomCenter';
+		this._instanceClayTooltip.selectors = ['.source-editor__fixed-text__help'];
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	disposed() {
+		this._instanceClayTooltip.dispose();
+	}
+
 	/**
 	 * Callback that propagates the <code>contentChanged</code> event when the
 	 * internal Ace Editor is modified.

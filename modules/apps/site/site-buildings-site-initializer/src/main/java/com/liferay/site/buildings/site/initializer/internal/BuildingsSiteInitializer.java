f2573739a9f6988cd3c93fc347a3d21a64373df1
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2019-06-28 12:29:37 +0200

    LPS-97863 Adds images

--- modules/apps/site/site-buildings-site-initializer/src/main/java/com/liferay/site/buildings/site/initializer/internal/BuildingsSiteInitializer.java
+++ modules/apps/site/site-buildings-site-initializer/src/main/java/com/liferay/site/buildings/site/initializer/internal/BuildingsSiteInitializer.java
@@ -14,26 +14,36 @@
 
 package com.liferay.site.buildings.site.initializer.internal;
 
+import com.liferay.document.library.kernel.model.DLFolderConstants;
+import com.liferay.document.library.kernel.service.DLAppLocalService;
+import com.liferay.document.library.util.DLURLHelper;
 import com.liferay.fragment.importer.FragmentsImporter;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.MimeTypesUtil;
 import com.liferay.site.exception.InitializationException;
 import com.liferay.site.initializer.SiteInitializer;
 
 import java.io.File;
+import java.io.InputStream;
 
 import java.net.URL;
 
+import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 import javax.servlet.ServletContext;
 
@@ -81,6 +91,8 @@ public class BuildingsSiteInitializer implements SiteInitializer {
 		try {
 			_createServiceContext(groupId);
 
+			_addImages();
+
 			_addFragments();
 		}
 		catch (Exception e) {
@@ -110,6 +122,38 @@ public class BuildingsSiteInitializer implements SiteInitializer {
 			file, false);
 	}
 
+	private void _addImages() throws Exception {
+		Enumeration<URL> urls = _bundle.findEntries(
+			_PATH + "/images", StringPool.STAR, false);
+
+		while (urls.hasMoreElements()) {
+			URL url = urls.nextElement();
+
+			byte[] bytes = null;
+
+			try (InputStream is = url.openStream()) {
+				bytes = FileUtil.getBytes(is);
+			}
+
+			String fileName = FileUtil.getShortFileName(url.getPath());
+
+			FileEntry fileEntry = _dlAppLocalService.addFileEntry(
+				_serviceContext.getUserId(), _serviceContext.getScopeGroupId(),
+				DLFolderConstants.DEFAULT_PARENT_FOLDER_ID, fileName,
+				MimeTypesUtil.getContentType(fileName), bytes, _serviceContext);
+
+			_fileEntriesObjectMap.put(
+				fileEntry.getFileName(),
+				JSONFactoryUtil.looseSerialize(fileEntry));
+
+			_fileEntriesLinkMap.put(
+				fileEntry.getFileName(),
+				_dlURLHelper.getPreviewURL(
+					fileEntry, fileEntry.getFileVersion(), null,
+					StringPool.BLANK, false, false));
+		}
+	}
+
 	private void _createServiceContext(long groupId) throws PortalException {
 		ServiceContext serviceContext = new ServiceContext();
 
@@ -131,10 +175,22 @@ public class BuildingsSiteInitializer implements SiteInitializer {
 
 	private static final String _NAME = "Buildings";
 
+	private static final String _PATH =
+		"com/liferay/site/buildings/site/initializer/internal/dependencies";
+
 	private static final Log _log = LogFactoryUtil.getLog(
 		BuildingsSiteInitializer.class);
 
 	private Bundle _bundle;
+
+	@Reference
+	private DLAppLocalService _dlAppLocalService;
+
+	@Reference
+	private DLURLHelper _dlURLHelper;
+
+	private final Map<String, String> _fileEntriesLinkMap = new HashMap<>();
+	private final Map<String, String> _fileEntriesObjectMap = new HashMap<>();
 
 	@Reference
 	private FragmentsImporter _fragmentsImporter;

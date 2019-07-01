f00a8b20875115fb217895d574b788f3324665d1
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2019-07-01 09:15:24 +0200

    LPS-97863 Adds web content

--- modules/apps/site/site-buildings-site-initializer/src/main/java/com/liferay/site/buildings/site/initializer/internal/BuildingsSiteInitializer.java
+++ modules/apps/site/site-buildings-site-initializer/src/main/java/com/liferay/site/buildings/site/initializer/internal/BuildingsSiteInitializer.java
@@ -20,13 +20,17 @@ import com.liferay.document.library.util.DLURLHelper;
 import com.liferay.dynamic.data.mapping.util.DefaultDDMStructureHelper;
 import com.liferay.fragment.importer.FragmentsImporter;
 import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.model.JournalFolderConstants;
+import com.liferay.journal.service.JournalArticleLocalService;
 import com.liferay.layout.page.template.constants.LayoutPageTemplateEntryTypeConstants;
 import com.liferay.layout.page.template.model.LayoutPageTemplateCollection;
 import com.liferay.layout.page.template.service.LayoutPageTemplateCollectionLocalService;
 import com.liferay.layout.page.template.service.LayoutPageTemplateEntryLocalService;
+import com.liferay.petra.string.CharPool;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
@@ -42,6 +46,7 @@ import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.MimeTypesUtil;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.site.exception.InitializationException;
 import com.liferay.site.initializer.SiteInitializer;
@@ -51,6 +56,7 @@ import java.io.InputStream;
 
 import java.net.URL;
 
+import java.util.Collections;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Locale;
@@ -111,6 +117,8 @@ public class BuildingsSiteInitializer implements SiteInitializer {
 			_addLayoutPageTemplateEntry();
 
 			_addDDMStructures();
+
+			_addWebContent();
 		}
 		catch (Exception e) {
 			_log.error(e, e);
@@ -251,6 +259,40 @@ public class BuildingsSiteInitializer implements SiteInitializer {
 		_addLayout(LayoutConstants.DEFAULT_PARENT_LAYOUT_ID, "News");
 	}
 
+	private void _addWebContent() throws Exception {
+		Enumeration<URL> urls = _bundle.findEntries(
+			_PATH + "/web-content", "*.json", false);
+
+		while (urls.hasMoreElements()) {
+			URL url = urls.nextElement();
+
+			JSONObject jsonObject = JSONFactoryUtil.createJSONObject(
+				StringUtil.read(url.openStream()));
+
+			String name = jsonObject.getString("name");
+
+			String contentPath = jsonObject.getString("contentPath");
+
+			String path = url.getPath();
+
+			int index = path.lastIndexOf(CharPool.SLASH);
+
+			String filePath = path.substring(0, index);
+
+			String content = StringUtil.replace(
+				StringUtil.read(getClass(), filePath + "/" + contentPath),
+				StringPool.DOLLAR, StringPool.DOLLAR, _fileEntriesObjectMap);
+
+			String ddmStructureKey = jsonObject.getString("ddmStructureKey");
+
+			_journalArticleLocalService.addArticle(
+				_serviceContext.getUserId(), _serviceContext.getScopeGroupId(),
+				JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+				Collections.singletonMap(LocaleUtil.US, name), null, content,
+				ddmStructureKey, null, _serviceContext);
+		}
+	}
+
 	private void _createServiceContext(long groupId) throws PortalException {
 		ServiceContext serviceContext = new ServiceContext();
 
@@ -294,6 +336,9 @@ public class BuildingsSiteInitializer implements SiteInitializer {
 
 	@Reference
 	private FragmentsImporter _fragmentsImporter;
+
+	@Reference
+	private JournalArticleLocalService _journalArticleLocalService;
 
 	@Reference
 	private LayoutLocalService _layoutLocalService;

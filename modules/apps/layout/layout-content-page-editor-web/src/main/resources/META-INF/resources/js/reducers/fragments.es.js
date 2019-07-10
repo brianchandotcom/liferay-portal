2304b5ec1f3d91d1dd1ead21ee3badbbaf85a519
Victor Galan <victor.galan@liferay.com>
2019-07-10 12:29:53 +0200

    LPS-97527 JSdocs

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/reducers/fragments.es.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/reducers/fragments.es.js
@@ -549,6 +549,7 @@ function updateFragmentEntryLinkConfigReducer(state, action) {
 /**
  * @param {object} state
  * @param {object} action
+ * @param {string} action.fragmentEntryLinkContent
  * @param {string} action.fragmentEntryLinkId
  * @return {object}
  * @review

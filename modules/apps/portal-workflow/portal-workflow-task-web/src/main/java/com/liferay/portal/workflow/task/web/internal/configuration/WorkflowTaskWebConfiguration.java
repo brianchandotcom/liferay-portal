b980c8faa32fccafe81499de5689a084e871f82c
Inácio Nery <inacio.nery@liferay.com>
2019-07-10 10:01:34 -0300

    LPS-97440 Fix option labels

--- modules/apps/portal-workflow/portal-workflow-task-web/src/main/java/com/liferay/portal/workflow/task/web/internal/configuration/WorkflowTaskWebConfiguration.java
+++ modules/apps/portal-workflow/portal-workflow-task-web/src/main/java/com/liferay/portal/workflow/task/web/internal/configuration/WorkflowTaskWebConfiguration.java
@@ -31,7 +31,7 @@ public interface WorkflowTaskWebConfiguration {
 
 	@Meta.AD(
 		deflt = "list", name = "default-display-view",
-		optionLabels = {"Descriptive", "List"},
+		optionLabels = {"%list", "%table"},
 		optionValues = {"descriptive", "list"}, required = false
 	)
 	public String defaultDisplayView();

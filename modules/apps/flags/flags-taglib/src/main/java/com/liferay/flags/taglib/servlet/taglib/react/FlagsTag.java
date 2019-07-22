c985d406d5d5efc98d748cc1876a9de755934546
Jorge González <jorge.gonzalez@liferay.com>
2019-07-22 18:25:14 +0200

    LPS-98033 SF

--- modules/apps/flags/flags-taglib/src/main/java/com/liferay/flags/taglib/servlet/taglib/react/FlagsTag.java
+++ modules/apps/flags/flags-taglib/src/main/java/com/liferay/flags/taglib/servlet/taglib/react/FlagsTag.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package com.liferay.flags.taglib.servlet.taglib.react;
 
 import com.liferay.flags.configuration.FlagsGroupServiceConfiguration;
@@ -28,6 +42,10 @@ import javax.portlet.PortletURL;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.PageContext;
 
+/**
+ * @author Ambrín Chaudhary
+ * @author Jorge González
+ */
 public class FlagsTag extends IncludeTag {
 
 	public String getClassName() {

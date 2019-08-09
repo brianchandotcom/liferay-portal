c34116ef2647933065eb47e924a49421799f7505
Jürgen Kappler <jurgen.kappler@liferay.com>
2019-08-09 09:43:42 +0200

    LPS-99503 Use transaction since we are going to add logic to add copy also resources to the other collection

--- modules/apps/fragment/fragment-web/src/main/java/com/liferay/fragment/web/internal/portlet/action/MoveFragmentEntryMVCActionCommand.java
+++ modules/apps/fragment/fragment-web/src/main/java/com/liferay/fragment/web/internal/portlet/action/MoveFragmentEntryMVCActionCommand.java
@@ -16,12 +16,19 @@ package com.liferay.fragment.web.internal.portlet.action;
 
 import com.liferay.fragment.constants.FragmentPortletKeys;
 import com.liferay.fragment.service.FragmentEntryService;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.transaction.Propagation;
+import com.liferay.portal.kernel.transaction.TransactionConfig;
+import com.liferay.portal.kernel.transaction.TransactionInvokerUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.StringUtil;
+
+import java.util.concurrent.Callable;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
@@ -48,32 +55,66 @@ public class MoveFragmentEntryMVCActionCommand extends BaseMVCActionCommand {
 			ActionRequest actionRequest, ActionResponse actionResponse)
 		throws Exception {
 
-		long[] fragmentEntryIds = StringUtil.split(
-			ParamUtil.getString(actionRequest, "fragmentEntryIds"), 0L);
-
 		long fragmentCollectionId = ParamUtil.getLong(
 			actionRequest, "fragmentCollectionId");
 
-		for (long fragmentEntryId : fragmentEntryIds) {
-			_fragmentEntryService.moveFragmentEntry(
-				fragmentEntryId, fragmentCollectionId);
+		Callable<Void> callable = new MoveFragmentEntryCallable(actionRequest);
+
+		try {
+			TransactionInvokerUtil.invoke(_transactionConfig, callable);
+
+			LiferayPortletResponse liferayPortletResponse =
+				_portal.getLiferayPortletResponse(actionResponse);
+
+			PortletURL redirectURL = liferayPortletResponse.createRenderURL();
+
+			redirectURL.setParameter(
+				"fragmentCollectionId", String.valueOf(fragmentCollectionId));
+
+			sendRedirect(actionRequest, actionResponse, redirectURL.toString());
 		}
-
-		LiferayPortletResponse liferayPortletResponse =
-			_portal.getLiferayPortletResponse(actionResponse);
-
-		PortletURL redirectURL = liferayPortletResponse.createRenderURL();
-
-		redirectURL.setParameter(
-			"fragmentCollectionId", String.valueOf(fragmentCollectionId));
-
-		sendRedirect(actionRequest, actionResponse, redirectURL.toString());
+		catch (Throwable t) {
+			_log.error(t, t);
+		}
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		MoveFragmentEntryMVCActionCommand.class);
+
+	private static final TransactionConfig _transactionConfig =
+		TransactionConfig.Factory.create(
+			Propagation.REQUIRED, new Class<?>[] {Exception.class});
 
 	@Reference
 	private FragmentEntryService _fragmentEntryService;
 
 	@Reference
 	private Portal _portal;
+
+	private class MoveFragmentEntryCallable implements Callable<Void> {
+
+		@Override
+		public Void call() throws Exception {
+			long[] fragmentEntryIds = StringUtil.split(
+				ParamUtil.getString(_actionRequest, "fragmentEntryIds"), 0L);
+
+			long fragmentCollectionId = ParamUtil.getLong(
+				_actionRequest, "fragmentCollectionId");
+
+			for (long fragmentEntryId : fragmentEntryIds) {
+				_fragmentEntryService.moveFragmentEntry(
+					fragmentEntryId, fragmentCollectionId);
+			}
+
+			return null;
+		}
+
+		private MoveFragmentEntryCallable(ActionRequest actionRequest) {
+			_actionRequest = actionRequest;
+		}
+
+		private final ActionRequest _actionRequest;
+
+	}
 
 }

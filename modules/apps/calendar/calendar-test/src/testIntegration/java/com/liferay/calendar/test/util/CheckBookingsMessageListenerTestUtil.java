eefbd3575caf97d0ada23fff0c20f0b2bdb28b17
Austin Chiang <austin.chiang@liferay.com>
2019-08-19 09:57:49 +0800

    Revert "LPS-99657 Move all *TestUtil classes to calendar-test-util to avoid conflicting packages"
    This reverts commit 2d22888a997cb53aeba48697bb994e3d73bca36b.

--- modules/apps/calendar/calendar-test/src/testIntegration/java/com/liferay/calendar/test/util/CheckBookingsMessageListenerTestUtil.java
+++ modules/apps/calendar/calendar-test/src/testIntegration/java/com/liferay/calendar/test/util/CheckBookingsMessageListenerTestUtil.java
@@ -16,6 +16,7 @@ package com.liferay.calendar.test.util;
 
 import com.liferay.calendar.service.CalendarBookingLocalService;
 import com.liferay.calendar.service.CalendarBookingLocalServiceUtil;
+import com.liferay.calendar.service.test.CalendarBookingLocalServiceTest;
 import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.kernel.util.ProxyUtil;
 
@@ -36,7 +37,7 @@ public class CheckBookingsMessageListenerTestUtil {
 
 	public static void setUp() {
 		Bundle bundle = FrameworkUtil.getBundle(
-			CheckBookingsMessageListenerTestUtil.class);
+			CalendarBookingLocalServiceTest.class);
 
 		BundleContext bundleContext = bundle.getBundleContext();
 

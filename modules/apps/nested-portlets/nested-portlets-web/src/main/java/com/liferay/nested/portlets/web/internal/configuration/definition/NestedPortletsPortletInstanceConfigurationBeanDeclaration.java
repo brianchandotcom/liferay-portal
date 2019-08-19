e5471cf6be3f498cf52b7dc894df40b88030943a
Austin Chiang <austin.chiang@liferay.com>
2019-08-19 09:59:31 +0800

    Revert "LPS-99657 Class that implements ConfigurationBeanDeclaration should be in package *.settings.definition"
    This reverts commit d8767e0d00b460c67ddd406e071e034b0769470a.

--- modules/apps/nested-portlets/nested-portlets-web/src/main/java/com/liferay/nested/portlets/web/internal/configuration/definition/NestedPortletsPortletInstanceConfigurationBeanDeclaration.java
+++ modules/apps/nested-portlets/nested-portlets-web/src/main/java/com/liferay/nested/portlets/web/internal/configuration/definition/NestedPortletsPortletInstanceConfigurationBeanDeclaration.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.nested.portlets.web.internal.settings.definition;
+package com.liferay.nested.portlets.web.internal.configuration.definition;
 
 import com.liferay.nested.portlets.web.internal.configuration.NestedPortletsPortletInstanceConfiguration;
 import com.liferay.portal.kernel.settings.definition.ConfigurationBeanDeclaration;

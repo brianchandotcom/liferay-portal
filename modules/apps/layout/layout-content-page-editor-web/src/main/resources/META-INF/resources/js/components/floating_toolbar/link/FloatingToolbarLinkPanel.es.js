14061ff4b9515bc841470b99104beb3d9986d7c6
Victor Galan <victor.galan@liferay.com>
2019-08-20 13:07:23 +0200

    LPS-100000 Only loads fields if structure mapping is set

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/floating_toolbar/link/FloatingToolbarLinkPanel.es.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/floating_toolbar/link/FloatingToolbarLinkPanel.es.js
@@ -369,7 +369,10 @@ class FloatingToolbarLinkPanel extends PortletBase {
 
 		this._clearFields();
 
-		if (this._selectedSourceTypeId === SOURCE_TYPE_IDS.structure) {
+		if (
+			this._selectedSourceTypeId === SOURCE_TYPE_IDS.structure &&
+			this.selectedMappingTypes.type
+		) {
 			const data = {
 				classNameId: this.selectedMappingTypes.type.id
 			};

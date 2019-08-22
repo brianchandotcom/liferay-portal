53fa62688ca4384681980f02f45e254fe41237d2
Pablo Molina <contact@pablomolina.me>
2019-08-22 11:06:23 +0200

    LPS-98955 Move api call to fetchUtils

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/utils/FragmentsEditorFetchUtils.es.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/utils/FragmentsEditorFetchUtils.es.js
@@ -137,6 +137,15 @@ function getAssetMappingFields(classNameId, classPK) {
 	}).then(response => response.json());
 }
 
+/**
+ * @param {string} ddmStructureId
+ */
+function getContentStructureMappingFields(ddmStructureId) {
+	return _fetch(_store.getState().getContentStructureMappingFieldsURL, {
+		ddmStructureId
+	}).then(response => response.json());
+}
+
 function getExperienceUsedPortletIds(segmentsExperienceId) {
 	return _fetch(_store.getState().getExperienceUsedPortletsURL, {
 		segmentsExperienceId
@@ -276,6 +285,7 @@ export {
 	editFragmentEntryLinkComment,
 	getAssetFieldValue,
 	getAssetMappingFields,
+	getContentStructureMappingFields,
 	getExperienceUsedPortletIds,
 	getMappedContents,
 	getStructureMappingFields,

6f7d33ce140978dbdbd068ba24bb324a1a162973
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2019-08-27 10:56:41 +0200

    LPS-100521 Don't filter by journal

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
@@ -45,7 +45,6 @@ import com.liferay.item.selector.criteria.DownloadURLItemSelectorReturnType;
 import com.liferay.item.selector.criteria.URLItemSelectorReturnType;
 import com.liferay.item.selector.criteria.image.criterion.ImageItemSelectorCriterion;
 import com.liferay.item.selector.criteria.url.criterion.URLItemSelectorCriterion;
-import com.liferay.journal.model.JournalArticle;
 import com.liferay.layout.admin.constants.LayoutAdminPortletKeys;
 import com.liferay.layout.content.page.editor.constants.ContentPageEditorPortletKeys;
 import com.liferay.layout.content.page.editor.constants.ContentPageEditorWebKeys;
@@ -325,9 +324,7 @@ public class ContentPageEditorDisplayContext {
 		).put(
 			"mappedContents",
 			MappedContentUtil.getMappedContentsJSONArray(
-				assetEntries, themeDisplay.getURLCurrent(),
-				new long[] {PortalUtil.getClassNameId(JournalArticle.class)},
-				request)
+				assetEntries, themeDisplay.getURLCurrent(), request)
 		).put(
 			"portletNamespace", _renderResponse.getNamespace()
 		);

1e1b02b7e0543d4d7464cbf6076d6445f36ef55c
pei-jung <pei-jung.lan@liferay.com>
2019-08-27 17:18:12 -0700

    LPS-99434 Update service to include webiste

--- modules/apps/account/account-service/src/main/java/com/liferay/account/service/impl/AccountEntryServiceImpl.java
+++ modules/apps/account/account-service/src/main/java/com/liferay/account/service/impl/AccountEntryServiceImpl.java
@@ -44,14 +44,15 @@ public class AccountEntryServiceImpl extends AccountEntryServiceBaseImpl {
 	@Override
 	public AccountEntry addAccountEntry(
 			long userId, long parentAccountEntryId, String name,
-			String description, long logoId, int status)
+			String description, long logoId, int status, String website)
 		throws PortalException {
 
 		PortalPermissionUtil.check(
 			getPermissionChecker(), AccountActionKeys.ADD_ACCOUNT_ENTRY);
 
 		return accountEntryLocalService.addAccountEntry(
-			userId, parentAccountEntryId, name, description, logoId, status);
+			userId, parentAccountEntryId, name, description, logoId, status,
+			website);
 	}
 
 	@Override

89b1364869a0c39cfabdc5db63a268870a3f58ea
Alejandro Tardín <alejandro.tardin@liferay.com>
2019-10-03 16:09:19 +0200

    LPS-102414 Auto SF

--- modules/apps/content-repository/content-repository-api/src/main/java/com/liferay/content/repository/exception/NoSuchEntryException.java
+++ modules/apps/content-repository/content-repository-api/src/main/java/com/liferay/content/repository/exception/NoSuchEntryException.java
@@ -11,6 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
+
 package com.liferay.content.repository.exception;
 
 import com.liferay.portal.kernel.exception.NoSuchModelException;

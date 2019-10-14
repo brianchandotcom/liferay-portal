e7ba59f2045d7815b34268a311b1301615ecbf64
Alicia Garcia <alicia.garcia@liferay.com>
2019-10-14 14:58:29 +0200

    LPS-100969 SF

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/GroupPagesPortlet.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/GroupPagesPortlet.java
@@ -182,11 +182,13 @@ public class GroupPagesPortlet extends MVCPortlet {
 				LayoutAdminWebKeys.LAYOUT_TEMPLATE_CONVERTER_REGISTRY,
 				_layoutConverterRegistry);
 
-			LayoutsAdminDisplayContext layoutsAdminDisplayContext = new LayoutsAdminDisplayContext(
-				_portal.getLiferayPortletRequest(renderRequest),
-				_portal.getLiferayPortletResponse(renderResponse));
+			LayoutsAdminDisplayContext layoutsAdminDisplayContext =
+				new LayoutsAdminDisplayContext(
+					_portal.getLiferayPortletRequest(renderRequest),
+					_portal.getLiferayPortletResponse(renderResponse));
 
-			layoutsAdminDisplayContext.setLayoutSEOLinkManager(_layoutSEOLinkManager);
+			layoutsAdminDisplayContext.setLayoutSEOLinkManager(
+				_layoutSEOLinkManager);
 
 			renderRequest.setAttribute(
 				LayoutAdminWebKeys.LAYOUT_PAGE_LAYOUT_ADMIN_DISPLAY_CONTEXT,
@@ -262,9 +264,9 @@ public class GroupPagesPortlet extends MVCPortlet {
 	private LayoutPrototypeLocalService _layoutPrototypeLocalService;
 
 	@Reference
-	private Portal _portal;
+	private LayoutSEOLinkManager _layoutSEOLinkManager;
 
 	@Reference
-	private LayoutSEOLinkManager _layoutSEOLinkManager;
+	private Portal _portal;
 
 }

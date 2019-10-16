567c3f922c70b1c553c40412babb79aeb3b5dd66
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2019-10-16 09:42:14 +0200

    LPS-96516 Change ldap-api to use SafeLdap

--- modules/apps/portal-security/portal-security-ldap-api/src/main/java/com/liferay/portal/security/ldap/exportimport/LDAPUserImporter.java
+++ modules/apps/portal-security/portal-security-ldap-api/src/main/java/com/liferay/portal/security/ldap/exportimport/LDAPUserImporter.java
@@ -16,6 +16,8 @@ package com.liferay.portal.security.ldap.exportimport;
 
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.security.exportimport.UserImporter;
+import com.liferay.portal.security.ldap.SafeLdapContext;
+import org.osgi.annotation.versioning.ProviderType;
 
 import javax.naming.directory.Attributes;
 import javax.naming.ldap.LdapContext;
@@ -23,10 +25,20 @@ import javax.naming.ldap.LdapContext;
 /**
  * @author Michael C. Han
  */
+@ProviderType
 public interface LDAPUserImporter extends UserImporter {
 
+	/**
+	 * @deprecated As of Mueller (7.2.x), with no direct replacement
+	 */
+	@Deprecated
 	public User importUser(
 			long ldapServerId, long companyId, LdapContext ldapContext,
+			Attributes attributes, String password)
+		throws Exception;
+
+	public User importUser(
+			long ldapServerId, long companyId, SafeLdapContext safeLdapContext,
 			Attributes attributes, String password)
 		throws Exception;
 

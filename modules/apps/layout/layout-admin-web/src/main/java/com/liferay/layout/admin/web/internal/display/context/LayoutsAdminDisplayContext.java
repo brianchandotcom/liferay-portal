80923a7c0f375d0e771421ebe72aa1f5f8a896d2
Adolfo Pérez <adolfo.perez@liferay.com>
2019-10-17 11:13:35 +0200

    LPS-100969 SF

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/display/context/LayoutsAdminDisplayContext.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/display/context/LayoutsAdminDisplayContext.java
@@ -811,7 +811,7 @@ public class LayoutsAdminDisplayContext {
 		return _layoutsSearchContainer;
 	}
 
-	public String getLayoutTitle() throws PortalException {
+	public String getPageTitle() throws PortalException {
 		String portletId = (String)_httpServletRequest.getAttribute(
 			WebKeys.PORTLET_ID);
 
@@ -923,7 +923,7 @@ public class LayoutsAdminDisplayContext {
 		return orphanPortletsURL.toString();
 	}
 
-	public String getPageTitle() throws PortalException {
+	public String getFullPageTitle() throws PortalException {
 		String portletId = (String)_httpServletRequest.getAttribute(
 			WebKeys.PORTLET_ID);
 
@@ -1233,10 +1233,10 @@ public class LayoutsAdminDisplayContext {
 		return _selPlid;
 	}
 
-	public String getSiteAndCompanyName() throws PortalException {
+	public String getPageTitleSuffix() throws PortalException {
 		Company company = _themeDisplay.getCompany();
 
-		return _layoutSEOLinkManager.getSiteAndCompanyName(
+		return _layoutSEOLinkManager.getPageTitleSuffix(
 			_selLayout, company.getName());
 	}
 

0e238651526c8aac2bb9c488553878a927021b29
Alejandro Tardín <alejandro.tardin@liferay.com>
2019-10-24 14:04:39 +0200

    LPS-101158 Simplifying redirection logic

--- modules/apps/depot/depot-web/src/main/java/com/liferay/depot/web/internal/portlet/action/EditDepotEntryMVCActionCommand.java
+++ modules/apps/depot/depot-web/src/main/java/com/liferay/depot/web/internal/portlet/action/EditDepotEntryMVCActionCommand.java
@@ -76,7 +76,11 @@ public class EditDepotEntryMVCActionCommand extends BaseMVCActionCommand {
 		}
 		catch (PortalException pe) {
 			_log.error(pe, pe);
+
 			SessionErrors.add(actionRequest, pe.getClass(), pe);
+
+			actionResponse.sendRedirect(
+				ParamUtil.getString(actionRequest, "redirect"));
 		}
 	}
 

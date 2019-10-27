3a27b00ce0fcaf1567dcc0accfcf00fbbacbfd0f
Alicia Garcia <alicia.garcia@liferay.com>
2019-10-27 18:14:18 +0100

    LPS-101158 SF

--- modules/apps/depot/depot-test/src/testIntegration/java/com/liferay/depot/service/test/DepotEntryLocalServiceTest.java
+++ modules/apps/depot/depot-test/src/testIntegration/java/com/liferay/depot/service/test/DepotEntryLocalServiceTest.java
@@ -241,8 +241,8 @@ public class DepotEntryLocalServiceTest {
 
 		Group group = _groupLocalService.getGroup(depotEntry.getGroupId());
 
-		Assert.assertEquals("Repository without name.", group.getName(LocaleUtil.getDefault()));
-
+		Assert.assertEquals(
+			"Repository without name.", group.getName(LocaleUtil.getDefault()));
 	}
 
 	@Test(expected = LocaleException.class)

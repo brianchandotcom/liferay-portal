e0cc7b2fae3af97fd7e26ed7eb25282047f49b86
Hugo Huijser <hugo.huijser@liferay.com>
2019-11-05 09:27:16 -0800

    LPS-103252 Sort put calls in *MapBuilder

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/exportimport/data/handler/JournalArticleStagedModelDataHandler.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/exportimport/data/handler/JournalArticleStagedModelDataHandler.java
@@ -221,9 +221,9 @@ public class JournalArticleStagedModelDataHandler
 		}
 
 		Map<String, String> referenceAttributes = HashMapBuilder.put(
-			"article-resource-uuid", articleResourceUuid
-		).put(
 			"article-id", article.getArticleId()
+		).put(
+			"article-resource-uuid", articleResourceUuid
 		).build();
 
 		long defaultUserId = 0;

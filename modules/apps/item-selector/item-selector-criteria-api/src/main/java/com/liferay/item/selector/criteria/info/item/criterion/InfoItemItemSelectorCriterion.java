3b842e5c77bea7502e13164497cff1c07f8c7684
Jürgen Kappler <jurgen.kappler@liferay.com>
2019-11-11 17:04:37 +0100

    LPS-104105 Adds mime types to the criterion

--- modules/apps/item-selector/item-selector-criteria-api/src/main/java/com/liferay/item/selector/criteria/info/item/criterion/InfoItemItemSelectorCriterion.java
+++ modules/apps/item-selector/item-selector-criteria-api/src/main/java/com/liferay/item/selector/criteria/info/item/criterion/InfoItemItemSelectorCriterion.java
@@ -29,6 +29,10 @@ public class InfoItemItemSelectorCriterion extends BaseItemSelectorCriterion {
 		return _itemType;
 	}
 
+	public String[] getMimeTypes() {
+		return _mimeTypes;
+	}
+
 	public void setItemSubtype(String itemSubtype) {
 		_itemSubtype = itemSubtype;
 	}
@@ -37,7 +41,12 @@ public class InfoItemItemSelectorCriterion extends BaseItemSelectorCriterion {
 		_itemType = itemType;
 	}
 
+	public void setMimeTypes(String[] mimeTypes) {
+		_mimeTypes = mimeTypes;
+	}
+
 	private String _itemSubtype;
 	private String _itemType;
+	private String[] _mimeTypes;
 
 }

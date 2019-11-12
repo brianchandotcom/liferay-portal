2afe271ccb68023d20a2325fb1102fa89d80f303
Pavel Savinov <pavel.savinov@liferay.com>
2019-11-12 14:33:03 -0800

    LPS-104359 Prevent submit as it was implemented in metal component

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/common/SearchForm.es.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/common/SearchForm.es.js
@@ -16,7 +16,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 const SearchForm = ({onChange, value}) => (
-	<form className="mb-3" role="search">
+	<form className="mb-3" role="search" onSubmit={event => event.preventDefault()}>
 		<div className="input-group">
 			<div className="input-group-item">
 				<label className="sr-only" htmlFor="searchInput">

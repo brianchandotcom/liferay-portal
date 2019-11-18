7cee190fc50391c79e1e6036c5ed9fe175f3060a
drewbrokke <drew.brokke@liferay.com>
2019-11-18 12:21:27 -0600

    LPS-99914 account-service - autogenerated

--- modules/apps/account/account-service/src/main/java/com/liferay/account/service/persistence/impl/AccountRolePersistenceImpl.java
+++ modules/apps/account/account-service/src/main/java/com/liferay/account/service/persistence/impl/AccountRolePersistenceImpl.java
@@ -1670,6 +1670,7 @@ public class AccountRolePersistenceImpl
 		}
 	}
 
+	@Override
 	public void clearCache(Set<Serializable> primaryKeys) {
 		finderCache.clearCache(FINDER_CLASS_NAME_ENTITY);
 		finderCache.clearCache(FINDER_CLASS_NAME_LIST_WITH_PAGINATION);

84100344b0d0a31e1d3a9a08396e7e4a89172997
Victor Galan <victor.galan@liferay.com>
2019-11-25 17:48:40 +0100

    LPS-104925 Add missing case when there is no segmentsExperience (master pages, display pages, page templates)

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/utils/computeValues.es.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/utils/computeValues.es.js
@@ -88,6 +88,12 @@ export function getComputedEditableValue(editableValue, configuration = {}) {
 		}
 	}
 
+	if (isNullOrUndefined(result.value)) {
+		result.value = isNullOrUndefined(selectedLanguageId)
+			? editableValue[defaultLanguageId]
+			: editableValue[selectedLanguageId];
+	}
+
 	return result;
 }
 

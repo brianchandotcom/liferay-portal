9e29b968a20cbc421644fc7195b7ab90b52c9785
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2019-11-28 12:20:42 +0000

    LPS-101248 Use ServiceTrackerMap

--- modules/apps/info/info-impl/src/main/java/com/liferay/info/internal/item/renderer/InfoItemRendererTrackerImpl.java
+++ modules/apps/info/info-impl/src/main/java/com/liferay/info/internal/item/renderer/InfoItemRendererTrackerImpl.java
@@ -17,22 +17,19 @@ package com.liferay.info.internal.item.renderer;
 import com.liferay.info.internal.util.GenericsUtil;
 import com.liferay.info.item.renderer.InfoItemRenderer;
 import com.liferay.info.item.renderer.InfoItemRendererTracker;
-import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.osgi.service.tracker.collections.map.PropertyServiceReferenceComparator;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
 import com.liferay.portal.kernel.util.Validator;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
-import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferenceCardinality;
-import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.Deactivate;
 
 /**
  * @author Jorge Ferrer
@@ -46,102 +43,63 @@ public class InfoItemRendererTrackerImpl implements InfoItemRendererTracker {
 			return null;
 		}
 
-		return _infoItemRenderers.get(key);
+		return _infoItemRenderersServiceTrackerMap.getService(key);
 	}
 
 	@Override
 	public List<InfoItemRenderer> getInfoItemRenderers() {
-		return new ArrayList<>(_infoItemRenderers.values());
+		return new ArrayList<>(_infoItemRenderersServiceTrackerMap.values());
 	}
 
 	@Override
 	public List<InfoItemRenderer> getInfoItemRenderers(String itemClassName) {
-		List<ServiceReference<InfoItemRenderer>> serviceReferences =
-			_itemClassNameInfoItemRendererServiceReferences.get(itemClassName);
+		List<InfoItemRenderer> infoItemRenderers =
+			_itemClassNameInfoItemRendererServiceTrackerMap.getService(
+				itemClassName);
 
-		if (serviceReferences != null) {
-			List<InfoItemRenderer> infoItemRenderers = new ArrayList<>();
-
-			for (ServiceReference<InfoItemRenderer> serviceReference :
-					serviceReferences) {
-
-				Bundle bundle = serviceReference.getBundle();
-
-				BundleContext bundleContext = bundle.getBundleContext();
-
-				InfoItemRenderer infoItemRenderer = bundleContext.getService(
-					serviceReference);
-
-				infoItemRenderers.add(infoItemRenderer);
-			}
-
-			return infoItemRenderers;
+		if (infoItemRenderers != null) {
+			return new ArrayList<>(infoItemRenderers);
 		}
 
 		return Collections.emptyList();
 	}
 
-	@Reference(
-		cardinality = ReferenceCardinality.MULTIPLE,
-		policy = ReferencePolicy.DYNAMIC, service = InfoItemRenderer.class
-	)
-	protected void setInfoItemRenderer(
-		ServiceReference<InfoItemRenderer> serviceReference) {
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_itemClassNameInfoItemRendererServiceTrackerMap =
+			ServiceTrackerMapFactory.openMultiValueMap(
+				bundleContext, InfoItemRenderer.class, null,
+				(serviceReference, emitter) -> {
+					InfoItemRenderer infoItemRenderer =
+						bundleContext.getService(serviceReference);
 
-		Bundle bundle = serviceReference.getBundle();
+					String className = GenericsUtil.getItemClassName(
+						infoItemRenderer);
 
-		BundleContext bundleContext = bundle.getBundleContext();
+					emitter.emit(className);
+				},
+				Collections.reverseOrder(
+					new PropertyServiceReferenceComparator("service.ranking")));
 
-		InfoItemRenderer infoItemRenderer = bundleContext.getService(
-			serviceReference);
+		_infoItemRenderersServiceTrackerMap =
+			ServiceTrackerMapFactory.openSingleValueMap(
+				bundleContext, InfoItemRenderer.class, null,
+				(serviceReference, emitter) -> {
+					InfoItemRenderer infoItemRenderer =
+						bundleContext.getService(serviceReference);
 
-		_infoItemRenderers.put(infoItemRenderer.getKey(), infoItemRenderer);
-
-		List<ServiceReference<InfoItemRenderer>> itemClassInfoItemRenderers =
-			_itemClassNameInfoItemRendererServiceReferences.computeIfAbsent(
-				GenericsUtil.getItemClassName(infoItemRenderer),
-				itemClass -> new ArrayList<>());
-
-		itemClassInfoItemRenderers.add(serviceReference);
-
-		Collections.sort(
-			itemClassInfoItemRenderers,
-			Collections.reverseOrder(
-				(serviceReference1, serviceReference2) -> {
-					Integer serviceRanking1 = GetterUtil.getInteger(
-						serviceReference1.getProperty("service.ranking"));
-					Integer serviceRanking2 = GetterUtil.getInteger(
-						serviceReference2.getProperty("service.ranking"));
-
-					return serviceRanking1.compareTo(serviceRanking2);
-				}));
+					emitter.emit(infoItemRenderer.getKey());
+				});
 	}
 
-	protected void unsetInfoItemRenderer(
-		ServiceReference<InfoItemRenderer> serviceReference) {
-
-		Bundle bundle = serviceReference.getBundle();
-
-		BundleContext bundleContext = bundle.getBundleContext();
-
-		InfoItemRenderer infoItemRenderer = bundleContext.getService(
-			serviceReference);
-
-		_infoItemRenderers.remove(infoItemRenderer.getKey());
-
-		List<ServiceReference<InfoItemRenderer>> itemClassInfoItemRenderers =
-			_itemClassNameInfoItemRendererServiceReferences.get(
-				GenericsUtil.getItemClassName(infoItemRenderer));
-
-		if (itemClassInfoItemRenderers != null) {
-			itemClassInfoItemRenderers.remove(serviceReference);
-		}
+	@Deactivate
+	protected void deactivate() {
+		_itemClassNameInfoItemRendererServiceTrackerMap.close();
 	}
 
-	private final Map<String, InfoItemRenderer> _infoItemRenderers =
-		new ConcurrentHashMap<>();
-	private final Map<String, List<ServiceReference<InfoItemRenderer>>>
-		_itemClassNameInfoItemRendererServiceReferences =
-			new ConcurrentHashMap<>();
+	private ServiceTrackerMap<String, InfoItemRenderer>
+		_infoItemRenderersServiceTrackerMap;
+	private ServiceTrackerMap<String, List<InfoItemRenderer>>
+		_itemClassNameInfoItemRendererServiceTrackerMap;
 
 }

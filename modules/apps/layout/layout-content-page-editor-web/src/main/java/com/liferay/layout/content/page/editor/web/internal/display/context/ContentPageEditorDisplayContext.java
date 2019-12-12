0f089476890f164284f8eb954c0dab6b66568af8
Sandro Chinea <sandrochinea@gmail.com>
2019-12-12 10:45:47 +0100

    LPS-105138 Take theme colors

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
@@ -207,6 +207,8 @@ public class ContentPageEditorDisplayContext {
 			).put(
 				"sidebarPanels", editorSoyContext.get("sidebarPanels")
 			).put(
+				"themeColorsCssClasses", _getThemeColorsCssClasses()
+			).put(
 				"updateLayoutPageTemplateDataURL",
 				editorSoyContext.get("updateLayoutPageTemplateDataURL")
 			).put(

eef2f4e7a9ab1a0dbc022b3f5871e4d820782375
Victor Galan <victor.galan@liferay.com>
2019-12-13 11:15:20 +0100

    LPS-104981 Add the delete comment url to the config

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
@@ -216,6 +216,10 @@ public class ContentPageEditorDisplayContext {
 				"editSegmentsEntryURL",
 				editorSoyContext.get("editSegmentsEntryURL")
 			).put(
+				"deleteFragmentEntryLinkCommentURL",
+				getFragmentEntryActionURL(
+					"/content_layout/delete_fragment_entry_link_comment")
+			).put(
 				"editFragmentEntryLinkCommentURL",
 				getFragmentEntryActionURL(
 					"/content_layout/edit_fragment_entry_link_comment",

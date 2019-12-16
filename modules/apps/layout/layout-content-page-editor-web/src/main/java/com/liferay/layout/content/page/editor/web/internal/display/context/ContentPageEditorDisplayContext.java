79701fb8fa674e18f0ce03760512e0ba3dfc188d
Rubén Pulido <ruben.pulido@liferay.com>
2019-12-16 15:50:08 +0100

    LPS-103454 backURL is no needed

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
@@ -378,7 +378,7 @@ public class ContentPageEditorDisplayContext {
 		).put(
 			"pageContents",
 			ContentUtil.getPageContentsJSONArray(
-				themeDisplay.getPlid(), themeDisplay.getURLCurrent(), request)
+				themeDisplay.getPlid(), request)
 		).put(
 			"pageType", String.valueOf(_getPageType())
 		).put(

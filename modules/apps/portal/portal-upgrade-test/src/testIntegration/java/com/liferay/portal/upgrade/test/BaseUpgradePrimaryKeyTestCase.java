07c9f5882e0dd6f5270e30586742e7371f83c588
Alberto Chaparro <alberto.chaparro@liferay.com>
2020-01-03 16:39:40 -0800

    LPS-103035 Adding new tests

---
+++ modules/apps/portal/portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/test/BaseUpgradePrimaryKeyTestCase.java
@@ -0,0 +1,93 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.upgrade.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.dao.db.DB;
+import com.liferay.portal.kernel.dao.db.DBInspector;
+import com.liferay.portal.kernel.dao.db.DBManagerUtil;
+import com.liferay.portal.kernel.dao.jdbc.DataAccess;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Alberto Chaparro
+ */
+@RunWith(Arquillian.class)
+public abstract class BaseUpgradePrimaryKeyTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	public List<String> getPrimaryKeyColumnNames() throws SQLException {
+		DatabaseMetaData databaseMetaData = connection.getMetaData();
+
+		List<String> primaryKeyColumnNames = new ArrayList<>();
+
+		try (ResultSet rs = databaseMetaData.getPrimaryKeys(
+				dbInspector.getCatalog(), dbInspector.getSchema(),
+				dbInspector.normalizeName(getTableName(), databaseMetaData))) {
+
+			while (rs.next()) {
+				primaryKeyColumnNames.add(rs.getString("COLUMN_NAME"));
+			}
+		}
+
+		return primaryKeyColumnNames;
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		connection = DataAccess.getConnection();
+
+		dbInspector = new DBInspector(connection);
+
+		db = DBManagerUtil.getDB();
+
+		db.runSQL(getSqlCreateTable());
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		db.runSQL("drop table " + getTableName());
+
+		DataAccess.cleanUp(connection);
+	}
+
+	protected abstract String getSqlCreateTable();
+
+	protected abstract String getTableName();
+
+	protected static Connection connection;
+	protected static DB db;
+	protected static DBInspector dbInspector;
+
+}

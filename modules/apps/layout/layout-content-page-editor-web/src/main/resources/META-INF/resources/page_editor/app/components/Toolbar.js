437aa08715bddf83742fb816318aa0907ac8ecbb
Julien Castelain <julien.castelain@liferay.com>
2020-01-07 15:42:47 +0100

    LPS-105861 Upper toolbar: Implement Publish button

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/Toolbar.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/Toolbar.js
@@ -39,7 +39,7 @@ function ToolbarBody() {
 
 	const {portletNamespace} = useContext(ConfigContext);
 
-	const {draft, singleSegmentsExperienceMode} = store;
+	const {draft, publishButtonEnabled, singleSegmentsExperienceMode} = store;
 
 	const {
 		classPK,
@@ -196,6 +196,7 @@ function ToolbarBody() {
 
 						<ClayButton
 							className="nav-btn"
+							disabled={!publishButtonEnabled}
 							displayType="primary"
 							small
 							type="submit"

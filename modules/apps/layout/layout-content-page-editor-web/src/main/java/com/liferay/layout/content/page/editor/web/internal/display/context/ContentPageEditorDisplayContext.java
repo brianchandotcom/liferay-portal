c43aac7e267682f22c018279b85afa2eab3204fe
Rubén Pulido <ruben.pulido@liferay.com>
2020-01-10 02:47:34 +0100

    LPS-106053 Read layoutConversionWarningMessages from MultiSessionMessages and add it to soy context

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
@@ -480,6 +480,9 @@ public class ContentPageEditorDisplayContext {
 		).put(
 			"languageId", themeDisplay.getLanguageId()
 		).put(
+			"layoutConversionWarningMessages",
+			_getLayoutConversionWarningMessages()
+		).put(
 			"layoutData", JSONFactoryUtil.createJSONObject(_getLayoutData())
 		).put(
 			"lookAndFeelURL", _getLookAndFeelURL()
@@ -1318,6 +1321,11 @@ public class ContentPageEditorDisplayContext {
 			_getImageItemSelectorCriterion(), _getURLItemSelectorCriterion());
 
 		return itemSelectorURL.toString();
+	}
+
+	private String[] _getLayoutConversionWarningMessages() {
+		return (String[])MultiSessionMessages.get(
+			_portletRequest, "layoutConversionWarningMessages");
 	}
 
 	private String _getLayoutData() throws PortalException {

b625f410b80e2b83eed6207af43d1108e8d83ed7
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-01-14 11:00:14 +0100

    LPS-106290 Moves to backend

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
@@ -219,7 +219,10 @@ public class ContentPageEditorDisplayContext {
 		return deleteLayoutURL.toString();
 	}
 
-	public Map<String, Object> getEditorReactContext() throws Exception {
+	public Map<String, Object> getEditorReactContext(
+			String npmResolvedPackageName)
+		throws Exception {
+
 		SoyContext editorSoyContext = getEditorSoyContext();
 
 		return HashMapBuilder.<String, Object>put(
@@ -316,6 +319,8 @@ public class ContentPageEditorDisplayContext {
 				"moveItemURL",
 				getFragmentEntryActionURL(
 					"/content_layout/move_fragment_entry_link_react")
+			).put(
+				"plugin_root", npmResolvedPackageName + "/page_editor/plugins"
 			).put(
 				"portletNamespace", editorSoyContext.get("portletNamespace")
 			).put(

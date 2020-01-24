4d70e9929f8831e9ad7ce43471ddd105905e7cc1
Carlos Sierra Andrés <carlos.sierra@liferay.com>
2020-01-24 15:39:42 +0100

    LPS-101720 Rename to "email" + namespace + action in past tense + "template content"

--- modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-email-otp-web/src/main/java/com/liferay/multi/factor/authentication/email/otp/web/internal/configuration/MFAEmailOTPConfiguration.java
+++ modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-email-otp-web/src/main/java/com/liferay/multi/factor/authentication/email/otp/web/internal/configuration/MFAEmailOTPConfiguration.java
@@ -64,11 +64,10 @@ public interface MFAEmailOTPConfiguration {
 	public int otpSize();
 
 	public final String DEFAULT_EMAIL_OTP_BODY =
-		"/META-INF/resources/dependencies" +
-			"/email_multi_factor_authentication_email_otp_body.tmpl";
+		"/META-INF/resources/dependencies/email_mfa_email_otp_sent_body.tmpl";
 
 	public final String DEFAULT_EMAIL_OTP_SUBJECT =
 		"/META-INF/resources/dependencies" +
-			"/email_multi_factor_authentication_email_otp_subject.tmpl";
+			"/email_mfa_email_otp_sent_subject.tmpl";
 
 }

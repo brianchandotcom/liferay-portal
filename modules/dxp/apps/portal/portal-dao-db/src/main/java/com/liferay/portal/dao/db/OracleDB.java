4d0b495538dddf3fa8a12a860667fdd5b26e91a1
shuyangzhou <shuyang.zhou@liferay.com>
2020-02-06 16:28:06 -0800

    LPS-108398 Remove useless parameter.

--- modules/dxp/apps/portal/portal-dao-db/src/main/java/com/liferay/portal/dao/db/OracleDB.java
+++ modules/dxp/apps/portal/portal-dao-db/src/main/java/com/liferay/portal/dao/db/OracleDB.java
@@ -50,7 +50,7 @@ public class OracleDB extends BaseDB {
 
 	@Override
 	public String buildSQL(String template) throws IOException {
-		template = replaceTemplate(template, getTemplate());
+		template = replaceTemplate(template);
 		template = reword(template);
 		template = StringUtil.replace(
 			template, new String[] {"\\\\", "\\'", "\\\""},
@@ -164,7 +164,7 @@ public class OracleDB extends BaseDB {
 	}
 
 	@Override
-	protected String replaceTemplate(String template, String[] actual) {
+	protected String replaceTemplate(String template) {
 
 		// LPS-12048
 
@@ -186,7 +186,7 @@ public class OracleDB extends BaseDB {
 
 		template = sb.toString();
 
-		return super.replaceTemplate(template, actual);
+		return super.replaceTemplate(template);
 	}
 
 	@Override

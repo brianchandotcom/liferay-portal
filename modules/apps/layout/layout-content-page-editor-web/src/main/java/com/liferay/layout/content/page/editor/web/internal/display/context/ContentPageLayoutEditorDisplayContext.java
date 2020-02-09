6c745e13ea834af6f8f26fc2659dbaf063773957
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-02-09 23:10:24 +0100

    LPS-108355 No needed method

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageLayoutEditorDisplayContext.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageLayoutEditorDisplayContext.java
@@ -105,25 +105,6 @@ public class ContentPageLayoutEditorDisplayContext
 	}
 
 	@Override
-	public SoyContext getFragmentsEditorToolbarSoyContext()
-		throws PortalException {
-
-		if (_fragmentsEditorToolbarSoyContext != null) {
-			return _fragmentsEditorToolbarSoyContext;
-		}
-
-		SoyContext soyContext = super.getFragmentsEditorToolbarSoyContext();
-
-		if (_isShowSegmentsExperiences()) {
-			_populateSegmentsExperiencesSoyContext(soyContext);
-		}
-
-		_fragmentsEditorToolbarSoyContext = soyContext;
-
-		return _fragmentsEditorToolbarSoyContext;
-	}
-
-	@Override
 	public boolean isSingleSegmentsExperienceMode() {
 		long segmentsExperienceId = ParamUtil.getLong(
 			PortalUtil.getOriginalServletRequest(httpServletRequest),
@@ -563,7 +544,6 @@ public class ContentPageLayoutEditorDisplayContext
 
 	private SoyContext _editorSoyContext;
 	private String _editSegmentsEntryURL;
-	private SoyContext _fragmentsEditorToolbarSoyContext;
 	private Boolean _lockedSegmentsExperience;
 	private Long _segmentsEntryId;
 	private Long _segmentsExperienceId;

brianchandotcom/liferay-portal

commit d7c356455174eb03e1b12492dcd1223b1c9b2164

LPS-107877 Register customizers by criterion

modules/apps/depot/depot-web/src/main/java/com/liferay/depot/web/internal/item/selector/provider/DepotItemSelectorViewRendererCustomizer.java
@@ -39,6 +39,7 @@ import com.liferay.taglib.util.PortalIncludeUtil;
 
 import java.io.IOException;
 
+import java.util.Collection;
 import java.util.Map;
 
 import javax.portlet.PortletURL;
@@ -148,6 +149,13 @@ public class DepotItemSelectorViewRendererCustomizer
 		}
 
 		return itemSelectorViewRenderer;
+	}
+
+	@Override
+	public Collection<Class<? extends ItemSelectorCriterion>>
+		getSupportedItemSelectorCriterionClasses() {
+
+		return _itemSelectorCriterionMap.keySet();
 	}
 
 	@Reference(

modules/apps/item-selector/item-selector-api/src/main/java/com/liferay/item/selector/provider/ItemSelectorViewRendererCustomizer.java
@@ -15,10 +15,9 @@
 package com.liferay.item.selector.provider;
 
 import com.liferay.item.selector.ItemSelectorCriterion;
-import com.liferay.item.selector.ItemSelectorView;
 import com.liferay.item.selector.ItemSelectorViewRenderer;
 
-import javax.portlet.PortletURL;
+import java.util.Collection;
 
 /**
  * @author Alejandro Tardín
@@ -27,5 +26,8 @@ public interface ItemSelectorViewRendererCustomizer {
 
 	public ItemSelectorViewRenderer customizeItemSelectorViewRenderer(
 		ItemSelectorViewRenderer itemSelectorViewRenderer);
+
+	public Collection<Class<? extends ItemSelectorCriterion>>
+		getSupportedItemSelectorCriterionClasses();
 
 }

modules/apps/item-selector/item-selector-web/src/main/java/com/liferay/item/selector/web/internal/ItemSelectorImpl.java
@@ -24,6 +24,9 @@ import com.liferay.item.selector.ItemSelectorView;
 import com.liferay.item.selector.ItemSelectorViewRenderer;
 import com.liferay.item.selector.constants.ItemSelectorPortletKeys;
 import com.liferay.item.selector.provider.ItemSelectorViewRendererCustomizer;
+import com.liferay.osgi.service.tracker.collections.map.ServiceReferenceMapperFactory;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.SystemException;
@@ -50,11 +53,13 @@ import javax.portlet.PortletModeException;
 import javax.portlet.PortletURL;
 import javax.portlet.WindowStateException;
 
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ReferenceCardinality;
 import org.osgi.service.component.annotations.ReferencePolicy;
-import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
  * @author Iván Zaera
@@ -182,12 +187,10 @@ public class ItemSelectorImpl implements ItemSelector {
 					itemSelectorCriteriaArray, themeDisplay);
 
 				itemSelectorViewRenderers.add(
-					_itemSelectorViewRendererCustomizer.
-						customizeItemSelectorViewRenderer(
-							new ItemSelectorViewRendererImpl(
-								itemSelectorView, itemSelectorCriterion,
-								portletURL, itemSelectedEventName,
-								isSearch(parameters))));
+					_applyCustomizations(
+						new ItemSelectorViewRendererImpl(
+							itemSelectorView, itemSelectorCriterion, portletURL,
+							itemSelectedEventName, isSearch(parameters))));
 			}
 		}
 
@@ -239,6 +242,28 @@ public class ItemSelectorImpl implements ItemSelector {
 		return getItemSelectorURL(
 			requestBackedPortletURLFactory, null, 0, itemSelectedEventName,
 			itemSelectorCriteria);
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_serviceTrackerMap = ServiceTrackerMapFactory.openMultiValueMap(
+			bundleContext, ItemSelectorViewRendererCustomizer.class, null,
+			ServiceReferenceMapperFactory.create(
+				bundleContext,
+				(itemSelectorViewRendererCustomizer, emitter) -> {
+					for (Class<? extends ItemSelectorCriterion>
+							itemSelectorCriterionClass :
+								itemSelectorViewRendererCustomizer.
+									getSupportedItemSelectorCriterionClasses()) {
+
+						emitter.emit(itemSelectorCriterionClass.getName());
+					}
+				}));
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_serviceTrackerMap.close();
 	}
 
 	protected List<Class<? extends ItemSelectorCriterion>>
@@ -401,6 +426,35 @@ public class ItemSelectorImpl implements ItemSelector {
 			itemSelectorCriterionClass.getName());
 	}
 
+	private ItemSelectorViewRenderer _applyCustomizations(
+		ItemSelectorViewRenderer itemSelectorViewRenderer) {
+
+		ItemSelectorCriterion itemSelectorCriterion =
+			itemSelectorViewRenderer.getItemSelectorCriterion();
+
+		Class<? extends ItemSelectorCriterion> clazz =
+			itemSelectorCriterion.getClass();
+
+		List<ItemSelectorViewRendererCustomizer>
+			itemSelectorViewRendererCustomizers =
+				_serviceTrackerMap.getService(clazz.getName());
+
+		if (itemSelectorViewRendererCustomizers == null) {
+			return itemSelectorViewRenderer;
+		}
+
+		for (ItemSelectorViewRendererCustomizer
+				itemSelectorViewRendererCustomizer :
+					itemSelectorViewRendererCustomizers) {
+
+			itemSelectorViewRenderer =
+				itemSelectorViewRendererCustomizer.
+					customizeItemSelectorViewRenderer(itemSelectorViewRenderer);
+		}
+
+		return itemSelectorViewRenderer;
+	}
+
 	@Reference
 	private Http _http;
 
@@ -409,15 +463,10 @@ public class ItemSelectorImpl implements ItemSelector {
 			_itemSelectionCriterionHandlers = new ConcurrentHashMap<>();
 	private ItemSelectorCriterionSerializer _itemSelectionCriterionSerializer;
 
-	@Reference(
-		cardinality = ReferenceCardinality.MANDATORY,
-		policy = ReferencePolicy.DYNAMIC,
-		policyOption = ReferencePolicyOption.GREEDY
-	)
-	private volatile ItemSelectorViewRendererCustomizer
-		_itemSelectorViewRendererCustomizer;
-
 	@Reference
 	private Portal _portal;
+
+	private ServiceTrackerMap<String, List<ItemSelectorViewRendererCustomizer>>
+		_serviceTrackerMap;
 
 }

modules/apps/item-selector/item-selector-web/src/main/java/com/liferay/item/selector/web/internal/provider/ItemSelectorViewRendererCustomizerImpl.java
@@ -1,41 +1,0 @@
-/**
- * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- */
-
-package com.liferay.item.selector.web.internal.provider;
-
-import com.liferay.item.selector.ItemSelectorCriterion;
-import com.liferay.item.selector.ItemSelectorView;
-import com.liferay.item.selector.ItemSelectorViewRenderer;
-import com.liferay.item.selector.provider.ItemSelectorViewRendererCustomizer;
-import com.liferay.item.selector.web.internal.ItemSelectorViewRendererImpl;
-
-import javax.portlet.PortletURL;
-
-import org.osgi.service.component.annotations.Component;
-
-/**
- * @author Alejandro Tardín
- */
-@Component(service = ItemSelectorViewRendererCustomizer.class)
-public class ItemSelectorViewRendererCustomizerImpl
-	implements ItemSelectorViewRendererCustomizer {
-
-	@Override
-	public ItemSelectorViewRenderer customizeItemSelectorViewRenderer(
-		ItemSelectorViewRenderer itemSelectorViewRenderer) {
-
-		return itemSelectorViewRenderer;
-	}
-
-}

commit 1824c76d2f90bd77319fc4f7ff2c4305675de21a

LPS-107877 Fix NPE, caused by d7c356455174eb03e1b12492dcd1223b1c9b2164 java.lang.NullPointerException 	at com.liferay.item.selector.web.internal.ItemSelectorImpl._applyCustomizations(ItemSelectorImpl.java:439) 	at com.liferay.item.selector.web.internal.ItemSelectorImpl.getItemSelectorRendering(ItemSelectorImpl.java:190) 	at com.liferay.item.selector.web.internal.ItemSelectorImplTest.getItemSelectorRendering(ItemSelectorImplTest.java:255) 	at com.liferay.item.selector.web.internal.ItemSelectorImplTest.testGetItemSelectorRendering(ItemSelectorImplTest.java:178)

modules/apps/item-selector/item-selector-web/src/test/java/com/liferay/item/selector/web/internal/ItemSelectorImplTest.java
@@ -21,6 +21,7 @@ import com.liferay.item.selector.ItemSelectorView;
 import com.liferay.item.selector.ItemSelectorViewRenderer;
 import com.liferay.item.selector.constants.ItemSelectorPortletKeys;
 import com.liferay.item.selector.web.internal.util.ItemSelectorCriterionSerializerImpl;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
 import com.liferay.portal.json.JSONFactoryImpl;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.model.Group;
@@ -30,6 +31,7 @@ import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.HttpUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.ProxyFactory;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.model.impl.GroupImpl;
 import com.liferay.portal.util.HttpImpl;
@@ -75,6 +77,9 @@ public class ItemSelectorImplTest extends PowerMockito {
 			_itemSelectorImpl, "_http", new HttpImpl());
 		ReflectionTestUtil.setFieldValue(
 			_itemSelectorImpl, "_portal", new PortalImpl());
+		ReflectionTestUtil.setFieldValue(
+			_itemSelectorImpl, "_serviceTrackerMap",
+			ProxyFactory.newDummyInstance(ServiceTrackerMap.class));
 
 		_mediaItemSelectorCriterion = new MediaItemSelectorCriterion();
 
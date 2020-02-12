776c1ff3e5d3fd148a292df2a1e2f916f577b349
Jürgen Kappler <jurgen.kappler@liferay.com>
2020-02-12 10:20:43 +0100

    LPS-108454 SF

--- modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/headless/delivery/dto/v1_0/PageDefinitionConverterUtil.java
+++ modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/headless/delivery/dto/v1_0/PageDefinitionConverterUtil.java
@@ -75,9 +75,7 @@ public class PageDefinitionConverterUtil {
 			LayoutPageTemplateStructureLocalServiceUtil.
 				fetchLayoutPageTemplateStructure(
 					layout.getGroupId(),
-					PortalUtil.getClassNameId(
-						com.liferay.portal.kernel.model.Layout.class),
-					layout.getPlid());
+					PortalUtil.getClassNameId(Layout.class), layout.getPlid());
 
 		String layoutData = LayoutDataConverter.convert(
 			layoutPageTemplateStructure.getData(0L));

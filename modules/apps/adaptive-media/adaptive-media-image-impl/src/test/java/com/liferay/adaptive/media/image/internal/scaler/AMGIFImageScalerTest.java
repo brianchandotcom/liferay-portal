brianchandotcom/liferay-portal

commit f0396971b11b2aea957d0e951a86845b8f268ecc

LPS-107620 SF - Make methods private

modules/apps/adaptive-media/adaptive-media-image-impl/src/main/java/com/liferay/adaptive/media/image/internal/scaler/AMGIFImageScaler.java
@@ -74,7 +74,7 @@ public class AMGIFImageScaler implements AMImageScaler {
 				ProcessUtil.execute(
 					CollectorOutputProcessor.INSTANCE, "gifsicle",
 					"--resize-fit",
-					getResizeFitValues(amImageConfigurationEntry), "--output",
+					_getResizeFitValues(amImageConfigurationEntry), "--output",
 					"-", file.getAbsolutePath());
 
 			Map.Entry<byte[], byte[]> objectValuePair = collectorFuture.get();
@@ -83,7 +83,7 @@ public class AMGIFImageScaler implements AMImageScaler {
 
 			byte[] bytes = objectValuePair.getKey();
 
-			Tuple<Integer, Integer> dimension = getDimension(bytes);
+			Tuple<Integer, Integer> dimension = _getDimension(bytes);
 
 			return new AMImageScaledImageImpl(
 				bytes, dimension.second, dimension.first);
@@ -102,7 +102,7 @@ public class AMGIFImageScaler implements AMImageScaler {
 			AMImageConfiguration.class, properties);
 	}
 
-	protected Tuple<Integer, Integer> getDimension(byte[] bytes)
+	private Tuple<Integer, Integer> _getDimension(byte[] bytes)
 		throws ImageResolutionException, IOException {
 
 		try (InputStream inputStream = new UnsyncByteArrayInputStream(bytes)) {
@@ -114,7 +114,7 @@ public class AMGIFImageScaler implements AMImageScaler {
 		}
 	}
 
-	protected String getResizeFitValues(
+	private String _getResizeFitValues(
 		AMImageConfigurationEntry amImageConfigurationEntry) {
 
 		Map<String, String> properties =

commit e7930f0ccd57966a10020e7f65c177483357d3a4

LPS-107620 Fix compile, caused by f0396971b11b2aea957d0e951a86845b8f268ecc

modules/apps/adaptive-media/adaptive-media-image-impl/src/test/java/com/liferay/adaptive/media/image/internal/scaler/AMGIFImageScalerTest.java
@@ -16,6 +16,7 @@ package com.liferay.adaptive.media.image.internal.scaler;
 
 import com.liferay.adaptive.media.image.configuration.AMImageConfigurationEntry;
 import com.liferay.adaptive.media.image.internal.configuration.AMImageConfigurationEntryImpl;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 
 import java.util.Map;
@@ -44,7 +45,7 @@ public class AMGIFImageScalerTest {
 
 		Assert.assertEquals(
 			"100x_",
-			amGIFImageScaler.getResizeFitValues(amImageConfigurationEntry));
+			_getResizeFitValues(amGIFImageScaler, amImageConfigurationEntry));
 	}
 
 	@Test
@@ -63,7 +64,7 @@ public class AMGIFImageScalerTest {
 
 		Assert.assertEquals(
 			"_x100",
-			amGIFImageScaler.getResizeFitValues(amImageConfigurationEntry));
+			_getResizeFitValues(amGIFImageScaler, amImageConfigurationEntry));
 	}
 
 	@Test
@@ -82,7 +83,7 @@ public class AMGIFImageScalerTest {
 
 		Assert.assertEquals(
 			"200x100",
-			amGIFImageScaler.getResizeFitValues(amImageConfigurationEntry));
+			_getResizeFitValues(amGIFImageScaler, amImageConfigurationEntry));
 	}
 
 	@Test
@@ -99,7 +100,7 @@ public class AMGIFImageScalerTest {
 
 		Assert.assertEquals(
 			"_x100",
-			amGIFImageScaler.getResizeFitValues(amImageConfigurationEntry));
+			_getResizeFitValues(amGIFImageScaler, amImageConfigurationEntry));
 	}
 
 	@Test
@@ -118,7 +119,7 @@ public class AMGIFImageScalerTest {
 
 		Assert.assertEquals(
 			"100x_",
-			amGIFImageScaler.getResizeFitValues(amImageConfigurationEntry));
+			_getResizeFitValues(amGIFImageScaler, amImageConfigurationEntry));
 	}
 
 	@Test
@@ -137,7 +138,17 @@ public class AMGIFImageScalerTest {
 
 		Assert.assertEquals(
 			"_x100",
-			amGIFImageScaler.getResizeFitValues(amImageConfigurationEntry));
+			_getResizeFitValues(amGIFImageScaler, amImageConfigurationEntry));
+	}
+
+	private String _getResizeFitValues(
+		AMGIFImageScaler amGIFImageScaler,
+		AMImageConfigurationEntry amImageConfigurationEntry) {
+
+		return ReflectionTestUtil.invoke(
+			amGIFImageScaler, "_getResizeFitValues",
+			new Class<?>[] {AMImageConfigurationEntry.class},
+			amImageConfigurationEntry);
 	}
 
 }
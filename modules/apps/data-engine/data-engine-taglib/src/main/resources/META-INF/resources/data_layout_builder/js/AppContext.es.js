9942f689ac10557e4b0c38fecc2899fa07efbbeb
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-02-24 15:00:03 +0100

    LPS-109284 Moves unimplementedProperties to java interface

--- modules/apps/data-engine/data-engine-taglib/src/main/resources/META-INF/resources/data_layout_builder/js/AppContext.es.js
+++ modules/apps/data-engine/data-engine-taglib/src/main/resources/META-INF/resources/data_layout_builder/js/AppContext.es.js
@@ -38,15 +38,7 @@ const AppContext = createContext();
 
 const initialState = {
 	config: {
-		unimplementedProperties: [
-			'fieldNamespace',
-			'indexType',
-			'localizable',
-			'readOnly',
-			'type',
-			'validation',
-			'visibilityExpression'
-		]
+		unimplementedProperties: []
 	},
 	dataDefinition: {
 		dataDefinitionFields: [],

24689e5e435e57056fd1305c6c5dea1304144117
tina.tian <tina.tian@liferay.com>
2020-03-17 13:41:35 -0700

    LPS-103577 portal-search-elasticsearch7-impl: add support to start sidecar, disable it for now

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/connection/ElasticsearchConnectionManager.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/connection/ElasticsearchConnectionManager.java
@@ -140,8 +140,7 @@ public class ElasticsearchConnectionManager
 		ElasticsearchConnection elasticsearchConnection) {
 
 		_elasticsearchConnections.put(
-			EmbeddedElasticsearchConnection.CONNECTION_ID,
-			elasticsearchConnection);
+			elasticsearchConnection.getConnectionId(), elasticsearchConnection);
 	}
 
 	public void setOperationMode(OperationMode operationMode) {
@@ -189,6 +188,14 @@ public class ElasticsearchConnectionManager
 	@Activate
 	protected void activate(Map<String, Object> properties) {
 		setConfiguration(properties);
+
+		if (_operationMode == OperationMode.EMBEDDED) {
+			ElasticsearchConnection elasticsearchConnection =
+				_elasticsearchConnections.get(
+					String.valueOf(OperationMode.EMBEDDED));
+
+			elasticsearchConnection.connect();
+		}
 	}
 
 	protected synchronized void createCompanyIndexes() {
@@ -247,7 +254,7 @@ public class ElasticsearchConnectionManager
 			}
 
 			return _elasticsearchConnections.get(
-				EmbeddedElasticsearchConnection.CONNECTION_ID);
+				String.valueOf(OperationMode.EMBEDDED));
 		}
 
 		if (preferLocalCluster && isCrossClusterReplicationEnabled()) {

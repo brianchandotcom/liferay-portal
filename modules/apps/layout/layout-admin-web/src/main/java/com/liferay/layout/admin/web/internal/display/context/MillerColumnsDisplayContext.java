3f904c25fdd81b495ea3258c302e06439a734fc7
Pavel Savinov <pavel.savinov@liferay.com>
2020-04-06 17:18:35 +0200

    LPS-111531 Edit action must be available only for the content pages

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/display/context/MillerColumnsDisplayContext.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/display/context/MillerColumnsDisplayContext.java
@@ -456,7 +456,7 @@ public class MillerColumnsDisplayContext {
 						"url", editLayoutURL
 					));
 			}
-			else {
+			else if (layout.isTypeContent()) {
 				jsonArray.put(
 					JSONUtil.put(
 						"id", "editLayout"

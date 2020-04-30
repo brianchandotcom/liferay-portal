7d1977a3c989c71c2aa63af2042e71dc1d30a4d4
Alicia Garcia <alicia.garcia@liferay.com>
2020-04-30 16:21:55 +0200

    LPS-112564 use data engine local client to update metadata

--- modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/portlet/action/UpdateDataDefinitionMVCActionCommand.java
+++ modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/portlet/action/UpdateDataDefinitionMVCActionCommand.java
@@ -14,9 +14,17 @@
 
 package com.liferay.document.library.web.internal.portlet.action;
 
+import com.liferay.data.engine.rest.client.dto.v2_0.DataDefinition;
+import com.liferay.data.engine.rest.client.serdes.v2_0.DataDefinitionSerDes;
+import com.liferay.data.engine.rest.client.serdes.v2_0.DataLayoutSerDes;
+import com.liferay.data.engine.rest.dto.v2_0.util.DataEngineUtil;
+import com.liferay.data.engine.rest.resource.v2_0.DataDefinitionResource;
 import com.liferay.document.library.constants.DLPortletKeys;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.WebKeys;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
@@ -42,6 +50,29 @@ public class UpdateDataDefinitionMVCActionCommand extends BaseMVCActionCommand {
 	protected void doProcessAction(
 			ActionRequest actionRequest, ActionResponse actionResponse)
 		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		long dataDefinitionId = ParamUtil.getLong(
+			actionRequest, "dataDefinitionId");
+		String dataDefinitionString = ParamUtil.getString(
+			actionRequest, "dataDefinition");
+		String dataLayout = ParamUtil.getString(actionRequest, "dataLayout");
+
+		DataDefinitionResource dataDefinitionResource =
+			DataDefinitionResource.builder(
+			).user(
+				themeDisplay.getUser()
+			).build();
+
+		DataDefinition dataDefinition = DataDefinitionSerDes.toDTO(
+			dataDefinitionString);
+
+		dataDefinition.setDefaultDataLayout(DataLayoutSerDes.toDTO(dataLayout));
+
+		dataDefinitionResource.putDataDefinition(
+			dataDefinitionId, DataEngineUtil.toDataDefinition(dataDefinition));
 	}
 
 }

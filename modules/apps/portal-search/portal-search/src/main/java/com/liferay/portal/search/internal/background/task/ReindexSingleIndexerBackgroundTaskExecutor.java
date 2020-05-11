brianchandotcom/liferay-portal

commit 0abddc328df9869760af1a6434a9661c2234553c

LPS-100538 portal-search: Instead of using utils, inject DS dependencies to allow testing.
https://issues.liferay.com/browse/LPS-100538

modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/background/task/ReindexSingleIndexerBackgroundTaskExecutor.java
@@ -19,14 +19,13 @@ import com.liferay.portal.kernel.backgroundtask.BackgroundTaskConstants;
 import com.liferay.portal.kernel.backgroundtask.BackgroundTaskExecutor;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.search.IndexWriterHelperUtil;
+import com.liferay.portal.kernel.search.IndexWriterHelper;
 import com.liferay.portal.kernel.search.Indexer;
-import com.liferay.portal.kernel.search.IndexerRegistryUtil;
+import com.liferay.portal.kernel.search.IndexerRegistry;
 import com.liferay.portal.kernel.search.SearchEngine;
 import com.liferay.portal.kernel.search.SearchEngineHelper;
-import com.liferay.portal.kernel.search.SearchEngineHelperUtil;
 import com.liferay.portal.kernel.search.background.task.ReindexBackgroundTaskConstants;
-import com.liferay.portal.kernel.search.background.task.ReindexStatusMessageSenderUtil;
+import com.liferay.portal.kernel.search.background.task.ReindexStatusMessageSender;
 import com.liferay.portal.kernel.util.Validator;
 
 import java.io.Serializable;
@@ -34,9 +33,20 @@ import java.io.Serializable;
 import java.util.Collection;
 import java.util.Map;
 
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
 /**
  * @author Andrew Betts
  */
+@Component(
+	immediate = true,
+	property = "background.task.executor.class.name=com.liferay.portal.search.internal.background.task.ReindexSingleIndexerBackgroundTaskExecutor",
+	service = {
+		BackgroundTaskExecutor.class,
+		ReindexSingleIndexerBackgroundTaskExecutor.class
+	}
+)
 public class ReindexSingleIndexerBackgroundTaskExecutor
 	extends ReindexBackgroundTaskExecutor {
 
@@ -46,7 +56,7 @@ public class ReindexSingleIndexerBackgroundTaskExecutor
 
 	@Override
 	public BackgroundTaskExecutor clone() {
-		return new ReindexSingleIndexerBackgroundTaskExecutor();
+		return this;
 	}
 
 	@Override
@@ -67,20 +77,17 @@ public class ReindexSingleIndexerBackgroundTaskExecutor
 	protected void reindex(String className, long[] companyIds)
 		throws Exception {
 
-		Indexer<?> indexer = IndexerRegistryUtil.getIndexer(className);
+		Indexer<?> indexer = indexerRegistry.getIndexer(className);
 
 		if (indexer == null) {
 			return;
 		}
 
-		SearchEngineHelper searchEngineHelper =
-			SearchEngineHelperUtil.getSearchEngineHelper();
-
 		Collection<SearchEngine> searchEngines =
 			searchEngineHelper.getSearchEngines();
 
 		for (long companyId : companyIds) {
-			ReindexStatusMessageSenderUtil.sendStatusMessage(
+			reindexStatusMessageSender.sendStatusMessage(
 				ReindexBackgroundTaskConstants.SINGLE_START, companyId,
 				companyIds);
 
@@ -89,7 +96,7 @@ public class ReindexSingleIndexerBackgroundTaskExecutor
 					searchEngine.initialize(companyId);
 				}
 
-				IndexWriterHelperUtil.deleteEntityDocuments(
+				indexWriterHelper.deleteEntityDocuments(
 					indexer.getSearchEngineId(), companyId, className, true);
 
 				indexer.reindex(new String[] {String.valueOf(companyId)});
@@ -98,12 +105,24 @@ public class ReindexSingleIndexerBackgroundTaskExecutor
 				_log.error(exception, exception);
 			}
 			finally {
-				ReindexStatusMessageSenderUtil.sendStatusMessage(
+				reindexStatusMessageSender.sendStatusMessage(
 					ReindexBackgroundTaskConstants.SINGLE_END, companyId,
 					companyIds);
 			}
 		}
 	}
+
+	@Reference
+	protected IndexerRegistry indexerRegistry;
+
+	@Reference
+	protected IndexWriterHelper indexWriterHelper;
+
+	@Reference
+	protected ReindexStatusMessageSender reindexStatusMessageSender;
+
+	@Reference
+	protected SearchEngineHelper searchEngineHelper;
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		ReindexSingleIndexerBackgroundTaskExecutor.class);

modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/background/task/BackgroundTaskExecutorConfigurator.java
@@ -44,11 +44,8 @@ public class BackgroundTaskExecutorConfigurator {
 		registerBackgroundTaskExecutor(
 			bundleContext, reindexPortalBackgroundTaskExecutor);
 
-		BackgroundTaskExecutor reindexSingleIndexerBackgroundTaskExecutor =
-			new ReindexSingleIndexerBackgroundTaskExecutor();
-
 		registerBackgroundTaskExecutor(
-			bundleContext, reindexSingleIndexerBackgroundTaskExecutor);
+			bundleContext, _reindexSingleIndexerBackgroundTaskExecutor);
 	}
 
 	@Deactivate
@@ -80,6 +77,10 @@ public class BackgroundTaskExecutorConfigurator {
 
 	@Reference
 	private PortalExecutorManager _portalExecutorManager;
+
+	@Reference
+	private ReindexSingleIndexerBackgroundTaskExecutor
+		_reindexSingleIndexerBackgroundTaskExecutor;
 
 	private final Set<ServiceRegistration<BackgroundTaskExecutor>>
 		_serviceRegistrations = new HashSet<>();

commit 92b5e197b2408501cfeb5a08d5c010cfcf05c8f5

LPS-112770 This regression is caused by 0abddc32

modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/background/task/ReindexSingleIndexerBackgroundTaskExecutor.java
@@ -26,6 +26,7 @@ import com.liferay.portal.kernel.search.SearchEngine;
 import com.liferay.portal.kernel.search.SearchEngineHelper;
 import com.liferay.portal.kernel.search.background.task.ReindexBackgroundTaskConstants;
 import com.liferay.portal.kernel.search.background.task.ReindexStatusMessageSender;
+import com.liferay.portal.kernel.util.ServiceProxyFactory;
 import com.liferay.portal.kernel.util.Validator;
 
 import java.io.Serializable;
@@ -112,11 +113,14 @@ public class ReindexSingleIndexerBackgroundTaskExecutor
 		}
 	}
 
-	@Reference
-	protected IndexerRegistry indexerRegistry;
+	protected static volatile IndexWriterHelper indexWriterHelper =
+		ServiceProxyFactory.newServiceTrackedInstance(
+			IndexWriterHelper.class,
+			ReindexSingleIndexerBackgroundTaskExecutor.class,
+			"indexWriterHelper", true);
 
 	@Reference
-	protected IndexWriterHelper indexWriterHelper;
+	protected IndexerRegistry indexerRegistry;
 
 	@Reference
 	protected ReindexStatusMessageSender reindexStatusMessageSender;

modules/apps/portal-search/source-formatter-suppressions.xml
@@ -7,4 +7,7 @@
 		<suppress checks="FactoryCheck" files="portal-search-test/src/testIntegration/java/com/liferay/portal/search/facet/faceted/searcher/test/SearchPermissionCheckerFacetedSearcherTest\.java" />
 		<suppress checks="MapBuilderCheck" files="portal-search-admin-web/src/main/java/com/liferay/portal/search/admin/web/internal/display/context/FieldMappingsDisplayBuilder\.java" />
 	</checkstyle>
+	<source-check>
+		<suppress checks="JavaModuleServiceProxyFactoryCheck" files="portal-search/src/main/java/com/liferay/portal/search/internal/background/task/ReindexSingleIndexerBackgroundTaskExecutor\.java" />
+	</source-check>
 </suppressions>
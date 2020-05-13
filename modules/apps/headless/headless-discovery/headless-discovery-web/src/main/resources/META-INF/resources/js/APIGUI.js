50da0e4e649afa199aa1b68414fb4bf7e63acd33
Javier Gamarra <nhpatt@gmail.com>
2020-05-13 20:45:00 +0200

    LPS-111024 show client version description

--- modules/apps/headless/headless-discovery/headless-discovery-web/src/main/resources/META-INF/resources/js/APIGUI.js
+++ modules/apps/headless/headless-discovery/headless-discovery-web/src/main/resources/META-INF/resources/js/APIGUI.js
@@ -40,6 +40,7 @@ const APIGUI = () => {
 		categories,
 		categoryKey,
 		filter,
+		info,
 		method,
 		path,
 		paths,
@@ -199,6 +200,16 @@ const APIGUI = () => {
 											/>
 										))}
 								</ClaySelect>
+							</ClayForm.Group>
+
+							<ClayForm.Group className="pt-3 px-3">
+								<label
+									className="d-flex justify-content-between"
+									htmlFor="categorySelect"
+								>
+									{Liferay.Language.get('description')}
+								</label>
+								<p>{info && info.description}</p>
 							</ClayForm.Group>
 
 							<ClayForm.Group className="pt-0 px-3">

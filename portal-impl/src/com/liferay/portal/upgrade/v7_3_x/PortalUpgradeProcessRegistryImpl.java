ade5c2a60b412390dc05e65d0190c6e810061dcb
Preston-Crary <preston.crary@liferay.com>
2020-05-13 15:52:35 -0700

    LPS-113635 Make expando mvcc enabled.

--- portal-impl/src/com/liferay/portal/upgrade/v7_3_x/PortalUpgradeProcessRegistryImpl.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_3_x/PortalUpgradeProcessRegistryImpl.java
@@ -84,6 +84,9 @@ public class PortalUpgradeProcessRegistryImpl
 				"DLFileEntry", "DLFileEntryMetadata", "DLFileEntryType",
 				"DLFileEntryTypes_DLFolders", "DLFileShortcut", "DLFileVersion",
 				"DLFolder"));
+
+		upgradeProcesses.put(
+			new Version(8, 8, 0), new UpgradeExpandoMVCCVersion());
 	}
 
 }

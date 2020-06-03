7b01b7099d96f30605440074aae05a25061461fa
Alejandro Tardín <alejandro.tardin@liferay.com>
2020-06-03 10:09:13 +0200

    LPS-114521 Using fluent api

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/info/item/provider/JournalArticleInfoItemFormProvider.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/info/item/provider/JournalArticleInfoItemFormProvider.java
@@ -216,14 +216,6 @@ public class JournalArticleInfoItemFormProvider
 			for (DDMFormFieldValue ddmFormFieldValue :
 					ddmFormValues.getDDMFormFieldValues()) {
 
-				InfoLocalizedValue.Builder builder =
-					InfoLocalizedValue.builder();
-
-				Value value = ddmFormFieldValue.getValue();
-
-				builder.defaultLocale(value.getDefaultLocale());
-				builder.addValues(value.getValues());
-
 				String fieldName = ddmFormFieldValue.getName();
 
 				InfoField infoField = new InfoField(
@@ -231,8 +223,17 @@ public class JournalArticleInfoItemFormProvider
 					InfoLocalizedValue.localize(getClass(), fieldName),
 					fieldName);
 
+				Value value = ddmFormFieldValue.getValue();
+
 				infoFieldValues.add(
-					new InfoFieldValue<>(infoField, builder.build()));
+					new InfoFieldValue<>(
+						infoField,
+						InfoLocalizedValue.builder(
+						).defaultLocale(
+							value.getDefaultLocale()
+						).addValues(
+							value.getValues()
+						).build()));
 			}
 		}
 		catch (PortalException portalException) {

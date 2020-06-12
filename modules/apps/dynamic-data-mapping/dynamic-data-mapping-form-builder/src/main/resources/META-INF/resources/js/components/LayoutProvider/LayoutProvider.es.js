316a8daa4561952f1838a17f5fee94de39da3f51
Renato Rego <renato.rego@liferay.com>
2020-06-12 09:13:31 -0300

    LPS-115095 Make the default behavior not autogenerate field name using field label.

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/LayoutProvider/LayoutProvider.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/LayoutProvider/LayoutProvider.es.js
@@ -43,7 +43,6 @@ import handleFieldSetAdded from './handlers/fieldSetAddedHandler.es';
 import handleFocusedFieldEvaluationEnded from './handlers/focusedFieldEvaluationEndedHandler.es';
 import handleLanguageIdDeleted from './handlers/languageIdDeletedHandler.es';
 import handleSectionAdded from './handlers/sectionAddedHandler.es';
-import {shouldAutoGenerateName} from './util/defaults.es';
 import {generateFieldName} from './util/fields.es';
 
 /**
@@ -680,10 +679,6 @@ class LayoutProvider extends Component {
 		});
 	}
 
-	_shouldAutoGenerateNameValueFn() {
-		return shouldAutoGenerateName;
-	}
-
 	_successPageSettingsValueFn() {
 		const {defaultLanguageId, initialSuccessPageSettings} = this.props;
 
@@ -800,6 +795,15 @@ LayoutProvider.PROPS = {
 	fieldSets: Config.array().value([]),
 
 	/**
+	 * @default false
+	 * @instance
+	 * @memberof LayoutProvider
+	 * @type {?bool}
+	 */
+
+	generateFieldNameUsingFieldLabel: Config.bool().value(false),
+
+	/**
 	 * @default undefined
 	 * @instance
 	 * @memberof LayoutProvider
@@ -839,17 +843,6 @@ LayoutProvider.PROPS = {
 	 */
 
 	rules: Config.arrayOf(ruleStructure),
-
-	/**
-	 * @default _shouldAutoGenerateNameValueFn
-	 * @instance
-	 * @memberof LayoutProvider
-	 * @type {?function}
-	 */
-
-	shouldAutoGenerateName: Config.func().valueFn(
-		'_shouldAutoGenerateNameValueFn'
-	),
 
 	/**
 	 * @default undefined

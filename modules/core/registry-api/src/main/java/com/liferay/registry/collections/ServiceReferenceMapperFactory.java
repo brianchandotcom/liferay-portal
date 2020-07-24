da59c09e0c5f06866d5a99d4a7890c0371bf13af
shuyangzhou <shuyang.zhou@liferay.com>
2020-07-24 01:07:52 -0700

    Revert "LPS-117619 SF"
    This reverts commit 86ced7ace9525526a0ae1af1fa34a3280d8ded46.

--- modules/core/registry-api/src/main/java/com/liferay/registry/collections/ServiceReferenceMapperFactory.java
+++ modules/core/registry-api/src/main/java/com/liferay/registry/collections/ServiceReferenceMapperFactory.java
@@ -26,30 +26,6 @@ import java.util.function.Function;
 public class ServiceReferenceMapperFactory {
 
 	public static <K, S> ServiceReferenceMapper<K, S> create(
-		final Function<S, K> getterFunction) {
-
-		return new ServiceReferenceMapper<K, S>() {
-
-			@Override
-			public void map(
-				ServiceReference<S> serviceReference, Emitter<K> emitter) {
-
-				Registry registry = RegistryUtil.getRegistry();
-
-				S service = registry.getService(serviceReference);
-
-				try {
-					emitter.emit(getterFunction.apply(service));
-				}
-				finally {
-					registry.ungetService(serviceReference);
-				}
-			}
-
-		};
-	}
-
-	public static <K, S> ServiceReferenceMapper<K, S> create(
 		final ServiceMapper<K, S> serviceMapper) {
 
 		return new ServiceReferenceMapper<K, S>() {
@@ -96,6 +72,30 @@ public class ServiceReferenceMapperFactory {
 				}
 				else {
 					emitter.emit((K)propertyValue);
+				}
+			}
+
+		};
+	}
+
+	public static <K, S> ServiceReferenceMapper<K, S> create(
+		final Function<S, K> getterFunction) {
+
+		return new ServiceReferenceMapper<K, S>() {
+
+			@Override
+			public void map(
+				ServiceReference<S> serviceReference, Emitter<K> emitter) {
+
+				Registry registry = RegistryUtil.getRegistry();
+
+				S service = registry.getService(serviceReference);
+
+				try {
+					emitter.emit(getterFunction.apply(service));
+				}
+				finally {
+					registry.ungetService(serviceReference);
 				}
 			}
 

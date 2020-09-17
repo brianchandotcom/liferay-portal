brianchandotcom/liferay-portal

commit 68b55c49e507ce8ea509c25d945113bb709092f4

LPS-112800 Modify the unlocalisable logic, now you can modify the element in every language but the value always be the same. Add defaultlanguage of the structure to availableLanguageIds when the portal is being used in a  different language.

modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
@@ -358,17 +358,7 @@ AUI.add(
 				if (instance.get('readOnly')) {
 					retVal = true;
 				}
-				else {
-					var form = instance.getForm();
-
-					if (
-						!instance.get('localizable') &&
-						form.getDefaultLocale() != instance.get('displayLocale')
-					) {
-						retVal = true;
-					}
-				}
-
+				
 				return retVal;
 			},
 		};
@@ -4178,11 +4168,15 @@ AUI.add(
 				},
 
 				fillEmptyLocales(instance, fields, availableLanguageIds) {
+					var defaultLocale = instance.getDefaultLocale();
+
+					if (availableLanguageIds.indexOf(defaultLocale) === -1){
+						availableLanguageIds.push(defaultLocale);
+					}
+
 					fields.forEach((field) => {
 						if (field.get('localizable')) {
 							var localizationMap = field.get('localizationMap');
-
-							var defaultLocale = field.getDefaultLocale();
 
 							availableLanguageIds.forEach((locale) => {
 								if (!localizationMap[locale]) {

commit e1188ceaee5764ab8b9abc6947a37e3ca3d04cca

LPS-120004 Non localizable DDM fields are editable in all the languages, caused by  68b55c49e507ce8ea509c25d945113bb709092f4 , behaviour clarified in PTR-1943

modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
@@ -360,6 +360,16 @@ AUI.add(
 				if (instance.get('readOnly')) {
 					retVal = true;
 				}
+				else {
+					var form = instance.getForm();
+
+					if (
+						!instance.get('localizable') &&
+						form.getDefaultLocale() != instance.get('displayLocale')
+					) {
+						retVal = true;
+					}
+				}
 
 				return retVal;
 			},
@@ -4151,15 +4161,11 @@ AUI.add(
 				},
 
 				fillEmptyLocales(instance, fields, availableLanguageIds) {
-					var defaultLocale = instance.getDefaultLocale();
-
-					if (availableLanguageIds.indexOf(defaultLocale) === -1) {
-						availableLanguageIds.push(defaultLocale);
-					}
-
 					fields.forEach((field) => {
 						if (field.get('localizable')) {
 							var localizationMap = field.get('localizationMap');
+
+							var defaultLocale = field.getDefaultLocale();
 
 							availableLanguageIds.forEach((locale) => {
 								if (!localizationMap[locale]) {
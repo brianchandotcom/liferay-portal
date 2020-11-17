29ba2b2a1f9fdf07ced0a04a8f51aa97470f28a0
Diego Nascimento <diego.nascimento@liferay.com>
2020-11-17 18:35:05 -0300

    LPS-123062 Replaces the field name by the field label when rendering the Calculator expression
    It also removes an unecessary `useCallback` usage

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/RuleBuilder/RulesSupport.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/RuleBuilder/RulesSupport.es.js
@@ -325,6 +325,10 @@ const findInvalidRule = (rule) => {
 const replaceFieldNameByFieldLabel = (expression, fields) => {
 	const operands = expression.match(DEFAULT_FIELD_NAMES_REGEX_FOR_EXPRESSION);
 
+	if (!operands) {
+		return expression;
+	}
+
 	let newExpression = expression;
 
 	operands.map((operand) => {

555b08160425752d3f9c0f7b5f11f21a2a4b039e
balazssk <balazs.safrany-kovalik@liferay.com>
2020-11-26 09:12:50 +0100

    LPS-123638 Escape CDATA in content

--- modules/apps/journal/journal-uad/src/main/java/com/liferay/journal/uad/exporter/JournalArticleUADExporter.java
+++ modules/apps/journal/journal-uad/src/main/java/com/liferay/journal/uad/exporter/JournalArticleUADExporter.java
@@ -14,13 +14,61 @@
 
 package com.liferay.journal.uad.exporter;
 
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.user.associated.data.exporter.UADExporter;
 
 import org.osgi.service.component.annotations.Component;
 
 /**
- * @author Brian Wing Shun Chan
+ * @author Balázs Sáfrány-Kovalik
  */
 @Component(immediate = true, service = UADExporter.class)
 public class JournalArticleUADExporter extends BaseJournalArticleUADExporter {
+
+	@Override
+	protected String toXmlString(JournalArticle journalArticle) {
+		StringBundler sb = new StringBundler(31);
+
+		sb.append("<model><model-name>");
+		sb.append("com.liferay.journal.model.JournalArticle");
+		sb.append("</model-name>");
+
+		sb.append(
+			"<column><column-name>id</column-name><column-value><![CDATA[");
+		sb.append(journalArticle.getId());
+		sb.append("]]></column-value></column>");
+		sb.append("<column><column-name>statusByUserId</column-name>");
+		sb.append("<column-value><![CDATA[");
+		sb.append(journalArticle.getStatusByUserId());
+		sb.append("]]></column-value></column>");
+		sb.append("<column><column-name>statusByUserName</column-name>");
+		sb.append("<column-value><![CDATA[");
+		sb.append(journalArticle.getStatusByUserName());
+		sb.append("]]></column-value></column>");
+		sb.append("<column><column-name>userId</column-name>");
+		sb.append("<column-value><![CDATA[");
+		sb.append(journalArticle.getUserId());
+		sb.append("]]></column-value></column>");
+		sb.append("<column><column-name>userName</column-name>");
+		sb.append("<column-value><![CDATA[");
+		sb.append(journalArticle.getUserName());
+		sb.append("]]></column-value></column>");
+		sb.append("<column><column-name>urlTitle</column-name>");
+		sb.append("<column-value><![CDATA[");
+		sb.append(journalArticle.getUrlTitle());
+		sb.append("]]></column-value></column>");
+		sb.append("<column><column-name>content</column-name>");
+		sb.append("<column-value><![CDATA[");
+		sb.append(
+			StringUtil.replace(
+				journalArticle.getContent(), "]]><", "]]]]><![CDATA[><"));
+		sb.append("]]></column-value></column>");
+
+		sb.append("</model>");
+
+		return sb.toString();
+	}
+
 }

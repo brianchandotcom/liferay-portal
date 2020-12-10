dcb763326e77cac8ab4b8160b33bf1e1c9e5ea25
Carlos Sierra Andrés <carlos.sierra@liferay.com>
2020-12-10 17:48:40 +0100

    LPS-118086 Add HashGenerationContext
    class that holds the configuration to generate hashes from inputs
    
    Co-authored-by: arthurchan35 <arthurchan35@gmail.com>

---
+++ modules/apps/portal-crypto-hash/portal-crypto-hash-api/src/main/java/com/liferay/portal/crypto/hash/generation/context/HashGenerationContext.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.crypto.hash.generation.context;
+
+import com.liferay.portal.crypto.hash.generation.context.salt.SaltCommand;
+import com.liferay.portal.crypto.hash.provider.context.CryptoHashProviderContext;
+
+import java.util.Map;
+
+import org.osgi.annotation.versioning.ProviderType;
+
+/**
+ * @author Carlos Sierra Andrés
+ * @author Arthur Chan
+ */
+@ProviderType
+public interface CryptoHashGenerationContext extends CryptoHashProviderContext {
+
+	public SaltCommand[] getSaltCommands();
+
+	public interface Builder {
+
+		public CryptoHashGenerationContext build(
+			String cryptoHashProviderName,
+			Map<String, ?> cryptoHashProviderProperties,
+			SaltCommand... saltCommands);
+
+	}
+
+}

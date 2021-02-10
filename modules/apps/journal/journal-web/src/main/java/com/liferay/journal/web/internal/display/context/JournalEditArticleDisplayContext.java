25d97dc2af9d87ad3f74ce8776e03fe7b4428d97
Antonio Ortega <60252917@liferay.com>
2021-02-10 12:43:42 +0100

    LPS-127734 In case the article is new, the default language should be the one from the site. In case the article already exists, then the default language is taken from the current article

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalEditArticleDisplayContext.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalEditArticleDisplayContext.java
@@ -378,7 +378,7 @@ public class JournalEditArticleDisplayContext {
 			siteDefaultLocale = LocaleUtil.getSiteDefault();
 		}
 
-		if (_article == null) {
+		if (Validator.isNull(getArticleId())) {
 			_defaultArticleLanguageId = LocaleUtil.toLanguageId(
 				siteDefaultLocale);
 		}

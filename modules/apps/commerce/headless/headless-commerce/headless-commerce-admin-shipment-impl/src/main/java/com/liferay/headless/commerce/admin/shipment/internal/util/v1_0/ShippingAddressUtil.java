0581d2691843d95c287850c36a8cca55f947b7c4
Riccardo <riccardo.alberti@liferay.com>
2021-03-03 12:00:09 +0100

    COMMERCE-4977 Use BeanUtils to retrieve address data

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-shipment-impl/src/main/java/com/liferay/headless/commerce/admin/shipment/internal/util/v1_0/ShippingAddressUtil.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-shipment-impl/src/main/java/com/liferay/headless/commerce/admin/shipment/internal/util/v1_0/ShippingAddressUtil.java
@@ -20,6 +20,7 @@ import com.liferay.commerce.service.CommerceAddressService;
 import com.liferay.commerce.service.CommerceShipmentService;
 import com.liferay.headless.commerce.admin.shipment.dto.v1_0.ShippingAddress;
 import com.liferay.headless.commerce.core.util.ServiceContextHelper;
+import com.liferay.portal.kernel.bean.BeanPropertiesUtil;
 import com.liferay.portal.kernel.model.Country;
 import com.liferay.portal.kernel.model.Region;
 import com.liferay.portal.kernel.service.CountryService;
@@ -51,34 +52,33 @@ public class ShippingAddressUtil {
 		return commerceShipmentService.updateAddress(
 			commerceShipment.getCommerceShipmentId(),
 			GetterUtil.get(
-				shippingAddress.getName(), _getName(commerceAddress)),
+				shippingAddress.getName(),
+				BeanPropertiesUtil.getString(commerceAddress, "name")),
 			GetterUtil.get(
 				shippingAddress.getDescription(),
-				_getDescription(commerceAddress)),
+				BeanPropertiesUtil.getString(commerceAddress, "description")),
 			GetterUtil.get(
-				shippingAddress.getStreet1(), _getStreet1(commerceAddress)),
+				shippingAddress.getStreet1(),
+				BeanPropertiesUtil.getString(commerceAddress, "street1")),
 			GetterUtil.get(
-				shippingAddress.getStreet2(), _getStreet2(commerceAddress)),
+				shippingAddress.getStreet2(),
+				BeanPropertiesUtil.getString(commerceAddress, "street2")),
 			GetterUtil.get(
-				shippingAddress.getStreet3(), _getStreet3(commerceAddress)),
+				shippingAddress.getStreet3(),
+				BeanPropertiesUtil.getString(commerceAddress, "street3")),
 			GetterUtil.get(
-				shippingAddress.getCity(), _getCity(commerceAddress)),
-			GetterUtil.get(shippingAddress.getZip(), _getZip(commerceAddress)),
+				shippingAddress.getCity(),
+				BeanPropertiesUtil.getString(commerceAddress, "city")),
+			GetterUtil.get(
+				shippingAddress.getZip(),
+				BeanPropertiesUtil.getString(commerceAddress, "zip")),
 			_getRegionId(
 				commerceAddress, country, regionService, shippingAddress),
 			_getCountryId(commerceAddress, country, shippingAddress),
 			GetterUtil.get(
 				shippingAddress.getPhoneNumber(),
-				_getPhoneNumber(commerceAddress)),
+				BeanPropertiesUtil.getString(commerceAddress, "phoneNumber")),
 			serviceContextHelper.getServiceContext());
-	}
-
-	private static String _getCity(CommerceAddress commerceAddress) {
-		if (commerceAddress == null) {
-			return null;
-		}
-
-		return commerceAddress.getCity();
 	}
 
 	private static long _getCountryId(
@@ -88,7 +88,7 @@ public class ShippingAddressUtil {
 		if (Validator.isNull(shippingAddress.getCountryISOCode()) &&
 			(commerceAddress != null)) {
 
-			return commerceAddress.getCommerceCountryId();
+			return commerceAddress.getCountryId();
 		}
 
 		if (country == null) {
@@ -96,30 +96,6 @@ public class ShippingAddressUtil {
 		}
 
 		return country.getCountryId();
-	}
-
-	private static String _getDescription(CommerceAddress commerceAddress) {
-		if (commerceAddress == null) {
-			return null;
-		}
-
-		return commerceAddress.getDescription();
-	}
-
-	private static String _getName(CommerceAddress commerceAddress) {
-		if (commerceAddress == null) {
-			return null;
-		}
-
-		return commerceAddress.getName();
-	}
-
-	private static String _getPhoneNumber(CommerceAddress commerceAddress) {
-		if (commerceAddress == null) {
-			return null;
-		}
-
-		return commerceAddress.getPhoneNumber();
 	}
 
 	private static long _getRegionId(
@@ -130,7 +106,7 @@ public class ShippingAddressUtil {
 		if (Validator.isNull(shippingAddress.getRegionISOCode()) &&
 			(commerceAddress != null)) {
 
-			return commerceAddress.getCommerceRegionId();
+			return commerceAddress.getRegionId();
 		}
 
 		long countryId = _getCountryId(
@@ -144,38 +120,6 @@ public class ShippingAddressUtil {
 		}
 
 		return region.getRegionId();
-	}
-
-	private static String _getStreet1(CommerceAddress commerceAddress) {
-		if (commerceAddress == null) {
-			return null;
-		}
-
-		return commerceAddress.getStreet1();
-	}
-
-	private static String _getStreet2(CommerceAddress commerceAddress) {
-		if (commerceAddress == null) {
-			return null;
-		}
-
-		return commerceAddress.getStreet2();
-	}
-
-	private static String _getStreet3(CommerceAddress commerceAddress) {
-		if (commerceAddress == null) {
-			return null;
-		}
-
-		return commerceAddress.getStreet3();
-	}
-
-	private static String _getZip(CommerceAddress commerceAddress) {
-		if (commerceAddress == null) {
-			return null;
-		}
-
-		return commerceAddress.getZip();
 	}
 
 }

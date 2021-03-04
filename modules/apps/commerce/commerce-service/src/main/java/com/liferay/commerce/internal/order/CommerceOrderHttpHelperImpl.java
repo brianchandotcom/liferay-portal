ed6245fdccc579744e35014a83a6768641f04909
Riccardo <riccardo.alberti@liferay.com>
2021-03-04 15:55:04 +0100

    COMMERCE-5955 Fetch commerce order from persistence to make sure info are up to date

--- modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/order/CommerceOrderHttpHelperImpl.java
+++ modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/order/CommerceOrderHttpHelperImpl.java
@@ -469,7 +469,17 @@ public class CommerceOrderHttpHelperImpl implements CommerceOrderHttpHelper {
 		CommerceOrder commerceOrder = _commerceOrderThreadLocal.get();
 
 		if (commerceOrder != null) {
-			return commerceOrder;
+			CommerceOrder persistenceCommerceOrder =
+				_commerceOrderLocalService.fetchCommerceOrder(
+					commerceOrder.getCommerceOrderId());
+
+			if (persistenceCommerceOrder == null) {
+				return commerceOrder;
+			}
+
+			_commerceOrderThreadLocal.set(persistenceCommerceOrder);
+
+			return persistenceCommerceOrder;
 		}
 
 		CommerceChannel commerceChannel =

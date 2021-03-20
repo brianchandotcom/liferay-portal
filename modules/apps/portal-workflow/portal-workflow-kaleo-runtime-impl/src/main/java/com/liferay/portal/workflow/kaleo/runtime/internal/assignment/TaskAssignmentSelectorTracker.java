d3ea62df23d7be79e1a546b74aaa71be9534f10f
Rafael Praxedes <rafael.praxedes@liferay.com>
2021-03-20 00:05:54 -0300

    LPS-127323 Update the old tracker to trigger the new one to keep the backward compatibility until the next version

--- modules/apps/portal-workflow/portal-workflow-kaleo-runtime-impl/src/main/java/com/liferay/portal/workflow/kaleo/runtime/internal/assignment/TaskAssignmentSelectorTracker.java
+++ modules/apps/portal-workflow/portal-workflow-kaleo-runtime-impl/src/main/java/com/liferay/portal/workflow/kaleo/runtime/internal/assignment/TaskAssignmentSelectorTracker.java
@@ -14,17 +14,29 @@
 
 package com.liferay.portal.workflow.kaleo.runtime.internal.assignment;
 
+import com.liferay.osgi.util.ServiceTrackerFactory;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.util.HashMapDictionary;
+import com.liferay.portal.workflow.kaleo.model.KaleoTaskAssignment;
+import com.liferay.portal.workflow.kaleo.runtime.ExecutionContext;
+import com.liferay.portal.workflow.kaleo.runtime.assignment.KaleoTaskAssignmentSelector;
+import com.liferay.portal.workflow.kaleo.runtime.assignment.KaleoTaskAssignmentSelectorRegistry;
 import com.liferay.portal.workflow.kaleo.runtime.assignment.TaskAssignmentSelector;
 import com.liferay.portal.workflow.kaleo.runtime.assignment.TaskAssignmentSelectorRegistry;
 
+import java.util.Collection;
+import java.util.Dictionary;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferenceCardinality;
-import org.osgi.service.component.annotations.ReferencePolicy;
-import org.osgi.service.component.annotations.ReferencePolicyOption;
+import org.osgi.util.tracker.ServiceTracker;
+import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
 /**
  * @author Leonardo Barros
@@ -40,36 +52,157 @@ public class TaskAssignmentSelectorTracker
 	public TaskAssignmentSelector getTaskAssignmentSelector(
 		String assigneeClassName) {
 
-		return _taskAssignmentSelectors.get(assigneeClassName);
+		KaleoTaskAssignmentSelector kaleoTaskAssignmentSelector =
+			_kaleoTaskAssignmentSelectorRegistry.getKaleoTaskAssignmentSelector(
+				assigneeClassName);
+
+		if (kaleoTaskAssignmentSelector instanceof
+				TaskAssignmentSelectorWrapper) {
+
+			TaskAssignmentSelectorWrapper taskAssignmentSelectorWrapper =
+				(TaskAssignmentSelectorWrapper)kaleoTaskAssignmentSelector;
+
+			return taskAssignmentSelectorWrapper._taskAssignmentSelector;
+		}
+
+		return new TaskAssignmentSelector() {
+
+			@Override
+			public Collection<KaleoTaskAssignment> calculateTaskAssignments(
+					KaleoTaskAssignment kaleoTaskAssignment,
+					ExecutionContext executionContext)
+				throws PortalException {
+
+				return kaleoTaskAssignmentSelector.getKaleoTaskAssignments(
+					kaleoTaskAssignment, executionContext);
+			}
+
+		};
 	}
 
-	@Reference(
-		cardinality = ReferenceCardinality.MULTIPLE,
-		policy = ReferencePolicy.DYNAMIC,
-		policyOption = ReferencePolicyOption.GREEDY,
-		target = "(assignee.class.name=*)"
-	)
-	protected void addTaskAssignmentSelector(
-		TaskAssignmentSelector taskAssignmentSelector,
-		Map<String, Object> properties) {
-
-		Object assigneeClassName = properties.get("assignee.class.name");
-
-		_taskAssignmentSelectors.put(
-			assigneeClassName.toString(), taskAssignmentSelector);
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_serviceTracker = ServiceTrackerFactory.open(
+			bundleContext,
+			"(&(objectClass=com.liferay.portal.workflow.kaleo.runtime." +
+				"assignment.TaskAssignmentSelector)(assignee.class.name=*))",
+			new TaskAssignmentSelectorServiceTrackerCustomizer(bundleContext));
 	}
 
-	protected void removeTaskAssignmentSelector(
-		TaskAssignmentSelector taskAssignmentSelector,
-		Map<String, Object> properties) {
+	@Reference
+	private KaleoTaskAssignmentSelectorRegistry
+		_kaleoTaskAssignmentSelectorRegistry;
 
-		String assigneeClassName = (String)properties.get(
-			"assignee.class.name");
-
-		_taskAssignmentSelectors.remove(assigneeClassName);
-	}
-
-	private final Map<String, TaskAssignmentSelector> _taskAssignmentSelectors =
+	private final Map<String, ServiceRegistration<?>> _serviceReferenceMap =
 		new ConcurrentHashMap<>();
+	private ServiceTracker<TaskAssignmentSelector, ServiceRegistration<?>>
+		_serviceTracker;
+
+	private final class TaskAssignmentSelectorServiceTrackerCustomizer
+		implements ServiceTrackerCustomizer
+			<TaskAssignmentSelector, ServiceRegistration<?>> {
+
+		public TaskAssignmentSelectorServiceTrackerCustomizer(
+			BundleContext bundleContext) {
+
+			_bundleContext = bundleContext;
+		}
+
+		@Override
+		public ServiceRegistration<?> addingService(
+			ServiceReference<TaskAssignmentSelector> serviceReference) {
+
+			Dictionary<String, Object> properties = new HashMapDictionary<>();
+
+			properties.put(
+				"assignee.class.name",
+				serviceReference.getProperty("assignee.class.name"));
+
+			TaskAssignmentSelector taskAssignmentSelector =
+				_bundleContext.getService(serviceReference);
+
+			ServiceRegistration<KaleoTaskAssignmentSelector>
+				serviceRegistration = _bundleContext.registerService(
+					KaleoTaskAssignmentSelector.class,
+					new TaskAssignmentSelectorWrapper(taskAssignmentSelector),
+					properties);
+
+			Class<?> taskAssignmentSelectorClass =
+				taskAssignmentSelector.getClass();
+
+			_serviceReferenceMap.put(
+				taskAssignmentSelectorClass.getName(), serviceRegistration);
+
+			return serviceRegistration;
+		}
+
+		@Override
+		public void modifiedService(
+			ServiceReference<TaskAssignmentSelector> serviceReference,
+			ServiceRegistration<?> serviceRegistration) {
+
+			removedService(serviceReference, serviceRegistration);
+
+			addingService(serviceReference);
+		}
+
+		@Override
+		public void removedService(
+			ServiceReference<TaskAssignmentSelector> serviceReference,
+			ServiceRegistration<?> serviceRegistration) {
+
+			TaskAssignmentSelector taskAssignmentSelector =
+				_bundleContext.getService(serviceReference);
+
+			Class<? extends TaskAssignmentSelector>
+				taskAssignmentSelectorClass = taskAssignmentSelector.getClass();
+
+			try {
+				if (_serviceReferenceMap.containsKey(
+						taskAssignmentSelectorClass.getName())) {
+
+					ServiceRegistration<?>
+						kaleoTaskAssignmentSelectorServiceRegistration =
+							_serviceReferenceMap.remove(
+								taskAssignmentSelectorClass.getName());
+
+					_bundleContext.ungetService(
+						kaleoTaskAssignmentSelectorServiceRegistration.
+							getReference());
+
+					kaleoTaskAssignmentSelectorServiceRegistration.unregister();
+				}
+			}
+			finally {
+				_bundleContext.ungetService(serviceReference);
+			}
+		}
+
+		private final BundleContext _bundleContext;
+
+	}
+
+	private final class TaskAssignmentSelectorWrapper
+		implements KaleoTaskAssignmentSelector {
+
+		@Override
+		public Collection<KaleoTaskAssignment> getKaleoTaskAssignments(
+				KaleoTaskAssignment kaleoTaskAssignment,
+				ExecutionContext executionContext)
+			throws PortalException {
+
+			return _taskAssignmentSelector.calculateTaskAssignments(
+				kaleoTaskAssignment, executionContext);
+		}
+
+		private TaskAssignmentSelectorWrapper(
+			TaskAssignmentSelector taskAssignmentSelector) {
+
+			_taskAssignmentSelector = taskAssignmentSelector;
+		}
+
+		private final TaskAssignmentSelector _taskAssignmentSelector;
+
+	}
 
 }

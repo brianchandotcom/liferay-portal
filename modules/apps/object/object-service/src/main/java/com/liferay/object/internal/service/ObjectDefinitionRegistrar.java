ea5bbd82e1a8b19641273e21c25960c815474fdc
shuyangzhou <shuyang.zhou@liferay.com>
2021-04-27 09:39:36 -0700

    LPS-130662 Async register ObjectDefinitions

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/service/ObjectDefinitionRegistrar.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/service/ObjectDefinitionRegistrar.java
@@ -14,9 +14,12 @@
 
 package com.liferay.object.internal.service;
 
+import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.model.ObjectField;
 import com.liferay.object.service.ObjectDefinitionLocalService;
 import com.liferay.object.service.ObjectFieldLocalService;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.dependency.manager.DependencyManagerSyncUtil;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
@@ -25,6 +28,7 @@ import com.liferay.portal.kernel.service.UserLocalService;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.FutureTask;
 
 import org.osgi.framework.BundleContext;
 import org.osgi.service.component.annotations.Activate;
@@ -41,7 +45,26 @@ public class ObjectDefinitionRegistrar {
 
 	@Activate
 	protected void activate(BundleContext bundleContext) throws Exception {
-		_objectDefinitionLocalService.registerObjectDefinitions();
+		List<ObjectDefinition> objectDefinitions =
+			_objectDefinitionLocalService.getObjectDefinitions(
+				QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+
+		if (!objectDefinitions.isEmpty()) {
+			FutureTask<Void> futureTask = new FutureTask<>(
+				() -> objectDefinitions.forEach(
+					_objectDefinitionLocalService::registerObjectDefinition),
+				null);
+
+			Thread objectDefinitionRegistrarThread = new Thread(
+				futureTask,
+				ObjectDefinitionRegistrar.class.getName() + "-Registrar");
+
+			objectDefinitionRegistrarThread.setDaemon(true);
+
+			objectDefinitionRegistrarThread.start();
+
+			DependencyManagerSyncUtil.registerSyncFuture(futureTask);
+		}
 
 		if (false) {
 			_addSampleObjectDefinition();

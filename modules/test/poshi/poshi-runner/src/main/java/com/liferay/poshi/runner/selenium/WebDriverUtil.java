d9414e79919c88e1f0396540872ba38a7b2b2aac
Kenji Heigel <kenji.heigel@liferay.com>
2021-05-14 20:26:25 -0700

    POSHI-169 Auto SF

--- modules/test/poshi/poshi-runner/src/main/java/com/liferay/poshi/runner/selenium/WebDriverUtil.java
+++ modules/test/poshi/poshi-runner/src/main/java/com/liferay/poshi/runner/selenium/WebDriverUtil.java
@@ -342,6 +342,7 @@ public class WebDriverUtil extends PropsValues {
 					UnexpectedAlertBehaviour.IGNORE);
 			}
 		};
+
 	private static final WebDriverUtil _webDriverUtil = new WebDriverUtil();
 
 	static {

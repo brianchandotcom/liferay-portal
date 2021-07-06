cbfbbc4a1f407b46bd94078a85410fcd116d525e
Julien Castelain <julien.castelain@liferay.com>
2021-07-06 15:28:57 +0200

    LPS-134549 Create the <clay:pagination-bar /> tag
    As requested in [LPS-134549](https://issues.liferay.com/browse/LPS-134549),
    we're adding the new `<clay:pagination-bar />` tag which renders
    a `ClayPaginationBarWithBasicItems` Clay component.

---
+++ modules/apps/frontend-taglib/frontend-taglib-clay/src/main/java/com/liferay/frontend/taglib/clay/servlet/taglib/PaginationBarTag.java
@@ -0,0 +1,318 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.taglib.clay.servlet.taglib;
+
+import com.liferay.frontend.taglib.clay.internal.servlet.taglib.BaseContainerTag;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.PaginationBarDelta;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.PaginationBarLabels;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.taglib.util.TagResourceBundleUtil;
+
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+
+/**
+ * @author Julien Castelain
+ */
+public class PaginationBarTag extends BaseContainerTag {
+
+	@Override
+	public int doStartTag() throws JspException {
+		setAttributeNamespace(_ATTRIBUTE_NAMESPACE);
+
+		PaginationBarLabels labels = getLabels();
+
+		if (labels == null) {
+			setLabels(
+				new PaginationBarLabels(
+					"Showing {0} to {1} of {2}", "{0} items", "{0} items"));
+		}
+
+		return super.doStartTag();
+	}
+
+	public Integer getActiveDelta() {
+		return _activeDelta;
+	}
+
+	public Integer getActivePage() {
+		return _activePage;
+	}
+
+	public List<PaginationBarDelta> getDeltas() {
+		return _deltas;
+	}
+
+	public List<Integer> getDisabledPages() {
+		return _disabledPages;
+	}
+
+	public Integer getEllipsisBuffer() {
+		return _ellipsisBuffer;
+	}
+
+	public PaginationBarLabels getLabels() {
+		return _labels;
+	}
+
+	public Integer getTotalItems() {
+		return _totalItems;
+	}
+
+	public void setActiveDelta(Integer activeDelta) {
+		_activeDelta = activeDelta;
+	}
+
+	public void setActivePage(Integer activePage) {
+		_activePage = activePage;
+	}
+
+	public void setDeltas(List<PaginationBarDelta> deltas) {
+		_deltas = deltas;
+	}
+
+	public void setDisabledPages(List<Integer> disabledPages) {
+		System.out.println("Disabled pages set");
+		_disabledPages = disabledPages;
+	}
+
+	public void setEllipsisBuffer(Integer ellipsisBuffer) {
+		_ellipsisBuffer = ellipsisBuffer;
+	}
+
+	public void setLabels(PaginationBarLabels labels) {
+		_labels = labels;
+	}
+
+	public void setTotalItems(Integer totalItems) {
+		_totalItems = totalItems;
+	}
+
+	@Override
+	protected void cleanUp() {
+		super.cleanUp();
+
+		_activeDelta = null;
+		_activePage = null;
+		_deltas = null;
+		_disabledPages = null;
+		_ellipsisBuffer = null;
+		_labels = null;
+		_totalItems = null;
+	}
+
+	@Override
+	protected String getHydratedModuleName() {
+		return "frontend-taglib-clay/PaginationBar";
+	}
+
+	@Override
+	protected Map<String, Object> prepareProps(Map<String, Object> props) {
+		props.put("activeDelta", _activeDelta);
+		props.put("activePage", _activePage);
+		props.put("deltas", _deltas);
+		props.put("disabledPages", _disabledPages);
+		props.put("ellipsisBuffer", _ellipsisBuffer);
+		props.put("labels", _labels);
+		props.put("totalItems", _totalItems);
+
+		return super.prepareProps(props);
+	}
+
+	@Override
+	protected int processStartTag() throws Exception {
+		super.processStartTag();
+
+		JspWriter jspWriter = pageContext.getOut();
+
+		jspWriter.write("<div class=\"pagination-bar\"><div class=\"");
+		jspWriter.write("dropdown pagination-items-per-page\">");
+
+		ResourceBundle resourceBundle = TagResourceBundleUtil.getResourceBundle(
+			pageContext);
+
+		Integer activeDelta = getActiveDelta();
+
+		if (activeDelta == null) {
+			PaginationBarDelta defaultDelta = getDeltas().get(0);
+
+			Integer label = (Integer)defaultDelta.get("label");
+
+			activeDelta = label;
+		}
+
+		jspWriter.write("<button class=\"dropdown-toggle btn btn-unstyled\"");
+		jspWriter.write(" type=\"button\">");
+		jspWriter.write(activeDelta.toString());
+		jspWriter.write(StringPool.SPACE);
+
+		jspWriter.write(
+			StringUtil.toLowerCase(LanguageUtil.get(resourceBundle, "items")));
+
+		IconTag iconTag = new IconTag();
+
+		iconTag.setSymbol("caret-double-l");
+
+		iconTag.doTag(pageContext);
+
+		if (_labels == null) {
+			_labels = new PaginationBarLabels(
+				"Showing {0} to {1} of {2}", "{0} items", "{0} items");
+		}
+
+		jspWriter.write("</button></div><div class=\"pagination-results\">");
+
+		jspWriter.write(LanguageUtil.get(resourceBundle, "showing"));
+		jspWriter.write(StringPool.SPACE);
+		jspWriter.write("1");
+		jspWriter.write(StringPool.SPACE);
+
+		jspWriter.write(
+			StringUtil.toLowerCase(LanguageUtil.get(resourceBundle, "to")));
+
+		jspWriter.write(StringPool.SPACE);
+		jspWriter.write(activeDelta.toString());
+		jspWriter.write(StringPool.SPACE);
+
+		jspWriter.write(
+			StringUtil.toLowerCase(LanguageUtil.get(resourceBundle, "of")));
+
+		jspWriter.write(StringPool.SPACE);
+		jspWriter.write(_totalItems.toString());
+		jspWriter.write(StringPool.SPACE);
+
+		jspWriter.write("</div><ul class=\"pagination pagination-root\">");
+		jspWriter.write("<li class=\"page-item disabled\">");
+
+		ButtonTag buttonTag = new ButtonTag();
+
+		buttonTag.setCssClass("page-link");
+		buttonTag.setDisplayType("unstyled");
+		buttonTag.setIcon("angle-left");
+
+		buttonTag.doTag(pageContext);
+
+		jspWriter.write("</li>");
+
+		int totalPages = (int)Math.ceil(_totalItems / activeDelta);
+
+		if (_activePage == null) {
+			_activePage = 1;
+		}
+
+		if (_ellipsisBuffer == null) {
+			_ellipsisBuffer = 2;
+		}
+
+		int ellipsisCount = _activePage + _ellipsisBuffer;
+
+		for (int i = 1; i < ellipsisCount; i++) {
+			jspWriter.write("<li class=\"page-item");
+
+			if (i == _activePage) {
+				jspWriter.write(" active");
+			}
+
+			if ((_disabledPages != null) &&
+				_disabledPages.contains(Integer.valueOf(i))) {
+
+				jspWriter.write(" disabled");
+			}
+
+			jspWriter.write("\">");
+
+			buttonTag = new ButtonTag();
+
+			buttonTag.setCssClass("page-link");
+			buttonTag.setDisplayType("unstyled");
+			buttonTag.setLabel(String.valueOf(i));
+
+			buttonTag.doTag(pageContext);
+
+			jspWriter.write("</li>");
+		}
+
+		jspWriter.write("<li class=\"dropdown page-item\">");
+
+		buttonTag = new ButtonTag();
+
+		buttonTag.setCssClass("dropdown-toggle page-link");
+		buttonTag.setDisplayType("unstyled");
+		buttonTag.setLabel("...");
+
+		buttonTag.doTag(pageContext);
+
+		jspWriter.write("</li>");
+
+		for (int i = ellipsisCount + 1; i < (totalPages + 1); i++) {
+			jspWriter.write("<li class=\"page-item");
+
+			if (i == _activePage) {
+				jspWriter.write(" active");
+			}
+
+			if ((_disabledPages != null) &&
+				_disabledPages.contains(Integer.valueOf(i))) {
+
+				jspWriter.write(" disabled");
+			}
+
+			jspWriter.write("\">");
+
+			buttonTag = new ButtonTag();
+
+			buttonTag.setCssClass("page-link");
+			buttonTag.setDisplayType("unstyled");
+
+			buttonTag.setLabel(String.valueOf(i));
+
+			buttonTag.doTag(pageContext);
+
+			jspWriter.write("</li>");
+		}
+
+		jspWriter.write("<li class=\"page-item\">");
+
+		buttonTag = new ButtonTag();
+
+		buttonTag.setCssClass("page-link");
+		buttonTag.setDisplayType("unstyled");
+		buttonTag.setIcon("angle-right");
+
+		buttonTag.doTag(pageContext);
+
+		jspWriter.write("</li></ul></div>");
+
+		return SKIP_BODY;
+	}
+
+	private static final String _ATTRIBUTE_NAMESPACE =
+		"clay:management-toolbar:";
+
+	private Integer _activeDelta;
+	private Integer _activePage;
+	private List<PaginationBarDelta> _deltas;
+	private List<Integer> _disabledPages;
+	private Integer _ellipsisBuffer;
+	private PaginationBarLabels _labels;
+	private Integer _totalItems;
+
+}

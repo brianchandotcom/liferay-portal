2360770dbd67fe87760f892dbe1d37c49c236ec9
Bryan Engler <bryan.engler@liferay.com>
2021-07-15 09:23:58 -0700

    LPS-133587 portal-search-tuning-rankings-service: Impl methods
    https://issues.liferay.com/browse/LPS-133587

--- modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-service/src/main/java/com/liferay/portal/search/tuning/rankings/service/impl/RankingLocalServiceImpl.java
+++ modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-service/src/main/java/com/liferay/portal/search/tuning/rankings/service/impl/RankingLocalServiceImpl.java
@@ -15,9 +15,22 @@
 package com.liferay.portal.search.tuning.rankings.service.impl;
 
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.search.model.uid.UIDFactory;
+import com.liferay.portal.search.tuning.rankings.model.Ranking;
 import com.liferay.portal.search.tuning.rankings.service.base.RankingLocalServiceBaseImpl;
 
+import java.util.List;
+import java.util.Map;
+
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * The implementation of the ranking local service.
@@ -38,9 +51,126 @@ import org.osgi.service.component.annotations.Component;
 )
 public class RankingLocalServiceImpl extends RankingLocalServiceBaseImpl {
 
-	/*
+	/**
 	 * NOTE FOR DEVELOPERS:
 	 *
 	 * Never reference this class directly. Use <code>com.liferay.portal.search.tuning.rankings.service.RankingLocalService</code> via injection or a <code>org.osgi.util.tracker.ServiceTracker</code> or use <code>com.liferay.portal.search.tuning.rankings.service.RankingLocalServiceUtil</code>.
 	 */
+	@Override
+	public Ranking addRanking(
+		List<String> aliases, List<String> hiddenDocumentIds, boolean inactive,
+		String indexName, String name, Map<Integer, String> documentIdsMap,
+		String queryString) {
+
+		Ranking ranking = rankingPersistence.create(
+			counterLocalService.increment());
+
+		JSONObject jsonObject = JSONUtil.put(
+			"indexName", indexName
+		).put(
+			"queryString", queryString
+		).put(
+			"rankingDocumentId", uidFactory.getUID(ranking)
+		);
+
+		ranking.setJson(jsonObject.toString());
+
+		return updateRanking(
+			ranking, aliases, hiddenDocumentIds, inactive, name,
+			documentIdsMap);
+	}
+
+	@Override
+	public Ranking addRanking(
+		String indexName, String name, String queryString) {
+
+		Ranking ranking = rankingPersistence.create(
+			counterLocalService.increment());
+
+		JSONObject jsonObject = JSONUtil.put(
+			"indexName", indexName
+		).put(
+			"name", name
+		).put(
+			"queryString", queryString
+		).put(
+			"rankingDocumentId", uidFactory.getUID(ranking)
+		);
+
+		ranking.setJson(jsonObject.toString());
+
+		return rankingPersistence.update(ranking);
+	}
+
+	@Override
+	public List<Ranking> getRankingsByCompanyId(long companyId) {
+		return rankingPersistence.findBycompanyId(companyId);
+	}
+
+	@Override
+	public Ranking updateRanking(
+			long rankingId, List<String> aliases,
+			List<String> hiddenDocumentIds, boolean inactive, String name,
+			Map<Integer, String> documentIdsMap)
+		throws PortalException {
+
+		return updateRanking(
+			fetchRanking(rankingId), aliases, hiddenDocumentIds, inactive, name,
+			documentIdsMap);
+	}
+
+	protected Ranking updateRanking(
+		Ranking ranking, List<String> aliases, List<String> hiddenDocumentIds,
+		boolean inactive, String name, Map<Integer, String> documentIdsMap) {
+
+		try {
+			JSONObject jsonObject = JSONFactoryUtil.createJSONObject(
+				ranking.getJson());
+
+			JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+
+			for (Map.Entry<Integer, String> entry : documentIdsMap.entrySet()) {
+				int position = entry.getKey();
+				String documentId = entry.getValue();
+
+				jsonArray.put(
+					JSONUtil.put(
+						"documentId", documentId
+					).put(
+						"position", position
+					));
+			}
+
+			jsonObject.put(
+				"aliases", JSONFactoryUtil.createJSONArray(aliases)
+			).put(
+				"hiddenDocumentIds",
+				JSONFactoryUtil.createJSONArray(hiddenDocumentIds)
+			).put(
+				"inactive", inactive
+			).put(
+				"name", name
+			).put(
+				"pins", jsonArray
+			);
+
+			ranking.setJson(jsonObject.toString());
+		}
+		catch (Exception exception) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(
+					"Unable to set json for Ranking with ID: " +
+						ranking.getRankingId());
+			}
+		}
+
+		return rankingPersistence.update(ranking);
+	}
+
+	@Reference
+	protected UIDFactory uidFactory;
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		RankingLocalServiceImpl.class);
+
 }

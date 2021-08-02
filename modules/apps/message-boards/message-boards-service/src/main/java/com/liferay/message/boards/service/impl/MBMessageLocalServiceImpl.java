d12e5e3bb13965fbdde109c36e1e5a6f905629c9
Javier de Arcos <javier.dearcos@liferay.com>
2021-08-02 10:16:42 +0200

    LPS-136580 Refactor MBMessageLocalServiceImpl to use the new method

--- modules/apps/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/service/impl/MBMessageLocalServiceImpl.java
+++ modules/apps/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/service/impl/MBMessageLocalServiceImpl.java
@@ -16,7 +16,6 @@ package com.liferay.message.boards.service.impl;
 
 import com.liferay.asset.kernel.model.AssetEntry;
 import com.liferay.asset.kernel.model.AssetLinkConstants;
-import com.liferay.asset.kernel.model.AssetTag;
 import com.liferay.comment.configuration.CommentGroupServiceConfiguration;
 import com.liferay.comment.constants.CommentConstants;
 import com.liferay.document.library.kernel.model.DLFileEntry;
@@ -2334,13 +2333,9 @@ public class MBMessageLocalServiceImpl extends MBMessageLocalServiceBaseImpl {
 			fromName, fromAddress, replyToAddress, emailAddress, fullName,
 			subjectLocalizedValuesMap, bodyLocalizedValuesMap, serviceContext);
 
-		AssetEntry assetEntry = assetEntryLocalService.getEntry(
-			MBMessage.class.getName(), message.getMessageId());
-
-		for (AssetTag assetTag : assetEntry.getTags()) {
-			subscriptionSender.addPersistedSubscribers(
-				AssetTag.class.getName(), assetTag.getTagId());
-		}
+		assetEntryLocalService.addAssetEntryAssetTagsPersistedSubscribers(
+			MBMessage.class.getName(), message.getMessageId(),
+			subscriptionSender);
 
 		subscriptionSender.addPersistedSubscribers(
 			MBCategory.class.getName(), message.getGroupId());

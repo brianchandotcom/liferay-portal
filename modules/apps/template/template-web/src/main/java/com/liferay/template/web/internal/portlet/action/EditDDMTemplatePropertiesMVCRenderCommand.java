a360fb89e66b8d19c92674a02c95d27ae32bf131
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2021-08-12 15:44:36 +0200

    LPS-137261 Separate property editing view based on the type

--- modules/apps/template/template-web/src/main/java/com/liferay/template/web/internal/portlet/action/EditDDMTemplatePropertiesMVCRenderCommand.java
+++ modules/apps/template/template-web/src/main/java/com/liferay/template/web/internal/portlet/action/EditDDMTemplatePropertiesMVCRenderCommand.java
@@ -81,9 +81,11 @@ public class EditDDMTemplatePropertiesMVCRenderCommand
 				new WidgetTemplatesEditDDMTemplateDisplayContext(
 					_portal.getLiferayPortletRequest(renderRequest),
 					_portal.getLiferayPortletResponse(renderResponse)));
+
+			return "/ddm_template/edit_widget_template_properties.jsp";
 		}
 
-		return "/ddm_template/edit_properties.jsp";
+		return "/ddm_template/edit_information_template_properties.jsp";
 	}
 
 	@Activate

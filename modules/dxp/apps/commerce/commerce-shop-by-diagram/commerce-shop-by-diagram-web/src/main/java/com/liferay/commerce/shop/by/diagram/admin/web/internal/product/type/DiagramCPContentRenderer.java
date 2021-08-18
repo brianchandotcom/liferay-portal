2100069590fefecddd8c753498f80d56ae1e1f65
Alessio Rendina <asilcomando@gmail.com>
2021-08-18 18:27:20 +0200

    COMMERCE-7065 Added diagram commerce product content renderer

---
+++ modules/dxp/apps/commerce/commerce-shop-by-diagram/commerce-shop-by-diagram-web/src/main/java/com/liferay/commerce/shop/by/diagram/admin/web/internal/product/type/DiagramCPContentRenderer.java
@@ -0,0 +1,88 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.commerce.shop.by.diagram.admin.web.internal.product.type;
+
+import com.liferay.commerce.product.catalog.CPCatalogEntry;
+import com.liferay.commerce.product.content.render.CPContentRenderer;
+import com.liferay.commerce.shop.by.diagram.constants.DiagramCPTypeConstants;
+import com.liferay.commerce.shop.by.diagram.util.DiagramCPTypeHelper;
+import com.liferay.frontend.taglib.servlet.taglib.util.JSPRenderer;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(
+	enabled = false, immediate = true,
+	property = {
+		"commerce.product.content.renderer.key=" + DiagramCPTypeConstants.NAME,
+		"commerce.product.content.renderer.order=" + Integer.MIN_VALUE,
+		"commerce.product.content.renderer.type=" + DiagramCPTypeConstants.NAME
+	},
+	service = CPContentRenderer.class
+)
+public class DiagramCPContentRenderer implements CPContentRenderer {
+
+	@Override
+	public String getKey() {
+		return DiagramCPTypeConstants.NAME;
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			"content.Language", locale, getClass());
+
+		return LanguageUtil.get(resourceBundle, DiagramCPTypeConstants.NAME);
+	}
+
+	@Override
+	public void render(
+			CPCatalogEntry cpCatalogEntry,
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws Exception {
+
+		httpServletRequest.setAttribute(
+			"diagramCPTypeHelper", _diagramCPTypeHelper);
+
+		_jspRenderer.renderJSP(
+			_servletContext, httpServletRequest, httpServletResponse,
+			"/render/view.jsp");
+	}
+
+	@Reference
+	private DiagramCPTypeHelper _diagramCPTypeHelper;
+
+	@Reference
+	private JSPRenderer _jspRenderer;
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.commerce.shop.by.diagram.web)"
+	)
+	private ServletContext _servletContext;
+
+}

9635ae36904771aaff8638c542c5c61ff0918063
Danny Situ <danny.situ@liferay.com>
2021-08-19 10:37:33 -0700

    COMMERCE-7002 Remove userId from CommercePriceModifier RemoteServices

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-pricing-impl/src/main/java/com/liferay/headless/commerce/admin/pricing/internal/resource/v2_0/PriceModifierResourceImpl.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-pricing-impl/src/main/java/com/liferay/headless/commerce/admin/pricing/internal/resource/v2_0/PriceModifierResourceImpl.java
@@ -252,7 +252,6 @@ public class PriceModifierResourceImpl extends BasePriceModifierResourceImpl {
 		CommercePriceModifier commercePriceModifier =
 			_commercePriceModifierService.addOrUpdateCommercePriceModifier(
 				priceModifier.getExternalReferenceCode(),
-				serviceContext.getUserId(),
 				GetterUtil.getLong(priceModifier.getId()),
 				commercePriceList.getGroupId(), priceModifier.getTitle(),
 				priceModifier.getTarget(),

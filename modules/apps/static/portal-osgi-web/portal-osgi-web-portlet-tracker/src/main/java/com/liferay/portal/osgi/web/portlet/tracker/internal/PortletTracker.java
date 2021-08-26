brianchandotcom/liferay-portal

commit db6c7960f4f3e571b0d13fba443a70a69849bb37

LPS-135731 portal-osgi-web-portlet-tracker: Manually handle deploying a portlet per company

modules/apps/static/portal-osgi-web/portal-osgi-web-portlet-tracker/src/main/java/com/liferay/portal/osgi/web/portlet/tracker/internal/PortletTracker.java
@@ -28,6 +28,7 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.ResourceActionsException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.CompanyConstants;
 import com.liferay.portal.kernel.model.EventDefinition;
 import com.liferay.portal.kernel.model.PortletApp;
@@ -1274,8 +1275,15 @@ public class PortletTracker
 			categoryNames.add("category.undefined");
 		}
 
-		_portletLocalService.deployRemotePortlet(
-			portletModel, ArrayUtil.toStringArray(categoryNames), false);
+		List<Company> companies = _companyLocalService.getCompanies(false);
+
+		_portletLocalService.clearCache();
+
+		for (Company company : companies) {
+			_portletLocalService.deployRemotePortlet(
+				portletModel, ArrayUtil.toStringArray(categoryNames), false,
+				false, new long[] {company.getCompanyId()});
+		}
 	}
 
 	protected Object get(

commit c3fd46c2d9797ac5be50690d54fd1da99e1774a4

LPS-136269 Cache clearing must happen after the deployment, otherwise we leave open a window of time that a request to the target portlet is coming before the deployment, causing us to miss the portlet in cache which in turn translated to undeployed portlet. Caused by db6c7960

modules/apps/static/portal-osgi-web/portal-osgi-web-portlet-tracker/src/main/java/com/liferay/portal/osgi/web/portlet/tracker/internal/PortletTracker.java
@@ -1327,8 +1327,6 @@ public class PortletTracker
 
 		List<Company> companies = _companyLocalService.getCompanies(false);
 
-		_portletLocalService.clearCache();
-
 		for (Company company : companies) {
 			futures.add(
 				_executorService.submit(
@@ -1354,6 +1352,8 @@ public class PortletTracker
 				throw new PortalException(exception);
 			}
 		}
+
+		_portletLocalService.clearCache();
 	}
 
 	protected Object get(
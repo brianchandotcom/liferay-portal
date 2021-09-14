71f98fc4b94254fc37061af92af42e4028236938
Gabriel Albuquerque <gabriel.albuquerque@liferay.com>
2021-09-14 00:43:27 -0300

    LPS-139284 buildService

--- modules/apps/list-type/list-type-service/src/main/java/com/liferay/list/type/service/base/ListTypeDefinitionLocalServiceBaseImpl.java
+++ modules/apps/list-type/list-type-service/src/main/java/com/liferay/list/type/service/base/ListTypeDefinitionLocalServiceBaseImpl.java
@@ -593,4 +593,8 @@ public abstract class ListTypeDefinitionLocalServiceBaseImpl
 	protected com.liferay.counter.kernel.service.CounterLocalService
 		counterLocalService;
 
+	@Reference
+	protected com.liferay.portal.kernel.service.ResourceLocalService
+		resourceLocalService;
+
 }

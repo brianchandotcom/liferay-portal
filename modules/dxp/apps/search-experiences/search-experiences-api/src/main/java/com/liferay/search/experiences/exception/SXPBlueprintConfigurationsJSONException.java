da42aba8928a66c210b38442f4a7f820b3315ef3
Hugo Huijser <hugo.huijser@liferay.com>
2021-09-16 07:52:17 -0600

    LPS-124256 SF

--- modules/dxp/apps/search-experiences/search-experiences-api/src/main/java/com/liferay/search/experiences/exception/SXPBlueprintConfigurationsJSONException.java
+++ modules/dxp/apps/search-experiences/search-experiences-api/src/main/java/com/liferay/search/experiences/exception/SXPBlueprintConfigurationsJSONException.java
@@ -11,6 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
+
 package com.liferay.search.experiences.exception;
 
 import com.liferay.portal.kernel.exception.PortalException;
@@ -27,7 +28,9 @@ public class SXPBlueprintConfigurationsJSONException extends PortalException {
 		super(msg);
 	}
 
-	public SXPBlueprintConfigurationsJSONException(String msg, Throwable throwable) {
+	public SXPBlueprintConfigurationsJSONException(
+		String msg, Throwable throwable) {
+
 		super(msg, throwable);
 	}
 
--- modules/dxp/apps/search-experiences/search-experiences-api/src/main/java/com/liferay/search/experiences/exception/SXPElementElementDefinitionJSONException.java
+++ modules/dxp/apps/search-experiences/search-experiences-api/src/main/java/com/liferay/search/experiences/exception/SXPElementElementDefinitionJSONException.java
@@ -11,6 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
+
 package com.liferay.search.experiences.exception;
 
 import com.liferay.portal.kernel.exception.PortalException;
@@ -27,7 +28,9 @@ public class SXPElementElementDefinitionJSONException extends PortalException {
 		super(msg);
 	}
 
-	public SXPElementElementDefinitionJSONException(String msg, Throwable throwable) {
+	public SXPElementElementDefinitionJSONException(
+		String msg, Throwable throwable) {
+
 		super(msg, throwable);
 	}
 

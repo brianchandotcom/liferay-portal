bd4bd210cc36d66ebfd477c8ea37cc6f2d5cd75e
Petteri Karttunen <petteri.karttunen@liferay.com>
2021-09-20 22:57:50 +0300

    LPS-112856 search-experiences-service: SearchContextUtil SF

--- modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/blueprint/util/SearchContextUtil.java
+++ modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/blueprint/util/SearchContextUtil.java
@@ -33,190 +33,167 @@ import java.util.stream.LongStream;
 public class SearchContextUtil {
 
 	public static Boolean getBooleanAttribute(
-			String attributeName, 
-			SearchRequestBuilder searchRequestBuilder) {
+		String attributeName, SearchRequestBuilder searchRequestBuilder) {
 
-		Object obj = _getAttribute(attributeName, searchRequestBuilder);
-		
-		if (Objects.isNull(obj)) {
+		Object object = _getAttributeObject(
+			attributeName, searchRequestBuilder);
+
+		if (Objects.isNull(object)) {
 			return null;
 		}
-		
+
 		return searchRequestBuilder.withSearchContextGet(
-				searchContext -> 
-					GetterUtil.getBoolean(obj));
-		}
+			searchContext -> GetterUtil.getBoolean(object));
+	}
 
 	public static long getCompanyId(SearchRequestBuilder searchRequestBuilder) {
 		return searchRequestBuilder.withSearchContextGet(
-				searchContext -> {
-					return searchContext.getCompanyId();
-				});
+			searchContext -> searchContext.getCompanyId());
 	}
 
 	public static Double getDoubleAttribute(
-			String attributeName, 
-			SearchRequestBuilder searchRequestBuilder) {
+		String attributeName, SearchRequestBuilder searchRequestBuilder) {
 
-		Object obj = _getAttribute(attributeName, searchRequestBuilder);
-		
-		if (Objects.isNull(obj)) {
+		Object object = _getAttributeObject(
+			attributeName, searchRequestBuilder);
+
+		if (Objects.isNull(object)) {
 			return null;
 		}
-		
+
 		return searchRequestBuilder.withSearchContextGet(
-				searchContext -> 
-					GetterUtil.getDouble(obj));
-		}
-	
+			searchContext -> GetterUtil.getDouble(object));
+	}
+
 	public static Float getFloatAttribute(
-			String attributeName, 
-			SearchRequestBuilder searchRequestBuilder) {
+		String attributeName, SearchRequestBuilder searchRequestBuilder) {
 
-		Object obj = _getAttribute(attributeName, searchRequestBuilder);
-		
-		if (Objects.isNull(obj)) {
+		Object object = _getAttributeObject(
+			attributeName, searchRequestBuilder);
+
+		if (Objects.isNull(object)) {
 			return null;
 		}
-		
+
 		return searchRequestBuilder.withSearchContextGet(
-				searchContext -> 
-					GetterUtil.getFloat(obj));
-		}
-	
-	public static Integer[] getIntegerArrayAttribute(String attributeName, SearchRequestBuilder searchRequestBuilder) {
+			searchContext -> GetterUtil.getFloat(object));
+	}
 
-		Object obj = _getAttribute(attributeName, searchRequestBuilder);
-		
-		if (Objects.isNull(obj)) {
+	public static Integer[] getIntegerArrayAttribute(
+		String attributeName, SearchRequestBuilder searchRequestBuilder) {
+
+		Object object = _getAttributeObject(
+			attributeName, searchRequestBuilder);
+
+		if (Objects.isNull(object)) {
 			return null;
 		}
-		
-		if (obj instanceof int[]) {
-			int[] arr = (int[])obj;
+
+		if (object instanceof int[]) {
+			int[] arr = (int[])object;
 
 			if (arr.length > 0) {
 				IntStream intStream = Arrays.stream(arr);
 
-				Integer[] boxedArray = intStream.boxed(
+				return intStream.boxed(
 				).toArray(
 					Integer[]::new
 				);
-
-				return boxedArray;
 			}
-		} else if (obj instanceof Integer[]) {
-			Integer[] arr = (Integer[])obj;
-
-			return  arr;
+		}
+		else if (object instanceof Integer[]) {
+			return (Integer[])object;
 		}
 
 		return null;
 	}
 
 	public static Integer getIntegerAttribute(
-			String attributeName, 
-			SearchRequestBuilder searchRequestBuilder) {
+		String attributeName, SearchRequestBuilder searchRequestBuilder) {
 
-		Object obj = _getAttribute(attributeName, searchRequestBuilder);
-		
-		if (Objects.isNull(obj)) {
+		Object object = _getAttributeObject(
+			attributeName, searchRequestBuilder);
+
+		if (Objects.isNull(object)) {
 			return null;
 		}
 
 		return searchRequestBuilder.withSearchContextGet(
-				searchContext -> 
-					 GetterUtil.getInteger(obj));
-		}
-	
+			searchContext -> GetterUtil.getInteger(object));
+	}
 
-	
-				
-	public static String getKeywords( 
-			SearchRequestBuilder searchRequestBuilder) {
+	public static String getKeywords(
+		SearchRequestBuilder searchRequestBuilder) {
 
 		return searchRequestBuilder.withSearchContextGet(
-				searchContext -> {
-					return searchContext.getKeywords();
-				});
+			searchContext -> searchContext.getKeywords());
 	}
 
 	public static Layout getLayout(SearchRequestBuilder searchRequestBuilder) {
-		
 		return searchRequestBuilder.withSearchContextGet(
-				searchContext -> {
-					return searchContext.getLayout();
-				});
-
+			searchContext -> searchContext.getLayout());
 	}
-	
-	public static Locale getLocale(
-			SearchRequestBuilder searchRequestBuilder) {
 
+	public static Locale getLocale(SearchRequestBuilder searchRequestBuilder) {
 		return searchRequestBuilder.withSearchContextGet(
-				searchContext -> 
-					searchContext.getLocale());
-		}
-	
+			searchContext -> searchContext.getLocale());
+	}
 
+	public static Long[] getLongArrayAttribute(
+		String attributeName, SearchRequestBuilder searchRequestBuilder) {
 
-	public static Long[] getLongArrayAttribute(String attributeName, SearchRequestBuilder searchRequestBuilder) {
+		Object object = _getAttributeObject(
+			attributeName, searchRequestBuilder);
 
-		Object obj = _getAttribute(attributeName, searchRequestBuilder);
-		
-		if (Objects.isNull(obj)) {
+		if (Objects.isNull(object)) {
 			return null;
 		}
 
-		if (obj instanceof long[]) {
-			long[] arr = (long[])obj;
+		if (object instanceof long[]) {
+			long[] arr = (long[])object;
 
 			if (arr.length > 0) {
 				LongStream longStream = Arrays.stream(arr);
 
-				Long[] boxedArray = longStream.boxed(
+				return longStream.boxed(
 				).toArray(
 					Long[]::new
 				);
-
-				return boxedArray;
 			}
-		} else if (obj instanceof Long[]) {
-			Long[] arr = (Long[])obj;
-
-			return  arr;
+		}
+		else if (object instanceof Long[]) {
+			return (Long[])object;
 		}
 
 		return null;
 	}
-	
+
 	public static Long getLongAttribute(
-			String attributeName, 
-			SearchRequestBuilder searchRequestBuilder) {
+		String attributeName, SearchRequestBuilder searchRequestBuilder) {
 
-		Object obj = _getAttribute(attributeName, searchRequestBuilder);
-		
-		if (Objects.isNull(obj)) {
+		Object object = _getAttributeObject(
+			attributeName, searchRequestBuilder);
+
+		if (Objects.isNull(object)) {
 			return null;
 		}
-		
+
 		return searchRequestBuilder.withSearchContextGet(
-				searchContext -> 
-					GetterUtil.getLong(obj));
-		}
-		
-	public static String[] getStringArrayAttribute(
-			String attributeName, 
-			SearchRequestBuilder searchRequestBuilder) {
+			searchContext -> GetterUtil.getLong(object));
+	}
 
-		Object obj = _getAttribute(attributeName, searchRequestBuilder);
-		
-		if (Objects.isNull(obj)) {
+	public static String[] getStringArrayAttribute(
+		String attributeName, SearchRequestBuilder searchRequestBuilder) {
+
+		Object object = _getAttributeObject(
+			attributeName, searchRequestBuilder);
+
+		if (Objects.isNull(object)) {
 			return null;
 		}
 
-		if (obj  instanceof String[]) {
-			String[] arr = (String[])obj;
+		if (object instanceof String[]) {
+			String[] arr = (String[])object;
 
 			if (arr.length > 0) {
 				return arr;
@@ -225,56 +202,46 @@ public class SearchContextUtil {
 
 		return null;
 	}
-	
-	public static String getStringAttribute(String attributeName, 
-			SearchRequestBuilder searchRequestBuilder) {
+
+	public static String getStringAttribute(
+		String attributeName, SearchRequestBuilder searchRequestBuilder) {
 
 		return searchRequestBuilder.withSearchContextGet(
-				searchContext -> {
-					
-					String attribute = GetterUtil.getString(
-							searchContext.getAttribute(attributeName));
+			searchContext -> {
+				String attribute = GetterUtil.getString(
+					searchContext.getAttribute(attributeName));
 
-					if (Validator.isBlank(attribute)) {
-						return null;
-					}
+				if (Validator.isBlank(attribute)) {
+					return null;
+				}
 
-					attribute = StringUtil.trim(attribute);
+				attribute = StringUtil.trim(attribute);
 
-					if (Validator.isBlank(attribute)) {
-						return null;
-					}
+				if (Validator.isBlank(attribute)) {
+					return null;
+				}
 
-					return attribute;
-				});
+				return attribute;
+			});
 	}
-			
-	
-	public static TimeZone getTimeZone( 
-			SearchRequestBuilder searchRequestBuilder) {
+
+	public static TimeZone getTimeZone(
+		SearchRequestBuilder searchRequestBuilder) {
 
 		return searchRequestBuilder.withSearchContextGet(
-				searchContext -> {
-					return searchContext.getTimeZone();
-				});
-	}
-	
-	
-	public static long getUserId( 
-			SearchRequestBuilder searchRequestBuilder) {
-
-		return searchRequestBuilder.withSearchContextGet(
-				searchContext -> {
-					return searchContext.getUserId();
-				});
-	}
-	
-	private static Object _getAttribute(String attributeName, 
-			SearchRequestBuilder searchRequestBuilder) {
-		
-		return searchRequestBuilder.withSearchContextGet(
-				searchContext -> searchContext.getAttribute(attributeName));
+			searchContext -> searchContext.getTimeZone());
 	}
 
+	public static long getUserId(SearchRequestBuilder searchRequestBuilder) {
+		return searchRequestBuilder.withSearchContextGet(
+			searchContext -> searchContext.getUserId());
+	}
+
+	private static Object _getAttributeObject(
+		String attributeName, SearchRequestBuilder searchRequestBuilder) {
+
+		return searchRequestBuilder.withSearchContextGet(
+			searchContext -> searchContext.getAttribute(attributeName));
+	}
 
 }

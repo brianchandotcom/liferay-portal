e87c166c393b73c1cd10f63778dfd4214e8479fa
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2021-09-21 12:12:19 +0200

    LPS-139524 Swap className/classPK when translating a page experience

--- modules/apps/translation/translation-web/src/main/java/com/liferay/translation/web/internal/display/context/TranslateDisplayContext.java
+++ modules/apps/translation/translation-web/src/main/java/com/liferay/translation/web/internal/display/context/TranslateDisplayContext.java
@@ -86,7 +86,7 @@ public class TranslateDisplayContext {
 			ffLayoutExperienceSelectorConfiguration,
 		InfoForm infoForm, LiferayPortletRequest liferayPortletRequest,
 		LiferayPortletResponse liferayPortletResponse, Object object,
-		String segmentsExperienceId,
+		long segmentsExperienceId,
 		InfoItemFieldValues sourceInfoItemFieldValues, String sourceLanguageId,
 		InfoItemFieldValues targetInfoItemFieldValues, String targetLanguageId,
 		TranslationInfoFieldChecker translationInfoFieldChecker) {
@@ -497,8 +497,7 @@ public class TranslateDisplayContext {
 			SegmentsExperienceConstants.getDefaultSegmentsExperienceName(
 				_themeDisplay.getLocale())
 		).put(
-			"value",
-			String.valueOf(SegmentsExperienceConstants.ID_DEFAULT)
+			"value", String.valueOf(SegmentsExperienceConstants.ID_DEFAULT)
 		).build();
 
 		List<Map<String, String>> options = new ArrayList<>();
@@ -603,7 +602,7 @@ public class TranslateDisplayContext {
 	private final InfoForm _infoForm;
 	private final LiferayPortletResponse _liferayPortletResponse;
 	private final Object _object;
-	private final String _segmentsExperienceId;
+	private final long _segmentsExperienceId;
 	private final InfoItemFieldValues _sourceInfoItemFieldValues;
 	private final String _sourceLanguageId;
 	private final Locale _sourceLocale;

8a8405c136f7fbd652ab45df7fd6f8c84bd0db54
Petteri Karttunen <petteri.karttunen@liferay.com>
2021-09-22 11:32:08 +0300

    LPS-112856: Add commonly used utility methods to ProblemUtil

--- modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/problem/ProblemUtil.java
+++ modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/problem/ProblemUtil.java
@@ -15,11 +15,13 @@
 package com.liferay.search.experiences.internal.problem;
 
 import com.liferay.petra.lang.CentralizedThreadLocal;
+import com.liferay.petra.string.StringBundler;
 import com.liferay.search.experiences.problem.Problem;
 import com.liferay.search.experiences.problem.Severity;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 /**
  * @author Petteri Karttunen
@@ -27,7 +29,90 @@ import java.util.List;
  */
 public class ProblemUtil {
 
+	public static void addError(
+		String className, String languageKey, Object rootObject,
+		String rootProperty, String rootValue, Throwable throwable) {
+
+		_addProblem(
+			className, languageKey, _getExceptionMessage(throwable, className),
+			rootObject, rootProperty, rootValue, Severity.ERROR, throwable);
+	}
+
+	public static void addInvalidConfigurationValueError(
+		String className, Object rootObject, String rootProperty,
+		String rootValue, Throwable throwable) {
+
+		_addProblem(
+			className, "invalid-configuration-value",
+			_getExceptionMessage(throwable, className), rootObject,
+			rootProperty, rootValue, Severity.ERROR, throwable);
+	}
+
+	public static void addInvalidConfigurationValueTypeError(
+		String className, String expectedType, Object rootObject,
+		String rootProperty, String rootValue) {
+
+		StringBundler sb = new StringBundler(5);
+
+		sb.append("[ ");
+		sb.append(rootProperty);
+		sb.append(" ] has to be of type [ ");
+		sb.append(expectedType);
+		sb.append(" ] ");
+
+		_addProblem(
+			className, "invalid-configuration-value-type", sb.toString(),
+			rootObject, rootProperty, rootValue, Severity.ERROR, null);
+	}
+
 	public static void addProblem(Problem problem) {
+		List<Problem> problems = _threadLocal.get();
+
+		problems.add(problem);
+	}
+
+	public static void addRequiredPropertyMissingError(
+		String className, Object rootObject, String rootProperty) {
+
+		_addProblem(
+			className, "a-required-field-is-missing",
+			"[ " + rootProperty + " ] must be defined", rootObject,
+			rootProperty, null, Severity.ERROR, null);
+	}
+
+	public static void addUnknownError(
+		String className, Object rootObject, String rootProperty,
+		String rootValue, Throwable throwable) {
+
+		_addProblem(
+			className, "there-was-an-unknown-error",
+			_getExceptionMessage(throwable, className), rootObject,
+			rootProperty, rootValue, Severity.ERROR, throwable);
+	}
+
+	public static void addUnknownError(String className, Throwable throwable) {
+		_addProblem(
+			className, "there-was-an-unknown-error",
+			_getExceptionMessage(throwable, className), null, null, null,
+			Severity.ERROR, throwable);
+	}
+
+	public static void addWarning(
+		String className, String languageKey, Object rootObject,
+		String rootProperty, String rootValue) {
+
+		_addProblem(
+			className, languageKey, null, rootObject, rootProperty, rootValue,
+			Severity.WARN, null);
+	}
+
+	public static void addWarning(
+		String className, String languageKey, Object rootObject,
+		String rootProperty, String rootValue, Throwable throwable) {
+
+		_addProblem(
+			className, languageKey, _getExceptionMessage(throwable, className),
+			rootObject, rootProperty, rootValue, Severity.WARN, throwable);
 	}
 
 	public static List<Problem> getProblems() {
@@ -39,7 +124,50 @@ public class ProblemUtil {
 	}
 
 	public static boolean hasErrors() {
-		return false;
+		List<Problem> problems = _threadLocal.get();
+
+		Stream<Problem> stream = problems.stream();
+
+		return stream.anyMatch(
+			m -> m.getSeverity(
+			).equals(
+				Severity.ERROR
+			));
+	}
+
+	private static void _addProblem(
+		String className, String languageKey, String message, Object rootObject,
+		String rootProperty, String rootValue, Severity severity,
+		Throwable throwable) {
+
+		addProblem(
+			new Problem.Builder().className(
+				className
+			).languageKey(
+				languageKey
+			).message(
+				message
+			).rootObject(
+				rootObject
+			).rootProperty(
+				rootProperty
+			).rootValue(
+				rootValue
+			).throwable(
+				throwable
+			).severity(
+				severity
+			).build());
+	}
+
+	private static String _getExceptionMessage(
+		Throwable throwable, String className) {
+
+		if (throwable != null) {
+			return throwable.getMessage();
+		}
+
+		return className + " reported an error";
 	}
 
 	private static final ThreadLocal<List<Problem>> _threadLocal =

a1cebfe896a188053636db3a396aa12e5fd04f2f
drewbrokke <drew.brokke@liferay.com>
2021-10-08 10:56:23 -0500

    LPS-140403 terms-of-use-web: adds DynamicInclude and UI to render the modal and submit a user's confirmation

---
+++ modules/apps/terms-of-use/terms-of-use-web/src/main/java/com/liferay/terms/of/use/web/internal/servlet/taglib/TermsOfUseJSPDynamicInclude.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.terms.of.use.web.internal.servlet.taglib;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.servlet.taglib.BaseJSPDynamicInclude;
+import com.liferay.portal.kernel.servlet.taglib.DynamicInclude;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.terms.of.use.web.internal.constants.TermsOfUseWebKeys;
+import com.liferay.terms.of.use.web.internal.manager.TermsOfUseManager;
+
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Drew Brokke
+ */
+@Component(immediate = true, service = DynamicInclude.class)
+public class TermsOfUseJSPDynamicInclude extends BaseJSPDynamicInclude {
+
+	@Override
+	public void include(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse, String key)
+		throws IOException {
+
+		if (_termsOfUseManager.isShowTermsOfUse(
+				_portal.getUserId(httpServletRequest))) {
+
+			httpServletRequest.setAttribute(
+				TermsOfUseWebKeys.MODAL_BODY_HTML,
+				_termsOfUseManager.getBodyHTML(
+					_portal.getLocale(httpServletRequest)));
+
+			super.include(httpServletRequest, httpServletResponse, key);
+		}
+	}
+
+	@Override
+	public void register(DynamicIncludeRegistry dynamicIncludeRegistry) {
+		dynamicIncludeRegistry.register("/html/common/themes/bottom.jsp#post");
+	}
+
+	@Override
+	protected String getJspPath() {
+		return "/dynamic_include/terms_of_use.jsp";
+	}
+
+	@Override
+	protected Log getLog() {
+		return _log;
+	}
+
+	@Override
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.terms.of.use.web)",
+		unbind = "-"
+	)
+	protected void setServletContext(ServletContext servletContext) {
+		super.setServletContext(servletContext);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		TermsOfUseJSPDynamicInclude.class);
+
+	@Reference
+	private Portal _portal;
+
+	@Reference
+	private TermsOfUseManager _termsOfUseManager;
+
+}

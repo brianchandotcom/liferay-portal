717fdade0d042526715d09ed55b737d4bcd8dd6e
Bryan Engler <bryan.engler@liferay.com>
2021-10-26 11:13:47 -0700

    LPS-141490 search-experiences-rest-impl: Implement methods
    https://issues.liferay.com/browse/LPS-141490

--- modules/dxp/apps/search-experiences/search-experiences-rest-impl/src/main/java/com/liferay/search/experiences/rest/internal/resource/v1_0/SXPAdminUtilResourceImpl.java
+++ modules/dxp/apps/search-experiences/search-experiences-rest-impl/src/main/java/com/liferay/search/experiences/rest/internal/resource/v1_0/SXPAdminUtilResourceImpl.java
@@ -14,9 +14,29 @@
 
 package com.liferay.search.experiences.rest.internal.resource.v1_0;
 
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.security.permission.ResourceActionsUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.search.asset.SearchableAssetClassNamesProvider;
+import com.liferay.portal.search.spi.model.query.contributor.KeywordQueryContributor;
+import com.liferay.portal.search.spi.model.query.contributor.ModelPreFilterContributor;
+import com.liferay.portal.search.spi.model.query.contributor.QueryPreFilterContributor;
 import com.liferay.search.experiences.rest.resource.v1_0.SXPAdminUtilResource;
 
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceReference;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -27,4 +47,109 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = SXPAdminUtilResource.class
 )
 public class SXPAdminUtilResourceImpl extends BaseSXPAdminUtilResourceImpl {
+
+	@Override
+	public String getSXPAdminUtilKeywordQueryContributors() throws Exception {
+		return _getComponents(KeywordQueryContributor.class.getName());
+	}
+
+	@Override
+	public String getSXPAdminUtilModelPrefilterContributors() throws Exception {
+		return _getComponents(ModelPreFilterContributor.class.getName());
+	}
+
+	@Override
+	public String getSXPAdminUtilQueryPrefilterContributors() throws Exception {
+		return _getComponents(QueryPreFilterContributor.class.getName());
+	}
+
+	@Override
+	public String getSXPAdminUtilSearchableAssetNameCompany(Long companyId)
+		throws Exception {
+
+		JSONArray jsonArray = JSONUtil.putAll(
+			_searchableAssetClassNamesProvider.getClassNames(companyId));
+
+		return jsonArray.toJSONString();
+	}
+
+	@Override
+	public String getSXPAdminUtilSearchableAssetNameCompanyLanguage(
+		Long companyId, String languageId) {
+
+		JSONArray jsonArray = _jsonFactory.createJSONArray();
+
+		String[] classNames = _searchableAssetClassNamesProvider.getClassNames(
+			companyId);
+
+		for (String className : classNames) {
+			jsonArray.put(
+				JSONUtil.put(
+					"className", className
+				).put(
+					"displayName", _getDisplayName(className, languageId)
+				));
+		}
+
+		return jsonArray.toJSONString();
+	}
+
+	private BundleContext _getBundleContext() {
+		Bundle bundle = FrameworkUtil.getBundle(SXPAdminUtilResourceImpl.class);
+
+		return bundle.getBundleContext();
+	}
+
+	private String _getComponents(String className) {
+		BundleContext bundleContext = _getBundleContext();
+
+		JSONArray jsonArray = _jsonFactory.createJSONArray();
+
+		try {
+			ServiceReference<?>[] references =
+				bundleContext.getAllServiceReferences(className, null);
+
+			for (ServiceReference<?> serviceReference : references) {
+				jsonArray.put(
+					(String)serviceReference.getProperty("component.name"));
+			}
+		}
+		catch (InvalidSyntaxException invalidSyntaxException) {
+			_log.error(
+				invalidSyntaxException.getMessage(), invalidSyntaxException);
+		}
+
+		return jsonArray.toJSONString();
+	}
+
+	private String _getDisplayName(String className, String languageId) {
+		String modelResource = ResourceActionsUtil.getModelResource(
+			LocaleUtil.fromLanguageId(languageId), className);
+
+		if (className.startsWith(ObjectDefinition.class.getName() + "#")) {
+			String[] parts = StringUtil.split(className, "#");
+
+			ObjectDefinition objectDefinition =
+				_objectDefinitionLocalService.fetchObjectDefinition(
+					Long.valueOf(parts[1]));
+
+			modelResource = objectDefinition.getLabel(languageId);
+		}
+
+		return modelResource;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		SXPAdminUtilResourceImpl.class);
+
+	@Reference
+	private JSONFactory _jsonFactory;
+
+	@Reference
+	private ObjectDefinitionLocalService _objectDefinitionLocalService;
+
+	@Reference
+	private SearchableAssetClassNamesProvider
+		_searchableAssetClassNamesProvider;
+
 }

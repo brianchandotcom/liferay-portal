a02ef45cb3c7ac4e87c20cd2cfeef47b71cab53c
Keven Leone <keven.leone@liferay.com>
2021-10-27 14:13:46 -0300

    LPS-140634 Update bnd.bnd

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/onboarding/pages/welcome.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/onboarding/pages/welcome.js
@@ -37,8 +37,8 @@ const Welcome = () => {
 			/>
 
 			<p className="mb-0 px-1 text-center text-neutral-2">
-				Let's download your DXP activation keys, add any team members to
-				your projects and give you a quick tour of the space.
+				Let&apos;s download your DXP activation keys, add any team
+				members to your projects and give you a quick tour of the space.
 			</p>
 		</Layout>
 	);

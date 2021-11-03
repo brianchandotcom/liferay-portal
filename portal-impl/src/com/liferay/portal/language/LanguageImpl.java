brianchandotcom/liferay-portal

commit 364edc5da6bda73497328b7c5fa89e8d611cc50f

LPS-133063 If default locale is not present in the available locales list, set it to the company default user's locale as default locale in the LocaleThreadLocal can become stale when company's default locale is updated.

portal-impl/src/com/liferay/portal/language/LanguageImpl.java
@@ -31,9 +31,11 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.CompanyConstants;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.GroupConstants;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.resource.bundle.ResourceBundleLoader;
 import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
 import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
+import com.liferay.portal.kernel.service.UserLocalServiceUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.CookieKeys;
@@ -2096,6 +2098,15 @@ public class LanguageImpl implements Language, Serializable {
 			Locale defaultLocale = LocaleUtil.getDefault();
 
 			String defaultLanguageId = LocaleUtil.toLanguageId(defaultLocale);
+
+			if (!ArrayUtil.contains(languageIds, defaultLanguageId)) {
+				User defaultUser = UserLocalServiceUtil.fetchDefaultUser(
+					companyId);
+
+				defaultLocale = defaultUser.getLocale();
+
+				defaultLanguageId = LocaleUtil.toLanguageId(defaultLocale);
+			}
 
 			_languageCodeLocalesMap.put(
 				defaultLocale.getLanguage(), defaultLocale);

commit 6769c6aea73ca221327301c76918a05dc8245b15

LPS-142040 The defaultUser was just fetched, which is a big hint it could be missing. When company is created, it updates virtualhost before creating default user leading to this case, This is caused by 364edc5d

portal-impl/src/com/liferay/portal/language/LanguageImpl.java
@@ -2105,10 +2105,15 @@ public class LanguageImpl implements Language, Serializable {
 				User defaultUser = UserLocalServiceUtil.fetchDefaultUser(
 					companyId);
 
-				if (defaultUser.getLocale() != null) {
-					defaultLocale = defaultUser.getLocale();
+				if (defaultUser != null) {
+					Locale defaultUserLocale = defaultUser.getLocale();
 
-					defaultLanguageId = LocaleUtil.toLanguageId(defaultLocale);
+					if (defaultUserLocale != null) {
+						defaultLocale = defaultUserLocale;
+
+						defaultLanguageId = LocaleUtil.toLanguageId(
+							defaultLocale);
+					}
 				}
 			}
 
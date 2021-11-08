b5ef7e03b98a2e28edaaad6c912f3b3cee8f2dd9
Marko Čikoš <marko.cikos@liferay.com>
2021-11-08 18:19:54 +0100

    LPS-135322 Create a Frontend Data Set sample

---
+++ modules/apps/frontend-data-set/frontend-data-set-sample-web/src/main/java/com/liferay/frontend/data/set/sample/web/internal/filter/DateRangeClayTableDataSetFilter.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.data.set.sample.web.internal.filter;
+
+import com.liferay.frontend.data.set.sample.web.internal.constants.FrontendDataSetSampleFrontendDataSetNames;
+import com.liferay.frontend.taglib.clay.data.set.filter.BaseDateRangeClayDataSetFilter;
+import com.liferay.frontend.taglib.clay.data.set.filter.ClayDataSetFilter;
+import com.liferay.frontend.taglib.clay.data.set.filter.DateClayDataSetFilterItem;
+
+import java.util.Calendar;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Javier de Arcos
+ */
+@Component(
+	property = "clay.data.set.display.name=" + FrontendDataSetSampleFrontendDataSetNames.HEADLESS_SAMPLE,
+	service = ClayDataSetFilter.class
+)
+public class DateRangeClayTableDataSetFilter
+	extends BaseDateRangeClayDataSetFilter {
+
+	@Override
+	public String getId() {
+		return "date";
+	}
+
+	@Override
+	public String getLabel() {
+		return "Date Range";
+	}
+
+	public DateClayDataSetFilterItem getMaxDateClayDataSetFilterItem() {
+		Calendar calendar = Calendar.getInstance();
+
+		return new DateClayDataSetFilterItem(
+			calendar.get(Calendar.DAY_OF_MONTH),
+			calendar.get(Calendar.MONTH) + 1, calendar.get(Calendar.YEAR));
+	}
+
+	@Override
+	public DateClayDataSetFilterItem getMinDateClayDataSetFilterItem() {
+		return new DateClayDataSetFilterItem(0, 0, 0);
+	}
+
+}
---
+++ modules/apps/frontend-data-set/frontend-data-set-sample-web/src/main/java/com/liferay/frontend/data/set/sample/web/internal/view/TableClayDataSetDisplayView.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.data.set.sample.web.internal.view;
+
+import com.liferay.frontend.data.set.sample.web.internal.constants.FrontendDataSetSampleFrontendDataSetNames;
+import com.liferay.frontend.taglib.clay.data.set.ClayDataSetDisplayView;
+import com.liferay.frontend.taglib.clay.data.set.view.table.BaseTableClayDataSetDisplayView;
+import com.liferay.frontend.taglib.clay.data.set.view.table.ClayTableSchema;
+import com.liferay.frontend.taglib.clay.data.set.view.table.ClayTableSchemaBuilder;
+import com.liferay.frontend.taglib.clay.data.set.view.table.ClayTableSchemaBuilderFactory;
+import com.liferay.frontend.taglib.clay.data.set.view.table.ClayTableSchemaField;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Javier Gamarra
+ * @author Javier de Arcos
+ */
+@Component(
+	enabled = true,
+	property = "clay.data.set.display.name=" + FrontendDataSetSampleFrontendDataSetNames.HEADLESS_SAMPLE,
+	service = ClayDataSetDisplayView.class
+)
+public class TableClayDataSetDisplayView
+	extends BaseTableClayDataSetDisplayView {
+
+	@Override
+	public ClayTableSchema getClayTableSchema() {
+		ClayTableSchemaBuilder clayTableSchemaBuilder =
+			_clayTableSchemaBuilderFactory.create();
+
+		clayTableSchemaBuilder.addClayTableSchemaField("id", "id");
+
+		clayTableSchemaBuilder.addClayTableSchemaField("title", "Title");
+
+		clayTableSchemaBuilder.addClayTableSchemaField(
+			"description", "Description");
+
+		clayTableSchemaBuilder.addClayTableSchemaField("date", "Date");
+
+		ClayTableSchemaField statusClayTableSchemaField =
+			clayTableSchemaBuilder.addClayTableSchemaField("status", "status");
+
+		statusClayTableSchemaField.setContentRenderer("status");
+
+		clayTableSchemaBuilder.addClayTableSchemaField(
+			"creator.name", "author");
+
+		return clayTableSchemaBuilder.build();
+	}
+
+	@Reference
+	private ClayTableSchemaBuilderFactory _clayTableSchemaBuilderFactory;
+
+}

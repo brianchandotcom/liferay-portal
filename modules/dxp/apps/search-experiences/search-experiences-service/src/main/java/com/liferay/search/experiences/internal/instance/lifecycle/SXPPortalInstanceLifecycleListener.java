6e5ccd592b08e05a251a3b5ba9221ff8c7ca51b7
Petteri Karttunen <petteri.karttunen@liferay.com>
2021-12-02 19:40:18 +0200

    LPS-143202 search-experiences-service: Check system elements exist

--- modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/instance/lifecycle/SXPPortalInstanceLifecycleListener.java
+++ modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/instance/lifecycle/SXPPortalInstanceLifecycleListener.java
@@ -17,18 +17,22 @@ package com.liferay.search.experiences.internal.instance.lifecycle;
 import com.liferay.portal.instance.lifecycle.BasePortalInstanceLifecycleListener;
 import com.liferay.portal.instance.lifecycle.PortalInstanceLifecycleListener;
 import com.liferay.portal.kernel.model.Company;
-import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.service.ServiceContext;
-import com.liferay.portal.kernel.util.ListUtil;
-import com.liferay.portal.kernel.util.LocaleUtil;
-import com.liferay.portal.kernel.util.MapUtil;
-import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.portal.vulcan.util.LocalizedMapUtil;
-import com.liferay.search.experiences.rest.dto.v1_0.SXPElement;
-import com.liferay.search.experiences.rest.dto.v1_0.util.SXPElementUtil;
-import com.liferay.search.experiences.service.SXPElementLocalService;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.search.document.Document;
+import com.liferay.portal.search.engine.adapter.SearchEngineAdapter;
+import com.liferay.portal.search.engine.adapter.search.SearchSearchRequest;
+import com.liferay.portal.search.engine.adapter.search.SearchSearchResponse;
+import com.liferay.portal.search.hits.SearchHit;
+import com.liferay.portal.search.hits.SearchHits;
+import com.liferay.portal.search.index.IndexNameBuilder;
+import com.liferay.portal.search.query.BooleanQuery;
+import com.liferay.portal.search.query.Queries;
+import com.liferay.search.experiences.internal.storage.SystemSXPElementStorage;
+import com.liferay.search.experiences.model.SXPElement;
 
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -45,81 +49,67 @@ public class SXPPortalInstanceLifecycleListener
 
 	@Override
 	public void portalInstanceRegistered(Company company) throws Exception {
-		for (String fileName : FILE_NAMES) {
-			_addSXPElement(company, fileName);
+		List<SXPElement> systemSXPElements =
+			SystemSXPElementStorage.getSXPElements();
+
+		List<Long> companySystemSXPElementIds =
+			_getIndexedCompanySystemSXPElementIds(company.getCompanyId());
+
+		if (companySystemSXPElementIds.size() != systemSXPElements.size()) {
+
+			// TODO reindex
+
 		}
 	}
 
-	protected SXPElement readSXPElement(String fileName) {
-		String json = StringUtil.read(
-			getClass(), "dependencies/" + fileName + ".json");
+	private List<Long> _getIndexedCompanySystemSXPElementIds(long companyId) {
+		BooleanQuery booleanQuery = _queries.booleanQuery();
 
-		return SXPElementUtil.toSXPElement(json);
-	}
+		SearchSearchRequest searchSearchRequest = new SearchSearchRequest();
 
-	protected static final String[] FILE_NAMES = {
-		"boost_all_keywords_match", "boost_asset_type",
-		"boost_contents_for_the_current_language",
-		"boost_contents_in_a_category_by_keyword_match",
-		"boost_contents_in_a_category_for_a_user_segment",
-		"boost_contents_in_a_category_for_a_period_of_time",
-		"boost_contents_in_a_category_for_guest_users",
-		"boost_contents_in_a_category_for_new_user_accounts",
-		"boost_contents_in_a_category_for_the_time_of_day",
-		"boost_contents_in_a_category", "boost_contents_on_my_sites",
-		"boost_contents_with_more_versions", "boost_freshness",
-		"boost_items_for_my_commerce_account_groups", "boost_longer_contents",
-		"boost_proximity", "boost_tagged_contents", "boost_tags_match",
-		"boost_web_contents_by_keyword_match", "filter_by_exact_terms_match",
-		"filter_by_exact_terms_match", "hide_by_an_exact_term_match",
-		"hide_comments", "hide_contents_in_a_category_for_guest_users",
-		"hide_contents_in_a_category", "hide_default_user",
-		"hide_hidden_contents",
-		"limit_search_to_contents_created_within_a_period_of_time",
-		"limit_search_to_head_version", "limit_search_to_my_contents",
-		"limit_search_to_my_sites", "limit_search_to_pdf_files",
-		"limit_search_to_published_contents",
-		"limit_search_to_the_current_site", "limit_search_to_these_sites",
-		"paste_any_elasticsearch_query", "scheduling_aware",
-		"search_with_the_lucene_syntax", "staging_aware",
-		"text_match_over_multiple_fields"
-	};
+		searchSearchRequest.setFetchSourceIncludes(
+			new String[] {Field.ENTRY_CLASS_PK});
+		searchSearchRequest.setIndexNames(
+			_indexNameBuilder.getIndexName(companyId));
+		searchSearchRequest.setPreferLocalCluster(false);
 
-	private void _addSXPElement(Company company, String fileName)
-		throws Exception {
+		booleanQuery.addMustQueryClauses(
+			_queries.term("readonly", true),
+			_queries.term(Field.ENTRY_CLASS_NAME, SXPElement.class.getName()));
 
-		SXPElement sxpElement = readSXPElement(fileName);
+		searchSearchRequest.setQuery(booleanQuery);
+		searchSearchRequest.setSize(200);
 
-		if (ListUtil.exists(
-				_sxpElementLocalService.getSXPElements(company.getCompanyId()),
-				serviceBuilderSXPElement -> Objects.equals(
-					MapUtil.getString(sxpElement.getTitle_i18n(), "en_US"),
-					serviceBuilderSXPElement.getTitle(LocaleUtil.US)))) {
+		SearchSearchResponse searchSearchResponse =
+			_searchEngineAdapter.execute(searchSearchRequest);
 
-			// TODO Fix performance issue with getting every SXP element
+		SearchHits searchHits = searchSearchResponse.getSearchHits();
 
-			return;
+		List<SearchHit> hits = searchHits.getSearchHits();
+
+		if (hits.isEmpty()) {
+			return Collections.emptyList();
 		}
 
-		User defaultUser = company.getDefaultUser();
+		List<Long> ids = new ArrayList<>();
 
-		_sxpElementLocalService.addSXPElement(
-			defaultUser.getUserId(),
-			LocalizedMapUtil.getLocalizedMap(sxpElement.getDescription_i18n()),
-			String.valueOf(sxpElement.getElementDefinition()), true,
-			LocalizedMapUtil.getLocalizedMap(sxpElement.getTitle_i18n()), 0,
-			new ServiceContext() {
-				{
-					setAddGroupPermissions(true);
-					setAddGuestPermissions(true);
-					setCompanyId(company.getCompanyId());
-					setScopeGroupId(company.getGroupId());
-					setUserId(defaultUser.getUserId());
-				}
+		hits.forEach(
+			searchHit -> {
+				Document document = searchHit.getDocument();
+
+				ids.add(document.getLong(Field.ENTRY_CLASS_PK));
 			});
+
+		return ids;
 	}
 
 	@Reference
-	private SXPElementLocalService _sxpElementLocalService;
+	private IndexNameBuilder _indexNameBuilder;
+
+	@Reference
+	private Queries _queries;
+
+	@Reference
+	private SearchEngineAdapter _searchEngineAdapter;
 
 }

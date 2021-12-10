brianchandotcom/liferay-portal

commit 2f30e184de3b05a9548aad9d926e8bcc9ff094c5

LPS-143603 I don't want to keep track of the files anymore

modules/dxp/apps/search-experiences/search-experiences-service/build.gradle
@@ -10,6 +10,7 @@ dependencies {
 	compileInclude group: "joda-time", name: "joda-time", version: "2.9.1"
 	compileInclude group: "org.joda", name: "joda-convert", version: "2.2.1"
 	compileInclude group: "org.json", name: "json", version: "20180813"
+	compileInclude group: "org.reflections", name: "reflections", version: "0.10.2"
 
 	compileOnly group: "biz.aQute.bnd", name: "biz.aQute.bnd.annotation", version: "4.2.0"
 	compileOnly group: "com.liferay.portal", name: "com.liferay.portal.kernel", version: "default"

modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/instance/lifecycle/SXPPortalInstanceLifecycleListener.java
@@ -23,17 +23,22 @@ import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.vulcan.util.LocalizedMapUtil;
-import com.liferay.portal.vulcan.util.TransformUtil;
 import com.liferay.search.experiences.rest.dto.v1_0.SXPElement;
 import com.liferay.search.experiences.rest.dto.v1_0.util.SXPElementUtil;
 import com.liferay.search.experiences.service.SXPElementLocalService;
 
+import java.io.IOException;
+
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+
+import org.reflections.Reflections;
+import org.reflections.scanners.Scanners;
 
 /**
  * @author André de Oliveira
@@ -46,6 +51,21 @@ import org.osgi.service.component.annotations.Reference;
 public class SXPPortalInstanceLifecycleListener
 	extends BasePortalInstanceLifecycleListener {
 
+	public SXPPortalInstanceLifecycleListener() throws IOException {
+		Reflections reflections = new Reflections(
+			"com.liferay.search.experiences.internal.instance.lifecycle." +
+				"dependencies",
+			Scanners.Resources);
+
+		for (String resourceName : reflections.getResources(".*\\.json")) {
+			Class<?> clazz = getClass();
+
+			sxpElements.add(
+				SXPElementUtil.toSXPElement(
+					StringUtil.read(clazz.getClassLoader(), resourceName)));
+		}
+	}
+
 	@Override
 	public void portalInstanceRegistered(Company company) throws Exception {
 
@@ -55,39 +75,7 @@ public class SXPPortalInstanceLifecycleListener
 		_addSXPElements(company);
 	}
 
-	protected SXPElement readSXPElement(String fileName) {
-		String json = StringUtil.read(
-			getClass(), "dependencies/" + fileName + ".json");
-
-		return SXPElementUtil.toSXPElement(json);
-	}
-
-	protected static final String[] FILE_NAMES = {
-		"boost_all_keywords_match", "boost_asset_type",
-		"boost_contents_for_the_current_language",
-		"boost_contents_in_a_category_by_keyword_match",
-		"boost_contents_in_a_category_for_a_user_segment",
-		"boost_contents_in_a_category_for_a_period_of_time",
-		"boost_contents_in_a_category_for_guest_users",
-		"boost_contents_in_a_category_for_new_user_accounts",
-		"boost_contents_in_a_category_for_the_time_of_day",
-		"boost_contents_in_a_category", "boost_contents_on_my_sites",
-		"boost_contents_with_more_versions", "boost_freshness",
-		"boost_items_for_my_commerce_account_groups", "boost_longer_contents",
-		"boost_proximity", "boost_tagged_contents", "boost_tags_match",
-		"boost_web_contents_by_keyword_match", "filter_by_exact_terms_match",
-		"hide_by_an_exact_term_match", "hide_comments",
-		"hide_contents_in_a_category_for_guest_users",
-		"hide_contents_in_a_category", "hide_hidden_contents",
-		"limit_search_to_contents_created_within_a_period_of_time",
-		"limit_search_to_head_version", "limit_search_to_my_contents",
-		"limit_search_to_my_sites", "limit_search_to_pdf_files",
-		"limit_search_to_published_contents",
-		"limit_search_to_the_current_site", "limit_search_to_these_sites",
-		"paste_any_elasticsearch_query", "scheduling_aware",
-		"search_with_the_lucene_syntax", "staging_aware",
-		"text_match_over_multiple_fields"
-	};
+	protected final List<SXPElement> sxpElements = new ArrayList<>();
 
 	private void _addSXPElements(Company company) throws Exception {
 		Set<String> titles = new HashSet<>();
@@ -99,7 +87,7 @@ public class SXPPortalInstanceLifecycleListener
 			titles.add(sxpPElement.getTitle(LocaleUtil.US));
 		}
 
-		for (SXPElement sxpElement : _sxpElements) {
+		for (SXPElement sxpElement : sxpElements) {
 
 			// TODO Should this be en_US or en-US?
 
@@ -131,8 +119,5 @@ public class SXPPortalInstanceLifecycleListener
 
 	@Reference
 	private SXPElementLocalService _sxpElementLocalService;
-
-	private final List<SXPElement> _sxpElements = TransformUtil.transformToList(
-		FILE_NAMES, fileName -> readSXPElement(fileName));
 
 }

modules/dxp/apps/search-experiences/search-experiences-service/src/test/java/com/liferay/search/experiences/internal/instance/lifecycle/SXPPortalInstanceLifecycleListenerTest.java
@@ -16,7 +16,6 @@ package com.liferay.search.experiences.internal.instance.lifecycle;
 
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 import com.liferay.search.experiences.rest.dto.v1_0.SXPElement;
-import com.liferay.search.experiences.rest.dto.v1_0.util.SXPElementUtil;
 
 import org.junit.Assert;
 import org.junit.ClassRule;
@@ -34,22 +33,18 @@ public class SXPPortalInstanceLifecycleListenerTest {
 		LiferayUnitTestRule.INSTANCE;
 
 	@Test
-	public void testReadSXPElement() {
-		for (String fileName : SXPPortalInstanceLifecycleListener.FILE_NAMES) {
-			_testReadSXPElement(fileName);
-		}
-	}
-
-	private void _testReadSXPElement(String fileName) {
+	public void testSXPElements() throws Exception {
 		SXPPortalInstanceLifecycleListener sxpPortalInstanceLifecycleListener =
 			new SXPPortalInstanceLifecycleListener();
 
-		SXPElement sxpElement =
-			sxpPortalInstanceLifecycleListener.readSXPElement(fileName);
+		Assert.assertNotEquals(
+			0, sxpPortalInstanceLifecycleListener.sxpElements.size());
 
-		Assert.assertEquals(
-			fileName, sxpElement.toString(),
-			String.valueOf(SXPElementUtil.toSXPElement(sxpElement.toString())));
+		for (SXPElement sxpElement :
+				sxpPortalInstanceLifecycleListener.sxpElements) {
+
+			Assert.assertNotNull(sxpElement);
+		}
 	}
 
 }

commit bb239b17196607c95e83f7f5b33e3c83f924bb84

LPS-143603 Fix broken resource loading caused by 2f30e184de3b05a9548aad9d926e8bcc9ff094c5

modules/dxp/apps/search-experiences/search-experiences-service/build.gradle
@@ -10,7 +10,6 @@ dependencies {
 	compileInclude group: "joda-time", name: "joda-time", version: "2.9.1"
 	compileInclude group: "org.joda", name: "joda-convert", version: "2.2.1"
 	compileInclude group: "org.json", name: "json", version: "20180813"
-	compileInclude group: "org.reflections", name: "reflections", version: "0.10.2"
 
 	compileOnly group: "biz.aQute.bnd", name: "biz.aQute.bnd.annotation", version: "4.2.0"
 	compileOnly group: "com.liferay.portal", name: "com.liferay.portal.kernel", version: "default"
@@ -33,6 +32,7 @@ dependencies {
 	compileOnly project(":apps:wiki:wiki-api")
 	compileOnly project(":core:osgi-service-tracker-collections")
 	compileOnly project(":core:petra:petra-function")
+	compileOnly project(":core:petra:petra-io")
 	compileOnly project(":core:petra:petra-lang")
 	compileOnly project(":core:petra:petra-reflect")
 	compileOnly project(":core:petra:petra-sql-dsl-api")

modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/instance/lifecycle/SXPPortalInstanceLifecycleListener.java
@@ -14,6 +14,8 @@
 
 package com.liferay.search.experiences.internal.instance.lifecycle;
 
+import com.liferay.petra.io.StreamUtil;
+import com.liferay.petra.string.CharPool;
 import com.liferay.portal.instance.lifecycle.BasePortalInstanceLifecycleListener;
 import com.liferay.portal.instance.lifecycle.PortalInstanceLifecycleListener;
 import com.liferay.portal.kernel.model.Company;
@@ -29,16 +31,18 @@ import com.liferay.search.experiences.service.SXPElementLocalService;
 
 import java.io.IOException;
 
+import java.net.URL;
+
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
-
-import org.reflections.Reflections;
-import org.reflections.scanners.Scanners;
 
 /**
  * @author André de Oliveira
@@ -52,17 +56,24 @@ public class SXPPortalInstanceLifecycleListener
 	extends BasePortalInstanceLifecycleListener {
 
 	public SXPPortalInstanceLifecycleListener() throws IOException {
-		Reflections reflections = new Reflections(
-			"com.liferay.search.experiences.internal.instance.lifecycle." +
-				"dependencies",
-			Scanners.Resources);
+		Class<?> clazz = getClass();
 
-		for (String resourceName : reflections.getResources(".*\\.json")) {
-			Class<?> clazz = getClass();
+		Bundle bundle = FrameworkUtil.getBundle(clazz);
+
+		Package pkg = clazz.getPackage();
+
+		String path = StringUtil.replace(
+			pkg.getName(), CharPool.PERIOD, CharPool.SLASH);
+
+		Enumeration<URL> enumeration = bundle.findEntries(
+			path.concat("/dependencies"), "*.json", false);
+
+		while (enumeration.hasMoreElements()) {
+			URL url = enumeration.nextElement();
 
 			sxpElements.add(
 				SXPElementUtil.toSXPElement(
-					StringUtil.read(clazz.getClassLoader(), resourceName)));
+					StreamUtil.toString(url.openStream())));
 		}
 	}
 
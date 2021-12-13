upalatucci/liferay-commerce

COMMERCE-7560 loading templates

modules/apps/batch-planner/batch-planner-web/src/main/resources/META-INF/resources/js/constants.js
@@ -21,6 +21,9 @@ export const EXPORT_FILE_NAME = 'Export.zip';
 export const FILE_SCHEMA_EVENT = 'file-schema';
 export const SCHEMA_SELECTED_EVENT = 'schema-selected';
 export const TEMPLATE_SELECTED_EVENT = 'template-selected';
+export const HEADLESS_ENDPOINT_POLICY_NAME = 'headlessEndpoint';
+export const TEMPLATE_DIRTY = 'template-dirty';
+export const NULL_TEMPLATE_VALUE = '';
 
 export const PARSE_FILE_CHUNK_SIZE = 64 * 1024;
 

modules/apps/batch-planner/batch-planner-web/src/main/resources/META-INF/resources/js/edit_batch_planner_plan.js
@@ -14,18 +14,34 @@
 
 import {fetch, openToast} from 'frontend-js-web';
 
-import {SCHEMA_SELECTED_EVENT} from './constants';
+import {
+	SCHEMA_SELECTED_EVENT,
+	TEMPLATE_DIRTY,
+	TEMPLATE_SELECTED_EVENT,
+} from './constants';
 
 const HEADERS = new Headers({
 	'content-type': 'application/json',
 	'x-csrf-token': window.Liferay.authToken,
 });
 
-function getOptionElement(label, schemaName, value) {
+function trimPackage(name) {
+	if (!name || name.lastIndexOf('.') < 0) {
+		return name;
+	}
+
+	return name.substr(name.lastIndexOf('.') + 1);
+}
+
+function getOptionElement(label, schemaName, selected, value) {
 	const optionElement = document.createElement('option');
 
 	optionElement.innerHTML = label;
 	optionElement.value = value;
+
+	if (selected) {
+		optionElement.selected = true;
+	}
 
 	if (schemaName) {
 		optionElement.setAttribute('schemaName', schemaName);
@@ -46,10 +62,43 @@ export default function ({namespace}) {
 		`#${namespace}taskItemDelegateName`
 	);
 
-	headlessEnpointSelect.addEventListener('change', async (event) => {
-		event.target.disabled = true;
+	Liferay.on(TEMPLATE_SELECTED_EVENT, async (event) => {
+		if (event.templateClassName && event.templateHeadlessEndpoint) {
+			const headlessTemplateOption = headlessEnpointSelect.querySelector(
+				`option[value='${event.templateHeadlessEndpoint}']`
+			);
+			headlessTemplateOption.selected = true;
+			await handleHeadlessSelectChange();
 
-		const headlessEnpoint = event.target.value;
+			const internalClassTemplateOption = internalClassNameSelect.querySelector(
+				`option[value='${event.templateClassName}']`
+			);
+			internalClassTemplateOption.selected = true;
+			await handleClassNameSelectChange();
+
+			var onChangeEvent = document.createEvent('CustomEvent');
+			onChangeEvent.initEvent('change', false, true);
+			internalClassTemplateOption.dispatchEvent(onChangeEvent);
+		}
+	});
+
+	headlessEnpointSelect.addEventListener(
+		'change',
+		handleHeadlessSelectChange
+	);
+
+	internalClassNameSelect.addEventListener(
+		'change',
+		handleClassNameSelectChange
+	);
+
+	async function handleHeadlessSelectChange(event) {
+		if (event) {
+			Liferay.fire(TEMPLATE_DIRTY);
+			event.target.disabled = true;
+		}
+
+		const headlessEnpoint = headlessEnpointSelect.value;
 
 		if (!headlessEnpoint) {
 			internalClassNameSelect.innerHTML = '';
@@ -70,7 +119,9 @@ export default function ({namespace}) {
 			const {components} = await response.json();
 			internalClassNameSelect.innerHTML = '';
 
-			internalClassNameSelect.appendChild(getOptionElement('', '', ''));
+			internalClassNameSelect.appendChild(
+				getOptionElement('', '', false, '')
+			);
 
 			const keys = Object.keys(components.schemas).sort();
 
@@ -88,6 +139,7 @@ export default function ({namespace}) {
 				const optionElement = getOptionElement(
 					trimPackage(className),
 					schemaName,
+					false,
 					className
 				);
 
@@ -109,16 +161,17 @@ export default function ({namespace}) {
 			console.error('Failed to fetch ' + error);
 		}
 		finally {
-			event.target.disabled = false;
+			if (event) {
+				event.target.disabled = false;
+			}
 		}
-	});
+	}
 
-	internalClassNameSelect.addEventListener(
-		'change',
-		handleClassNameSelectChange
-	);
+	async function handleClassNameSelectChange(event) {
+		if (event) {
+			Liferay.fire(TEMPLATE_DIRTY);
+		}
 
-	async function handleClassNameSelectChange() {
 		const headlessEnpointValue = headlessEnpointSelect.value;
 
 		const selectedOption =
@@ -134,7 +187,7 @@ export default function ({namespace}) {
 			schemaName || selectedOption.value
 		);
 
-		if (!headlessEnpointValue || !internalClassNameValue) {
+		if (!internalClassNameValue) {
 			Liferay.fire(SCHEMA_SELECTED_EVENT, {
 				schema: null,
 			});
@@ -168,13 +221,5 @@ export default function ({namespace}) {
 
 			console.error(`Failed to fetch ${error}`);
 		}
-	}
-
-	function trimPackage(name) {
-		if (!name || name.lastIndexOf('.') < 0) {
-			return name;
-		}
-
-		return name.substr(name.lastIndexOf('.') + 1);
 	}
 }

modules/apps/batch-planner/batch-planner-web/test/components/TemplateSelect/TemplateSelect.js
@@ -30,7 +30,7 @@ const BASE_PROPS = {
 		{label: 'Hello', selected: true, value: '42147'},
 	],
 };
-
+const headlessEndpoint = '/o/headless-commerce-admin-channel/v1.0/openapi.json';
 const internalClassName =
 	'com.liferay.headless.commerce.admin.channel.dto.v1_0.Channel';
 const mockedMapping = {
@@ -61,33 +61,39 @@ describe('TemplateSelect', () => {
 	});
 
 	it('must fire new template event when preselected', async () => {
-		const mockTempalteSelected = jest.fn();
-		Liferay.on(TEMPLATE_SELECTED_EVENT, mockTempalteSelected);
+		const mockTemplateSelected = jest.fn();
+		Liferay.on(TEMPLATE_SELECTED_EVENT, mockTemplateSelected);
 		render(
 			<TemplateSelect
 				{...BASE_PROPS}
 				selectedTemplateClassName={internalClassName}
+				selectedTemplateHeadlessEndpoint={headlessEndpoint}
 				selectedTemplateMapping={mockedMapping}
 			/>
 		);
+
+		var documentReadyEvent = document.createEvent('CustomEvent');
+		documentReadyEvent.initEvent('readystatechange', false, true);
+		document.dispatchEvent(documentReadyEvent);
 
 		await wait(() => {
 			const expectedEvent = new CustomEvent(TEMPLATE_SELECTED_EVENT);
 			expectedEvent.templateClassName = internalClassName;
 			expectedEvent.templateMapping = mockedMapping;
-
-			expect(mockTempalteSelected).toBeCalledWith(expectedEvent);
+			expectedEvent.templateHeadlessEndpoint = headlessEndpoint;
+			expect(mockTemplateSelected).toBeCalledWith(expectedEvent);
 		});
 	});
 
 	it('must fire empty event when no template get selected', async () => {
-		const mockTempalteSelected = jest.fn();
-		Liferay.on(TEMPLATE_SELECTED_EVENT, mockTempalteSelected);
+		const mockTemplateSelected = jest.fn();
+		Liferay.on(TEMPLATE_SELECTED_EVENT, mockTemplateSelected);
 
 		const {getByLabelText} = render(
 			<TemplateSelect
 				{...BASE_PROPS}
 				selectedTemplateClassName={internalClassName}
+				selectedTemplateHeadlessEndpoint={headlessEndpoint}
 				selectedTemplateMapping={mockedMapping}
 			/>
 		);
@@ -102,7 +108,8 @@ describe('TemplateSelect', () => {
 			const expectedEvent = new CustomEvent(TEMPLATE_SELECTED_EVENT);
 			expectedEvent.templateClassName = null;
 			expectedEvent.templateMapping = null;
-			expect(mockTempalteSelected).toHaveBeenLastCalledWith(
+			expectedEvent.templateHeadlessEndpoint = null;
+			expect(mockTemplateSelected).toHaveBeenLastCalledWith(
 				expectedEvent
 			);
 		});
@@ -123,6 +130,7 @@ describe('TemplateSelect', () => {
 			const expectedEvent = new CustomEvent(TEMPLATE_SELECTED_EVENT);
 			expectedEvent.templateClassName = internalClassName;
 			expectedEvent.templateMapping = mockedMapping;
+			expectedEvent.templateHeadlessEndpoint = headlessEndpoint;
 
 			expect(mockTempalteSelected).toBeCalledWith(expectedEvent);
 		});
@@ -199,6 +207,12 @@ const mockGetPlan = {
 			name: 'saveExport',
 			planId: 106902,
 			value: 'saveExport',
+		},
+		{
+			id: 54404,
+			name: 'headlessEndpoint',
+			planId: 54402,
+			value: headlessEndpoint,
 		},
 		{
 			id: 106903,

brianchandotcom/liferay-portal

modules/apps/batch-planner/batch-planner-web/src/main/resources/META-INF/resources/js/constants.js
@@ -21,6 +21,9 @@ export const EXPORT_FILE_NAME = 'Export.zip';
 export const FILE_SCHEMA_EVENT = 'file-schema';
 export const SCHEMA_SELECTED_EVENT = 'schema-selected';
 export const TEMPLATE_SELECTED_EVENT = 'template-selected';
+export const HEADLESS_ENDPOINT_POLICY_NAME = 'headlessEndpoint';
+export const TEMPLATE_SOILED = 'template-soiled';
+export const NULL_TEMPLATE_VALUE = '';
 
 export const PARSE_FILE_CHUNK_SIZE = 64 * 1024;
 

modules/apps/batch-planner/batch-planner-web/src/main/resources/META-INF/resources/js/edit_batch_planner_plan.js
@@ -12,20 +12,38 @@
  * details.
  */
 
+import {render} from '@liferay/frontend-js-react-web';
 import {fetch, openToast} from 'frontend-js-web';
 
-import {SCHEMA_SELECTED_EVENT} from './constants';
+import TemplateSelect from './TemplateSelect';
+import {
+	SCHEMA_SELECTED_EVENT,
+	TEMPLATE_SELECTED_EVENT,
+	TEMPLATE_SOILED,
+} from './constants';
 
 const HEADERS = new Headers({
 	'content-type': 'application/json',
 	'x-csrf-token': window.Liferay.authToken,
 });
 
-function getOptionElement(label, schemaName, value) {
+function trimPackage(name) {
+	if (!name || name.lastIndexOf('.') < 0) {
+		return name;
+	}
+
+	return name.substr(name.lastIndexOf('.') + 1);
+}
+
+function getOptionElement(label, schemaName, selected, value) {
 	const optionElement = document.createElement('option');
 
 	optionElement.innerHTML = label;
 	optionElement.value = value;
+
+	if (selected) {
+		optionElement.selected = true;
+	}
 
 	if (schemaName) {
 		optionElement.setAttribute('schemaName', schemaName);
@@ -34,22 +52,63 @@ function getOptionElement(label, schemaName, value) {
 	return optionElement;
 }
 
-export default function ({namespace}) {
+export default function ({
+	initialTemplateClassName,
+	initialTemplateHeadlessEndpoint,
+	initialTemplateMapping,
+	namespace,
+	templatesOptions,
+}) {
 	const headlessEnpointSelect = document.querySelector(
 		`#${namespace}headlessEndpoint`
 	);
 	const internalClassNameSelect = document.querySelector(
 		`#${namespace}internalClassName`
 	);
-
 	const taskItemDelegateNameInput = document.querySelector(
 		`#${namespace}taskItemDelegateName`
 	);
 
-	headlessEnpointSelect.addEventListener('change', async (event) => {
-		event.target.disabled = true;
+	const handleTemplateSelectedEvent = async (event) => {
+		const {template} = event;
+		if (template) {
+			const headlessTemplateOption = headlessEnpointSelect.querySelector(
+				`option[value='${template.headlessEndpoint}']`
+			);
 
-		const headlessEnpoint = event.target.value;
+			headlessTemplateOption.selected = true;
+
+			await handleHeadlessSelectChange();
+
+			const internalClassTemplateOption = internalClassNameSelect.querySelector(
+				`option[value='${template.internalClassName}']`
+			);
+
+			internalClassTemplateOption.selected = true;
+
+			await handleClassNameSelectChange();
+		}
+	};
+
+	Liferay.on(TEMPLATE_SELECTED_EVENT, handleTemplateSelectedEvent);
+
+	headlessEnpointSelect.addEventListener(
+		'change',
+		handleHeadlessSelectChange
+	);
+
+	internalClassNameSelect.addEventListener(
+		'change',
+		handleClassNameSelectChange
+	);
+
+	async function handleHeadlessSelectChange(event) {
+		if (event) {
+			Liferay.fire(TEMPLATE_SOILED);
+			event.target.disabled = true;
+		}
+
+		const headlessEnpoint = headlessEnpointSelect.value;
 
 		if (!headlessEnpoint) {
 			internalClassNameSelect.innerHTML = '';
@@ -68,9 +127,12 @@ export default function ({namespace}) {
 			}
 
 			const {components} = await response.json();
+
 			internalClassNameSelect.innerHTML = '';
 
-			internalClassNameSelect.appendChild(getOptionElement('', '', ''));
+			internalClassNameSelect.appendChild(
+				getOptionElement('', '', false, '')
+			);
 
 			const keys = Object.keys(components.schemas).sort();
 
@@ -82,12 +144,12 @@ export default function ({namespace}) {
 				}
 
 				const className = properties['x-class-name'].default;
-
 				const schemaName = properties['x-schema-name']?.default;
 
 				const optionElement = getOptionElement(
 					trimPackage(className),
 					schemaName,
+					false,
 					className
 				);
 
@@ -109,16 +171,17 @@ export default function ({namespace}) {
 			console.error('Failed to fetch ' + error);
 		}
 		finally {
-			event.target.disabled = false;
+			if (event) {
+				event.target.disabled = false;
+			}
 		}
-	});
+	}
 
-	internalClassNameSelect.addEventListener(
-		'change',
-		handleClassNameSelectChange
-	);
+	async function handleClassNameSelectChange(event) {
+		if (event) {
+			Liferay.fire(TEMPLATE_SOILED);
+		}
 
-	async function handleClassNameSelectChange() {
 		const headlessEnpointValue = headlessEnpointSelect.value;
 
 		const selectedOption =
@@ -134,7 +197,7 @@ export default function ({namespace}) {
 			schemaName || selectedOption.value
 		);
 
-		if (!headlessEnpointValue || !internalClassNameValue) {
+		if (!internalClassNameValue) {
 			Liferay.fire(SCHEMA_SELECTED_EVENT, {
 				schema: null,
 			});
@@ -170,11 +233,35 @@ export default function ({namespace}) {
 		}
 	}
 
-	function trimPackage(name) {
-		if (!name || name.lastIndexOf('.') < 0) {
-			return name;
-		}
+	let initialTemplate;
 
-		return name.substr(name.lastIndexOf('.') + 1);
+	if (
+		initialTemplateHeadlessEndpoint &&
+		initialTemplateClassName &&
+		initialTemplateMapping
+	) {
+		initialTemplate = {
+			headlessEndpoint: initialTemplateHeadlessEndpoint,
+			internalClassName: initialTemplateClassName,
+			mapping: initialTemplateMapping,
+		};
 	}
+
+	render(
+		TemplateSelect,
+		{
+			initialTemplate,
+			templatesOptions,
+		},
+		document.getElementById(`${namespace}templateSelect`)
+	);
+
+	return {
+		dispose: () => {
+			Liferay.detach(
+				TEMPLATE_SELECTED_EVENT,
+				handleTemplateSelectedEvent
+			);
+		},
+	};
 }

modules/apps/batch-planner/batch-planner-web/test/components/TemplateSelect/TemplateSelect.js
@@ -30,15 +30,22 @@ const BASE_PROPS = {
 		{label: 'Hello', selected: true, value: '42147'},
 	],
 };
-
+const headlessEndpoint = '/o/headless-commerce-admin-channel/v1.0/openapi.json';
 const internalClassName =
 	'com.liferay.headless.commerce.admin.channel.dto.v1_0.Channel';
+
 const mockedMapping = {
 	currencyCode: 'currencyCode',
 	id: 'externalReferenceCode',
 	siteGroupId: 'siteGroupId',
 	testFieldName: 'name',
 	type: 'type',
+};
+
+const initialTemplate = {
+	headlessEndpoint,
+	internalClassName,
+	mapping: mockedMapping,
 };
 const mockPlanId = 106902;
 const getPlanInfoURL = `${HEADERS_BATCH_PLANNER_URL}/plans/${mockPlanId}`;
@@ -54,40 +61,40 @@ describe('TemplateSelect', () => {
 		cleanup();
 	});
 
-	it('must has label', () => {
+	it('must have label', () => {
 		const {getByLabelText} = render(<TemplateSelect {...BASE_PROPS} />);
 
 		getByLabelText(Liferay.Language.get('template'));
 	});
 
 	it('must fire new template event when preselected', async () => {
-		const mockTempalteSelected = jest.fn();
-		Liferay.on(TEMPLATE_SELECTED_EVENT, mockTempalteSelected);
+		const mockTemplateSelected = jest.fn();
+		Liferay.on(TEMPLATE_SELECTED_EVENT, mockTemplateSelected);
 		render(
-			<TemplateSelect
-				{...BASE_PROPS}
-				selectedTemplateClassName={internalClassName}
-				selectedTemplateMapping={mockedMapping}
-			/>
+			<TemplateSelect {...BASE_PROPS} initialTemplate={initialTemplate} />
 		);
+
+		const documentReadyEvent = document.createEvent('CustomEvent');
+		documentReadyEvent.initEvent('readystatechange', false, true);
+		document.dispatchEvent(documentReadyEvent);
 
 		await wait(() => {
 			const expectedEvent = new CustomEvent(TEMPLATE_SELECTED_EVENT);
-			expectedEvent.templateClassName = internalClassName;
-			expectedEvent.templateMapping = mockedMapping;
 
-			expect(mockTempalteSelected).toBeCalledWith(expectedEvent);
+			expectedEvent.template = {...initialTemplate};
+			expect(mockTemplateSelected).toBeCalledWith(expectedEvent);
 		});
 	});
 
 	it('must fire empty event when no template get selected', async () => {
-		const mockTempalteSelected = jest.fn();
-		Liferay.on(TEMPLATE_SELECTED_EVENT, mockTempalteSelected);
+		const mockTemplateSelected = jest.fn();
+		Liferay.on(TEMPLATE_SELECTED_EVENT, mockTemplateSelected);
 
 		const {getByLabelText} = render(
 			<TemplateSelect
 				{...BASE_PROPS}
 				selectedTemplateClassName={internalClassName}
+				selectedTemplateHeadlessEndpoint={headlessEndpoint}
 				selectedTemplateMapping={mockedMapping}
 			/>
 		);
@@ -100,9 +107,8 @@ describe('TemplateSelect', () => {
 
 		await wait(() => {
 			const expectedEvent = new CustomEvent(TEMPLATE_SELECTED_EVENT);
-			expectedEvent.templateClassName = null;
-			expectedEvent.templateMapping = null;
-			expect(mockTempalteSelected).toHaveBeenLastCalledWith(
+			expectedEvent.template = null;
+			expect(mockTemplateSelected).toHaveBeenLastCalledWith(
 				expectedEvent
 			);
 		});
@@ -121,8 +127,8 @@ describe('TemplateSelect', () => {
 
 		await wait(() => {
 			const expectedEvent = new CustomEvent(TEMPLATE_SELECTED_EVENT);
-			expectedEvent.templateClassName = internalClassName;
-			expectedEvent.templateMapping = mockedMapping;
+
+			expectedEvent.template = {...initialTemplate};
 
 			expect(mockTempalteSelected).toBeCalledWith(expectedEvent);
 		});
@@ -199,6 +205,12 @@ const mockGetPlan = {
 			name: 'saveExport',
 			planId: 106902,
 			value: 'saveExport',
+		},
+		{
+			id: 54404,
+			name: 'headlessEndpoint',
+			planId: 54402,
+			value: headlessEndpoint,
 		},
 		{
 			id: 106903,
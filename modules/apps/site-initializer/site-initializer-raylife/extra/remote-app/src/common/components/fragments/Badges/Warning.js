5f8ce45163d0e13dca52b3a5faebd6a25deb7721
Ilza Medeiros <ilza.medeiros@liferay.com>
2021-12-14 09:39:12 -0300

    LPS-143330 Update forms after the inputs and cards are merged

--- modules/apps/site-initializer/site-initializer-raylife/extra/remote-app/src/common/components/fragments/Badges/Warning.js
+++ modules/apps/site-initializer/site-initializer-raylife/extra/remote-app/src/common/components/fragments/Badges/Warning.js
@@ -4,13 +4,13 @@ import React from 'react';
 
 export function WarningBadge({children}) {
 	return (
-		<ClayLabel className="label-inverse-danger mt-6 rounded">
-			<div className="align-items-center badge d-flex ml-2 warning">
-				<span className="inline-item inline-item-before">
+		<ClayLabel className="d-block label-inverse-danger mt-1 rounded">
+			<div className="align-items-start badge d-flex ml-2 warning">
+				<span className="inline-item inline-item-before mr-1">
 					<ClayIcon symbol="exclamation-full" />
 				</span>
 
-				<span className="text-paragraph-sm">{children}</span>
+				<span className="text-paragraph-sm text-start">{children}</span>
 			</div>
 		</ClayLabel>
 	);

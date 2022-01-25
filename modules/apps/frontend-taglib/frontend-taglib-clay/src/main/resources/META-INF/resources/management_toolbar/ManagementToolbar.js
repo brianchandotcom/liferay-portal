5c15726c6b39bd3bf1a3490982c18e3368cc8364
tinycarol <carolalonsou@gmail.com>
2022-01-25 16:08:15 +0100

    LPS-144536 - code review, simplify conditions and syntax

--- modules/apps/frontend-taglib/frontend-taglib-clay/src/main/resources/META-INF/resources/management_toolbar/ManagementToolbar.js
+++ modules/apps/frontend-taglib/frontend-taglib-clay/src/main/resources/META-INF/resources/management_toolbar/ManagementToolbar.js
@@ -30,6 +30,8 @@ import SelectionControls from './SelectionControls';
 
 import './ManagementToolbar.scss';
 
+const noop = () => {};
+
 function ManagementToolbar({
 	clearResultsURL,
 	clearSelectionURL,
@@ -43,15 +45,15 @@ function ManagementToolbar({
 	initialCheckboxStatus,
 	initialSelectAllButtonVisible,
 	initialSelectedItems,
-	onActionButtonClick = () => {},
-	onCheckboxChange = () => {},
-	onClearSelectionButtonClick = () => {},
-	onCreateButtonClick = () => {},
-	onCreationMenuItemClick = () => {},
-	onInfoButtonClick = () => {},
-	onFilterDropdownItemClick = () => {},
-	onOrderDropdownItemClick = () => {},
-	onSelectAllButtonClick = () => {},
+	onActionButtonClick = noop,
+	onCheckboxChange = noop,
+	onClearSelectionButtonClick = noop,
+	onCreateButtonClick = noop,
+	onCreationMenuItemClick = noop,
+	onInfoButtonClick = noop,
+	onFilterDropdownItemClick = noop,
+	onOrderDropdownItemClick = noop,
+	onSelectAllButtonClick = noop,
 	onShowMoreButtonClick,
 	orderDropdownItems,
 	searchActionURL,

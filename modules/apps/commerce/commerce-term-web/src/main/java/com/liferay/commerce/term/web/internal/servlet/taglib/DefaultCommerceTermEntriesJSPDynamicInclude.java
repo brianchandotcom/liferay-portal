f0f18ca85b90ef2a0b112695a41b15163a9dab26
Andrea Sbarra <andrea.ale.sbarra@gmail.com>
2022-02-02 16:44:10 +0100

    COMMERCE-8194 commerce-term-web added logic to select default commerce term for account

---
+++ modules/apps/commerce/commerce-term-web/src/main/java/com/liferay/commerce/term/web/internal/servlet/taglib/DefaultCommerceTermEntriesJSPDynamicInclude.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.term.web.internal.servlet.taglib;
+
+import com.liferay.account.service.AccountEntryLocalService;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.servlet.taglib.BaseJSPDynamicInclude;
+import com.liferay.portal.kernel.servlet.taglib.DynamicInclude;
+
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Andrea Sbarra
+ */
+@Component(enabled = false, immediate = true, service = DynamicInclude.class)
+public class DefaultCommerceTermEntriesJSPDynamicInclude
+	extends BaseJSPDynamicInclude {
+
+	@Override
+	public void include(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse, String key)
+		throws IOException {
+
+		super.include(httpServletRequest, httpServletResponse, key);
+	}
+
+	@Override
+	public void register(DynamicIncludeRegistry dynamicIncludeRegistry) {
+		dynamicIncludeRegistry.register(
+			"com.liferay.commerce.term.web#" +
+				"/default_commerce_term_entries.jsp#");
+	}
+
+	@Override
+	protected String getJspPath() {
+		return "/dynamic_include/default_commerce_term_entries.jsp";
+	}
+
+	@Override
+	protected Log getLog() {
+		return _log;
+	}
+
+	@Override
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.commerce.term.web)",
+		unbind = "-"
+	)
+	protected void setServletContext(ServletContext servletContext) {
+		super.setServletContext(servletContext);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		DefaultCommerceTermEntriesJSPDynamicInclude.class);
+
+	@Reference
+	private AccountEntryLocalService _accountEntryLocalService;
+
+}

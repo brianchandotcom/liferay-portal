b2d5366ae5001c3acb325486e5a8fc09f420fd1b
Iván Zaera Avellón <ivan.zaera@liferay.com>
2022-02-11 10:21:33 +0100

    LPS-145293 SF after gradle buildService

--- modules/apps/remote-app/remote-app-api/src/main/java/com/liferay/remote/app/exception/RemoteAppEntryBundledAppURLException.java
+++ modules/apps/remote-app/remote-app-api/src/main/java/com/liferay/remote/app/exception/RemoteAppEntryBundledAppURLException.java
@@ -11,6 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
+
 package com.liferay.remote.app.exception;
 
 import com.liferay.portal.kernel.exception.PortalException;
@@ -27,7 +28,9 @@ public class RemoteAppEntryBundledAppURLException extends PortalException {
 		super(msg);
 	}
 
-	public RemoteAppEntryBundledAppURLException(String msg, Throwable throwable) {
+	public RemoteAppEntryBundledAppURLException(
+		String msg, Throwable throwable) {
+
 		super(msg, throwable);
 	}
 

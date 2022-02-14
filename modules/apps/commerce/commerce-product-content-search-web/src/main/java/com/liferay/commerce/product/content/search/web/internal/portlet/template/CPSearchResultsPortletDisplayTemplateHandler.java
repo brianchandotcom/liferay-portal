3aa10cefa22a62bbdeecf53e0bf28cdc89fbf77d
Ivica Cardic <ivica.cardic@liferay.com>
2022-02-14 18:05:56 +0100

    COMMERCE-1489 - SF

--- modules/apps/commerce/commerce-product-content-search-web/src/main/java/com/liferay/commerce/product/content/search/web/internal/portlet/template/CPSearchResultsPortletDisplayTemplateHandler.java
+++ modules/apps/commerce/commerce-product-content-search-web/src/main/java/com/liferay/commerce/product/content/search/web/internal/portlet/template/CPSearchResultsPortletDisplayTemplateHandler.java
@@ -16,8 +16,8 @@ package com.liferay.commerce.product.content.search.web.internal.portlet.templat
 
 import com.liferay.commerce.product.catalog.CPCatalogEntry;
 import com.liferay.commerce.product.constants.CPPortletKeys;
-import com.liferay.commerce.product.content.search.web.internal.result.display.context.CPSearchResultsDisplayContext;
 import com.liferay.commerce.product.content.search.web.internal.portlet.CPSearchResultsPortlet;
+import com.liferay.commerce.product.content.search.web.internal.result.display.context.CPSearchResultsDisplayContext;
 import com.liferay.commerce.product.service.CPDefinitionLocalService;
 import com.liferay.commerce.product.service.CPDefinitionService;
 import com.liferay.petra.string.StringBundler;

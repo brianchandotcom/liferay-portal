5bef90f70e95ada2d67e8086ac6dacc970912347
Mateus Santana <mateus.santana@liferay.com>
2022-03-09 11:06:35 -0300

    LPS-148845 Add new EntityField on entityFieldsMap

--- modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/odata/entity/v1_0/ObjectEntryEntityModel.java
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/odata/entity/v1_0/ObjectEntryEntityModel.java
@@ -42,6 +42,8 @@ public class ObjectEntryEntityModel implements EntityModel {
 
 	public ObjectEntryEntityModel(List<ObjectField> objectFields) {
 		_entityFieldsMap = HashMapBuilder.<String, EntityField>put(
+			"creator", new StringEntityField("creator", locale -> "creator")
+		).put(
 			"creatorId",
 			new IntegerEntityField("creatorId", locale -> Field.USER_ID)
 		).put(

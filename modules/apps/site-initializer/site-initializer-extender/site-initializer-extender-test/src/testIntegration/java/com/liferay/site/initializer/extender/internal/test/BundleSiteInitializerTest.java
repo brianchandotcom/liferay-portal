282cdff37df14cfd9b2a59783f585b68eefc9acc
Nilton Vieira <nilton.vieira@liferay.com>
2022-05-02 16:34:30 -0300

    LPS-152469 _assertUserOrganizations

--- modules/apps/site-initializer/site-initializer-extender/site-initializer-extender-test/src/testIntegration/java/com/liferay/site/initializer/extender/internal/test/BundleSiteInitializerTest.java
+++ modules/apps/site-initializer/site-initializer-extender/site-initializer-extender-test/src/testIntegration/java/com/liferay/site/initializer/extender/internal/test/BundleSiteInitializerTest.java
@@ -981,6 +981,7 @@ public class BundleSiteInitializerTest {
 		Organization organization1 = organizationsPage1.fetchFirstItem();
 
 		Assert.assertNotNull(organization1);
+		_assertUserOrganizations(organization1.getId(), serviceContext, 1);
 
 		Page<Organization> organizationsPage2 =
 			organizationResource.getOrganizationsPage(
@@ -992,6 +993,7 @@ public class BundleSiteInitializerTest {
 
 		Assert.assertNotNull(organization2);
 		Assert.assertTrue(organization2.getNumberOfOrganizations() == 1);
+		_assertUserOrganizations(organization2.getId(), serviceContext, 1);
 
 		Page<Organization> organizationsPage3 =
 			organizationResource.getOrganizationChildOrganizationsPage(
@@ -1001,6 +1003,7 @@ public class BundleSiteInitializerTest {
 
 		Assert.assertNotNull(organization3);
 		Assert.assertEquals("Test Organization 3", organization3.getName());
+		_assertUserOrganizations(organization3.getId(), serviceContext, 0);
 	}
 
 	private void _assertPermissions(Group group) throws Exception {
@@ -1194,6 +1197,27 @@ public class BundleSiteInitializerTest {
 
 		Page<UserAccount> page = userAccountResource.getAccountUserAccountsPage(
 			accountId, null, null, null, null);
+
+		Assert.assertNotNull(page);
+		Assert.assertEquals(totalCount, page.getTotalCount());
+	}
+
+	private void _assertUserOrganizations(
+			String organizationId, ServiceContext serviceContext,
+			int totalCount)
+		throws Exception {
+
+		UserAccountResource.Builder userAccountResourceBuilder =
+			_userAccountResourceFactory.create();
+
+		UserAccountResource userAccountResource =
+			userAccountResourceBuilder.user(
+				serviceContext.fetchUser()
+			).build();
+
+		Page<UserAccount> page =
+			userAccountResource.getOrganizationUserAccountsPage(
+				organizationId, null, null, null, null);
 
 		Assert.assertNotNull(page);
 		Assert.assertEquals(totalCount, page.getTotalCount());

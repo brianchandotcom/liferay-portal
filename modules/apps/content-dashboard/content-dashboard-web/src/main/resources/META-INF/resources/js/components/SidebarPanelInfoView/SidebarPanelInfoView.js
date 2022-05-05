dc5ca5d05737eb8ba9003828d5ddcac99d0c34f5
Cristina González Castellano <cristina.gonzalez@liferay.com>
2022-05-05 09:47:30 +0100

    LPS-158043 Add bell to the frontend.

--- modules/apps/content-dashboard/content-dashboard-web/src/main/resources/META-INF/resources/js/components/SidebarPanelInfoView/SidebarPanelInfoView.js
+++ modules/apps/content-dashboard/content-dashboard-web/src/main/resources/META-INF/resources/js/components/SidebarPanelInfoView/SidebarPanelInfoView.js
@@ -58,6 +58,7 @@ const SidebarPanelInfoView = ({
 	languageTag = 'en',
 	modifiedDate,
 	specificFields = {},
+	subscribe,
 	subType,
 	tags = [],
 	title,
@@ -104,6 +105,21 @@ const SidebarPanelInfoView = ({
 	return (
 		<>
 			<Sidebar.Header title={title} />
+			{subscribe && (
+				<>
+					<ClayLink
+						borderless
+						data-tooltip-align="top"
+						displayType="secondary"
+						href={subscribe.url}
+						monospaced
+						outline
+						title={subscribe.label}
+					>
+						<ClayIcon symbol={subscribe.icon} />
+					</ClayLink>
+				</>
+			)}
 
 			<Sidebar.Body className="px-0">
 				{error && (

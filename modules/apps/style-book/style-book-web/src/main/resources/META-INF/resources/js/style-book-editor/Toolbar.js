25390fe6ac8b7646729c248b6093447a02cbe830
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2022-05-11 11:48:48 +0200

    LPS-153140 Add SET_DRAFT_STATUS action

--- modules/apps/style-book/style-book-web/src/main/resources/META-INF/resources/js/style-book-editor/Toolbar.js
+++ modules/apps/style-book/style-book-web/src/main/resources/META-INF/resources/js/style-book-editor/Toolbar.js
@@ -17,11 +17,11 @@ import ClayLayout from '@clayui/layout';
 import ClayPopover from '@clayui/popover';
 import classNames from 'classnames';
 import {ALIGN_POSITIONS, align} from 'frontend-js-web';
-import React, {useContext, useLayoutEffect, useRef, useState} from 'react';
+import React, {useLayoutEffect, useRef, useState} from 'react';
 
 import PreviewSelector from './PreviewSelector';
 import PublishButton from './PublishButton';
-import {StyleBookContext, usePreviewLayout} from './StyleBookContext';
+import {useDraftStatus, usePreviewLayout} from './StyleBookContext';
 import Undo from './Undo';
 import {config} from './config';
 import {DRAFT_STATUS} from './constants/draftStatusConstants';
@@ -75,7 +75,7 @@ export default function Toolbar() {
 }
 
 function DraftStatus() {
-	const {draftStatus} = useContext(StyleBookContext);
+	const draftStatus = useDraftStatus();
 
 	return (
 		<div>

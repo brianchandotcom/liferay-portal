5e173d3b7e9493b19f18b5c436ccefd40163ed20
Jürgen Kappler <jurgen.kappler@liferay.com>
2022-05-31 15:17:19 +0200

    LPS-155070 Sticker for basic and input type fragments

--- modules/apps/fragment/fragment-web/src/main/java/com/liferay/fragment/web/internal/servlet/taglib/clay/ContributedFragmentEntryVerticalCard.java
+++ modules/apps/fragment/fragment-web/src/main/java/com/liferay/fragment/web/internal/servlet/taglib/clay/ContributedFragmentEntryVerticalCard.java
@@ -128,6 +128,10 @@ public class ContributedFragmentEntryVerticalCard
 			return "code";
 		}
 
+		if (type == FragmentConstants.TYPE_INPUT) {
+			return "forms";
+		}
+
 		if (type == FragmentConstants.TYPE_REACT) {
 			return "react";
 		}

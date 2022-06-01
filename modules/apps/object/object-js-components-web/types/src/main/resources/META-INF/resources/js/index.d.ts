b7773ec5220abe1b17ced12140d5e2cb96010937
Daniel Bonasser <daniel.bonasser@liferay.com>
2022-06-01 19:14:04 -0300

    LPS-153278 Create Notification Template component

--- modules/apps/object/object-js-components-web/types/src/main/resources/META-INF/resources/js/index.d.ts
+++ modules/apps/object/object-js-components-web/types/src/main/resources/META-INF/resources/js/index.d.ts
@@ -25,6 +25,8 @@ export {Input} from './components/Input';
 export {InputLocalized} from './components/InputLocalized';
 export {RichTextLocalized} from './components/RichTextLocalized';
 export {Select} from './components/Select';
+export {invalidateRequired, useForm, FormError} from './hooks/useForm';
+export {onActionDropdownItemClick} from './utils/fdsUtil';
 export {
 	closeSidePanel,
 	openToast,

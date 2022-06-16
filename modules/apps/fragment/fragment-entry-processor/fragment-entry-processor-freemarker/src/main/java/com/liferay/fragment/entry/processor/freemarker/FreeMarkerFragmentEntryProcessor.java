8b458be5d774b94eacfcc00584fc51470d651986
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2022-06-16 08:04:00 +0100

    LPS-155876 Inline

--- modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-freemarker/src/main/java/com/liferay/fragment/entry/processor/freemarker/FreeMarkerFragmentEntryProcessor.java
+++ modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-freemarker/src/main/java/com/liferay/fragment/entry/processor/freemarker/FreeMarkerFragmentEntryProcessor.java
@@ -437,9 +437,7 @@ public class FreeMarkerFragmentEntryProcessor
 				Optional<Boolean> decimalOptional =
 					infoField.getAttributeOptional(NumberInfoFieldType.DECIMAL);
 
-				boolean decimal = decimalOptional.orElse(false);
-
-				if (decimal) {
+				if (decimalOptional.orElse(false)) {
 					dataType = "decimal";
 				}
 			}

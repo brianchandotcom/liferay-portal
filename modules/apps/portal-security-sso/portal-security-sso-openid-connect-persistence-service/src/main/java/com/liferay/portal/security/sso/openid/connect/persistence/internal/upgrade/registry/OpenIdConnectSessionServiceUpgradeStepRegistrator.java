52045dca84ef90eaf6f37df1d3cbfa4c5afdae64
arthurchan35 <arthurchan35@gmail.com>
2022-07-09 12:44:58 +0800

    Revert "LPS-155600 Use UpgradeStepFactory API"
    This reverts commit 7b55bf171c744bfe22b7ef2b11149784adab3d50.

--- modules/apps/portal-security-sso/portal-security-sso-openid-connect-persistence-service/src/main/java/com/liferay/portal/security/sso/openid/connect/persistence/internal/upgrade/registry/OpenIdConnectSessionServiceUpgradeStepRegistrator.java
+++ modules/apps/portal-security-sso/portal-security-sso-openid-connect-persistence-service/src/main/java/com/liferay/portal/security/sso/openid/connect/persistence/internal/upgrade/registry/OpenIdConnectSessionServiceUpgradeStepRegistrator.java
@@ -15,7 +15,6 @@
 package com.liferay.portal.security.sso.openid.connect.persistence.internal.upgrade.registry;
 
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
-import com.liferay.portal.upgrade.step.util.UpgradeStepFactory;
 
 import org.osgi.service.cm.ConfigurationAdmin;
 import org.osgi.service.component.annotations.Component;
@@ -37,17 +36,9 @@ public class OpenIdConnectSessionServiceUpgradeStepRegistrator
 
 		registry.register(
 			"1.1.0", "2.0.0",
-			UpgradeStepFactory.addColumns(
-				"OpenIdConnectSession",
-				"authServerWellKnownURI VARCHAR(256) null",
-				"clientId VARCHAR(256) null"),
-			UpgradeStepFactory.dropColumns(
-				"OpenIdConnectSession", "providerName"),
 			new com.liferay.portal.security.sso.openid.connect.persistence.
 				internal.upgrade.v2_0_0.OpenIdConnectSessionUpgradeProcess(
-					_configurationAdmin),
-			UpgradeStepFactory.dropColumns(
-				"OpenIdConnectSession", "configurationPid"));
+					_configurationAdmin));
 	}
 
 	@Reference

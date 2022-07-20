217aa6398004be5a1867d68227d18127581ca4ab
Matuzalém Teles <matuzalemteles@gmail.com>
2022-07-20 17:16:04 -0500

    LPS-158096 updates tests and some components with accessibility improvements for ClayDropDown

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/common/components/ColorPicker/ColorPicker.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/common/components/ColorPicker/ColorPicker.js
@@ -384,7 +384,7 @@ export function ColorPicker({
 															) =>
 																event.preventDefault()
 															}
-															role="option"
+															roleItem="option"
 															value={token.label}
 														/>
 													)

brandonyee333/test

LPS-140227 buildService

modules/apps/object/object-api/src/main/java/com/liferay/object/service/ObjectRelationshipLocalService.java
@@ -318,6 +318,10 @@ public interface ObjectRelationshipLocalService
 	public List<ObjectRelationship> getObjectRelationships(
 		long objectDefinitionId1, long objectDefinition2, String type);
 
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public List<ObjectRelationship> getObjectRelationships(
+		long objectDefinitionId1, String deletionType, boolean reverse);
+
 	/**
 	 * Returns the number of object relationships.
 	 *
@@ -340,6 +344,10 @@ public interface ObjectRelationshipLocalService
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public PersistedModel getPersistedModel(Serializable primaryKeyObj)
 		throws PortalException;
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public boolean hasDeletionType(
+		long objectDefinitionId1, String deletionType, boolean reverse);
 
 	@Indexable(type = IndexableType.REINDEX)
 	public ObjectRelationship updateObjectRelationship(

modules/apps/object/object-api/src/main/java/com/liferay/object/service/ObjectRelationshipLocalServiceUtil.java
@@ -381,6 +381,13 @@ public class ObjectRelationshipLocalServiceUtil {
 			objectDefinitionId1, objectDefinition2, type);
 	}
 
+	public static List<ObjectRelationship> getObjectRelationships(
+		long objectDefinitionId1, String deletionType, boolean reverse) {
+
+		return getService().getObjectRelationships(
+			objectDefinitionId1, deletionType, reverse);
+	}
+
 	/**
 	 * Returns the number of object relationships.
 	 *
@@ -406,6 +413,13 @@ public class ObjectRelationshipLocalServiceUtil {
 		throws PortalException {
 
 		return getService().getPersistedModel(primaryKeyObj);
+	}
+
+	public static boolean hasDeletionType(
+		long objectDefinitionId1, String deletionType, boolean reverse) {
+
+		return getService().hasDeletionType(
+			objectDefinitionId1, deletionType, reverse);
 	}
 
 	public static ObjectRelationship updateObjectRelationship(

brianchandotcom/liferay-portal

modules/apps/object/object-api/src/main/java/com/liferay/object/service/ObjectRelationshipLocalService.java
@@ -318,6 +318,10 @@ public interface ObjectRelationshipLocalService
 	public List<ObjectRelationship> getObjectRelationships(
 		long objectDefinitionId1, long objectDefinition2, String type);
 
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public List<ObjectRelationship> getObjectRelationships(
+		long objectDefinitionId1, String deletionType, boolean reverse);
+
 	/**
 	 * Returns the number of object relationships.
 	 *
@@ -340,6 +344,10 @@ public interface ObjectRelationshipLocalService
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public PersistedModel getPersistedModel(Serializable primaryKeyObj)
 		throws PortalException;
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public boolean hasObjectRelationshipByDeletionType(
+		long objectDefinitionId1, String deletionType, boolean reverse);
 
 	@Indexable(type = IndexableType.REINDEX)
 	public ObjectRelationship updateObjectRelationship(

modules/apps/object/object-api/src/main/java/com/liferay/object/service/ObjectRelationshipLocalServiceUtil.java
@@ -381,6 +381,13 @@ public class ObjectRelationshipLocalServiceUtil {
 			objectDefinitionId1, objectDefinition2, type);
 	}
 
+	public static List<ObjectRelationship> getObjectRelationships(
+		long objectDefinitionId1, String deletionType, boolean reverse) {
+
+		return getService().getObjectRelationships(
+			objectDefinitionId1, deletionType, reverse);
+	}
+
 	/**
 	 * Returns the number of object relationships.
 	 *
@@ -406,6 +413,13 @@ public class ObjectRelationshipLocalServiceUtil {
 		throws PortalException {
 
 		return getService().getPersistedModel(primaryKeyObj);
+	}
+
+	public static boolean hasObjectRelationshipByDeletionType(
+		long objectDefinitionId1, String deletionType, boolean reverse) {
+
+		return getService().hasObjectRelationshipByDeletionType(
+			objectDefinitionId1, deletionType, reverse);
 	}
 
 	public static ObjectRelationship updateObjectRelationship(
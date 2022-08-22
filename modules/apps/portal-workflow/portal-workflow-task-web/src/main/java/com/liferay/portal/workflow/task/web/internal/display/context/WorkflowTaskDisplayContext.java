ea037d1ff883141acc6b2ee1b1d209fdc02c2586
Feliphe Marinho <feliphe.marinho@liferay.com>
2022-08-22 11:17:28 -0300

    LPS-146600 Rename and avoid duplications

--- modules/apps/portal-workflow/portal-workflow-task-web/src/main/java/com/liferay/portal/workflow/task/web/internal/display/context/WorkflowTaskDisplayContext.java
+++ modules/apps/portal-workflow/portal-workflow-task-web/src/main/java/com/liferay/portal/workflow/task/web/internal/display/context/WorkflowTaskDisplayContext.java
@@ -613,6 +613,14 @@ public class WorkflowTaskDisplayContext {
 			_workflowTaskRequestHelper.getRequest(), "nobody");
 	}
 
+	public List<WorkflowTransition> getWorkflowTaskWorkflowTransitions(
+			WorkflowTask workflowTask)
+		throws PortalException {
+
+		return WorkflowTaskManagerUtil.getWorkflowTaskWorkflowTransitions(
+			workflowTask.getWorkflowTaskId());
+	}
+
 	public String getWorkflowTransitionLabel(
 		WorkflowTransition workflowTransition) {
 
@@ -630,14 +638,6 @@ public class WorkflowTaskDisplayContext {
 
 		return LanguageUtil.get(
 			_workflowTaskRequestHelper.getRequest(), _PROCEED_KEY);
-	}
-
-	public List<WorkflowTransition> getWorkflowTransitions(
-			WorkflowTask workflowTask)
-		throws PortalException {
-
-		return WorkflowTaskManagerUtil.getNextWorkflowTransitions(
-			workflowTask.getWorkflowTaskId());
 	}
 
 	public boolean hasAssignableUsers(WorkflowTask workflowTask)

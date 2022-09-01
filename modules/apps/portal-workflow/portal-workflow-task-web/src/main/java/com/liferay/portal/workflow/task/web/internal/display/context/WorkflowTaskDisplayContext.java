eaa218ee092ab39df74de57cc994c7b091b8dc29
Feliphe Marinho <feliphe.marinho@liferay.com>
2022-09-01 20:11:44 -0300

    LPS-146600 Rename

--- modules/apps/portal-workflow/portal-workflow-task-web/src/main/java/com/liferay/portal/workflow/task/web/internal/display/context/WorkflowTaskDisplayContext.java
+++ modules/apps/portal-workflow/portal-workflow-task-web/src/main/java/com/liferay/portal/workflow/task/web/internal/display/context/WorkflowTaskDisplayContext.java
@@ -442,7 +442,8 @@ public class WorkflowTaskDisplayContext {
 				PortalUtil.getUserName(
 					workflowLog.getAuditUserId(),
 					String.valueOf(workflowLog.getAuditUserId()))),
-			workflowLog.getStateLabel(_workflowTaskRequestHelper.getLocale())
+			workflowLog.getCurrentWorkflowNodeLabel(
+				_workflowTaskRequestHelper.getLocale())
 		};
 	}
 
@@ -481,9 +482,10 @@ public class WorkflowTaskDisplayContext {
 				PortalUtil.getUserName(
 					workflowLog.getAuditUserId(),
 					String.valueOf(workflowLog.getAuditUserId()))),
-			workflowLog.getPreviousStateLabel(
+			workflowLog.getPreviousWorkflowNodeLabel(
 				_workflowTaskRequestHelper.getLocale()),
-			workflowLog.getStateLabel(_workflowTaskRequestHelper.getLocale())
+			workflowLog.getCurrentWorkflowNodeLabel(
+				_workflowTaskRequestHelper.getLocale())
 		};
 	}
 

5a2fd584bc9b5576f00907a36a4d77c4750d4fe7
Selton Guedes <sfgs@ecomp.poli.br>
2022-09-05 10:38:53 -0300

    LPS-161025 Create static classes in ObjectLayoutBoxException

---
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/exception/ObjectLayoutBoxException.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.object.exception;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.util.StringBundler;
+
+/**
+ * @author Paulo Albuquerque
+ * @author Selton Guedes
+ */
+public class ObjectLayoutBoxException extends PortalException {
+
+	public static class MustBeDefaultStorageType
+		extends ObjectLayoutBoxException {
+
+		public MustBeDefaultStorageType(String name) {
+			super(
+				StringBundler.concat(
+					"The ", name, " layout box only can be used in object ",
+					"definitions with default storage type"));
+		}
+
+	}
+
+	public static class MustBeEnabled extends ObjectLayoutBoxException {
+
+		public MustBeEnabled(String name) {
+			super(
+				StringBundler.concat(
+					"The ", name, " layout box must be enabled on the object ",
+					"definition to be used"));
+		}
+
+	}
+
+	public static class MustNotHaveObjectLayoutRows
+		extends ObjectLayoutBoxException {
+
+		public MustNotHaveObjectLayoutRows(String name) {
+			super(
+				StringBundler.concat(
+					"The ", name, " layout box must not have layout rows"));
+		}
+
+	}
+
+	public static class MustOnlyContainOnePerLayout
+		extends ObjectLayoutBoxException {
+
+		public MustOnlyContainOnePerLayout(String name) {
+			super(
+				StringBundler.concat(
+					"There can only be one ", name, " layout box per layout"));
+		}
+
+	}
+
+	public static class RequiredType extends ObjectLayoutBoxException {
+
+		public RequiredType() {
+			super("No value defined for type");
+		}
+
+	}
+
+	private ObjectLayoutBoxException(String msg) {
+		super(msg);
+	}
+
+}

3fc457dd4673a8b5a4f9f589eae317d1960bc9d9
Brian Wulbern <brian.wulbern@liferay.com>
2022-09-16 15:13:31 +0800

    LRQA-76996 buildService

--- modules/util/portal-tools-service-builder-test-service/src/main/java/com/liferay/portal/tools/service/builder/test/service/base/RedundantIndexEntryLocalServiceBaseImpl.java
+++ modules/util/portal-tools-service-builder-test-service/src/main/java/com/liferay/portal/tools/service/builder/test/service/base/RedundantIndexEntryLocalServiceBaseImpl.java
@@ -28,6 +28,8 @@ import com.liferay.portal.kernel.dao.orm.IndexableActionableDynamicQuery;
 import com.liferay.portal.kernel.dao.orm.Projection;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.PersistedModel;
 import com.liferay.portal.kernel.module.framework.service.IdentifiableOSGiService;
 import com.liferay.portal.kernel.search.Indexable;
@@ -335,6 +337,11 @@ public abstract class RedundantIndexEntryLocalServiceBaseImpl
 	public PersistedModel deletePersistedModel(PersistedModel persistedModel)
 		throws PortalException {
 
+		if (_log.isWarnEnabled()) {
+			_log.warn(
+				"Implement RedundantIndexEntryLocalServiceImpl#deleteRedundantIndexEntry(RedundantIndexEntry) to avoid orphaned data");
+		}
+
 		return redundantIndexEntryLocalService.deleteRedundantIndexEntry(
 			(RedundantIndexEntry)persistedModel);
 	}
@@ -551,6 +558,9 @@ public abstract class RedundantIndexEntryLocalServiceBaseImpl
 	)
 	protected com.liferay.counter.kernel.service.CounterLocalService
 		counterLocalService;
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		RedundantIndexEntryLocalServiceBaseImpl.class);
 
 	@ServiceReference(type = PersistedModelLocalServiceRegistry.class)
 	protected PersistedModelLocalServiceRegistry

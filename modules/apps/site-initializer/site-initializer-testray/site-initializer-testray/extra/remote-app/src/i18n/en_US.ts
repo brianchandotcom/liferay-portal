592193a91e09c80be8a308fd590aeb9b42b99a82
Elvinho <elvison.victor@liferay.com>
2022-10-07 15:01:33 -0300

    LPS-165064 - Created a New Build From an Existing Template
    Co-authored-by: Rafael Ferreira <rafaelfs98@users.noreply.github.com>

--- modules/apps/site-initializer/site-initializer-testray/site-initializer-testray/extra/remote-app/src/i18n/en_US.ts
+++ modules/apps/site-initializer/site-initializer-testray/site-initializer-testray/extra/remote-app/src/i18n/en_US.ts
@@ -257,6 +257,7 @@ export default {
 	'screen-name': 'Screen Name',
 	'search': 'Search',
 	'search-in': 'Search in...',
+	'search-x': 'Search {0}',
 	'select-build': 'Select Build',
 	'select-case-parameters': 'Select Case Parameters',
 	'select-cases': 'Select Cases',

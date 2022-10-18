bbe4dfaa5b93e30912dbd0f67f2a26dc698fa438
Feliphe Marinho <feliphe.marinho@liferay.com>
2022-10-18 14:24:59 -0300

    LPS-165697 Build Rest

--- modules/apps/notification/notification-rest-test/src/testIntegration/java/com/liferay/notification/rest/resource/v1_0/test/BaseNotificationTemplateResourceTestCase.java
+++ modules/apps/notification/notification-rest-test/src/testIntegration/java/com/liferay/notification/rest/resource/v1_0/test/BaseNotificationTemplateResourceTestCase.java
@@ -191,11 +191,9 @@ public abstract class BaseNotificationTemplateResourceTestCase {
 		NotificationTemplate notificationTemplate =
 			randomNotificationTemplate();
 
-		notificationTemplate.setBcc(regex);
-		notificationTemplate.setCc(regex);
 		notificationTemplate.setDescription(regex);
-		notificationTemplate.setFrom(regex);
 		notificationTemplate.setName(regex);
+		notificationTemplate.setRecipientType(regex);
 		notificationTemplate.setType(regex);
 
 		String json = NotificationTemplateSerDes.toJSON(notificationTemplate);
@@ -204,11 +202,9 @@ public abstract class BaseNotificationTemplateResourceTestCase {
 
 		notificationTemplate = NotificationTemplateSerDes.toDTO(json);
 
-		Assert.assertEquals(regex, notificationTemplate.getBcc());
-		Assert.assertEquals(regex, notificationTemplate.getCc());
 		Assert.assertEquals(regex, notificationTemplate.getDescription());
-		Assert.assertEquals(regex, notificationTemplate.getFrom());
 		Assert.assertEquals(regex, notificationTemplate.getName());
+		Assert.assertEquals(regex, notificationTemplate.getRecipientType());
 		Assert.assertEquals(regex, notificationTemplate.getType());
 	}
 
@@ -981,14 +977,6 @@ public abstract class BaseNotificationTemplateResourceTestCase {
 				continue;
 			}
 
-			if (Objects.equals("bcc", additionalAssertFieldName)) {
-				if (notificationTemplate.getBcc() == null) {
-					valid = false;
-				}
-
-				continue;
-			}
-
 			if (Objects.equals("body", additionalAssertFieldName)) {
 				if (notificationTemplate.getBody() == null) {
 					valid = false;
@@ -997,32 +985,8 @@ public abstract class BaseNotificationTemplateResourceTestCase {
 				continue;
 			}
 
-			if (Objects.equals("cc", additionalAssertFieldName)) {
-				if (notificationTemplate.getCc() == null) {
-					valid = false;
-				}
-
-				continue;
-			}
-
 			if (Objects.equals("description", additionalAssertFieldName)) {
 				if (notificationTemplate.getDescription() == null) {
-					valid = false;
-				}
-
-				continue;
-			}
-
-			if (Objects.equals("from", additionalAssertFieldName)) {
-				if (notificationTemplate.getFrom() == null) {
-					valid = false;
-				}
-
-				continue;
-			}
-
-			if (Objects.equals("fromName", additionalAssertFieldName)) {
-				if (notificationTemplate.getFromName() == null) {
 					valid = false;
 				}
 
@@ -1063,16 +1027,16 @@ public abstract class BaseNotificationTemplateResourceTestCase {
 				continue;
 			}
 
-			if (Objects.equals("subject", additionalAssertFieldName)) {
-				if (notificationTemplate.getSubject() == null) {
+			if (Objects.equals("recipients", additionalAssertFieldName)) {
+				if (notificationTemplate.getRecipients() == null) {
 					valid = false;
 				}
 
 				continue;
 			}
 
-			if (Objects.equals("to", additionalAssertFieldName)) {
-				if (notificationTemplate.getTo() == null) {
+			if (Objects.equals("subject", additionalAssertFieldName)) {
+				if (notificationTemplate.getSubject() == null) {
 					valid = false;
 				}
 
@@ -1206,32 +1170,10 @@ public abstract class BaseNotificationTemplateResourceTestCase {
 				continue;
 			}
 
-			if (Objects.equals("bcc", additionalAssertFieldName)) {
-				if (!Objects.deepEquals(
-						notificationTemplate1.getBcc(),
-						notificationTemplate2.getBcc())) {
-
-					return false;
-				}
-
-				continue;
-			}
-
 			if (Objects.equals("body", additionalAssertFieldName)) {
 				if (!equals(
 						(Map)notificationTemplate1.getBody(),
 						(Map)notificationTemplate2.getBody())) {
-
-					return false;
-				}
-
-				continue;
-			}
-
-			if (Objects.equals("cc", additionalAssertFieldName)) {
-				if (!Objects.deepEquals(
-						notificationTemplate1.getCc(),
-						notificationTemplate2.getCc())) {
 
 					return false;
 				}
@@ -1265,28 +1207,6 @@ public abstract class BaseNotificationTemplateResourceTestCase {
 				if (!Objects.deepEquals(
 						notificationTemplate1.getDescription(),
 						notificationTemplate2.getDescription())) {
-
-					return false;
-				}
-
-				continue;
-			}
-
-			if (Objects.equals("from", additionalAssertFieldName)) {
-				if (!Objects.deepEquals(
-						notificationTemplate1.getFrom(),
-						notificationTemplate2.getFrom())) {
-
-					return false;
-				}
-
-				continue;
-			}
-
-			if (Objects.equals("fromName", additionalAssertFieldName)) {
-				if (!equals(
-						(Map)notificationTemplate1.getFromName(),
-						(Map)notificationTemplate2.getFromName())) {
 
 					return false;
 				}
@@ -1351,10 +1271,10 @@ public abstract class BaseNotificationTemplateResourceTestCase {
 				continue;
 			}
 
-			if (Objects.equals("subject", additionalAssertFieldName)) {
-				if (!equals(
-						(Map)notificationTemplate1.getSubject(),
-						(Map)notificationTemplate2.getSubject())) {
+			if (Objects.equals("recipients", additionalAssertFieldName)) {
+				if (!Objects.deepEquals(
+						notificationTemplate1.getRecipients(),
+						notificationTemplate2.getRecipients())) {
 
 					return false;
 				}
@@ -1362,10 +1282,10 @@ public abstract class BaseNotificationTemplateResourceTestCase {
 				continue;
 			}
 
-			if (Objects.equals("to", additionalAssertFieldName)) {
+			if (Objects.equals("subject", additionalAssertFieldName)) {
 				if (!equals(
-						(Map)notificationTemplate1.getTo(),
-						(Map)notificationTemplate2.getTo())) {
+						(Map)notificationTemplate1.getSubject(),
+						(Map)notificationTemplate2.getSubject())) {
 
 					return false;
 				}
@@ -1492,25 +1412,9 @@ public abstract class BaseNotificationTemplateResourceTestCase {
 				"Invalid entity field " + entityFieldName);
 		}
 
-		if (entityFieldName.equals("bcc")) {
-			sb.append("'");
-			sb.append(String.valueOf(notificationTemplate.getBcc()));
-			sb.append("'");
-
-			return sb.toString();
-		}
-
 		if (entityFieldName.equals("body")) {
 			throw new IllegalArgumentException(
 				"Invalid entity field " + entityFieldName);
-		}
-
-		if (entityFieldName.equals("cc")) {
-			sb.append("'");
-			sb.append(String.valueOf(notificationTemplate.getCc()));
-			sb.append("'");
-
-			return sb.toString();
 		}
 
 		if (entityFieldName.equals("dateCreated")) {
@@ -1589,19 +1493,6 @@ public abstract class BaseNotificationTemplateResourceTestCase {
 			return sb.toString();
 		}
 
-		if (entityFieldName.equals("from")) {
-			sb.append("'");
-			sb.append(String.valueOf(notificationTemplate.getFrom()));
-			sb.append("'");
-
-			return sb.toString();
-		}
-
-		if (entityFieldName.equals("fromName")) {
-			throw new IllegalArgumentException(
-				"Invalid entity field " + entityFieldName);
-		}
-
 		if (entityFieldName.equals("id")) {
 			throw new IllegalArgumentException(
 				"Invalid entity field " + entityFieldName);
@@ -1626,16 +1517,19 @@ public abstract class BaseNotificationTemplateResourceTestCase {
 		}
 
 		if (entityFieldName.equals("recipientType")) {
+			sb.append("'");
+			sb.append(String.valueOf(notificationTemplate.getRecipientType()));
+			sb.append("'");
+
+			return sb.toString();
+		}
+
+		if (entityFieldName.equals("recipients")) {
 			throw new IllegalArgumentException(
 				"Invalid entity field " + entityFieldName);
 		}
 
 		if (entityFieldName.equals("subject")) {
-			throw new IllegalArgumentException(
-				"Invalid entity field " + entityFieldName);
-		}
-
-		if (entityFieldName.equals("to")) {
 			throw new IllegalArgumentException(
 				"Invalid entity field " + entityFieldName);
 		}
@@ -1694,16 +1588,15 @@ public abstract class BaseNotificationTemplateResourceTestCase {
 
 		return new NotificationTemplate() {
 			{
-				bcc = StringUtil.toLowerCase(RandomTestUtil.randomString());
-				cc = StringUtil.toLowerCase(RandomTestUtil.randomString());
 				dateCreated = RandomTestUtil.nextDate();
 				dateModified = RandomTestUtil.nextDate();
 				description = StringUtil.toLowerCase(
 					RandomTestUtil.randomString());
-				from = StringUtil.toLowerCase(RandomTestUtil.randomString());
 				id = RandomTestUtil.randomLong();
 				name = StringUtil.toLowerCase(RandomTestUtil.randomString());
 				objectDefinitionId = RandomTestUtil.randomLong();
+				recipientType = StringUtil.toLowerCase(
+					RandomTestUtil.randomString());
 				type = StringUtil.toLowerCase(RandomTestUtil.randomString());
 			}
 		};

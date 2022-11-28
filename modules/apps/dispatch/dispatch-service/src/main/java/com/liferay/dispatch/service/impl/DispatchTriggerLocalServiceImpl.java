5d868a0acbbdf0aded3789d73808862fd1e5e6bd
Igor Beslic <igor.beslic@liferay.com>
2022-11-28 17:43:10 +0100

    LPS-167050 dispatch-service - throw exception generated by service builder

--- modules/apps/dispatch/dispatch-service/src/main/java/com/liferay/dispatch/service/impl/DispatchTriggerLocalServiceImpl.java
+++ modules/apps/dispatch/dispatch-service/src/main/java/com/liferay/dispatch/service/impl/DispatchTriggerLocalServiceImpl.java
@@ -17,8 +17,8 @@ package com.liferay.dispatch.service.impl;
 import com.liferay.dispatch.exception.DispatchTriggerEndDateException;
 import com.liferay.dispatch.exception.DispatchTriggerNameException;
 import com.liferay.dispatch.exception.DispatchTriggerStartDateException;
+import com.liferay.dispatch.exception.DispatchTriggerTypeException;
 import com.liferay.dispatch.exception.DuplicateDispatchTriggerException;
-import com.liferay.dispatch.exception.NoSuchDispatchTaskExecutor;
 import com.liferay.dispatch.executor.DispatchTaskClusterMode;
 import com.liferay.dispatch.executor.DispatchTaskExecutor;
 import com.liferay.dispatch.executor.DispatchTaskExecutorRegistry;
@@ -378,9 +378,9 @@ public class DispatchTriggerLocalServiceImpl
 				dispatchTaskExecutorType);
 
 		if (dispatchTaskExecutor == null) {
-			throw new NoSuchDispatchTaskExecutor(
+			throw new DispatchTriggerTypeException(
 				StringBundler.concat(
-					"Unable to get dispatch task executor type for \"",
+					"Unknown dispatch task executor type \"",
 					dispatchTaskExecutorType, "\""));
 		}
 	}

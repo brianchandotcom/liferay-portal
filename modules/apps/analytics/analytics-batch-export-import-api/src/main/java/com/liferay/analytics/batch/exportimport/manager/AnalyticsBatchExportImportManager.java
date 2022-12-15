827fd408f7867103fad350f82de3f59f570f1a5c
Riccardo Ferrari <riccardo.ferrari@liferay.com>
2022-12-15 13:01:14 +0100

    LRAC-12608 analytics-batch-export-import-api Support custom filtering

--- modules/apps/analytics/analytics-batch-export-import-api/src/main/java/com/liferay/analytics/batch/exportimport/manager/AnalyticsBatchExportImportManager.java
+++ modules/apps/analytics/analytics-batch-export-import-api/src/main/java/com/liferay/analytics/batch/exportimport/manager/AnalyticsBatchExportImportManager.java
@@ -27,7 +27,7 @@ public interface AnalyticsBatchExportImportManager {
 
 	public void exportToAnalyticsCloud(
 			String batchEngineExportTaskItemDelegateName, long companyId,
-			List<String> fieldNamesList,
+			List<String> fieldNamesList, String filter,
 			UnsafeConsumer<String, Exception> notificationUnsafeConsumer,
 			Date resourceLastModifiedDate, String resourceName, long userId)
 		throws Exception;

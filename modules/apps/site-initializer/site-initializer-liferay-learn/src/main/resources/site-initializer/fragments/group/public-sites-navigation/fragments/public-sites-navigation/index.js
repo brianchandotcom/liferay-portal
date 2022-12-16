5b8f779ca0723883d9b340daf4190fe17993a92f
Allen Ziegenfus <allen.ziegenfus@liferay.com>
2022-12-16 13:22:58 -0600

    ISSITES-5874 SF

--- modules/apps/site-initializer/site-initializer-liferay-learn/src/main/resources/site-initializer/fragments/group/public-sites-navigation/fragments/public-sites-navigation/index.js
+++ modules/apps/site-initializer/site-initializer-liferay-learn/src/main/resources/site-initializer/fragments/group/public-sites-navigation/fragments/public-sites-navigation/index.js
@@ -15,11 +15,11 @@
 /* eslint-disable no-undef */
 
 adtSpatialNavigationProvider = new navigation.default.SpatialNavigationProvider(
-    '.adt-submenu-item-link'
+	'.adt-submenu-item-link'
 );
 
 spatialNavigationProvider = new navigation.default.SpatialNavigationProvider(
-    '.adt-nav-text'
+	'.adt-nav-text'
 );
 
 const primaryNav = fragmentElement.querySelector('.primary-nav');
@@ -40,7 +40,6 @@ window.addEventListener('load', () => {
 		true
 	);
 
-
 	new navigation.default.DropdownProvider(
 		'.menu-button-group',
 		'.menu-button-group',
@@ -54,19 +53,18 @@ window.addEventListener('load', () => {
 		true
 	);
 
-    new navigation.default.DropdownProvider(
-        '.adt-nav-text',
-        '.adt-submenu',
-        'dropdown-open',
-        false,
-        (menu) => {
-            adtSpatialNavigationProvider.addFocusableClasses(menu);
-
-        },
-        (menu) => {
-            adtSpatialNavigationProvider.removeFocusableClasses(menu);
-        }
-    );
+	new navigation.default.DropdownProvider(
+		'.adt-nav-text',
+		'.adt-submenu',
+		'dropdown-open',
+		false,
+		(menu) => {
+			adtSpatialNavigationProvider.addFocusableClasses(menu);
+		},
+		(menu) => {
+			adtSpatialNavigationProvider.removeFocusableClasses(menu);
+		}
+	);
 
 	new navigation.default.DropdownProvider(
 		'.language',

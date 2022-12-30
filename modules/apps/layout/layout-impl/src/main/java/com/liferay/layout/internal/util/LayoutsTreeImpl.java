dde042fd57f20091d8c79139f55f6f3c71aa74e2
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2022-12-30 13:20:11 +0100

    LPS-169356 Don't link to a page that will result in a 404 response because the layout hasn't any published version and either the user doesn't have update permission

--- modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/util/LayoutsTreeImpl.java
+++ modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/util/LayoutsTreeImpl.java
@@ -666,7 +666,14 @@ public class LayoutsTreeImpl implements LayoutsTree {
 			).put(
 				"privateLayout", layout.isPrivateLayout()
 			).put(
-				"regularURL", layout.getRegularURL(httpServletRequest)
+				"regularURL",
+				() -> {
+					if (hasUpdatePermission || layout.isPublished()) {
+						return layout.getRegularURL(httpServletRequest);
+					}
+
+					return StringPool.BLANK;
+				}
 			).put(
 				"sortable",
 				hasManageLayoutsPermission && !mobile &&

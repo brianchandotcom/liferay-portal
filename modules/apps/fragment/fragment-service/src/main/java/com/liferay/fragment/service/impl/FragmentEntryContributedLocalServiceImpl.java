cd8d65b950d9dc127f1635b543a7299fc5d22559
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2023-01-19 09:58:02 +0100

    LPS-171006 Adds service methods

--- modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/service/impl/FragmentEntryContributedLocalServiceImpl.java
+++ modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/service/impl/FragmentEntryContributedLocalServiceImpl.java
@@ -14,8 +14,11 @@
 
 package com.liferay.fragment.service.impl;
 
+import com.liferay.fragment.model.FragmentEntryContributed;
 import com.liferay.fragment.service.base.FragmentEntryContributedLocalServiceBaseImpl;
 import com.liferay.portal.aop.AopService;
+
+import java.util.Date;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -28,4 +31,43 @@ import org.osgi.service.component.annotations.Component;
 )
 public class FragmentEntryContributedLocalServiceImpl
 	extends FragmentEntryContributedLocalServiceBaseImpl {
+
+	@Override
+	public FragmentEntryContributed addOrUpdateFragmentEntryContributed(
+		String fragmentEntryKey, String css, String html, String js,
+		String configuration, int type) {
+
+		FragmentEntryContributed fragmentEntryContributed =
+			fragmentEntryContributedPersistence.fetchByFragmentEntryKey(
+				fragmentEntryKey);
+
+		if (fragmentEntryContributed == null) {
+			fragmentEntryContributed =
+				fragmentEntryContributedPersistence.create(
+					counterLocalService.increment());
+
+			fragmentEntryContributed.setCreateDate(new Date());
+			fragmentEntryContributed.setFragmentEntryKey(fragmentEntryKey);
+		}
+
+		fragmentEntryContributed.setModifiedDate(new Date());
+		fragmentEntryContributed.setFragmentEntryKey(fragmentEntryKey);
+		fragmentEntryContributed.setCss(css);
+		fragmentEntryContributed.setHtml(html);
+		fragmentEntryContributed.setJs(js);
+		fragmentEntryContributed.setConfiguration(configuration);
+		fragmentEntryContributed.setType(type);
+
+		return fragmentEntryContributedPersistence.update(
+			fragmentEntryContributed);
+	}
+
+	@Override
+	public FragmentEntryContributed fetchByFragmentEntryKey(
+		String fragmentEntryKey) {
+
+		return fragmentEntryContributedPersistence.fetchByFragmentEntryKey(
+			fragmentEntryKey);
+	}
+
 }

brianchandotcom/liferay-portal

commit 19523c8fe43f505008b1663c970ec24b2a329e28

LPS-158021 Implement the preregister portalInstanceLifecycle phase

portal-impl/src/com/liferay/portal/service/impl/CompanyLocalServiceImpl.java
@@ -1442,14 +1442,27 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 		return company;
 	}
 
-	protected void preregisterCompany(long companyId) {
+	protected void preregisterCompany(Company company) {
 		try {
-			SearchEngineHelperUtil.initialize(companyId);
+			SearchEngineHelperUtil.initialize(company.getCompanyId());
 		}
 		catch (Exception exception) {
 			_log.error(
-				"Unable to initialize search engine for company " + companyId,
+				"Unable to initialize search engine for company " +
+					company.getCompanyId(),
 				exception);
+		}
+
+		PortalInstanceLifecycleManager portalInstanceLifecycleManager =
+			_serviceTracker.getService();
+
+		if (portalInstanceLifecycleManager != null) {
+			portalInstanceLifecycleManager.preregisterCompany(company);
+		}
+		else {
+			synchronized (_preregisterPendingCompanies) {
+				_preregisterPendingCompanies.add(company);
+			}
 		}
 	}
 
@@ -1949,7 +1962,7 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 			LocaleThreadLocal.getSiteDefaultLocale();
 
 		try {
-			preregisterCompany(company.getCompanyId());
+			preregisterCompany(company);
 
 			Locale companyDefaultLocale = LocaleUtil.fromLanguageId(
 				PropsValues.COMPANY_DEFAULT_LOCALE);
@@ -2235,6 +2248,8 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 	@BeanReference(type = PortletPersistence.class)
 	private PortletPersistence _portletPersistence;
 
+	private final Set<Company> _preregisterPendingCompanies = new HashSet<>();
+
 	@BeanReference(type = RoleLocalService.class)
 	private RoleLocalService _roleLocalService;
 
@@ -2270,6 +2285,15 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 
 			PortalInstanceLifecycleManager portalInstanceLifecycleManager =
 				_bundleContext.getService(serviceReference);
+
+			synchronized (_preregisterPendingCompanies) {
+				forEachCompany(
+					company -> portalInstanceLifecycleManager.registerCompany(
+						company),
+					new ArrayList<Company>(_preregisterPendingCompanies));
+
+				_preregisterPendingCompanies.clear();
+			}
 
 			synchronized (_pendingCompanies) {
 				forEachCompany(

modules/apps/portal/portal-instance-lifecycle-impl/src/main/java/com/liferay/portal/instance/lifecycle/internal/PortalInstanceLifecycleListenerManagerImpl.java
@@ -52,6 +52,15 @@ public class PortalInstanceLifecycleListenerManagerImpl
 	implements AopService, PortalInstanceLifecycleManager {
 
 	@Override
+	public void preregisterCompany(Company company) {
+		for (PortalInstanceLifecycleListener portalInstanceLifecycleListener :
+				_portalInstanceLifecycleListeners) {
+
+			preregisterCompany(portalInstanceLifecycleListener, company);
+		}
+	}
+
+	@Override
 	public void preunregisterCompany(Company company) {
 		for (PortalInstanceLifecycleListener portalInstanceLifecycleListener :
 				_portalInstanceLifecycleListeners) {
@@ -122,6 +131,9 @@ public class PortalInstanceLifecycleListenerManagerImpl
 					TransactionInvokerUtil.invoke(
 						_transactionConfig,
 						() -> {
+							preregisterCompany(
+								portalInstanceLifecycleListener, company);
+
 							registerCompany(
 								portalInstanceLifecycleListener, company);
 
@@ -133,6 +145,33 @@ public class PortalInstanceLifecycleListenerManagerImpl
 				}
 			},
 			new ArrayList<>(_companies));
+	}
+
+	protected void preregisterCompany(
+		PortalInstanceLifecycleListener portalInstanceLifecycleListener,
+		Company company) {
+
+		if (!(portalInstanceLifecycleListener instanceof Clusterable) &&
+			!clusterMasterExecutor.isMaster()) {
+
+			if (_log.isDebugEnabled()) {
+				_log.debug("Skipping " + portalInstanceLifecycleListener);
+			}
+
+			return;
+		}
+
+		try {
+			portalInstanceLifecycleListener.portalInstancePreregistered(
+				company);
+		}
+		catch (Exception exception) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(
+					"Unable to preregister portal instance " + company,
+					exception);
+			}
+		}
 	}
 
 	protected void preunregisterCompany(

commit f5baa8f66db6d97a2b731d2d367841a9c9c84edb

LPS-173535 Calling the wrong method, caused by 19523c8f

portal-impl/src/com/liferay/portal/service/impl/CompanyLocalServiceImpl.java
@@ -2240,8 +2240,9 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 
 			synchronized (_preregisterPendingCompanies) {
 				forEachCompany(
-					company -> portalInstanceLifecycleManager.registerCompany(
-						company),
+					company ->
+						portalInstanceLifecycleManager.preregisterCompany(
+							company),
 					new ArrayList<Company>(_preregisterPendingCompanies));
 
 				_preregisterPendingCompanies.clear();
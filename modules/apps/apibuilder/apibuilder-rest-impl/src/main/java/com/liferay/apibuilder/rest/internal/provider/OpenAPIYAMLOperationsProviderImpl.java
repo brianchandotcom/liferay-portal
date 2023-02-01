3cd05b9fc43208d11d46cc17af898f5ca20f9d66
Carlos Correa <carlos.correa@liferay.com>
2023-02-01 13:32:51 +0100

    LPS-171473 Add REST API and build an Operation based on a given OpenAPIYAML instance

---
+++ modules/apps/apibuilder/apibuilder-rest-impl/src/main/java/com/liferay/apibuilder/rest/internal/provider/OpenAPIYAMLOperationsProviderImpl.java
@@ -0,0 +1,372 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.apibuilder.rest.internal.provider;
+
+import com.liferay.apibuilder.constants.APIBuilderConstants;
+import com.liferay.apibuilder.operation.MediaType;
+import com.liferay.apibuilder.operation.Operation;
+import com.liferay.apibuilder.operation.PathConfiguration;
+import com.liferay.apibuilder.operation.handler.OperationHandler;
+import com.liferay.apibuilder.rest.provider.OpenAPIYAMLOperationsProvider;
+import com.liferay.apibuilder.util.URLUtil;
+import com.liferay.info.field.InfoField;
+import com.liferay.info.field.type.BooleanInfoFieldType;
+import com.liferay.info.field.type.DateInfoFieldType;
+import com.liferay.info.field.type.InfoFieldType;
+import com.liferay.info.field.type.NumberInfoFieldType;
+import com.liferay.info.field.type.PrimaryKeyInfoFieldType;
+import com.liferay.info.field.type.TextInfoFieldType;
+import com.liferay.info.form.InfoForm;
+import com.liferay.info.item.InfoItemServiceRegistry;
+import com.liferay.info.item.provider.InfoItemFormProvider;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
+import com.liferay.portal.kernel.util.SetUtil;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.vulcan.yaml.openapi.Components;
+import com.liferay.portal.vulcan.yaml.openapi.Content;
+import com.liferay.portal.vulcan.yaml.openapi.FieldDefinition;
+import com.liferay.portal.vulcan.yaml.openapi.Info;
+import com.liferay.portal.vulcan.yaml.openapi.Method;
+import com.liferay.portal.vulcan.yaml.openapi.OpenAPIYAML;
+import com.liferay.portal.vulcan.yaml.openapi.OperationDefinition;
+import com.liferay.portal.vulcan.yaml.openapi.Parameter;
+import com.liferay.portal.vulcan.yaml.openapi.PathItem;
+import com.liferay.portal.vulcan.yaml.openapi.ResponseCode;
+import com.liferay.portal.vulcan.yaml.openapi.Schema;
+import com.liferay.portal.vulcan.yaml.openapi.SchemaDefinition;
+
+import java.io.InvalidObjectException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+import javax.ws.rs.core.Response;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Carlos Correa
+ */
+@Component(service = OpenAPIYAMLOperationsProvider.class)
+public class OpenAPIYAMLOperationsProviderImpl
+	implements OpenAPIYAMLOperationsProvider {
+
+	@Override
+	public List<Operation> getOperations(
+			long companyId, OpenAPIYAML openAPIYAML)
+		throws Exception {
+
+		List<Operation> operations = new ArrayList<>();
+
+		Components components = openAPIYAML.getComponents();
+
+		Map<String, PathItem> pathItems = openAPIYAML.getPathItems();
+
+		Info info = openAPIYAML.getInfo();
+
+		for (Map.Entry<String, PathItem> entry : pathItems.entrySet()) {
+			PathItem pathItem = entry.getValue();
+
+			_validate(pathItem);
+
+			operations.addAll(
+				_getOperations(
+					companyId,
+					URLUtil.getPathConfiguration(
+						entry.getKey(), info.getVersion()),
+					pathItem, components.getSchemas()));
+		}
+
+		return operations;
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_operationHandlerServiceTrackerMap =
+			ServiceTrackerMapFactory.openSingleValueMap(
+				bundleContext, OperationHandler.class,
+				APIBuilderConstants.OPERATION_NAME);
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_operationHandlerServiceTrackerMap.close();
+	}
+
+	private Set<InfoFieldType> _getCompatibleInfoFieldTypes(Schema schema) {
+		String type = schema.getType();
+
+		if (StringUtil.equals(type, "boolean")) {
+			return SetUtil.fromArray(BooleanInfoFieldType.INSTANCE);
+		}
+		else if (StringUtil.equals(type, "integer")) {
+			if (StringUtil.equals(schema.getFormat(), "int64")) {
+				return SetUtil.fromArray(
+					NumberInfoFieldType.INSTANCE,
+					PrimaryKeyInfoFieldType.INSTANCE);
+			}
+			else if (StringUtil.equals(schema.getFormat(), "int32")) {
+				return SetUtil.fromArray(NumberInfoFieldType.INSTANCE);
+			}
+		}
+		else if (StringUtil.equals(type, "string")) {
+			if (StringUtil.equals(schema.getFormat(), "date-time")) {
+				return SetUtil.fromArray(DateInfoFieldType.INSTANCE);
+			}
+
+			return SetUtil.fromArray(TextInfoFieldType.INSTANCE);
+		}
+
+		throw new UnsupportedOperationException(
+			"Impossible to get the InfoFieldType of the type " + type);
+	}
+
+	private Map<String, InfoField> _getInfoFields(
+			String entityName, Map<String, Schema> schemas)
+		throws Exception {
+
+		InfoItemFormProvider<?> infoItemFormProvider =
+			_infoItemServiceRegistry.getFirstInfoItemService(
+				InfoItemFormProvider.class, entityName);
+
+		if (infoItemFormProvider == null) {
+			throw new InvalidObjectException(
+				"InfoItemFormProvider not found for the entity name " +
+					entityName);
+		}
+
+		Map<String, InfoField> infoFields = new HashMap<>();
+
+		InfoForm infoForm = infoItemFormProvider.getInfoForm();
+
+		for (Map.Entry<String, Schema> entry : schemas.entrySet()) {
+			Schema schema = entry.getValue();
+
+			FieldDefinition fieldDefinition = schema.getFieldDefinition();
+
+			if (fieldDefinition == null) {
+				throw new InvalidObjectException(
+					"FieldDefinition not found for the property " +
+						entry.getKey());
+			}
+
+			String internalFieldName = fieldDefinition.getName();
+
+			InfoField infoField = infoForm.getInfoField(internalFieldName);
+
+			if (infoField == null) {
+				throw new InvalidObjectException(
+					StringBundler.concat(
+						"There is no InfoField '", internalFieldName,
+						"' registered for the class name '", entityName, "'"));
+			}
+
+			String externalFieldName = entry.getKey();
+
+			Set<InfoFieldType> compatibleInfoFieldTypes =
+				_getCompatibleInfoFieldTypes(schemas.get(externalFieldName));
+
+			if (!compatibleInfoFieldTypes.contains(
+					infoField.getInfoFieldType())) {
+
+				throw new InvalidObjectException(
+					StringBundler.concat(
+						"Invalid types between the fields ", externalFieldName,
+						" and ", infoField.getName()));
+			}
+
+			infoFields.put(externalFieldName, infoField);
+		}
+
+		return infoFields;
+	}
+
+	private List<Operation> _getOperations(
+			long companyId, PathConfiguration pathConfiguration,
+			PathItem pathItem, Map<String, Schema> schemas)
+		throws Exception {
+
+		List<Operation> operations = new ArrayList<>();
+
+		for (Method method : Method.values()) {
+			com.liferay.portal.vulcan.yaml.openapi.Operation operation =
+				pathItem.get(method);
+
+			if (operation == null) {
+				continue;
+			}
+
+			OperationDefinition operationDefinition =
+				operation.getOperationDefinition();
+
+			Operation.Builder builder = new Operation.Builder(
+			).withCompanyId(
+				companyId
+			).withMethod(
+				com.liferay.apibuilder.operation.Method.valueOf(method.name())
+			).withOperationType(
+				operationDefinition.getType()
+			).withPathConfiguration(
+				pathConfiguration
+			);
+
+			Map<ResponseCode, com.liferay.portal.vulcan.yaml.openapi.Response>
+				responses = operation.getResponses();
+
+			Map<String, InfoField> successfulInfoFields = null;
+
+			for (Map.Entry
+					<ResponseCode,
+					 com.liferay.portal.vulcan.yaml.openapi.Response> entry :
+						responses.entrySet()) {
+
+				com.liferay.portal.vulcan.yaml.openapi.Response response =
+					entry.getValue();
+
+				Map<String, Content> contentMap = response.getContent();
+
+				for (Map.Entry<String, Content> entry2 :
+						contentMap.entrySet()) {
+
+					Content content = entry2.getValue();
+
+					Schema schema = content.getSchema();
+
+					String schemaName = StringUtil.removeFirst(
+						schema.getReference(), "#/components/schemas/");
+
+					schema = schemas.get(schemaName);
+
+					SchemaDefinition schemaDefinition =
+						schema.getSchemaDefinition();
+
+					Map<String, InfoField> infoFields = _getInfoFields(
+						schemaDefinition.getEntityName(),
+						schema.getPropertySchemas());
+
+					com.liferay.apibuilder.operation.response.ResponseCode
+						responseCode = _toResponseCode(entry.getKey());
+
+					if (Objects.equals(
+							com.liferay.apibuilder.operation.response.
+								ResponseCode.SUCCESSFUL,
+							responseCode)) {
+
+						successfulInfoFields = infoFields;
+					}
+
+					builder.withResponse(
+						new com.liferay.apibuilder.operation.Response(
+							schemaDefinition.getEntityName(), infoFields,
+							schemaName),
+						MediaType.parse(entry2.getKey()), responseCode);
+				}
+			}
+
+			if (successfulInfoFields == null) {
+				throw new IllegalStateException(
+					"There is no schema defined for a successful code");
+			}
+
+			for (Parameter operationParameter : operation.getParameters()) {
+				InfoField<?> infoField = successfulInfoFields.get(
+					operationParameter.getName());
+
+				if (infoField == null) {
+					throw new IllegalStateException();
+				}
+
+				builder.withParameterInfoField(
+					infoField, operationParameter.getName());
+			}
+
+			operations.add(builder.build());
+		}
+
+		return operations;
+	}
+
+	private com.liferay.apibuilder.operation.response.ResponseCode
+		_toResponseCode(ResponseCode responseCode) {
+
+		Response.Status.Family family = Response.Status.Family.familyOf(
+			responseCode.getHttpCode());
+
+		if (Objects.equals(family, Response.Status.Family.SUCCESSFUL)) {
+			return com.liferay.apibuilder.operation.response.ResponseCode.
+				SUCCESSFUL;
+		}
+		else if (Objects.equals(family, Response.Status.Family.CLIENT_ERROR)) {
+			return com.liferay.apibuilder.operation.response.ResponseCode.
+				NOT_FOUND;
+		}
+		else if (Objects.equals(family, Response.Status.Family.SERVER_ERROR)) {
+			return com.liferay.apibuilder.operation.response.ResponseCode.ERROR;
+		}
+
+		throw new IllegalStateException();
+	}
+
+	private void _validate(PathItem pathItem) throws Exception {
+		boolean empty = true;
+
+		for (Method method : Method.values()) {
+			com.liferay.portal.vulcan.yaml.openapi.Operation operation =
+				pathItem.get(method);
+
+			if (operation == null) {
+				continue;
+			}
+
+			empty = false;
+
+			OperationDefinition operationDefinition =
+				operation.getOperationDefinition();
+
+			if (operationDefinition == null) {
+				throw new InvalidObjectException(
+					"Missing operation definition");
+			}
+
+			String type = operationDefinition.getType();
+
+			if (!_operationHandlerServiceTrackerMap.containsKey(type)) {
+				throw new InvalidObjectException(
+					"OperationProvider not defined for the type " + type);
+			}
+		}
+
+		if (empty) {
+			throw new InvalidObjectException("There is no operation defined");
+		}
+	}
+
+	@Reference
+	private InfoItemServiceRegistry _infoItemServiceRegistry;
+
+	private ServiceTrackerMap<String, OperationHandler>
+		_operationHandlerServiceTrackerMap;
+
+}
---
+++ modules/apps/apibuilder/apibuilder-rest-impl/src/main/java/com/liferay/apibuilder/rest/internal/resource/APIBuilderResourceImpl.java
@@ -0,0 +1,209 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.apibuilder.rest.internal.resource;
+
+import com.liferay.apibuilder.operation.MediaType;
+import com.liferay.apibuilder.operation.Method;
+import com.liferay.apibuilder.operation.Operation;
+import com.liferay.apibuilder.operation.OperationContext;
+import com.liferay.apibuilder.operation.handler.OperationHandler;
+import com.liferay.apibuilder.operation.provider.OperationProvider;
+import com.liferay.apibuilder.operation.registry.OperationHandlerRegistry;
+import com.liferay.apibuilder.operation.response.NotFoundOperationResponse;
+import com.liferay.apibuilder.operation.response.OperationResponse;
+import com.liferay.apibuilder.operation.response.ResponseCode;
+import com.liferay.apibuilder.operation.response.SuccessfulOperationResponse;
+import com.liferay.apibuilder.rest.internal.dto.converter.APIBuilderElementDTOConverter;
+import com.liferay.apibuilder.rest.resource.APIBuilderResource;
+import com.liferay.apibuilder.util.URLUtil;
+import com.liferay.info.field.InfoField;
+import com.liferay.info.field.InfoFieldValue;
+import com.liferay.info.field.type.PrimaryKeyInfoFieldType;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.vulcan.dto.converter.DTOConverterContext;
+import com.liferay.portal.vulcan.dto.converter.DefaultDTOConverterContext;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.Problem;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import javax.servlet.http.HttpServletRequest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Response;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ServiceScope;
+
+/**
+ * @author Carlos Correa
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.APIBuilder.Application)",
+		"osgi.jaxrs.resource=true"
+	},
+	scope = ServiceScope.PROTOTYPE, service = APIBuilderResource.class
+)
+public class APIBuilderResourceImpl extends BaseAPIBuilderResourceImpl {
+
+	@GET
+	@Path("{any: .*}")
+	@Produces({"application/json", "application/xml"})
+	public Response get() throws Exception {
+		Operation operation = _operationProvider.getOperation(
+			_portal.getCompanyId(contextHttpServletRequest), Method.GET,
+			contextHttpServletRequest.getRequestURI());
+
+		OperationHandler operationHandler =
+			_operationHandlerRegistry.getOperationHandler(operation);
+
+		MediaType mediaType = MediaType.parse(
+			contextHttpServletRequest.getHeader(HttpHeaders.ACCEPT));
+
+		OperationResponse operationResponse = operationHandler.handle(
+			operation, _getOperationContext(mediaType, operation));
+
+		return _toResponse(operationResponse);
+	}
+
+	private DTOConverterContext _getDTOConverterContext(
+		com.liferay.apibuilder.operation.Response response) {
+
+		DTOConverterContext dtoConverterContext =
+			new DefaultDTOConverterContext(null, null, null, null, null);
+
+		dtoConverterContext.setAttribute("response", response);
+
+		return dtoConverterContext;
+	}
+
+	private OperationContext _getOperationContext(
+		MediaType mediaType, Operation operation) {
+
+		OperationContext.Builder builder = new OperationContext.Builder();
+
+		for (InfoFieldValue<?> infoFieldValue :
+				_getParameterInfoFieldValues(
+					operation, contextHttpServletRequest)) {
+
+			InfoField infoField = infoFieldValue.getInfoField();
+
+			if (infoField.getInfoFieldType() instanceof
+					PrimaryKeyInfoFieldType) {
+
+				builder.withPrimaryKeyInfoFieldValue(infoFieldValue);
+			}
+			else {
+				builder.withInfoFieldValue(infoFieldValue);
+			}
+		}
+
+		return builder.withMediaType(
+			mediaType
+		).build();
+	}
+
+	private List<InfoFieldValue<?>> _getParameterInfoFieldValues(
+		Operation operation, HttpServletRequest httpServletRequest) {
+
+		List<InfoFieldValue<?>> infoFieldValues = new ArrayList<>();
+
+		Map<String, String> pathParams = URLUtil.getPathParams(
+			httpServletRequest.getRequestURI(),
+			operation.getPathConfiguration());
+
+		Map<String, InfoField> parameterInfoFields =
+			operation.getParameterInfoFields();
+
+		for (Map.Entry<String, InfoField> entry :
+				parameterInfoFields.entrySet()) {
+
+			InfoField infoField = entry.getValue();
+
+			String externalFieldName = entry.getKey();
+
+			if (!pathParams.containsKey(externalFieldName)) {
+				if (infoField.isRequired()) {
+					throw new IllegalArgumentException();
+				}
+
+				continue;
+			}
+
+			infoFieldValues.add(
+				new InfoFieldValue(
+					infoField, pathParams.get(externalFieldName)));
+		}
+
+		return infoFieldValues;
+	}
+
+	private Response _toResponse(OperationResponse operationResponse)
+		throws Exception {
+
+		if (Objects.equals(
+				operationResponse.getResponseCode(), ResponseCode.SUCCESSFUL)) {
+
+			SuccessfulOperationResponse successfulOperationResponse =
+				(SuccessfulOperationResponse)operationResponse;
+
+			return Response.status(
+				Response.Status.OK
+			).entity(
+				_dtoConverter.toDTO(
+					_getDTOConverterContext(
+						successfulOperationResponse.getResponse()),
+					successfulOperationResponse.getApiBuilderEntry())
+			).build();
+		}
+		else if (Objects.equals(
+					operationResponse.getResponseCode(),
+					ResponseCode.NOT_FOUND)) {
+
+			NotFoundOperationResponse notFoundOperationResponse =
+				(NotFoundOperationResponse)operationResponse;
+
+			return Response.status(
+				Response.Status.NOT_FOUND
+			).entity(
+				new Problem(
+					Response.Status.NOT_FOUND,
+					notFoundOperationResponse.getMessage())
+			).build();
+		}
+
+		throw new UnsupportedOperationException();
+	}
+
+	@Reference
+	private APIBuilderElementDTOConverter _dtoConverter;
+
+	@Reference
+	private OperationHandlerRegistry _operationHandlerRegistry;
+
+	@Reference
+	private OperationProvider _operationProvider;
+
+	@Reference
+	private Portal _portal;
+
+}

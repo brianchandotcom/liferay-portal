brianchandotcom/liferay-portal

commit a313b127aa73822e5856ca3a12ec5d391a62a173

LPS-162237 Log exception instead of swallowing

modules/apps/portal/portal-proxy/src/main/java/com/liferay/portal/proxy/internal/ASMDelegateProxyFactory.java
@@ -17,6 +17,8 @@ package com.liferay.portal.proxy.internal;
 import com.liferay.petra.reflect.ReflectionUtil;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.DelegateProxyFactory;
 import com.liferay.portal.kernel.util.StringUtil;
 
@@ -64,6 +66,10 @@ public class ASMDelegateProxyFactory implements DelegateProxyFactory {
 						asmWrapperClassName);
 				}
 				catch (ClassNotFoundException classNotFoundException) {
+					if (_log.isDebugEnabled()) {
+						_log.debug(classNotFoundException);
+					}
+
 					byte[] classData = _generateASMWrapperClassData(
 						StringUtil.replace(asmWrapperClassName, '.', '/'),
 						interfaceClass, delegateObject, defaultObject);
@@ -174,6 +180,10 @@ public class ASMDelegateProxyFactory implements DelegateProxyFactory {
 					_getClassBinaryName(delegateObjectClass));
 			}
 			catch (NoSuchMethodException noSuchMethodException) {
+				if (_log.isDebugEnabled()) {
+					_log.debug(noSuchMethodException);
+				}
+
 				_generateMethod(
 					classWriter, method, asmWrapperClassBinaryName, "_default",
 					defaultObjectClassDescriptor,
@@ -252,6 +262,9 @@ public class ASMDelegateProxyFactory implements DelegateProxyFactory {
 
 		return StringUtil.replace(className, '.', '/');
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ASMDelegateProxyFactory.class);
 
 	private static final Method _defineClassMethod;
 	private static final Method _equalsMethod;

commit d6807cefe06161eb179f7928531b014087e5387e

LPS-175193 Fix classloading issue in ASMDelegateProxyFactory and its test. Low level infra code is very sensitive to logging. It could cause many classloading issues. Caused by a313b127

modules/apps/portal/portal-proxy/src/main/java/com/liferay/portal/proxy/internal/ASMDelegateProxyFactory.java
@@ -17,8 +17,6 @@ package com.liferay.portal.proxy.internal;
 import com.liferay.petra.reflect.ReflectionUtil;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.DelegateProxyFactory;
 import com.liferay.portal.kernel.util.StringUtil;
 
@@ -66,10 +64,6 @@ public class ASMDelegateProxyFactory implements DelegateProxyFactory {
 						asmWrapperClassName);
 				}
 				catch (ClassNotFoundException classNotFoundException) {
-					if (_log.isDebugEnabled()) {
-						_log.debug(classNotFoundException);
-					}
-
 					byte[] classData = _generateASMWrapperClassData(
 						StringUtil.replace(asmWrapperClassName, '.', '/'),
 						interfaceClass, delegateObject, defaultObject);
@@ -180,10 +174,6 @@ public class ASMDelegateProxyFactory implements DelegateProxyFactory {
 					_getClassBinaryName(delegateObjectClass));
 			}
 			catch (NoSuchMethodException noSuchMethodException) {
-				if (_log.isDebugEnabled()) {
-					_log.debug(noSuchMethodException);
-				}
-
 				_generateMethod(
 					classWriter, method, asmWrapperClassBinaryName, "_default",
 					defaultObjectClassDescriptor,
@@ -262,9 +252,6 @@ public class ASMDelegateProxyFactory implements DelegateProxyFactory {
 
 		return StringUtil.replace(className, '.', '/');
 	}
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		ASMDelegateProxyFactory.class);
 
 	private static final Method _defineClassMethod;
 	private static final Method _equalsMethod;

modules/apps/portal/source-formatter-suppressions.xml
@@ -7,6 +7,7 @@
 		<suppress checks="MethodNamingCheck" files="portal-url-rewrite-filter/src/main/java/com/liferay/portal/url/rewrite/filter/internal/ServletContextDelegate\.java" />
 		<suppress checks="PersistenceCallCheck" files="portal-dao-test/src/testIntegration/java/com/liferay/portal/dao/orm/hibernate/test/SuspendedSessionConnectionTest\.java" />
 		<suppress checks="PersistenceCallCheck" files="portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/test/BaseBuildAutoUpgradeTestCase\.java" />
+		<suppress checks="UnprocessedExceptionCheck" files="portal-proxy/src/main/java/com/liferay/portal/proxy/internal/ASMDelegateProxyFactory\.java" />
 	</checkstyle>
 	<source-check>
 		<suppress checks="BNDDefinitionKeysCheck" files="portal-tika/bnd\.bnd" />

modules/apps/portal/portal-proxy/src/test/java/com/liferay/portal/proxy/internal/ASMDelegateProxyFactoryTest.java
@@ -24,6 +24,7 @@ import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ReflectionUtilTestUtil;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.DelegateProxyFactory;
+import com.liferay.portal.kernel.util.SystemProperties;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 
 import java.lang.reflect.Method;
@@ -75,6 +76,10 @@ public class ASMDelegateProxyFactoryTest {
 	@NewEnv(type = NewEnv.Type.CLASSLOADER)
 	@Test
 	public void testClassInitializationFailure() throws Exception {
+		Class.forName(
+			SystemProperties.class.getName(), true,
+			SystemProperties.class.getClassLoader());
+
 		SecurityException securityException = new SecurityException();
 
 		try (SwappableSecurityManager swappableSecurityManager =
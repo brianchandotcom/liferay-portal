60d1e4be3e0fefc7028efbd8ece1a99d0a3e686e
Alejandro Tardín <alejandro.tardin@liferay.com>
2023-02-15 22:06:00 +0100

    LPS-171473 Improving tests

--- modules/apps/headless/headless-builder/headless-builder-test/src/testIntegration/java/com/liferay/headless/builder/test/HeadlessBuilderTest.java
+++ modules/apps/headless/headless-builder/headless-builder-test/src/testIntegration/java/com/liferay/headless/builder/test/HeadlessBuilderTest.java
@@ -20,13 +20,13 @@ import com.liferay.blogs.test.util.BlogsTestUtil;
 import com.liferay.headless.builder.operation.OpenAPIOperationFactory;
 import com.liferay.headless.builder.operation.Operation;
 import com.liferay.headless.builder.operation.OperationRegistry;
+import com.liferay.petra.function.UnsafeRunnable;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
-import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.servlet.HttpHeaders;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
-import com.liferay.portal.kernel.test.util.CompanyTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
@@ -44,21 +44,25 @@ import com.liferay.portal.vulcan.yaml.openapi.OpenAPIYAML;
 
 import java.io.InputStream;
 
+import java.text.SimpleDateFormat;
+
+import java.util.Date;
 import java.util.List;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import org.skyscreamer.jsonassert.JSONAssert;
+
 /**
  * @author Carlos Correa
  */
 @RunWith(Arquillian.class)
-public class HeadlessBuilderDeployerTest {
+public class HeadlessBuilderTest {
 
 	@ClassRule
 	@Rule
@@ -79,9 +83,7 @@ public class HeadlessBuilderDeployerTest {
 	}
 
 	@After
-	public void tearDown() throws Exception {
-		_unregisterOperations();
-
+	public void tearDown() {
 		PropsUtil.addProperties(
 			UnicodePropertiesBuilder.setProperty(
 				"feature.flag.LPS-171047", "false"
@@ -89,43 +91,85 @@ public class HeadlessBuilderDeployerTest {
 	}
 
 	@Test
-	public void testDeployHeadlessBuilderApplication() throws Exception {
-		JSONObject jsonObject = _invoke(
-			"headless-builder/v1.0/blogs/" + _blogsEntry.getEntryId(),
-			Http.Method.GET);
+	public void testHeadlessBuilderApplication() throws Exception {
+		_withHeadlessBuilderApplication(
+			TestPropsValues.getCompanyId(), "blogs-entry-openapi.yaml",
+			() -> {
+				JSONObject jsonObject = _invoke(
+					"headless-builder/v1.0/blogs/" + _blogsEntry.getEntryId(),
+					Http.Method.GET);
 
-		Assert.assertEquals("NOT_FOUND", jsonObject.getString("status"));
-
-		_registerOperations(
-			TestPropsValues.getCompanyId(), "blogs-entry-openapi.yaml");
-
-		jsonObject = _invoke(
-			"headless-builder/v1.0/blogs/" + _blogsEntry.getEntryId(),
-			Http.Method.GET);
-
-		Assert.assertEquals(_blogsEntry.getEntryId(), jsonObject.getLong("id"));
+				JSONAssert.assertEquals(
+					JSONUtil.put(
+						"alternativeHeadline", _blogsEntry.getSubtitle()
+					).put(
+						"articleBody", _blogsEntry.getContent()
+					).put(
+						"dateCreated", _formatDate(_blogsEntry.getCreateDate())
+					).put(
+						"dateModified",
+						_formatDate(_blogsEntry.getModifiedDate())
+					).put(
+						"datePublished",
+						_formatDate(_blogsEntry.getDisplayDate())
+					).put(
+						"description", _blogsEntry.getDescription()
+					).put(
+						"externalReferenceCode",
+						_blogsEntry.getExternalReferenceCode()
+					).put(
+						"friendlyUrlPath", _blogsEntry.getUrlTitle()
+					).put(
+						"headline", _blogsEntry.getTitle()
+					).put(
+						"id", (int)_blogsEntry.getEntryId()
+					).put(
+						"siteId", (int)_blogsEntry.getGroupId()
+					).toString(),
+					jsonObject.toString(), true);
+			});
 	}
 
 	@Test
-	public void testDeployHeadlessBuilderApplicationForADifferentCompany()
+	public void testHeadlessBuilderApplicationOnADifferentCompany()
 		throws Exception {
 
-		_testCompany = CompanyTestUtil.addCompany();
+		_withHeadlessBuilderApplication(
+			0, "blogs-entry-openapi.yaml",
+			() -> {
+				JSONObject jsonObject = _invoke(
+					"headless-builder/v1.0/blogs/" + _blogsEntry.getEntryId(),
+					Http.Method.GET);
 
+				JSONAssert.assertEquals(
+					JSONUtil.put(
+						"status", "NOT_FOUND"
+					).put(
+						"title", "Operation not found"
+					).toString(),
+					jsonObject.toString(), true);
+			});
+	}
+
+	@Test
+	public void testMissingHeadlessBuilderApplication() throws Exception {
 		JSONObject jsonObject = _invoke(
-			"headless-builder/v1.0/blogs/" + _blogsEntry.getEntryId(),
-			Http.Method.GET);
+			"headless-builder/v1.0/test/1234", Http.Method.GET);
 
-		Assert.assertEquals("NOT_FOUND", jsonObject.getString("status"));
+		JSONAssert.assertEquals(
+			JSONUtil.put(
+				"status", "NOT_FOUND"
+			).put(
+				"title", "Operation not found"
+			).toString(),
+			jsonObject.toString(), true);
+	}
 
-		_registerOperations(
-			_testCompany.getCompanyId(), "blogs-entry-openapi.yaml");
+	private String _formatDate(Date date) {
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(
+			"yyyy-MM-dd'T'HH:mm:ss'Z'");
 
-		jsonObject = _invoke(
-			"headless-builder/v1.0/blogs/" + _blogsEntry.getEntryId(),
-			Http.Method.GET);
-
-		Assert.assertEquals("NOT_FOUND", jsonObject.getString("status"));
+		return simpleDateFormat.format(date);
 	}
 
 	private JSONObject _invoke(String endpoint, Http.Method method)
@@ -153,23 +197,28 @@ public class HeadlessBuilderDeployerTest {
 		}
 	}
 
-	private void _registerOperations(long companyId, String openAPIFileName)
+	private void _withHeadlessBuilderApplication(
+			long companyId, String openApiYamlFile,
+			UnsafeRunnable<Exception> unsafeRunnable)
 		throws Exception {
 
 		OpenAPIYAML openAPIYAML = _readOpenAPIYAML(
-			StringPool.SLASH + openAPIFileName);
+			StringPool.SLASH + openApiYamlFile);
 
-		_operations = _openAPIOperationFactory.getOperations(
+		List<Operation> operations = _openAPIOperationFactory.getOperations(
 			companyId, openAPIYAML);
 
-		for (Operation operation : _operations) {
-			_operationRegistry.register(operation);
-		}
-	}
+		try {
+			for (Operation operation : operations) {
+				_operationRegistry.register(operation);
+			}
 
-	private void _unregisterOperations() throws Exception {
-		for (Operation operation : _operations) {
-			_operationRegistry.unregister(operation);
+			unsafeRunnable.run();
+		}
+		finally {
+			for (Operation operation : operations) {
+				_operationRegistry.unregister(operation);
+			}
 		}
 	}
 
@@ -180,8 +229,5 @@ public class HeadlessBuilderDeployerTest {
 
 	@Inject
 	private OperationRegistry _operationRegistry;
-
-	private List<Operation> _operations;
-	private Company _testCompany;
 
 }

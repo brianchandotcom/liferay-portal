48e4f77f33efc3dad9386d0f1f09b4c8e92bc0b2
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2023-02-22 14:24:24 +0100

    LPS-176402 Remove the latest attribute but preserve the head attribute

--- modules/apps/asset/asset-service/src/main/java/com/liferay/asset/internal/info/collection/provider/AssetEntriesWithAssetCategoriesInTheSameAssetVocabulariesRelatedInfoItemCollectionProvider.java
+++ modules/apps/asset/asset-service/src/main/java/com/liferay/asset/internal/info/collection/provider/AssetEntriesWithAssetCategoriesInTheSameAssetVocabulariesRelatedInfoItemCollectionProvider.java
@@ -345,8 +345,6 @@ public class
 				Field.STATUS, WorkflowConstants.STATUS_APPROVED
 			).put(
 				"head", true
-			).put(
-				"latest", true
 			).build(),
 			serviceContext.getCompanyId(), null, themeDisplay.getLayout(), null,
 			serviceContext.getScopeGroupId(), null, serviceContext.getUserId());

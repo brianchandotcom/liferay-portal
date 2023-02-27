brianchandotcom/liferay-portal

commit b4a568f9ce449dc11aa5409325e877863d9426a7

LPS-175923 SF

modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPDefinitionLocalServiceImpl.java
@@ -1793,21 +1793,21 @@ public class CPDefinitionLocalServiceImpl
 			for (Map.Entry<String, List<String>> entry : facetMap.entrySet()) {
 				String fieldName = entry.getKey();
 
+				if (fieldName.equals("assetCategoryIds")) {
+					searchContext.setAssetCategoryIds(
+						TransformUtil.transformToLongArray(
+							Arrays.asList(values), GetterUtil::getLong));
+				}
+
 				String[] values = ArrayUtil.toStringArray(entry.getValue());
+
+				searchContext.setAttribute(fieldName, StringUtil.merge(values));
 
 				MultiValueFacet multiValueFacet = new MultiValueFacet(
 					searchContext);
 
 				multiValueFacet.setFieldName(fieldName);
 				multiValueFacet.setValues(values);
-
-				searchContext.setAttribute(fieldName, StringUtil.merge(values));
-
-				if (fieldName.equals("assetCategoryIds")) {
-					searchContext.setAssetCategoryIds(
-						TransformUtil.transformToLongArray(
-							Arrays.asList(values), GetterUtil::getLong));
-				}
 
 				facets.add(multiValueFacet);
 			}

commit c3579ccdb490abc1172a6e68c01cae5ca27c6313

LPS-175923 Fixed error caused by b4a568f9ce449dc11aa5409325e877863d9426a7

modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPDefinitionLocalServiceImpl.java
@@ -1793,13 +1793,13 @@ public class CPDefinitionLocalServiceImpl
 			for (Map.Entry<String, List<String>> entry : facetMap.entrySet()) {
 				String fieldName = entry.getKey();
 
+				String[] values = ArrayUtil.toStringArray(entry.getValue());
+
 				if (fieldName.equals("assetCategoryIds")) {
 					searchContext.setAssetCategoryIds(
 						TransformUtil.transformToLongArray(
 							Arrays.asList(values), GetterUtil::getLong));
 				}
-
-				String[] values = ArrayUtil.toStringArray(entry.getValue());
 
 				searchContext.setAttribute(fieldName, StringUtil.merge(values));
 
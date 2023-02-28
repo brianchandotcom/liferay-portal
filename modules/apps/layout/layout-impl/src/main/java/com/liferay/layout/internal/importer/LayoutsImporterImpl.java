b51f15cb4ca873d105c0325566cc825bc0d601ad
Jürgen Kappler <jurgen.kappler@liferay.com>
2023-02-28 09:17:38 +0100

    LPS-176949 Update usage

--- modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/importer/LayoutsImporterImpl.java
+++ modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/importer/LayoutsImporterImpl.java
@@ -1203,7 +1203,7 @@ public class LayoutsImporterImpl implements LayoutsImporter {
 				layoutUtilityPageEntry =
 					_layoutUtilityPageEntryService.addLayoutUtilityPageEntry(
 						externalReferenceCode, groupId, 0, 0, false, name, type,
-						0);
+						0, ServiceContextThreadLocal.getServiceContext());
 
 				added = true;
 			}

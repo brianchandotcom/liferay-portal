848106a25e3a1830965d64f93558c91f5227ccb1
Paulo Albuquerque <paulo.albuquerque@liferay.com>
2023-03-08 20:50:09 -0300

    LPS-176590 Create structures and prepare chain of responsability to NotificationTermsEvaluator

---
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/notification/term/evaluator/handler/NotificationTermEvaluatorHandler.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.object.notification.term.evaluator.handler;
+
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.portal.kernel.exception.PortalException;
+
+import java.util.Map;
+
+/**
+ * @author Paulo Albuquerque
+ */
+public interface NotificationTermEvaluatorHandler {
+
+	public String evaluate(
+			Map<String, Object> variables, ObjectDefinition objectDefinition,
+			String termName)
+		throws PortalException;
+
+	public NotificationTermEvaluatorHandler getNext();
+
+	public boolean isTermNameCriteriaMet(
+		ObjectDefinition objectDefinition, String termName);
+
+}

eb4f430128f66406336da284866b4cd305270bad
Selton Guedes <sfgs@ecomp.poli.br>
2023-03-16 16:02:44 -0300

    LPS-178809 Enable categorization for modifiable system objects post method

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
@@ -909,7 +909,7 @@ public class ObjectDefinitionLocalServiceImpl
 				objectDefinition.getObjectDefinitionId(), className, modifiable,
 				system));
 		objectDefinition.setEnableCategorization(
-			!system &&
+			!objectDefinition.isUnmodifiableSystemObject() &&
 			StringUtil.equals(
 				storageType, ObjectDefinitionConstants.STORAGE_TYPE_DEFAULT));
 		objectDefinition.setEnableComments(enableComments);

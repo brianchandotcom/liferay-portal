4481b4bfbbee050891983e7fa627269c92764063
Gabriel Albuquerque <gabriel.albuquerque@liferay.com>
2023-04-01 13:15:07 -0300

    LPS-180197 Allow users to enable audit for published objects through API

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
@@ -1291,7 +1291,6 @@ public class ObjectDefinitionLocalServiceImpl
 			enableComments, objectDefinition.isModifiable(),
 			objectDefinition.getStorageType(), objectDefinition.isSystem());
 		_validateEnableObjectEntryHistory(
-			objectDefinition.isApproved(),
 			objectDefinition.isEnableObjectEntryHistory() !=
 				enableObjectEntryHistory,
 			objectDefinition.isModifiable(), objectDefinition.getStorageType(),
@@ -1536,8 +1535,8 @@ public class ObjectDefinitionLocalServiceImpl
 	}
 
 	private void _validateEnableObjectEntryHistory(
-			boolean approved, boolean enableObjectEntryHistoryChanged,
-			boolean modifiable, String storageType, boolean system)
+			boolean enableObjectEntryHistoryChanged, boolean modifiable,
+			String storageType, boolean system)
 		throws PortalException {
 
 		if (!enableObjectEntryHistoryChanged) {
@@ -1556,12 +1555,6 @@ public class ObjectDefinitionLocalServiceImpl
 			throw new ObjectDefinitionEnableObjectEntryHistoryException(
 				"Enable object entry history is only allowed for object " +
 					"definitions with the default storage type");
-		}
-
-		if (approved) {
-			throw new ObjectDefinitionEnableObjectEntryHistoryException(
-				"Enable object entry history cannot be updated when the " +
-					"object definition is published");
 		}
 	}
 

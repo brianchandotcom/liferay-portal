32b3797569bbb060c69effd486cf2ec766de97f7
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2023-04-20 11:37:53 +0100

    LPS-156497 More usages of old method.
    For LPS-150388.

--- portal-impl/src/com/liferay/portal/internal/servlet/MainServlet.java
+++ portal-impl/src/com/liferay/portal/internal/servlet/MainServlet.java
@@ -752,7 +752,7 @@ public class MainServlet extends HttpServlet {
 				GetterUtil.getString(
 					PropsValues.COMPANY_DEFAULT_VIRTUAL_HOST_MAIL_DOMAIN,
 					PropsValues.COMPANY_DEFAULT_WEB_ID),
-				0, true);
+				0, true, null, null, null, null, null, null);
 		}
 
 		String[] webIds = PortalInstances.getWebIds();

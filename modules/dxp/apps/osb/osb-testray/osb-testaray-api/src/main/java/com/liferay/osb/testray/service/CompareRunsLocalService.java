brianchandotcom/liferay-portal

commit 8f87b00ffe466d35bd46985e686401db62192f7a

LPS-180081 Create Testray OSGi module

modules/dxp/apps/osb/osb-testray/osb-testray-service/build.gradle
@@ -1,0 +1,10 @@
+buildService {
+	apiDir = "../osb-testaray-api/src/main/java"
+}
+
+dependencies {
+	compileOnly group: "com.liferay.portal", name: "com.liferay.portal.kernel", version: "default"
+	compileOnly group: "org.osgi", name: "org.osgi.service.component.annotations", version: "1.4.0"
+	compileOnly project(":apps:portal:portal-aop-api")
+	compileOnly project(":dxp:apps:osb:osb-testray:osb-testray-api")
+}

modules/dxp/apps/osb/osb-testray/osb-testray-api/build.gradle
@@ -1,0 +1,4 @@
+dependencies {
+	compileOnly group: "com.liferay.portal", name: "com.liferay.portal.kernel", version: "default"
+	compileOnly group: "org.osgi", name: "org.osgi.annotation.versioning", version: "1.1.0"
+}

modules/dxp/apps/osb/osb-testray/osb-testray-api/src/main/java/com/liferay/osb/testray/service/CompareRunsLocalService.java
@@ -1,0 +1,55 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.osb.testray.service;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.service.BaseLocalService;
+import com.liferay.portal.kernel.transaction.Isolation;
+import com.liferay.portal.kernel.transaction.Transactional;
+
+import org.osgi.annotation.versioning.ProviderType;
+
+/**
+ * Provides the local service interface for CompareRuns. Methods of this
+ * service will not have security checks based on the propagated JAAS
+ * credentials because this service can only be accessed from within the same
+ * VM.
+ *
+ * @author José Abelenda
+ * @see CompareRunsLocalServiceUtil
+ * @generated
+ */
+@ProviderType
+@Transactional(
+	isolation = Isolation.PORTAL,
+	rollbackFor = {PortalException.class, SystemException.class}
+)
+public interface CompareRunsLocalService extends BaseLocalService {
+
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never modify this interface directly. Add custom service methods to <code>com.liferay.osb.testray.service.impl.CompareRunsLocalServiceImpl</code> and rerun ServiceBuilder to automatically copy the method declarations to this interface. Consume the compare runs local service via injection or a <code>org.osgi.util.tracker.ServiceTracker</code>. Use {@link CompareRunsLocalServiceUtil} if injection and service tracking are not available.
+	 */
+
+	/**
+	 * Returns the OSGi service identifier.
+	 *
+	 * @return the OSGi service identifier
+	 */
+	public String getOSGiServiceIdentifier();
+
+}

modules/dxp/apps/osb/osb-testray/osb-testray-api/src/main/java/com/liferay/osb/testray/service/CompareRunsLocalServiceUtil.java
@@ -1,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.osb.testray.service;
+
+/**
+ * Provides the local service utility for CompareRuns. This utility wraps
+ * <code>com.liferay.osb.testray.service.impl.CompareRunsLocalServiceImpl</code> and
+ * is an access point for service operations in application layer code running
+ * on the local server. Methods of this service will not have security checks
+ * based on the propagated JAAS credentials because this service can only be
+ * accessed from within the same VM.
+ *
+ * @author José Abelenda
+ * @see CompareRunsLocalService
+ * @generated
+ */
+public class CompareRunsLocalServiceUtil {
+
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never modify this class directly. Add custom service methods to <code>com.liferay.osb.testray.service.impl.CompareRunsLocalServiceImpl</code> and rerun ServiceBuilder to regenerate this class.
+	 */
+
+	/**
+	 * Returns the OSGi service identifier.
+	 *
+	 * @return the OSGi service identifier
+	 */
+	public static java.lang.String getOSGiServiceIdentifier() {
+		return getService().getOSGiServiceIdentifier();
+	}
+
+	public static CompareRunsLocalService getService() {
+		return _service;
+	}
+
+	private static volatile CompareRunsLocalService _service;
+
+}

modules/dxp/apps/osb/osb-testray/osb-testray-api/src/main/java/com/liferay/osb/testray/service/CompareRunsLocalServiceWrapper.java
@@ -1,0 +1,64 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.osb.testray.service;
+
+import com.liferay.portal.kernel.service.ServiceWrapper;
+
+/**
+ * Provides a wrapper for {@link CompareRunsLocalService}.
+ *
+ * @author José Abelenda
+ * @see CompareRunsLocalService
+ * @generated
+ */
+public class CompareRunsLocalServiceWrapper
+	implements CompareRunsLocalService,
+			   ServiceWrapper<CompareRunsLocalService> {
+
+	public CompareRunsLocalServiceWrapper() {
+		this(null);
+	}
+
+	public CompareRunsLocalServiceWrapper(
+		CompareRunsLocalService compareRunsLocalService) {
+
+		_compareRunsLocalService = compareRunsLocalService;
+	}
+
+	/**
+	 * Returns the OSGi service identifier.
+	 *
+	 * @return the OSGi service identifier
+	 */
+	@Override
+	public String getOSGiServiceIdentifier() {
+		return _compareRunsLocalService.getOSGiServiceIdentifier();
+	}
+
+	@Override
+	public CompareRunsLocalService getWrappedService() {
+		return _compareRunsLocalService;
+	}
+
+	@Override
+	public void setWrappedService(
+		CompareRunsLocalService compareRunsLocalService) {
+
+		_compareRunsLocalService = compareRunsLocalService;
+	}
+
+	private CompareRunsLocalService _compareRunsLocalService;
+
+}

modules/dxp/apps/osb/osb-testray/osb-testray-api/src/main/java/com/liferay/osb/testray/service/CompareRunsService.java
@@ -1,0 +1,58 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.osb.testray.service;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.jsonwebservice.JSONWebService;
+import com.liferay.portal.kernel.security.access.control.AccessControlled;
+import com.liferay.portal.kernel.service.BaseService;
+import com.liferay.portal.kernel.transaction.Isolation;
+import com.liferay.portal.kernel.transaction.Transactional;
+
+import org.osgi.annotation.versioning.ProviderType;
+
+/**
+ * Provides the remote service interface for CompareRuns. Methods of this
+ * service are expected to have security checks based on the propagated JAAS
+ * credentials because this service can be accessed remotely.
+ *
+ * @author José Abelenda
+ * @see CompareRunsServiceUtil
+ * @generated
+ */
+@AccessControlled
+@JSONWebService
+@ProviderType
+@Transactional(
+	isolation = Isolation.PORTAL,
+	rollbackFor = {PortalException.class, SystemException.class}
+)
+public interface CompareRunsService extends BaseService {
+
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never modify this interface directly. Add custom service methods to <code>com.liferay.osb.testray.service.impl.CompareRunsServiceImpl</code> and rerun ServiceBuilder to automatically copy the method declarations to this interface. Consume the compare runs remote service via injection or a <code>org.osgi.util.tracker.ServiceTracker</code>. Use {@link CompareRunsServiceUtil} if injection and service tracking are not available.
+	 */
+
+	/**
+	 * Returns the OSGi service identifier.
+	 *
+	 * @return the OSGi service identifier
+	 */
+	public String getOSGiServiceIdentifier();
+
+}

modules/dxp/apps/osb/osb-testray/osb-testray-api/src/main/java/com/liferay/osb/testray/service/CompareRunsServiceUtil.java
@@ -1,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.osb.testray.service;
+
+/**
+ * Provides the remote service utility for CompareRuns. This utility wraps
+ * <code>com.liferay.osb.testray.service.impl.CompareRunsServiceImpl</code> and is an
+ * access point for service operations in application layer code running on a
+ * remote server. Methods of this service are expected to have security checks
+ * based on the propagated JAAS credentials because this service can be
+ * accessed remotely.
+ *
+ * @author José Abelenda
+ * @see CompareRunsService
+ * @generated
+ */
+public class CompareRunsServiceUtil {
+
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never modify this class directly. Add custom service methods to <code>com.liferay.osb.testray.service.impl.CompareRunsServiceImpl</code> and rerun ServiceBuilder to regenerate this class.
+	 */
+
+	/**
+	 * Returns the OSGi service identifier.
+	 *
+	 * @return the OSGi service identifier
+	 */
+	public static java.lang.String getOSGiServiceIdentifier() {
+		return getService().getOSGiServiceIdentifier();
+	}
+
+	public static CompareRunsService getService() {
+		return _service;
+	}
+
+	private static volatile CompareRunsService _service;
+
+}

modules/dxp/apps/osb/osb-testray/osb-testray-api/src/main/java/com/liferay/osb/testray/service/CompareRunsServiceWrapper.java
@@ -1,0 +1,59 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.osb.testray.service;
+
+import com.liferay.portal.kernel.service.ServiceWrapper;
+
+/**
+ * Provides a wrapper for {@link CompareRunsService}.
+ *
+ * @author José Abelenda
+ * @see CompareRunsService
+ * @generated
+ */
+public class CompareRunsServiceWrapper
+	implements CompareRunsService, ServiceWrapper<CompareRunsService> {
+
+	public CompareRunsServiceWrapper() {
+		this(null);
+	}
+
+	public CompareRunsServiceWrapper(CompareRunsService compareRunsService) {
+		_compareRunsService = compareRunsService;
+	}
+
+	/**
+	 * Returns the OSGi service identifier.
+	 *
+	 * @return the OSGi service identifier
+	 */
+	@Override
+	public String getOSGiServiceIdentifier() {
+		return _compareRunsService.getOSGiServiceIdentifier();
+	}
+
+	@Override
+	public CompareRunsService getWrappedService() {
+		return _compareRunsService;
+	}
+
+	@Override
+	public void setWrappedService(CompareRunsService compareRunsService) {
+		_compareRunsService = compareRunsService;
+	}
+
+	private CompareRunsService _compareRunsService;
+
+}

commit d116eb6163ea56524777ae16c6e711908103e468

LPS-184791 This is a very confusing typo... caused by 8f87b00ffe466d35bd46985e686401db62192f7a

modules/dxp/apps/osb/osb-testray/osb-testaray-api/src/main/java/com/liferay/osb/testray/service/CompareRunsLocalService.java
@@ -1,55 +1,0 @@
-/**
- * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- *
- * The contents of this file are subject to the terms of the Liferay Enterprise
- * Subscription License ("License"). You may not use this file except in
- * compliance with the License. You can obtain a copy of the License by
- * contacting Liferay, Inc. See the License for the specific language governing
- * permissions and limitations under the License, including but not limited to
- * distribution rights of the Software.
- *
- *
- *
- */
-
-package com.liferay.osb.testray.service;
-
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.exception.SystemException;
-import com.liferay.portal.kernel.service.BaseLocalService;
-import com.liferay.portal.kernel.transaction.Isolation;
-import com.liferay.portal.kernel.transaction.Transactional;
-
-import org.osgi.annotation.versioning.ProviderType;
-
-/**
- * Provides the local service interface for CompareRuns. Methods of this
- * service will not have security checks based on the propagated JAAS
- * credentials because this service can only be accessed from within the same
- * VM.
- *
- * @author José Abelenda
- * @see CompareRunsLocalServiceUtil
- * @generated
- */
-@ProviderType
-@Transactional(
-	isolation = Isolation.PORTAL,
-	rollbackFor = {PortalException.class, SystemException.class}
-)
-public interface CompareRunsLocalService extends BaseLocalService {
-
-	/*
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never modify this interface directly. Add custom service methods to <code>com.liferay.osb.testray.service.impl.CompareRunsLocalServiceImpl</code> and rerun ServiceBuilder to automatically copy the method declarations to this interface. Consume the compare runs local service via injection or a <code>org.osgi.util.tracker.ServiceTracker</code>. Use {@link CompareRunsLocalServiceUtil} if injection and service tracking are not available.
-	 */
-
-	/**
-	 * Returns the OSGi service identifier.
-	 *
-	 * @return the OSGi service identifier
-	 */
-	public String getOSGiServiceIdentifier();
-
-}

modules/dxp/apps/osb/osb-testray/osb-testaray-api/src/main/java/com/liferay/osb/testray/service/CompareRunsLocalServiceUtil.java
@@ -1,56 +1,0 @@
-/**
- * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- *
- * The contents of this file are subject to the terms of the Liferay Enterprise
- * Subscription License ("License"). You may not use this file except in
- * compliance with the License. You can obtain a copy of the License by
- * contacting Liferay, Inc. See the License for the specific language governing
- * permissions and limitations under the License, including but not limited to
- * distribution rights of the Software.
- *
- *
- *
- */
-
-package com.liferay.osb.testray.service;
-
-/**
- * Provides the local service utility for CompareRuns. This utility wraps
- * <code>com.liferay.osb.testray.service.impl.CompareRunsLocalServiceImpl</code> and
- * is an access point for service operations in application layer code running
- * on the local server. Methods of this service will not have security checks
- * based on the propagated JAAS credentials because this service can only be
- * accessed from within the same VM.
- *
- * @author José Abelenda
- * @see CompareRunsLocalService
- * @generated
- */
-public class CompareRunsLocalServiceUtil {
-
-	/*
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never modify this class directly. Add custom service methods to <code>com.liferay.osb.testray.service.impl.CompareRunsLocalServiceImpl</code> and rerun ServiceBuilder to regenerate this class.
-	 */
-
-	/**
-	 * Returns the OSGi service identifier.
-	 *
-	 * @return the OSGi service identifier
-	 */
-	public static java.lang.String getOSGiServiceIdentifier() {
-		return getService().getOSGiServiceIdentifier();
-	}
-
-	public static CompareRunsLocalService getService() {
-		return _service;
-	}
-
-	public static void setService(CompareRunsLocalService service) {
-		_service = service;
-	}
-
-	private static volatile CompareRunsLocalService _service;
-
-}

modules/dxp/apps/osb/osb-testray/osb-testaray-api/src/main/java/com/liferay/osb/testray/service/CompareRunsLocalServiceWrapper.java
@@ -1,64 +1,0 @@
-/**
- * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- *
- * The contents of this file are subject to the terms of the Liferay Enterprise
- * Subscription License ("License"). You may not use this file except in
- * compliance with the License. You can obtain a copy of the License by
- * contacting Liferay, Inc. See the License for the specific language governing
- * permissions and limitations under the License, including but not limited to
- * distribution rights of the Software.
- *
- *
- *
- */
-
-package com.liferay.osb.testray.service;
-
-import com.liferay.portal.kernel.service.ServiceWrapper;
-
-/**
- * Provides a wrapper for {@link CompareRunsLocalService}.
- *
- * @author José Abelenda
- * @see CompareRunsLocalService
- * @generated
- */
-public class CompareRunsLocalServiceWrapper
-	implements CompareRunsLocalService,
-			   ServiceWrapper<CompareRunsLocalService> {
-
-	public CompareRunsLocalServiceWrapper() {
-		this(null);
-	}
-
-	public CompareRunsLocalServiceWrapper(
-		CompareRunsLocalService compareRunsLocalService) {
-
-		_compareRunsLocalService = compareRunsLocalService;
-	}
-
-	/**
-	 * Returns the OSGi service identifier.
-	 *
-	 * @return the OSGi service identifier
-	 */
-	@Override
-	public String getOSGiServiceIdentifier() {
-		return _compareRunsLocalService.getOSGiServiceIdentifier();
-	}
-
-	@Override
-	public CompareRunsLocalService getWrappedService() {
-		return _compareRunsLocalService;
-	}
-
-	@Override
-	public void setWrappedService(
-		CompareRunsLocalService compareRunsLocalService) {
-
-		_compareRunsLocalService = compareRunsLocalService;
-	}
-
-	private CompareRunsLocalService _compareRunsLocalService;
-
-}

modules/dxp/apps/osb/osb-testray/osb-testaray-api/src/main/java/com/liferay/osb/testray/service/CompareRunsService.java
@@ -1,58 +1,0 @@
-/**
- * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- *
- * The contents of this file are subject to the terms of the Liferay Enterprise
- * Subscription License ("License"). You may not use this file except in
- * compliance with the License. You can obtain a copy of the License by
- * contacting Liferay, Inc. See the License for the specific language governing
- * permissions and limitations under the License, including but not limited to
- * distribution rights of the Software.
- *
- *
- *
- */
-
-package com.liferay.osb.testray.service;
-
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.exception.SystemException;
-import com.liferay.portal.kernel.jsonwebservice.JSONWebService;
-import com.liferay.portal.kernel.security.access.control.AccessControlled;
-import com.liferay.portal.kernel.service.BaseService;
-import com.liferay.portal.kernel.transaction.Isolation;
-import com.liferay.portal.kernel.transaction.Transactional;
-
-import org.osgi.annotation.versioning.ProviderType;
-
-/**
- * Provides the remote service interface for CompareRuns. Methods of this
- * service are expected to have security checks based on the propagated JAAS
- * credentials because this service can be accessed remotely.
- *
- * @author José Abelenda
- * @see CompareRunsServiceUtil
- * @generated
- */
-@AccessControlled
-@JSONWebService
-@ProviderType
-@Transactional(
-	isolation = Isolation.PORTAL,
-	rollbackFor = {PortalException.class, SystemException.class}
-)
-public interface CompareRunsService extends BaseService {
-
-	/*
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never modify this interface directly. Add custom service methods to <code>com.liferay.osb.testray.service.impl.CompareRunsServiceImpl</code> and rerun ServiceBuilder to automatically copy the method declarations to this interface. Consume the compare runs remote service via injection or a <code>org.osgi.util.tracker.ServiceTracker</code>. Use {@link CompareRunsServiceUtil} if injection and service tracking are not available.
-	 */
-
-	/**
-	 * Returns the OSGi service identifier.
-	 *
-	 * @return the OSGi service identifier
-	 */
-	public String getOSGiServiceIdentifier();
-
-}

modules/dxp/apps/osb/osb-testray/osb-testaray-api/src/main/java/com/liferay/osb/testray/service/CompareRunsServiceUtil.java
@@ -1,56 +1,0 @@
-/**
- * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- *
- * The contents of this file are subject to the terms of the Liferay Enterprise
- * Subscription License ("License"). You may not use this file except in
- * compliance with the License. You can obtain a copy of the License by
- * contacting Liferay, Inc. See the License for the specific language governing
- * permissions and limitations under the License, including but not limited to
- * distribution rights of the Software.
- *
- *
- *
- */
-
-package com.liferay.osb.testray.service;
-
-/**
- * Provides the remote service utility for CompareRuns. This utility wraps
- * <code>com.liferay.osb.testray.service.impl.CompareRunsServiceImpl</code> and is an
- * access point for service operations in application layer code running on a
- * remote server. Methods of this service are expected to have security checks
- * based on the propagated JAAS credentials because this service can be
- * accessed remotely.
- *
- * @author José Abelenda
- * @see CompareRunsService
- * @generated
- */
-public class CompareRunsServiceUtil {
-
-	/*
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never modify this class directly. Add custom service methods to <code>com.liferay.osb.testray.service.impl.CompareRunsServiceImpl</code> and rerun ServiceBuilder to regenerate this class.
-	 */
-
-	/**
-	 * Returns the OSGi service identifier.
-	 *
-	 * @return the OSGi service identifier
-	 */
-	public static java.lang.String getOSGiServiceIdentifier() {
-		return getService().getOSGiServiceIdentifier();
-	}
-
-	public static CompareRunsService getService() {
-		return _service;
-	}
-
-	public static void setService(CompareRunsService service) {
-		_service = service;
-	}
-
-	private static volatile CompareRunsService _service;
-
-}

modules/dxp/apps/osb/osb-testray/osb-testaray-api/src/main/java/com/liferay/osb/testray/service/CompareRunsServiceWrapper.java
@@ -1,59 +1,0 @@
-/**
- * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- *
- * The contents of this file are subject to the terms of the Liferay Enterprise
- * Subscription License ("License"). You may not use this file except in
- * compliance with the License. You can obtain a copy of the License by
- * contacting Liferay, Inc. See the License for the specific language governing
- * permissions and limitations under the License, including but not limited to
- * distribution rights of the Software.
- *
- *
- *
- */
-
-package com.liferay.osb.testray.service;
-
-import com.liferay.portal.kernel.service.ServiceWrapper;
-
-/**
- * Provides a wrapper for {@link CompareRunsService}.
- *
- * @author José Abelenda
- * @see CompareRunsService
- * @generated
- */
-public class CompareRunsServiceWrapper
-	implements CompareRunsService, ServiceWrapper<CompareRunsService> {
-
-	public CompareRunsServiceWrapper() {
-		this(null);
-	}
-
-	public CompareRunsServiceWrapper(CompareRunsService compareRunsService) {
-		_compareRunsService = compareRunsService;
-	}
-
-	/**
-	 * Returns the OSGi service identifier.
-	 *
-	 * @return the OSGi service identifier
-	 */
-	@Override
-	public String getOSGiServiceIdentifier() {
-		return _compareRunsService.getOSGiServiceIdentifier();
-	}
-
-	@Override
-	public CompareRunsService getWrappedService() {
-		return _compareRunsService;
-	}
-
-	@Override
-	public void setWrappedService(CompareRunsService compareRunsService) {
-		_compareRunsService = compareRunsService;
-	}
-
-	private CompareRunsService _compareRunsService;
-
-}

modules/dxp/apps/osb/osb-testray/osb-testray-service/build.gradle
@@ -1,5 +1,5 @@
 buildService {
-	apiDir = "../osb-testaray-api/src/main/java"
+	apiDir = "../osb-testray-api/src/main/java"
 }
 
 dependencies {
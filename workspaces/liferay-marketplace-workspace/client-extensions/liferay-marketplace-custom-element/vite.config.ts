e265b4c5b0d6aae984a1a4793191b12d2e653dff
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2023-05-23 15:31:28 +0400

    LPS-184802 - Fix merge conflicts

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/vite.config.ts
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/vite.config.ts
@@ -1,21 +1,36 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import react from '@vitejs/plugin-react';
+import {defineConfig} from 'vite';
 
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  build: {
-    outDir: "build/vite",
-    rollupOptions: {
-      output: {
-        assetFileNames: "marketplace-custom-element-assets/[name][extname]",
-        chunkFileNames: "[name]-[hash].js",
-        entryFileNames: "[name]-[hash].js",
-      },
-    },
-  },
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
+	build: {
+		outDir: 'build/vite',
+		rollupOptions: {
+			output: {
+				assetFileNames:
+					'marketplace-custom-element-assets/[name][extname]',
+				chunkFileNames: '[name]-[hash].js',
+				entryFileNames: '[name]-[hash].js',
+			},
+		},
+	},
+	plugins: [react()],
+	server: {
+		port: 3000,
+	},
 });

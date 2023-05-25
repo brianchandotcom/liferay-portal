f076c8e4df8095f7d7f736a28d3e883d958da455
pei-jung <pei-jung.lan@liferay.com>
2023-05-24 17:27:02 -0700

    LPS-184787 Implement test helpers

--- modules/apps/change-tracking/change-tracking-rest-test/src/testIntegration/java/com/liferay/change/tracking/rest/resource/v1_0/test/PublicationResourceTest.java
+++ modules/apps/change-tracking/change-tracking-rest-test/src/testIntegration/java/com/liferay/change/tracking/rest/resource/v1_0/test/PublicationResourceTest.java
@@ -15,14 +15,87 @@
 package com.liferay.change.tracking.rest.resource.v1_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.change.tracking.rest.client.dto.v1_0.Publication;
 
-import org.junit.Ignore;
 import org.junit.runner.RunWith;
 
 /**
  * @author David Truong
  */
-@Ignore
 @RunWith(Arquillian.class)
 public class PublicationResourceTest extends BasePublicationResourceTestCase {
+
+	@Override
+	protected String[] getAdditionalAssertFieldNames() {
+		return new String[] {"description", "name"};
+	}
+
+	@Override
+	protected Publication testDeletePublication_addPublication()
+		throws Exception {
+
+		return publicationResource.postPublication(randomPublication());
+	}
+
+	@Override
+	protected Publication testGetPublication_addPublication() throws Exception {
+		return publicationResource.postPublication(randomPublication());
+	}
+
+	@Override
+	protected Publication testGetPublicationsPage_addPublication(
+			Publication publication)
+		throws Exception {
+
+		return publicationResource.postPublication(publication);
+	}
+
+	@Override
+	protected Publication testGraphQLPublication_addPublication()
+		throws Exception {
+
+		return publicationResource.postPublication(randomPublication());
+	}
+
+	@Override
+	protected Publication testPatchPublication_addPublication()
+		throws Exception {
+
+		return publicationResource.postPublication(randomPublication());
+	}
+
+	@Override
+	protected Publication testPostPublication_addPublication(
+			Publication publication)
+		throws Exception {
+
+		return publicationResource.postPublication(publication);
+	}
+
+	@Override
+	protected Publication testPostPublicationCheckout_addPublication()
+		throws Exception {
+
+		return publicationResource.postPublication(randomPublication());
+	}
+
+	@Override
+	protected Publication testPostPublicationPublish_addPublication()
+		throws Exception {
+
+		return publicationResource.postPublication(randomPublication());
+	}
+
+	@Override
+	protected Publication testPostPublicationSchedulePublish_addPublication()
+		throws Exception {
+
+		return publicationResource.postPublication(randomPublication());
+	}
+
+	@Override
+	protected Publication testPutPublication_addPublication() throws Exception {
+		return publicationResource.postPublication(randomPublication());
+	}
+
 }

8e5957b0e0f1e53959e1ee86e6b8cfcec59955de
Carlos Correa <carlos.correa@liferay.com>
2023-06-28 10:32:49 +0200

    LPS-188999 Fix tests

--- modules/util/portal-tools-rest-builder/src/test/resources/com/liferay/portal/tools/rest/builder/dependencies/expected/sample-impl/src/main/java/com/example/sample/internal/resource/v1_0_0/BaseFolderResourceImpl.java
+++ modules/util/portal-tools-rest-builder/src/test/resources/com/liferay/portal/tools/rest/builder/dependencies/expected/sample-impl/src/main/java/com/example/sample/internal/resource/v1_0_0/BaseFolderResourceImpl.java
@@ -46,12 +46,12 @@ import com.liferay.portal.vulcan.pagination.Page;
 import com.liferay.portal.vulcan.pagination.Pagination;
 import com.liferay.portal.vulcan.resource.EntityModelResource;
 import com.liferay.portal.vulcan.util.ActionUtil;
-import com.liferay.portal.vulcan.util.TransformUtil;
 
 import java.io.Serializable;
 
 import java.lang.reflect.Array;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -419,12 +419,7 @@ public abstract class BaseFolderResourceImpl
 	protected <T, R, E extends Throwable> long[] transformToLongArray(
 		Collection<T> collection, UnsafeFunction<T, R, E> unsafeFunction) {
 
-		try {
-			return unsafeTransformToLongArray(collection, unsafeFunction);
-		}
-		catch (Throwable throwable) {
-			throw new RuntimeException(throwable);
-		}
+		return TransformUtil.transformToLongArray(collection, unsafeFunction);
 	}
 
 	protected <T, R, E extends Throwable> List<R> unsafeTransform(
@@ -461,8 +456,8 @@ public abstract class BaseFolderResourceImpl
 			Collection<T> collection, UnsafeFunction<T, R, E> unsafeFunction)
 		throws E {
 
-		return (long[])_unsafeTransformToPrimitiveArray(
-			collection, unsafeFunction, long[].class);
+		return TransformUtil.unsafeTransformToLongArray(
+			collection, unsafeFunction);
 	}
 
 	protected AcceptLanguage contextAcceptLanguage;
@@ -485,21 +480,241 @@ public abstract class BaseFolderResourceImpl
 	protected VulcanBatchEngineImportTaskResource
 		vulcanBatchEngineImportTaskResource;
 
-	private <T, R, E extends Throwable> Object _unsafeTransformToPrimitiveArray(
-			Collection<T> collection, UnsafeFunction<T, R, E> unsafeFunction,
-			Class<?> clazz)
-		throws E {
+	public static class TransformUtil {
 
-		List<R> list = unsafeTransform(collection, unsafeFunction);
+		public static <T, R, E extends Throwable> List<R> transform(
+			Collection<T> collection, UnsafeFunction<T, R, E> unsafeFunction) {
 
-		Object array = clazz.cast(
-			Array.newInstance(clazz.getComponentType(), list.size()));
-
-		for (int i = 0; i < list.size(); i++) {
-			Array.set(array, i, list.get(i));
+			try {
+				return unsafeTransform(collection, unsafeFunction);
+			}
+			catch (Throwable throwable) {
+				throw new RuntimeException(throwable);
+			}
 		}
 
-		return array;
+		public static <T, R, E extends Throwable> R[] transform(
+			T[] array, UnsafeFunction<T, R, E> unsafeFunction, Class<?> clazz) {
+
+			try {
+				return unsafeTransform(array, unsafeFunction, clazz);
+			}
+			catch (Throwable throwable) {
+				throw new RuntimeException(throwable);
+			}
+		}
+
+		public static <T, R, E extends Throwable> R[] transformToArray(
+			Collection<T> collection, UnsafeFunction<T, R, E> unsafeFunction,
+			Class<?> clazz) {
+
+			try {
+				return unsafeTransformToArray(
+					collection, unsafeFunction, clazz);
+			}
+			catch (Throwable throwable) {
+				throw new RuntimeException(throwable);
+			}
+		}
+
+		public static <T, R, E extends Throwable> int[] transformToIntArray(
+			Collection<T> collection, UnsafeFunction<T, R, E> unsafeFunction) {
+
+			try {
+				return unsafeTransformToIntArray(collection, unsafeFunction);
+			}
+			catch (Throwable throwable) {
+				throw new RuntimeException(throwable);
+			}
+		}
+
+		public static <R, E extends Throwable> List<R> transformToList(
+			int[] array, UnsafeFunction<Integer, R, E> unsafeFunction) {
+
+			try {
+				return unsafeTransformToList(array, unsafeFunction);
+			}
+			catch (Throwable throwable) {
+				throw new RuntimeException(throwable);
+			}
+		}
+
+		public static <R, E extends Throwable> List<R> transformToList(
+			long[] array, UnsafeFunction<Long, R, E> unsafeFunction) {
+
+			try {
+				return unsafeTransformToList(array, unsafeFunction);
+			}
+			catch (Throwable throwable) {
+				throw new RuntimeException(throwable);
+			}
+		}
+
+		public static <T, R, E extends Throwable> List<R> transformToList(
+			T[] array, UnsafeFunction<T, R, E> unsafeFunction) {
+
+			try {
+				return unsafeTransformToList(array, unsafeFunction);
+			}
+			catch (Throwable throwable) {
+				throw new RuntimeException(throwable);
+			}
+		}
+
+		public static <T, R, E extends Throwable> long[] transformToLongArray(
+			Collection<T> collection, UnsafeFunction<T, R, E> unsafeFunction) {
+
+			try {
+				return unsafeTransformToLongArray(collection, unsafeFunction);
+			}
+			catch (Throwable throwable) {
+				throw new RuntimeException(throwable);
+			}
+		}
+
+		public static <T, R, E extends Throwable> List<R> unsafeTransform(
+				Collection<T> collection,
+				UnsafeFunction<T, R, E> unsafeFunction)
+			throws E {
+
+			if (collection == null) {
+				return new ArrayList<>();
+			}
+
+			List<R> list = new ArrayList<>(collection.size());
+
+			for (T item : collection) {
+				R newItem = unsafeFunction.apply(item);
+
+				if (newItem != null) {
+					list.add(newItem);
+				}
+			}
+
+			return list;
+		}
+
+		public static <T, R, E extends Throwable> R[] unsafeTransform(
+				T[] array, UnsafeFunction<T, R, E> unsafeFunction,
+				Class<?> clazz)
+			throws E {
+
+			List<R> list = unsafeTransformToList(array, unsafeFunction);
+
+			return list.toArray((R[])Array.newInstance(clazz, 0));
+		}
+
+		public static <T, R, E extends Throwable> R[] unsafeTransformToArray(
+				Collection<T> collection,
+				UnsafeFunction<T, R, E> unsafeFunction, Class<?> clazz)
+			throws E {
+
+			List<R> list = unsafeTransform(collection, unsafeFunction);
+
+			return list.toArray((R[])Array.newInstance(clazz, 0));
+		}
+
+		public static <T, R, E extends Throwable> int[]
+				unsafeTransformToIntArray(
+					Collection<T> collection,
+					UnsafeFunction<T, R, E> unsafeFunction)
+			throws E {
+
+			return (int[])_unsafeTransformToPrimitiveArray(
+				collection, unsafeFunction, int[].class);
+		}
+
+		public static <R, E extends Throwable> List<R> unsafeTransformToList(
+				int[] array, UnsafeFunction<Integer, R, E> unsafeFunction)
+			throws E {
+
+			if (array == null) {
+				return new ArrayList<>();
+			}
+
+			List<R> list = new ArrayList<>(array.length);
+
+			for (Integer item : array) {
+				R newItem = unsafeFunction.apply(item);
+
+				if (newItem != null) {
+					list.add(newItem);
+				}
+			}
+
+			return list;
+		}
+
+		public static <R, E extends Throwable> List<R> unsafeTransformToList(
+				long[] array, UnsafeFunction<Long, R, E> unsafeFunction)
+			throws E {
+
+			if (array == null) {
+				return new ArrayList<>();
+			}
+
+			List<R> list = new ArrayList<>(array.length);
+
+			for (Long item : array) {
+				R newItem = unsafeFunction.apply(item);
+
+				if (newItem != null) {
+					list.add(newItem);
+				}
+			}
+
+			return list;
+		}
+
+		public static <T, R, E extends Throwable> List<R> unsafeTransformToList(
+				T[] array, UnsafeFunction<T, R, E> unsafeFunction)
+			throws E {
+
+			if (array == null) {
+				return new ArrayList<>();
+			}
+
+			List<R> list = new ArrayList<>(array.length);
+
+			for (T item : array) {
+				R newItem = unsafeFunction.apply(item);
+
+				if (newItem != null) {
+					list.add(newItem);
+				}
+			}
+
+			return list;
+		}
+
+		public static <T, R, E extends Throwable> long[]
+				unsafeTransformToLongArray(
+					Collection<T> collection,
+					UnsafeFunction<T, R, E> unsafeFunction)
+			throws E {
+
+			return (long[])_unsafeTransformToPrimitiveArray(
+				collection, unsafeFunction, long[].class);
+		}
+
+		private static <T, R, E extends Throwable> Object
+				_unsafeTransformToPrimitiveArray(
+					Collection<T> collection,
+					UnsafeFunction<T, R, E> unsafeFunction, Class<?> clazz)
+			throws E {
+
+			List<R> list = unsafeTransform(collection, unsafeFunction);
+
+			Object array = clazz.cast(
+				Array.newInstance(clazz.getComponentType(), list.size()));
+
+			for (int i = 0; i < list.size(); i++) {
+				Array.set(array, i, list.get(i));
+			}
+
+			return array;
+		}
+
 	}
 
 	private static final com.liferay.portal.kernel.log.Log _log =
--- modules/util/portal-tools-rest-builder/src/test/resources/com/liferay/portal/tools/rest/builder/dependencies/expected/sample-impl/src/main/java/com/example/sample/internal/resource/v1_0_0/BaseTestBooleanResourceImpl.java
+++ modules/util/portal-tools-rest-builder/src/test/resources/com/liferay/portal/tools/rest/builder/dependencies/expected/sample-impl/src/main/java/com/example/sample/internal/resource/v1_0_0/BaseTestBooleanResourceImpl.java
@@ -30,10 +30,10 @@ import com.liferay.portal.odata.filter.FilterParserProvider;
 import com.liferay.portal.odata.sort.SortParserProvider;
 import com.liferay.portal.vulcan.accept.language.AcceptLanguage;
 import com.liferay.portal.vulcan.util.ActionUtil;
-import com.liferay.portal.vulcan.util.TransformUtil;
 
 import java.lang.reflect.Array;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -238,12 +238,7 @@ public abstract class BaseTestBooleanResourceImpl
 	protected <T, R, E extends Throwable> long[] transformToLongArray(
 		Collection<T> collection, UnsafeFunction<T, R, E> unsafeFunction) {
 
-		try {
-			return unsafeTransformToLongArray(collection, unsafeFunction);
-		}
-		catch (Throwable throwable) {
-			throw new RuntimeException(throwable);
-		}
+		return TransformUtil.transformToLongArray(collection, unsafeFunction);
 	}
 
 	protected <T, R, E extends Throwable> List<R> unsafeTransform(
@@ -280,8 +275,8 @@ public abstract class BaseTestBooleanResourceImpl
 			Collection<T> collection, UnsafeFunction<T, R, E> unsafeFunction)
 		throws E {
 
-		return (long[])_unsafeTransformToPrimitiveArray(
-			collection, unsafeFunction, long[].class);
+		return TransformUtil.unsafeTransformToLongArray(
+			collection, unsafeFunction);
 	}
 
 	protected AcceptLanguage contextAcceptLanguage;
@@ -299,21 +294,241 @@ public abstract class BaseTestBooleanResourceImpl
 	protected RoleLocalService roleLocalService;
 	protected SortParserProvider sortParserProvider;
 
-	private <T, R, E extends Throwable> Object _unsafeTransformToPrimitiveArray(
-			Collection<T> collection, UnsafeFunction<T, R, E> unsafeFunction,
-			Class<?> clazz)
-		throws E {
+	public static class TransformUtil {
 
-		List<R> list = unsafeTransform(collection, unsafeFunction);
+		public static <T, R, E extends Throwable> List<R> transform(
+			Collection<T> collection, UnsafeFunction<T, R, E> unsafeFunction) {
 
-		Object array = clazz.cast(
-			Array.newInstance(clazz.getComponentType(), list.size()));
-
-		for (int i = 0; i < list.size(); i++) {
-			Array.set(array, i, list.get(i));
+			try {
+				return unsafeTransform(collection, unsafeFunction);
+			}
+			catch (Throwable throwable) {
+				throw new RuntimeException(throwable);
+			}
 		}
 
-		return array;
+		public static <T, R, E extends Throwable> R[] transform(
+			T[] array, UnsafeFunction<T, R, E> unsafeFunction, Class<?> clazz) {
+
+			try {
+				return unsafeTransform(array, unsafeFunction, clazz);
+			}
+			catch (Throwable throwable) {
+				throw new RuntimeException(throwable);
+			}
+		}
+
+		public static <T, R, E extends Throwable> R[] transformToArray(
+			Collection<T> collection, UnsafeFunction<T, R, E> unsafeFunction,
+			Class<?> clazz) {
+
+			try {
+				return unsafeTransformToArray(
+					collection, unsafeFunction, clazz);
+			}
+			catch (Throwable throwable) {
+				throw new RuntimeException(throwable);
+			}
+		}
+
+		public static <T, R, E extends Throwable> int[] transformToIntArray(
+			Collection<T> collection, UnsafeFunction<T, R, E> unsafeFunction) {
+
+			try {
+				return unsafeTransformToIntArray(collection, unsafeFunction);
+			}
+			catch (Throwable throwable) {
+				throw new RuntimeException(throwable);
+			}
+		}
+
+		public static <R, E extends Throwable> List<R> transformToList(
+			int[] array, UnsafeFunction<Integer, R, E> unsafeFunction) {
+
+			try {
+				return unsafeTransformToList(array, unsafeFunction);
+			}
+			catch (Throwable throwable) {
+				throw new RuntimeException(throwable);
+			}
+		}
+
+		public static <R, E extends Throwable> List<R> transformToList(
+			long[] array, UnsafeFunction<Long, R, E> unsafeFunction) {
+
+			try {
+				return unsafeTransformToList(array, unsafeFunction);
+			}
+			catch (Throwable throwable) {
+				throw new RuntimeException(throwable);
+			}
+		}
+
+		public static <T, R, E extends Throwable> List<R> transformToList(
+			T[] array, UnsafeFunction<T, R, E> unsafeFunction) {
+
+			try {
+				return unsafeTransformToList(array, unsafeFunction);
+			}
+			catch (Throwable throwable) {
+				throw new RuntimeException(throwable);
+			}
+		}
+
+		public static <T, R, E extends Throwable> long[] transformToLongArray(
+			Collection<T> collection, UnsafeFunction<T, R, E> unsafeFunction) {
+
+			try {
+				return unsafeTransformToLongArray(collection, unsafeFunction);
+			}
+			catch (Throwable throwable) {
+				throw new RuntimeException(throwable);
+			}
+		}
+
+		public static <T, R, E extends Throwable> List<R> unsafeTransform(
+				Collection<T> collection,
+				UnsafeFunction<T, R, E> unsafeFunction)
+			throws E {
+
+			if (collection == null) {
+				return new ArrayList<>();
+			}
+
+			List<R> list = new ArrayList<>(collection.size());
+
+			for (T item : collection) {
+				R newItem = unsafeFunction.apply(item);
+
+				if (newItem != null) {
+					list.add(newItem);
+				}
+			}
+
+			return list;
+		}
+
+		public static <T, R, E extends Throwable> R[] unsafeTransform(
+				T[] array, UnsafeFunction<T, R, E> unsafeFunction,
+				Class<?> clazz)
+			throws E {
+
+			List<R> list = unsafeTransformToList(array, unsafeFunction);
+
+			return list.toArray((R[])Array.newInstance(clazz, 0));
+		}
+
+		public static <T, R, E extends Throwable> R[] unsafeTransformToArray(
+				Collection<T> collection,
+				UnsafeFunction<T, R, E> unsafeFunction, Class<?> clazz)
+			throws E {
+
+			List<R> list = unsafeTransform(collection, unsafeFunction);
+
+			return list.toArray((R[])Array.newInstance(clazz, 0));
+		}
+
+		public static <T, R, E extends Throwable> int[]
+				unsafeTransformToIntArray(
+					Collection<T> collection,
+					UnsafeFunction<T, R, E> unsafeFunction)
+			throws E {
+
+			return (int[])_unsafeTransformToPrimitiveArray(
+				collection, unsafeFunction, int[].class);
+		}
+
+		public static <R, E extends Throwable> List<R> unsafeTransformToList(
+				int[] array, UnsafeFunction<Integer, R, E> unsafeFunction)
+			throws E {
+
+			if (array == null) {
+				return new ArrayList<>();
+			}
+
+			List<R> list = new ArrayList<>(array.length);
+
+			for (Integer item : array) {
+				R newItem = unsafeFunction.apply(item);
+
+				if (newItem != null) {
+					list.add(newItem);
+				}
+			}
+
+			return list;
+		}
+
+		public static <R, E extends Throwable> List<R> unsafeTransformToList(
+				long[] array, UnsafeFunction<Long, R, E> unsafeFunction)
+			throws E {
+
+			if (array == null) {
+				return new ArrayList<>();
+			}
+
+			List<R> list = new ArrayList<>(array.length);
+
+			for (Long item : array) {
+				R newItem = unsafeFunction.apply(item);
+
+				if (newItem != null) {
+					list.add(newItem);
+				}
+			}
+
+			return list;
+		}
+
+		public static <T, R, E extends Throwable> List<R> unsafeTransformToList(
+				T[] array, UnsafeFunction<T, R, E> unsafeFunction)
+			throws E {
+
+			if (array == null) {
+				return new ArrayList<>();
+			}
+
+			List<R> list = new ArrayList<>(array.length);
+
+			for (T item : array) {
+				R newItem = unsafeFunction.apply(item);
+
+				if (newItem != null) {
+					list.add(newItem);
+				}
+			}
+
+			return list;
+		}
+
+		public static <T, R, E extends Throwable> long[]
+				unsafeTransformToLongArray(
+					Collection<T> collection,
+					UnsafeFunction<T, R, E> unsafeFunction)
+			throws E {
+
+			return (long[])_unsafeTransformToPrimitiveArray(
+				collection, unsafeFunction, long[].class);
+		}
+
+		private static <T, R, E extends Throwable> Object
+				_unsafeTransformToPrimitiveArray(
+					Collection<T> collection,
+					UnsafeFunction<T, R, E> unsafeFunction, Class<?> clazz)
+			throws E {
+
+			List<R> list = unsafeTransform(collection, unsafeFunction);
+
+			Object array = clazz.cast(
+				Array.newInstance(clazz.getComponentType(), list.size()));
+
+			for (int i = 0; i < list.size(); i++) {
+				Array.set(array, i, list.get(i));
+			}
+
+			return array;
+		}
+
 	}
 
 	private static final com.liferay.portal.kernel.log.Log _log =

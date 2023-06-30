e8db833573e461e38a2f5e060fe7223de36c8df2
Rafael Praxedes <rafael.praxedes@liferay.com>
2023-06-30 10:11:44 -0300

    LPS-183518 Make SDK changes backwards compatible

--- modules/sdk/project-templates/project-templates-portlet-provider/src/main/resources/archetype-resources/src/main/java/portlet/__className__AddPortletProvider.java
+++ modules/sdk/project-templates/project-templates-portlet-provider/src/main/resources/archetype-resources/src/main/java/portlet/__className__AddPortletProvider.java
@@ -2,8 +2,13 @@ package ${package}.portlet;
 
 import ${package}.constants.${className}PortletKeys;
 
+#if (!${liferayVersion.startsWith("7.4")})
+import com.liferay.portal.kernel.portlet.AddPortletProvider;
+#end
 import com.liferay.portal.kernel.portlet.BasePortletProvider;
+#if (${liferayVersion.startsWith("7.4")})
 import com.liferay.portal.kernel.portlet.PortletProvider;
+#end
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 
 import javax.portlet.PortletPreferences;
@@ -15,31 +20,41 @@ import org.osgi.service.component.annotations.Component;
  */
 @Component(
 	property = "model.class.name=com.liferay.asset.kernel.model.AssetEntry",
+#if (${liferayVersion.startsWith("7.4")})
 	service = PortletProvider.class
+#else
+	service = AddPortletProvider.class
+#end
 )
 public class ${className}AddPortletProvider
+#if (${liferayVersion.startsWith("7.4")})
 	extends BasePortletProvider {
+#else
+	extends BasePortletProvider implements AddPortletProvider {
+#end
 
 	@Override
 	public String getPortletName() {
 		return ${className}PortletKeys.${className.toUpperCase()};
 	}
 
-	@Override
-	public void updatePortletPreferences(
-			PortletPreferences portletPreferences, String portletId,
-			String className, long classPK, ThemeDisplay themeDisplay)
-		throws Exception {
-
-		portletPreferences.setValue("className", className);
-		portletPreferences.setValue("classPK", String.valueOf(classPK));
-	}
-
+#if (${liferayVersion.startsWith("7.4")})
 	@Override
 	public Action[] getSupportedActions() {
 		return _supportedActions;
 	}
 
 	private final Action[] _supportedActions = {Action.ADD};
+#else
+	@Override
+	public void updatePortletPreferences(
+		PortletPreferences portletPreferences, String portletId,
+		String className, long classPK, ThemeDisplay themeDisplay)
+		throws Exception {
+
+		portletPreferences.setValue("className", className);
+		portletPreferences.setValue("classPK", String.valueOf(classPK));
+	}
+#end
 
 }

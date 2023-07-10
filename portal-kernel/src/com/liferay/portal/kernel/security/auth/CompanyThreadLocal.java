971f35f518b587aa313ff6c9b6a6ed85c125d552
Luis Ortiz <luis.ortiz@liferay.com>
2023-07-10 18:35:00 +0200

    LPS-189289 Applying changes to classes using the property

--- portal-kernel/src/com/liferay/portal/kernel/security/auth/CompanyThreadLocal.java
+++ portal-kernel/src/com/liferay/portal/kernel/security/auth/CompanyThreadLocal.java
@@ -19,6 +19,7 @@ import com.liferay.petra.lang.SafeCloseable;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.change.tracking.CTCollectionThreadLocal;
 import com.liferay.portal.kernel.dao.jdbc.DataAccess;
+import com.liferay.portal.kernel.db.partition.DBPartition;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.CompanyConstants;
@@ -26,9 +27,7 @@ import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.model.UserConstants;
 import com.liferay.portal.kernel.service.UserLocalServiceUtil;
 import com.liferay.portal.kernel.spring.orm.LastSessionRecorderHelperUtil;
-import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleThreadLocal;
-import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.kernel.util.TimeZoneThreadLocal;
 
 import java.sql.Connection;
@@ -254,13 +253,16 @@ public class CompanyThreadLocal {
 	}
 
 	private static void _syncLastDBPartitionSessionState() {
-		if (_DATABASE_PARTITION_ENABLED) {
+		if (DBPartition.isPartitionEnabled()) {
 			LastSessionRecorderHelperUtil.syncLastSessionState(false);
 		}
 	}
 
+<<<<<<< HEAD
 	private static final boolean _DATABASE_PARTITION_ENABLED;
 
+=======
+>>>>>>> e3142d83 (LPS-189289 Applying changes to classes using the property)
 	private static final Log _log = LogFactoryUtil.getLog(
 		CompanyThreadLocal.class);
 

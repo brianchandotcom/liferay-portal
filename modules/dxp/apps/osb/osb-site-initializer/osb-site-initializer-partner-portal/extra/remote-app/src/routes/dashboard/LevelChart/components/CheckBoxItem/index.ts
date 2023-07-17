60fc3b1a4b207711a87043da2f68153149522ba0
Joao Marcelo Alves <joao.alves@liferay.com>
2023-07-17 17:32:20 -0300

    LPS-187710 Refactored Dashboard Level Chart

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/extra/remote-app/src/routes/dashboard/LevelChart/components/CheckBoxItem/index.ts
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/extra/remote-app/src/routes/dashboard/LevelChart/components/CheckBoxItem/index.ts
@@ -3,6 +3,4 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-export default function getBooleanValue(value: string) {
-	return value === 'true' ? 'Yes' : 'No';
-}
+export {default} from './CheckBoxItem';

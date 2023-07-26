05a7dc605890ba695e34a549d920bf599218e392
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2023-07-26 17:18:44 +0200

    LPS-189184 Update the import of useSessionState

--- modules/apps/frontend-js/frontend-js-components-web/src/main/resources/META-INF/resources/index.d.ts
+++ modules/apps/frontend-js/frontend-js-components-web/src/main/resources/META-INF/resources/index.d.ts
@@ -12,6 +12,7 @@ export const activeLanguageIdsAtom: Atom<any>;
 export {default as ClientExtension} from './ClientExtension';
 
 export {default as useId} from './hooks/useId';
+export {default as useSessionState} from './hooks/useSessionState';
 
 export {default as ManagementToolbar} from './management_toolbar/ManagementToolbar';
 

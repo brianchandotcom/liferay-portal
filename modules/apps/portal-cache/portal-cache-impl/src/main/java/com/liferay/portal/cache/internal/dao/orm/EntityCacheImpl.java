brianchandotcom/liferay-portal

commit 536dd5cb56e7a78b3fbe61ba0b035a83017f5c96

LPS-190641 Order

modules/apps/portal-cache/portal-cache-impl/src/main/java/com/liferay/portal/cache/internal/dao/orm/EntityCacheImpl.java
@@ -315,8 +315,8 @@ public class EntityCacheImpl
 	}
 
 	private void _notify(
-		String className, BaseModel<?> baseModel, Boolean removePortalCache,
-		long companyId) {
+		long companyId, String className, BaseModel<?> baseModel,
+		Boolean removePortalCache) {
 
 		try (SafeCloseable safeCloseable =
 				CompanyThreadLocal.setWithSafeCloseable(companyId)) {
@@ -356,8 +356,8 @@ public class EntityCacheImpl
 		String className, BaseModel<?> baseModel, Boolean removePortalCache) {
 
 		_notify(
-			className, baseModel, removePortalCache,
-			CompanyThreadLocal.getCompanyId());
+			CompanyThreadLocal.getCompanyId(), className, baseModel,
+			removePortalCache);
 
 		if (!_clusterExecutor.isEnabled() ||
 			!ClusterInvokeThreadLocal.isEnabled()) {
@@ -471,8 +471,8 @@ public class EntityCacheImpl
 	private static final Snapshot<FinderCache> _finderCacheSnapshot =
 		new Snapshot<>(EntityCacheImpl.class, FinderCache.class);
 	private static final MethodKey _notifyMethodKey = new MethodKey(
-		EntityCacheImpl.class, "_notify", String.class, BaseModel.class,
-		Boolean.class, long.class);
+		EntityCacheImpl.class, "_notify", long.class, String.class,
+		BaseModel.class, Boolean.class);
 
 	@Reference
 	private ClusterExecutor _clusterExecutor;

commit ed8e5c1e9f1e219e74379f26950687a0f4f276d6

LPS-190641 Fix order, caused by 536dd5cb

modules/apps/portal-cache/portal-cache-impl/src/main/java/com/liferay/portal/cache/internal/dao/orm/EntityCacheImpl.java
@@ -367,8 +367,8 @@ public class EntityCacheImpl
 			MethodHandler methodHandler = new MethodHandler(
 				_notifyMethodKey,
 				new Object[] {
-					className, baseModel, removePortalCache,
-					CompanyThreadLocal.getCompanyId()
+					CompanyThreadLocal.getCompanyId(), className, baseModel,
+					removePortalCache
 				});
 
 			ClusterRequest clusterRequest =
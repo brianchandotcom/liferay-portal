e26f7361392c965101a794b07840ea28fcca08cc
Joao Victor <joao.victor@liferay.com>
2023-09-05 16:10:58 -0300

    LPS-195397 Inline DDMFormUploadResponseHandler in UploadFileEntryMVCActionCommand

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/portlet/action/UploadFileEntryMVCActionCommand.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/portlet/action/UploadFileEntryMVCActionCommand.java
@@ -5,15 +5,37 @@
 
 package com.liferay.dynamic.data.mapping.form.web.internal.portlet.action;
 
+import com.liferay.document.library.kernel.exception.FileExtensionException;
+import com.liferay.document.library.kernel.exception.FileNameException;
+import com.liferay.document.library.kernel.exception.FileSizeException;
+import com.liferay.document.library.kernel.exception.InvalidFileException;
 import com.liferay.dynamic.data.mapping.constants.DDMPortletKeys;
 import com.liferay.dynamic.data.mapping.form.web.internal.upload.DDMFormUploadFileEntryHandler;
-import com.liferay.dynamic.data.mapping.form.web.internal.upload.DDMFormUploadResponseHandler;
+import com.liferay.dynamic.data.mapping.form.web.internal.upload.DDMFormUploadValidator;
+import com.liferay.object.exception.ObjectEntryValuesException;
+import com.liferay.object.model.ObjectFieldSetting;
+import com.liferay.object.service.ObjectFieldSettingLocalService;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.upload.UploadPortletRequest;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.upload.UploadHandler;
+import com.liferay.upload.UploadResponseHandler;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
+import javax.portlet.PortletRequest;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -46,10 +68,98 @@ public class UploadFileEntryMVCActionCommand extends BaseMVCActionCommand {
 	@Reference
 	private DDMFormUploadFileEntryHandler _ddmFormUploadFileEntryHandler;
 
+	private final DDMFormUploadResponseHandler _ddmFormUploadResponseHandler =
+		new DDMFormUploadResponseHandler();
+
+	@Reference(target = "(upload.response.handler.system.default=true)")
+	private UploadResponseHandler _defaultUploadResponseHandler;
+
 	@Reference
-	private DDMFormUploadResponseHandler _ddmFormUploadResponseHandler;
+	private Language _language;
+
+	@Reference
+	private ObjectFieldSettingLocalService _objectFieldSettingLocalService;
 
 	@Reference
 	private UploadHandler _uploadHandler;
+
+	private class DDMFormUploadResponseHandler
+		implements UploadResponseHandler {
+
+		@Override
+		public JSONObject onFailure(
+				PortletRequest portletRequest, PortalException portalException)
+			throws PortalException {
+
+			if (_log.isDebugEnabled()) {
+				_log.debug(portalException);
+			}
+
+			JSONObject jsonObject = _defaultUploadResponseHandler.onFailure(
+				portletRequest, portalException);
+
+			String errorMessage = StringPool.BLANK;
+
+			ThemeDisplay themeDisplay =
+				(ThemeDisplay)portletRequest.getAttribute(
+					WebKeys.THEME_DISPLAY);
+
+			if (portalException instanceof FileExtensionException) {
+				errorMessage = themeDisplay.translate(
+					"please-enter-a-file-with-a-valid-extension-x",
+					StringUtil.merge(
+						DDMFormUploadValidator.getGuestUploadFileExtensions(),
+						StringPool.COMMA_AND_SPACE));
+			}
+			else if (portalException instanceof FileNameException) {
+				errorMessage = themeDisplay.translate(
+					"please-enter-a-file-with-a-valid-file-name");
+			}
+			else if (portalException instanceof FileSizeException) {
+				errorMessage = themeDisplay.translate(
+					"please-enter-a-file-with-a-valid-file-size-no-larger-" +
+						"than-x",
+					_language.formatStorageSize(
+						DDMFormUploadValidator.getGuestUploadMaximumFileSize(),
+						themeDisplay.getLocale()));
+			}
+			else if (portalException instanceof InvalidFileException) {
+				errorMessage = themeDisplay.translate(
+					"please-enter-a-valid-file");
+			}
+			else if (portalException instanceof
+						ObjectEntryValuesException.InvalidFileExtension) {
+
+				ObjectFieldSetting objectFieldSetting =
+					_objectFieldSettingLocalService.fetchObjectFieldSetting(
+						ParamUtil.getLong(portletRequest, "objectFieldId"),
+						"acceptedFileExtensions");
+
+				errorMessage = themeDisplay.translate(
+					"please-enter-a-file-with-a-valid-extension-x",
+					objectFieldSetting.getValue());
+			}
+			else {
+				errorMessage = themeDisplay.translate(
+					"an-unexpected-error-occurred-while-uploading-your-file");
+			}
+
+			return jsonObject.put(
+				"error", JSONUtil.put("message", errorMessage));
+		}
+
+		@Override
+		public JSONObject onSuccess(
+				UploadPortletRequest uploadPortletRequest, FileEntry fileEntry)
+			throws PortalException {
+
+			return _defaultUploadResponseHandler.onSuccess(
+				uploadPortletRequest, fileEntry);
+		}
+
+		private final Log _log = LogFactoryUtil.getLog(
+			DDMFormUploadResponseHandler.class);
+
+	}
 
 }

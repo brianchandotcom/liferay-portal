4b6672c4a4bf006656491070e36e6f028df23d4d
Joao Victor <joao.victor@liferay.com>
2023-09-05 16:32:32 -0300

    LPS-195397 Inline DDMUserPersonalFolderUploadFileEntryHandler in UploadDDMUserPersonalFolderMVCActionCommand

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/portlet/action/UploadDDMUserPersonalFolderMVCActionCommand.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/portlet/action/UploadDDMUserPersonalFolderMVCActionCommand.java
@@ -5,9 +5,14 @@
 
 package com.liferay.dynamic.data.mapping.form.web.internal.portlet.action;
 
+import com.liferay.document.library.kernel.exception.NoSuchFileEntryException;
+import com.liferay.document.library.kernel.model.DLFileEntry;
+import com.liferay.document.library.kernel.service.DLAppService;
+import com.liferay.document.library.kernel.service.DLFileEntryLocalService;
+import com.liferay.document.library.kernel.util.DLValidator;
 import com.liferay.document.library.util.DLURLHelper;
 import com.liferay.dynamic.data.mapping.constants.DDMPortletKeys;
-import com.liferay.dynamic.data.mapping.form.web.internal.upload.DDMUserPersonalFolderUploadFileEntryHandler;
+import com.liferay.expando.kernel.model.ExpandoBridge;
 import com.liferay.item.selector.ItemSelectorUploadResponseHandler;
 import com.liferay.object.exception.ObjectEntryValuesException;
 import com.liferay.object.model.ObjectFieldSetting;
@@ -21,15 +26,30 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
 import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.repository.model.Folder;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermissionUtil;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextFactory;
 import com.liferay.portal.kernel.servlet.ServletResponseConstants;
 import com.liferay.portal.kernel.servlet.SessionMessages;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.upload.UploadPortletRequest;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.upload.UniqueFileNameProvider;
+import com.liferay.upload.UploadFileEntryHandler;
 import com.liferay.upload.UploadHandler;
 import com.liferay.upload.UploadResponseHandler;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
@@ -62,16 +82,29 @@ public class UploadDDMUserPersonalFolderMVCActionCommand
 			actionResponse);
 	}
 
-	@Reference
-	private DDMUserPersonalFolderUploadFileEntryHandler
-		_ddmUserPersonalFolderUploadFileEntryHandler;
-
+	private final DDMUserPersonalFolderUploadFileEntryHandler
+		_ddmUserPersonalFolderUploadFileEntryHandler =
+			new DDMUserPersonalFolderUploadFileEntryHandler();
 	private final DDMUserPersonalFolderUploadResponseHandler
 		_ddmUserPersonalFolderUploadResponseHandler =
 			new DDMUserPersonalFolderUploadResponseHandler();
 
 	@Reference
+	private DLAppService _dlAppService;
+
+	@Reference
+	private DLFileEntryLocalService _dlFileEntryLocalService;
+
+	@Reference
 	private DLURLHelper _dlURLHelper;
+
+	@Reference
+	private DLValidator _dlValidator;
+
+	@Reference(
+		target = "(model.class.name=com.liferay.portal.kernel.repository.model.Folder)"
+	)
+	private ModelResourcePermission<Folder> _folderModelResourcePermission;
 
 	@Reference
 	private ItemSelectorUploadResponseHandler
@@ -84,7 +117,167 @@ public class UploadDDMUserPersonalFolderMVCActionCommand
 	private Portal _portal;
 
 	@Reference
+	private UniqueFileNameProvider _uniqueFileNameProvider;
+
+	@Reference
 	private UploadHandler _uploadHandler;
+
+	private class DDMUserPersonalFolderUploadFileEntryHandler
+		implements UploadFileEntryHandler {
+
+		@Override
+		public FileEntry upload(UploadPortletRequest uploadPortletRequest)
+			throws IOException, PortalException {
+
+			ThemeDisplay themeDisplay =
+				(ThemeDisplay)uploadPortletRequest.getAttribute(
+					WebKeys.THEME_DISPLAY);
+
+			long folderId = ParamUtil.getLong(uploadPortletRequest, "folderId");
+
+			ModelResourcePermissionUtil.check(
+				_folderModelResourcePermission,
+				themeDisplay.getPermissionChecker(),
+				themeDisplay.getScopeGroupId(), folderId,
+				ActionKeys.ADD_DOCUMENT);
+
+			FileEntry fileEntry = null;
+
+			long fileEntryId = GetterUtil.getLong(
+				uploadPortletRequest.getParameter("fileEntryId"));
+
+			if (fileEntryId > 0) {
+				try {
+					fileEntry = _dlAppService.getFileEntry(fileEntryId);
+				}
+				catch (NoSuchFileEntryException noSuchFileEntryException) {
+					if (_log.isDebugEnabled()) {
+						_log.debug(noSuchFileEntryException);
+					}
+				}
+			}
+
+			String fileName = uploadPortletRequest.getFileName(
+				"imageSelectorFileName");
+
+			if (Validator.isNotNull(fileName)) {
+				try (InputStream inputStream =
+						uploadPortletRequest.getFileAsStream(
+							"imageSelectorFileName")) {
+
+					return _addFileEntry(
+						fileEntry, fileName, folderId, inputStream,
+						"imageSelectorFileName", themeDisplay,
+						uploadPortletRequest);
+				}
+			}
+
+			try (InputStream inputStream = uploadPortletRequest.getFileAsStream(
+					"imageBlob")) {
+
+				return _addFileEntry(
+					fileEntry, fileEntry.getFileName(), folderId, inputStream,
+					"imageBlob", themeDisplay, uploadPortletRequest);
+			}
+		}
+
+		private FileEntry _addFileEntry(
+				FileEntry fileEntry, String fileName, long folderId,
+				InputStream inputStream, String parameterName,
+				ThemeDisplay themeDisplay,
+				UploadPortletRequest uploadPortletRequest)
+			throws PortalException {
+
+			long size = uploadPortletRequest.getSize(parameterName);
+
+			_dlValidator.validateFileSize(
+				themeDisplay.getScopeGroupId(), fileName,
+				uploadPortletRequest.getContentType(parameterName), size);
+
+			long objectFieldId = ParamUtil.getLong(
+				uploadPortletRequest, "objectFieldId");
+
+			if (objectFieldId > 0) {
+				_validateAttachmentObjectField(fileName, objectFieldId);
+			}
+
+			long repositoryId = ParamUtil.getLong(
+				uploadPortletRequest, "repositoryId");
+
+			String uniqueFileName = _uniqueFileNameProvider.provide(
+				fileName,
+				curFileName -> _exists(repositoryId, folderId, curFileName));
+
+			String description = StringPool.BLANK;
+
+			if (fileEntry != null) {
+				description = fileEntry.getDescription();
+			}
+
+			ServiceContext serviceContext = ServiceContextFactory.getInstance(
+				DLFileEntry.class.getName(), uploadPortletRequest);
+
+			if ((fileEntry != null) &&
+				(fileEntry.getModel() instanceof DLFileEntry)) {
+
+				ExpandoBridge expandoBridge = fileEntry.getExpandoBridge();
+
+				serviceContext.setExpandoBridgeAttributes(
+					expandoBridge.getAttributes());
+			}
+
+			return _dlAppService.addFileEntry(
+				null, repositoryId, folderId, uniqueFileName,
+				uploadPortletRequest.getContentType(parameterName),
+				uniqueFileName, uniqueFileName, description, StringPool.BLANK,
+				inputStream, size, null, null, serviceContext);
+		}
+
+		private boolean _exists(
+			long repositoryId, long folderId, String fileName) {
+
+			try {
+				FileEntry fileEntry = _dlAppService.getFileEntry(
+					repositoryId, folderId, fileName);
+
+				if (fileEntry != null) {
+					return true;
+				}
+
+				return false;
+			}
+			catch (PortalException portalException) {
+				if (_log.isDebugEnabled()) {
+					_log.debug(portalException);
+				}
+
+				return false;
+			}
+		}
+
+		private void _validateAttachmentObjectField(
+				String fileName, long objectFieldId)
+			throws PortalException {
+
+			ObjectFieldSetting objectFieldSetting =
+				_objectFieldSettingLocalService.fetchObjectFieldSetting(
+					objectFieldId, "acceptedFileExtensions");
+
+			String value = objectFieldSetting.getValue();
+
+			if (!ArrayUtil.contains(
+					value.split("\\s*,\\s*"), FileUtil.getExtension(fileName),
+					true)) {
+
+				throw new ObjectEntryValuesException.InvalidFileExtension(
+					FileUtil.getExtension(fileName), fileName);
+			}
+		}
+
+		private final Log _log = LogFactoryUtil.getLog(
+			DDMUserPersonalFolderUploadFileEntryHandler.class);
+
+	}
 
 	private class DDMUserPersonalFolderUploadResponseHandler
 		implements UploadResponseHandler {

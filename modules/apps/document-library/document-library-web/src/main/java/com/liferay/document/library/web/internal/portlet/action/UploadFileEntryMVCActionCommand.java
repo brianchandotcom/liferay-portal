f97bf3de74e908b1d027211f68428e7a9f0ec197
Joao Victor <joao.victor@liferay.com>
2023-09-05 16:38:02 -0300

    LPS-195397 Inline DLUploadResponseHandler in UploadFileEntryMVCActionCommand

--- modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/portlet/action/UploadFileEntryMVCActionCommand.java
+++ modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/portlet/action/UploadFileEntryMVCActionCommand.java
@@ -6,15 +6,26 @@
 package com.liferay.document.library.web.internal.portlet.action;
 
 import com.liferay.document.library.constants.DLPortletKeys;
+import com.liferay.document.library.util.DLURLHelper;
 import com.liferay.document.library.web.internal.upload.DLUploadFileEntryHandler;
-import com.liferay.document.library.web.internal.upload.DLUploadResponseHandler;
+import com.liferay.item.selector.ItemSelectorUploadResponseHandler;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.upload.UploadPortletRequest;
+import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.upload.UploadHandler;
+import com.liferay.upload.UploadResponseHandler;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
+import javax.portlet.PortletRequest;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -47,10 +58,73 @@ public class UploadFileEntryMVCActionCommand extends BaseMVCActionCommand {
 	@Reference
 	private DLUploadFileEntryHandler _dlUploadFileEntryHandler;
 
+	private final DLUploadResponseHandler _dlUploadResponseHandler =
+		new DLUploadResponseHandler();
+
 	@Reference
-	private DLUploadResponseHandler _dlUploadResponseHandler;
+	private DLURLHelper _dlURLHelper;
+
+	@Reference
+	private ItemSelectorUploadResponseHandler
+		_itemSelectorUploadResponseHandler;
 
 	@Reference
 	private UploadHandler _uploadHandler;
+
+	private class DLUploadResponseHandler implements UploadResponseHandler {
+
+		@Override
+		public JSONObject onFailure(
+				PortletRequest portletRequest, PortalException portalException)
+			throws PortalException {
+
+			return _itemSelectorUploadResponseHandler.onFailure(
+				portletRequest, portalException);
+		}
+
+		@Override
+		public JSONObject onSuccess(
+				UploadPortletRequest uploadPortletRequest, FileEntry fileEntry)
+			throws PortalException {
+
+			JSONObject jsonObject =
+				_itemSelectorUploadResponseHandler.onSuccess(
+					uploadPortletRequest, fileEntry);
+
+			JSONObject fileJSONObject = jsonObject.getJSONObject("file");
+
+			fileJSONObject.put("url", _getURL(uploadPortletRequest, fileEntry));
+
+			return jsonObject;
+		}
+
+		private String _getURL(
+			UploadPortletRequest uploadPortletRequest, FileEntry fileEntry) {
+
+			try {
+				ThemeDisplay themeDisplay =
+					(ThemeDisplay)uploadPortletRequest.getAttribute(
+						WebKeys.THEME_DISPLAY);
+
+				return _dlURLHelper.getPreviewURL(
+					fileEntry, fileEntry.getLatestFileVersion(), themeDisplay,
+					StringPool.BLANK);
+			}
+			catch (PortalException portalException) {
+				if (_log.isWarnEnabled()) {
+					_log.warn(
+						"Unable to get URL for file entry " +
+							fileEntry.getFileEntryId(),
+						portalException);
+				}
+			}
+
+			return StringPool.BLANK;
+		}
+
+		private final Log _log = LogFactoryUtil.getLog(
+			DLUploadResponseHandler.class);
+
+	}
 
 }

c5b34bcd4d79c5155c1e9bb8974e8bf278485291
Alan Huang <alan.huang@liferay.com>
2023-09-08 15:53:04 +0800

    LPS-177111 Use set call with inlined if-statement

--- modules/apps/object/object-admin-rest-test/src/testIntegration/java/com/liferay/object/admin/rest/resource/v1_0/test/ObjectDefinitionVulcanBatchEngineTaskItemDelegateTest.java
+++ modules/apps/object/object-admin-rest-test/src/testIntegration/java/com/liferay/object/admin/rest/resource/v1_0/test/ObjectDefinitionVulcanBatchEngineTaskItemDelegateTest.java
@@ -182,11 +182,6 @@ public class ObjectDefinitionVulcanBatchEngineTaskItemDelegateTest {
 					RandomTestUtil.randomString());
 				id = RandomTestUtil.randomLong();
 				label = Collections.singletonMap("en_US", "O" + finalName);
-
-				if (FeatureFlagManagerUtil.isEnabled("LPS-167253")) {
-					modifiable = !finalSystem;
-				}
-
 				name = "O" + finalName;
 				objectFields = new ObjectField[] {_createObjectField()};
 				panelAppOrder = StringUtil.toLowerCase(
@@ -200,18 +195,27 @@ public class ObjectDefinitionVulcanBatchEngineTaskItemDelegateTest {
 				restContextPath = StringUtil.toLowerCase(
 					RandomTestUtil.randomString());
 				scope = ObjectDefinitionConstants.SCOPE_COMPANY;
-
-				if (FeatureFlagManagerUtil.isEnabled("LPS-135430")) {
-					storageType = StringUtil.toLowerCase(
-						RandomTestUtil.randomString());
-				}
-				else {
-					storageType = StringPool.BLANK;
-				}
-
 				system = finalSystem;
 				titleObjectFieldName = StringUtil.toLowerCase(
 					RandomTestUtil.randomString());
+
+				setModifiable(
+					() -> {
+						if (!FeatureFlagManagerUtil.isEnabled("LPS-167253")) {
+							return null;
+						}
+
+						return !finalSystem;
+					});
+				setStorageType(
+					() -> {
+						if (!FeatureFlagManagerUtil.isEnabled("LPS-135430")) {
+							return StringPool.BLANK;
+						}
+
+						return StringUtil.toLowerCase(
+							RandomTestUtil.randomString());
+					});
 			}
 		};
 	}

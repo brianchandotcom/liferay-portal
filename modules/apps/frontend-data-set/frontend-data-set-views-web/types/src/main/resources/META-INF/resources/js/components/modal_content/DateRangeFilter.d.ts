82fd607cf5c471141a49852ad0d78c1655d6b8a7
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2023-09-13 11:48:57 +0400

    LPS-195531 - Refactor

---
+++ modules/apps/frontend-data-set/frontend-data-set-views-web/types/src/main/resources/META-INF/resources/js/components/modal_content/DateRangeFilter.d.ts
@@ -0,0 +1,22 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+/// <reference types="react" />
+
+declare function Header(): JSX.Element;
+declare function Body({
+	from,
+	isValidDateRange,
+	namespace,
+	onFromChange,
+	onToChange,
+	onValidDateChange,
+	to,
+}: any): JSX.Element;
+declare const _default: {
+	Body: typeof Body;
+	Header: typeof Header;
+};
+export default _default;

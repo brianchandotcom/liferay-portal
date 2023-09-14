c96ca282a006bba1473cfb91d9a47387a167b087
Brian Chan <brian.chan@liferay.com>
2023-09-14 14:33:24 -0300

    LPS-194616 Sort

--- modules/apps/headless/headless-builder/headless-builder-impl/src/main/java/com/liferay/headless/builder/internal/resource/HeadlessBuilderResourceImpl.java
+++ modules/apps/headless/headless-builder/headless-builder-impl/src/main/java/com/liferay/headless/builder/internal/resource/HeadlessBuilderResourceImpl.java
@@ -82,8 +82,8 @@ public class HeadlessBuilderResourceImpl {
 			path + "/" + pathParameterValue,
 			APIApplication.Endpoint.Scope.COMPANY,
 			endpoint -> _endpointHelper.getResponseEntityMap(
-				_company.getCompanyId(), endpoint.getResponseSchema(),
-				endpoint.getPathParameter(), pathParameterValue, null));
+				_company.getCompanyId(), endpoint.getPathParameter(),
+				pathParameterValue, endpoint.getResponseSchema(), null));
 	}
 
 	@GET
@@ -102,8 +102,8 @@ public class HeadlessBuilderResourceImpl {
 			path + "/" + pathParameterValue,
 			APIApplication.Endpoint.Scope.GROUP,
 			endpoint -> _endpointHelper.getResponseEntityMap(
-				_company.getCompanyId(), endpoint.getResponseSchema(),
-				endpoint.getPathParameter(), pathParameterValue, scopeKey));
+				_company.getCompanyId(), endpoint.getPathParameter(),
+				pathParameterValue, endpoint.getResponseSchema(), scopeKey));
 	}
 
 	private <T> Response _executeEndpoint(

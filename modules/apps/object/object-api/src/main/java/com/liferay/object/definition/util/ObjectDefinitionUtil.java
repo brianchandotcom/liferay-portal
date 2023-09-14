4c685847ca70c4039e89521d0c15657277ceb373
Gabriel Albuquerque <gabriel.albuquerque@liferay.com>
2023-09-14 14:20:51 -0300

    LPS-196211 Move ObjectDefinitionUtil to object-api

--- modules/apps/object/object-api/src/main/java/com/liferay/object/definition/util/ObjectDefinitionUtil.java
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/definition/util/ObjectDefinitionUtil.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.object.internal.definition.util;
+package com.liferay.object.definition.util;
 
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.PortalRunMode;

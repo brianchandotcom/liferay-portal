4408879e86485cd485cf90a207d6d11e7bdffca6
Murilo Stodolni <murilo.stodolni@liferay.com>
2023-09-15 11:53:42 -0300

    LPS-196238 Adapt Exception and ExceptionMapper to return message in API and move ObjectExceptionMapperUtil to object-api to reused in another one

--- modules/apps/object/object-api/src/main/java/com/liferay/object/jaxrs/exception/mapper/util/ObjectExceptionMapperUtil.java
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/jaxrs/exception/mapper/util/ObjectExceptionMapperUtil.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.object.admin.rest.internal.jaxrs.exception.mapper.util;
+package com.liferay.object.jaxrs.exception.mapper.util;
 
 import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.util.Validator;

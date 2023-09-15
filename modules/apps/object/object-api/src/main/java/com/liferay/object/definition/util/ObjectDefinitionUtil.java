c6720e80ce9cf20972158f929e6d17147a3daa67
nikki-pru <nikki.prudencio@liferay.com>
2023-09-15 13:04:14 -0700

    Revert "LPS-193127 Auto SF"
    This reverts commit c24de7ad70b207779ca3c006cc9c3b65025dbe82.

--- modules/apps/object/object-api/src/main/java/com/liferay/object/definition/util/ObjectDefinitionUtil.java
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/definition/util/ObjectDefinitionUtil.java
@@ -6,6 +6,7 @@
 package com.liferay.object.definition.util;
 
 import com.liferay.batch.engine.unit.BatchEngineUnitThreadLocal;
+import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.PortalRunMode;
 import com.liferay.portal.kernel.util.StringUtil;

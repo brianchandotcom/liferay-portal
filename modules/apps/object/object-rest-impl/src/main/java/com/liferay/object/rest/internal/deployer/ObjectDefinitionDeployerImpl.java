1aca9f2af24101b37ce49a575497e06e47152a1a
Murilo Stodolni <murilo.stodolni@liferay.com>
2023-09-15 11:53:42 -0300

    LPS-196238 Adapt Exception and ExceptionMapper to return message in API and move ObjectExceptionMapperUtil to object-api to reused in another one

--- modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/deployer/ObjectDefinitionDeployerImpl.java
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/deployer/ObjectDefinitionDeployerImpl.java
@@ -640,7 +640,7 @@ public class ObjectDefinitionDeployerImpl implements ObjectDefinitionDeployer {
 				).build()),
 			_bundleContext.registerService(
 				ExceptionMapper.class,
-				new ObjectRelationshipDeletionTypeExceptionMapper(),
+				new ObjectRelationshipDeletionTypeExceptionMapper(_language),
 				HashMapDictionaryBuilder.<String, Object>put(
 					"osgi.jaxrs.application.select",
 					"(osgi.jaxrs.name=" + jaxRsApplicationName + ")"

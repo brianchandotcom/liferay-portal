39fab818dacaa1a5cc99382540cf5956396465d3
Rebeca Silva <rebeca.silva@liferay.com>
2023-09-19 11:33:16 -0300

    LPS-196468 Fix search of the LeftSidebar

--- modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModelBuilder/LeftSidebar/LeftSidebar.tsx
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModelBuilder/LeftSidebar/LeftSidebar.tsx
@@ -83,7 +83,7 @@ export default function LeftSidebar({
 
 			return {
 				...sidebarItem,
-				objectDefinitions: newObjectDefinitions,
+				leftSidebarObjectDefinitionItems: newObjectDefinitions,
 			};
 		});
 	}, [query, leftSidebarItems]);

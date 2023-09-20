9b912e019bcf75e264307df0b90a5508ba2c28fe
mikellorza <mikel.lorza@liferay.com>
2023-09-20 13:48:22 +0200

    LPS-196591 Add LockedLayoutsGroupConfiguration

---
+++ modules/apps/layout/layout-locked-layouts-web/src/main/java/com/liferay/layout/locked/layouts/web/internal/configuration/LockedLayoutsGroupConfiguration.java
@@ -0,0 +1,37 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.layout.locked.layouts.web.internal.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Mikel Lorza
+ */
+@ExtendedObjectClassDefinition(
+	category = "pages", generateUI = false,
+	scope = ExtendedObjectClassDefinition.Scope.GROUP
+)
+@Meta.OCD(
+	id = "com.liferay.layout.locked.layouts.web.internal.configuration.LockedLayoutsGroupConfiguration",
+	localization = "content/Language", name = "locked-pages-configuration-name"
+)
+public interface LockedLayoutsGroupConfiguration {
+
+	@Meta.AD(
+		deflt = "true", name = "allow-automatic-unlocking-process",
+		required = false
+	)
+	public boolean allowAutomaticUnlockingProcess();
+
+	@Meta.AD(
+		deflt = "5", max = "99999", min = "1", name = "time-without-autosave",
+		required = false
+	)
+	public int timeWithoutAutosave();
+
+}

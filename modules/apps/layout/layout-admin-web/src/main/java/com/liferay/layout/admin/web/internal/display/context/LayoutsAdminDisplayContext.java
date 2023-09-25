7996badf2769c3ba5dcbed6c8ce26c51eb0f4497
mikellorza <mikel.lorza@liferay.com>
2023-09-25 11:03:01 +0200

    LPS-196413 Create constant for designConfigurationModified type setting

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/display/context/LayoutsAdminDisplayContext.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/display/context/LayoutsAdminDisplayContext.java
@@ -32,6 +32,7 @@ import com.liferay.layout.admin.constants.LayoutAdminPortletKeys;
 import com.liferay.layout.admin.constants.LayoutScreenNavigationEntryConstants;
 import com.liferay.layout.admin.web.internal.helper.LayoutActionsHelper;
 import com.liferay.layout.admin.web.internal.util.FaviconUtil;
+import com.liferay.layout.constants.LayoutTypeSettingsConstants;
 import com.liferay.layout.helper.LayoutCopyHelper;
 import com.liferay.layout.page.template.constants.LayoutPageTemplateEntryTypeConstants;
 import com.liferay.layout.page.template.model.LayoutPageTemplateCollection;
@@ -2042,7 +2043,8 @@ public class LayoutsAdminDisplayContext {
 
 		if (GetterUtil.getBoolean(
 				typeSettingsUnicodeProperties.getProperty(
-					"designConfigurationModified"))) {
+					LayoutTypeSettingsConstants.
+						DESIGN_CONFIGURATION_MODIFIED))) {
 
 			return true;
 		}

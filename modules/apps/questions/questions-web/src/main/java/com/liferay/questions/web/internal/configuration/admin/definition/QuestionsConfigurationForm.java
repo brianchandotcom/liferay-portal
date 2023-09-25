4922d006b40fbfad13bfff358207352f05eb274d
Nilton Vieira <nilton.vieira@liferay.com>
2023-09-25 15:52:04 -0300

    LPS-185756 move to admin package

--- modules/apps/questions/questions-web/src/main/java/com/liferay/questions/web/internal/configuration/admin/definition/QuestionsConfigurationForm.java
+++ modules/apps/questions/questions-web/src/main/java/com/liferay/questions/web/internal/configuration/admin/definition/QuestionsConfigurationForm.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.questions.web.internal.configuration.definition;
+package com.liferay.questions.web.internal.configuration.admin.definition;
 
 import com.liferay.dynamic.data.mapping.annotations.DDMForm;
 import com.liferay.dynamic.data.mapping.annotations.DDMFormLayout;

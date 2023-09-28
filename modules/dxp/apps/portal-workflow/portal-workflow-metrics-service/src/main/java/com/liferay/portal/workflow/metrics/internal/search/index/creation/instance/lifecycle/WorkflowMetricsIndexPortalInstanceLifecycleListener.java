c7f7ff85c41cf555536318ce74ae516f6d331333
MarinhoFeliphe <feliphemarinho18@gmail.com>
2023-09-28 11:09:42 -0300

    Revert "LPS-189313 Not need"
    This reverts commit 5bd0f19d629b485c2008a7fb7783947605d5fa2d.

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-service/src/main/java/com/liferay/portal/workflow/metrics/internal/search/index/creation/instance/lifecycle/WorkflowMetricsIndexPortalInstanceLifecycleListener.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-service/src/main/java/com/liferay/portal/workflow/metrics/internal/search/index/creation/instance/lifecycle/WorkflowMetricsIndexPortalInstanceLifecycleListener.java
@@ -10,13 +10,17 @@ import com.liferay.portal.instance.lifecycle.PortalInstanceLifecycleListener;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.workflow.metrics.internal.search.index.creation.helper.WorkflowMetricsIndexCreator;
 
+import org.osgi.framework.Constants;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Rafael Praxedes
  */
-@Component(service = PortalInstanceLifecycleListener.class)
+@Component(
+	property = Constants.SERVICE_RANKING + ":Integer=200",
+	service = PortalInstanceLifecycleListener.class
+)
 public class WorkflowMetricsIndexPortalInstanceLifecycleListener
 	extends BasePortalInstanceLifecycleListener {
 

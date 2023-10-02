b6a9c0bf2d07b628cd76b37943191410c730f6a4
Michael Cavalcanti <michael.cavalcanti@liferay.com>
2023-10-02 16:45:46 -0300

    LPS-197590 Excluding JavaUpgradeOnAfterUpdateParameterCheck

--- modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/UpgradeSourceProcessorTest.java
+++ modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/UpgradeSourceProcessorTest.java
@@ -323,11 +323,6 @@ public class UpgradeSourceProcessorTest extends BaseSourceProcessorTestCase {
 	}
 
 	@Test
-	public void testUpgradeJavaOnAfterUpdateParameterCheck() throws Exception {
-		test("upgrade/UpgradeJavaOnAfterUpdateParameterCheck.testjava");
-	}
-
-	@Test
 	public void testUpgradeJavaPhoneLocalServiceUtilCheck() throws Exception {
 		test("upgrade/UpgradeJavaPhoneLocalServiceUtilCheck.testjava");
 	}

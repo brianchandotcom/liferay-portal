2c1f4ff0a7b7b3aa267c5ccb43c9b21045f98705
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2023-10-10 12:46:05 +0100

    LPS-197917 Inline to make it consistent with EditBlogsEntryContentDashboardItemAction

--- modules/apps/content-dashboard/content-dashboard-document-library-impl/src/main/java/com/liferay/content/dashboard/document/library/internal/item/action/EditFileEntryContentDashboardItemAction.java
+++ modules/apps/content-dashboard/content-dashboard-document-library-impl/src/main/java/com/liferay/content/dashboard/document/library/internal/item/action/EditFileEntryContentDashboardItemAction.java
@@ -11,7 +11,6 @@ import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.model.Portlet;
 import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.service.PortletLocalService;
 import com.liferay.portal.kernel.util.Portal;
@@ -63,26 +62,19 @@ public class EditFileEntryContentDashboardItemAction
 
 	@Override
 	public String getURL() {
-		Portlet portlet = _portletLocalService.getPortletById(
-			_portal.getPortletId(_httpServletRequest));
-
-		HttpServletRequest httpServletRequest =
-			DynamicServletRequestUtil.createDynamicServletRequest(
-				_httpServletRequest, portlet,
-				Collections.singletonMap(
-					"redirect",
-					new String[] {_portal.getCurrentURL(_httpServletRequest)}),
-				true);
-
 		try {
-			String url = _infoEditURLProvider.getURL(
-				_fileEntry, httpServletRequest);
-
-			if (url == null) {
-				return StringPool.BLANK;
-			}
-
-			return url;
+			return _infoEditURLProvider.getURL(
+				_fileEntry,
+				DynamicServletRequestUtil.createDynamicServletRequest(
+					_httpServletRequest,
+					_portletLocalService.getPortletById(
+						_portal.getPortletId(_httpServletRequest)),
+					Collections.singletonMap(
+						"redirect",
+						new String[] {
+							_portal.getCurrentURL(_httpServletRequest)
+						}),
+					true));
 		}
 		catch (Exception exception) {
 			_log.error(exception);

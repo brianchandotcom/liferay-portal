0069c3e17e767330afd4adf0861edbc2cddde9a7
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2023-10-10 12:46:05 +0100

    LPS-197917 Inline to make it consistent with EditBlogsEntryContentDashboardItemAction

--- modules/apps/content-dashboard/content-dashboard-journal-impl/src/main/java/com/liferay/content/dashboard/journal/internal/item/action/EditJournalArticleContentDashboardItemAction.java
+++ modules/apps/content-dashboard/content-dashboard-journal-impl/src/main/java/com/liferay/content/dashboard/journal/internal/item/action/EditJournalArticleContentDashboardItemAction.java
@@ -12,7 +12,6 @@ import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.model.Portlet;
 import com.liferay.portal.kernel.service.PortletLocalService;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.servlet.DynamicServletRequestUtil;
@@ -64,26 +63,19 @@ public class EditJournalArticleContentDashboardItemAction
 
 	@Override
 	public String getURL() {
-		Portlet portlet = _portletLocalService.getPortletById(
-			_portal.getPortletId(_httpServletRequest));
-
-		HttpServletRequest httpServletRequest =
-			DynamicServletRequestUtil.createDynamicServletRequest(
-				_httpServletRequest, portlet,
-				Collections.singletonMap(
-					"redirect",
-					new String[] {_portal.getCurrentURL(_httpServletRequest)}),
-				true);
-
 		try {
-			String url = _infoEditURLProvider.getURL(
-				_journalArticle, httpServletRequest);
-
-			if (url == null) {
-				return StringPool.BLANK;
-			}
-
-			return url;
+			return _infoEditURLProvider.getURL(
+				_journalArticle,
+				DynamicServletRequestUtil.createDynamicServletRequest(
+					_httpServletRequest,
+					_portletLocalService.getPortletById(
+						_portal.getPortletId(_httpServletRequest)),
+					Collections.singletonMap(
+						"redirect",
+						new String[] {
+							_portal.getCurrentURL(_httpServletRequest)
+						}),
+					true));
 		}
 		catch (Exception exception) {
 			_log.error(exception);

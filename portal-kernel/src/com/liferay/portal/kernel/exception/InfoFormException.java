5178929b66fbe30176bd131b37621084622ee74e
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2023-10-10 15:37:45 +0100

    LPS-197921 Moves InfoFormException to portal-kernel

--- portal-kernel/src/com/liferay/portal/kernel/exception/InfoFormException.java
+++ portal-kernel/src/com/liferay/portal/kernel/exception/InfoFormException.java
@@ -3,9 +3,8 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.info.exception;
+package com.liferay.portal.kernel.exception;
 
-import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.language.LanguageUtil;
 
 import java.util.Locale;

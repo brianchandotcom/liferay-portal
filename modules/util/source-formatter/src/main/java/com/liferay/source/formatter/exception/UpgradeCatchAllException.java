b76776cf4e99afb77b760636b7fc7fcf40876d49
Nícolas Moura <nicolas_moura02@outlook.com>
2023-10-11 10:55:10 -0300

    LPS-192413 Follow up - create the exception package for source-formatter module

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/exception/UpgradeCatchAllException.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/exception/UpgradeCatchAllException.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.source.formatter;
+package com.liferay.source.formatter.exception;
 
 /**
  * @author Nícolas Moura

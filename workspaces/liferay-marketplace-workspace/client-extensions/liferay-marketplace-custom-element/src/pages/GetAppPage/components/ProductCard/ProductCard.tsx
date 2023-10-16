6b5978e6928ac107271d4e31612794b1246ae6be
Joao Marcelo Alves <joao.alves@liferay.com>
2023-10-16 17:52:49 -0300

    ISSD-1346 Fix Free appearing in all app types

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/GetAppPage/components/ProductCard/ProductCard.tsx
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/GetAppPage/components/ProductCard/ProductCard.tsx
@@ -38,40 +38,27 @@ const ProductCard = ({
 	const [hasTrial, setHasTrial] = useState(false);
 	const [basePrice, setBasePrice] = useState(0);
 
-	const productHasTrialSKU = (skus: SKU[]) => {
-		skus.forEach(async (sku) => {
-			const licenseUsageType = sku?.skuOptions.find((option) => {
-				return (
-					option?.key === 'trial' &&
-					option?.value === 'yes' &&
-					option?.key
-				);
-			});
-			if (
-				licenseUsageType &&
-				licenseUsageType?.key.toLowerCase() ===
-					SkuOptions.TRIAL.toLowerCase()
-			) {
-				setHasTrial(true);
-			}
-		});
-	};
-
-	const getProductBasePrice = (product: Product) => {
-		product?.skus?.forEach((sku) => {
-			const licenseUsageType = sku?.skuOptions.find(
+	const getProductBasePriceAndTrial = (skus: SKU[]) => {
+		skus?.forEach((sku) => {
+			const licenseUsageTypes = sku?.skuOptions.filter(
 				(skuOption) =>
-					skuOption?.key === 'standard' &&
-					skuOption?.value === 'yes' &&
-					skuOption?.key
+					skuOption?.value === 'standard' ||
+					skuOption?.value === 'trial'
 			);
 
-			if (
-				licenseUsageType?.key.toLowerCase() ===
-				SkuOptions.STANDARD.toLowerCase()
-			) {
-				setBasePrice(sku.price);
-			}
+			licenseUsageTypes.forEach((licenseUsageType) => {
+				switch (licenseUsageType?.value.toLowerCase()) {
+					case SkuOptions.STANDARD.toLowerCase():
+						setBasePrice(sku.price);
+						break;
+					case SkuOptions.TRIAL.toLowerCase():
+						setHasTrial(true);
+						break;
+
+					default:
+						break;
+				}
+			});
 		});
 	};
 
@@ -120,8 +107,7 @@ const ProductCard = ({
 
 	useEffect(() => {
 		if (product) {
-			productHasTrialSKU(product.skus);
-			getProductBasePrice(product);
+			getProductBasePriceAndTrial(product.skus);
 		}
 	}, [product]);
 

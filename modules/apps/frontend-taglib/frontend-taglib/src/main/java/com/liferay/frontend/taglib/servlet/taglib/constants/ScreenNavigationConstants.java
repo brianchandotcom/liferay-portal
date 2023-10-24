8258b58c474db6a21a8e221e67dbc8eb16e7fcfe
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2023-10-24 16:37:33 +0100

    LPS-197521 Adds new class for constants

---
+++ modules/apps/frontend-taglib/frontend-taglib/src/main/java/com/liferay/frontend/taglib/servlet/taglib/constants/ScreenNavigationConstants.java
@@ -0,0 +1,19 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.frontend.taglib.servlet.taglib.constants;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class ScreenNavigationConstants {
+
+	public static final String SELECTED_CATEGORY_KEY =
+		"selectedScreenNavigationCategoryKey";
+
+	public static final String SELECTED_ENTRY_KEY =
+		"selectedScreenNavigationEntryKey";
+
+}

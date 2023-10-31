1d3b9f1fa71e29e8a19cfa1dd71307dbda9fa211
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2023-10-31 17:19:00 +0000

    LPS-200114 Rename

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
@@ -1034,7 +1034,7 @@ public class JournalDisplayContext {
 		return articleSearchContainer.getTotal();
 	}
 
-	public VerticalNavItemList getVerticalNavDDMStructureList() {
+	public VerticalNavItemList getDDMStructureVerticalNavItemList() {
 		VerticalNavItemList verticalNavItemList = new VerticalNavItemList();
 
 		for (DDMStructure ddmStructure :

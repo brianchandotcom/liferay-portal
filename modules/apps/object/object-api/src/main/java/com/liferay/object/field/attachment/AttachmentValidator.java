371acaf35d1ce4e9abd38146133bd16cbd3a8c99
Carlos Correa <carlos.correa@liferay.com>
2023-11-02 18:32:56 +0100

    LPS-200329 Move AttachmentValidator to objects-api and objects-service

---
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/field/attachment/AttachmentValidator.java
@@ -0,0 +1,28 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.field.attachment;
+
+import com.liferay.document.library.kernel.exception.FileExtensionException;
+import com.liferay.document.library.kernel.exception.FileSizeException;
+
+import java.util.List;
+
+/**
+ * @author Carlos Correa
+ */
+public interface AttachmentValidator {
+	public String[] getAcceptedFileExtensions(long objectFieldId);
+
+	public long getMaximumFileSize(long objectFieldId, boolean signedIn);
+
+	public void validateFileExtension(String fileName, long objectFieldId)
+		throws FileExtensionException;
+
+	public void validateFileSize(
+		String fileName, long fileSize, long objectFieldId,
+		boolean signedIn) throws FileSizeException;
+
+}

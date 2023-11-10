69065089e4583871211e4c2abbe06cce65c9f57b
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2023-11-10 17:02:21 +0100

    LPS-201244 Avoid duplicated client extensions. Also, since the order matters add Layout, Master and LayoutSet and then reverse to add to the page in the correct order.
    So, if we have:
    
    Layout Set
    Global CSS
    Layout Set CSS
    
    Master
    Global CSS
    Master CSS
    
    Layout
    Global CSS
    
    We should return:
    
    Layout Set CSS
    Master CSS
    Global CSS

--- modules/apps/client-extension/client-extension-service/src/main/java/com/liferay/client/extension/internal/service/taglib/util/ClientExtensionDynamicIncludeUtil.java
+++ modules/apps/client-extension/client-extension-service/src/main/java/com/liferay/client/extension/internal/service/taglib/util/ClientExtensionDynamicIncludeUtil.java
@@ -10,10 +10,13 @@ import com.liferay.client.extension.service.ClientExtensionEntryRelLocalServiceU
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.LayoutSet;
 import com.liferay.portal.kernel.service.LayoutLocalServiceUtil;
+import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @author Eudaldo Alonso
@@ -29,24 +32,57 @@ public class ClientExtensionDynamicIncludeUtil {
 			new ArrayList<>(
 				ClientExtensionEntryRelLocalServiceUtil.
 					getClientExtensionEntryRels(
-						PortalUtil.getClassNameId(LayoutSet.class),
-						layoutSet.getLayoutSetId(), type));
+						PortalUtil.getClassNameId(Layout.class),
+						layout.getPlid(), type));
 
 		Layout masterLayout = LayoutLocalServiceUtil.fetchLayout(
 			layout.getMasterLayoutPlid());
 
 		if (masterLayout != null) {
-			clientExtensionEntryRels.addAll(
+			List<ClientExtensionEntryRel> masterLayoutClientExtensionEntryRels =
 				ClientExtensionEntryRelLocalServiceUtil.
 					getClientExtensionEntryRels(
 						PortalUtil.getClassNameId(Layout.class),
-						masterLayout.getPlid(), type));
+						masterLayout.getPlid(), type);
+
+			for (ClientExtensionEntryRel masterLayoutClientExtensionEntryRel :
+					masterLayoutClientExtensionEntryRels) {
+
+				if (!ListUtil.exists(
+						clientExtensionEntryRels,
+						clientExtensionEntryRel -> Objects.equals(
+							masterLayoutClientExtensionEntryRel.
+								getCETExternalReferenceCode(),
+							clientExtensionEntryRel.
+								getCETExternalReferenceCode()))) {
+
+					clientExtensionEntryRels.add(
+						masterLayoutClientExtensionEntryRel);
+				}
+			}
 		}
 
-		clientExtensionEntryRels.addAll(
+		List<ClientExtensionEntryRel> layoutSetClientExtensionEntryRels =
 			ClientExtensionEntryRelLocalServiceUtil.getClientExtensionEntryRels(
-				PortalUtil.getClassNameId(Layout.class), layout.getPlid(),
-				type));
+				PortalUtil.getClassNameId(LayoutSet.class),
+				layoutSet.getLayoutSetId(), type);
+
+		for (ClientExtensionEntryRel layoutSetClientExtensionEntryRel :
+				layoutSetClientExtensionEntryRels) {
+
+			if (!ListUtil.exists(
+					clientExtensionEntryRels,
+					clientExtensionEntryRel -> Objects.equals(
+						layoutSetClientExtensionEntryRel.
+							getCETExternalReferenceCode(),
+						clientExtensionEntryRel.
+							getCETExternalReferenceCode()))) {
+
+				clientExtensionEntryRels.add(layoutSetClientExtensionEntryRel);
+			}
+		}
+
+		Collections.reverse(clientExtensionEntryRels);
 
 		return clientExtensionEntryRels;
 	}

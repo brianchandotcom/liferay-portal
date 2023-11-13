2e682e3b21057299684a96e378824104fd2de1dd
Alicia García <alicia.garcia@liferay.com>
2023-11-13 15:53:50 +0100

    LPS-197916 autoSF

--- modules/apps/saved-content/saved-content-api/src/main/java/com/liferay/saved/content/exception/DuplicateSavedContentEntryException.java
+++ modules/apps/saved-content/saved-content-api/src/main/java/com/liferay/saved/content/exception/DuplicateSavedContentEntryException.java
@@ -2,6 +2,7 @@
  * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
+
 package com.liferay.saved.content.exception;
 
 import com.liferay.portal.kernel.exception.PortalException;
@@ -18,7 +19,9 @@ public class DuplicateSavedContentEntryException extends PortalException {
 		super(msg);
 	}
 
-	public DuplicateSavedContentEntryException(String msg, Throwable throwable) {
+	public DuplicateSavedContentEntryException(
+		String msg, Throwable throwable) {
+
 		super(msg, throwable);
 	}
 

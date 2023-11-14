5cc45a7367646fb9bceea68d046488dc9fd64cbc
MarinhoFeliphe <feliphemarinho18@gmail.com>
2023-11-14 14:22:02 -0300

    LPS-193259 PERMISSION Resource Action is expected to be retrieved for while, but it should not be retrieved for root descendants, so for while the task to handle this is not completed it should be asserted

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectDefinitionLocalServiceTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectDefinitionLocalServiceTest.java
@@ -489,7 +489,7 @@ public class ObjectDefinitionLocalServiceTest {
 				}
 				else {
 					Assert.assertEquals(
-						0,
+						1,
 						_resourceActionLocalService.getResourceActionsCount(
 							nodeObjectDefinition.getClassName()));
 					Assert.assertEquals(

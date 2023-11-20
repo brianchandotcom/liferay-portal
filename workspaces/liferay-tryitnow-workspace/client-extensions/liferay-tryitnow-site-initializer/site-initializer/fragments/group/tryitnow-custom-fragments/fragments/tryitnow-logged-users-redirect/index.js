7d212a2527baafc18720c1208f22c9f97fe77166
Mu'men Tayyem <mumen_tayyem@outlook.com>
2023-11-20 13:28:13 +0400

    LPS-197318 - adding fragments

--- workspaces/liferay-tryitnow-workspace/client-extensions/liferay-tryitnow-site-initializer/site-initializer/fragments/group/tryitnow-custom-fragments/fragments/tryitnow-logged-users-redirect/index.js
+++ workspaces/liferay-tryitnow-workspace/client-extensions/liferay-tryitnow-site-initializer/site-initializer/fragments/group/tryitnow-custom-fragments/fragments/tryitnow-logged-users-redirect/index.js
@@ -1,6 +1,10 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
 const editMode = document.body.classList.contains('has-edit-mode-menu');
 const signIn = Liferay.ThemeDisplay.isSignedIn();
-var URLredirect = Liferay.ThemeDisplay.getURLHome();
+const URLredirect = Liferay.ThemeDisplay.getURLHome();
 
 if (!editMode && signIn) {
 	window.location.href = '/web/'+ (configuration.groupKey == 'test'? 'test-1' : configuration.groupKey) +'/home';

56a0bd718fcfba11045d1f8761aa2a0c308e57c8
Mu'men Tayyem <mumen_tayyem@outlook.com>
2023-11-21 11:04:18 +0400

    LPS-197318 - Fragments SF

--- workspaces/liferay-tryitnow-workspace/client-extensions/liferay-tryitnow-site-initializer/site-initializer/fragments/group/tryitnow-custom-fragments/fragments/tryitnow-logged-users-redirect/index.js
+++ workspaces/liferay-tryitnow-workspace/client-extensions/liferay-tryitnow-site-initializer/site-initializer/fragments/group/tryitnow-custom-fragments/fragments/tryitnow-logged-users-redirect/index.js
@@ -7,8 +7,14 @@ const signIn = Liferay.ThemeDisplay.isSignedIn();
 const URLredirect = Liferay.ThemeDisplay.getURLHome();
 
 if (!editMode && signIn) {
-	window.location.href = '/web/'+ (configuration.groupKey == 'test'? 'test-1' : configuration.groupKey) +'/home';
-} else if (editMode) {
+	window.location.href =
+		'/web/' +
+		(configuration.groupKey === 'test'
+			? 'test-1'
+			: configuration.groupKey) +
+		'/home';
+}
+else if (editMode) {
 	fragmentElement.querySelector('.url-div').classList.remove('d-none');
 	fragmentElement.querySelector('.url-link').innerHTML = URLredirect;
 }

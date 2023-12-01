brandonyee333/test

LPS-201373 Reuse AssetDisplayPageSelectorCriterion to avoid duplicate code, also to be able to use the new functionalities that are going to be added in AssetDisplayPageSelectorCriterion like folders navigation.

modules/apps/commerce/commerce-product-definitions-web/src/main/resources/META-INF/resources/js/EditDisplayLayout.js
@@ -141,9 +141,11 @@ export default function ({
 							return;
 						}
 
-						pagesContainerInput.value = selectedItem.id;
+						const itemValue = JSON.parse(selectedItem.value);
 
-						displayPageNameInput.innerHTML = selectedItem.name;
+						pagesContainerInput.value = itemValue.id;
+
+						displayPageNameInput.innerHTML = itemValue.name;
 
 						displayPageItemRemoveIcon.classList.remove('hide');
 					},

brianchandotcom/liferay-portal

modules/apps/commerce/commerce-product-definitions-web/src/main/resources/META-INF/resources/js/EditDisplayLayout.js
@@ -165,9 +165,11 @@ export default function ({
 								return;
 							}
 
-							pagesContainerInput.value = selectedItem.id;
+							const itemValue = JSON.parse(selectedItem.value);
 
-							displayPageNameInput.innerHTML = selectedItem.name;
+							pagesContainerInput.value = itemValue.uuid;
+
+							displayPageNameInput.innerHTML = itemValue.name;
 
 							displayPageItemRemoveIcon.classList.remove('hide');
 						},
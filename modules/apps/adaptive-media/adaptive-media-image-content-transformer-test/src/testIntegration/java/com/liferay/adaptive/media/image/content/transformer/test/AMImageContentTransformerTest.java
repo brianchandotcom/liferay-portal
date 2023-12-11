09206af85d0df61cfa6929f6d59fe330b15a031e
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2023-12-11 16:23:13 +0100

    LPS-203653 No longer needed

--- modules/apps/adaptive-media/adaptive-media-image-content-transformer-test/src/testIntegration/java/com/liferay/adaptive/media/image/content/transformer/test/AMImageContentTransformerTest.java
+++ modules/apps/adaptive-media/adaptive-media-image-content-transformer-test/src/testIntegration/java/com/liferay/adaptive/media/image/content/transformer/test/AMImageContentTransformerTest.java
@@ -5,7 +5,6 @@
 
 package com.liferay.adaptive.media.image.content.transformer.test;
 
-import com.liferay.adaptive.media.content.transformer.ContentTransformer;
 import com.liferay.adaptive.media.content.transformer.ContentTransformerHandler;
 import com.liferay.adaptive.media.content.transformer.constants.ContentTransformerContentTypes;
 import com.liferay.adaptive.media.image.configuration.AMImageConfigurationEntry;
@@ -126,12 +125,5 @@ public class AMImageContentTransformerTest {
 
 	@DeleteAfterTestRun
 	private Group _group;
-
-	// See LPS-202810
-
-	@Inject(
-		filter = "component.name=com.liferay.adaptive.media.image.content.transformer.internal.HtmlContentTransformerImpl"
-	)
-	private ContentTransformer<String> _htmlContentTransformer;
 
 }

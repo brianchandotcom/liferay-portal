46d387342c2dfe3221e0783e7fdf462b699fd35a
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2023-12-11 14:24:07 +0100

    LPS-203653 Move classes to content transformer module

--- modules/apps/adaptive-media/adaptive-media-content-transformer-impl/src/main/java/com/liferay/adaptive/media/content/transformer/internal/HtmlContentTransformerImpl.java
+++ modules/apps/adaptive-media/adaptive-media-content-transformer-impl/src/main/java/com/liferay/adaptive/media/content/transformer/internal/HtmlContentTransformerImpl.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.adaptive.media.image.content.transformer.internal;
+package com.liferay.adaptive.media.content.transformer.internal;
 
 import com.liferay.adaptive.media.content.transformer.BaseRegexStringContentTransformer;
 import com.liferay.adaptive.media.content.transformer.ContentTransformer;

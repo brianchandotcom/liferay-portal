350089064706be2fe69d314e943b71b5427d2c76
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2023-12-14 13:28:50 +0100

    LPS-203214 Caches in a var to prevent a weird behavior in the corner case that the company configuration is changed during the configuration view is rendering

--- modules/apps/site/site-sitemap-web/src/main/java/com/liferay/site/sitemap/web/internal/display/context/SitemapGroupConfigurationDisplayContext.java
+++ modules/apps/site/site-sitemap-web/src/main/java/com/liferay/site/sitemap/web/internal/display/context/SitemapGroupConfigurationDisplayContext.java
@@ -38,35 +38,59 @@ public class SitemapGroupConfigurationDisplayContext {
 	}
 
 	public boolean isIncludeCategoriesDisabled() throws ConfigurationException {
+		if (_includeCategoriesDisabled != null) {
+			return _includeCategoriesDisabled;
+		}
+
 		if (_sitemapConfigurationManager.includeCategoriesCompanyEnabled(
 				_themeDisplay.getCompanyId())) {
 
-			return false;
+			_includeCategoriesDisabled = false;
+		}
+		else {
+			_includeCategoriesDisabled = true;
 		}
 
-		return true;
+		return _includeCategoriesDisabled;
 	}
 
 	public boolean isIncludePagesDisabled() throws ConfigurationException {
+		if (_includePagesDisabled != null) {
+			return _includePagesDisabled;
+		}
+
 		if (_sitemapConfigurationManager.includePagesCompanyEnabled(
 				_themeDisplay.getCompanyId())) {
 
-			return false;
+			_includePagesDisabled = false;
+		}
+		else {
+			_includePagesDisabled = true;
 		}
 
-		return true;
+		return _includePagesDisabled;
 	}
 
 	public boolean isIncludeWebContentDisabled() throws ConfigurationException {
+		if (_includeWebContentDisabled != null) {
+			return _includeWebContentDisabled;
+		}
+
 		if (_sitemapConfigurationManager.includeWebContentCompanyEnabled(
 				_themeDisplay.getCompanyId())) {
 
-			return false;
+			_includeWebContentDisabled = false;
+		}
+		else {
+			_includeWebContentDisabled = true;
 		}
 
-		return true;
+		return _includeWebContentDisabled;
 	}
 
+	private Boolean _includeCategoriesDisabled;
+	private Boolean _includePagesDisabled;
+	private Boolean _includeWebContentDisabled;
 	private final SitemapConfigurationManager _sitemapConfigurationManager;
 	private final ThemeDisplay _themeDisplay;
 

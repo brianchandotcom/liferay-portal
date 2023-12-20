b8ed50ebd3e93a6d332a806ab3ec015421b259f4
Luca Pellizzon <luca.pellizzon@liferay.com>
2023-12-20 16:38:26 +0100

    COMMERCE-12880 CX implementation

---
+++ workspaces/liferay-sample-workspace/client-extensions/liferay-sample-commerce-shipping-engine/src/main/java/com/liferay/sample/OptionLabelController.java
@@ -0,0 +1,61 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.sample;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.json.JSONObject;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Luca Pellizzon
+ */
+@RequestMapping("/option-label")
+@RestController
+public class OptionLabelController extends BaseRestController {
+
+	@PostMapping
+	public ResponseEntity<String> post(
+			@AuthenticationPrincipal Jwt jwt, @RequestBody String json)
+		throws Exception {
+
+		log(jwt, _log, json);
+
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		Map<String, String> requestBodyMap = objectMapper.readValue(
+			json, HashMap.class);
+
+		if (requestBodyMap.containsKey("name")) {
+			return new ResponseEntity<>(
+				new JSONObject(
+				).put(
+					"name", "Shipping Option Name"
+				).toString(),
+				HttpStatus.OK);
+		}
+
+		return null;
+	}
+
+	private static final Log _log = LogFactory.getLog(
+		OptionLabelController.class);
+
+}

b388896446fe2f926cc6aa0ec86a28048f2e372f
gustavolimav <gustavo.lima@liferay.com>
2023-12-21 21:02:33 -0300

    LPS-197723 LPS-203746 search-experiences-web: Refactor and remove unecessary code

--- modules/dxp/apps/search-experiences/search-experiences-web/src/main/java/com/liferay/search/experiences/web/internal/blueprint/admin/portlet/action/GetSitesMVCResourceCommand.java
+++ modules/dxp/apps/search-experiences/search-experiences-web/src/main/java/com/liferay/search/experiences/web/internal/blueprint/admin/portlet/action/GetSitesMVCResourceCommand.java
@@ -5,26 +5,17 @@
 
 package com.liferay.search.experiences.web.internal.blueprint.admin.portlet.action;
 
-import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONObject;
-import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.model.Group;
-import com.liferay.portal.kernel.model.GroupConstants;
 import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
-import com.liferay.portal.kernel.service.GroupService;
-import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.search.site.SitesProvider;
 import com.liferay.search.experiences.constants.SXPPortletKeys;
 
 import java.io.IOException;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.portlet.ResourceRequest;
 import javax.portlet.ResourceResponse;
@@ -51,9 +42,21 @@ public class GetSitesMVCResourceCommand implements MVCResourceCommand {
 		ResourceRequest resourceRequest, ResourceResponse resourceResponse) {
 
 		try {
+			String cmd = ParamUtil.getString(resourceRequest, Constants.CMD);
+
+			JSONObject jsonObject = null;
+
+			if (cmd.equals("getSitesJSONObject")) {
+				jsonObject = _sitesProvider.getSitesJSONObject(resourceRequest);
+			}
+			else if (cmd.equals("getSiteByExternalReferenceCodeJSONObject")) {
+				jsonObject =
+					_sitesProvider.getSiteByExternalReferenceCodeJSONObject(
+						resourceRequest);
+			}
+
 			_writeJSONPortletResponse(
-				resourceRequest, resourceResponse,
-				_getJSONObject(resourceRequest));
+				resourceRequest, resourceResponse, jsonObject);
 
 			return false;
 		}
@@ -62,135 +65,6 @@ public class GetSitesMVCResourceCommand implements MVCResourceCommand {
 
 			throw new RuntimeException(exception);
 		}
-	}
-
-	protected JSONObject getSiteByExternalReferenceCodeJSONObject(
-			ResourceRequest resourceRequest)
-		throws Exception {
-
-		ThemeDisplay themeDisplay = (ThemeDisplay)resourceRequest.getAttribute(
-			WebKeys.THEME_DISPLAY);
-
-		Group group = _groupService.fetchGroupByExternalReferenceCode(
-			ParamUtil.getString(resourceRequest, "externalReferenceCode"),
-			themeDisplay.getCompanyId());
-
-		if (group == null) {
-			return null;
-		}
-
-		return JSONUtil.put(
-			"descriptiveName",
-			group.getDescriptiveName(themeDisplay.getLocale())
-		).put(
-			"externalReferenceCode", group.getExternalReferenceCode()
-		).put(
-			"groupId", group.getGroupId()
-		).put(
-			"name", group.getName(themeDisplay.getLocale())
-		);
-	}
-
-	protected JSONObject getSitesJSONObject(ResourceRequest resourceRequest)
-		throws Exception {
-
-		ThemeDisplay themeDisplay = (ThemeDisplay)resourceRequest.getAttribute(
-			WebKeys.THEME_DISPLAY);
-
-		List<Group> allGroups = new ArrayList<>();
-
-		_addGroups(
-			allGroups,
-			_groupService.getGroups(
-				themeDisplay.getCompanyId(),
-				GroupConstants.DEFAULT_PARENT_GROUP_ID, true));
-
-		allGroups.sort(
-			(g1, g2) -> {
-				try {
-					return g1.getDescriptiveName(
-						themeDisplay.getLocale()
-					).compareTo(
-						g2.getDescriptiveName(themeDisplay.getLocale())
-					);
-				}
-				catch (PortalException portalException) {
-					_log.error(portalException);
-				}
-
-				return 0;
-			});
-
-		int pageSize = ParamUtil.getInteger(resourceRequest, "pageSize");
-		int totalCount = allGroups.size();
-
-		int lastPage = (int)Math.ceil((double)totalCount / pageSize);
-
-		int page = ParamUtil.getInteger(resourceRequest, "page");
-
-		if (page > lastPage) {
-			page = lastPage;
-		}
-
-		int end = pageSize * page;
-
-		int start = end - pageSize;
-
-		if (end > totalCount) {
-			end = totalCount;
-		}
-
-		return JSONUtil.put(
-			"items",
-			JSONUtil.toJSONArray(
-				allGroups.subList(start, end),
-				group -> JSONUtil.put(
-					"descriptiveName",
-					group.getDescriptiveName(themeDisplay.getLocale())
-				).put(
-					"externalReferenceCode", group.getExternalReferenceCode()
-				).put(
-					"groupId", group.getGroupId()
-				).put(
-					"name", group.getName(themeDisplay.getLocale())
-				))
-		).put(
-			"lastPage", lastPage
-		).put(
-			"page", page
-		).put(
-			"pageSize", pageSize
-		).put(
-			"totalCount", totalCount
-		);
-	}
-
-	private void _addGroups(List<Group> allGroups, List<Group> groups) {
-		groups.forEach(
-			group -> {
-				if (!group.isActive()) {
-					return;
-				}
-
-				_addGroups(allGroups, group.getChildren(true));
-
-				allGroups.add(group);
-			});
-	}
-
-	private JSONObject _getJSONObject(ResourceRequest resourceRequest)
-		throws Exception {
-
-		String cmd = ParamUtil.getString(resourceRequest, Constants.CMD);
-
-		if (cmd.equals("getSitesJSONObject")) {
-			return getSitesJSONObject(resourceRequest);
-		}
-		else if (cmd.equals("getSiteByExternalReferenceCodeJSONObject")) {
-			return getSiteByExternalReferenceCodeJSONObject(resourceRequest);
-		}
-
-		return null;
 	}
 
 	private void _writeJSONPortletResponse(
@@ -214,6 +88,6 @@ public class GetSitesMVCResourceCommand implements MVCResourceCommand {
 		GetSitesMVCResourceCommand.class);
 
 	@Reference
-	private GroupService _groupService;
+	private SitesProvider _sitesProvider;
 
 }

9f3f0f066a9ae034c012e15168177fda6d36be8f
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2023-12-27 16:52:53 +0000

    LPS-204724 Creates new interface and reuse the same Document

--- modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-styles/src/main/java/com/liferay/fragment/entry/processor/styles/StylesFragmentEntryProcessor.java
+++ modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-styles/src/main/java/com/liferay/fragment/entry/processor/styles/StylesFragmentEntryProcessor.java
@@ -7,12 +7,7 @@ package com.liferay.fragment.entry.processor.styles;
 
 import com.liferay.fragment.processor.FragmentEntryProcessor;
 import com.liferay.portal.kernel.json.JSONArray;
-import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -28,33 +23,6 @@ public class StylesFragmentEntryProcessor implements FragmentEntryProcessor {
 	@Override
 	public JSONArray getDataAttributesJSONArray() {
 		return JSONUtil.put("lfr-styles");
-	}
-
-	@Override
-	public JSONObject getDefaultEditableValuesJSONObject(
-		String html, String configuration) {
-
-		Document document = _getDocument(html);
-
-		Elements elements = document.select("[data-lfr-styles]");
-
-		if (elements.isEmpty()) {
-			return null;
-		}
-
-		return JSONUtil.put("hasCommonStyles", true);
-	}
-
-	private Document _getDocument(String html) {
-		Document document = Jsoup.parseBodyFragment(html);
-
-		Document.OutputSettings outputSettings = new Document.OutputSettings();
-
-		outputSettings.prettyPrint(false);
-
-		document.outputSettings(outputSettings);
-
-		return document;
 	}
 
 }

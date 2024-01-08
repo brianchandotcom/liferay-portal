brandonyee333/test

LPS-197331 refactor DLExportableRepositoryPublisherHelper, convert to a util class, removing components and apply to usages

modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/exportimport/staged/model/repository/FileEntryStagedModelRepository.java
@@ -5,7 +5,7 @@
 
 package com.liferay.document.library.internal.exportimport.staged.model.repository;
 
-import com.liferay.document.library.internal.helper.DLExportableRepositoryPublisherHelper;
+import com.liferay.document.library.internal.util.DLExportableRepositoryPublisherHelperUtil;
 import com.liferay.document.library.kernel.model.DLFileEntry;
 import com.liferay.document.library.kernel.model.DLFileEntryConstants;
 import com.liferay.document.library.kernel.model.DLFileVersion;
@@ -99,7 +99,7 @@ public class FileEntryStagedModelRepository
 		PortletDataContext portletDataContext) {
 
 		Collection<Long> exportableRepositoryIds =
-			_dlExportableRepositoryPublisherHelper.publish(
+			DLExportableRepositoryPublisherHelperUtil.publish(
 				portletDataContext.getScopeGroupId());
 
 		ExportActionableDynamicQuery exportActionableDynamicQuery =
@@ -213,10 +213,6 @@ public class FileEntryStagedModelRepository
 
 	@Reference
 	private DLAppLocalService _dlAppLocalService;
-
-	@Reference
-	private DLExportableRepositoryPublisherHelper
-		_dlExportableRepositoryPublisherHelper;
 
 	@Reference
 	private DLFileEntryLocalService _dlFileEntryLocalService;

modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/exportimport/staged/model/repository/FolderStagedModelRepository.java
@@ -5,7 +5,7 @@
 
 package com.liferay.document.library.internal.exportimport.staged.model.repository;
 
-import com.liferay.document.library.internal.helper.DLExportableRepositoryPublisherHelper;
+import com.liferay.document.library.internal.util.DLExportableRepositoryPublisherHelperUtil;
 import com.liferay.document.library.kernel.model.DLFolder;
 import com.liferay.document.library.kernel.model.DLFolderConstants;
 import com.liferay.document.library.kernel.service.DLAppLocalService;
@@ -88,7 +88,7 @@ public class FolderStagedModelRepository
 		PortletDataContext portletDataContext) {
 
 		Collection<Long> exportableRepositoryIds =
-			_dlExportableRepositoryPublisherHelper.publish(
+			DLExportableRepositoryPublisherHelperUtil.publish(
 				portletDataContext.getScopeGroupId());
 
 		ExportActionableDynamicQuery exportActionableDynamicQuery =
@@ -151,10 +151,6 @@ public class FolderStagedModelRepository
 
 	@Reference
 	private DLAppLocalService _dlAppLocalService;
-
-	@Reference
-	private DLExportableRepositoryPublisherHelper
-		_dlExportableRepositoryPublisherHelper;
 
 	@Reference
 	private DLFolderLocalService _dlFolderLocalService;

modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/exportimport/staged/model/repository/RepositoryStagedModelRepository.java
@@ -6,7 +6,7 @@
 package com.liferay.document.library.internal.exportimport.staged.model.repository;
 
 import com.liferay.document.library.constants.DLPortletKeys;
-import com.liferay.document.library.internal.helper.DLExportableRepositoryPublisherHelper;
+import com.liferay.document.library.internal.util.DLExportableRepositoryPublisherHelperUtil;
 import com.liferay.exportimport.kernel.lar.PortletDataContext;
 import com.liferay.exportimport.kernel.lar.PortletDataException;
 import com.liferay.exportimport.kernel.lar.StagedModelType;
@@ -96,7 +96,7 @@ public class RepositoryStagedModelRepository
 		PortletDataContext portletDataContext) {
 
 		Collection<Long> exportableRepositoryIds =
-			_dlExportableRepositoryPublisherHelper.publish(
+			DLExportableRepositoryPublisherHelperUtil.publish(
 				portletDataContext.getScopeGroupId());
 
 		ExportActionableDynamicQuery exportActionableDynamicQuery =
@@ -186,10 +186,6 @@ public class RepositoryStagedModelRepository
 
 		throw new UnsupportedOperationException();
 	}
-
-	@Reference
-	private DLExportableRepositoryPublisherHelper
-		_dlExportableRepositoryPublisherHelper;
 
 	@Reference
 	private Portal _portal;

modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/model/listener/DLFileVersionStagingModelListener.java
@@ -5,7 +5,7 @@
 
 package com.liferay.document.library.internal.model.listener;
 
-import com.liferay.document.library.internal.helper.DLExportableRepositoryPublisherHelper;
+import com.liferay.document.library.internal.util.DLExportableRepositoryPublisherHelperUtil;
 import com.liferay.document.library.kernel.model.DLFileEntry;
 import com.liferay.document.library.kernel.model.DLFileVersion;
 import com.liferay.portal.kernel.exception.ModelListenerException;
@@ -49,7 +49,7 @@ public class DLFileVersionStagingModelListener
 		}
 
 		Collection<Long> exportableRepositoryIds =
-			_dlExportableRepositoryPublisherHelper.publish(
+			DLExportableRepositoryPublisherHelperUtil.publish(
 				dlFileEntry.getGroupId());
 
 		if (!exportableRepositoryIds.contains(dlFileEntry.getRepositoryId())) {
@@ -82,7 +82,7 @@ public class DLFileVersionStagingModelListener
 		}
 
 		Collection<Long> exportableRepositoryIds =
-			_dlExportableRepositoryPublisherHelper.publish(
+			DLExportableRepositoryPublisherHelperUtil.publish(
 				dlFileEntry.getGroupId());
 
 		if (!exportableRepositoryIds.contains(dlFileEntry.getRepositoryId())) {
@@ -94,10 +94,6 @@ public class DLFileVersionStagingModelListener
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		DLFileVersionStagingModelListener.class);
-
-	@Reference
-	private DLExportableRepositoryPublisherHelper
-		_dlExportableRepositoryPublisherHelper;
 
 	@Reference
 	private StagingModelListener<DLFileEntry> _stagingModelListener;

modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/model/listener/RepositoryStagingModelListener.java
@@ -6,7 +6,7 @@
 package com.liferay.document.library.internal.model.listener;
 
 import com.liferay.document.library.constants.DLPortletKeys;
-import com.liferay.document.library.internal.helper.DLExportableRepositoryPublisherHelper;
+import com.liferay.document.library.internal.util.DLExportableRepositoryPublisherHelperUtil;
 import com.liferay.portal.kernel.exception.ModelListenerException;
 import com.liferay.portal.kernel.model.BaseModelListener;
 import com.liferay.portal.kernel.model.ModelListener;
@@ -77,7 +77,7 @@ public class RepositoryStagingModelListener
 		}
 
 		Collection<Long> exportableRepositoryIds =
-			_dlExportableRepositoryPublisherHelper.publish(
+			DLExportableRepositoryPublisherHelperUtil.publish(
 				repository.getGroupId());
 		String portletId = repository.getPortletId();
 
@@ -91,10 +91,6 @@ public class RepositoryStagingModelListener
 
 		return true;
 	}
-
-	@Reference
-	private DLExportableRepositoryPublisherHelper
-		_dlExportableRepositoryPublisherHelper;
 
 	@Reference
 	private Portal _portal;

brianchandotcom/liferay-portal

modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/exportimport/staged/model/repository/FileEntryStagedModelRepository.java
@@ -5,7 +5,7 @@
 
 package com.liferay.document.library.internal.exportimport.staged.model.repository;
 
-import com.liferay.document.library.internal.helper.DLExportableRepositoryPublisherHelper;
+import com.liferay.document.library.internal.util.DLExportableRepositoryPublisherUtil;
 import com.liferay.document.library.kernel.model.DLFileEntry;
 import com.liferay.document.library.kernel.model.DLFileEntryConstants;
 import com.liferay.document.library.kernel.model.DLFileVersion;
@@ -99,7 +99,7 @@ public class FileEntryStagedModelRepository
 		PortletDataContext portletDataContext) {
 
 		Collection<Long> exportableRepositoryIds =
-			_dlExportableRepositoryPublisherHelper.publish(
+			DLExportableRepositoryPublisherUtil.publish(
 				portletDataContext.getScopeGroupId());
 
 		ExportActionableDynamicQuery exportActionableDynamicQuery =
@@ -213,10 +213,6 @@ public class FileEntryStagedModelRepository
 
 	@Reference
 	private DLAppLocalService _dlAppLocalService;
-
-	@Reference
-	private DLExportableRepositoryPublisherHelper
-		_dlExportableRepositoryPublisherHelper;
 
 	@Reference
 	private DLFileEntryLocalService _dlFileEntryLocalService;

modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/exportimport/staged/model/repository/FolderStagedModelRepository.java
@@ -5,7 +5,7 @@
 
 package com.liferay.document.library.internal.exportimport.staged.model.repository;
 
-import com.liferay.document.library.internal.helper.DLExportableRepositoryPublisherHelper;
+import com.liferay.document.library.internal.util.DLExportableRepositoryPublisherUtil;
 import com.liferay.document.library.kernel.model.DLFolder;
 import com.liferay.document.library.kernel.model.DLFolderConstants;
 import com.liferay.document.library.kernel.service.DLAppLocalService;
@@ -88,7 +88,7 @@ public class FolderStagedModelRepository
 		PortletDataContext portletDataContext) {
 
 		Collection<Long> exportableRepositoryIds =
-			_dlExportableRepositoryPublisherHelper.publish(
+			DLExportableRepositoryPublisherUtil.publish(
 				portletDataContext.getScopeGroupId());
 
 		ExportActionableDynamicQuery exportActionableDynamicQuery =
@@ -151,10 +151,6 @@ public class FolderStagedModelRepository
 
 	@Reference
 	private DLAppLocalService _dlAppLocalService;
-
-	@Reference
-	private DLExportableRepositoryPublisherHelper
-		_dlExportableRepositoryPublisherHelper;
 
 	@Reference
 	private DLFolderLocalService _dlFolderLocalService;

modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/exportimport/staged/model/repository/RepositoryStagedModelRepository.java
@@ -6,7 +6,7 @@
 package com.liferay.document.library.internal.exportimport.staged.model.repository;
 
 import com.liferay.document.library.constants.DLPortletKeys;
-import com.liferay.document.library.internal.helper.DLExportableRepositoryPublisherHelper;
+import com.liferay.document.library.internal.util.DLExportableRepositoryPublisherUtil;
 import com.liferay.exportimport.kernel.lar.PortletDataContext;
 import com.liferay.exportimport.kernel.lar.PortletDataException;
 import com.liferay.exportimport.kernel.lar.StagedModelType;
@@ -96,7 +96,7 @@ public class RepositoryStagedModelRepository
 		PortletDataContext portletDataContext) {
 
 		Collection<Long> exportableRepositoryIds =
-			_dlExportableRepositoryPublisherHelper.publish(
+			DLExportableRepositoryPublisherUtil.publish(
 				portletDataContext.getScopeGroupId());
 
 		ExportActionableDynamicQuery exportActionableDynamicQuery =
@@ -186,10 +186,6 @@ public class RepositoryStagedModelRepository
 
 		throw new UnsupportedOperationException();
 	}
-
-	@Reference
-	private DLExportableRepositoryPublisherHelper
-		_dlExportableRepositoryPublisherHelper;
 
 	@Reference
 	private Portal _portal;

modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/model/listener/DLFileVersionStagingModelListener.java
@@ -5,7 +5,7 @@
 
 package com.liferay.document.library.internal.model.listener;
 
-import com.liferay.document.library.internal.helper.DLExportableRepositoryPublisherHelper;
+import com.liferay.document.library.internal.util.DLExportableRepositoryPublisherUtil;
 import com.liferay.document.library.kernel.model.DLFileEntry;
 import com.liferay.document.library.kernel.model.DLFileVersion;
 import com.liferay.portal.kernel.exception.ModelListenerException;
@@ -49,7 +49,7 @@ public class DLFileVersionStagingModelListener
 		}
 
 		Collection<Long> exportableRepositoryIds =
-			_dlExportableRepositoryPublisherHelper.publish(
+			DLExportableRepositoryPublisherUtil.publish(
 				dlFileEntry.getGroupId());
 
 		if (!exportableRepositoryIds.contains(dlFileEntry.getRepositoryId())) {
@@ -82,7 +82,7 @@ public class DLFileVersionStagingModelListener
 		}
 
 		Collection<Long> exportableRepositoryIds =
-			_dlExportableRepositoryPublisherHelper.publish(
+			DLExportableRepositoryPublisherUtil.publish(
 				dlFileEntry.getGroupId());
 
 		if (!exportableRepositoryIds.contains(dlFileEntry.getRepositoryId())) {
@@ -94,10 +94,6 @@ public class DLFileVersionStagingModelListener
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		DLFileVersionStagingModelListener.class);
-
-	@Reference
-	private DLExportableRepositoryPublisherHelper
-		_dlExportableRepositoryPublisherHelper;
 
 	@Reference
 	private StagingModelListener<DLFileEntry> _stagingModelListener;

modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/model/listener/RepositoryStagingModelListener.java
@@ -6,7 +6,7 @@
 package com.liferay.document.library.internal.model.listener;
 
 import com.liferay.document.library.constants.DLPortletKeys;
-import com.liferay.document.library.internal.helper.DLExportableRepositoryPublisherHelper;
+import com.liferay.document.library.internal.util.DLExportableRepositoryPublisherUtil;
 import com.liferay.portal.kernel.exception.ModelListenerException;
 import com.liferay.portal.kernel.model.BaseModelListener;
 import com.liferay.portal.kernel.model.ModelListener;
@@ -77,7 +77,7 @@ public class RepositoryStagingModelListener
 		}
 
 		Collection<Long> exportableRepositoryIds =
-			_dlExportableRepositoryPublisherHelper.publish(
+			DLExportableRepositoryPublisherUtil.publish(
 				repository.getGroupId());
 		String portletId = repository.getPortletId();
 
@@ -91,10 +91,6 @@ public class RepositoryStagingModelListener
 
 		return true;
 	}
-
-	@Reference
-	private DLExportableRepositoryPublisherHelper
-		_dlExportableRepositoryPublisherHelper;
 
 	@Reference
 	private Portal _portal;
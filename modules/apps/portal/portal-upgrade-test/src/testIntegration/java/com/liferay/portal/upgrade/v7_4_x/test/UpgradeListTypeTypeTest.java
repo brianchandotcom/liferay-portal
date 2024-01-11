d4bcced11aa0011d7ab44bc0a58e43e4fe596fb1
javalosjr96 <jorge.avalos@lifray.com>
2024-01-11 11:31:26 -0800

    LPS-205471 improve UpgradeListTypeType test

--- modules/apps/portal/portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/v7_4_x/test/UpgradeListTypeTypeTest.java
+++ modules/apps/portal/portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/v7_4_x/test/UpgradeListTypeTypeTest.java
@@ -6,14 +6,18 @@
 package com.liferay.portal.upgrade.v7_4_x.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.dao.orm.FinderCacheUtil;
+import com.liferay.portal.kernel.model.ListType;
+import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
 import com.liferay.portal.kernel.service.ListTypeLocalService;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.upgrade.v7_4_x.UpgradeListTypeType;
-import com.liferay.portal.util.PortalInstances;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -30,16 +34,45 @@ public class UpgradeListTypeTypeTest {
 	public static final AggregateTestRule aggregateTestRule =
 		new LiferayIntegrationTestRule();
 
+	@Before
+	public void setUp() throws Exception {
+		_companyId = CompanyThreadLocal.getCompanyId();
+
+		_listTypeLocalService.addListType(
+			_companyId, _LIST_TYPE_INTRANET, _LIST_TYPE_ACCOUNT_ENTRY);
+
+		_listTypeLocalService.addListType(
+			_companyId, _LIST_TYPE_INTRANET, _LIST_TYPE_COMPANY);
+	}
+
 	@Test
 	public void testUpgrade() throws Exception {
-		_listTypeLocalService.addListType(
-			PortalInstances.getDefaultCompanyId(), "intranet",
-			"com.liferay.account.model.AccountEntry.address");
-
 		UpgradeProcess upgradeProcess = new UpgradeListTypeType();
 
 		upgradeProcess.upgrade();
+
+		FinderCacheUtil.clearCache();
+
+		ListType accountEntryListType = _listTypeLocalService.getListType(
+			_companyId, _LIST_TYPE_INTRANET, _LIST_TYPE_ACCOUNT_ENTRY);
+
+		ListType companyListType = _listTypeLocalService.getListType(
+			_companyId, _LIST_TYPE_INTRANET, _LIST_TYPE_COMPANY);
+
+		Assert.assertNull(accountEntryListType);
+
+		Assert.assertNotNull(companyListType);
 	}
+
+	private static final String _LIST_TYPE_ACCOUNT_ENTRY =
+		"com.liferay.account.model.AccountEntry.address";
+
+	private static final String _LIST_TYPE_COMPANY =
+		"com.liferay.portal.kernel.model.Company.website";
+
+	private static final String _LIST_TYPE_INTRANET = "intranet";
+
+	private static Long _companyId;
 
 	@Inject
 	private ListTypeLocalService _listTypeLocalService;

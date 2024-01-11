95a815c789cc1476cf5ac398bc01110ff2915791
Jaime León <jaime.leon@liferay.com>
2024-01-11 10:27:17 +0100

    LPS-203508 Add batch-planner config files

--- modules/test/playwright/playwright.config.ts
+++ modules/test/playwright/playwright.config.ts
@@ -6,6 +6,7 @@
 import {defineConfig} from '@playwright/test';
 
 import {config as exportImportWeb} from './tests/export-import-web/config';
+import {config as batchPlanner} from './tests/batch-planner/config';
 import {config as setup} from './tests/global.setup.config';
 import {config as object} from './tests/object-web/config';
 import {config as portalWeb} from './tests/portal-web/config';
@@ -13,7 +14,7 @@ import {config as usersAdminWeb} from './tests/users-admin-web/config';
 
 export default defineConfig({
 	forbidOnly: !!process.env.CI,
-	projects: [exportImportWeb, object, portalWeb, setup, usersAdminWeb],
+	projects: [batchPlanner, exportImportWeb, object, portalWeb, setup, usersAdminWeb],
 	reporter: [
 		[
 			'html',

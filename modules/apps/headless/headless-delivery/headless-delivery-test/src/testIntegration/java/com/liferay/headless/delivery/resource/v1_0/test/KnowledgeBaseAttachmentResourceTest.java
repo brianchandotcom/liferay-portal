59db89e63a6eedc0d11b78123b0d7181bc665495
Alejandro Tardín <alejandro.tardin@liferay.com>
2024-01-12 11:34:17 +0100

    LPS-203472 Testing in vulcan

--- modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/KnowledgeBaseAttachmentResourceTest.java
+++ modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/KnowledgeBaseAttachmentResourceTest.java
@@ -8,7 +8,6 @@ package com.liferay.headless.delivery.resource.v1_0.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.headless.delivery.client.dto.v1_0.KnowledgeBaseAttachment;
 import com.liferay.headless.delivery.client.http.HttpInvoker;
-import com.liferay.headless.delivery.client.resource.v1_0.KnowledgeBaseAttachmentResource;
 import com.liferay.knowledge.base.model.KBArticle;
 import com.liferay.knowledge.base.model.KBFolder;
 import com.liferay.knowledge.base.service.KBArticleLocalServiceUtil;
@@ -18,7 +17,6 @@ import com.liferay.portal.kernel.test.constants.TestDataConstants;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
-import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
 
 import java.io.File;
@@ -146,43 +144,6 @@ public class KnowledgeBaseAttachmentResourceTest
 	}
 
 	@Override
-	@Test
-	public void testPostKnowledgeBaseArticleKnowledgeBaseAttachment()
-		throws Exception {
-
-		super.testPostKnowledgeBaseArticleKnowledgeBaseAttachment();
-
-		KnowledgeBaseAttachment randomKnowledgeBaseAttachment =
-			randomKnowledgeBaseAttachment();
-
-		Map<String, File> multipartFiles = getMultipartFiles();
-
-		KnowledgeBaseAttachmentResource.Builder builder =
-			KnowledgeBaseAttachmentResource.builder();
-
-		KnowledgeBaseAttachmentResource knowledgeBaseAttachmentResource =
-			builder.authentication(
-				"test@liferay.com", "test"
-			).chunkSize(
-				50
-			).locale(
-				LocaleUtil.getDefault()
-			).build();
-
-		KnowledgeBaseAttachment postKnowledgeBaseAttachment =
-			knowledgeBaseAttachmentResource.
-				postKnowledgeBaseArticleKnowledgeBaseAttachment(
-					testGetKnowledgeBaseArticleKnowledgeBaseAttachmentsPage_getKnowledgeBaseArticleId(),
-					randomKnowledgeBaseAttachment, multipartFiles);
-
-		assertEquals(
-			postKnowledgeBaseAttachment, randomKnowledgeBaseAttachment);
-		assertValid(postKnowledgeBaseAttachment);
-
-		assertValid(postKnowledgeBaseAttachment, multipartFiles);
-	}
-
-	@Override
 	protected void assertValid(
 			KnowledgeBaseAttachment knowledgeBaseAttachment,
 			Map<String, File> multipartFiles)
@@ -197,7 +158,7 @@ public class KnowledgeBaseAttachmentResourceTest
 
 	@Override
 	protected String[] getAdditionalAssertFieldNames() {
-		return new String[] {"externalReferenceCode", "title"};
+		return new String[] {"title"};
 	}
 
 	@Override

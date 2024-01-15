fcf1e76a85b0dbd0f7b1174c4c6b16fe2fee160c
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2024-01-15 14:27:14 +0400

    LPS-206108 - Update item-selector-upload-web to ESM

--- modules/apps/item-selector/item-selector-upload-web/npmscripts.config.js
+++ modules/apps/item-selector/item-selector-upload-web/npmscripts.config.js
@@ -3,4 +3,9 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-export {ItemSelectorRepositoryEntryBrowser} from 'item-selector-taglib';
+module.exports = {
+	build: {
+		main:
+			'src/main/resources/META-INF/resources/js/ItemSelectorRepositoryEntryBrowser.js',
+	},
+};

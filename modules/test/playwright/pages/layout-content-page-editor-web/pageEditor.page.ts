d5f3175519d8f130a4eb78c9eddf7a7ec6d8903d
Alejandro Tardín <alejandro.tardin@liferay.com>
2024-01-15 17:42:41 +0100

    LPD-15350 Using the new autogenerated clients

---
+++ modules/test/playwright/pages/layout-content-page-editor-web/pageEditor.page.ts
@@ -0,0 +1,49 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+// @ts-ignore
+
+import {Page} from '@playwright/test';
+
+import {SitePage} from '../../../../apps/headless/headless-delivery/headless-delivery-client-js/src/main/resources/META-INF/resources/node';
+import {Site} from '../../../../apps/headless/headless-site/headless-site-client-js/src/main/resources/META-INF/resources/node';
+
+type Viewport = 'Desktop' | 'Landscape Phone' | 'Portrait Phone' | 'Tablet';
+
+export class PageEditorPage {
+	readonly page: Page;
+
+	constructor(page: Page) {
+		this.page = page;
+	}
+
+	async goToConfigurationTab(tab: ConfigurationTab) {
+		await this.page.getByRole('tab', {name: tab}).click();
+	}
+
+	async goToEditMode(site: Site, layout: SitePage) {
+		await this.page.goto(
+			`/web${site.friendlyUrlPath}${layout.friendlyUrlPath}?p_l_mode=edit`
+		);
+	}
+
+	async selectFragment(fragmentId: string, isDesktop = true) {
+		if (isDesktop) {
+			await this.page
+				.locator(`.lfr-layout-structure-item-${fragmentId}`)
+				.click();
+		}
+		else {
+			await this.page
+				.frameLocator('.page-editor__global-context-iframe')
+				.locator(`.lfr-layout-structure-item-${fragmentId}`)
+				.click();
+		}
+	}
+
+	async switchViewport(viewport: Viewport) {
+		await this.page.getByLabel(viewport).click();
+	}
+}

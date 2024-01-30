b000003aa01c3fe5091685aad711cff50d5f7893
Alejandro Tardín <alejandro.tardin@liferay.com>
2024-01-30 16:17:42 +0100

    LPD-15597 Inlining

--- modules/test/playwright/tests/headless-builder-web/post.spec.ts
+++ modules/test/playwright/tests/headless-builder-web/post.spec.ts
@@ -35,130 +35,6 @@ const basicApiApplication = {
 	title: 'Basic application',
 };
 
-const subjectObjectDefinition = {
-	active: true,
-	externalReferenceCode: 'subject-definition',
-	label: {
-		en_US: 'Subject',
-	},
-	name: 'Subject',
-	objectFields: [
-		{
-			DBType: 'String',
-			businessType: 'Text',
-			externalReferenceCode: 'subject-name-field',
-			indexed: true,
-			indexedAsKeyword: false,
-			indexedLanguageId: 'en_US',
-			label: {
-				en_US: 'Subject name',
-			},
-			listTypeDefinitionId: 0,
-			name: 'subjectName',
-			required: false,
-			state: false,
-			system: false,
-			type: 'String',
-		},
-	],
-	panelCategoryKey: 'control_panel.object',
-	pluralLabel: {
-		en_US: 'Subjects',
-	},
-	portlet: true,
-	restContextPath: '/o/c/subjects',
-	scope: 'company',
-	status: {
-		code: 0,
-	},
-};
-
-const studentObjectDefinition = {
-	active: true,
-	externalReferenceCode: 'student-definition',
-	label: {
-		en_US: 'Student',
-	},
-	name: 'Student',
-	objectFields: [
-		{
-			DBType: 'String',
-			businessType: 'Text',
-			externalReferenceCode: 'student-name-field',
-			indexed: true,
-			indexedAsKeyword: false,
-			indexedLanguageId: 'en_US',
-			label: {
-				en_US: 'Student name',
-			},
-			listTypeDefinitionId: 0,
-			name: 'studentName',
-			required: true,
-			state: false,
-			system: false,
-			type: 'String',
-		},
-	],
-	objectRelationships: [
-		{
-			deletionType: 'cascade',
-			externalReferenceCode: 'student-subjects-relationship',
-			label: {
-				en_US: 'Student subjects',
-			},
-			name: 'studentSubjects',
-			objectDefinitionExternalReferenceCode1: 'student-definition',
-			objectDefinitionExternalReferenceCode2: 'subject-definition',
-			objectDefinitionModifiable2: true,
-			objectDefinitionName2: 'Subject',
-			objectDefinitionSystem2: false,
-			objectField: {
-				DBType: 'Long',
-				businessType: 'Relationship',
-				externalReferenceCode: 'student-subjects-relationship-field',
-				indexed: true,
-				indexedAsKeyword: false,
-				indexedLanguageId: '',
-				label: {
-					en_US: 'Student subjects',
-				},
-				name: 'r_studentSubjects_c_studentId',
-				objectFieldSettings: [
-					{
-						name: 'objectDefinition1ShortName',
-						value: 'Student',
-					},
-					{
-						name: 'objectRelationshipERCObjectFieldName',
-						value: 'r_studentSubjects_c_studentERC',
-					},
-				],
-				relationshipType: 'oneToMany',
-				required: false,
-				state: false,
-				system: false,
-				type: 'Long',
-				unique: false,
-			},
-			parameterObjectFieldId: 0,
-			parameterObjectFieldName: '',
-			reverse: false,
-			system: false,
-			type: 'oneToMany',
-		},
-	],
-	panelCategoryKey: 'control_panel.object',
-	pluralLabel: {
-		en_US: 'Students',
-	},
-	portlet: true,
-	restContextPath: '/o/c/students',
-	scope: 'company',
-	status: {
-		code: 0,
-	},
-};
-
 const studentSubjectsApplication = {
 	apiApplicationToAPISchemas: [
 		{
@@ -203,12 +79,129 @@ test('can create post endpoint with different request and response schema', asyn
 	headlessBuilderPage,
 	page,
 }) => {
-	const subjectResponse = await apiHelpers.objectAdmin.postObjectDefinition(
-		subjectObjectDefinition
-	);
-	const studentResponse = await apiHelpers.objectAdmin.postObjectDefinition(
-		studentObjectDefinition
-	);
+	const subjectResponse = await apiHelpers.objectAdmin.postObjectDefinition({
+		active: true,
+		externalReferenceCode: 'subject-definition',
+		label: {
+			en_US: 'Subject',
+		},
+		name: 'Subject',
+		objectFields: [
+			{
+				DBType: 'String',
+				businessType: 'Text',
+				externalReferenceCode: 'subject-name-field',
+				indexed: true,
+				indexedAsKeyword: false,
+				indexedLanguageId: 'en_US',
+				label: {
+					en_US: 'Subject name',
+				},
+				listTypeDefinitionId: 0,
+				name: 'subjectName',
+				required: false,
+				state: false,
+				system: false,
+				type: 'String',
+			},
+		],
+		panelCategoryKey: 'control_panel.object',
+		pluralLabel: {
+			en_US: 'Subjects',
+		},
+		portlet: true,
+		restContextPath: '/o/c/subjects',
+		scope: 'company',
+		status: {
+			code: 0,
+		},
+	});
+	const studentResponse = await apiHelpers.objectAdmin.postObjectDefinition({
+		active: true,
+		externalReferenceCode: 'student-definition',
+		label: {
+			en_US: 'Student',
+		},
+		name: 'Student',
+		objectFields: [
+			{
+				DBType: 'String',
+				businessType: 'Text',
+				externalReferenceCode: 'student-name-field',
+				indexed: true,
+				indexedAsKeyword: false,
+				indexedLanguageId: 'en_US',
+				label: {
+					en_US: 'Student name',
+				},
+				listTypeDefinitionId: 0,
+				name: 'studentName',
+				required: true,
+				state: false,
+				system: false,
+				type: 'String',
+			},
+		],
+		objectRelationships: [
+			{
+				deletionType: 'cascade',
+				externalReferenceCode: 'student-subjects-relationship',
+				label: {
+					en_US: 'Student subjects',
+				},
+				name: 'studentSubjects',
+				objectDefinitionExternalReferenceCode1: 'student-definition',
+				objectDefinitionExternalReferenceCode2: 'subject-definition',
+				objectDefinitionModifiable2: true,
+				objectDefinitionName2: 'Subject',
+				objectDefinitionSystem2: false,
+				objectField: {
+					DBType: 'Long',
+					businessType: 'Relationship',
+					externalReferenceCode:
+						'student-subjects-relationship-field',
+					indexed: true,
+					indexedAsKeyword: false,
+					indexedLanguageId: '',
+					label: {
+						en_US: 'Student subjects',
+					},
+					name: 'r_studentSubjects_c_studentId',
+					objectFieldSettings: [
+						{
+							name: 'objectDefinition1ShortName',
+							value: 'Student',
+						},
+						{
+							name: 'objectRelationshipERCObjectFieldName',
+							value: 'r_studentSubjects_c_studentERC',
+						},
+					],
+					relationshipType: 'oneToMany',
+					required: false,
+					state: false,
+					system: false,
+					type: 'Long',
+					unique: false,
+				},
+				parameterObjectFieldId: 0,
+				parameterObjectFieldName: '',
+				reverse: false,
+				system: false,
+				type: 'oneToMany',
+			},
+		],
+		panelCategoryKey: 'control_panel.object',
+		pluralLabel: {
+			en_US: 'Students',
+		},
+		portlet: true,
+		restContextPath: '/o/c/students',
+		scope: 'company',
+		status: {
+			code: 0,
+		},
+	});
 	await apiHelpers.object.postObjectEntry(
 		studentSubjectsApplication,
 		'headless-builder/applications'

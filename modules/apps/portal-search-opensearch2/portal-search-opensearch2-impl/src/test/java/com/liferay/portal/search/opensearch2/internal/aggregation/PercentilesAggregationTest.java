bfee881e0c4d67874347849a10ae2751150bd886
Petteri Karttunen <petteri.karttunen@liferay.com>
2024-01-31 12:07:56 +0200

    LPD-8714 portal-search-opensearch2-impl: Aggregation tests

---
+++ modules/apps/portal-search-opensearch2/portal-search-opensearch2-impl/src/test/java/com/liferay/portal/search/opensearch2/internal/aggregation/PercentilesAggregationTest.java
@@ -0,0 +1,47 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.search.opensearch2.internal.aggregation;
+
+import com.liferay.portal.search.opensearch2.internal.OpenSearchTestRule;
+import com.liferay.portal.search.opensearch2.internal.indexing.LiferayOpenSearchIndexingFixtureFactory;
+import com.liferay.portal.search.test.util.aggregation.metrics.BasePercentilesAggregationTestCase;
+import com.liferay.portal.search.test.util.indexing.IndexingFixture;
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
+
+import org.junit.ClassRule;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * @author Rafael Praxedes
+ */
+public class PercentilesAggregationTest
+	extends BasePercentilesAggregationTestCase {
+
+	@ClassRule
+	public static final LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	@ClassRule
+	public static final OpenSearchTestRule openSearchTestRule =
+		OpenSearchTestRule.INSTANCE;
+
+	@Ignore
+	@Test
+	public void testPercentileRanksAggregationHDR() throws Exception {
+	}
+
+	@Ignore
+	@Test
+	public void testPercentileRanksAggregationTDigest() throws Exception {
+	}
+
+	@Override
+	protected IndexingFixture createIndexingFixture() throws Exception {
+		return LiferayOpenSearchIndexingFixtureFactory.getInstance();
+	}
+
+}

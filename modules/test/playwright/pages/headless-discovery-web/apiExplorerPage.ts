db42f1664e67a1ba4847051d508944db233bd235
jalber786 <alberto.moreno@liferay.com>
2024-02-01 14:40:47 +0100

    LPD-15246 Added new API Explorer Page Object Model

---
+++ modules/test/playwright/pages/headless-discovery-web/apiExplorerPage.ts
@@ -0,0 +1,58 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {Locator, Page, expect} from '@playwright/test';
+
+import {liferayConfig} from '../../liferay.config';
+
+export class ApiExplorerPage {
+	readonly page: Page;
+
+	constructor(page: Page) {
+		this.page = page;
+	}
+
+	async endpointHasSpecificParameters(
+		endpointPath: string,
+		parameters: string[]
+	) {
+		await (await this.getEndpointLocator(endpointPath)).click();
+		for (const parameter of parameters) {
+			await expect(
+				this.page.getByRole('cell', {exact: true, name: parameter})
+			).toBeVisible();
+		}
+		await (await this.getEndpointLocator(endpointPath)).click();
+	}
+
+	async endpointHasNotSpecificParameters(
+		endpointPath: string,
+		parameters: string[]
+	) {
+		await (await this.getEndpointLocator(endpointPath)).click();
+		for (const parameter of parameters) {
+			await expect(
+				this.page.getByRole('cell', {exact: true, name: parameter})
+			).toBeHidden();
+		}
+		await (await this.getEndpointLocator(endpointPath)).click();
+	}
+
+	async goto() {
+		await this.page.goto('/o/api');
+	}
+
+	async goToSpecificApplication(applicationURL: string) {
+		await this.page.goto(
+			`/o/api?endpoint=${liferayConfig.environment.baseUrl}/o/${applicationURL}/openapi.json`
+		);
+	}
+
+	async getEndpointLocator(endpointPath: string): Promise<Locator> {
+		return await this.page.locator(
+			`//span[@data-path="${endpointPath}"]/a/span`
+		);
+	}
+}

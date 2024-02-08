128c12cc5a7d56d3f97ab041d888ac6a60754318
Petteri Karttunen <petteri.karttunen@liferay.com>
2024-02-08 13:54:59 +0200

    LPD-8714 portal-search-opensearch2-impl: Keyed ranges are by not guaranteed to be returned in the given order because of hashing. Use safe keys

--- modules/apps/portal-search-opensearch2/portal-search-opensearch2-impl/src/test/java/com/liferay/portal/search/opensearch2/internal/aggregation/DateRangeAggregationTest.java
+++ modules/apps/portal-search-opensearch2/portal-search-opensearch2-impl/src/test/java/com/liferay/portal/search/opensearch2/internal/aggregation/DateRangeAggregationTest.java
@@ -5,14 +5,15 @@
 
 package com.liferay.portal.search.opensearch2.internal.aggregation;
 
+import com.liferay.portal.search.aggregation.bucket.DateRangeAggregation;
 import com.liferay.portal.search.opensearch2.internal.OpenSearchTestRule;
 import com.liferay.portal.search.opensearch2.internal.indexing.LiferayOpenSearchIndexingFixtureFactory;
+import com.liferay.portal.search.test.util.aggregation.AggregationAssert;
 import com.liferay.portal.search.test.util.aggregation.bucket.BaseDateRangeAggregationTestCase;
 import com.liferay.portal.search.test.util.indexing.IndexingFixture;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 
 import org.junit.ClassRule;
-import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -28,10 +29,38 @@ public class DateRangeAggregationTest extends BaseDateRangeAggregationTestCase {
 	public static final OpenSearchTestRule openSearchTestRule =
 		OpenSearchTestRule.INSTANCE;
 
-	@Ignore
 	@Override
 	@Test
 	public void testDateRangesWithKeys() throws Exception {
+		addDocument(getDate("2016-02-01T00:00:00"));
+		addDocument(getDate("2016-02-02T00:00:00"));
+		addDocument(getDate("2017-02-02T00:00:00"));
+		addDocument(getDate("2017-02-03T00:00:00"));
+		addDocument(getDate("2018-02-03T00:00:00"));
+		addDocument(getDate("2018-02-03T00:00:00"));
+		addDocument(getDate("2019-02-05T00:00:00"));
+
+		DateRangeAggregation dateRangeAggregation = getAggregation("yyyyMMdd");
+
+		dateRangeAggregation.setKeyed(true);
+
+		dateRangeAggregation.addRange("1", "20160101", "20161231");
+		dateRangeAggregation.addRange("2", "20170101", "20171231");
+		dateRangeAggregation.addRange("3", "20180101", null);
+
+		assertSearch(
+			indexingTestHelper -> {
+				indexingTestHelper.defineRequest(
+					searchRequestBuilder -> searchRequestBuilder.addAggregation(
+						dateRangeAggregation));
+
+				indexingTestHelper.search();
+
+				AggregationAssert.assertBuckets(
+					"[1=2, 2=2, 3=3]",
+					indexingTestHelper.getAggregationResult(
+						dateRangeAggregation));
+			});
 	}
 
 	@Override

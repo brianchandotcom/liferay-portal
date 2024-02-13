c19809f04dcdfb3bf6886fc2bde5a90a9a9fe1e2
Krešimir Čoko <kresimir.coko@liferay.com>
2024-02-13 06:59:09 +0100

    LPD-17091 - Add a test to assert amount of table columns in FDSEntries

---
+++ modules/test/playwright/tests/frontend-data-set-views-web/entries.ts
@@ -0,0 +1,24 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {expect, mergeTests} from '@playwright/test';
+
+import {loginTest} from '../../fixtures/loginTest';
+import {dataSetsPageTest} from './fixtures/dataSetsPageTest';
+
+export const test = mergeTests(dataSetsPageTest, loginTest);
+
+test('Assert table columns', async ({dataSetsPage, page}) => {
+	await dataSetsPage.goto();
+	await dataSetsPage.createSampleDataSetUI();
+
+	const tableColumns = await page
+		.locator('.dnd-thead > .dnd-tr')
+		.first()
+		.locator('.dnd-th')
+		.count();
+
+	await expect(tableColumns).toBe(6);
+});

69e8bdddff28a881735b0ae5749456b8e13e4cb8
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2024-02-14 16:04:09 +0400

    LPD-17434 - Update portal-settings-web to ESM

--- modules/apps/marketplace/marketplace-store-web/src/main/resources/META-INF/resources/js/index.js
+++ modules/apps/marketplace/marketplace-store-web/src/main/resources/META-INF/resources/js/index.js
@@ -3,6 +3,4 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {CountryRegionDynamicSelect} from '@liferay/address-web';
-
-export default CountryRegionDynamicSelect;
+export {};

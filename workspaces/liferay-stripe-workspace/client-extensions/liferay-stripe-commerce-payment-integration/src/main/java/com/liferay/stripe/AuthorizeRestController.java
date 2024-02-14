3b9ae0534d7b36b7907eaf439defffe4157eaa62
Brian Chan <brian.chan@liferay.com>
2024-02-14 15:15:51 +0100

    LPD-13569 SF

--- workspaces/liferay-stripe-workspace/client-extensions/liferay-stripe-commerce-payment-integration/src/main/java/com/liferay/stripe/AuthorizeRestController.java
+++ workspaces/liferay-stripe-workspace/client-extensions/liferay-stripe-commerce-payment-integration/src/main/java/com/liferay/stripe/AuthorizeRestController.java
@@ -65,27 +65,12 @@ public class AuthorizeRestController extends BaseRestController {
 			JSONObject commercePaymentEntryJSONObject =
 				jsonObject.getJSONObject("commercePaymentEntry");
 
-			String className = commercePaymentEntryJSONObject.getString(
-				"className");
-
-			SessionCreateParams sessionCreateParams = null;
-
-			if (Objects.equals(
-					className, "com.liferay.commerce.model.CommerceOrder")) {
-
-				sessionCreateParams = _getSessionCreateParams(
-					commercePaymentEntryJSONObject, jwt.getTokenValue());
-			}
-			else {
-				sessionCreateParams = _getSessionCreateParams(
-					commercePaymentEntryJSONObject);
-			}
-
-			Session session = Session.create(sessionCreateParams);
+			Session session = _createSession(
+				commercePaymentEntryJSONObject, jwt);
 
 			if (Objects.equals(session.getStatus(), "open")) {
-				redirectURL = session.getUrl();
 				paymentStatus = "2";
+				redirectURL = session.getUrl();
 				transactionCode = session.getId();
 			}
 		}
@@ -109,6 +94,79 @@ public class AuthorizeRestController extends BaseRestController {
 			HttpStatus.OK);
 	}
 
+	private Session _createSession(
+			JSONObject commercePaymentEntryJSONObject, Jwt jwt)
+		throws Exception {
+
+		SessionCreateParams sessionCreateParams = null;
+
+		if (Objects.equals(
+				commercePaymentEntryJSONObject.getString("className"),
+				"com.liferay.commerce.model.CommerceOrder")) {
+
+			JSONObject orderJSONObject = new JSONObject(
+				Objects.requireNonNull(
+					WebClient.create(
+					).get(
+					).uri(
+						StringBundler.concat(
+							lxcDXPServerProtocol, "://", lxcDXPMainDomain,
+							"/o/headless-commerce-admin-order/v1.0/orders/",
+							commercePaymentEntryJSONObject.getLong("classPK"),
+							"?nestedFields=orderItems")
+					).accept(
+						MediaType.APPLICATION_JSON
+					).header(
+						HttpHeaders.AUTHORIZATION,
+						"Bearer " + jwt.getTokenValue()
+					).retrieve(
+					).bodyToMono(
+						String.class
+					).block()));
+
+			sessionCreateParams = SessionCreateParams.builder(
+			).addPaymentMethodType(
+				SessionCreateParams.PaymentMethodType.CARD
+			).setCurrency(
+				orderJSONObject.getString("currencyCode")
+			).setMode(
+				SessionCreateParams.Mode.PAYMENT
+			).setSuccessUrl(
+				commercePaymentEntryJSONObject.getString("callbackURL")
+			).setCancelUrl(
+				commercePaymentEntryJSONObject.getString("cancelURL")
+			).addAllLineItem(
+				_getLineItems(
+					orderJSONObject.getString("currencyCode"),
+					commercePaymentEntryJSONObject.getString("languageId"),
+					orderJSONObject.getJSONArray("orderItems"))
+			).addShippingOption(
+				_getShippingOption(
+					orderJSONObject.getString("currencyCode"),
+					orderJSONObject.getLong("shippingAmountValue"),
+					orderJSONObject.getString("shippingOption"))
+			).build();
+		}
+		else {
+			sessionCreateParams = SessionCreateParams.builder(
+			).addPaymentMethodType(
+				SessionCreateParams.PaymentMethodType.CARD
+			).setCurrency(
+				commercePaymentEntryJSONObject.getString("currencyCode")
+			).setMode(
+				SessionCreateParams.Mode.PAYMENT
+			).setSuccessUrl(
+				commercePaymentEntryJSONObject.getString("callbackURL")
+			).setCancelUrl(
+				commercePaymentEntryJSONObject.getString("cancelURL")
+			).addAllLineItem(
+				_getLineItems(commercePaymentEntryJSONObject)
+			).build();
+		}
+
+		return Session.create(sessionCreateParams);
+	}
+
 	private List<SessionCreateParams.LineItem> _getLineItems(
 		JSONObject commercePaymentEntryJSONObject) {
 
@@ -116,18 +174,10 @@ public class AuthorizeRestController extends BaseRestController {
 
 		SessionCreateParams.LineItem lineItem =
 			SessionCreateParams.LineItem.builder(
-			).setQuantity(
-				BigDecimal.ONE.longValue()
 			).setPriceData(
 				SessionCreateParams.LineItem.PriceData.builder(
 				).setCurrency(
 					commercePaymentEntryJSONObject.getString("currencyCode")
-				).setUnitAmount(
-					BigDecimal.valueOf(
-						commercePaymentEntryJSONObject.getDouble("amount")
-					).multiply(
-						BigDecimal.valueOf(100)
-					).longValue()
 				).setProductData(
 					SessionCreateParams.LineItem.PriceData.ProductData.builder(
 					).setName(
@@ -141,7 +191,15 @@ public class AuthorizeRestController extends BaseRestController {
 							commercePaymentEntryJSONObject.getString("classPK")
 						).toString()
 					).build()
+				).setUnitAmount(
+					BigDecimal.valueOf(
+						commercePaymentEntryJSONObject.getDouble("amount")
+					).multiply(
+						BigDecimal.valueOf(100)
+					).longValue()
 				).build()
+			).setQuantity(
+				BigDecimal.ONE.longValue()
 			).build();
 
 		lineItems.add(lineItem);
@@ -162,21 +220,10 @@ public class AuthorizeRestController extends BaseRestController {
 
 			SessionCreateParams.LineItem lineItem =
 				SessionCreateParams.LineItem.builder(
-				).setQuantity(
-					quantity
 				).setPriceData(
 					SessionCreateParams.LineItem.PriceData.builder(
 					).setCurrency(
 						currencyCode
-					).setUnitAmount(
-						BigDecimal.valueOf(
-							orderItemJSONObject.getDouble(
-								"finalPriceWithTaxAmount")
-						).divide(
-							BigDecimal.valueOf(quantity)
-						).multiply(
-							BigDecimal.valueOf(100)
-						).longValue()
 					).setProductData(
 						SessionCreateParams.LineItem.PriceData.ProductData.
 							builder(
@@ -187,85 +234,24 @@ public class AuthorizeRestController extends BaseRestController {
 									languageId
 								)
 							).build()
+					).setUnitAmount(
+						BigDecimal.valueOf(
+							orderItemJSONObject.getDouble(
+								"finalPriceWithTaxAmount")
+						).divide(
+							BigDecimal.valueOf(quantity)
+						).multiply(
+							BigDecimal.valueOf(100)
+						).longValue()
 					).build()
+				).setQuantity(
+					quantity
 				).build();
 
 			lineItems.add(lineItem);
 		}
 
 		return lineItems;
-	}
-
-	private JSONObject _getOrderJSONObject(
-		JSONObject commercePaymentEntryJSONObject, String tokenValue) {
-
-		return new JSONObject(
-			Objects.requireNonNull(
-				WebClient.create(
-				).get(
-				).uri(
-					StringBundler.concat(
-						lxcDXPServerProtocol, "://", lxcDXPMainDomain,
-						"/o/headless-commerce-admin-order/v1.0/orders/",
-						commercePaymentEntryJSONObject.getLong("classPK"),
-						"?nestedFields=orderItems")
-				).accept(
-					MediaType.APPLICATION_JSON
-				).header(
-					HttpHeaders.AUTHORIZATION, "Bearer " + tokenValue
-				).retrieve(
-				).bodyToMono(
-					String.class
-				).block()));
-	}
-
-	private SessionCreateParams _getSessionCreateParams(
-		JSONObject commercePaymentEntryJSONObject) {
-
-		return SessionCreateParams.builder(
-		).addPaymentMethodType(
-			SessionCreateParams.PaymentMethodType.CARD
-		).setCurrency(
-			commercePaymentEntryJSONObject.getString("currencyCode")
-		).setMode(
-			SessionCreateParams.Mode.PAYMENT
-		).setSuccessUrl(
-			commercePaymentEntryJSONObject.getString("callbackURL")
-		).setCancelUrl(
-			commercePaymentEntryJSONObject.getString("cancelURL")
-		).addAllLineItem(
-			_getLineItems(commercePaymentEntryJSONObject)
-		).build();
-	}
-
-	private SessionCreateParams _getSessionCreateParams(
-		JSONObject commercePaymentEntryJSONObject, String tokenValue) {
-
-		JSONObject orderJSONObject = _getOrderJSONObject(
-			commercePaymentEntryJSONObject, tokenValue);
-
-		return SessionCreateParams.builder(
-		).addPaymentMethodType(
-			SessionCreateParams.PaymentMethodType.CARD
-		).setCurrency(
-			orderJSONObject.getString("currencyCode")
-		).setMode(
-			SessionCreateParams.Mode.PAYMENT
-		).setSuccessUrl(
-			commercePaymentEntryJSONObject.getString("callbackURL")
-		).setCancelUrl(
-			commercePaymentEntryJSONObject.getString("cancelURL")
-		).addAllLineItem(
-			_getLineItems(
-				orderJSONObject.getString("currencyCode"),
-				commercePaymentEntryJSONObject.getString("languageId"),
-				orderJSONObject.getJSONArray("orderItems"))
-		).addShippingOption(
-			_getShippingOption(
-				orderJSONObject.getString("currencyCode"),
-				orderJSONObject.getLong("shippingAmountValue"),
-				orderJSONObject.getString("shippingOption"))
-		).build();
 	}
 
 	private SessionCreateParams.ShippingOption _getShippingOption(
@@ -279,14 +265,14 @@ public class AuthorizeRestController extends BaseRestController {
 			).setFixedAmount(
 				SessionCreateParams.ShippingOption.ShippingRateData.FixedAmount.
 					builder(
-					).setCurrency(
-						currencyCode
 					).setAmount(
 						BigDecimal.valueOf(
 							shippingAmountValue
 						).multiply(
 							BigDecimal.valueOf(100)
 						).longValue()
+					).setCurrency(
+						currencyCode
 					).build()
 			).setType(
 				SessionCreateParams.ShippingOption.ShippingRateData.Type.

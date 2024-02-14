d02f39e1e04e777818d5e415dc8948c8bcd79e9e
Tina Tian <tina.tian@liferay.com>
2024-02-13 17:15:53 -0800

    LPS-206629 Move test logic to PortalInstancePoolTest

--- modules/apps/portal/portal-util-test/src/testIntegration/java/com/liferay/portal/util/test/PortalInstancesTest.java
+++ modules/apps/portal/portal-util-test/src/testIntegration/java/com/liferay/portal/util/test/PortalInstancesTest.java
@@ -9,7 +9,6 @@ import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.layout.test.util.LayoutTestUtil;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.instance.PortalInstancePool;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
@@ -26,7 +25,6 @@ import com.liferay.portal.kernel.test.util.CompanyTestUtil;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
-import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.StringUtil;
@@ -37,8 +35,6 @@ import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.util.PortalInstances;
 import com.liferay.portal.util.PropsValues;
-
-import java.util.List;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -164,25 +160,6 @@ public class PortalInstancesTest {
 			languageId, group.getPublicLayoutSet(), hostname);
 
 		_testGetVirtualHostLanguageId(languageId, hostname);
-	}
-
-	@Test
-	public void testGetWebIdsAfterInitCompany() {
-		PortalInstances.initCompany(_company);
-
-		List<String> webIds = ListUtil.fromArray(
-			PortalInstancePool.getWebIds());
-
-		Assert.assertTrue(webIds.contains(_company.getWebId()));
-
-		_company.setWebId(RandomTestUtil.randomString());
-
-		PortalInstances.initCompany(
-			_companyLocalService.updateCompany(_company));
-
-		webIds = ListUtil.fromArray(PortalInstancePool.getWebIds());
-
-		Assert.assertTrue(webIds.contains(_company.getWebId()));
 	}
 
 	private void _testGetCompanyId(

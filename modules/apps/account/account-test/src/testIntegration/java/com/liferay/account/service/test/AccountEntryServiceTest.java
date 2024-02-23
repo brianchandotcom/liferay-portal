d44317aa55d1c3a08d703209b84cdf0c2e2bfa2e
Stefano Motta <117469730+smottal@users.noreply.github.com>
2024-02-23 11:45:08 +0100

    LPD-18571 Fix flaky test and performance

--- modules/apps/account/account-test/src/testIntegration/java/com/liferay/account/service/test/AccountEntryServiceTest.java
+++ modules/apps/account/account-test/src/testIntegration/java/com/liferay/account/service/test/AccountEntryServiceTest.java
@@ -6,6 +6,7 @@
 package com.liferay.account.service.test;
 
 import com.liferay.account.model.AccountEntry;
+import com.liferay.account.service.AccountEntryLocalService;
 import com.liferay.account.service.AccountEntryService;
 import com.liferay.account.service.test.util.AccountEntryArgs;
 import com.liferay.account.service.test.util.AccountEntryTestUtil;
@@ -14,20 +15,25 @@ import com.liferay.portal.kernel.model.Address;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.ListTypeConstants;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
 import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
 import com.liferay.portal.kernel.service.AddressLocalService;
+import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.service.ListTypeLocalService;
 import com.liferay.portal.kernel.test.rule.DataGuard;
 import com.liferay.portal.kernel.test.util.CompanyTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -36,7 +42,7 @@ import org.junit.runner.RunWith;
 /**
  * @author Danny Situ
  */
-@DataGuard(scope = DataGuard.Scope.METHOD)
+@DataGuard(scope = DataGuard.Scope.NONE)
 @RunWith(Arquillian.class)
 public class AccountEntryServiceTest {
 
@@ -45,27 +51,44 @@ public class AccountEntryServiceTest {
 	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
 		new LiferayIntegrationTestRule();
 
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		_accountEntry1 = AccountEntryTestUtil.addAccountEntry();
+		_company = CompanyTestUtil.addCompany();
+	}
+
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+		String name = PrincipalThreadLocal.getName();
+
+		PrincipalThreadLocal.setName(TestPropsValues.getUserId());
+
+		try {
+			_accountEntryLocalService.deleteAccountEntry(_accountEntry1);
+			_companyLocalService.deleteCompany(_company);
+		}
+		finally {
+			PrincipalThreadLocal.setName(name);
+		}
+	}
+
 	@Test
 	public void testUpdateInvalidAddressId() throws Exception {
 		PermissionChecker originalPermissionChecker =
 			PermissionThreadLocal.getPermissionChecker();
 
-		AccountEntry accountEntry1 = AccountEntryTestUtil.addAccountEntry();
-
 		Address address = _addressLocalService.addAddress(
-			null, accountEntry1.getUserId(), AccountEntry.class.getName(),
-			accountEntry1.getAccountEntryId(), RandomTestUtil.randomString(),
+			null, _accountEntry1.getUserId(), AccountEntry.class.getName(),
+			_accountEntry1.getAccountEntryId(), RandomTestUtil.randomString(),
 			RandomTestUtil.randomString(), RandomTestUtil.randomString(), null,
 			null, RandomTestUtil.randomString(), null, 0, 0,
 			_listTypeLocalService.getListTypeId(
-				accountEntry1.getCompanyId(), "personal",
+				_accountEntry1.getCompanyId(), "personal",
 				ListTypeConstants.CONTACT_ADDRESS),
 			false, false, "1234567890",
 			ServiceContextTestUtil.getServiceContext());
 
-		Company company = CompanyTestUtil.addCompany();
-
-		User user = UserTestUtil.addCompanyAdminUser(company);
+		User user = UserTestUtil.addCompanyAdminUser(_company);
 
 		AccountEntry accountEntry2 = AccountEntryTestUtil.addAccountEntry(
 			AccountEntryArgs.withOwner(user));
@@ -94,13 +117,23 @@ public class AccountEntryServiceTest {
 		}
 	}
 
+	private static AccountEntry _accountEntry1;
+
+	@Inject
+	private static AccountEntryLocalService _accountEntryLocalService;
+
+	@Inject
+	private static AddressLocalService _addressLocalService;
+
+	private static Company _company;
+
+	@Inject
+	private static CompanyLocalService _companyLocalService;
+
 	@Inject
 	private static ListTypeLocalService _listTypeLocalService;
 
 	@Inject
 	private AccountEntryService _accountEntryService;
-
-	@Inject
-	private AddressLocalService _addressLocalService;
 
 }

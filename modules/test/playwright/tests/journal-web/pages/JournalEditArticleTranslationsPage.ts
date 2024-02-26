6c39dcf4b48fa316beb0ce346083f1a968a9a707
mikellorza <mikel.lorza@liferay.com>
2024-02-26 11:02:59 +0100

    LPD-18387 Add test to the error message in translations view for concurrent users.

--- modules/test/playwright/tests/journal-web/pages/JournalEditArticleTranslationsPage.ts
+++ modules/test/playwright/tests/journal-web/pages/JournalEditArticleTranslationsPage.ts
@@ -3,13 +3,14 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {Locator, Page} from '@playwright/test';
+import {Locator, Page, expect} from '@playwright/test';
 
 import {JournalPage} from './JournalPage';
 
 export class JournalEditArticleTranslationsPage {
 	readonly page: Page;
 
+	readonly concurrentUserErrorMessage: Locator;
 	readonly journalPage: JournalPage;
 	readonly publishButton: Locator;
 	readonly titleInput: Locator;
@@ -17,11 +18,22 @@ export class JournalEditArticleTranslationsPage {
 	constructor(page: Page) {
 		this.page = page;
 
+		this.concurrentUserErrorMessage = page.getByText(
+			'Another user has made changes since you started editing. Publish this version to save it and overwrite the recent changes.'
+		);
 		this.journalPage = new JournalPage(page);
 		this.publishButton = page.getByRole('button', {name: 'Publish'});
 		this.titleInput = page.locator(
 			'#_com_liferay_translation_web_internal_portlet_TranslationPortlet_infoField--JournalArticle_title--0'
 		);
+	}
+
+	async assertErrorInEditBasicArticleTranslations(url: string) {
+		await this.editBasicArticleTranslations('title', url);
+
+		await this.concurrentUserErrorMessage.waitFor();
+
+		await expect(this.concurrentUserErrorMessage).toBeVisible();
 	}
 
 	async editBasicArticleTranslations(title: string, url: string) {

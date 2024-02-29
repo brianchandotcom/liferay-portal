f402957f0a843ff640c6b877bb41b98de7ffb4a2
Brian Chan <brian.chan@liferay.com>
2024-02-29 12:05:22 -0300

    LPD-18197 SF

--- modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/resource/v1_0/test/ObjectEntryResourceTest.java
+++ modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/resource/v1_0/test/ObjectEntryResourceTest.java
@@ -7010,7 +7010,6 @@ public class ObjectEntryResourceTest {
 		JSONArray itemsJSONArray = jsonObject.getJSONArray("items");
 
 		Assert.assertNull(jsonObject.get("title"));
-
 		Assert.assertNull(jsonObject.get("status"));
 
 		Assert.assertEquals(1, itemsJSONArray.length());

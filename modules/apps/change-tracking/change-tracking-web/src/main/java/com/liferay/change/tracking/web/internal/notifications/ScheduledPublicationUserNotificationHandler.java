2f181a045a14ebc14e16452559a5e4d7e8af5258
victorhcunha <victor.cunha@liferay.com>
2024-03-07 13:27:03 -0300

    Revert "LPD-17344 Send notifications when conflicts are found during scheduled conflict checks"
    This reverts commit c2a149de779d9581ccf0a22e258e3f0c55d8f157.

--- modules/apps/change-tracking/change-tracking-web/src/main/java/com/liferay/change/tracking/web/internal/notifications/ScheduledPublicationUserNotificationHandler.java
+++ modules/apps/change-tracking/change-tracking-web/src/main/java/com/liferay/change/tracking/web/internal/notifications/ScheduledPublicationUserNotificationHandler.java
@@ -63,17 +63,6 @@ public class ScheduledPublicationUserNotificationHandler
 		boolean showConflicts = jsonObject.getBoolean("showConflicts");
 
 		if (showConflicts) {
-			boolean scheduled = jsonObject.getBoolean("scheduled");
-
-			String title = null;
-
-			if (scheduled) {
-				title = "x-scheduled-publication-has-conflicts-with-production";
-			}
-			else {
-				title = "x-scheduled-publication-failed";
-			}
-
 			return StringUtil.replace(
 				getBodyTemplate(), new String[] {"[$BODY$]", "[$TITLE$]"},
 				new String[] {
@@ -82,7 +71,8 @@ public class ScheduledPublicationUserNotificationHandler
 						"click-on-this-notification-to-see-the-list-of-" +
 							"conflicts-that-need-to-be-manually-resolved"),
 					_language.format(
-						serviceContext.getLocale(), title,
+						serviceContext.getLocale(),
+						"x-scheduled-publication-failed",
 						new Object[] {jsonObject.getString("ctCollectionName")},
 						false)
 				});

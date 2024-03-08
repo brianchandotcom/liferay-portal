9bf9fd41fc86c2d0214479b3edee79e7018b9cb7
Brian Chan <brian.chan@liferay.com>
2024-03-08 22:02:24 +0400

    LPD-13560 SF

--- modules/apps/commerce/commerce-product-item-selector-web/src/main/java/com/liferay/commerce/product/item/selector/web/internal/util/CPItemSelectorViewUtil.java
+++ modules/apps/commerce/commerce-product-item-selector-web/src/main/java/com/liferay/commerce/product/item/selector/web/internal/util/CPItemSelectorViewUtil.java
@@ -154,13 +154,13 @@ public class CPItemSelectorViewUtil {
 
 		Sort sort = null;
 
-		if (Objects.equals(orderByCol, "priority")) {
-			sort = SortFactoryUtil.create(
-				Field.PRIORITY + "_sortable", reverse);
-		}
-		else if (Objects.equals(orderByCol, "modified-date")) {
+		if (Objects.equals(orderByCol, "modified-date")) {
 			sort = SortFactoryUtil.create(
 				Field.MODIFIED_DATE + "_sortable", reverse);
+		}
+		else if (Objects.equals(orderByCol, "priority")) {
+			sort = SortFactoryUtil.create(
+				Field.PRIORITY + "_sortable", reverse);
 		}
 		else if (Objects.equals(orderByCol, "title")) {
 			sort = SortFactoryUtil.create(

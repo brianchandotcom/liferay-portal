1daa8b860c2af0b3964b33b468aa9e39ba9a1c44
Alejandro Tardín <alejandro.tardin@liferay.com>
2024-03-12 10:58:18 +0100

    LPD-20184 Following the existing pattern

--- modules/test/playwright/tests/headless-builder-web/fixtures/headlessBuilderPagesTest.ts
+++ modules/test/playwright/tests/headless-builder-web/fixtures/headlessBuilderPagesTest.ts
@@ -3,8 +3,8 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {ApplicationPage} from '../pages/applicationPage';
-import {HeadlessBuilderPage} from '../pages/headlessBuilderPage';
+import {ApplicationPage} from '../pages/ApplicationPage';
+import {HeadlessBuilderPage} from '../pages/HeadlessBuilderPage';
 import {headlessBuilderTest} from './headlessBuilderTest';
 
 const headlessBuilderPagesTest = headlessBuilderTest.extend<{

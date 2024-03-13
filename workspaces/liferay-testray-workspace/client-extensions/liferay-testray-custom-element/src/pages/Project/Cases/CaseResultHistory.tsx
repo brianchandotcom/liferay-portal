f6d34d7bca50e61f7eaa528910adb3bbb0482cae
Clarice Brasil <clarice.brasil@liferay.com>
2024-03-13 18:28:29 -0300

    ISSD-3018 Implement Sort For Case Result History Table

--- workspaces/liferay-testray-workspace/client-extensions/liferay-testray-custom-element/src/pages/Project/Cases/CaseResultHistory.tsx
+++ workspaces/liferay-testray-workspace/client-extensions/liferay-testray-custom-element/src/pages/Project/Cases/CaseResultHistory.tsx
@@ -32,6 +32,12 @@ const CaseResultHistory: React.FC<CaseResultHistoryProps> = ({
 
 	return (
 		<ListView
+			initialContext={{
+				sort: {
+					direction: 'DESC',
+					key: 'dateCreated',
+				},
+			}}
 			managementToolbarProps={{
 				applyFilters: true,
 				filterSchema: 'buildResultsHistory',

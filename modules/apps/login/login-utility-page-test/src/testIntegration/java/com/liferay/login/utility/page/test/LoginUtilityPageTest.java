c9447ab1ddeb2c96c82275726ba6fbc56e593337
Istvan Sajtos <istvan.sajtos@liferay.com>
2024-04-04 22:18:19 +0200

    LPD-6870 Add test

--- modules/apps/login/login-utility-page-test/src/testIntegration/java/com/liferay/login/utility/page/test/LoginUtilityPageTest.java
+++ modules/apps/login/login-utility-page-test/src/testIntegration/java/com/liferay/login/utility/page/test/LoginUtilityPageTest.java
@@ -6,14 +6,25 @@
 package com.liferay.login.utility.page.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.layout.utility.page.kernel.LayoutUtilityPageEntryViewRendererRegistryUtil;
+import com.liferay.layout.utility.page.kernel.constants.LayoutUtilityPageEntryConstants;
+import com.liferay.layout.utility.page.model.LayoutUtilityPageEntry;
 import com.liferay.layout.utility.page.service.LayoutUtilityPageEntryLocalService;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.test.rule.FeatureFlags;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -31,8 +42,37 @@ public class LoginUtilityPageTest {
 	public static final AggregateTestRule aggregateTestRule =
 		new LiferayIntegrationTestRule();
 
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+
+		_serviceContext = ServiceContextTestUtil.getServiceContext(
+			_group.getGroupId());
+
+		ServiceContextThreadLocal.pushServiceContext(_serviceContext);
+	}
+
+	@Test
+	public void testAddLoginUtilityPageEntry() throws PortalException {
+		LayoutUtilityPageEntry layoutUtilityPageEntry =
+			_layoutUtilityPageEntryLocalService.addLayoutUtilityPageEntry(
+				null, _serviceContext.getUserId(), _group.getGroupId(), 0, 0,
+				true, RandomTestUtil.randomString(),
+				LayoutUtilityPageEntryConstants.TYPE_LOGIN, 0, _serviceContext);
+
+		Assert.assertNotNull(layoutUtilityPageEntry);
+	}
+
 	@Test
 	public void testDefaultUtilityPagesAfterSiteInitialization() {
+	}
+
+	@Test
+	public void testLoginUtilityPageTypeHasBeenRegistered() {
+		Assert.assertNotNull(
+			LayoutUtilityPageEntryViewRendererRegistryUtil.
+				getLayoutUtilityPageEntryViewRenderer(
+					LayoutUtilityPageEntryConstants.TYPE_LOGIN));
 	}
 
 	@DeleteAfterTestRun
@@ -41,5 +81,7 @@ public class LoginUtilityPageTest {
 	@Inject
 	private LayoutUtilityPageEntryLocalService
 		_layoutUtilityPageEntryLocalService;
+
+	private ServiceContext _serviceContext;
 
 }

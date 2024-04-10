322da154930b4913e0c37b6343d6dc464bf287d3
Jeff Wu <jeffwu0724@gmail.com>
2024-04-10 09:03:09 -0700

    Revert "LPD-1558 Make it clear that the checking is needed for command execution."
    This reverts commit ff3053d7c4ab2f8a881fb54f9c93218e6f6a98c0.

--- modules/apps/gogo-shell/gogo-shell-web/src/main/java/com/liferay/gogo/shell/web/internal/portlet/GogoShellPortlet.java
+++ modules/apps/gogo-shell/gogo-shell-web/src/main/java/com/liferay/gogo/shell/web/internal/portlet/GogoShellPortlet.java
@@ -91,13 +91,6 @@ public class GogoShellPortlet extends MVCPortlet {
 			ActionRequest actionRequest, ActionResponse actionResponse)
 		throws Exception {
 
-		try {
-			CaptchaUtil.check(actionRequest);
-		}
-		catch (CaptchaException captchaException) {
-			throw new PortletException(captchaException);
-		}
-
 		String command = ParamUtil.getString(actionRequest, "command");
 
 		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
@@ -160,6 +153,13 @@ public class GogoShellPortlet extends MVCPortlet {
 	public void processAction(
 			ActionRequest actionRequest, ActionResponse actionResponse)
 		throws IOException, PortletException {
+
+		try {
+			CaptchaUtil.check(actionRequest);
+		}
+		catch (CaptchaException captchaException) {
+			throw new PortletException(captchaException);
+		}
 
 		_checkOmniadmin();
 

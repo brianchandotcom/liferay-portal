13822c528dafa8a1c560aa40af83ea13be4751fc
Daniel Szimko <daniel.szimko@liferay.com>
2024-04-18 14:50:08 +0200

    LPD-21445 Set skipMerge to true to avoid unwanted site template propagations when calling LayoutLocalService.getLayout() method

--- portal-impl/src/com/liferay/portal/security/auth/SessionAuthToken.java
+++ portal-impl/src/com/liferay/portal/security/auth/SessionAuthToken.java
@@ -5,6 +5,7 @@
 
 package com.liferay.portal.security.auth;
 
+import com.liferay.exportimport.kernel.staging.MergeLayoutPrototypesThreadLocal;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Layout;
@@ -81,7 +82,11 @@ public class SessionAuthToken implements AuthToken {
 
 		long plid = liferayPortletURL.getPlid();
 
+		boolean skipMerge = MergeLayoutPrototypesThreadLocal.isSkipMerge();
+
 		try {
+			MergeLayoutPrototypesThreadLocal.setSkipMerge(true);
+
 			Layout layout = LayoutLocalServiceUtil.getLayout(plid);
 
 			LayoutTypePortlet layoutTypePortlet =
@@ -95,6 +100,9 @@ public class SessionAuthToken implements AuthToken {
 			if (_log.isDebugEnabled()) {
 				_log.debug(exception);
 			}
+		}
+		finally {
+			MergeLayoutPrototypesThreadLocal.setSkipMerge(skipMerge);
 		}
 
 		liferayPortletURL.setParameter(

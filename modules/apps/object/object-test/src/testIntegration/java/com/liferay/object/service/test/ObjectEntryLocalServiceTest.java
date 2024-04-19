560fb3eda8682f1806637ae495faa4e589f9ab92
Brian Chan <brian.chan@liferay.com>
2024-04-19 14:36:28 +0100

    LPD-16413 Rename

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectEntryLocalServiceTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectEntryLocalServiceTest.java
@@ -1696,7 +1696,7 @@ public class ObjectEntryLocalServiceTest {
 	}
 
 	@Test
-	public void testAddOrUpdateObjectEntry() throws Exception {
+	public void testAddOrUpdateObjectEntryWithObjectActions() throws Exception {
 		_assertCount(0);
 
 		Map<String, Serializable> values =
@@ -3203,7 +3203,7 @@ public class ObjectEntryLocalServiceTest {
 	}
 
 	@Test
-	public void testUpdateObjectEntryWithActions() throws Exception {
+	public void testAddOrUpdateObjectEntry() throws Exception {
 		ObjectDefinition publishedObjectDefinition =
 			_publishCustomObjectDefinition(
 				false,

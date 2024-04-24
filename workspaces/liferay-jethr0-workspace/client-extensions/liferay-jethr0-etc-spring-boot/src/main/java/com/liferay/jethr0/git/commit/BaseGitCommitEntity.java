d62fc0e6765e5a5fb8d019122ca0e75c69a88c6f
Michael Hashimoto <michael.hashimoto@liferay.com>
2024-04-24 16:15:33 -0700

    LRCI-4139 Add basic GitCommit, GitPull, & GitUser entity classes

---
+++ workspaces/liferay-jethr0-workspace/client-extensions/liferay-jethr0-etc-spring-boot/src/main/java/com/liferay/jethr0/git/commit/BaseGitCommitEntity.java
@@ -0,0 +1,50 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.jethr0.git.commit;
+
+import com.liferay.jethr0.entity.BaseEntity;
+
+import org.json.JSONObject;
+
+/**
+ * @author Michael Hashimoto
+ */
+public abstract class BaseGitCommitEntity
+	extends BaseEntity implements GitCommitEntity {
+
+	@Override
+	public JSONObject getJSONObject() {
+		JSONObject jsonObject = super.getJSONObject();
+
+		jsonObject.put("sha", getSHA());
+
+		return jsonObject;
+	}
+
+	@Override
+	public String getSHA() {
+		return _sha;
+	}
+
+	@Override
+	public void setJSONObject(JSONObject jsonObject) {
+		super.setJSONObject(jsonObject);
+
+		_sha = jsonObject.getString("sha");
+	}
+
+	@Override
+	public void setSHA(String sha) {
+		_sha = sha;
+	}
+
+	protected BaseGitCommitEntity(JSONObject jsonObject) {
+		super(jsonObject);
+	}
+
+	private String _sha;
+
+}
---
+++ workspaces/liferay-jethr0-workspace/client-extensions/liferay-jethr0-etc-spring-boot/src/main/java/com/liferay/jethr0/git/user/BaseGitUserEntity.java
@@ -0,0 +1,53 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.jethr0.git.user;
+
+import com.liferay.jethr0.entity.BaseEntity;
+import com.liferay.jethr0.util.StringUtil;
+
+import java.net.URL;
+
+import org.json.JSONObject;
+
+/**
+ * @author Michael Hashimoto
+ */
+public abstract class BaseGitUserEntity
+	extends BaseEntity implements GitUserEntity {
+
+	@Override
+	public JSONObject getJSONObject() {
+		JSONObject jsonObject = super.getJSONObject();
+
+		jsonObject.put("url", getURL());
+
+		return jsonObject;
+	}
+
+	@Override
+	public URL getURL() {
+		return _url;
+	}
+
+	@Override
+	public void setJSONObject(JSONObject jsonObject) {
+		super.setJSONObject(jsonObject);
+
+		_url = StringUtil.toURL(jsonObject.getString("url"));
+	}
+
+	@Override
+	public void setURL(URL sha) {
+		_url = sha;
+	}
+
+	protected BaseGitUserEntity(JSONObject jsonObject) {
+		super(jsonObject);
+	}
+
+	private URL _url;
+
+}

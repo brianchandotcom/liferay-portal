f42a7936c4a8d5d87bab116cc0a1f229bd908458
MarinhoFeliphe <feliphemarinho18@gmail.com>
2024-04-27 15:09:28 -0300

    LPD-23693 Replace groovy with java delegate based action executor becacondition evaluator because it is safer - ApplicationStatusConditionEvaluator

---
+++ modules/apps/site-initializer/site-initializer-raylife-ap/src/main/java/site/initializer/workflow/definitions/underwriting/condition/evaluator/ApplicationStatusConditionEvaluator.java
@@ -0,0 +1,58 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package site.initializer.workflow.definitions.underwriting.condition.evaluator;
+
+import com.liferay.object.model.ObjectEntry;
+import com.liferay.object.service.ObjectEntryLocalService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.workflow.kaleo.model.KaleoCondition;
+import com.liferay.portal.workflow.kaleo.model.KaleoInstanceToken;
+import com.liferay.portal.workflow.kaleo.runtime.ExecutionContext;
+import com.liferay.portal.workflow.kaleo.runtime.condition.ConditionEvaluator;
+
+import java.io.Serializable;
+
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Feliphe Marinho
+ */
+@Component(
+	property = "scripting.language=java", service = ConditionEvaluator.class
+)
+public class ApplicationStatusConditionEvaluator implements ConditionEvaluator {
+
+	@Override
+	public String evaluate(
+			KaleoCondition kaleoCondition, ExecutionContext executionContext)
+		throws PortalException {
+
+		KaleoInstanceToken kaleoInstanceToken =
+			executionContext.getKaleoInstanceToken();
+
+		ObjectEntry objectEntry = _objectEntryLocalService.getObjectEntry(
+			kaleoInstanceToken.getClassPK());
+
+		Map<String, Serializable> values = objectEntry.getValues();
+
+		String status = values.get(
+			"applicationStatus"
+		).toString();
+
+		if (status.equals("underwriting")) {
+			return "yes";
+		}
+
+		return "no";
+	}
+
+	@Reference
+	private ObjectEntryLocalService _objectEntryLocalService;
+
+}

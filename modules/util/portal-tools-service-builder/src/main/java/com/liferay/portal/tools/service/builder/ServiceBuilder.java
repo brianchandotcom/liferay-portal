brianchandotcom/liferay-portal

commit c9efb6d9ead353f740b7e6d3df05279682346045

LPD-17572 Fix service builder package path resolving

modules/util/portal-tools-service-builder/src/main/java/com/liferay/portal/tools/service/builder/ServiceBuilder.java
@@ -8055,21 +8055,21 @@ public class ServiceBuilder {
 
 		int startIndex = 0;
 
-		if (fileName.startsWith(_apiDirName)) {
-			startIndex = _apiDirName.length();
-		}
-		else if (fileName.startsWith(_implDirName)) {
-			startIndex = _implDirName.length();
-		}
-		else if (fileName.startsWith(_uadDirName)) {
-			startIndex = _uadDirName.length();
-		}
-		else if (fileName.startsWith(_testDirName)) {
-			startIndex = _testDirName.length();
+		int index = fileName.indexOf("/java/");
+
+		if (index > 0) {
+			startIndex = index + 6;
 		}
 		else {
-			throw new ServiceBuilderException(
-				"Unable to parse package path from " + fileName);
+			index = fileName.indexOf("/src/");
+
+			if (index > 0) {
+				startIndex = index + 5;
+			}
+			else {
+				throw new ServiceBuilderException(
+					"Unable to parse package path from " + fileName);
+			}
 		}
 
 		ToolsUtil.writeFileRaw(
@@ -8078,7 +8078,7 @@ public class ServiceBuilder {
 				file,
 				StringUtil.replace(
 					fileName.substring(
-						startIndex + 1, fileName.lastIndexOf(StringPool.SLASH)),
+						startIndex, fileName.lastIndexOf(StringPool.SLASH)),
 					CharPool.SLASH, CharPool.PERIOD),
 				content, _MAX_LINE_LENGTH, false),
 			modifiedFileNames);

commit 5198eca067e0e47ddbcbd88223fd5f1460ba5e9a

LPD-24744 Fix ServiceBuilder path resolving for older branches that still have portal-impl integration tests. Caused by c9efb6d9

modules/util/portal-tools-service-builder/src/main/java/com/liferay/portal/tools/service/builder/ServiceBuilder.java
@@ -8065,8 +8065,18 @@ public class ServiceBuilder {
 				startIndex = index + 5;
 			}
 			else {
-				throw new ServiceBuilderException(
-					"Unable to parse package path from " + fileName);
+
+				// Older branches still have integration tests in portal-impl
+
+				index = fileName.indexOf("/test/integration/");
+
+				if (index > 0) {
+					startIndex = index + 18;
+				}
+				else {
+					throw new ServiceBuilderException(
+						"Unable to parse package path from " + fileName);
+				}
 			}
 		}
 
d9907be2ddab575cdf60e1804b2a03c6c7437cec
Carolina Barbosa <carolina.barbosa@liferay.com>
2024-05-06 10:33:14 -0300

    LPD-24564 Simplify

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/internal/upgrade/v9_2_0/test/ObjectDefinitionUpgradeProcessTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/internal/upgrade/v9_2_0/test/ObjectDefinitionUpgradeProcessTest.java
@@ -6,16 +6,13 @@
 package com.liferay.object.internal.upgrade.v9_2_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
-import com.liferay.object.constants.ObjectDefinitionConstants;
-import com.liferay.object.constants.ObjectFieldConstants;
-import com.liferay.object.field.util.ObjectFieldUtil;
+import com.liferay.object.field.builder.TextObjectFieldBuilder;
 import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.object.test.util.ObjectDefinitionTestUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
-import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
-import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
@@ -23,7 +20,7 @@ import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 import com.liferay.portal.upgrade.test.util.UpgradeTestUtil;
 import com.liferay.portal.vulcan.util.LocalizedMapUtil;
 
-import java.util.Arrays;
+import java.util.Collections;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -48,24 +45,15 @@ public class ObjectDefinitionUpgradeProcessTest {
 
 	@BeforeClass
 	public static void setUpClass() throws Exception {
-		String randomString = "A" + RandomTestUtil.randomString();
-
-		_objectDefinition =
-			_objectDefinitionLocalService.addCustomObjectDefinition(
-				TestPropsValues.getUserId(), 0, false, true, false, false,
-				LocalizedMapUtil.getLocalizedMap(randomString), randomString,
-				null, null, LocalizedMapUtil.getLocalizedMap(randomString),
-				true, ObjectDefinitionConstants.SCOPE_COMPANY,
-				ObjectDefinitionConstants.STORAGE_TYPE_DEFAULT,
-				Arrays.asList(
-					ObjectFieldUtil.createObjectField(
-						ObjectFieldConstants.BUSINESS_TYPE_TEXT,
-						ObjectFieldConstants.DB_TYPE_STRING,
-						RandomTestUtil.randomString(), StringUtil.randomId())));
-
-		_objectDefinitionLocalService.publishCustomObjectDefinition(
-			TestPropsValues.getUserId(),
-			_objectDefinition.getObjectDefinitionId());
+		_objectDefinition = ObjectDefinitionTestUtil.publishObjectDefinition(
+			Collections.singletonList(
+				new TextObjectFieldBuilder(
+				).labelMap(
+					LocalizedMapUtil.getLocalizedMap(
+						RandomTestUtil.randomString())
+				).name(
+					"a" + RandomTestUtil.randomString()
+				).build()));
 	}
 
 	@After
@@ -74,7 +62,7 @@ public class ObjectDefinitionUpgradeProcessTest {
 	}
 
 	@Test
-	public void testUpgradeEnableIndexedSearchDefaultValue() throws Exception {
+	public void testUpgradeProcess() throws Exception {
 		UpgradeProcess upgradeProcess = UpgradeTestUtil.getUpgradeStep(
 			_upgradeStepRegistrator,
 			"com.liferay.object.internal.upgrade.v9_2_0." +

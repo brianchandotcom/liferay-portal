b276c83bd1be255e444fbde9579d3593e20312ab
Keven Leone <keven.leone@liferay.com>
2024-05-26 13:26:01 -0300

    LPD-26707 Remove FF

--- modules/test/playwright/tests/marketplace-web/newSolutionFlow.spec.ts
+++ modules/test/playwright/tests/marketplace-web/newSolutionFlow.spec.ts
@@ -6,16 +6,12 @@
 import {expect, mergeTests} from '@playwright/test';
 
 import {dataApiHelpersTest} from '../../fixtures/dataApiHelpersTest';
-import {featureFlagsTest} from '../../fixtures/featureFlagsTest';
 import {loginTest} from '../../fixtures/loginTest';
 import {publisherDashboardPagesTest} from './fixtures/dashboardPagesTest';
 import {marketplaceSiteFixture} from './fixtures/marketplaceSite';
 
 export const test = mergeTests(
 	dataApiHelpersTest,
-	featureFlagsTest({
-		'LPD-20229': true,
-	}),
 	marketplaceSiteFixture,
 	publisherDashboardPagesTest,
 	loginTest()

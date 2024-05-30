brianchandotcom/liferay-portal

commit 52ee5fc87eb577600ddc7606456c31e36fb3a78c

LPS-140408 Unwrap the runnable when invoking rejectedExecutionHandler

modules/core/petra/petra-concurrent/src/main/java/com/liferay/petra/concurrent/NoticeableThreadPoolExecutor.java
@@ -110,7 +110,12 @@ public class NoticeableThreadPoolExecutor
 
 				return thread;
 			},
-			rejectedExecutionHandler) {
+			(runnable, threadPoolExecutor) -> {
+				DispatchRunnable dispatchRunnable = (DispatchRunnable)runnable;
+
+				rejectedExecutionHandler.rejectedExecution(
+					dispatchRunnable.getRunnable(), threadPoolExecutor);
+			}) {
 
 			@Override
 			protected void terminated() {
@@ -149,7 +154,7 @@ public class NoticeableThreadPoolExecutor
 		}
 
 		_dispatcherThreadPoolExecutor.execute(
-			() -> _workerThreadPoolExecutor.execute(runnable));
+			new DispatchRunnable(_workerThreadPoolExecutor, runnable));
 	}
 
 	public int getActiveCount() {
@@ -246,5 +251,28 @@ public class NoticeableThreadPoolExecutor
 	private final DefaultNoticeableFuture<Void>
 		_terminationDefaultNoticeableFuture;
 	private final ThreadPoolExecutor _workerThreadPoolExecutor;
+
+	private static class DispatchRunnable implements Runnable {
+
+		public Runnable getRunnable() {
+			return _runnable;
+		}
+
+		@Override
+		public void run() {
+			_threadPoolExecutor.execute(_runnable);
+		}
+
+		private DispatchRunnable(
+			ThreadPoolExecutor threadPoolExecutor, Runnable runnable) {
+
+			_threadPoolExecutor = threadPoolExecutor;
+			_runnable = runnable;
+		}
+
+		private final Runnable _runnable;
+		private final ThreadPoolExecutor _threadPoolExecutor;
+
+	}
 
 }

commit c2eba93ede218870379048fae5c125bb8435dd9d

LPD-27213 Wrap DispatchRunnable inside _dispatcherThreadPoolExecutor to make sure rejectedExecutionHandler always see the same runnable type. Regression caused by 52ee5fc8

modules/core/petra/petra-concurrent/src/main/java/com/liferay/petra/concurrent/NoticeableThreadPoolExecutor.java
@@ -109,6 +109,12 @@ public class NoticeableThreadPoolExecutor
 			}) {
 
 			@Override
+			public void execute(Runnable runnable) {
+				super.execute(
+					new DispatchRunnable(_workerThreadPoolExecutor, runnable));
+			}
+
+			@Override
 			protected void terminated() {
 				if (terminationCounter.decrementAndGet() == 0) {
 					_terminationDefaultNoticeableFuture.run();
@@ -144,8 +150,7 @@ public class NoticeableThreadPoolExecutor
 			throw new NullPointerException("Runnable is null");
 		}
 
-		_dispatcherThreadPoolExecutor.execute(
-			new DispatchRunnable(_workerThreadPoolExecutor, runnable));
+		_dispatcherThreadPoolExecutor.execute(runnable);
 	}
 
 	public int getActiveCount() {
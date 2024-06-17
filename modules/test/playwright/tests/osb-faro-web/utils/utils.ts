3cc1cb5b82d482a2a663f971d36dce0b7a47c46c
Vinicius Lopes <vinicius.lopes@liferay.com>
2024-06-17 09:58:32 -0300

    LPD-28681 Add the createIndividuals function so you can create individuals directly in the AC database

--- modules/test/playwright/tests/osb-faro-web/utils/utils.ts
+++ modules/test/playwright/tests/osb-faro-web/utils/utils.ts
@@ -14,3 +14,19 @@ export async function searchTerm(page: Page, searchTerm: string) {
 	await page.getByPlaceholder('Search').fill(searchTerm);
 	await page.getByPlaceholder('Search').press('Enter');
 }
+
+export async function createIndividuals(apiHelpers, names: string[]) {
+    const individuals = names.map(name => ({
+        emailAddress: `${name}@liferay.com`,
+        fields: [
+            { dataSourceId: 0, name: 'givenName', value: name },
+            { dataSourceId: 0, name: 'familyName', value: name },
+            { dataSourceId: 0, name: 'email', value: `${name}@liferay.com` },
+        ],
+        firstName: name,
+        id: `${name}@liferay.com`,
+        lastName: name,
+    }));
+
+    await apiHelpers.jsonWebServicesOSBAsah.createIndividuals(individuals);
+}

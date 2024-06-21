3295f952a847086c3b940748c3f9b5b5282a51eb
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2024-06-21 15:01:03 +0200

    LPD-29355 SF moves and rename to match the tested class

--- modules/apps/site-navigation/site-navigation-menu-item-layout-test/src/testIntegration/java/com/liferay/site/navigation/menu/item/layout/internal/type/test/LayoutSiteNavigationMenuItemTypeTest.java
+++ modules/apps/site-navigation/site-navigation-menu-item-layout-test/src/testIntegration/java/com/liferay/site/navigation/menu/item/layout/internal/type/test/LayoutSiteNavigationMenuItemTypeTest.java
@@ -1,9 +1,9 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.site.navigation.menu.item.layout.test;
+package com.liferay.site.navigation.menu.item.layout.internal.type.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.portal.kernel.exception.PortalException;
@@ -40,7 +40,7 @@ import org.junit.runner.RunWith;
  * @author Eudaldo Alonso
  */
 @RunWith(Arquillian.class)
-public class SiteNavigationMenuItemLayoutTest {
+public class LayoutSiteNavigationMenuItemTypeTest {
 
 	@ClassRule
 	@Rule

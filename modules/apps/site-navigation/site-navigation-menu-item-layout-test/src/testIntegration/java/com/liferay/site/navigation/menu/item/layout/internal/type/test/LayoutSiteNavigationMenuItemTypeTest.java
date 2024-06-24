9f2a6c1d81e945dd93d95e6e41af0bbb02eed145
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2024-06-24 08:22:37 +0200

    LPD-29355 Adds an integration test to assert getLayout verifying that the warn trace in case of null layout

--- modules/apps/site-navigation/site-navigation-menu-item-layout-test/src/testIntegration/java/com/liferay/site/navigation/menu/item/layout/internal/type/test/LayoutSiteNavigationMenuItemTypeTest.java
+++ modules/apps/site-navigation/site-navigation-menu-item-layout-test/src/testIntegration/java/com/liferay/site/navigation/menu/item/layout/internal/type/test/LayoutSiteNavigationMenuItemTypeTest.java
@@ -6,29 +6,41 @@
 package com.liferay.site.navigation.menu.item.layout.internal.type.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.layout.test.util.LayoutTestUtil;
+import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.LayoutConstants;
 import com.liferay.portal.kernel.service.LayoutService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.UnicodePropertiesBuilder;
+import com.liferay.portal.test.log.LogCapture;
+import com.liferay.portal.test.log.LogEntry;
+import com.liferay.portal.test.log.LoggerTestUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
 import com.liferay.site.navigation.constants.SiteNavigationConstants;
+import com.liferay.site.navigation.menu.item.layout.constants.SiteNavigationMenuItemTypeConstants;
 import com.liferay.site.navigation.model.SiteNavigationMenu;
+import com.liferay.site.navigation.model.SiteNavigationMenuItem;
 import com.liferay.site.navigation.service.SiteNavigationMenuItemLocalService;
 import com.liferay.site.navigation.service.SiteNavigationMenuLocalService;
+import com.liferay.site.navigation.type.SiteNavigationMenuItemType;
+import com.liferay.site.navigation.type.SiteNavigationMenuItemTypeRegistry;
 
 import java.util.HashMap;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -142,6 +154,73 @@ public class LayoutSiteNavigationMenuItemTypeTest {
 				_siteNavigationMenu.getSiteNavigationMenuId()));
 	}
 
+	@Test
+	public void testGetLayout() throws Exception {
+		_assertGetLayout(null);
+		_assertGetLayout(LayoutTestUtil.addTypePortletLayout(_group));
+	}
+
+	private void _assertGetLayout(Layout layout) throws Exception {
+		SiteNavigationMenuItemType siteNavigationMenuItemType =
+			_siteNavigationMenuItemTypeRegistry.getSiteNavigationMenuItemType(
+				SiteNavigationMenuItemTypeConstants.LAYOUT);
+
+		String layoutUuid = RandomTestUtil.randomString();
+
+		if (layout != null) {
+			layoutUuid = layout.getUuid();
+		}
+
+		SiteNavigationMenuItem siteNavigationMenuItem =
+			_siteNavigationMenuItemLocalService.addSiteNavigationMenuItem(
+				null, TestPropsValues.getUserId(), _group.getGroupId(),
+				_siteNavigationMenu.getSiteNavigationMenuId(), 0,
+				SiteNavigationMenuItemTypeConstants.LAYOUT,
+				UnicodePropertiesBuilder.create(
+					true
+				).put(
+					"groupId", String.valueOf(_group.getGroupId())
+				).put(
+					"layoutUuid", layoutUuid
+				).put(
+					"privateLayout", false
+				).put(
+					"title", RandomTestUtil.randomString()
+				).buildString(),
+				_serviceContext);
+
+		try (LogCapture logCapture = LoggerTestUtil.configureLog4JLogger(
+				"com.liferay.site.navigation.menu.item.layout.internal.type." +
+					"LayoutSiteNavigationMenuItemType",
+				LoggerTestUtil.WARN)) {
+
+			Assert.assertEquals(
+				layout,
+				siteNavigationMenuItemType.getLayout(siteNavigationMenuItem));
+
+			List<LogEntry> logEntries = logCapture.getLogEntries();
+
+			if (layout != null) {
+				Assert.assertEquals(
+					logEntries.toString(), 0, logEntries.size());
+
+				return;
+			}
+
+			Assert.assertEquals(logEntries.toString(), 1, logEntries.size());
+
+			LogEntry logEntry = logEntries.get(0);
+
+			Assert.assertEquals(
+				StringBundler.concat(
+					"No layout found for site navigation menu item ",
+					siteNavigationMenuItem.getSiteNavigationMenuItemId(),
+					" with layoutUuid ", layoutUuid,
+					" and privateLayout false"),
+				logEntry.getMessage());
+		}
+	}
+
 	@DeleteAfterTestRun
 	private Group _group;
 
@@ -154,6 +233,10 @@ public class LayoutSiteNavigationMenuItemTypeTest {
 	@Inject
 	private SiteNavigationMenuItemLocalService
 		_siteNavigationMenuItemLocalService;
+
+	@Inject
+	private SiteNavigationMenuItemTypeRegistry
+		_siteNavigationMenuItemTypeRegistry;
 
 	@Inject
 	private SiteNavigationMenuLocalService _siteNavigationMenuLocalService;

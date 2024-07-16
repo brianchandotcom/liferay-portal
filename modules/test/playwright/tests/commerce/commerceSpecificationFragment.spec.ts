4d4fb981e4ac1f3049b486c5f806705c5a646607
Alessio Rendina <asilcomando@gmail.com>
2024-07-16 09:52:40 +0200

    LPD-31561 Remove release FF

--- modules/test/playwright/tests/commerce/commerceSpecificationFragment.spec.ts
+++ modules/test/playwright/tests/commerce/commerceSpecificationFragment.spec.ts
@@ -9,7 +9,6 @@ import {apiHelpersTest} from '../../fixtures/apiHelpersTest';
 import {applicationsMenuPageTest} from '../../fixtures/applicationsMenuPageTest';
 import {commercePagesTest} from '../../fixtures/commercePagesTest';
 import {dataApiHelpersTest} from '../../fixtures/dataApiHelpersTest';
-import {featureFlagsTest} from '../../fixtures/featureFlagsTest';
 import {loginTest} from '../../fixtures/loginTest';
 import getRandomString from '../../utils/getRandomString';
 
@@ -18,9 +17,6 @@ export const test = mergeTests(
 	applicationsMenuPageTest,
 	commercePagesTest,
 	dataApiHelpersTest,
-	featureFlagsTest({
-		'LPD-10856': true,
-	}),
 	loginTest()
 );
 

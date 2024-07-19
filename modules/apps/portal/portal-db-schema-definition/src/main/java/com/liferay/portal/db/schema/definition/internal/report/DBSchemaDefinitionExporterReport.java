b4035c8207ffabe2b39d9a1e7045ab8e96ceb7a6
Alberto Chaparro <alberto.chaparro@liferay.com>
2024-07-19 08:09:19 +0200

    LPD-30604 This is a report

--- modules/apps/portal/portal-db-schema-definition/src/main/java/com/liferay/portal/db/schema/definition/internal/report/DBSchemaDefinitionExporterReport.java
+++ modules/apps/portal/portal-db-schema-definition/src/main/java/com/liferay/portal/db/schema/definition/internal/report/DBSchemaDefinitionExporterReport.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.db.schema.definition.internal.validation;
+package com.liferay.portal.db.schema.definition.internal.report;
 
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
@@ -35,9 +35,9 @@ import javax.sql.DataSource;
 /**
  * @author Mariano Álvaro Sáiz
  */
-public class DBSchemaDefinitionExporterValidation {
+public class DBSchemaDefinitionExporterReport {
 
-	public static void validateSchemaExport(String path, DBType targetDBType)
+	public static void generateReport(String path, DBType targetDBType)
 		throws Exception {
 
 		Set<String> dbTables = _getDBTables();

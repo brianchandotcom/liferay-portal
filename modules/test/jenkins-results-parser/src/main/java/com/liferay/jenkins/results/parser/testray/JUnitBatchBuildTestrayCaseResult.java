3a3e0ccb27530a5d9ac466f8c75310952ac2f832
CalumR23 <searagan@gmail.com>
2024-08-01 10:29:43 -0700

    LRCI-4444 Check if result has been skipped

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/testray/JUnitBatchBuildTestrayCaseResult.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/testray/JUnitBatchBuildTestrayCaseResult.java
@@ -176,6 +176,10 @@ public class JUnitBatchBuildTestrayCaseResult
 			return Status.FAILED;
 		}
 
+		if (_isTestClassResultsSkipped()) {
+			return Status.UNTESTED;
+		}
+
 		if (_isTestClassResultsFailing()) {
 			return Status.FAILED;
 		}
@@ -299,6 +303,16 @@ public class JUnitBatchBuildTestrayCaseResult
 		}
 
 		return testResults;
+	}
+
+	private boolean _isTestClassResultsSkipped() {
+		for (TestClassResult testClassResult : _getTestClassResults()) {
+			if (testClassResult.isSkipped()) {
+				return true;
+			}
+		}
+
+		return false;
 	}
 
 	private boolean _isTestClassResultsFailing() {

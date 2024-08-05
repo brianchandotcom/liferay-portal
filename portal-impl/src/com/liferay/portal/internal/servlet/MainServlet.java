1c9b24e226a56d005416d3a9afcc97534495804b
Dante Wang <dante.wang@liferay.com>
2024-08-05 10:14:44 +0800

    LPD-28759 Add a portal property and apply to the default admin creation of the default instance

--- portal-impl/src/com/liferay/portal/internal/servlet/MainServlet.java
+++ portal-impl/src/com/liferay/portal/internal/servlet/MainServlet.java
@@ -769,7 +769,8 @@ public class MainServlet extends HttpServlet {
 				GetterUtil.getString(
 					PropsValues.COMPANY_DEFAULT_VIRTUAL_HOST_MAIL_DOMAIN,
 					PropsValues.COMPANY_DEFAULT_WEB_ID),
-				0, true, null, null, null, null, null, null);
+				0, true, PropsValues.COMPANY_DEFAULT_CREATE_DEFAULT_ADMIN, null,
+				null, null, null, null, null);
 		}
 
 		if (Validator.isNull(PropsValues.COMPANY_DEFAULT_WEB_ID)) {

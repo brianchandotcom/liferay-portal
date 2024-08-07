8b9c86656b7138a448899815db0b24710391b883
Iván Zaera Avellón <ivan.zaera@liferay.com>
2024-08-07 11:30:53 +0200

    LPD-33244 Remove --all argument from check:preflight and stop checking tsc

--- modules/_node-scripts/preflight/index.mjs
+++ modules/_node-scripts/preflight/index.mjs
@@ -3,13 +3,36 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import getNamedArguments from '../util/getNamedArguments.mjs';
-import runPreflight from './runPreflight.mjs';
+import {checkConfigFileNames} from './checkConfigFileNames.mjs';
+import {checkNodeScriptsHash} from './checkNodeScriptsHash.mjs';
+import {checkPackageJSONFiles} from './checkPackageJSONFiles.mjs';
+import {checkYarnLock} from './checkYarnLock.mjs';
 
+/**
+ * Run "lightweight" global checks not implemented by ESLint or Prettier.
+ *
+ * Since preflight checks are global and there are not switches to choose subsets of things to
+ * check, they must not take a long time to run.
+ *
+ * Any long check (eg: TypeScript) must be moved to its own command and invoked explicitly from the
+ * outer layer.
+ */
 export default async function preflight() {
-	const {all} = getNamedArguments({
-		all: '--all',
-	});
+	const results = await Promise.all([
+		checkConfigFileNames(),
+		checkPackageJSONFiles(),
+		checkYarnLock(),
+		checkNodeScriptsHash(),
+	]);
 
-	await runPreflight({all});
+	const errors = results.flat();
+
+	if (errors.length) {
+		console.error(`
+❌ Preflight check failed:
+${errors.map((error) => `   · ${error}`).join('\n')}
+`);
+
+		throw new Error();
+	}
 }

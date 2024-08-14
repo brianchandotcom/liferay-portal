7ae3a3cdd8ecb06dbb00e6ebab1689419f90de7b
Jerry Adrianoo <jerry.adriano@liferay.com>
2024-08-14 18:18:54 -0300

    Revert "LPD-32128 Rename"
    This reverts commit 8110af14ce59662c8b9e568d47bbecfc8bf538a8.

--- modules/apps/static/portal-lpkg-deployer/portal-lpkg-deployer-impl/src/main/java/com/liferay/portal/lpkg/deployer/internal/WABWrapperBundleTrackerCustomizer.java
+++ modules/apps/static/portal-lpkg-deployer/portal-lpkg-deployer-impl/src/main/java/com/liferay/portal/lpkg/deployer/internal/WABWrapperBundleTrackerCustomizer.java
@@ -3,12 +3,11 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.lpkg.deployer.internal.osgi.util.tracker;
+package com.liferay.portal.lpkg.deployer.internal;
 
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.lpkg.deployer.internal.WABWrapperUtil;
 
 import java.net.MalformedURLException;
 import java.net.URL;

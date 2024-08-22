1666f4bc1de0bbe1544dbee3e8d23748d3f83b91
Alicia García <alicia.garcia@liferay.com>
2024-08-22 17:39:22 +0200

    LPD-33786 create test to check if the asset library/depot only see the tags created by them or the global

---
+++ modules/test/playwright/tests/depot-web/depot.spec.ts
@@ -0,0 +1,88 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {expect, mergeTests} from '@playwright/test';
+
+import {apiHelpersTest} from '../../fixtures/apiHelpersTest';
+import {isolatedSiteTest} from '../../fixtures/isolatedSiteTest';
+import {loginTest} from '../../fixtures/loginTest';
+import getGlobalSiteId from '../../utils/getGlobalSiteId';
+import getRandomString from '../../utils/getRandomString';
+import {depotsPagesTest} from './fixtures/depotsPagesTest';
+
+const test = mergeTests(
+	apiHelpersTest,
+	depotsPagesTest,
+	isolatedSiteTest,
+	loginTest()
+);
+
+test(
+	'Asset library can only see tags from its own scope',
+	{tag: '@LPD-1628'},
+	async ({apiHelpers, documentLibraryEditFilePage, page}) => {
+
+		// Create one tag on Global
+
+		const globalSiteId = await getGlobalSiteId(apiHelpers);
+
+		const globalTagName = getRandomString();
+
+		const globalTag =
+			await apiHelpers.headlessAdminTaxonomy.postSiteKeyword({
+				name: globalTagName,
+				siteId: globalSiteId,
+			});
+
+		const depotName = getRandomString();
+
+		const depot =
+			await apiHelpers.jsonWebServicesDepot.addDepotEntry(depotName);
+
+		const depotTagName = getRandomString();
+
+		const depotTag =
+			await apiHelpers.headlessAdminTaxonomy.postAssetLibraryKeyword({
+				depotEntryId: depot.depotEntryId,
+				name: depotTagName,
+			});
+
+		await documentLibraryEditFilePage.goto(
+			`/asset-library-${depot.depotEntryId}`
+		);
+
+		const fieldset = await page.locator(
+			'#_com_liferay_document_library_web_portlet_DLAdminPortlet_categorization'
+		);
+
+		if (await fieldset.locator('.panel-body').isHidden()) {
+			await fieldset
+				.getByRole('button', {name: 'Categorization'})
+				.click();
+		}
+
+		await page.getByRole('button', {name: 'Select Tags'}).click();
+
+		const modalTag = page.frameLocator('iframe[title="Tags"]');
+
+		await expect(
+			modalTag.locator(`tr:has-text('${depotTagName}')`)
+		).toBeVisible();
+
+		await expect(
+			modalTag.locator(`tr:has-text('${globalTagName}')`)
+		).toBeVisible();
+
+		// Remove the tag created on Global
+
+		await apiHelpers.headlessAdminTaxonomy.deleteKeyword({
+			id: globalTag.id,
+		});
+
+		await apiHelpers.jsonWebServicesDepot.deleteDepotEntry(
+			depot.depotEntryId
+		);
+	}
+);

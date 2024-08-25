f526464ac1eec12057dfd1846395689ce65421eb
Brian Chan <brian.chan@liferay.com>
2024-08-25 17:04:38 -0300

    LPD-34733 Move

--- portal-kernel/src/com/liferay/portal/kernel/portlet/PortletFriendlyURLMapperMatch.java
+++ portal-kernel/src/com/liferay/portal/kernel/portlet/PortletFriendlyURLMapperMatch.java
@@ -3,9 +3,9 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.kernel.model;
+package com.liferay.portal.kernel.portlet;
 
-import com.liferay.portal.kernel.portlet.FriendlyURLMapper;
+import com.liferay.portal.kernel.model.Portlet;
 
 /**
  * @author Shuyang Zhou

fd794c9764845a4c8db619d4ade754394d05d972
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2024-09-02 10:27:13 +0200

    LPD-33733 Extend from BasePortletDisplayTemplateConfigurationAction to process properly the displayScopeGroupExternalReferenceCode

--- modules/apps/rss/rss-web/src/main/java/com/liferay/rss/web/internal/portlet/action/RSSConfigurationAction.java
+++ modules/apps/rss/rss-web/src/main/java/com/liferay/rss/web/internal/portlet/action/RSSConfigurationAction.java
@@ -7,13 +7,13 @@ package com.liferay.rss.web.internal.portlet.action;
 
 import com.liferay.portal.kernel.module.configuration.ConfigurationException;
 import com.liferay.portal.kernel.portlet.ConfigurationAction;
-import com.liferay.portal.kernel.portlet.DefaultConfigurationAction;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.PropertiesParamUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portlet.display.template.portlet.action.BasePortletDisplayTemplateConfigurationAction;
 import com.liferay.rss.constants.RSSPortletKeys;
 
 import java.util.LinkedHashMap;
@@ -34,7 +34,8 @@ import org.osgi.service.component.annotations.Component;
 	property = "javax.portlet.name=" + RSSPortletKeys.RSS,
 	service = ConfigurationAction.class
 )
-public class RSSConfigurationAction extends DefaultConfigurationAction {
+public class RSSConfigurationAction
+	extends BasePortletDisplayTemplateConfigurationAction {
 
 	@Override
 	public String getJspPath(HttpServletRequest httpServletRequest) {

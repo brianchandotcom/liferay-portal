29becf8787c0b9466b03b0f0b3dded2108893800
ZakiMiklos <miklos.zakanyi@liferay.com>
2024-09-05 10:14:41 +0200

    LPD-29652 reorder

--- modules/util/portal-tools-rest-builder/src/main/java/com/liferay/portal/tools/rest/builder/internal/freemarker/tool/FreeMarkerTool.java
+++ modules/util/portal-tools-rest-builder/src/main/java/com/liferay/portal/tools/rest/builder/internal/freemarker/tool/FreeMarkerTool.java
@@ -337,13 +337,11 @@ public class FreeMarkerTool {
 		String fieldName = TextFormatter.format(value, TextFormatter.H);
 
 		fieldName = fieldName.replaceFirst("^\\-([0-9])", "negative_$1");
-
 		fieldName = fieldName.replaceFirst("^([0-9])", "positive_$1");
-
-		fieldName = fieldName.replaceAll("[ \\-\\/]", "_");
 
 		fieldName = fieldName.replaceAll("\\.", "_point_");
 
+		fieldName = fieldName.replaceAll("[ \\-\\/]", "_");
 		fieldName = fieldName.replaceAll("[^a-zA-Z0-9_]", "");
 
 		fieldName = fieldName.replaceAll("_+", "_");

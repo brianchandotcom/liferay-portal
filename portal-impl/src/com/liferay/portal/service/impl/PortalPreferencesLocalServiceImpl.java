brianchandotcom/liferay-portal

commit a4e417419b61f5e6b8d58a587d35748021478db2

LPS-124338 Apply new service.

portal-impl/src/com/liferay/portal/service/impl/PortalPreferencesLocalServiceImpl.java
@@ -15,17 +15,29 @@
 package com.liferay.portal.service.impl;
 
 import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.PortalPreferenceValue;
 import com.liferay.portal.kernel.model.PortalPreferences;
 import com.liferay.portal.kernel.model.PortletConstants;
 import com.liferay.portal.kernel.portlet.PortletPreferencesFactoryUtil;
+import com.liferay.portal.kernel.search.Indexable;
+import com.liferay.portal.kernel.search.IndexableType;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.service.base.PortalPreferencesLocalServiceBaseImpl;
+import com.liferay.portlet.PortalPreferenceKey;
 import com.liferay.portlet.PortalPreferencesImpl;
 import com.liferay.portlet.PortalPreferencesWrapper;
 import com.liferay.portlet.PortalPreferencesWrapperCacheUtil;
+
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 import javax.portlet.PortletPreferences;
 
@@ -62,7 +74,17 @@ public class PortalPreferencesLocalServiceImpl
 			defaultPreferences = PortletConstants.DEFAULT_PREFERENCES;
 		}
 
-		portalPreferences.setPreferences(defaultPreferences);
+		if (!Objects.equals(
+				PortletConstants.DEFAULT_PREFERENCES, defaultPreferences)) {
+
+			PortalPreferencesImpl portalPreferencesImpl =
+				(PortalPreferencesImpl)PortletPreferencesFactoryUtil.fromXML(
+					ownerId, ownerType, defaultPreferences);
+
+			_updatePortalPreferences(
+				portalPreferences, Collections.emptyMap(),
+				portalPreferencesImpl.getPreferences());
+		}
 
 		try {
 			portalPreferences = portalPreferencesPersistence.update(
@@ -85,6 +107,28 @@ public class PortalPreferencesLocalServiceImpl
 		}
 
 		return portalPreferences;
+	}
+
+	@Indexable(type = IndexableType.DELETE)
+	@Override
+	public PortalPreferences deletePortalPreferences(long portalPreferencesId)
+		throws PortalException {
+
+		portalPreferenceValuePersistence.removeByPortalPreferencesId(
+			portalPreferencesId);
+
+		return super.deletePortalPreferences(portalPreferencesId);
+	}
+
+	@Indexable(type = IndexableType.DELETE)
+	@Override
+	public PortalPreferences deletePortalPreferences(
+		PortalPreferences portalPreferences) {
+
+		portalPreferenceValuePersistence.removeByPortalPreferencesId(
+			portalPreferences.getPortalPreferencesId());
+
+		return super.deletePortalPreferences(portalPreferences);
 	}
 
 	@Override
@@ -119,8 +163,10 @@ public class PortalPreferencesLocalServiceImpl
 					ownerId, ownerType, defaultPreferences);
 		}
 
-		PortalPreferencesImpl portalPreferencesImpl = new PortalPreferencesImpl(
-			portalPreferences, false);
+		PortalPreferencesImpl portalPreferencesImpl =
+			(PortalPreferencesImpl)
+				portalPreferenceValueLocalService.getPortalPreferences(
+					portalPreferences, false);
 
 		portalPreferencesWrapper = new PortalPreferencesWrapper(
 			portalPreferencesImpl);
@@ -136,36 +182,190 @@ public class PortalPreferencesLocalServiceImpl
 		long ownerId, int ownerType,
 		com.liferay.portal.kernel.portlet.PortalPreferences portalPreferences) {
 
-		String xml = PortletPreferencesFactoryUtil.toXML(portalPreferences);
+		PortalPreferencesImpl portalPreferencesImpl =
+			(PortalPreferencesImpl)portalPreferences;
 
-		return updatePreferences(ownerId, ownerType, xml);
+		return _updatePortalPreferences(
+			ownerId, ownerType, portalPreferencesImpl.getPreferences());
 	}
 
 	@Override
 	public PortalPreferences updatePreferences(
 		long ownerId, int ownerType, String xml) {
 
+		PortalPreferencesImpl portalPreferencesImpl =
+			(PortalPreferencesImpl)PortletPreferencesFactoryUtil.fromXML(
+				ownerId, ownerType, xml);
+
+		return _updatePortalPreferences(
+			ownerId, ownerType, portalPreferencesImpl.getPreferences());
+	}
+
+	private PortalPreferences _updatePortalPreferences(
+		long ownerId, int ownerType,
+		Map<PortalPreferenceKey, String[]> preferencesMap) {
+
 		PortalPreferencesWrapperCacheUtil.remove(ownerId, ownerType);
 
-		PortalPreferences portalPreferences =
+		PortalPreferences portalPreferencesModel =
 			portalPreferencesPersistence.fetchByO_O(ownerId, ownerType);
 
-		if (portalPreferences == null) {
+		Map<PortalPreferenceKey, List<PortalPreferenceValue>>
+			portalPreferenceValuesMap = Collections.emptyMap();
+
+		if (portalPreferencesModel == null) {
 			long portalPreferencesId = counterLocalService.increment();
 
-			portalPreferences = portalPreferencesPersistence.create(
+			portalPreferencesModel = portalPreferencesPersistence.create(
 				portalPreferencesId);
 
-			portalPreferences.setOwnerId(ownerId);
-			portalPreferences.setOwnerType(ownerType);
+			portalPreferencesModel.setOwnerId(ownerId);
+			portalPreferencesModel.setOwnerType(ownerType);
+		}
+		else {
+			portalPreferenceValuesMap =
+				PortalPreferenceValueLocalServiceImpl.
+					getPortalPreferenceValuesMap(
+						portalPreferenceValuePersistence,
+						portalPreferencesModel.getPortalPreferencesId(), true);
 		}
 
-		portalPreferences.setPreferences(xml);
+		_updatePortalPreferences(
+			portalPreferencesModel, portalPreferenceValuesMap, preferencesMap);
 
-		return portalPreferencesPersistence.update(portalPreferences);
+		return portalPreferencesPersistence.update(portalPreferencesModel);
+	}
+
+	private void _updatePortalPreferences(
+		PortalPreferences portalPreferences,
+		Map<PortalPreferenceKey, List<PortalPreferenceValue>>
+			portalPreferenceValueMap,
+		Map<PortalPreferenceKey, String[]> preferencesMap) {
+
+		List<Map.Entry<List<PortalPreferenceValue>, PortalPreference>>
+			preferenceEntries = new ArrayList<>(preferencesMap.size());
+
+		int newCount = 0;
+
+		for (Map.Entry<PortalPreferenceKey, String[]> entry :
+				preferencesMap.entrySet()) {
+
+			String[] values = entry.getValue();
+
+			if (values == null) {
+				continue;
+			}
+
+			int size = 0;
+
+			List<PortalPreferenceValue> portalPreferenceValues =
+				portalPreferenceValueMap.remove(entry.getKey());
+
+			if (portalPreferenceValues != null) {
+				size = portalPreferenceValues.size();
+			}
+
+			if (values.length > size) {
+				newCount += values.length - size;
+			}
+
+			preferenceEntries.add(
+				new AbstractMap.SimpleImmutableEntry<>(
+					portalPreferenceValues,
+					new PortalPreference(entry.getKey(), values)));
+		}
+
+		for (List<PortalPreferenceValue> portalPreferenceValues :
+				portalPreferenceValueMap.values()) {
+
+			for (PortalPreferenceValue portalPreferenceValue :
+					portalPreferenceValues) {
+
+				portalPreferenceValuePersistence.remove(portalPreferenceValue);
+			}
+		}
+
+		long batchCounter = 0;
+
+		if (newCount > 0) {
+			batchCounter = counterLocalService.increment(
+				PortalPreferenceValue.class.getName(), newCount);
+
+			batchCounter -= newCount;
+		}
+
+		for (Map.Entry<List<PortalPreferenceValue>, PortalPreference> entry :
+				preferenceEntries) {
+
+			List<PortalPreferenceValue> portalPreferenceValues = entry.getKey();
+
+			PortalPreference portalPreference = entry.getValue();
+
+			PortalPreferenceKey portalPreferenceKey =
+				portalPreference._portalPreferenceKey;
+			String[] newValues = portalPreference._values;
+
+			int oldSize = 0;
+
+			if (portalPreferenceValues != null) {
+				oldSize = portalPreferenceValues.size();
+			}
+
+			for (int i = 0; i < newValues.length; i++) {
+				String value = newValues[i];
+
+				if (oldSize > i) {
+					PortalPreferenceValue portalPreferenceValue =
+						portalPreferenceValues.get(i);
+
+					if (!Objects.equals(
+							newValues[i], portalPreferenceValue.getValue())) {
+
+						portalPreferenceValue.setValue(value);
+
+						portalPreferenceValuePersistence.update(
+							portalPreferenceValue);
+					}
+				}
+				else {
+					PortalPreferenceValue portalPreferenceValue =
+						portalPreferenceValuePersistence.create(++batchCounter);
+
+					portalPreferenceValue.setPortalPreferencesId(
+						portalPreferences.getPortalPreferencesId());
+					portalPreferenceValue.setIndex(i);
+					portalPreferenceValue.setKey(portalPreferenceKey.getKey());
+					portalPreferenceValue.setNamespace(
+						portalPreferenceKey.getNamespace());
+					portalPreferenceValue.setValue(value);
+
+					portalPreferenceValuePersistence.update(
+						portalPreferenceValue);
+				}
+			}
+
+			for (int i = newValues.length; i < oldSize; i++) {
+				portalPreferenceValuePersistence.remove(
+					portalPreferenceValues.get(i));
+			}
+		}
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		PortalPreferencesLocalServiceImpl.class);
+
+	private static class PortalPreference {
+
+		private PortalPreference(
+			PortalPreferenceKey portalPreferenceKey, String[] values) {
+
+			_portalPreferenceKey = portalPreferenceKey;
+			_values = values;
+		}
+
+		private final PortalPreferenceKey _portalPreferenceKey;
+		private final String[] _values;
+
+	}
 
 }

portal-impl/src/com/liferay/portal/model/impl/PortalPreferenceValueImpl.java
@@ -15,22 +15,39 @@
 package com.liferay.portal.model.impl;
 
 /**
- * The extended model implementation for the PortalPreferenceValue service. Represents a row in the &quot;PortalPreferenceValue&quot; database table, with each column mapped to a property of this class.
- *
- * <p>
- * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the <code>com.liferay.portal.kernel.model.PortalPreferenceValue</code> interface.
- * </p>
- *
- * @author Brian Wing Shun Chan
+ * @author Preston Crary
  */
 public class PortalPreferenceValueImpl extends PortalPreferenceValueBaseImpl {
 
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. All methods that expect a portal preference value model instance should use the {@link com.liferay.portal.kernel.model.PortalPreferenceValue} interface instead.
-	 */
-	public PortalPreferenceValueImpl() {
+	public static final int SMALL_VALUE_MAX_LENGTH = 255;
+
+	@Override
+	public String getValue() {
+		String value = getLargeValue();
+
+		if (value.isEmpty()) {
+			value = getSmallValue();
+		}
+
+		return value;
+	}
+
+	@Override
+	public void setValue(String value) {
+		String largeValue = null;
+		String smallValue = null;
+
+		if (value != null) {
+			if (value.length() > SMALL_VALUE_MAX_LENGTH) {
+				largeValue = value;
+			}
+			else {
+				smallValue = value;
+			}
+		}
+
+		setLargeValue(largeValue);
+		setSmallValue(smallValue);
 	}
 
 }

portal-impl/src/com/liferay/portal/service/impl/LayoutLocalServiceImpl.java
@@ -17,16 +17,16 @@ package com.liferay.portal.service.impl;
 import com.liferay.exportimport.kernel.staging.MergeLayoutPrototypesThreadLocal;
 import com.liferay.layout.admin.kernel.model.LayoutTypePortletConstants;
 import com.liferay.petra.lang.CentralizedThreadLocal;
+import com.liferay.petra.sql.dsl.DSLQueryFactoryUtil;
+import com.liferay.petra.sql.dsl.query.DSLQuery;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.bean.BeanReference;
 import com.liferay.portal.kernel.change.tracking.CTTransactionException;
 import com.liferay.portal.kernel.dao.orm.DynamicQuery;
-import com.liferay.portal.kernel.dao.orm.DynamicQueryFactoryUtil;
 import com.liferay.portal.kernel.dao.orm.Property;
 import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
-import com.liferay.portal.kernel.dao.orm.RestrictionsFactoryUtil;
 import com.liferay.portal.kernel.exception.NoSuchLayoutException;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.RequiredLayoutException;
@@ -49,7 +49,9 @@ import com.liferay.portal.kernel.model.LayoutSet;
 import com.liferay.portal.kernel.model.LayoutSetPrototype;
 import com.liferay.portal.kernel.model.LayoutType;
 import com.liferay.portal.kernel.model.LayoutTypePortlet;
+import com.liferay.portal.kernel.model.PortalPreferenceValueTable;
 import com.liferay.portal.kernel.model.PortalPreferences;
+import com.liferay.portal.kernel.model.PortalPreferencesTable;
 import com.liferay.portal.kernel.model.PortletConstants;
 import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.ResourcePermission;
@@ -89,7 +91,6 @@ import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.kernel.workflow.WorkflowThreadLocal;
 import com.liferay.portal.service.base.LayoutLocalServiceBaseImpl;
 import com.liferay.portal.util.PropsValues;
-import com.liferay.portlet.PortalPreferencesImpl;
 import com.liferay.sites.kernel.util.Sites;
 import com.liferay.sites.kernel.util.SitesUtil;
 
@@ -3717,26 +3718,34 @@ public class LayoutLocalServiceImpl extends LayoutLocalServiceBaseImpl {
 	}
 
 	private void _resetPortalPreferences(Layout layout) {
-		DynamicQuery dynamicQuery = DynamicQueryFactoryUtil.forClass(
-			PortalPreferences.class, getClassLoader());
+		String namespace = CustomizedPages.namespacePlid(layout.getPlid());
 
-		dynamicQuery.add(
-			RestrictionsFactoryUtil.eq(
-				"ownerType", ResourceConstants.SCOPE_INDIVIDUAL));
-		dynamicQuery.add(
-			RestrictionsFactoryUtil.like(
-				"preferences",
-				"%" + CustomizedPages.namespacePlid(layout.getPlid()) + "%"));
+		DSLQuery dslQuery = DSLQueryFactoryUtil.select(
+			PortalPreferencesTable.INSTANCE
+		).from(
+			PortalPreferencesTable.INSTANCE
+		).innerJoinON(
+			PortalPreferenceValueTable.INSTANCE,
+			PortalPreferenceValueTable.INSTANCE.portalPreferencesId.eq(
+				PortalPreferencesTable.INSTANCE.portalPreferencesId)
+		).where(
+			PortalPreferencesTable.INSTANCE.ownerType.eq(
+				ResourceConstants.SCOPE_INDIVIDUAL
+			).and(
+				PortalPreferenceValueTable.INSTANCE.namespace.eq(namespace)
+			)
+		);
 
-		List<PortalPreferences> portalPreferenceses =
-			portalPreferencesLocalService.dynamicQuery(dynamicQuery);
+		for (PortalPreferences portalPreferencesModel :
+				portalPreferencesPersistence.<List<PortalPreferences>>dslQuery(
+					dslQuery)) {
 
-		for (PortalPreferences portalPreferences : portalPreferenceses) {
-			PortalPreferencesImpl portalPreferencesImpl =
-				new PortalPreferencesImpl(portalPreferences, false);
+			com.liferay.portal.kernel.portlet.PortalPreferences
+				portalPreferences =
+					portalPreferenceValueLocalService.getPortalPreferences(
+						portalPreferencesModel, false);
 
-			portalPreferencesImpl.resetValues(
-				CustomizedPages.namespacePlid(layout.getPlid()));
+			portalPreferences.resetValues(namespace);
 		}
 	}
 

portal-impl/src/com/liferay/portal/service/impl/PortalPreferenceValueLocalServiceImpl.java
@@ -14,28 +14,95 @@
 
 package com.liferay.portal.service.impl;
 
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.model.PortalPreferenceValue;
+import com.liferay.portal.kernel.model.PortalPreferences;
+import com.liferay.portal.kernel.service.persistence.PortalPreferenceValuePersistence;
 import com.liferay.portal.service.base.PortalPreferenceValueLocalServiceBaseImpl;
+import com.liferay.portlet.PortalPreferenceKey;
+import com.liferay.portlet.PortalPreferencesImpl;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
- * The implementation of the portal preference value local service.
- *
- * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the <code>com.liferay.portal.kernel.service.PortalPreferenceValueLocalService</code> interface.
- *
- * <p>
- * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
- * </p>
- *
- * @author Brian Wing Shun Chan
- * @see PortalPreferenceValueLocalServiceBaseImpl
+ * @author Preston Crary
  */
 public class PortalPreferenceValueLocalServiceImpl
 	extends PortalPreferenceValueLocalServiceBaseImpl {
 
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. Use <code>com.liferay.portal.kernel.service.PortalPreferenceValueLocalService</code> via injection or a <code>org.osgi.util.tracker.ServiceTracker</code> or use <code>com.liferay.portal.kernel.service.PortalPreferenceValueLocalServiceUtil</code>.
-	 */
+	public static Map<PortalPreferenceKey, String[]> getPreferenceMap(
+		PortalPreferenceValuePersistence portalPreferenceValuePersistence,
+		long portalPreferencesId, boolean useFinderCache) {
+
+		Map<PortalPreferenceKey, List<PortalPreferenceValue>>
+			portalPreferenceValuesMap = getPortalPreferenceValuesMap(
+				portalPreferenceValuePersistence, portalPreferencesId,
+				useFinderCache);
+
+		Map<PortalPreferenceKey, String[]> preferenceMap = new HashMap<>();
+
+		for (Map.Entry<PortalPreferenceKey, List<PortalPreferenceValue>> entry :
+				portalPreferenceValuesMap.entrySet()) {
+
+			List<PortalPreferenceValue> portalPreferenceValues =
+				entry.getValue();
+
+			String[] values = new String[portalPreferenceValues.size()];
+
+			for (int i = 0; i < portalPreferenceValues.size(); i++) {
+				PortalPreferenceValue portalPreferenceValue =
+					portalPreferenceValues.get(i);
+
+				values[i] = portalPreferenceValue.getValue();
+			}
+
+			preferenceMap.put(entry.getKey(), values);
+		}
+
+		return preferenceMap;
+	}
+
+	@Override
+	public com.liferay.portal.kernel.portlet.PortalPreferences
+		getPortalPreferences(
+			PortalPreferences portalPreferences, boolean signedIn) {
+
+		Map<PortalPreferenceKey, String[]> preferenceMap = getPreferenceMap(
+			portalPreferenceValuePersistence,
+			portalPreferences.getPortalPreferencesId(), true);
+
+		return new PortalPreferencesImpl(
+			portalPreferences.getOwnerId(), portalPreferences.getOwnerType(),
+			preferenceMap, signedIn);
+	}
+
+	protected static Map<PortalPreferenceKey, List<PortalPreferenceValue>>
+		getPortalPreferenceValuesMap(
+			PortalPreferenceValuePersistence portalPreferenceValuePersistence,
+			long portalPreferencesId, boolean useFinderCache) {
+
+		Map<PortalPreferenceKey, List<PortalPreferenceValue>>
+			portalPreferenceValuesMap = new HashMap<>();
+
+		for (PortalPreferenceValue portalPreferenceValue :
+				portalPreferenceValuePersistence.findByPortalPreferencesId(
+					portalPreferencesId, QueryUtil.ALL_POS, QueryUtil.ALL_POS,
+					null, useFinderCache)) {
+
+			List<PortalPreferenceValue> portalPreferenceValues =
+				portalPreferenceValuesMap.computeIfAbsent(
+					new PortalPreferenceKey(
+						portalPreferenceValue.getNamespace(),
+						portalPreferenceValue.getKey()),
+					key -> new ArrayList<>(1));
+
+			portalPreferenceValues.add(portalPreferenceValue);
+		}
+
+		return portalPreferenceValuesMap;
+	}
 
 }

portal-impl/src/com/liferay/portlet/PortalPreferenceKey.java
@@ -1,0 +1,92 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet;
+
+import com.liferay.petra.lang.HashUtil;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.util.Objects;
+
+/**
+ * @author Preston Crary
+ */
+public class PortalPreferenceKey {
+
+	public PortalPreferenceKey(String namespace, String key) {
+		if (key == null) {
+			throw new IllegalArgumentException();
+		}
+
+		_namespace = namespace;
+		_key = key;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (object == this) {
+			return true;
+		}
+
+		if (!(object instanceof PortalPreferenceKey)) {
+			return false;
+		}
+
+		PortalPreferenceKey portalPreferenceKey = (PortalPreferenceKey)object;
+
+		if ((Objects.equals(portalPreferenceKey._namespace, _namespace) ||
+			 (Validator.isNull(portalPreferenceKey._namespace) &&
+			  Validator.isNull(_namespace))) &&
+			Objects.equals(portalPreferenceKey._key, _key)) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	public String getKey() {
+		return _key;
+	}
+
+	public String getNamespace() {
+		return _namespace;
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = 0;
+
+		if (Validator.isNotNull(_namespace)) {
+			hash = HashUtil.hash(0, _namespace);
+		}
+
+		return HashUtil.hash(hash, _key);
+	}
+
+	@Override
+	public String toString() {
+		if (Validator.isNull(_namespace)) {
+			return _key;
+		}
+
+		return StringBundler.concat(_namespace, StringPool.POUND, _key);
+	}
+
+	private final String _key;
+	private final String _namespace;
+
+}

commit 2d25ac255b154a0dd0424a69ba0d5a96d7d1e80e

LPD-35850 Fix this long overdued bug since the beginning of PortalPreferenceValue PortalPreferences separation, caused by a4e41741 . When dealing with company PortalPreferences, the ownerId is the companyId, we must propagate the correct companyId to all its PortalPreferenceValue, rather than relying on the fragile CompanyThreadLocal.

portal-impl/src/com/liferay/portal/service/impl/PortalPreferencesLocalServiceImpl.java
@@ -343,6 +343,13 @@ public class PortalPreferencesLocalServiceImpl
 						portalPreferenceKey.getNamespace());
 					portalPreferenceValue.setValue(value);
 
+					if (portalPreferences.getOwnerType() ==
+							PortletKeys.PREFS_OWNER_TYPE_COMPANY) {
+
+						portalPreferenceValue.setCompanyId(
+							portalPreferences.getOwnerId());
+					}
+
 					_portalPreferenceValuePersistence.update(
 						portalPreferenceValue);
 				}
9fea55f28ff767f0c48a4b682cf26e6ef1ff42df
Ryan Schuhler <ryan.schuhler@liferay.com>
2024-09-12 11:25:54 -0700

    LRSD-5420 Update run.sh

--- workspaces/liferay-partner-workspace/quickstart/run.sh
+++ workspaces/liferay-partner-workspace/quickstart/run.sh
@@ -1,9 +1,69 @@
 #!/bin/bash
 
-pushd .. > /dev/null
+function check_health {
+	docker inspect --format='{{.State.Health.Status}}' "${1}" | grep -q "healthy"
 
-./gradlew clean deploy
+	if [[ $? -eq 0 ]]; then
+		echo "Container '${1}' is healthy."
 
-popd > /dev/null
+		return 0
+	fi
 
-docker compose up
+	echo "Container '${1}' is not healthy."
+
+	sleep 10
+
+	check_health "${1}"
+}
+
+function check_logs {
+	docker logs -f "${container}" | grep -q "${1}"
+
+	if [[ $? -eq 0 ]]; then
+		echo "Text '${1}' found in logs."
+
+		return 0
+	fi
+
+	echo "Text '${1}' not found in logs."
+
+	sleep 10
+
+	check_logs "${1}"
+}
+
+function deploy {
+	./gradlew :client-extensions:${1}:clean :client-extensions:${1}:deploy "-Ddeploy.docker.container.id=${container}"
+}
+
+function main {
+	local container="liferay-partner-workspace-liferay"
+
+	docker compose up -d
+
+	check_health ${container}
+
+	pushd .. > /dev/null
+
+	deploy "liferay-partner-custom-element"
+	deploy "liferay-partner-etc-cron"
+	deploy "liferay-partner-etc-spring-boot"
+	deploy "liferay-partner-site-initializer-code"
+
+	check_logs "Initialized Liferay Partner"
+
+	deploy "liferay-partner-site-initializer-quickstart"
+
+	if command -v op &> /dev/null
+	then
+		op read "op://Customer Solutions/PRM UAT SFDC CX/notesPlain" --out-file ./client-extensions/liferay-partner-instance-settings-quickstart/client-extension.yaml
+
+		deploy "liferay-partner-instance-settings-quickstart"
+	fi
+
+	popd > /dev/null
+
+	docker compose up
+}
+
+main "${@}"

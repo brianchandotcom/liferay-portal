a545fcae9f4f1d868222c6a3b751951cd55f7656
Jerry Adrianoo <jerry.adriano@liferay.com>
2024-09-30 11:00:31 -0300

    Revert "LPD-35168 Add feature flag"
    This reverts commit 309b9447fc39714bbbffd56498880bed43999525.

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectRelationshipLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectRelationshipLocalServiceImpl.java
@@ -1521,7 +1521,7 @@ public class ObjectRelationshipLocalServiceImpl
 			boolean edge, ObjectRelationship objectRelationship)
 		throws PortalException {
 
-		if (!edge || !FeatureFlagManagerUtil.isEnabled("LPS-187142")) {
+		if (!edge) {
 			return;
 		}
 

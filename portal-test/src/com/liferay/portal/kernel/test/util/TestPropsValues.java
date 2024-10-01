5a3027be36cc02bed199dce964ca41b348b61394
Alberto Chaparro <alberto.chaparro@liferay.com>
2024-10-01 23:51:37 +0200

    LPD-30986 Having a test propery simplifies the logic and make possible to change the virtual host if needed. Also I don't think we need to validate the existance of a company in the static code for TestPropsValues since we will get an understandable error just after that when getCompanyId is called

--- portal-test/src/com/liferay/portal/kernel/test/util/TestPropsValues.java
+++ portal-test/src/com/liferay/portal/kernel/test/util/TestPropsValues.java
@@ -15,7 +15,6 @@ import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.CompanyLocalServiceUtil;
 import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
 import com.liferay.portal.kernel.service.LayoutLocalServiceUtil;
-import com.liferay.portal.kernel.test.constants.DBPartitionTestConstants;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.PropsUtil;
@@ -57,18 +56,8 @@ public class TestPropsValues {
 
 		try {
 			if (DBPartition.isPartitionEnabled()) {
-				Company company =
-					CompanyLocalServiceUtil.fetchCompanyByVirtualHost(
-						DBPartitionTestConstants.DB_PARTITION_VIRTUAL_HOSTNAME);
-
-				if (company == null) {
-					throw new PortalException(
-						"Missing partitioned company: " +
-							DBPartitionTestConstants.
-								DB_PARTITION_VIRTUAL_HOSTNAME);
-				}
-
-				companyWebId = company.getWebId();
+				companyWebId = TestPropsUtil.get(
+					"database.partition.company.web.id");
 			}
 			else if (Validator.isNull(companyWebId)) {
 				companyWebId = GetterUtil.getString(

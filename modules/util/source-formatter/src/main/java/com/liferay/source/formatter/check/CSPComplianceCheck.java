74aaa4a984aebc00500aa20f21a0fed288058c89
Iván Zaera Avellón <ivan.zaera@liferay.com>
2024-10-04 13:12:14 +0200

    Revert "LPD-36212 SF"
    This reverts commit 745d1a477332d6d86caaf343126b8336d467ea13.

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/CSPComplianceCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/CSPComplianceCheck.java
@@ -11,7 +11,7 @@ import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -96,7 +96,7 @@ public class CSPComplianceCheck extends BaseTagAttributesCheck {
 					continue;
 				}
 
-				List<String> ignoredTagPrefixes = new ArrayList<>();
+				List<String> ignoredTagPrefixes = Collections.emptyList();
 
 				if (lowerCaseFileName.endsWith(".ftl")) {
 					ignoredTagPrefixes = getAttributeValues(

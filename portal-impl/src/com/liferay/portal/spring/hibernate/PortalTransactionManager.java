brianchandotcom/liferay-portal

commit 31456559fc2a2a2b3f867e3e35d1b43edce52830

LPD-39425 Sort

portal-impl/src/com/liferay/portal/spring/hibernate/PortalTransactionManager.java
@@ -149,13 +149,13 @@ public class PortalTransactionManager
 				SpringHibernateThreadLocalUtil.getResources(true);
 
 			SpringHibernateThreadLocalUtil.setResource(
-				resources, _dataSource, newConnectionHolder);
+				_dataSource, newConnectionHolder, resources);
 
 			hibernateTransactionObject.setConnectionHolder(newConnectionHolder);
 
 			if (hibernateTransactionObject.isNewSessionHolder()) {
 				SpringHibernateThreadLocalUtil.setResource(
-					resources, _sessionFactory, sessionHolder);
+					_sessionFactory, sessionHolder, resources);
 			}
 
 			sessionHolder.setSynchronizedWithTransaction(true);
@@ -335,13 +335,13 @@ public class PortalTransactionManager
 			SpringHibernateThreadLocalUtil.getResources(true);
 
 		SpringHibernateThreadLocalUtil.setResource(
-			resources, _sessionFactory,
-			suspendedResourcesHolder._sessionHolder);
+			_sessionFactory, suspendedResourcesHolder._sessionHolder,
+			resources);
 
 		if (suspendedResourcesHolder._connectionHolder != null) {
 			SpringHibernateThreadLocalUtil.setResource(
-				resources, _dataSource,
-				suspendedResourcesHolder._connectionHolder);
+				_dataSource, suspendedResourcesHolder._connectionHolder,
+				resources);
 		}
 	}
 
@@ -423,9 +423,9 @@ public class PortalTransactionManager
 
 		return new SuspendedResourcesHolder(
 			SpringHibernateThreadLocalUtil.setResource(
-				resources, _dataSource, null),
+				_dataSource, null, resources),
 			SpringHibernateThreadLocalUtil.setResource(
-				resources, _sessionFactory, null));
+				_sessionFactory, null, resources));
 	}
 
 	@Override

portal-impl/src/com/liferay/portal/spring/hibernate/PortletTransactionManager.java
@@ -149,8 +149,9 @@ public class PortletTransactionManager implements PlatformTransactionManager {
 		};
 
 		SpringHibernateThreadLocalUtil.setResource(
-			resources, portletSessionFactory,
-			_createSessionHolder(portletSession, portalSessionHolder));
+			portletSessionFactory,
+			_createSessionHolder(portletSession, portalSessionHolder),
+			resources);
 
 		if (DBPartition.isPartitionEnabled()) {
 			LastSessionRecorderUtil.addPortletSession(portletSession);
@@ -301,8 +302,8 @@ public class PortletTransactionManager implements PlatformTransactionManager {
 			}
 			finally {
 				SpringHibernateThreadLocalUtil.setResource(
-					SpringHibernateThreadLocalUtil.getResources(true),
-					_portletSessionFactory, _previousPortletSessionHolder);
+					_portletSessionFactory, _previousPortletSessionHolder,
+					SpringHibernateThreadLocalUtil.getResources(true));
 			}
 		}
 

portal-impl/src/com/liferay/portal/spring/hibernate/SpringHibernateThreadLocalUtil.java
@@ -69,7 +69,7 @@ public class SpringHibernateThreadLocalUtil {
 	}
 
 	public static <T> T setResource(
-		Map<Object, Object> resources, Object key, Object resource) {
+		Object key, Object resource, Map<Object, Object> resources) {
 
 		Object oldResource = null;
 

commit 65ff62cfb9ba5ca61278904ccdc8697a4e7cf19d

LPD-39425 Missed 2 usages during the sort, caused by 31456559fc2a2a2b3f867e3e35d1b43edce52830

portal-impl/src/com/liferay/portal/spring/hibernate/PortalTransactionManager.java
@@ -203,11 +203,11 @@ public class PortalTransactionManager
 
 		if (hibernateTransactionObject.isNewSessionHolder()) {
 			SpringHibernateThreadLocalUtil.setResource(
-				resources, _sessionFactory, null);
+				_sessionFactory, null, resources);
 		}
 
 		SpringHibernateThreadLocalUtil.setResource(
-			resources, _dataSource, null);
+			_dataSource, null, resources);
 
 		SessionHolder sessionHolder =
 			hibernateTransactionObject.getSessionHolder();
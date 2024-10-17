d1df19a9decc9107306c9598b0a9ae17d476bc67
Carlos Correa <carlos.correa@liferay.com>
2024-10-17 10:15:37 +0200

    LPD-39562 Use LinkedHashMap to keep the original order

--- modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/manager/v1_0/DefaultObjectEntryManagerImpl.java
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/manager/v1_0/DefaultObjectEntryManagerImpl.java
@@ -118,6 +118,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -1213,7 +1214,8 @@ public class DefaultObjectEntryManagerImpl
 			ObjectDefinition objectDefinition, ObjectEntry objectEntry)
 		throws Exception {
 
-		Map<String, ObjectRelationship> objectRelationships = new HashMap<>();
+		Map<String, ObjectRelationship> objectRelationships =
+			new LinkedHashMap<>();
 
 		Map<String, Object> properties = objectEntry.getProperties();
 

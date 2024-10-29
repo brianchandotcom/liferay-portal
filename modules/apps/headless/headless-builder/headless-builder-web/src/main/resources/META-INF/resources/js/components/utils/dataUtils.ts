8e29b6a4ae60fcb967089793f344e3a183ee801c
Jaime Leon <jaime.leon@liferay.com>
2024-10-29 12:20:07 +0100

    LPD-40178 Handle description change validation on save action

--- modules/apps/headless/headless-builder/headless-builder-web/src/main/resources/META-INF/resources/js/components/utils/dataUtils.ts
+++ modules/apps/headless/headless-builder/headless-builder-web/src/main/resources/META-INF/resources/js/components/utils/dataUtils.ts
@@ -145,7 +145,9 @@ export function hasEndpointDataChanged({
 		scope: uiScope,
 	} = localUIData;
 
-	const descriptionChanged = description !== uiDescription;
+	const descriptionChanged =
+		!(description === uiDescription) &&
+		!(description === '' && uiDescription === undefined);
 
 	const filtersArrayLengthChanged = !!(
 		localUIData.apiEndpointToAPIFilters &&

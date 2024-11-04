15a67c7ed1bbd88b9f5122aa3494fef9ce08790e
Javier Moral <javier.moral@liferay.com>
2024-11-04 15:23:01 +0100

    LPD-40360 PageTemplateSet: implement get

--- modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/PageTemplateSetResourceImpl.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/PageTemplateSetResourceImpl.java
@@ -5,11 +5,15 @@
 
 package com.liferay.headless.admin.site.internal.resource.v1_0;
 
+import com.liferay.headless.admin.site.dto.v1_0.PageTemplateSet;
 import com.liferay.headless.admin.site.resource.v1_0.PageTemplateSetResource;
+import com.liferay.layout.page.template.exception.NoSuchPageTemplateCollectionException;
+import com.liferay.layout.page.template.model.LayoutPageTemplateCollection;
 import com.liferay.layout.page.template.service.LayoutPageTemplateCollectionService;
 import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.vulcan.dto.converter.DTOConverter;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -42,11 +46,49 @@ public class PageTemplateSetResourceImpl
 			pageTemplateSetExternalReferenceCode, group.getGroupId());
 	}
 
+	@Override
+	public PageTemplateSet getSiteSiteByExternalReferenceCodePageTemplateSet(
+			String siteExternalReferenceCode,
+			String pageTemplateSetExternalReferenceCode)
+		throws Exception {
+
+		if (!FeatureFlagManagerUtil.isEnabled("LPD-35443")) {
+			throw new UnsupportedOperationException();
+		}
+
+		Group group = _groupLocalService.getGroupByExternalReferenceCode(
+			siteExternalReferenceCode, contextCompany.getCompanyId());
+
+		LayoutPageTemplateCollection layoutPageTemplateCollection =
+			_layoutPageTemplateCollectionService.
+				fetchLayoutPageTemplateCollection(
+					pageTemplateSetExternalReferenceCode, group.getGroupId());
+
+		if (layoutPageTemplateCollection == null) {
+			throw new NoSuchPageTemplateCollectionException();
+		}
+
+		return _toPageTemplateSet(layoutPageTemplateCollection);
+	}
+
+	private PageTemplateSet _toPageTemplateSet(
+			LayoutPageTemplateCollection layoutPageTemplateCollection)
+		throws Exception {
+
+		return _pageTemplateSetDTOConverter.toDTO(layoutPageTemplateCollection);
+	}
+
 	@Reference
 	private GroupLocalService _groupLocalService;
 
 	@Reference
 	private LayoutPageTemplateCollectionService
 		_layoutPageTemplateCollectionService;
+
+	@Reference(
+		target = "(component.name=com.liferay.headless.admin.site.internal.dto.v1_0.converter.PageTemplateSetDTOConverter)"
+	)
+	private DTOConverter<LayoutPageTemplateCollection, PageTemplateSet>
+		_pageTemplateSetDTOConverter;
 
 }

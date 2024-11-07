4686ec05b16704d949805e7a6beafefe5b3b1195
Keven Leone <keven.leone@liferay.com>
2024-11-07 16:10:58 -0300

    LPD-41296 Create AI and Setting Rest Controller

---
+++ workspaces/liferay-aicontentwizard-workspace/client-extensions/liferay-aicontentwizard-etc-spring-boot/src/main/java/com/liferay/ai/content/wizard/AI.java
@@ -0,0 +1,96 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.ai.content.wizard;
+
+import com.liferay.client.extension.util.spring.boot.BaseRestController;
+import com.liferay.ai.content.wizard.interfaces.LiferayAssistant;
+import com.liferay.ai.content.wizard.models.AIContext;
+import com.liferay.ai.content.wizard.openai.OpenAIChatModelConfiguration;
+import com.liferay.ai.content.wizard.openai.OpenAIImageModelConfiguration;
+import com.liferay.ai.content.wizard.services.LiferayService;
+import com.liferay.ai.content.wizard.tools.AccountTool;
+import com.liferay.ai.content.wizard.tools.BlogTool;
+import com.liferay.ai.content.wizard.tools.CategoryTool;
+import com.liferay.ai.content.wizard.tools.KnowledgeBaseTool;
+import com.liferay.ai.content.wizard.tools.SiteTool;
+
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.service.AiServices;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.json.JSONObject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Keven Leone
+ */
+@RequestMapping("/ai")
+@RestController
+public class AI extends BaseRestController {
+
+	@PostMapping(
+		produces = MediaType.APPLICATION_JSON_VALUE, value = "/generate"
+	)
+	public ResponseEntity<String> generate(@RequestBody String json) {
+		JSONObject jsonObject = new JSONObject(json);
+
+		String question = jsonObject.getString("question");
+
+		ChatLanguageModel chatLanguageModel =
+			_openAIChatModelConfiguration.getOpenAIChatLanguageModel();
+
+		AIContext aiContext = new AIContext(
+			_liferayService, openAIImageModelConfiguration);
+
+		aiContext.setSiteId(jsonObject.getLong("siteId"));
+
+		LiferayAssistant assistant = AiServices.builder(
+			LiferayAssistant.class
+		).chatLanguageModel(
+			chatLanguageModel
+		).tools(
+			new AccountTool(aiContext), new BlogTool(aiContext),
+			new CategoryTool(aiContext), new KnowledgeBaseTool(aiContext),
+			new SiteTool(aiContext)
+		).chatMemory(
+			MessageWindowChatMemory.withMaxMessages(10)
+		).build();
+
+		if (_log.isInfoEnabled()) {
+			_log.info("Asked question: " + question);
+		}
+
+		return new ResponseEntity<>(
+			new JSONObject(
+			).put(
+				"output", assistant.chat(question)
+			).toString(),
+			HttpStatus.OK);
+	}
+
+	@Autowired
+	protected OpenAIImageModelConfiguration openAIImageModelConfiguration;
+
+	private static final Log _log = LogFactory.getLog(AI.class);
+
+	@Autowired
+	private LiferayService _liferayService;
+
+	@Autowired
+	private OpenAIChatModelConfiguration _openAIChatModelConfiguration;
+
+}
---
+++ workspaces/liferay-aicontentwizard-workspace/client-extensions/liferay-aicontentwizard-etc-spring-boot/src/main/java/com/liferay/ai/content/wizard/Settings.java
@@ -0,0 +1,85 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.ai.content.wizard;
+
+import com.liferay.client.extension.util.spring.boot.BaseRestController;
+import com.liferay.ai.content.wizard.services.LiferayService;
+import com.liferay.ai.content.wizard.services.WizardSettingService;
+
+import org.json.JSONObject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Keven Leone
+ */
+@RequestMapping("/settings")
+@RestController
+public class Settings extends BaseRestController {
+
+	@DeleteMapping("/{id}")
+	public void deleteSetting(@PathVariable("id") String id) {
+		_liferayService.deleteContentWizardSetting(id);
+	}
+
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> getSettings() {
+		return new ResponseEntity<>(
+			_liferayService.getContentWizardSettings(), HttpStatus.OK);
+	}
+
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE, value = "/status")
+	public ResponseEntity<String> getStatus() {
+		JSONObject settingJSONObject =
+			_wizardSettingService.getActiveWizardSettingJSONObject();
+
+		if (settingJSONObject == null) {
+			return new ResponseEntity<>(
+				new JSONObject(
+				).put(
+					"active", false
+				).toString(),
+				HttpStatus.OK);
+		}
+
+		return new ResponseEntity<>(
+			new JSONObject(
+			).put(
+				"active", true
+			).put(
+				"provider",
+				settingJSONObject.getJSONObject(
+					"provider"
+				).getString(
+					"key"
+				)
+			).toString(),
+			HttpStatus.OK);
+	}
+
+	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> postSetting(@RequestBody String json) {
+		return new ResponseEntity<>(
+			_liferayService.postContentWizardSettings(json), HttpStatus.OK);
+	}
+
+	@Autowired
+	private LiferayService _liferayService;
+
+	@Autowired
+	private WizardSettingService _wizardSettingService;
+
+}

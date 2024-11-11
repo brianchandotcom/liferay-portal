327cd27f2236e4ed7af77ef5315a828783ca65d7
Antonio Ortega <60252917@liferay.com>
2024-11-11 12:51:35 +0100

    LPD-40036 Add new test to check search iterator headers' size

---
+++ modules/apps/frontend-taglib/frontend-taglib-sample-web/src/main/java/com/liferay/frontend/taglib/sample/web/internal/display/context/SearchIteratorDisplayContext.java
@@ -0,0 +1,80 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.frontend.taglib.sample.web.internal.display.context;
+
+import com.liferay.frontend.taglib.sample.web.internal.constants.TaglibSamplePortletKeys;
+import com.liferay.portal.kernel.dao.search.SearchContainer;
+import com.liferay.portal.kernel.dao.search.SearchPaginationUtil;
+import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.portlet.PortletURLUtil;
+import com.liferay.portal.kernel.portlet.SearchOrderByUtil;
+import com.liferay.portal.kernel.util.Portal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+/**
+ * @author Antonio Ortega
+ */
+public class SearchIteratorDisplayContext {
+
+	public SearchIteratorDisplayContext(
+		Portal portal, RenderRequest renderRequest,
+		RenderResponse renderResponse) {
+
+		_portal = portal;
+		_renderRequest = renderRequest;
+		_renderResponse = renderResponse;
+	}
+
+	public SearchContainer<String> getSearchContainer() {
+		LiferayPortletRequest liferayPortletRequest =
+			_portal.getLiferayPortletRequest(_renderRequest);
+		LiferayPortletResponse liferayPortletResponse =
+			_portal.getLiferayPortletResponse(_renderResponse);
+
+		SearchContainer<String> searchContainer = new SearchContainer<>(
+			_renderRequest,
+			PortletURLUtil.getCurrent(
+				liferayPortletRequest, liferayPortletResponse),
+			null, "no-items-were-found");
+
+		searchContainer.setId("StringItemSearchContainer");
+		searchContainer.setOrderByCol(
+			SearchOrderByUtil.getOrderByCol(
+				_renderRequest, TaglibSamplePortletKeys.TAGLIB_SAMPLE_PORTLET,
+				"order-by-col", "name"));
+		searchContainer.setOrderByType(
+			SearchOrderByUtil.getOrderByType(
+				_renderRequest, TaglibSamplePortletKeys.TAGLIB_SAMPLE_PORTLET,
+				"order-by-type", "asc"));
+
+		List<String> itemList = new ArrayList<>();
+
+		for (int i = 1; i < 500; i++) {
+			itemList.add(String.valueOf(i));
+		}
+
+		int[] startAndEnd = SearchPaginationUtil.calculateStartAndEnd(
+			searchContainer.getStart(), searchContainer.getEnd(),
+			itemList.size());
+
+		searchContainer.setResultsAndTotal(
+			() -> itemList.subList(startAndEnd[0], startAndEnd[1]),
+			itemList.size());
+
+		return searchContainer;
+	}
+
+	private final Portal _portal;
+	private final RenderRequest _renderRequest;
+	private final RenderResponse _renderResponse;
+
+}

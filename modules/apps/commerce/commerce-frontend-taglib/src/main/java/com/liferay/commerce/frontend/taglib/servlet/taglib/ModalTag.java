a0f6b945958c8d204ce3dc7ffcd300b75a8a5e4c
Brian Chan <brian.chan@liferay.com>
2024-11-12 19:59:20 +0100

    LPD-40612 formatJavadoc

--- modules/apps/commerce/commerce-frontend-taglib/src/main/java/com/liferay/commerce/frontend/taglib/servlet/taglib/ModalTag.java
+++ modules/apps/commerce/commerce-frontend-taglib/src/main/java/com/liferay/commerce/frontend/taglib/servlet/taglib/ModalTag.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.PageContext;
 
 /**
- * @author Fabio Diego Mastrorilli
+ * @author     Fabio Diego Mastrorilli
  * @deprecated As of Cavanaugh (7.4.x)
  */
 @Deprecated

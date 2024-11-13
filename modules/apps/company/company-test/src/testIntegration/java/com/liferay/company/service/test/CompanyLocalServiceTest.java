1b2f8fbd2cdf010d7b672d08c9b8453cd4c690fa
Brian Chan <brian.chan@liferay.com>
2024-11-13 09:27:59 +0100

    LPD-39518 Wordsmith

--- modules/apps/company/company-test/src/testIntegration/java/com/liferay/company/service/test/CompanyLocalServiceTest.java
+++ modules/apps/company/company-test/src/testIntegration/java/com/liferay/company/service/test/CompanyLocalServiceTest.java
@@ -472,7 +472,7 @@ public class CompanyLocalServiceTest {
 
 			StartupHelperUtil.setDBNew(false);
 
-			// This simulates a reboot
+			// Simulate a reboot
 
 			CounterFinder counterFinder = ReflectionTestUtil.getFieldValue(
 				_counterLocalService, "counterFinder");

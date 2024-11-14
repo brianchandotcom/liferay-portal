83d6daa5399788b4f8fece6b32956179ec3a60dc
Alan Huang <alan.huang@liferay.com>
2024-11-14 13:43:24 +0800

    LPD-39462 Sort content in @FeatureFlags and @TestInfo

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/JavaTestInfoAnnotationCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/JavaTestInfoAnnotationCheck.java
@@ -5,6 +5,8 @@
 
 package com.liferay.source.formatter.check;
 
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.util.NaturalOrderStringComparator;
 import com.liferay.portal.kernel.util.StringUtil;
 
@@ -23,44 +25,109 @@ public class JavaTestInfoAnnotationCheck extends BaseFileCheck {
 	protected String doProcess(
 		String fileName, String absolutePath, String content) {
 
-		if (!absolutePath.contains("/test/") &&
-			!absolutePath.contains("/testIntegration/")) {
+		for (String annotationName : _ANNOTATION_NAMES) {
+			int x = -1;
 
-			return content;
+			while (true) {
+				x = content.indexOf("\t@" + annotationName + "(", x + 1);
+
+				if (x == -1) {
+					break;
+				}
+
+				int y = content.indexOf(")", x);
+
+				if (y == -1) {
+					continue;
+				}
+
+				String annotationContent = content.substring(
+					x + annotationName.length() + 3, y);
+
+				String trimmmedAnnotationContent = annotationContent.trim();
+
+				if (trimmmedAnnotationContent.startsWith("\"") &&
+					trimmmedAnnotationContent.endsWith("\"")) {
+
+					trimmmedAnnotationContent =
+						trimmmedAnnotationContent.substring(
+							1, trimmmedAnnotationContent.length() - 1);
+
+					String[] values = trimmmedAnnotationContent.split(",");
+
+					if (values.length < 2) {
+						continue;
+					}
+
+					Arrays.sort(values, new NaturalOrderStringComparator());
+
+					StringBundler sb = new StringBundler(values.length * 4);
+
+					for (String value : values) {
+						sb.append(StringPool.QUOTE);
+						sb.append(value);
+						sb.append(StringPool.QUOTE);
+						sb.append(StringPool.COMMA_AND_SPACE);
+					}
+
+					if (sb.index() > 0) {
+						sb.setIndex(sb.index() - 1);
+					}
+
+					return StringUtil.replaceFirst(
+						content, annotationContent, "{" + sb.toString() + "}",
+						x);
+				}
+				else if (trimmmedAnnotationContent.startsWith("{") &&
+						 trimmmedAnnotationContent.endsWith("}")) {
+
+					trimmmedAnnotationContent =
+						trimmmedAnnotationContent.substring(
+							1, trimmmedAnnotationContent.length() - 1);
+
+					trimmmedAnnotationContent =
+						trimmmedAnnotationContent.replaceAll("\n\t+", " ");
+					trimmmedAnnotationContent =
+						trimmmedAnnotationContent.trim();
+
+					String[] values = trimmmedAnnotationContent.split(", ");
+
+					if (values.length < 2) {
+						continue;
+					}
+
+					NaturalOrderStringComparator comparator =
+						new NaturalOrderStringComparator();
+					String previousValue = null;
+
+					for (String value : values) {
+						if (previousValue == null) {
+							previousValue = value;
+
+							continue;
+						}
+
+						if (comparator.compare(previousValue, value) > 0) {
+							addMessage(
+								fileName,
+								StringBundler.concat(
+									"Incorrect order in @", annotationName,
+									": ", previousValue, " should come after ",
+									value),
+								getLineNumber(content, x));
+
+							break;
+						}
+					}
+				}
+			}
 		}
 
-		int x = -1;
-
-		while (true) {
-			x = content.indexOf("\n\t@TestInfo(\"", x + 1);
-
-			if (x == -1) {
-				return content;
-			}
-
-			int y = content.indexOf("\")", x);
-
-			if (y == -1) {
-				return content;
-			}
-
-			String testInfo = content.substring(x + 13, y);
-
-			String[] array = testInfo.split(",");
-
-			if (array.length < 2) {
-				return content;
-			}
-
-			Arrays.sort(array, new NaturalOrderStringComparator());
-
-			String newTestInfo = StringUtil.merge(array);
-
-			if (!testInfo.equals(newTestInfo)) {
-				return StringUtil.replaceFirst(
-					content, testInfo, newTestInfo, x);
-			}
-		}
+		return content;
 	}
+
+	private static final String[] _ANNOTATION_NAMES = {
+		"FeatureFlags", "TestInfo"
+	};
 
 }

1d26e255df5263c8562958881415b832b6146dd6
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2024-11-20 15:58:42 +0100

    LPD-42618 Adapt DTOConverter

--- modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/dto/v1_0/converter/FriendlyURLHistoryDTOConverter.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/dto/v1_0/converter/FriendlyURLHistoryDTOConverter.java
@@ -5,22 +5,31 @@
 
 package com.liferay.headless.admin.site.internal.dto.v1_0.converter;
 
-import com.liferay.friendly.url.model.FriendlyURLEntry;
+import com.liferay.friendly.url.service.FriendlyURLEntryLocalService;
+import com.liferay.friendly.url.util.comparator.FriendlyURLEntryLocalizationComparator;
 import com.liferay.headless.admin.site.dto.v1_0.FriendlyUrlHistory;
+import com.liferay.layout.friendly.url.LayoutFriendlyURLEntryHelper;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.vulcan.dto.converter.DTOConverter;
 import com.liferay.portal.vulcan.dto.converter.DTOConverterContext;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Eudaldo Alonso
  */
 @Component(
-	property = "dto.class.name=com.liferay.friendly.url.model.FriendlyURLEntry",
+	property = "dto.class.name=com.liferay.portal.kernel.model.Layout",
 	service = DTOConverter.class
 )
 public class FriendlyURLHistoryDTOConverter
-	implements DTOConverter<FriendlyURLEntry, FriendlyUrlHistory> {
+	implements DTOConverter<Layout, FriendlyUrlHistory> {
 
 	@Override
 	public String getContentType() {
@@ -29,16 +38,52 @@ public class FriendlyURLHistoryDTOConverter
 
 	@Override
 	public FriendlyUrlHistory toDTO(
-			DTOConverterContext dtoConverterContext,
-			FriendlyURLEntry friendlyURLEntry)
+			DTOConverterContext dtoConverterContext, Layout layout)
 		throws Exception {
 
 		return new FriendlyUrlHistory() {
 			{
 				setFriendlyUrlPath_i18n(
-					friendlyURLEntry::getLanguageIdToUrlTitleMap);
+					() -> _getFriendlyUrlPathJSONObject(layout));
 			}
 		};
 	}
+
+	private JSONObject _getFriendlyUrlPathJSONObject(Layout layout)
+		throws Exception {
+
+		JSONObject jsonObject = _jsonFactory.createJSONObject();
+
+		long classNameId = _layoutFriendlyURLEntryHelper.getClassNameId(
+			layout.isPrivateLayout());
+
+		for (String languageId : layout.getAvailableLanguageIds()) {
+			jsonObject.put(
+				LocaleUtil.toBCP47LanguageId(languageId),
+				JSONUtil.toJSONArray(
+					_friendlyURLEntryLocalService.
+						getFriendlyURLEntryLocalizations(
+							layout.getGroupId(), classNameId, layout.getPlid(),
+							languageId, QueryUtil.ALL_POS, QueryUtil.ALL_POS,
+							_friendlyURLEntryLocalizationComparator),
+					friendlyURLEntryLocalization ->
+						friendlyURLEntryLocalization.getUrlTitle()));
+		}
+
+		return jsonObject;
+	}
+
+	private final FriendlyURLEntryLocalizationComparator
+		_friendlyURLEntryLocalizationComparator =
+			FriendlyURLEntryLocalizationComparator.getInstance(false);
+
+	@Reference
+	private FriendlyURLEntryLocalService _friendlyURLEntryLocalService;
+
+	@Reference
+	private JSONFactory _jsonFactory;
+
+	@Reference
+	private LayoutFriendlyURLEntryHelper _layoutFriendlyURLEntryHelper;
 
 }

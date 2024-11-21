405390b6366ba3c3aed9a2d6820153d657ce306b
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2024-11-21 21:59:41 +0000

    LPD-39796 We should actually test this in testGetV2Users

--- modules/dxp/apps/scim/scim-rest-test/src/testIntegration/java/com/liferay/scim/rest/resource/v1_0/test/UserResourceTest.java
+++ modules/dxp/apps/scim/scim-rest-test/src/testIntegration/java/com/liferay/scim/rest/resource/v1_0/test/UserResourceTest.java
@@ -145,27 +145,6 @@ public class UserResourceTest extends BaseUserResourceTestCase {
 			404, userResource.deleteV2UserHttpResponse("12345"));
 	}
 
-	@Test
-	public void testExactSearchBySCIMClientId() throws Exception {
-		User user1 = testDeleteV2User_addUser();
-		User user2 = testDeleteV2User_addUser();
-
-		_assertListResponse(userResource.getV2Users(5, 0), 2, 2, user1, user2);
-
-		long userId = GetterUtil.getLong(user2.getId());
-
-		ScimTestUtil.saveSCIMClientId(
-			com.liferay.portal.kernel.model.User.class.getName(), userId,
-			TestPropsValues.getCompanyId(),
-			"SCIM_scim-client-test" + RandomTestUtil.randomString());
-
-		_reindexUser(userId);
-
-		_assertListResponse(userResource.getV2Users(5, 0), 1, 1, user1);
-
-		ConfigurationTestUtil.deleteConfiguration(_pid);
-	}
-
 	@Override
 	@Test
 	public void testGetV2UserById() throws Exception {
@@ -202,6 +181,17 @@ public class UserResourceTest extends BaseUserResourceTestCase {
 
 		_assertListResponse(
 			userResource.getV2Users(5, 3), 3, 1, user1, user2, user3);
+
+		long userId = GetterUtil.getLong(user3.getId());
+
+		ScimTestUtil.saveSCIMClientId(
+			com.liferay.portal.kernel.model.User.class.getName(), userId,
+			TestPropsValues.getCompanyId(),
+			"SCIM_scim-client-test" + RandomTestUtil.randomString());
+
+		_reindexUser(userId);
+
+		_assertListResponse(userResource.getV2Users(5, 0), 2, 2, user1, user2);
 
 		ConfigurationTestUtil.deleteConfiguration(_pid);
 

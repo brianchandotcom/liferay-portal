a399c112a82bb2a5d6732ae9aa87901d7febe3f0
Sam Ziemer <Samuel.Ziemer@liferay.com>
2024-11-25 15:15:02 -0500

    LPD-42567 SF

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-test-util/src/main/java/com/liferay/dynamic/data/mapping/test/util/DDMStructureTestUtil.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-test-util/src/main/java/com/liferay/dynamic/data/mapping/test/util/DDMStructureTestUtil.java
@@ -277,10 +277,6 @@ public class DDMStructureTestUtil {
 			"en_US");
 	}
 
-	public static DDMStructure updateStructure(DDMStructure ddmStructure) {
-		return DDMStructureLocalServiceUtil.updateDDMStructure(ddmStructure);
-	}
-
 	protected static Document createDocumentContent(
 		String availableLocales, String defaultLocale) {
 

092a4c7b9d460c9640f2f84d5243a7cdaf720c5b
Evan Thibodeau <evan.thibodeau@liferay.com>
2024-11-28 09:50:23 +0400

    LPD-39924 - Add Sidebar and Sidebar Trigger page fragments.

---
+++ modules/apps/site-initializer/site-initializer-cms/src/main/resources/site-initializer/fragments/group/sidebar/index.js
@@ -0,0 +1,72 @@
+const sidebarId = `${fragmentEntryLinkNamespace}sidebar`;
+const sidebarOpenKey = `${fragmentEntryLinkNamespace}sidebarOpen`;
+
+const sidebar = fragmentElement.querySelector('.sidebar-container');
+const sidebarCloseButton = fragmentElement.querySelector('.sidebar-close');
+const sidebarContainer = fragmentElement.querySelector('.sidebar-fragment');
+const sidebarTrigger = fragmentElement.querySelector('.sidebar-toggle');
+
+if (sidebarTrigger) {
+	sidebar.setAttribute('id', sidebarId);
+	sidebarTrigger.setAttribute('aria-controls', sidebarId);
+
+	addSidebarToggleClickEventListener(sidebarTrigger);
+}
+
+addSidebarToggleClickEventListener(sidebarCloseButton);
+
+const sidebarOpenString = Liferay.Util.LocalStorage.getItem(
+	sidebarOpenKey, Liferay.Util.LocalStorage.TYPES.FUNCTIONAL);
+
+let sidebarOpen = sidebarOpenString === 'false' ? false : true;
+
+if (!sidebarOpen) {
+	toggleSidebar(false);
+}
+
+function addSidebarToggleClickEventListener(node) {
+	node.addEventListener('click', () => handleSidebarToggleClick());
+
+	node.removeAttribute('disabled');
+}
+
+function handleSidebarToggleClick() {
+	sidebarOpen = !sidebarOpen;
+
+	toggleSidebar();
+
+	Liferay.Util.LocalStorage.setItem(
+		sidebarOpenKey,
+		sidebarOpen,
+		Liferay.Util.LocalStorage.TYPES.FUNCTIONAL
+	);
+}
+
+function toggleSidebar(moveFocus = true) {
+	sidebarContainer.classList.toggle('open');
+
+	sidebar.toggleAttribute('inert');
+
+	if (sidebarTrigger) {
+		sidebarTrigger.querySelectorAll('.inline-item').forEach(
+			node => node.classList.toggle('hide')
+		);
+
+		sidebarTrigger.toggleAttribute('aria-expanded');
+		sidebarTrigger.toggleAttribute('aria-pressed');
+
+		if (sidebarOpen) {
+			sidebarTrigger.setAttribute('title', sidebarTrigger.dataset.titleclose);
+		} else {
+			sidebarTrigger.setAttribute('title', sidebarTrigger.dataset.titleopen);
+		}
+	}
+
+	if (moveFocus) {
+		if (sidebarOpen) {
+			sidebarCloseButton.focus();
+		} else if (sidebarTrigger) {
+			sidebarTrigger.focus();
+		}
+	}
+}

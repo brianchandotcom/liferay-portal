brianchandotcom/liferay-portal

commit 96942b3596319ac22284cfc4a98fed7f13e5eff7

LPD-42652 SF

modules/util/portal-tools-rest-builder/src/main/java/com/liferay/portal/tools/rest/builder/RESTBuilder.java
@@ -2094,11 +2094,11 @@ public class RESTBuilder {
 
 		Files.deleteIfExists(Paths.get("./openapitools.json"));
 		Files.deleteIfExists(Paths.get(outputPathString, ".gitignore"));
+		Files.deleteIfExists(Paths.get(outputPathString, ".openapi-generator"));
 		Files.deleteIfExists(
 			Paths.get(outputPathString, ".openapi-generator", "FILES"));
 		Files.deleteIfExists(
 			Paths.get(outputPathString, ".openapi-generator", "VERSION"));
-		Files.deleteIfExists(Paths.get(outputPathString, ".openapi-generator"));
 		Files.deleteIfExists(
 			Paths.get(outputPathString, ".openapi-generator-ignore"));
 		Files.deleteIfExists(Paths.get(outputPathString, "git_push.sh"));

commit 55785d67c4cd0f07aae0a5d6f5f27788ec07c47c

LPD-43780 Avoiding directory not empty exception
The folder must be the last to be deleted.

Caused by 96942b3596319ac22284cfc4a98fed7f13e5eff7

modules/util/portal-tools-rest-builder/src/main/java/com/liferay/portal/tools/rest/builder/RESTBuilder.java
@@ -2094,13 +2094,13 @@ public class RESTBuilder {
 
 		Files.deleteIfExists(Paths.get("./openapitools.json"));
 		Files.deleteIfExists(Paths.get(outputPathString, ".gitignore"));
-		Files.deleteIfExists(Paths.get(outputPathString, ".openapi-generator"));
 		Files.deleteIfExists(
 			Paths.get(outputPathString, ".openapi-generator", "FILES"));
 		Files.deleteIfExists(
 			Paths.get(outputPathString, ".openapi-generator", "VERSION"));
 		Files.deleteIfExists(
 			Paths.get(outputPathString, ".openapi-generator-ignore"));
+		Files.deleteIfExists(Paths.get(outputPathString, ".openapi-generator"));
 		Files.deleteIfExists(Paths.get(outputPathString, "git_push.sh"));
 	}
 
brianchandotcom/liferay-portal

commit 2bc59b52ed4490b8910b786b62a041976e489744

LPD-41800 Remove all references and logic for Sybase support

portal-impl/src/com/liferay/portal/spring/hibernate/DialectDetector.java
@@ -15,7 +15,6 @@ import com.liferay.portal.dao.orm.hibernate.MariaDBDialect;
 import com.liferay.portal.dao.orm.hibernate.Oracle10gDialect;
 import com.liferay.portal.dao.orm.hibernate.SQLServer2005Dialect;
 import com.liferay.portal.dao.orm.hibernate.SQLServer2008Dialect;
-import com.liferay.portal.dao.orm.hibernate.SybaseASE157Dialect;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
@@ -81,17 +80,8 @@ public class DialectDetector {
 							"portal-ext.properties."));
 				}
 			}
-			else if (dbName.equals("Adaptive Server Enterprise") &&
-					 (dbMajorVersion >= 15)) {
 
-				dialect = new SybaseASE157Dialect();
-			}
-			else if (dbName.equals("ASE")) {
-				throw new RuntimeException(
-					"jTDS is no longer suppported. Please use the Sybase " +
-						"JDBC driver to connect to Sybase.");
-			}
-			else if (dbName.startsWith("DB2") && (dbMajorVersion >= 9)) {
+			if (dbName.startsWith("DB2") && (dbMajorVersion >= 9)) {
 				dialect = new DB2Dialect();
 			}
 			else if (dbName.startsWith("MariaDB")) {

commit c7d0d622239df2ee163bb8e13e2ee6c9d65f81b0

LPD-43939 Fix the major HSQL regression caused by 2bc59b52ed4490b8910b786b62a041976e489744

portal-impl/src/com/liferay/portal/spring/hibernate/DialectDetector.java
@@ -80,8 +80,7 @@ public class DialectDetector {
 							"portal-ext.properties."));
 				}
 			}
-
-			if (dbName.startsWith("DB2") && (dbMajorVersion >= 9)) {
+			else if (dbName.startsWith("DB2") && (dbMajorVersion >= 9)) {
 				dialect = new DB2Dialect();
 			}
 			else if (dbName.startsWith("MariaDB")) {
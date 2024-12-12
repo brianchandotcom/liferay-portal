151afafda62f5b776220d376e6e9dcf98d7be2f0
Alan Huang <alan.huang@liferay.com>
2024-12-12 10:49:13 +0800

    LPD-43137 | LPD-43138 Auto SF

--- modules/dxp/apps/osb/osb-faro/set_nexus_credentials.sh
+++ modules/dxp/apps/osb/osb-faro/set_nexus_credentials.sh
@@ -4,25 +4,25 @@ function main {
 	pushd ../../../../../
 
 	touch "build.$(whoami).properties"
-	
+
 	if [[ $(grep -c "build.repository.private.password" "build.$(whoami).properties") == 0 ]]
 	then
 		local nexus_password=$(op read "op://Analytics Cloud Team/Nexus Private Repository Account/password")
-		
+
 		echo "build.repository.private.password=${nexus_password}" >> "build.$(whoami).properties"
 	fi
 
 	if [[ $(grep -c "build.repository.private.url" "build.$(whoami).properties") == 0 ]]
 	then
 		local nexus_url=$(op read "op://Analytics Cloud Team/Nexus Private Repository Account/website")
-		
+
 		echo "build.repository.private.url=${nexus_url}" >> "build.$(whoami).properties"
 	fi
 
 	if [[ $(grep -c "build.repository.private.username" "build.$(whoami).properties") == 0 ]]
 	then
 		local nexus_username=$(op read "op://Analytics Cloud Team/Nexus Private Repository Account/username")
-		
+
 		echo "build.repository.private.username=${nexus_username}" >> "build.$(whoami).properties"
 	fi
 

3c886e7f592f64d11ba7b75291f156cda6ce8ef6
Alberto Moreno <alberto.moreno@liferay.com>
2024-12-18 13:27:35 +0100

    LPD-44648 Create upgrade process to delete the states of the picklists from the databases

---
+++ modules/apps/headless/headless-builder/headless-builder-impl/src/main/java/com/liferay/headless/builder/internal/upgrade/v0_2_0/DeletePiklistObjectStatesUpgradeProcess.java
@@ -0,0 +1,97 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.headless.builder.internal.upgrade.v0_2_0;
+
+import com.liferay.list.type.model.ListTypeDefinition;
+import com.liferay.list.type.model.ListTypeEntry;
+import com.liferay.list.type.service.ListTypeDefinitionLocalService;
+import com.liferay.list.type.service.ListTypeEntryLocalService;
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.model.ObjectField;
+import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.object.service.ObjectFieldLocalService;
+import com.liferay.object.service.ObjectStateFlowLocalService;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+
+/**
+ * @author Alberto Javier Moreno Lage
+ */
+public class DeletePiklistObjectStatesUpgradeProcess extends UpgradeProcess {
+
+	public DeletePiklistObjectStatesUpgradeProcess(
+		CompanyLocalService companyLocalService,
+		ListTypeDefinitionLocalService listTypeDefinitionLocalService,
+		ListTypeEntryLocalService listTypeEntryLocalService,
+		ObjectDefinitionLocalService objectDefinitionLocalService,
+		ObjectFieldLocalService objectFieldLocalService,
+		ObjectStateFlowLocalService objectStateFlowLocalService) {
+
+		_companyLocalService = companyLocalService;
+		_listTypeDefinitionLocalService = listTypeDefinitionLocalService;
+		_listTypeEntryLocalService = listTypeEntryLocalService;
+		_objectDefinitionLocalService = objectDefinitionLocalService;
+		_objectFieldLocalService = objectFieldLocalService;
+		_objectStateFlowLocalService = objectStateFlowLocalService;
+	}
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		_companyLocalService.forEachCompanyId(
+			companyId -> {
+				ObjectDefinition objectDefinition =
+					_objectDefinitionLocalService.
+						fetchObjectDefinitionByExternalReferenceCode(
+							"L_API_APPLICATION", companyId);
+
+				if (objectDefinition == null) {
+					return;
+				}
+
+				ObjectField applicationStatusObjectField =
+					_objectFieldLocalService.fetchObjectField(
+						"APPLICATION_STATUS",
+						objectDefinition.getObjectDefinitionId());
+
+				_objectStateFlowLocalService.deleteObjectFieldObjectStateFlow(
+					applicationStatusObjectField.getObjectFieldId());
+
+				ListTypeDefinition applicationStatusListTypeDefinition =
+					_listTypeDefinitionLocalService.
+						fetchListTypeDefinitionByExternalReferenceCode(
+							"APPLICATION_STATUS_PICKLIST", companyId);
+
+				applicationStatusListTypeDefinition.setName(
+					"Application Status");
+
+				ListTypeEntry publishedListTypeEntry =
+					_listTypeEntryLocalService.getListTypeEntry(
+						applicationStatusListTypeDefinition.
+							getListTypeDefinitionId(),
+						"published");
+
+				publishedListTypeEntry.setExternalReferenceCode("PUBLISHED");
+
+				ListTypeEntry unpublishedListTypeEntry =
+					_listTypeEntryLocalService.getListTypeEntry(
+						applicationStatusListTypeDefinition.
+							getListTypeDefinitionId(),
+						"unpublished");
+
+				unpublishedListTypeEntry.setExternalReferenceCode(
+					"UNPUBLISHED");
+			});
+	}
+
+	private final CompanyLocalService _companyLocalService;
+	private final ListTypeDefinitionLocalService
+		_listTypeDefinitionLocalService;
+	private final ListTypeEntryLocalService _listTypeEntryLocalService;
+	private final ObjectDefinitionLocalService _objectDefinitionLocalService;
+	private final ObjectFieldLocalService _objectFieldLocalService;
+	private final ObjectStateFlowLocalService _objectStateFlowLocalService;
+
+}

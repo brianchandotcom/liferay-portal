3f8e25a634f4814aaa70d8469304df7cb387bdc2
Mikel Lorza <mikel.lorza@liferay.com>
2024-12-23 12:12:21 +0100

    LPD-43391 Moving from asset.util to util

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/util/JournalArticleLayoutClassedModelUsageActionMenuContributor.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/util/JournalArticleLayoutClassedModelUsageActionMenuContributor.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.journal.web.internal.asset.util;
+package com.liferay.journal.web.internal.util;
 
 import com.liferay.asset.kernel.model.AssetRendererFactory;
 import com.liferay.fragment.model.FragmentEntryLink;

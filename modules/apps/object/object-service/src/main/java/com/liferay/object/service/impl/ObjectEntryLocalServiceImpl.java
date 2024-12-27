e8292bcf57ca5b16926e308771e6074a3d6c7b3c
Mauricio Valdivia <mauricio.valdivia@liferay.com>
2024-12-27 13:27:10 +0100

    LPD-42559 feat validate business relationship

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -5125,7 +5125,7 @@ public class ObjectEntryLocalServiceImpl
 			throw new ObjectEntryValuesException.Required(
 				objectField.getName());
 		}
-		else if (StringUtil.equals(
+ 		else if (StringUtil.equals(
 					objectField.getBusinessType(),
 					ObjectFieldConstants.BUSINESS_TYPE_ATTACHMENT)) {
 
@@ -5194,6 +5194,11 @@ public class ObjectEntryLocalServiceImpl
 		else if (StringUtil.equals(
 					objectField.getBusinessType(),
 					ObjectFieldConstants.BUSINESS_TYPE_RELATIONSHIP)) {
+
+			if (Validator.isNull(GetterUtil.getLong(value)) && objectField.isRequired()){
+				throw new ObjectEntryValuesException.Required(
+					objectField.getName());
+			}
 
 			if (!objectDefinition.isAccountEntryRestricted() ||
 				!Objects.equals(

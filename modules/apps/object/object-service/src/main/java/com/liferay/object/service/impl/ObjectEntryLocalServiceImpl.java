85a9b9d188a3c376fbee28d5cc725eaeb79568a9
Mauricio Valdivia <mauricio.valdivia@liferay.com>
2025-01-02 16:39:00 +0100

    LPD-42559 sf

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -5125,7 +5125,7 @@ public class ObjectEntryLocalServiceImpl
 			throw new ObjectEntryValuesException.Required(
 				objectField.getName());
 		}
- 		else if (StringUtil.equals(
+		else if (StringUtil.equals(
 					objectField.getBusinessType(),
 					ObjectFieldConstants.BUSINESS_TYPE_ATTACHMENT)) {
 
@@ -5195,7 +5195,9 @@ public class ObjectEntryLocalServiceImpl
 					objectField.getBusinessType(),
 					ObjectFieldConstants.BUSINESS_TYPE_RELATIONSHIP)) {
 
-			if (Validator.isNull(GetterUtil.getLong(value)) && objectField.isRequired()){
+			if (Validator.isNull(GetterUtil.getLong(value)) &&
+				objectField.isRequired()) {
+
 				throw new ObjectEntryValuesException.Required(
 					objectField.getName());
 			}

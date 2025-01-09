bd0a10feaea587df4bc3dde450076b239e7b8ecb
David Truong <david.truong@liferay.com>
2025-01-08 21:46:18 -0600

    LPD-45587 build service

---
+++ modules/apps/change-tracking/change-tracking-api/src/main/java/com/liferay/change/tracking/service/persistence/CTScoreFinder.java
@@ -0,0 +1,20 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.change.tracking.service.persistence;
+
+import org.osgi.annotation.versioning.ProviderType;
+
+/**
+ * @author Brian Wing Shun Chan
+ * @generated
+ */
+@ProviderType
+public interface CTScoreFinder {
+
+	public com.liferay.change.tracking.model.CTScore updateScore(
+		long ctCollectionId, int score);
+
+}

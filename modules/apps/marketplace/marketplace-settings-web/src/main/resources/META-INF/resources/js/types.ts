91e69933b19fc3b25331a2d14c1effb8ce25ac5c
Keven Leone <keven.leone@liferay.com>
2025-01-13 15:01:23 -0300

    LPD-34740 Create Marketplace Connection flow using OAuth2

---
+++ modules/apps/marketplace/marketplace-settings-web/src/main/resources/META-INF/resources/js/types.ts
@@ -0,0 +1,30 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+export type AccountBrief = {id: string; logoURL?: string; name: string};
+
+export type Authorization = {
+	authorized: boolean;
+	data: null | {
+		serviceURL: string;
+		settings: any;
+		url: string;
+	};
+	loading: boolean;
+};
+
+export type MyUserAccount = {
+	accountBriefs: AccountBrief[];
+	id: number;
+	name: string;
+};
+
+export type MarketplaceSettingsProps = {
+	baseResourceURL: string;
+	clientId: string;
+	portletNamespace: string;
+	redirect: string;
+	url: string;
+};

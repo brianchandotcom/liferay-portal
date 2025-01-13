68cf212273c886998b5f13e3c7737a1836ddf858
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2025-01-13 10:32:33 +0100

    LPD-42587 SF Auto

--- modules/apps/layout/layout-utility-page-api/src/main/java/com/liferay/layout/utility/page/exception/DefaultLayoutUtilityPageEntryException.java
+++ modules/apps/layout/layout-utility-page-api/src/main/java/com/liferay/layout/utility/page/exception/DefaultLayoutUtilityPageEntryException.java
@@ -2,6 +2,7 @@
  * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
+
 package com.liferay.layout.utility.page.exception;
 
 import com.liferay.portal.kernel.exception.PortalException;
@@ -18,7 +19,9 @@ public class DefaultLayoutUtilityPageEntryException extends PortalException {
 		super(msg);
 	}
 
-	public DefaultLayoutUtilityPageEntryException(String msg, Throwable throwable) {
+	public DefaultLayoutUtilityPageEntryException(
+		String msg, Throwable throwable) {
+
 		super(msg, throwable);
 	}
 

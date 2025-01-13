505499a76a559031b6e58b39f29c4a42d7b1b54a
Keven Leone <keven.leone@liferay.com>
2025-01-13 14:58:49 -0300

    LPD-34740 Create Action Commands to handle Marketplace Authorization, Connection and Disconnect

---
+++ modules/apps/marketplace/marketplace-settings-web/src/main/java/com/liferay/marketplace/settings/web/internal/portlet/action/ConnectMVCResourceCommand.java
@@ -0,0 +1,76 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.marketplace.settings.web.internal.portlet.action;
+
+import com.liferay.configuration.admin.constants.ConfigurationAdminPortletKeys;
+import com.liferay.marketplace.settings.web.internal.model.Authorization;
+import com.liferay.marketplace.settings.web.internal.model.Payload;
+import com.liferay.marketplace.settings.web.internal.util.MarketplaceHttpUtil;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCResourceCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Keven Leone
+ */
+@Component(
+	property = {
+		"javax.portlet.name=" + ConfigurationAdminPortletKeys.INSTANCE_SETTINGS,
+		"mvc.command.name=/marketplace_settings/connect"
+	},
+	service = MVCResourceCommand.class
+)
+public class ConnectMVCResourceCommand extends BaseMVCResourceCommand {
+
+	@Override
+	protected void doServeResource(
+			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)resourceRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		PermissionChecker permissionChecker =
+			themeDisplay.getPermissionChecker();
+
+		if (!permissionChecker.isCompanyAdmin(themeDisplay.getCompanyId())) {
+			throw new PrincipalException.MustBeCompanyAdmin(permissionChecker);
+		}
+
+		Authorization authorization = MarketplaceHttpUtil.exchangeToken(
+			themeDisplay.getCompanyId(),
+			new Payload(
+				ParamUtil.getString(resourceRequest, "code"),
+				ParamUtil.getString(resourceRequest, "codeVerifier"),
+				ParamUtil.getString(resourceRequest, "serviceURL"),
+				ParamUtil.getString(resourceRequest, "settings")),
+			null);
+
+		JSONPortletResponseUtil.writeJSON(
+			resourceRequest, resourceResponse,
+			_jsonFactory.createJSONObject(
+			).put(
+				"success", Validator.isNotNull(authorization.accessToken)
+			));
+	}
+
+	@Reference
+	private JSONFactory _jsonFactory;
+
+}
---
+++ modules/apps/marketplace/marketplace-settings-web/src/main/java/com/liferay/marketplace/settings/web/internal/portlet/action/GetAuthorizationMVCResourceCommand.java
@@ -0,0 +1,87 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.marketplace.settings.web.internal.portlet.action;
+
+import com.liferay.configuration.admin.constants.ConfigurationAdminPortletKeys;
+import com.liferay.marketplace.settings.web.internal.model.Authorization;
+import com.liferay.marketplace.settings.web.internal.model.Payload;
+import com.liferay.marketplace.settings.web.internal.util.MarketplaceHttpUtil;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCResourceCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.PrefsPropsUtil;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Keven Leone
+ */
+@Component(
+	property = {
+		"javax.portlet.name=" + ConfigurationAdminPortletKeys.INSTANCE_SETTINGS,
+		"mvc.command.name=/marketplace_settings/get_authorization"
+	},
+	service = MVCResourceCommand.class
+)
+public class GetAuthorizationMVCResourceCommand extends BaseMVCResourceCommand {
+
+	@Override
+	protected void doServeResource(
+			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)resourceRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		Authorization authorization = new Authorization(
+			PrefsPropsUtil.getString(
+				themeDisplay.getCompanyId(),
+				PropsKeys.MARKETPLACE_ACCESS_TOKEN),
+			PrefsPropsUtil.getLong(
+				themeDisplay.getCompanyId(),
+				PropsKeys.MARKETPLACE_ACCESS_TOKEN_EXPIRES_IN));
+
+		if (System.currentTimeMillis() > authorization.expiresIn) {
+			authorization = MarketplaceHttpUtil.exchangeToken(
+				themeDisplay.getCompanyId(),
+				new Payload(
+					PrefsPropsUtil.getString(
+						themeDisplay.getCompanyId(),
+						PropsKeys.MARKETPLACE_CODE),
+					null,
+					PrefsPropsUtil.getString(
+						themeDisplay.getCompanyId(),
+						PropsKeys.MARKETPLACE_SERVICE_URL),
+					PrefsPropsUtil.getString(
+						themeDisplay.getCompanyId(),
+						PropsKeys.MARKETPLACE_SETTINGS)),
+				PrefsPropsUtil.getString(
+					themeDisplay.getCompanyId(),
+					PropsKeys.MARKETPLACE_REFRESH_TOKEN));
+		}
+
+		JSONPortletResponseUtil.writeJSON(
+			resourceRequest, resourceResponse,
+			_jsonFactory.createJSONObject(
+			).put(
+				"accessToken", authorization.accessToken
+			).put(
+				"expiresIn", authorization.expiresIn
+			));
+	}
+
+	@Reference
+	private JSONFactory _jsonFactory;
+
+}

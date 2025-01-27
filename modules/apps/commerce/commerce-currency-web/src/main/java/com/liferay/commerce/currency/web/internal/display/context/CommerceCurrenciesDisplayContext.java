c881c15eeaac771ea686e17564b42a6644a78d1f
Brian Chan <brian.chan@liferay.com>
2025-01-27 22:30:23 +0800

    LPD-45133 SF

--- modules/apps/commerce/commerce-currency-web/src/main/java/com/liferay/commerce/currency/web/internal/display/context/CommerceCurrenciesDisplayContext.java
+++ modules/apps/commerce/commerce-currency-web/src/main/java/com/liferay/commerce/currency/web/internal/display/context/CommerceCurrenciesDisplayContext.java
@@ -87,6 +87,7 @@ public class CommerceCurrenciesDisplayContext {
 			renderRequest);
 		_liferayPortletResponse = PortalUtil.getLiferayPortletResponse(
 			renderResponse);
+
 		_httpServletRequest = _liferayPortletRequest.getHttpServletRequest();
 	}
 

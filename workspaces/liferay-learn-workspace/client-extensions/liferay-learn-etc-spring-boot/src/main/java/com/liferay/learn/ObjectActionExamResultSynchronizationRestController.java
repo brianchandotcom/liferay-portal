6322780adfc9db01e11dfd33a63b528ea9ad0e45
Gregory Amerson <gregory.amerson@liferay.com>
2025-01-27 15:09:04 -0600

    LCD-44249 update usages springboot3

--- workspaces/liferay-learn-workspace/client-extensions/liferay-learn-etc-spring-boot/src/main/java/com/liferay/learn/ObjectActionExamResultSynchronizationRestController.java
+++ workspaces/liferay-learn-workspace/client-extensions/liferay-learn-etc-spring-boot/src/main/java/com/liferay/learn/ObjectActionExamResultSynchronizationRestController.java
@@ -5,8 +5,8 @@
 
 package com.liferay.learn;
 
-import com.liferay.client.extension.util.spring.boot2.BaseRestController;
-import com.liferay.client.extension.util.spring.boot2.LiferayOAuth2AccessTokenManager;
+import com.liferay.client.extension.util.spring.boot3.BaseRestController;
+import com.liferay.client.extension.util.spring.boot3.LiferayOAuth2AccessTokenManager;
 import com.liferay.petra.string.StringBundler;
 
 import java.time.OffsetDateTime;

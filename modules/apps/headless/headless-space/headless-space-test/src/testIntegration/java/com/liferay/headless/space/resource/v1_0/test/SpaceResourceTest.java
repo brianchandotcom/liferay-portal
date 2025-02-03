4574e0ff3dd5929e780df45b3cfe974d0e5bb547
Roberto Díaz <roberto.diaz@liferay.com>
2025-02-04 00:32:52 +0100

    LPD-46642 Override test methods

---
+++ modules/apps/headless/headless-space/headless-space-test/src/testIntegration/java/com/liferay/headless/space/resource/v1_0/test/SpaceResourceTest.java
@@ -0,0 +1,171 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.headless.space.resource.v1_0.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.depot.model.DepotEntry;
+import com.liferay.depot.service.DepotEntryGroupRelLocalService;
+import com.liferay.depot.service.DepotEntryLocalService;
+import com.liferay.headless.space.client.dto.v1_0.Space;
+import com.liferay.headless.space.client.problem.Problem;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.test.rule.Inject;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Roberto Díaz
+ */
+@RunWith(Arquillian.class)
+public class SpaceResourceTest extends BaseSpaceResourceTestCase {
+
+	@Before
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+	}
+
+	@After
+	@Override
+	public void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	@Override
+	@Test
+	public void testDeleteSpace() throws Exception {
+		super.testDeleteSpace();
+
+		// Nonexistent space ID
+
+		long siteId = RandomTestUtil.randomLong();
+
+		try {
+			spaceResource.deleteSpace(siteId);
+
+			Assert.fail();
+		}
+		catch (Problem.ProblemException problemException) {
+			Problem problem = problemException.getProblem();
+
+			Assert.assertEquals("NOT_FOUND", problem.getStatus());
+			Assert.assertNull(problem.getTitle());
+		}
+	}
+
+	@Override
+	@Test
+	public void testDeleteSpaceLinkToSite() throws Exception {
+		Space space = testDeleteSpaceLinkToSite_addSpace();
+
+		_assertLinkedSites(space);
+
+		space = spaceResource.deleteSpaceLinkToSite(
+			space.getId(), testGroup.getGroupId());
+
+		Assert.assertTrue(ArrayUtil.isEmpty(space.getLinkedSiteIds()));
+		Assert.assertTrue(
+			ArrayUtil.isEmpty(space.getLinkedSitesExternalReferenceCodes()));
+	}
+
+	@Override
+	@Test
+	public void testPatchSpace() throws Exception {
+		super.testPatchSpace();
+	}
+
+	@Override
+	@Test
+	public void testPostSpaceLinkToSite() throws Exception {
+		Space space = testPostSpaceLinkToSite_addSpace(randomSpace());
+
+		_assertLinkedSites(space);
+	}
+
+	@Override
+	protected Space randomPatchSpace() throws Exception {
+		Space space = randomSpace();
+
+		space.setName(RandomTestUtil.randomString());
+
+		return space;
+	}
+
+	@Override
+	protected Space testDeleteSpace_addSpace() throws Exception {
+		return _addRandomSpace();
+	}
+
+	@Override
+	protected Space testDeleteSpaceLinkToSite_addSpace() throws Exception {
+		Space space = _addRandomSpace();
+
+		DepotEntry depotEntry = _depotEntryLocalService.getGroupDepotEntry(
+			space.getId());
+
+		_depotEntryGroupRelLocalService.addDepotEntryGroupRel(
+			depotEntry.getDepotEntryId(), testGroup.getGroupId());
+
+		return spaceResource.getSpace(space.getId());
+	}
+
+	@Override
+	protected Space testGetSpace_addSpace() throws Exception {
+		return _addRandomSpace();
+	}
+
+	@Override
+	protected Space testPatchSpace_addSpace() throws Exception {
+		return _addRandomSpace();
+	}
+
+	@Override
+	protected Space testPostSpace_addSpace(Space space) throws Exception {
+		return spaceResource.postSpace(space);
+	}
+
+	@Override
+	protected Space testPostSpaceLinkToSite_addSpace(Space space)
+		throws Exception {
+
+		space = spaceResource.postSpace(space);
+
+		DepotEntry depotEntry = _depotEntryLocalService.getGroupDepotEntry(
+			space.getId());
+
+		_depotEntryGroupRelLocalService.addDepotEntryGroupRel(
+			depotEntry.getDepotEntryId(), testGroup.getGroupId());
+
+		return spaceResource.getSpace(space.getId());
+	}
+
+	private Space _addRandomSpace() throws Exception {
+		return spaceResource.postSpace(randomSpace());
+	}
+
+	private void _assertLinkedSites(Space space) {
+		Long[] linkedSiteIds = space.getLinkedSiteIds();
+		String[] linkedSitesExternalReferenceCodes =
+			space.getLinkedSitesExternalReferenceCodes();
+
+		Assert.assertEquals(testGroup.getGroupId(), (long)linkedSiteIds[0]);
+		Assert.assertEquals(
+			testGroup.getExternalReferenceCode(),
+			linkedSitesExternalReferenceCodes[0]);
+	}
+
+	@Inject
+	private DepotEntryGroupRelLocalService _depotEntryGroupRelLocalService;
+
+	@Inject
+	private DepotEntryLocalService _depotEntryLocalService;
+
+}

f16a40789e5bb04dbded79967bd30b51b1f4891e
Iván Zaera Avellón <ivan.zaera@liferay.com>
2025-02-04 12:49:54 +0100

    LPD-47736 Why use Liferay when you can use ESM?
    As per https://github.com/liferay-frontend/liferay-portal/pull/4709#discussion_r1940862937

--- modules/apps/frontend-js/frontend-js-spa-web/src/main/resources/META-INF/resources/util/openToast.js
+++ modules/apps/frontend-js/frontend-js-spa-web/src/main/resources/META-INF/resources/util/openToast.js
@@ -3,6 +3,8 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import {getSpritemap} from '@liferay/frontend-icons-web';
+
 /**
  * This is an alternative lightweight implementation of openToast() method of
  * frontend-js-web.
@@ -27,7 +29,7 @@ const ICON_MAP = {
 };
 
 export function openToast(type, title, message) {
-	const {spritemap} = Liferay.Icons;
+	const spritemap = getSpritemap();
 
 	const html = `
 <div class="alert-container cadmin container">

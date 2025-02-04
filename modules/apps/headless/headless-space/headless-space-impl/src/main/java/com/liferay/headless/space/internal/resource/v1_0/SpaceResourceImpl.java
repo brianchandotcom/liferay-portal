40d023a20034ab45742485a19e37e1248691ee48
Roberto Díaz <roberto.diaz@liferay.com>
2025-02-04 12:31:31 +0100

    LPD-46642 Implement delete, get and patch

--- modules/apps/headless/headless-space/headless-space-impl/src/main/java/com/liferay/headless/space/internal/resource/v1_0/SpaceResourceImpl.java
+++ modules/apps/headless/headless-space/headless-space-impl/src/main/java/com/liferay/headless/space/internal/resource/v1_0/SpaceResourceImpl.java
@@ -5,16 +5,26 @@
 
 package com.liferay.headless.space.internal.resource.v1_0;
 
+import com.liferay.depot.model.DepotAppCustomization;
 import com.liferay.depot.model.DepotEntry;
+import com.liferay.depot.service.DepotAppCustomizationLocalService;
 import com.liferay.depot.service.DepotEntryService;
 import com.liferay.headless.space.dto.v1_0.Space;
 import com.liferay.headless.space.resource.v1_0.SpaceResource;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextFactory;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.vulcan.dto.converter.DTOConverter;
 import com.liferay.portal.vulcan.dto.converter.DTOConverterRegistry;
 import com.liferay.portal.vulcan.dto.converter.DefaultDTOConverterContext;
 import com.liferay.portal.vulcan.util.LocalizedMapUtil;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -30,6 +40,51 @@ import org.osgi.service.component.annotations.ServiceScope;
 public class SpaceResourceImpl extends BaseSpaceResourceImpl {
 
 	@Override
+	public void deleteSpace(Long spaceId) throws Exception {
+		DepotEntry depotEntry = _depotEntryService.getDepotEntry(spaceId);
+
+		_depotEntryService.deleteDepotEntry(depotEntry.getDepotEntryId());
+	}
+
+	@Override
+	public Space getSpace(Long spaceId) throws Exception {
+		return _toSpace(_depotEntryService.getDepotEntry(spaceId));
+	}
+
+	@Override
+	public Space patchSpace(Long spaceId, Space space) throws Exception {
+		DepotEntry depotEntry = _depotEntryService.getDepotEntry(spaceId);
+
+		Group group = depotEntry.getGroup();
+
+		List<DepotAppCustomization> depotAppCustomizations =
+			_depotAppCustomizationLocalService.getDepotAppCustomizations(
+				depotEntry.getDepotEntryId());
+
+		Map<String, Boolean> depotAppCustomizationMap = new HashMap<>();
+
+		for (DepotAppCustomization depotAppCustomization :
+				depotAppCustomizations) {
+
+			depotAppCustomizationMap.put(
+				depotAppCustomization.getPortletId(),
+				depotAppCustomization.isEnabled());
+		}
+
+		return _toSpace(
+			_depotEntryService.updateDepotEntry(
+				depotEntry.getDepotEntryId(),
+				LocalizedMapUtil.getLocalizedMap(
+					contextAcceptLanguage.getPreferredLocale(), space.getName(),
+					space.getName_i18n()),
+				LocalizedMapUtil.getLocalizedMap(
+					contextAcceptLanguage.getPreferredLocale(),
+					space.getDescription(), space.getDescription_i18n()),
+				depotAppCustomizationMap, group.getTypeSettingsProperties(),
+				_getServiceContext()));
+	}
+
+	@Override
 	public Space postSpace(Space space) throws Exception {
 		return _toSpace(
 			_depotEntryService.addDepotEntry(
@@ -39,8 +94,16 @@ public class SpaceResourceImpl extends BaseSpaceResourceImpl {
 				LocalizedMapUtil.getLocalizedMap(
 					contextAcceptLanguage.getPreferredLocale(),
 					space.getDescription(), space.getDescription_i18n()),
-				ServiceContextFactory.getInstance(
-					DepotEntry.class.getName(), contextHttpServletRequest)));
+				_getServiceContext()));
+	}
+
+	private ServiceContext _getServiceContext() throws Exception {
+		ServiceContext serviceContext = ServiceContextFactory.getInstance(
+			DepotEntry.class.getName(), contextHttpServletRequest);
+
+		serviceContext.setModifiedDate(new Date());
+
+		return serviceContext;
 	}
 
 	private Space _toSpace(DepotEntry depotEntry) throws Exception {
@@ -49,11 +112,23 @@ public class SpaceResourceImpl extends BaseSpaceResourceImpl {
 				contextAcceptLanguage.isAcceptAllLanguages(),
 				HashMapBuilder.put(
 					"create", addAction("add", depotEntry, "postSpace")
+				).put(
+					"delete",
+					addAction(ActionKeys.DELETE, depotEntry, "deleteSpace")
+				).put(
+					"get", addAction(ActionKeys.VIEW, depotEntry, "getSpace")
+				).put(
+					"update",
+					addAction(ActionKeys.UPDATE, depotEntry, "patchSpace")
 				).build(),
 				_dtoConverterRegistry, depotEntry.getDepotEntryId(),
 				contextAcceptLanguage.getPreferredLocale(), contextUriInfo,
 				contextUser));
 	}
+
+	@Reference
+	private DepotAppCustomizationLocalService
+		_depotAppCustomizationLocalService;
 
 	@Reference
 	private DepotEntryService _depotEntryService;

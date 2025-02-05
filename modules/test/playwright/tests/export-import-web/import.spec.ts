627bf51e716c8daa16cdcc7ef76616d05bda1292
Alberto Moreno <alberto.moreno@liferay.com>
2025-02-05 16:51:39 +0100

    LPD-47216 Add test for checking the new default name for exporting through Export-Import

--- modules/test/playwright/tests/export-import-web/import.spec.ts
+++ modules/test/playwright/tests/export-import-web/import.spec.ts
@@ -420,6 +420,7 @@ test('can import custom object entries at instance level with or without permiss
 
 	const exportFilePath = await companyExportImportPage.export(
 		'Tests 1 Items',
+		false,
 		true
 	);
 

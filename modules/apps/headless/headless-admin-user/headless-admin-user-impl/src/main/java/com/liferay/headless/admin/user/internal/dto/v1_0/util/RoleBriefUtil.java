97f734db9f4506d5e9cd815ec4b71ae1f6a9f63c
brbalazs <balazs.breier@gmail.com>
2025-02-10 14:44:12 +0100

    LPD-48440 Adjustment to RoleBriefUtil

--- modules/apps/headless/headless-admin-user/headless-admin-user-impl/src/main/java/com/liferay/headless/admin/user/internal/dto/v1_0/util/RoleBriefUtil.java
+++ modules/apps/headless/headless-admin-user/headless-admin-user-impl/src/main/java/com/liferay/headless/admin/user/internal/dto/v1_0/util/RoleBriefUtil.java
@@ -7,18 +7,40 @@ package com.liferay.headless.admin.user.internal.dto.v1_0.util;
 
 import com.liferay.headless.admin.user.dto.v1_0.RoleBrief;
 import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.vulcan.dto.converter.DTOConverterContext;
+import com.liferay.portal.vulcan.util.LocalizedMapUtil;
 
 /**
  * @author Crescenzo Rega
  */
 public class RoleBriefUtil {
 
-	public static RoleBrief toRoleBrief(Role role) {
+	public static RoleBrief toRoleBrief(
+		DTOConverterContext dtoConverterContext, Role role) {
+
 		return new RoleBrief() {
 			{
 				setExternalReferenceCode(role::getExternalReferenceCode);
 				setId(role::getRoleId);
-				setName(role::getName);
+				setName(
+					() -> {
+						if (dtoConverterContext == null) {
+							return role.getName();
+						}
+
+						return role.getTitle(dtoConverterContext.getLocale());
+					});
+				setName_i18n(
+					() -> {
+						if (dtoConverterContext == null) {
+							return null;
+						}
+
+						return LocalizedMapUtil.getI18nMap(
+							dtoConverterContext.isAcceptAllLanguages(),
+							role.getTitleMap());
+					});
+				setRoleType(role::getType);
 			}
 		};
 	}

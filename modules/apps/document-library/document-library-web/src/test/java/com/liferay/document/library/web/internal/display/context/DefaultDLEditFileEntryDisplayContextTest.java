a5fc3e62aab4c10e8fb676609579616c81bb9fc1
Mikel Lorza <mikel.lorza@liferay.com>
2025-02-10 18:14:17 +0100

    LPD-48521 Add tests

---
+++ modules/apps/document-library/document-library-web/src/test/java/com/liferay/document/library/web/internal/display/context/DefaultDLEditFileEntryDisplayContextTest.java
@@ -0,0 +1,121 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.document.library.web.internal.display.context;
+
+import com.liferay.document.library.kernel.model.DLFileEntryType;
+import com.liferay.document.library.kernel.util.DLValidator;
+import com.liferay.document.library.web.internal.display.context.helper.FileEntryDisplayContextHelper;
+import com.liferay.dynamic.data.mapping.form.values.factory.DDMFormValuesFactory;
+import com.liferay.dynamic.data.mapping.storage.DDMStorageEngineManager;
+import com.liferay.portal.bean.BeanPropertiesImpl;
+import com.liferay.portal.configuration.module.configuration.ConfigurationProvider;
+import com.liferay.portal.kernel.bean.BeanPropertiesUtil;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import org.mockito.Mockito;
+
+import org.springframework.mock.web.MockHttpServletRequest;
+
+/**
+ * @author Mikel Lorza
+ */
+public class DefaultDLEditFileEntryDisplayContextTest {
+
+	@ClassRule
+	public static LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	@Before
+	public void setUp() {
+		BeanPropertiesUtil beanPropertiesUtil = new BeanPropertiesUtil();
+
+		beanPropertiesUtil.setBeanProperties(new BeanPropertiesImpl());
+
+		_configurationProvider = Mockito.mock(ConfigurationProvider.class);
+
+		_ddmFormValuesFactory = Mockito.mock(DDMFormValuesFactory.class);
+
+		_ddmStorageEngineManager = Mockito.mock(DDMStorageEngineManager.class);
+
+		_dlFileEntryType = Mockito.mock(DLFileEntryType.class);
+
+		_dlValidator = Mockito.mock(DLValidator.class);
+	}
+
+	@Test
+	public void testIsPublishButtonDisabled() throws Exception {
+		DefaultDLEditFileEntryDisplayContext
+			defaultDLEditFileEntryDisplayContext =
+				_getDefaultDLEditFileEntryDisplayContext();
+
+		FileEntryDisplayContextHelper fileEntryDisplayContextHelper =
+			Mockito.mock(FileEntryDisplayContextHelper.class);
+
+		Mockito.when(
+			fileEntryDisplayContextHelper.isCheckedOutByOther()
+		).thenReturn(
+			false
+		);
+
+		ReflectionTestUtil.setFieldValue(
+			defaultDLEditFileEntryDisplayContext,
+			"_fileEntryDisplayContextHelper", fileEntryDisplayContextHelper);
+
+		Assert.assertFalse(
+			defaultDLEditFileEntryDisplayContext.isPublishButtonDisabled());
+
+		Mockito.when(
+			fileEntryDisplayContextHelper.isCheckedOutByOther()
+		).thenReturn(
+			true
+		);
+
+		ReflectionTestUtil.setFieldValue(
+			defaultDLEditFileEntryDisplayContext,
+			"_fileEntryDisplayContextHelper", fileEntryDisplayContextHelper);
+
+		Assert.assertTrue(
+			defaultDLEditFileEntryDisplayContext.isPublishButtonDisabled());
+	}
+
+	private DefaultDLEditFileEntryDisplayContext
+		_getDefaultDLEditFileEntryDisplayContext() {
+
+		ThemeDisplay themeDisplay = Mockito.mock(ThemeDisplay.class);
+
+		Mockito.when(
+			themeDisplay.getCompanyId()
+		).thenReturn(
+			123456L
+		);
+
+		MockHttpServletRequest mockHttpServletRequest =
+			new MockHttpServletRequest();
+
+		mockHttpServletRequest.setAttribute(
+			WebKeys.THEME_DISPLAY, themeDisplay);
+
+		return new DefaultDLEditFileEntryDisplayContext(
+			_configurationProvider, _ddmFormValuesFactory,
+			_ddmStorageEngineManager, _dlFileEntryType, _dlValidator,
+			mockHttpServletRequest);
+	}
+
+	private ConfigurationProvider _configurationProvider;
+	private DDMFormValuesFactory _ddmFormValuesFactory;
+	private DDMStorageEngineManager _ddmStorageEngineManager;
+	private DLFileEntryType _dlFileEntryType;
+	private DLValidator _dlValidator;
+
+}

7c293b77693da9bab948835f31c957e8138ae97c
Mumen Tayyem <mumen_tayyem@outlook.com>
2025-02-12 10:32:52 +0400

    LPD-45276 SF

--- modules/apps/site-initializer/site-initializer-teaser-showcase/src/main/resources/site-initializer/fragments/group/tracking/fragments/interest/index.js
+++ modules/apps/site-initializer/site-initializer-teaser-showcase/src/main/resources/site-initializer/fragments/group/tracking/fragments/interest/index.js
@@ -1,8 +1,13 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
 function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/;secure";
+	const d = new Date();
+	d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+	const expires = 'expires=' + d.toUTCString();
+	document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/;secure';
 }
 
-setCookie('interest', 'products', 1)
+setCookie('interest', 'products', 1);
--- modules/apps/site-initializer/site-initializer-teaser-showcase/src/main/resources/site-initializer/fragments/group/tracking/fragments/remove-interest/index.js
+++ modules/apps/site-initializer/site-initializer-teaser-showcase/src/main/resources/site-initializer/fragments/group/tracking/fragments/remove-interest/index.js
@@ -1,21 +1,28 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
 function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return "";
+	const name = cname + '=';
+	const decodedCookie = decodeURIComponent(document.cookie);
+	const ca = decodedCookie.split(';');
+	for (let i = 0; i < ca.length; i++) {
+		let c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+
+	return '';
 }
 function eraseCookie(name) {
-    if( getCookie( name ) ) {
-      document.cookie = name + "=xxx; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT";
-  }
+	if (getCookie(name)) {
+		document.cookie =
+			name + '=xxx; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT';
+	}
 }
-eraseCookie('interest')
+eraseCookie('interest');

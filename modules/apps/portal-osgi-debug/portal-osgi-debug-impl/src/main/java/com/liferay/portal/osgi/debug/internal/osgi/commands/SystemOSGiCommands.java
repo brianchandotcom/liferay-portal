brianchandotcom/liferay-portal

commit 8f6306d05e2879a32e2e4315a253f41ae56ad72f

LPD-48736 Inline to avoid confusion

modules/apps/portal-osgi-debug/portal-osgi-debug-impl/src/main/java/com/liferay/portal/osgi/debug/internal/osgi/commands/SystemOSGiCommands.java
@@ -226,18 +226,6 @@ public class SystemOSGiCommands implements OSGiCommands {
 		}
 	}
 
-	private boolean _matches(Set<String> spiTypesSet, URL url) {
-		if (spiTypesSet.isEmpty()) {
-			return true;
-		}
-
-		String path = url.getPath();
-
-		int index = path.lastIndexOf('/');
-
-		return spiTypesSet.contains(path.substring(index + 1));
-	}
-
 	private void _listSPIProviders(
 			Bundle bundle, Enumeration<URL> enumeration,
 			Set<String> spiTypesSet)
@@ -249,7 +237,15 @@ public class SystemOSGiCommands implements OSGiCommands {
 			while (enumeration.hasMoreElements()) {
 				URL url = enumeration.nextElement();
 
-				if (_matches(spiTypesSet, url)) {
+				if (!spiTypesSet.isEmpty()) {
+					continue;
+				}
+
+				String path = url.getPath();
+
+				int index = path.lastIndexOf('/');
+
+				if (spiTypesSet.contains(path.substring(index + 1))) {
 					urls.add(url);
 				}
 			}

commit 3c031ecd61c326d791df88249e19ad786a946bdd

LPD-48736 Fix broken logic caused by 8f6306d0 , the inline changed logic, empty spiTypesSet means unconditionally adding the url to the list.

modules/apps/portal-osgi-debug/portal-osgi-debug-impl/src/main/java/com/liferay/portal/osgi/debug/internal/osgi/commands/SystemOSGiCommands.java
@@ -303,7 +303,9 @@ public class SystemOSGiCommands implements OSGiCommands {
 			while (enumeration.hasMoreElements()) {
 				URL url = enumeration.nextElement();
 
-				if (!spiTypesSet.isEmpty()) {
+				if (spiTypesSet.isEmpty()) {
+					urls.add(url);
+
 					continue;
 				}
 
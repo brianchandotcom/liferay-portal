2c8d50f24f5c12581f973d22936e6a6137d314c6
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2025-03-21 13:19:19 +0100

    LPD-51848 Use cell renderers in all asset views

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main/FDSPropsTransformer/ContentsFDSPropsTransformer.ts
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main/FDSPropsTransformer/ContentsFDSPropsTransformer.ts
@@ -3,8 +3,14 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import {IInternalRenderer} from '@liferay/frontend-data-set-web';
+
 import createAssetAction from './actions/createAssetAction';
 import createFolderAction from './actions/createFolderAction';
+import AuthorRenderer from './cell_renderers/AuthorRenderer';
+import NameRenderer from './cell_renderers/NameRenderer';
+import SpaceRenderer from './cell_renderers/SpaceRenderer';
+import TypeRenderer from './cell_renderers/TypeRenderer';
 import addOnClickToCreationMenuItems from './utils/addOnClickToCreationMenuItems';
 
 const ACTIONS = {
@@ -27,6 +33,30 @@ export default function ContentFDSPropsTransformer({
 				creationMenu.primaryItems,
 				ACTIONS
 			),
+		},
+		customRenderers: {
+			tableCell: [
+				{
+					component: AuthorRenderer,
+					name: 'authorTableCellRenderer',
+					type: 'internal',
+				} as IInternalRenderer,
+				{
+					component: NameRenderer,
+					name: 'nameTableCellRenderer',
+					type: 'internal',
+				} as IInternalRenderer,
+				{
+					component: SpaceRenderer,
+					name: 'spaceTableCellRenderer',
+					type: 'internal',
+				} as IInternalRenderer,
+				{
+					component: TypeRenderer,
+					name: 'typeTableCellRenderer',
+					type: 'internal',
+				} as IInternalRenderer,
+			],
 		},
 	};
 }

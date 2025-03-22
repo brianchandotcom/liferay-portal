909fe7d01b97bed730e96b6768bc93bdab63c032
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2025-03-22 13:16:32 +0100

    LPD-51848 Configuration is not used

--- modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/fragment/renderer/ViewVocabulariesFragmentRenderer.java
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/fragment/renderer/ViewVocabulariesFragmentRenderer.java
@@ -7,35 +7,27 @@ package com.liferay.site.cms.site.initializer.internal.fragment.renderer;
 
 import com.liferay.fragment.renderer.FragmentRenderer;
 import com.liferay.fragment.renderer.FragmentRendererContext;
-import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.WebKeys;
-import com.liferay.site.cms.site.initializer.internal.configuration.CMSSiteInitializerConfiguration;
 import com.liferay.site.cms.site.initializer.internal.display.context.ViewVocabulariesDisplayContext;
 
 import java.io.IOException;
 
 import java.util.Locale;
-import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Modified;
 import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Noor Najjar
  */
-@Component(
-	configurationPid = "com.liferay.site.cms.site.initializer.internal.configuration.CMSSiteInitializerConfiguration",
-	service = FragmentRenderer.class
-)
+@Component(service = FragmentRenderer.class)
 public class ViewVocabulariesFragmentRenderer
 	extends BaseSectionFragmentRenderer {
 
@@ -60,15 +52,11 @@ public class ViewVocabulariesFragmentRenderer
 			RequestDispatcher requestDispatcher =
 				_servletContext.getRequestDispatcher("/view_vocabularies.jsp");
 
-			ThemeDisplay themeDisplay =
-				(ThemeDisplay)httpServletRequest.getAttribute(
-					WebKeys.THEME_DISPLAY);
-
 			httpServletRequest.setAttribute(
 				ViewVocabulariesDisplayContext.class.getName(),
 				new ViewVocabulariesDisplayContext(
-					_cmsSiteInitializerConfiguration, httpServletRequest,
-					themeDisplay));
+					(ThemeDisplay)httpServletRequest.getAttribute(
+						WebKeys.THEME_DISPLAY)));
 
 			requestDispatcher.include(httpServletRequest, httpServletResponse);
 		}
@@ -76,16 +64,6 @@ public class ViewVocabulariesFragmentRenderer
 			throw new RuntimeException(exception);
 		}
 	}
-
-	@Activate
-	@Modified
-	protected void activate(Map<String, Object> properties) {
-		_cmsSiteInitializerConfiguration = ConfigurableUtil.createConfigurable(
-			CMSSiteInitializerConfiguration.class, properties);
-	}
-
-	private volatile CMSSiteInitializerConfiguration
-		_cmsSiteInitializerConfiguration;
 
 	@Reference
 	private Language _language;

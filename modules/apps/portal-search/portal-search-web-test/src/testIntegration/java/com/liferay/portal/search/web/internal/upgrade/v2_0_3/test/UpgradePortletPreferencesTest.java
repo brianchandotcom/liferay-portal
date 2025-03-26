b54b783116179002c0e9d5964f1000d511f1cb16
Joshua Cords <joshua.cords@liferay.com>
2025-03-26 11:14:47 -0700

    LPD-49259 portal-search-web-test: add upgrade test
    https://liferay.atlassian.net/browse/LPD-49259

--- modules/apps/portal-search/portal-search-web-test/src/testIntegration/java/com/liferay/portal/search/web/internal/upgrade/v2_0_3/test/UpgradePortletPreferencesTest.java
+++ modules/apps/portal-search/portal-search-web-test/src/testIntegration/java/com/liferay/portal/search/web/internal/upgrade/v2_0_3/test/UpgradePortletPreferencesTest.java
@@ -7,10 +7,20 @@ package com.liferay.portal.search.web.internal.upgrade.v2_0_3.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.portal.kernel.version.Version;
+import com.liferay.portal.search.web.constants.SearchBarPortletKeys;
+import com.liferay.portal.search.web.constants.SearchResultsPortletKeys;
 import com.liferay.portal.search.web.internal.category.facet.constants.CategoryFacetPortletKeys;
+import com.liferay.portal.search.web.internal.custom.facet.constants.CustomFacetPortletKeys;
+import com.liferay.portal.search.web.internal.custom.filter.constants.CustomFilterPortletKeys;
+import com.liferay.portal.search.web.internal.folder.facet.constants.FolderFacetPortletKeys;
+import com.liferay.portal.search.web.internal.modified.facet.constants.ModifiedFacetPortletKeys;
+import com.liferay.portal.search.web.internal.site.facet.constants.SiteFacetPortletKeys;
+import com.liferay.portal.search.web.internal.sort.constants.SortPortletKeys;
+import com.liferay.portal.search.web.internal.tag.facet.constants.TagFacetPortletKeys;
+import com.liferay.portal.search.web.internal.type.facet.constants.TypeFacetPortletKeys;
+import com.liferay.portal.search.web.internal.user.facet.constants.UserFacetPortletKeys;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
-import com.liferay.portlet.display.template.test.util.BaseUpgradePortletPreferencesTestCase;
 
 import org.junit.runner.RunWith;
 
@@ -22,8 +32,21 @@ public class UpgradePortletPreferencesTest
 	extends BaseUpgradePortletPreferencesTestCase {
 
 	@Override
-	protected String getPortletId() {
-		return CategoryFacetPortletKeys.CATEGORY_FACET;
+	protected String[] getPortletIds() {
+		return new String[] {
+			CategoryFacetPortletKeys.CATEGORY_FACET + "_INSTANCE_%",
+			CustomFacetPortletKeys.CUSTOM_FACET + "_INSTANCE_%",
+			CustomFilterPortletKeys.CUSTOM_FILTER + "_INSTANCE_%",
+			FolderFacetPortletKeys.FOLDER_FACET + "_INSTANCE_%",
+			ModifiedFacetPortletKeys.MODIFIED_FACET + "_INSTANCE_%",
+			SearchBarPortletKeys.SEARCH_BAR + "_INSTANCE_%",
+			SearchResultsPortletKeys.SEARCH_RESULTS + "_INSTANCE_%",
+			SiteFacetPortletKeys.SITE_FACET + "_INSTANCE_%",
+			SortPortletKeys.SORT + "_INSTANCE_%",
+			TagFacetPortletKeys.TAG_FACET + "_INSTANCE_%",
+			TypeFacetPortletKeys.TYPE_FACET + "_INSTANCE_%",
+			UserFacetPortletKeys.USER_FACET + "_INSTANCE_%"
+		};
 	}
 
 	@Override

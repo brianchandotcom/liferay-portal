brianchandotcom/liferay-portal

commit 45c022419744f249c2b1303f55be32bc77390eb8

LPD-38914 Fix "Missing upgrade process created record" when running external data source controller tests. It's possible that the TestEntityUpgradeStepRegistrator gets tracked earlier than the Spring extender registers the SchemaCreatorImpl for the service module, and when this happens, the upgrade is skipped because the schema version doesn't exist yet.

modules/test/external-data-source-test-service/src/main/java/com/liferay/external/data/source/test/internal/upgrade/registry/TestEntityUpgradeStepRegistrator.java
@@ -5,10 +5,12 @@
 
 package com.liferay.external.data.source.test.internal.upgrade.registry;
 
+import com.liferay.portal.kernel.model.Release;
 import com.liferay.portal.kernel.upgrade.UpgradeProcessFactory;
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Shuyang Zhou
@@ -26,5 +28,10 @@ public class TestEntityUpgradeStepRegistrator
 				"insert into TestEntity (id_, data_) values (-1, 'Test " +
 					"Upgrade Value')"));
 	}
+
+	@Reference(
+		target = "(&(release.bundle.symbolic.name=com.liferay.external.data.source.test.service)(&(release.schema.version>=1.0.0)))"
+	)
+	private Release _release;
 
 }

commit 24071ff013efb5dd350b794d7f56f9c4ea5fe4cc

LPD-52371 TestEntityUpgradeStepRegistrator can not depend on the release that is going to be upgraded by itself. Doing so will lead to self deactivating and reactivating, which will cause deadlock in org.apache.felix.scr.impl.manager.RegistrationManager.changeRegistration(), caused by 45c02241 . Move the upgrade execution control into test to get rid of the race from SCR.

modules/test/external-data-source-test-controller-test/build.gradle
@@ -1,7 +1,9 @@
 dependencies {
 	testIntegrationImplementation group: "com.liferay", name: "org.hsqldb", version: "2.7.1.LIFERAY-PATCHED-1"
 	testIntegrationImplementation group: "com.liferay.portal", name: "com.liferay.portal.impl", version: "default"
+	testIntegrationImplementation group: "org.osgi", name: "org.osgi.service.component", version: "1.4.0"
 	testIntegrationImplementation group: "org.osgi", name: "org.osgi.service.component.annotations", version: "1.4.0"
+	testIntegrationImplementation group: "org.osgi", name: "org.osgi.util.promise", version: "1.2.0"
 	testIntegrationImplementation group: "org.osgi", name: "osgi.core", version: "6.0.0"
 	testIntegrationImplementation project(":apps:xstream:xstream-configurator-api")
 	testIntegrationImplementation project(":test:arquillian-extension-junit-bridge")

modules/test/external-data-source-test-controller-test/src/testIntegration/java/com/liferay/external/data/source/test/controller/test/ExternalDataSourceControllerTest.java
@@ -15,6 +15,7 @@ import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.StreamUtil;
+import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.util.PropsUtil;
 import com.liferay.portal.util.PropsValues;
@@ -45,6 +46,9 @@ import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.FrameworkUtil;
 import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.runtime.ServiceComponentRuntime;
+import org.osgi.service.component.runtime.dto.ComponentDescriptionDTO;
+import org.osgi.util.promise.Promise;
 
 /**
  * @author Preston Crary
@@ -83,6 +87,17 @@ public class ExternalDataSourceControllerTest {
 			PropsUtil.set(PropsKeys.UPGRADE_DATABASE_AUTO_RUN, "true");
 
 			_serviceBundle.start();
+
+			ComponentDescriptionDTO componentDescriptionDTO =
+				_serviceComponentRuntime.getComponentDescriptionDTO(
+					_serviceBundle,
+					"com.liferay.external.data.source.test.internal.upgrade." +
+						"registry.TestEntityUpgradeStepRegistrator");
+
+			Promise<Void> promise = _serviceComponentRuntime.enableComponent(
+				componentDescriptionDTO);
+
+			promise.getValue();
 		}
 		finally {
 			PropsUtil.set(
@@ -210,6 +225,9 @@ public class ExternalDataSourceControllerTest {
 	private Bundle _apiBundle;
 	private BundleContext _bundleContext;
 	private Bundle _serviceBundle;
+
+	@Inject
+	private ServiceComponentRuntime _serviceComponentRuntime;
 
 	/**
 	 * A carrier Throwable to overcome Arquillian Exception serialization

modules/test/external-data-source-test-service/src/main/java/com/liferay/external/data/source/test/internal/upgrade/registry/TestEntityUpgradeStepRegistrator.java
@@ -5,17 +5,15 @@
 
 package com.liferay.external.data.source.test.internal.upgrade.registry;
 
-import com.liferay.portal.kernel.model.Release;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Shuyang Zhou
  */
-@Component(service = UpgradeStepRegistrator.class)
+@Component(enabled = false, service = UpgradeStepRegistrator.class)
 public class TestEntityUpgradeStepRegistrator
 	implements UpgradeStepRegistrator {
 
@@ -35,10 +33,5 @@ public class TestEntityUpgradeStepRegistrator
 
 			});
 	}
-
-	@Reference(
-		target = "(&(release.bundle.symbolic.name=com.liferay.external.data.source.test.service)(&(release.schema.version>=1.0.0)))"
-	)
-	private Release _release;
 
 }
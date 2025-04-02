3c0386d3e8f1daa4ee4b29f6609cb72e198431cd
Rubén Pulido <ruben.pulido@liferay.com>
2025-04-02 11:02:52 +0200

    LPD-49099 "Validator.isNotNull" should only be used for type "Long", "Serializable" or "String"

--- modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/MasterPageResourceImpl.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/MasterPageResourceImpl.java
@@ -349,7 +349,7 @@ public class MasterPageResourceImpl extends BaseMasterPageResourceImpl {
 
 			Scope scope = itemExternalReferences[i].getScope();
 
-			if (Validator.isNotNull(scope)) {
+			if (scope != null) {
 				scopeGroupId = GroupUtil.getGroupId(
 					true, true, group.getCompanyId(),
 					scope.getExternalReferenceCode());

6d3766fad1be4ae099b1583140002ee4f47d7646
Carlos Correa <carlos.correa@liferay.com>
2025-04-09 17:37:41 +0200

    LPD-51941 Update comments based on the pattern we are testing

--- modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/resource/v1_0/test/ObjectEntryResourceTest.java
+++ modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/resource/v1_0/test/ObjectEntryResourceTest.java
@@ -8098,7 +8098,7 @@ public class ObjectEntryResourceTest {
 
 			user = _userLocalService.updateUser(user);
 
-			// With +0400 time zone offset
+			// Format: "yyyy-MM-dd'T'HH:mm:ss.SSSZ" and positive offset
 
 			JSONAssert.assertEquals(
 				JSONUtil.put(
@@ -8119,7 +8119,7 @@ public class ObjectEntryResourceTest {
 				).toString(),
 				JSONCompareMode.LENIENT);
 
-			// With -0400 time zone offset
+			// Format: "yyyy-MM-dd'T'HH:mm:ss.SSSZ" and negative offset
 
 			JSONAssert.assertEquals(
 				JSONUtil.put(
@@ -8140,7 +8140,7 @@ public class ObjectEntryResourceTest {
 				).toString(),
 				JSONCompareMode.LENIENT);
 
-			// With UTC time zone
+			// Format: "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
 
 			JSONAssert.assertEquals(
 				JSONUtil.put(
@@ -8161,7 +8161,7 @@ public class ObjectEntryResourceTest {
 				).toString(),
 				JSONCompareMode.LENIENT);
 
-			// With timezone abbreviation
+			// Format: "EEE MMM dd HH:mm:ss zzz yyyy"
 
 			JSONAssert.assertEquals(
 				JSONUtil.put(
@@ -8182,7 +8182,7 @@ public class ObjectEntryResourceTest {
 				).toString(),
 				JSONCompareMode.LENIENT);
 
-			// Without time zone
+			// Format: "yyyy-MM-dd'T'HH:mm:ss.SSS"
 
 			JSONAssert.assertEquals(
 				JSONUtil.put(

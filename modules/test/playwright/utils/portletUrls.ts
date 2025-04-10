5fe56b8562e38b0599755bd56877845da3a1ebe2
Noor Najjar <noor.najjar@liferay.com>
2025-04-10 08:47:29 -0700

    LPD-52862 Add playwright tests

--- modules/test/playwright/utils/portletUrls.ts
+++ modules/test/playwright/utils/portletUrls.ts
@@ -12,9 +12,11 @@ export const PORTLET_URLS = {
 	bookmarks:
 		'/~/control_panel/manage?p_p_id=com_liferay_bookmarks_web_portlet_BookmarksAdminPortlet',
 	categoriesAdmin: '/~/control_panel/manage/-/categories_admin/vocabularies',
+	cmsNewVocabulary: 'web/cms/categorization/new_vocabulary',
 	cmsPicklistBuilder: 'web/cms/picklist-builder',
 	cmsStructureBuilder: 'web/cms/structure-builder',
 	cmsStructures: 'web/cms/structures',
+	cmsVocabularies: 'web/cms/categorization/view_vocabularies',
 	collections:
 		'/~/control_panel/manage?p_p_id=com_liferay_asset_list_web_portlet_AssetListPortlet',
 	contentDashboard:

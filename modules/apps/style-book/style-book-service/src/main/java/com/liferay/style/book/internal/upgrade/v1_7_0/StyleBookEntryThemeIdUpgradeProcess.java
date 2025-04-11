brianchandotcom/liferay-portal

commit 608f1a0c17b9d1001fb258db8b43d8229f6368c0

LPD-52966 Wordsmith

modules/apps/style-book/style-book-service/src/main/java/com/liferay/style/book/internal/upgrade/v1_7_0/StyleBookEntryThemeIdUpgradeProcess.java
@@ -63,9 +63,8 @@ public class StyleBookEntryThemeIdUpgradeProcess extends UpgradeProcess {
 					if (_log.isWarnEnabled()) {
 						_log.warn(
 							StringBundler.concat(
-								"Unable to find group with id ", groupId,
-								". Removing orphaned style book entry with id ",
-								styleBookEntryId, "."));
+								"Deleting style book entry ", styleBookEntryId,
+								" because group ", groupId, "does not exist"));
 					}
 
 					preparedStatement3.setLong(1, styleBookEntryId);

modules/apps/style-book/style-book-service/src/main/java/com/liferay/style/book/internal/upgrade/v1_8_0/StyleBookEntryVersionThemeIdUpgradeProcess.java
@@ -62,9 +62,9 @@ public class StyleBookEntryVersionThemeIdUpgradeProcess extends UpgradeProcess {
 					if (_log.isWarnEnabled()) {
 						_log.warn(
 							StringBundler.concat(
-								"Unable to find group with id ", groupId,
-								". Removing orphaned style book entry version ",
-								"with id ", styleBookEntryId, "."));
+								"Deleting style book entry version for style ",
+								"book entry", styleBookEntryId,
+								" because group ", groupId, "does not exist"));
 					}
 
 					preparedStatement3.setLong(1, styleBookEntryId);

modules/apps/style-book/style-book-test/src/testIntegration/java/com/liferay/style/book/internal/upgrade/v1_7_0/test/StyleBookEntryThemeIdUpgradeProcessTest.java
@@ -93,9 +93,9 @@ public class StyleBookEntryThemeIdUpgradeProcessTest {
 
 			Assert.assertEquals(
 				StringBundler.concat(
-					"Unable to find group with id ", groupId,
-					". Removing orphaned style book entry with id ",
-					orphanedStyleBookEntry.getStyleBookEntryId(), "."),
+					"Deleting style book entry ",
+					orphanedStyleBookEntry.getStyleBookEntryId(),
+					" because group ", groupId, "does not exist"),
 				logEntry.getMessage());
 		}
 

modules/apps/style-book/style-book-test/src/testIntegration/java/com/liferay/style/book/internal/upgrade/v1_8_0/test/StyleBookEntryVersionThemeIdUpgradeProcessTest.java
@@ -93,9 +93,9 @@ public class StyleBookEntryVersionThemeIdUpgradeProcessTest {
 
 			Assert.assertEquals(
 				StringBundler.concat(
-					"Unable to find group with id ", groupId,
-					". Removing orphaned style book entry version with id ",
-					orphanedStyleBookEntry.getStyleBookEntryId(), "."),
+					"Deleting style book entry version for style book entry ",
+					orphanedStyleBookEntry.getStyleBookEntryId(),
+					" because group ", groupId, "does not exist"),
 				logEntry.getMessage());
 		}
 

commit be9dc22c38eecbacd4f16ccdf995664207e6d27f

LPD-52966 SF caused by 608f1a0c17b9d1001fb258db8b43d8229f6368c0

modules/apps/style-book/style-book-service/src/main/java/com/liferay/style/book/internal/upgrade/v1_7_0/StyleBookEntryThemeIdUpgradeProcess.java
@@ -64,7 +64,7 @@ public class StyleBookEntryThemeIdUpgradeProcess extends UpgradeProcess {
 						_log.warn(
 							StringBundler.concat(
 								"Deleting style book entry ", styleBookEntryId,
-								" because group ", groupId, "does not exist"));
+								" because group ", groupId, " does not exist"));
 					}
 
 					preparedStatement3.setLong(1, styleBookEntryId);

modules/apps/style-book/style-book-service/src/main/java/com/liferay/style/book/internal/upgrade/v1_8_0/StyleBookEntryVersionThemeIdUpgradeProcess.java
@@ -63,8 +63,8 @@ public class StyleBookEntryVersionThemeIdUpgradeProcess extends UpgradeProcess {
 						_log.warn(
 							StringBundler.concat(
 								"Deleting style book entry version for style ",
-								"book entry", styleBookEntryId,
-								" because group ", groupId, "does not exist"));
+								"book entry ", styleBookEntryId,
+								" because group ", groupId, " does not exist"));
 					}
 
 					preparedStatement3.setLong(1, styleBookEntryId);

modules/apps/style-book/style-book-test/src/testIntegration/java/com/liferay/style/book/internal/upgrade/v1_7_0/test/StyleBookEntryThemeIdUpgradeProcessTest.java
@@ -96,7 +96,7 @@ public class StyleBookEntryThemeIdUpgradeProcessTest {
 				StringBundler.concat(
 					"Deleting style book entry ",
 					orphanedStyleBookEntry.getStyleBookEntryId(),
-					" because group ", groupId, "does not exist"),
+					" because group ", groupId, " does not exist"),
 				logEntry.getMessage());
 		}
 

modules/apps/style-book/style-book-test/src/testIntegration/java/com/liferay/style/book/internal/upgrade/v1_8_0/test/StyleBookEntryVersionThemeIdUpgradeProcessTest.java
@@ -94,7 +94,7 @@ public class StyleBookEntryVersionThemeIdUpgradeProcessTest {
 				StringBundler.concat(
 					"Deleting style book entry version for style book entry ",
 					orphanedStyleBookEntry.getStyleBookEntryId(),
-					" because group ", groupId, "does not exist"),
+					" because group ", groupId, " does not exist"),
 				logEntry.getMessage());
 		}
 
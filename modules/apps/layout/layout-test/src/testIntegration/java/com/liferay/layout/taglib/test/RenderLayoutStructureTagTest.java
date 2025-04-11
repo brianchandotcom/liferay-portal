67ea13889fd60eb6764825803513b382e9540d84
Brian Chan <brian.chan@liferay.com>
2025-04-11 12:35:42 +0200

    LPD-52923 SF

--- modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/taglib/test/RenderLayoutStructureTagTest.java
+++ modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/taglib/test/RenderLayoutStructureTagTest.java
@@ -3022,16 +3022,11 @@ public class RenderLayoutStructureTagTest {
 
 		String content = mockHttpServletResponse.getContentAsString();
 
-		String formStartHTML = "<form action=\"";
-
-		Assert.assertFalse(content.contains(formStartHTML));
-
+		Assert.assertFalse(
+			content.contains("<form action=\""));
+		Assert.assertFalse(
+			content.contains("<p>InputName:" + infoField.getName() + "</p>"));
 		Assert.assertTrue(content, content.contains(expectedSuccessHTML));
-
-		String expectedInfoFieldInput =
-			"<p>InputName:" + infoField.getName() + "</p>";
-
-		Assert.assertFalse(content.contains(expectedInfoFieldInput));
 	}
 
 	private void _testRenderLayoutWithLocale(

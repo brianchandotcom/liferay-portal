brianchandotcom/liferay-portal

commit 8c99ad887a01e4541de90266da6a81c85a723f6a

LPD-52644 Rename

modules/apps/object/object-service/src/main/java/com/liferay/object/internal/entry/util/ObjectEntryUtil.java
@@ -115,7 +115,7 @@ public class ObjectEntryUtil {
 					Collections.emptyMap(), jsonFactory, modelClass, userId);
 
 				if (MapUtil.isEmpty(
-						ObjectEntryThreadLocal.getExpandoValues())) {
+						ObjectEntryThreadLocal.getExpandoBridgeAttributes())) {
 
 					return originalDTOMap;
 				}
@@ -125,11 +125,11 @@ public class ObjectEntryUtil {
 				originalDTOMap.put(
 					"customFields",
 					CustomFieldsUtil.toCustomFields(
-						false, ObjectEntryThreadLocal.getExpandoValues(),
-						modelClass.getName(),
+						false, modelClass.getName(),
 						GetterUtil.getLong(
 							originalBaseModel.getPrimaryKeyObj()),
-						objectDefinition.getCompanyId(), user.getLocale()));
+						objectDefinition.getCompanyId(), user.getLocale(),
+						ObjectEntryThreadLocal.getExpandoBridgeAttributes()));
 
 				return originalDTOMap;
 			}

modules/apps/account/account-service/src/main/java/com/liferay/account/service/impl/AccountEntryLocalServiceImpl.java
@@ -694,7 +694,7 @@ public class AccountEntryLocalServiceImpl
 			accountEntryId);
 
 		try {
-			ObjectEntryThreadLocal.setExpandoValues(
+			ObjectEntryThreadLocal.setExpandoBridgeAttributes(
 				expandoBridge.getAttributes());
 
 			accountEntry.setExpandoBridgeAttributes(serviceContext);
@@ -737,7 +737,7 @@ public class AccountEntryLocalServiceImpl
 			return accountEntry;
 		}
 		finally {
-			ObjectEntryThreadLocal.clearExpandoValues();
+			ObjectEntryThreadLocal.clearExpandoBridgeAttributes();
 		}
 	}
 

modules/apps/object/object-api/src/main/java/com/liferay/object/entry/util/ObjectEntryThreadLocal.java
@@ -25,12 +25,12 @@ public class ObjectEntryThreadLocal {
 		validatedObjectEntryIds.add(objectEntryId);
 	}
 
-	public static void clearExpandoValues() {
-		_expandoValues.remove();
+	public static void clearExpandoBridgeAttributes() {
+		_expandoBridgeAttributes.remove();
 	}
 
-	public static Map<String, Serializable> getExpandoValues() {
-		return _expandoValues.get();
+	public static Map<String, Serializable> getExpandoBridgeAttributes() {
+		return _expandoBridgeAttributes.get();
 	}
 
 	public static boolean isDisassociateRelatedModels() {
@@ -62,10 +62,10 @@ public class ObjectEntryThreadLocal {
 			disassociateRelatedModels);
 	}
 
-	public static void setExpandoValues(
+	public static void setExpandoBridgeAttributes(
 		Map<String, Serializable> expandoValues) {
 
-		_expandoValues.set(expandoValues);
+		_expandoBridgeAttributes.set(expandoValues);
 	}
 
 	public static void setSkipObjectEntryResourcePermission(
@@ -91,9 +91,9 @@ public class ObjectEntryThreadLocal {
 		_disassociateRelatedModels = new CentralizedThreadLocal<>(
 			ObjectEntryThreadLocal.class + "._disassociateRelatedModels",
 			() -> false);
-	private static final ThreadLocal<Map<String, Serializable>> _expandoValues =
-		new CentralizedThreadLocal<>(
-			ObjectEntryThreadLocal.class + "._expandoValues");
+	private static final ThreadLocal<Map<String, Serializable>>
+		_expandoBridgeAttributes = new CentralizedThreadLocal<>(
+			ObjectEntryThreadLocal.class + "._expandoBridgeAttributes");
 	private static final ThreadLocal<Boolean>
 		_skipObjectEntryResourcePermission = new CentralizedThreadLocal<>(
 			ObjectEntryThreadLocal.class +

modules/apps/portal-vulcan/portal-vulcan-api/src/main/java/com/liferay/portal/vulcan/custom/field/CustomFieldsUtil.java
@@ -41,14 +41,27 @@ import java.util.function.Function;
 public class CustomFieldsUtil {
 
 	public static CustomField[] toCustomFields(
-		boolean acceptAllLanguages, Map<String, Serializable> attributes,
-		String className, long classPK, long companyId, Locale locale) {
+		boolean acceptAllLanguages, String className, long classPK,
+		long companyId, Locale locale) {
+
+		ExpandoBridge expandoBridge = ExpandoBridgeFactoryUtil.getExpandoBridge(
+			companyId, className, classPK);
+
+		return toCustomFields(
+			acceptAllLanguages, className, classPK, companyId,
+			expandoBridge.getAttributes(), locale);
+	}
+
+	public static CustomField[] toCustomFields(
+		boolean acceptAllLanguages, String className, long classPK,
+		long companyId, Map<String, Serializable> expandoBridgeAttributes,
+		Locale locale) {
 
 		ExpandoBridge expandoBridge = ExpandoBridgeFactoryUtil.getExpandoBridge(
 			companyId, className, classPK);
 
 		return TransformUtil.transformToArray(
-			attributes.entrySet(),
+			expandoBridgeAttributes.entrySet(),
 			entry -> {
 				UnicodeProperties unicodeProperties =
 					expandoBridge.getAttributeProperties(entry.getKey());
@@ -67,18 +80,6 @@ public class CustomFieldsUtil {
 					entry, expandoBridge, locale);
 			},
 			CustomField.class);
-	}
-
-	public static CustomField[] toCustomFields(
-		boolean acceptAllLanguages, String className, long classPK,
-		long companyId, Locale locale) {
-
-		ExpandoBridge expandoBridge = ExpandoBridgeFactoryUtil.getExpandoBridge(
-			companyId, className, classPK);
-
-		return toCustomFields(
-			acceptAllLanguages, expandoBridge.getAttributes(), className,
-			classPK, companyId, locale);
 	}
 
 	public static Map<String, Serializable> toMap(

commit 76b988091661d9cfbf22657a6d2567b661fa0bfd

LPD-52644 Sort parameters passing and use new package path
Caused by 8c99ad887a01e4541de90266da6a81c85a723f6a

modules/apps/object/object-service/src/main/java/com/liferay/object/internal/entry/util/ObjectEntryUtil.java
@@ -6,7 +6,6 @@
 package com.liferay.object.internal.entry.util;
 
 import com.liferay.expando.kernel.model.ExpandoBridge;
-import com.liferay.headless.delivery.dto.v1_0.util.CustomFieldsUtil;
 import com.liferay.object.entry.util.ObjectEntryThreadLocal;
 import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.model.ObjectEntry;
@@ -26,6 +25,7 @@ import com.liferay.portal.kernel.service.UserLocalServiceUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.vulcan.custom.field.CustomFieldsUtil;
 import com.liferay.portal.vulcan.dto.converter.DTOConverter;
 import com.liferay.portal.vulcan.dto.converter.DTOConverterRegistry;
 import com.liferay.portal.vulcan.dto.converter.DefaultDTOConverterContext;
@@ -128,8 +128,9 @@ public class ObjectEntryUtil {
 						false, modelClass.getName(),
 						GetterUtil.getLong(
 							originalBaseModel.getPrimaryKeyObj()),
-						objectDefinition.getCompanyId(), user.getLocale(),
-						ObjectEntryThreadLocal.getExpandoBridgeAttributes()));
+						objectDefinition.getCompanyId(),
+						ObjectEntryThreadLocal.getExpandoBridgeAttributes(),
+						user.getLocale()));
 
 				return originalDTOMap;
 			}
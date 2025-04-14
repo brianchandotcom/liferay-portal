00358739b95af5bc2e1c6c94a5f3fd76adb85334
barbaracabrera <barbara.cabrera@liferay.com>
2025-04-14 13:00:09 +0100

    LPD-53189 Add transition to the accordion

--- modules/apps/fragment/fragment-collection-contributor/fragment-collection-contributor-basic-component/src/main/resources/com/liferay/fragment/collection/contributor/basic/component/dependencies/accordion/index.js
+++ modules/apps/fragment/fragment-collection-contributor/fragment-collection-contributor-basic-component/src/main/resources/com/liferay/fragment/collection/contributor/basic/component/dependencies/accordion/index.js
@@ -4,14 +4,28 @@ const body = fragmentElement.querySelector('.panel-collapse');
 function main() {
 	if (layoutMode !== 'edit') {
 		button.addEventListener('click', () => {
-			button.classList.toggle('collapsed');
-			body.classList.toggle('collapse');
-		});
+			const isOpen = body.classList.contains('show');
 
-		button.setAttribute(
-			'aria-expanded',
-			!button.classList.contains('collapsed')
-		);
+			body.style.height = body.scrollHeight + 'px';
+			body.classList.remove('collapse', 'show');
+			body.classList.add('collapsing');
+
+			requestAnimationFrame(() => {
+				body.style.height = isOpen ? '0px' : body.scrollHeight + 'px';
+			});
+
+			body.addEventListener('transitionend', function handler() {
+				body.classList.remove('collapsing');
+				body.classList.add('collapse');
+				if (!isOpen) {
+					body.classList.add('show');
+				}
+				body.style.height = '';
+				body.removeEventListener('transitionend', handler);
+			});
+
+			button.setAttribute('aria-expanded', String(!isOpen));
+		});
 	}
 }
 

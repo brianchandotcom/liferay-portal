2b0bfcec057efc9b0793168c01515d3a9dbc9035
Brian Chan <brian.chan@liferay.com>
2025-04-17 07:44:48 +0530

    LPD-53532 Proper dir

--- modules/apps/client-extension/client-extension-test/src/testIntegration/java/com/liferay/client/extension/internal/instance/lifecycle/test/GlobalClientExtensionPortalInstanceLifecycleListenerTest.java
+++ modules/apps/client-extension/client-extension-test/src/testIntegration/java/com/liferay/client/extension/internal/instance/lifecycle/test/GlobalClientExtensionPortalInstanceLifecycleListenerTest.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.client.extension.internal.event.test;
+package com.liferay.client.extension.internal.instance.lifecycle.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.client.extension.constants.ClientExtensionEntryConstants;

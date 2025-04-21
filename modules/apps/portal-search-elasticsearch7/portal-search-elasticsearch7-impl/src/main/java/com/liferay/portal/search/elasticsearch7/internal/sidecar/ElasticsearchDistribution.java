159049d7d91acf0deaa9ae2ab861442111d3eeb0
Bryan Engler <bryan.engler@liferay.com>
2025-04-21 12:02:54 -0700

    LPD-53692 portal-search-elasticsearch7-impl: Update checksums
    https://liferay.atlassian.net/browse/LPD-53692

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/sidecar/ElasticsearchDistribution.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/sidecar/ElasticsearchDistribution.java
@@ -46,23 +46,23 @@ public class ElasticsearchDistribution implements Distribution {
 	}
 
 	private static final String _ELASTICSEARCH_CHECKSUM =
-		"5b32714d5683059c213fbe610de21d1016048e97ee4215ae4104aa6065d409c33d7c" +
-			"e13b5e1443b559ea2167f7ebe8a5b26609ff1e085e4f4995bf46083afa35";
+		"228241d7b4b02d6f97da51b3069767d65e4d94116aba31d215229942350ae6b8cfab" +
+			"88d662ff344e7bc3ed7f351fd4ea5e5e1e1068e052a44d4e166e211ffe8a";
 
 	private static final String _ICU_CHECKSUM =
-		"68f1024daea400b100db8a4a6ec81ad5b9df76f1ef60f98cad06389a9aea58ee21b2" +
-			"939ba770f7d3c8e1901eab347cfd4d68590f85d730217271f17cd392bfc6";
+		"33c0899e6552ee3bdc9e0c7dbff8b111a33f82f6a802c8506260ba2367fdb75dec72" +
+			"e2378ef99a49a61a99b557b14b9999b5b64a56bf67c08bfb4af6cf19d0ce";
 
 	private static final String _KUROMOJI_CHECKSUM =
-		"16226951b084dd4f33d640da87a69a132336e53e5537d441711080a594dd616b5c3f" +
-			"5c5b2e4b678afc06c8d3472d642018c258e4e07b105ad69ffb6b02f7892f";
+		"4c0311ae8a7f31caef64d1184899473b8439cee10b0b3c86204f6d86c5beec0f5278" +
+			"528e63cbf06e9280d8cf301219f32ebd3a2810d204369be1fa45d847e9cb";
 
 	private static final String _SMARTCN_CHECKSUM =
-		"df4babcd8a937fda74fb7c31f1a4d138cfb0c2e6a87f31e092a3193df5e138c9182e" +
-			"af521370178cb9b5b0a4dabacf56a5236c8d6927460d072c49cd46da1148";
+		"f3afa5961e2a9673b11991eba0fffc01e442d9a23ad85e27b454d95a19146149badd" +
+			"81c2d0038e62bb7886db99ab4933d9948dc0d3f270049bb4e075008955e2";
 
 	private static final String _STEMPEL_CHECKSUM =
-		"524df432270e94ff2b21d3efc0f2a82a2bb2c0fd36c3dbe33333237dfff0c67eff4d" +
-			"de289bfc6f274959b7b544535f5ded49cd58ee9a2202c4bcd228756aef69";
+		"e9576d5c3b4c643cf1dc60f28baa8b1bec6daba514c90141dc5525b9d01521d543c2" +
+			"84750a00fa856d5f95c8cfadd5d44be2f3d89d1218f9e3951a3e69fdeb35";
 
 }

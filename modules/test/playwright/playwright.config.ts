7a257da2e8ae7731d1c9338bfeb85ff51f65c9b8
Sandro Chinea <sandrochinea@gmail.com>
2025-04-29 09:09:52 +0100

    LPD-53232 Move permissions tests to users-admin-web under new permissions folder

--- modules/test/playwright/playwright.config.ts
+++ modules/test/playwright/playwright.config.ts
@@ -131,6 +131,7 @@ import {config as stagingConfig} from './tests/staging-configuration-web/config'
 import {config as stylebookWebConfig} from './tests/style-book-web/config';
 import {config as templateWebConfig} from './tests/template-web/config';
 import {config as usersAdminWebConfig} from './tests/users-admin-web/config';
+import {config as usersAdminWebPermissionsConfig} from './tests/users-admin-web/permissions/config';
 import {config as wikiWebConfig} from './tests/wiki-web/config';
 import {config as customerConfig} from './tests/workspaces/liferay-customer-workspace/config';
 import {config as commerceWorkspaceConfig} from './tests/workspaces/liferay-workspace-commerce/config';
@@ -272,6 +273,7 @@ export default defineConfig({
 		stylebookWebConfig,
 		templateWebConfig,
 		usersAdminWebConfig,
+		usersAdminWebPermissionsConfig,
 		wikiWebConfig,
 		...setupProjects,
 	],

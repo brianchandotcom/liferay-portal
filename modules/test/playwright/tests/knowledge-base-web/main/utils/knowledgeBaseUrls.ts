f838ab085540242b2d5ebad5aab989c6f9711c57
Sandro Chinea <sandrochinea@gmail.com>
2025-04-30 09:07:20 +0100

    LPD-54411 Add main folder for knowledge-base-web

--- modules/test/playwright/tests/knowledge-base-web/main/utils/knowledgeBaseUrls.ts
+++ modules/test/playwright/tests/knowledge-base-web/main/utils/knowledgeBaseUrls.ts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {PORTLET_URLS} from '../../../utils/portletUrls';
+import {PORTLET_URLS} from '../../../../utils/portletUrls';
 
 export class KnowledgeBaseUrls {
 	readonly home: string;

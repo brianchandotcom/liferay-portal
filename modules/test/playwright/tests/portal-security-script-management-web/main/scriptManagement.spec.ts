caec1e9762fd819d9f85e708243bd60d6d9fcd67
Sandro Chinea <sandrochinea@gmail.com>
2025-04-30 09:59:23 +0100

    LPD-54411 Add main folder for portal-security-script-management-web

--- modules/test/playwright/tests/portal-security-script-management-web/main/scriptManagement.spec.ts
+++ modules/test/playwright/tests/portal-security-script-management-web/main/scriptManagement.spec.ts
@@ -11,10 +11,10 @@ import {
 } from '@liferay/object-admin-rest-client-js';
 import {expect, mergeTests} from '@playwright/test';
 
-import {apiHelpersTest} from '../../fixtures/apiHelpersTest';
-import {loginTest} from '../../fixtures/loginTest';
-import {scriptManagementPagesTest} from '../../fixtures/scriptManagementPagesTest';
-import {getRandomInt} from '../../utils/getRandomInt';
+import {apiHelpersTest} from '../../../fixtures/apiHelpersTest';
+import {loginTest} from '../../../fixtures/loginTest';
+import {scriptManagementPagesTest} from '../../../fixtures/scriptManagementPagesTest';
+import {getRandomInt} from '../../../utils/getRandomInt';
 
 export const test = mergeTests(
 	apiHelpersTest,

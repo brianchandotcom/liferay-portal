e23c17941c5bfd0a46111eca7e345e36667b1d28
Carlos Correa <carlos.correa@liferay.com>
2025-05-07 18:08:33 +0200

    LPD-54468 Optimize and use existing methods. I think we don't need to pass any content type in general, that is handled internally by JAX-RS. So I remove the new method

--- modules/util/portal-tools-rest-builder/src/main/java/com/liferay/portal/tools/rest/builder/internal/freemarker/tool/FreeMarkerTool.java
+++ modules/util/portal-tools-rest-builder/src/main/java/com/liferay/portal/tools/rest/builder/internal/freemarker/tool/FreeMarkerTool.java
@@ -930,44 +930,6 @@ public class FreeMarkerTool {
 			configYAML, true, openAPIYAML, schema, schemas);
 	}
 
-	public boolean hasCorrespondingMethodWithMediaType(
-		List<JavaMethodSignature> javaMethodSignatures,
-		JavaMethodSignature sourceJavaMethodSignature,
-		String targetMethodPrefix, String sourceMethodPrefix,
-		String... mediaTypes) {
-
-		if ((sourceJavaMethodSignature == null) ||
-			!sourceJavaMethodSignature.getMethodName(
-			).startsWith(
-				sourceMethodPrefix
-			)) {
-
-			return false;
-		}
-
-		String methodSuffix = sourceJavaMethodSignature.getMethodName(
-		).substring(
-			sourceMethodPrefix.length()
-		);
-
-		String targetMethodName = targetMethodPrefix + methodSuffix;
-
-		JavaMethodSignature targetJavaMethodSignature = getJavaMethodSignature(
-			javaMethodSignatures, targetMethodName);
-
-		if (targetJavaMethodSignature == null) {
-			return false;
-		}
-
-		for (String mediaType : mediaTypes) {
-			if (hasRequestBodyMediaType(targetJavaMethodSignature, mediaType)) {
-				return true;
-			}
-		}
-
-		return false;
-	}
-
 	public boolean hasHTTPMethod(
 		JavaMethodSignature javaMethodSignature, String... httpMethods) {
 

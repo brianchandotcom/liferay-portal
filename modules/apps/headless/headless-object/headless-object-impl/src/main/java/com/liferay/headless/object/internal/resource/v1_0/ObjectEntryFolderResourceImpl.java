766a007cd4c6cac1f79436cf7bec67fbb9624c3d
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2025-05-12 13:28:51 +0200

    LPD-54692 Return null to indicate absence of value
    This will be used in the PATCH method, so that it will be possible to
    move entries to the root folder (right now the root folder is ignored).

--- modules/apps/headless/headless-object/headless-object-impl/src/main/java/com/liferay/headless/object/internal/resource/v1_0/ObjectEntryFolderResourceImpl.java
+++ modules/apps/headless/headless-object/headless-object-impl/src/main/java/com/liferay/headless/object/internal/resource/v1_0/ObjectEntryFolderResourceImpl.java
@@ -237,7 +237,9 @@ public class ObjectEntryFolderResourceImpl
 
 		return _addObjectEntryFolder(
 			groupId,
-			_getParentObjectEntryFolderId(false, groupId, objectEntryFolder),
+			GetterUtil.getLong(
+				_getParentObjectEntryFolderId(
+					false, groupId, objectEntryFolder)),
 			objectEntryFolder);
 	}
 
@@ -262,14 +264,18 @@ public class ObjectEntryFolderResourceImpl
 		if (persistedObjectEntryFolder == null) {
 			return _addObjectEntryFolder(
 				groupId,
-				_getParentObjectEntryFolderId(true, groupId, objectEntryFolder),
+				GetterUtil.getLong(
+					_getParentObjectEntryFolderId(
+						true, groupId, objectEntryFolder)),
 				objectEntryFolder);
 		}
 
 		return _toObjectEntryFolder(
 			_objectEntryFolderService.updateObjectEntryFolder(
 				persistedObjectEntryFolder.getObjectEntryFolderId(),
-				_getParentObjectEntryFolderId(true, groupId, objectEntryFolder),
+				GetterUtil.getLong(
+					_getParentObjectEntryFolderId(
+						true, groupId, objectEntryFolder)),
 				objectEntryFolder.getDescription(),
 				LocalizedMapUtil.getLocalizedMap(
 					contextAcceptLanguage.getPreferredLocale(),
@@ -316,7 +322,7 @@ public class ObjectEntryFolderResourceImpl
 		throw new NoSuchGroupException();
 	}
 
-	private long _getParentObjectEntryFolderId(
+	private Long _getParentObjectEntryFolderId(
 			boolean addObjectEntryFolder, long groupId,
 			ObjectEntryFolder objectEntryFolder)
 		throws Exception {
@@ -329,8 +335,7 @@ public class ObjectEntryFolderResourceImpl
 
 		if (Validator.isNull(parentObjectEntryFolderExternalReferenceCode)) {
 			if (parentObjectEntryFolderId == null) {
-				return ObjectEntryFolderConstants.
-					PARENT_OBJECT_ENTRY_FOLDER_ID_DEFAULT;
+				return null;
 			}
 
 			if (parentObjectEntryFolderId != ObjectEntryFolderConstants.
@@ -396,13 +401,10 @@ public class ObjectEntryFolderResourceImpl
 				persistedObjectEntryFolder.getLabelMap());
 		}
 
-		long parentObjectEntryFolderId = _getParentObjectEntryFolderId(
+		Long parentObjectEntryFolderId = _getParentObjectEntryFolderId(
 			false, persistedObjectEntryFolder.getGroupId(), objectEntryFolder);
 
-		if (parentObjectEntryFolderId ==
-				ObjectEntryFolderConstants.
-					PARENT_OBJECT_ENTRY_FOLDER_ID_DEFAULT) {
-
+		if (parentObjectEntryFolderId == null) {
 			parentObjectEntryFolderId =
 				persistedObjectEntryFolder.getObjectEntryFolderId();
 		}

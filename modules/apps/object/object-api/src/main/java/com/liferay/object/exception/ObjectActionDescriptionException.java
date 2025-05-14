d06107d6b63ab81b6a296acd4f54ac42e70ce755
Aquiles Duarte <aquiles.duarte@liferay.com>
2025-05-14 13:58:47 -0300

    LPD-20481 Adapting the ObjectActionDescriptionException class

--- modules/apps/object/object-api/src/main/java/com/liferay/object/exception/ObjectActionDescriptionException.java
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/exception/ObjectActionDescriptionException.java
@@ -2,28 +2,49 @@
  * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
+
 package com.liferay.object.exception;
 
 import com.liferay.portal.kernel.exception.PortalException;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author Marco Leo
  */
 public class ObjectActionDescriptionException extends PortalException {
 
-	public ObjectActionDescriptionException() {
+	public List<Object> getArguments() {
+		return _arguments;
 	}
 
-	public ObjectActionDescriptionException(String msg) {
-		super(msg);
+	public String getMessageKey() {
+		return _messageKey;
 	}
 
-	public ObjectActionDescriptionException(String msg, Throwable throwable) {
-		super(msg, throwable);
+	public static class MustBeLessThan4000Characters
+		extends ObjectActionDescriptionException {
+
+		public MustBeLessThan4000Characters() {
+			super(
+				Arrays.asList(4000, "description"),
+				"Description must be less than 4000 characters",
+				"only-x-characters-are-allowed-in-the-x-field");
+		}
+
 	}
 
-	public ObjectActionDescriptionException(Throwable throwable) {
-		super(throwable);
+	private ObjectActionDescriptionException(
+		List<Object> arguments, String message, String messageKey) {
+
+		super(message);
+
+		_arguments = arguments;
+		_messageKey = messageKey;
 	}
+
+	private final List<Object> _arguments;
+	private final String _messageKey;
 
 }

ee038d852bc247d1dca53915d6e557e58c518845
Crescenzo Rega <crescenzo.rega@liferay.com>
2025-05-23 09:59:32 +0200

    LPD-55683 Adds playwright tests

--- modules/test/playwright/fixtures/commercePagesTest.ts
+++ modules/test/playwright/fixtures/commercePagesTest.ts
@@ -18,6 +18,7 @@ import {CommerceAdminChannelsPage} from '../pages/commerce/commerce-channel-web/
 import {CheckoutPage} from '../pages/commerce/commerce-checkout-web/checkoutPage';
 import {CommerceAdminCurrenciesPage} from '../pages/commerce/commerce-currency-web/commerceAdminCurrenciesPage';
 import {CommerceAdminCurrencyDetailsPage} from '../pages/commerce/commerce-currency-web/commerceAdminCurrencyDetailsPage';
+import {CommerceAdminHealthCheckPage} from '../pages/commerce/commerce-health-status-web/commerceAdminHealthCheckPage';
 import {CommerceLayoutsPage} from '../pages/commerce/commerce-order-content-web/commerceLayoutsPage';
 import {PendingOrdersPage} from '../pages/commerce/commerce-order-content-web/pendingOrdersPage';
 import {PlacedOrderPage} from '../pages/commerce/commerce-order-content-web/placedOrderPage';
@@ -79,6 +80,7 @@ const commercePagesTest = test.extend<{
 	commerceAdminCurrencyDetailsPage: CommerceAdminCurrencyDetailsPage;
 	commerceAdminDiscountDetailsPage: CommerceAdminDiscountDetailsPage;
 	commerceAdminDiscountsPage: CommerceAdminDiscountsPage;
+	commerceAdminHealthCheckPage: CommerceAdminHealthCheckPage;
 	commerceAdminOrderDetailsPage: CommerceAdminOrderDetailsPage;
 	commerceAdminOrderNotesPage: CommerceAdminOrderNotesPage;
 	commerceAdminOrderTypeDetailsPage: CommerceAdminOrderTypeDetailsPage;
@@ -164,6 +166,9 @@ const commercePagesTest = test.extend<{
 	},
 	commerceAdminDiscountsPage: async ({page}, use) => {
 		await use(new CommerceAdminDiscountsPage(page));
+	},
+	commerceAdminHealthCheckPage: async ({page}, use) => {
+		await use(new CommerceAdminHealthCheckPage(page));
 	},
 	commerceAdminOrderDetailsPage: async ({page}, use) => {
 		await use(new CommerceAdminOrderDetailsPage(page));

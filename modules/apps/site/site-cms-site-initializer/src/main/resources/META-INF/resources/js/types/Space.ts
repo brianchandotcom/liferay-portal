5181c20ca26bebb1d75975b68c105df29919320d
Bruno Carvalho <bruno.carvalho@liferay.com>
2025-06-13 10:16:26 -0300

    LPD-57740 Unify Space types

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/types/Space.ts
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/types/Space.ts
@@ -3,10 +3,14 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import ClaySticker from '@clayui/sticker';
+
+export type LogoColor = React.ComponentProps<typeof ClaySticker>['displayType'];
+
 export type Space = {
 	creatorUserId: string;
 	externalReferenceCode: string;
 	id: string;
 	name: string;
-	settings?: {logoColor: string};
+	settings?: {logoColor: LogoColor};
 };

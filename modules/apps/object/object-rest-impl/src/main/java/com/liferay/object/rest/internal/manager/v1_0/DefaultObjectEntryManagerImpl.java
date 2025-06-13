cbad51de8eb15bcb05fcb5c0c35a43282d4212fc
Yuri Monteiro <yuri.monteiro@liferay.com>
2025-06-13 09:56:41 -0300

    LPD-57397 Adding methods in DefaultObjectEntryManager, DefaultObjectEntryManagerImpl and ObjectEntryResourceImpl

--- modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/manager/v1_0/DefaultObjectEntryManagerImpl.java
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/manager/v1_0/DefaultObjectEntryManagerImpl.java
@@ -259,7 +259,7 @@ public class DefaultObjectEntryManagerImpl
 			dtoConverterContext, objectEntryId, version);
 
 		return _copyVersionedObjectEntry(
-			dtoConverterContext, objectDefinition, objectEntry);
+			dtoConverterContext, objectDefinition, objectEntry, null);
 	}
 
 	@Override
@@ -278,7 +278,26 @@ public class DefaultObjectEntryManagerImpl
 			version);
 
 		return _copyVersionedObjectEntry(
-			dtoConverterContext, objectDefinition, objectEntry);
+			dtoConverterContext, objectDefinition, objectEntry, null);
+	}
+
+	@Override
+	public ObjectEntry copyObjectEntryByVersion(
+			DTOConverterContext dtoConverterContext,
+			String externalReferenceCode, ObjectDefinition objectDefinition,
+			String scopeKey, int version)
+		throws Exception {
+
+		if (!objectDefinition.isEnableObjectEntryVersioning()) {
+			throw new UnsupportedOperationException();
+		}
+
+		ObjectEntry objectEntry = getObjectEntryByVersion(
+			dtoConverterContext, objectDefinition.getCompanyId(),
+			objectDefinition, scopeKey, externalReferenceCode, version);
+
+		return _copyVersionedObjectEntry(
+			dtoConverterContext, objectDefinition, objectEntry, scopeKey);
 	}
 
 	@Override
@@ -1349,7 +1368,8 @@ public class DefaultObjectEntryManagerImpl
 
 	private ObjectEntry _copyVersionedObjectEntry(
 			DTOConverterContext dtoConverterContext,
-			ObjectDefinition objectDefinition, ObjectEntry objectEntry)
+			ObjectDefinition objectDefinition, ObjectEntry objectEntry,
+			String scopeKey)
 		throws Exception {
 
 		objectEntry.setExternalReferenceCode(() -> null);
@@ -1358,8 +1378,7 @@ public class DefaultObjectEntryManagerImpl
 		_removeReadOnlyProperties(objectDefinition, objectEntry);
 
 		return addObjectEntry(
-			dtoConverterContext, objectDefinition, objectEntry,
-			objectEntry.getScopeKey());
+			dtoConverterContext, objectDefinition, objectEntry, scopeKey);
 	}
 
 	private ServiceContext _createServiceContext(

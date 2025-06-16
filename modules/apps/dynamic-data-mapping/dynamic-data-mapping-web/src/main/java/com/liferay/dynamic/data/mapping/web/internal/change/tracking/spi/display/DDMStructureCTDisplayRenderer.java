brianchandotcom/liferay-portal

commit 6993d72c065ddb6e4b8a369338ca780d156b6132

LPS-147406 (dynamic-data-mapping-web) Change path and parameters

modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/java/com/liferay/dynamic/data/mapping/web/internal/change/tracking/spi/display/DDMStructureCTDisplayRenderer.java
@@ -7,14 +7,15 @@ package com.liferay.dynamic.data.mapping.web.internal.change.tracking.spi.displa
 
 import com.liferay.change.tracking.spi.display.BaseCTDisplayRenderer;
 import com.liferay.change.tracking.spi.display.CTDisplayRenderer;
-import com.liferay.dynamic.data.mapping.constants.DDMPortletKeys;
 import com.liferay.dynamic.data.mapping.model.DDMStructure;
+import com.liferay.journal.constants.JournalPortletKeys;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
 import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 
 import java.util.Locale;
@@ -60,16 +61,18 @@ public class DDMStructureCTDisplayRenderer
 
 		return PortletURLBuilder.create(
 			_portal.getControlPanelPortletURL(
-				httpServletRequest, group, DDMPortletKeys.DYNAMIC_DATA_MAPPING,
-				0, 0, PortletRequest.RENDER_PHASE)
+				httpServletRequest, group, JournalPortletKeys.JOURNAL, 0, 0,
+				PortletRequest.RENDER_PHASE)
 		).setMVCPath(
-			"/admin/edit_ddm_structure.jsp"
+			"/edit_data_definition.jsp"
 		).setRedirect(
 			_portal.getCurrentURL(httpServletRequest)
 		).setParameter(
-			"ddmStructureId", ddmStructure.getStructureId()
+			"classNameId", _portalUtil.getClassNameId(DDMStructure.class)
 		).setParameter(
-			"groupId", ddmStructure.getGroupId()
+			"classPK", ddmStructure.getStructureId()
+		).setParameter(
+			"ddmStructureId", ddmStructure.getStructureId()
 		).buildString();
 	}
 
@@ -115,5 +118,8 @@ public class DDMStructureCTDisplayRenderer
 
 	@Reference
 	private Portal _portal;
+
+	@Reference
+	private PortalUtil _portalUtil;
 
 }

modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/build.gradle
@@ -19,6 +19,7 @@ dependencies {
 	compileOnly project(":apps:frontend-taglib:frontend-taglib")
 	compileOnly project(":apps:frontend-taglib:frontend-taglib-clay")
 	compileOnly project(":apps:info:info-api")
+	compileOnly project(":apps:journal:journal-api")
 	compileOnly project(":apps:portal-template:portal-template-engine-api")
 	compileOnly project(":apps:portlet-display-template:portlet-display-template-api")
 	compileOnly project(":apps:staging:staging-api")

commit 88ced338f64c146cc4cd2dbcafe4c950f2e8ec1d

LPD-58183 Remove this nonsense reference, caused by 6993d72c

modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/java/com/liferay/dynamic/data/mapping/web/internal/change/tracking/spi/display/DDMStructureCTDisplayRenderer.java
@@ -15,7 +15,6 @@ import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
 import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.Portal;
-import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 
 import jakarta.portlet.PortletRequest;
@@ -68,7 +67,7 @@ public class DDMStructureCTDisplayRenderer
 		).setRedirect(
 			_portal.getCurrentURL(httpServletRequest)
 		).setParameter(
-			"classNameId", _portalUtil.getClassNameId(DDMStructure.class)
+			"classNameId", _portal.getClassNameId(DDMStructure.class)
 		).setParameter(
 			"classPK", ddmStructure.getStructureId()
 		).setParameter(
@@ -118,8 +117,5 @@ public class DDMStructureCTDisplayRenderer
 
 	@Reference
 	private Portal _portal;
-
-	@Reference
-	private PortalUtil _portalUtil;
 
 }
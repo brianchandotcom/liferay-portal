3f761ab88e5901a864aabf7653613ce342396922
Mauricio Valdivia <mauricio.valdivia@liferay.com>
2025-06-24 12:20:06 +0200

    LPD-53392 Refactor upgrade process to class

---
+++ modules/apps/portal-security/portal-security-service-access-policy-service/src/main/java/com/liferay/portal/security/service/access/policy/internal/upgrade/v3_0_2/SAPEntryServiceSignatureUpgradeProcess.java
@@ -0,0 +1,44 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.security.service.access.policy.internal.upgrade.v3_0_2;
+
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+
+/**
+ * @author Mauricio Valdivia
+ */
+public class SAPEntryServiceSignatureUpgradeProcess extends UpgradeProcess {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		_updateObjectEntryRelationshipServiceSignature();
+	}
+
+	private void _updateObjectEntryRelationshipServiceSignature()
+		throws Exception {
+
+		String deprecatedSignature = StringBundler.concat(
+			"com.liferay.object.rest.internal.resource.v1_0.",
+			"ObjectEntryResourceImpl#",
+			"putByExternalReferenceCodeCurrentExternalReferenceCode",
+			"ObjectRelationshipNameRelatedExternalReferenceCode");
+
+		String updatedSignature = StringBundler.concat(
+			"com.liferay.object.rest.internal.resource.v1_0.",
+			"ObjectEntryRelatedObjectsResourceImpl#",
+			"putByExternalReferenceCodeCurrentExternalReferenceCode",
+			"ObjectRelationshipNameRelatedExternalReferenceCode");
+
+		runSQL(
+			StringBundler.concat(
+				"update SAPEntry set allowedServiceSignatures = ",
+				"REPLACE(allowedServiceSignatures, '", deprecatedSignature,
+				"', '", updatedSignature, "') where allowedServiceSignatures ",
+				"like '%", deprecatedSignature, "%'"));
+	}
+
+}

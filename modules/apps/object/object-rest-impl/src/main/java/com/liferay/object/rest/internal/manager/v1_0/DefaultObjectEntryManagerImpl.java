950fc8777fe35f8ba1f3f55976c18372d5ee2fae
Guilherme Camacho <guilherme.camacho@liferay.com>
2025-06-25 17:07:03 -0300

    LPD-56548 Not necessary

--- modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/manager/v1_0/DefaultObjectEntryManagerImpl.java
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/manager/v1_0/DefaultObjectEntryManagerImpl.java
@@ -258,7 +258,7 @@ public class DefaultObjectEntryManagerImpl
 			dtoConverterContext, objectEntryId, version);
 
 		return _copyVersionedObjectEntry(
-			dtoConverterContext, objectDefinition, objectEntry, null);
+			dtoConverterContext, objectDefinition, objectEntry);
 	}
 
 	@Override
@@ -277,7 +277,7 @@ public class DefaultObjectEntryManagerImpl
 			scopeKey, version);
 
 		return _copyVersionedObjectEntry(
-			dtoConverterContext, objectDefinition, objectEntry, scopeKey);
+			dtoConverterContext, objectDefinition, objectEntry);
 	}
 
 	@Override
@@ -1293,8 +1293,7 @@ public class DefaultObjectEntryManagerImpl
 
 	private ObjectEntry _copyVersionedObjectEntry(
 			DTOConverterContext dtoConverterContext,
-			ObjectDefinition objectDefinition, ObjectEntry objectEntry,
-			String scopeKey)
+			ObjectDefinition objectDefinition, ObjectEntry objectEntry)
 		throws Exception {
 
 		objectEntry.setExternalReferenceCode(() -> null);
@@ -1302,12 +1301,9 @@ public class DefaultObjectEntryManagerImpl
 
 		_removeReadOnlyProperties(objectDefinition, objectEntry);
 
-		if (scopeKey == null) {
-			scopeKey = String.valueOf(objectEntry.getScopeKey());
-		}
-
 		return addObjectEntry(
-			dtoConverterContext, objectDefinition, objectEntry, scopeKey);
+			dtoConverterContext, objectDefinition, objectEntry,
+			objectEntry.getScopeKey());
 	}
 
 	private ServiceContext _createServiceContext(

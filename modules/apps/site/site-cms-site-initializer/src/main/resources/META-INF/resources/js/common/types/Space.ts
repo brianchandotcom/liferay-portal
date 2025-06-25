6b02d9daa665b2ff13b5d26422e2b40407e6bbd8
Bruno Carvalho <bruno.carvalho@liferay.com>
2025-06-25 07:16:29 -0300

    LPD-57575 Fix LogoColor import

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/common/types/Space.ts
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/common/types/Space.ts
@@ -3,9 +3,17 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import ClaySticker from '@clayui/sticker';
-
-export type LogoColor = React.ComponentProps<typeof ClaySticker>['displayType'];
+export type LogoColor =
+	| 'outline-0'
+	| 'outline-1'
+	| 'outline-2'
+	| 'outline-3'
+	| 'outline-4'
+	| 'outline-5'
+	| 'outline-6'
+	| 'outline-7'
+	| 'outline-8'
+	| 'outline-9';
 
 export type Space = {
 	creatorUserId: string;

8e658b97840e23e0ed73e6ea8ae80fc15aef9d8b
Marcela Cunha <marcela.cunha@liferay.com>
2025-07-07 09:15:51 -0300

    LPD-57301 Return early for scheduled status as versioning is already handled by updateStatus()

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -5810,6 +5810,12 @@ public class ObjectEntryLocalServiceImpl
 
 		_deleteTempFileEntries(dlFileEntriesMap);
 
+		if (objectEntry.isScheduled() ||
+			(originalObjectEntry.isScheduled() && objectEntry.isApproved())) {
+
+			return objectEntry;
+		}
+
 		if (objectEntry.isPending() || originalObjectEntry.isDraft() ||
 			originalObjectEntry.isExpired()) {
 

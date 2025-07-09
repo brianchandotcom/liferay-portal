211826191018838ec15a601e428a7c6a89768a9f
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2025-07-09 17:27:48 +0200

    LPD-59736 Modal shows current sites and have the ability to connect more sites.

---
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/common/services/SiteService.ts
@@ -0,0 +1,25 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {Site} from '../types/Site';
+import ApiHelper from './ApiHelper';
+
+async function connectSiteToSpace(groupId: string, siteId: string) {
+	return await ApiHelper.put<Site>(
+		`/o/headless-asset-library/v1.0/asset-libraries/${groupId}/sites/${siteId}`
+	);
+}
+
+async function getConnectedSitesToSpace(groupId: string) {
+	return await ApiHelper.get<{items: Site[]}>(
+		`/o/headless-asset-library/v1.0/asset-libraries/${groupId}/sites`
+	);
+}
+
+async function getAllSites() {
+	return await ApiHelper.get<{items: Site[]}>(`/o/headless-site/v1.0/sites`);
+}
+
+export default {connectSiteToSpace, getAllSites, getConnectedSitesToSpace};
--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/spaces/SpaceSitesModal.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/spaces/SpaceSitesModal.tsx
@@ -3,10 +3,216 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import ClayButton, {ClayButtonWithIcon} from '@clayui/button';
+import {ClayDropDownWithItems} from '@clayui/drop-down';
+import ClayLayout from '@clayui/layout';
 import ClayModal from '@clayui/modal';
-import React from 'react';
+import ClaySticker from '@clayui/sticker';
+import {openToast} from 'frontend-js-components-web';
+import React, {useEffect, useId, useState} from 'react';
 
-export default function SpaceSitesModal() {
+import {FieldPicker} from '../../common/components/forms';
+import SiteService from '../../common/services/SiteService';
+import {Site} from '../../common/types/Site';
+
+const showError = (message: string) => {
+	openToast({
+		message,
+		type: 'danger',
+	});
+};
+
+const SiteActions = ({groupId, site}: {groupId: string; site: Site}) => {
+	const {searchable} = site;
+
+	const disconnectSite = () => {
+		console.log('disconnnect: ' + site.id);
+	};
+
+	const changeSearchable = () => {
+		console.log('changeSearchable' + site.id);
+	};
+
+	return (
+		<div className="align-items-center d-flex">
+			<span className="mr-2 text-secondary">
+				{Liferay.Language.get('searchable-content')}:{' '}
+
+				{searchable
+					? Liferay.Language.get('yes')
+					: Liferay.Language.get('no')}
+			</span>
+
+			<ClayDropDownWithItems
+				items={[
+					{
+						label: searchable
+							? Liferay.Language.get('make-unsearchable')
+							: Liferay.Language.get('make-searchable'),
+						onClick: changeSearchable,
+					},
+					{
+						label: Liferay.Language.get('disconnect'),
+						onClick: disconnectSite,
+					},
+				]}
+				trigger={
+					<ClayButtonWithIcon
+						aria-label={Liferay.Language.get('site-actions')}
+						borderless
+						displayType="secondary"
+						size="sm"
+						symbol="ellipsis-v"
+						title={Liferay.Language.get('actions')}
+					/>
+				}
+			/>
+		</div>
+	);
+};
+
+const SitesSelector = ({
+	groupId,
+	onSiteConnected,
+}: {
+	groupId: string;
+	onSiteConnected?: Function;
+}) => {
+	const [sites, setSites] = useState<Site[]>([]);
+	const [siteSelected, setSiteSelected] = useState<string>();
+
+	const connectSiteToSpace = async () => {
+		if (siteSelected) {
+			const {data, error} = await SiteService.connectSiteToSpace(
+				groupId,
+				siteSelected
+			);
+
+			if (data) {
+				onSiteConnected?.({site: data});
+			}
+			else if (error) {
+				showError(
+					error ||
+						Liferay.Language.get('unable-to-connect-site-to-space')
+				);
+			}
+		}
+	};
+
+	useEffect(() => {
+		const fetchSites = async () => {
+			const {data} = await SiteService.getAllSites();
+
+			if (data) {
+				setSites(data.items as Site[]);
+			}
+		};
+
+		fetchSites();
+	}, []);
+
+	return (
+		<>
+			<ClayLayout.Row>
+				<ClayLayout.Col size={9}>
+					<FieldPicker
+						items={sites.map((site) => {
+							return {label: site.name, value: site.id};
+						})}
+						label={Liferay.Language.get('sites')}
+						name="siteSelector"
+						onSelectionChange={(value: string) => {
+							setSiteSelected(value);
+						}}
+						selectedKey={siteSelected}
+						title={Liferay.Language.get('select-a-site')}
+					/>
+				</ClayLayout.Col>
+
+				<ClayLayout.Col
+					className="align-items-center d-flex justify-content-end"
+					size={3}
+				>
+					<ClayButton onClick={connectSiteToSpace}>
+						{Liferay.Language.get('connect')}
+					</ClayButton>
+				</ClayLayout.Col>
+			</ClayLayout.Row>
+
+			<hr className="mb-4" />
+		</>
+	);
+};
+
+const EmptyResult = ({
+	groupId,
+	isAdmin,
+	onSiteConnected,
+}: {
+	groupId: string;
+	isAdmin: boolean;
+	onSiteConnected?: Function;
+}) => {
+	return (
+		<>
+			{isAdmin && (
+				<SitesSelector
+					groupId={groupId}
+					onSiteConnected={onSiteConnected}
+				/>
+			)}
+
+			<div className="text-center">
+				<h2 className="font-weight-semi-bold text-4">
+					{Liferay.Language.get('no-sites-are-connected-yet')}
+				</h2>
+
+				{isAdmin && (
+					<p className="text-3">
+						{Liferay.Language.get('connect-sites-to-this-space')}
+					</p>
+				)}
+			</div>
+		</>
+	);
+};
+
+export default function SpaceSitesModal({
+	groupId,
+	isAdmin = true,
+}: {
+	groupId: string;
+	isAdmin?: boolean;
+}) {
+	const [connectedSites, setConnectedSites] = useState<Site[]>([]);
+	const listLabelId = useId();
+
+	useEffect(() => {
+		const fetchConnectedSitesToSpace = async () => {
+			const {data} = await SiteService.getConnectedSitesToSpace(groupId);
+
+			if (data) {
+				setConnectedSites(data.items as Site[]);
+			}
+		};
+
+		fetchConnectedSitesToSpace();
+	}, [groupId]);
+
+	const onSiteConnected = ({site}: {site: Site}) => {
+		console.log('onSiteConnected');
+		setConnectedSites((prevConnectedSites) => {
+			if (
+				prevConnectedSites.some((prevSite) => prevSite.id === site.id)
+			) {
+				return prevConnectedSites;
+			}
+
+			return [...prevConnectedSites, site];
+		});
+	};
+
 	return (
 		<>
 			<ClayModal.Header>
@@ -14,7 +220,61 @@ export default function SpaceSitesModal() {
 			</ClayModal.Header>
 
 			<ClayModal.Body>
-				<p>{Liferay.Language.get('connected-sites')}</p>
+				{!connectedSites.length ? (
+					<EmptyResult
+						groupId={groupId}
+						isAdmin={isAdmin}
+						onSiteConnected={onSiteConnected}
+					/>
+				) : (
+					<>
+						{isAdmin && (
+							<>
+								<SitesSelector
+									groupId={groupId}
+									onSiteConnected={onSiteConnected}
+								/>
+
+								<label className="d-block" id={listLabelId}>
+									{Liferay.Language.get('connected-sites')}
+								</label>
+							</>
+						)}
+
+						<ul
+							aria-labelledby={listLabelId}
+							className="list-unstyled mb-0"
+						>
+							{connectedSites.map((site) => {
+								return (
+									<li className="mb-2" key={site.id}>
+										<div className="align-items-center d-flex justify-content-between">
+											<div className="align-items-center d-flex">
+												<ClaySticker
+													className="c-mr-2"
+													displayType="secondary"
+													shape="circle"
+													size="lg"
+												>
+													<ClaySticker.Image
+														alt={site.name}
+														src={site.logo}
+													/>
+												</ClaySticker>
+
+												{site.name}
+											</div>
+
+											{isAdmin && (
+												<SiteActions site={site} />
+											)}
+										</div>
+									</li>
+								);
+							})}
+						</ul>
+					</>
+				)}
 			</ClayModal.Body>
 		</>
 	);

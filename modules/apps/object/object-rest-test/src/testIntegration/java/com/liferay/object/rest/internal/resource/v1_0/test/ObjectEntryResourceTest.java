0e3fb186bd925170641788bf7138f28253375b17
brookedalton <brooke.dalton@liferay.com>
2025-07-16 15:28:09 -0700

    Revert "LPD-60244 Add previewCTCollectionId"
    This reverts commit 5f1463a23832ecf3b2a5d0ab7ba7da0dd98f2fdf.

--- modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/resource/v1_0/test/ObjectEntryResourceTest.java
+++ modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/resource/v1_0/test/ObjectEntryResourceTest.java
@@ -14533,8 +14533,6 @@ public class ObjectEntryResourceTest {
 						URLCodec.encodeURL(fileEntry.getName()), "/",
 						serviceBuilderFileEntry.getUuid(), "?version=",
 						fileVersion.getVersion(), "&t=", modifiedDate.getTime(),
-						"&previewCTCollectionId=",
-						dlFileEntry.getCtCollectionId(),
 						"&download=true&objectDefinitionExternalReferenceCode=",
 						objectDefinition.getExternalReferenceCode(),
 						"&objectEntryExternalReferenceCode=",

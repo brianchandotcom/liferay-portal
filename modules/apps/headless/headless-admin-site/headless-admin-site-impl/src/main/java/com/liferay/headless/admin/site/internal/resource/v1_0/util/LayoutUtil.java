1c83ac730fc250809d6825336257e88f47ddb399
Alberto Chaparro <alberto.chaparro@liferay.com>
2025-07-20 19:00:18 +0200

    LPD-55438 We must register a lazy reference here. Since the headless framework is not ready yet, we throw an exception. Replace when it is ready

--- modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/util/LayoutUtil.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/util/LayoutUtil.java
@@ -7,6 +7,8 @@ package com.liferay.headless.admin.site.internal.resource.v1_0.util;
 
 import com.liferay.client.extension.constants.ClientExtensionEntryConstants;
 import com.liferay.client.extension.service.ClientExtensionEntryRelLocalServiceUtil;
+import com.liferay.client.extension.type.CET;
+import com.liferay.client.extension.type.manager.CETManager;
 import com.liferay.document.library.kernel.model.DLFileEntry;
 import com.liferay.document.library.kernel.service.DLFileEntryServiceUtil;
 import com.liferay.headless.admin.site.dto.v1_0.ClientExtension;
@@ -25,6 +27,7 @@ import com.liferay.layout.page.template.service.LayoutPageTemplateEntryServiceUt
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.LayoutSet;
 import com.liferay.portal.kernel.model.LayoutSetPrototype;
+import com.liferay.portal.kernel.module.service.Snapshot;
 import com.liferay.portal.kernel.service.LayoutLocalServiceUtil;
 import com.liferay.portal.kernel.service.LayoutServiceUtil;
 import com.liferay.portal.kernel.service.LayoutSetLocalServiceUtil;
@@ -657,6 +660,8 @@ public class LayoutUtil {
 			return;
 		}
 
+		CETManager cetManager = _cetManagerSnapshot.get();
+
 		for (ClientExtension clientExtension : clientExtensions) {
 			ClientExtensionEntryRelLocalServiceUtil.addClientExtensionEntryRel(
 				serviceContext.getUserId(), layout.getGroupId(),
@@ -666,6 +671,14 @@ public class LayoutUtil {
 					clientExtension.getClientExtensionConfig(), true
 				).buildString(),
 				serviceContext);
+
+			CET cet = cetManager.getCET(
+				layout.getCompanyId(),
+				clientExtension.getExternalReferenceCode());
+
+			if (cet == null) {
+				throw new UnsupportedOperationException();
+			}
 		}
 	}
 
@@ -826,5 +839,8 @@ public class LayoutUtil {
 				layout, pageExperience, segmentsExperience, serviceContext);
 		}
 	}
+
+	private static final Snapshot<CETManager> _cetManagerSnapshot =
+		new Snapshot<>(LayoutUtil.class, CETManager.class);
 
 }

963ed0f5868e766108689ca9154d07a2d46dbd9b
Michael Hashimoto <michael.hashimoto@liferay.com>
2025-07-21 16:14:48 -0700

    LRCI-5991 Remove unused method message upstream job failure element

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/BaseBuild.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/BaseBuild.java
@@ -735,11 +735,6 @@ public abstract class BaseBuild implements Build {
 	}
 
 	@Override
-	public Element getGitHubMessageUpstreamJobFailureElement() {
-		return upstreamJobFailureMessageElement;
-	}
-
-	@Override
 	public Map<String, String> getInjectedEnvironmentVariablesMap()
 		throws IOException {
 
@@ -3079,7 +3074,6 @@ public abstract class BaseBuild implements Build {
 	protected String gitRepositoryName;
 	protected Long invokedTime;
 	protected Long startTime;
-	protected Element upstreamJobFailureMessageElement;
 
 	private void _archive(String content, boolean required, String urlSuffix) {
 		boolean readyToArchive = true;

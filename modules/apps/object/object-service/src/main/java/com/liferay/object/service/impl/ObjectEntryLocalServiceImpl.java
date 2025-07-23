48ae5a77bb63db9109208d856ff18e12830ccc65
Pedro Leite <pedro.leite@liferay.com>
2025-07-23 16:05:27 -0300

    LPD-61449 Trigger onAfterDelete when an object entry is trashed

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -2139,9 +2139,16 @@ public class ObjectEntryLocalServiceImpl
 		}
 
 		if (!ObjectActionThreadLocal.isSkipObjectActionExecution()) {
+			String objectActionTrigger =
+				ObjectActionTriggerConstants.KEY_ON_AFTER_UPDATE;
+
+			if (status == WorkflowConstants.STATUS_IN_TRASH) {
+				objectActionTrigger =
+					ObjectActionTriggerConstants.KEY_ON_AFTER_DELETE;
+			}
+
 			_executeObjectActions(
-				objectEntry.getCompanyId(),
-				ObjectActionTriggerConstants.KEY_ON_AFTER_UPDATE,
+				objectEntry.getCompanyId(), objectActionTrigger,
 				objectDefinition, objectEntry, originalObjectEntry,
 				serviceContext.getLanguageId(), user);
 		}

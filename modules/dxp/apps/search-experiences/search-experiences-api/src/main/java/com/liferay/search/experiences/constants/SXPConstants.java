5010a6fbf31b53929afdec48963f0e6eb1284efb
Brian Chan <brian.chan@liferay.com>
2025-07-25 11:42:14 +0800

    LPD-60525 Move to its own class to match SXPElementConstants

--- modules/dxp/apps/search-experiences/search-experiences-api/src/main/java/com/liferay/search/experiences/constants/SXPConstants.java
+++ modules/dxp/apps/search-experiences/search-experiences-api/src/main/java/com/liferay/search/experiences/constants/SXPConstants.java
@@ -12,6 +12,4 @@ public class SXPConstants {
 
 	public static final String RESOURCE_NAME = "com.liferay.search.experiences";
 
-	public static final String SXP_BLUEPRINT_SCHEMA_VERSION = "1.2";
-
 }

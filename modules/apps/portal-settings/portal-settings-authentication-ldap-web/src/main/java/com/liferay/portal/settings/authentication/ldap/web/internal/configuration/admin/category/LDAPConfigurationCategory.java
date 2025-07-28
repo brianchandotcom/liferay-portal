f28f42c015ed7364e85b1a4b1038f2eedeb1188f
Rafael Praxedes <rafael.praxedes@liferay.com>
2025-07-28 17:16:31 -0300

    LPD-59415 Move LDAP configuration category class to the portal-settings-authentication-ldap-web module

--- modules/apps/portal-settings/portal-settings-authentication-ldap-web/src/main/java/com/liferay/portal/settings/authentication/ldap/web/internal/configuration/admin/category/LDAPConfigurationCategory.java
+++ modules/apps/portal-settings/portal-settings-authentication-ldap-web/src/main/java/com/liferay/portal/settings/authentication/ldap/web/internal/configuration/admin/category/LDAPConfigurationCategory.java
@@ -3,10 +3,9 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.configuration.admin.web.internal.category;
+package com.liferay.portal.settings.authentication.ldap.web.internal.configuration.admin.category;
 
 import com.liferay.configuration.admin.category.ConfigurationCategory;
-
 import org.osgi.service.component.annotations.Component;
 
 /**

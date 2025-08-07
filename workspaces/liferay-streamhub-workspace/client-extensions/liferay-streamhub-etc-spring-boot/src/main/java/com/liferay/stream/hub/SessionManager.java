fddd37bd4c3cfe8674cd364f5809461e58c3de8d
Mahmoud Hussein Tayem <77802068+mahmoudhtayem87@users.noreply.github.com>
2025-08-07 19:18:06 +0300

    LPD-57523 Pushing Stream Hub Full Code

---
+++ workspaces/liferay-streamhub-workspace/client-extensions/liferay-streamhub-etc-spring-boot/src/main/java/com/liferay/stream/hub/SessionManager.java
@@ -0,0 +1,92 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.stream.hub;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketSession;
+
+/**
+ * @author Mahmoud Hussein Tayem
+ */
+@Component
+public class SessionManager {
+
+	public SessionManager(UserResource userResource) {
+		_userResource = userResource;
+	}
+
+	public void addSession(String userId, WebSocketSession session)
+		throws Exception {
+
+		if (userId.equals("Guest")) {
+			_userRoles.put(userId, List.of("Guest"));
+		}
+		else {
+			_userRoles.put(
+				userId,
+				_userResource.getUserAccountRoles(Long.valueOf(userId)));
+		}
+
+		_userSessions.computeIfAbsent(
+			userId, k -> new CopyOnWriteArraySet<>()
+		).add(
+			session
+		);
+	}
+
+	public Set<WebSocketSession> getSessions(String userId) {
+		return _userSessions.getOrDefault(userId, Collections.emptySet());
+	}
+
+	public List<String> getUsersIfInRoles(List<String> roles) {
+		List<String> userIds = new ArrayList<>();
+
+		_userRoles.forEach(
+			(userId, userRoles) -> {
+				if (userRoles.stream(
+					).anyMatch(
+						roles::contains
+					)) {
+
+					userIds.add(userId);
+				}
+			});
+
+		return userIds;
+	}
+
+	public void removeSession(WebSocketSession sessionToRemove) {
+		_userSessions.entrySet(
+		).removeIf(
+			entry -> {
+				entry.getValue(
+				).removeIf(
+					session -> Objects.equals(
+						session.getId(), sessionToRemove.getId())
+				);
+
+				return entry.getValue(
+				).isEmpty();
+			}
+		);
+	}
+
+	private final UserResource _userResource;
+	private final Map<String, List<String>> _userRoles =
+		new ConcurrentHashMap<>();
+	private final Map<String, Set<WebSocketSession>> _userSessions =
+		new ConcurrentHashMap<>();
+
+}
--- workspaces/liferay-streamhub-workspace/client-extensions/liferay-streamhub-etc-spring-boot/src/main/java/com/liferay/stream/hub/StreamHubEventRestController.java
+++ workspaces/liferay-streamhub-workspace/client-extensions/liferay-streamhub-etc-spring-boot/src/main/java/com/liferay/stream/hub/StreamHubEventRestController.java
@@ -1,12 +1,28 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
 package com.liferay.stream.hub;
 
-import com.liferay.client.extension.util.spring.boot3.BaseRestController;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import com.liferay.client.extension.util.spring.boot3.BaseRestController;
+import com.liferay.client.extension.util.spring.boot3.client.LiferayOAuth2AccessTokenManager;
+import com.liferay.stream.hub.constants.ApplicationConstants;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -14,6 +30,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Mono;
 
 /**
  * @author Mahmoud Hussein Tayem
@@ -22,11 +41,175 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StreamHubEventRestController extends BaseRestController {
 
-	@PostMapping
-	public ResponseEntity<String> post(
-		@AuthenticationPrincipal Jwt jwt, @RequestBody String json) {
+	public StreamHubEventRestController(
+		SessionManager sessionManager, WebSocketHandler webSocketHandler,
+		WebClient.Builder webClientBuilder) {
 
-		return null;
+		_sessionManager = sessionManager;
+		_webSocketHandler = webSocketHandler;
+		_webClientBuilder = webClientBuilder;
 	}
+
+	public List<JSONObject> getConfigurationEntryByObjectDefinitionId(
+			String objectDefinitionId)
+		throws JsonProcessingException {
+
+		String token = _liferayOAuth2AccessTokenManager.getAuthorization(
+			ApplicationConstants.OAUTH_SERVER_APPLICATION_NAME);
+
+		WebClient client = _webClientBuilder.baseUrl(
+			_lxcDXPServerProtocol + "://" + _lxcDXPMainDomain
+		).defaultHeader(
+			"Authorization", token
+		).build();
+
+		Mono<String> responseMono = client.get(
+		).uri(
+			uriBuilder -> uriBuilder.path(
+				ApplicationConstants.CONFIGURATION_ENDPOINT_RELATIVE_URL
+			).queryParam(
+				"filter", "objectDefinitionId eq '" + objectDefinitionId + "'"
+			).build()
+		).retrieve(
+		).bodyToMono(
+			String.class
+		);
+
+		String rawJSON = responseMono.block();
+
+		JSONObject resultJSONObject = new JSONObject(rawJSON);
+
+		if (resultJSONObject.has("totalCount") &&
+			(resultJSONObject.getInt("totalCount") > 0)) {
+
+			JSONArray itemsJSONArray = resultJSONObject.getJSONArray("items");
+
+			JSONObject streamConfigurationJSONObject =
+				itemsJSONArray.getJSONObject(0);
+
+			String configurationString =
+				streamConfigurationJSONObject.getString("configuration");
+
+			JSONObject configurationJSONObject = new JSONObject(
+				configurationString);
+
+			JSONArray actionsListJSONArray =
+				configurationJSONObject.optJSONArray("actionsList");
+
+			if (actionsListJSONArray == null) {
+				return List.of();
+			}
+
+			List<Object> rawList = actionsListJSONArray.toList();
+			List<JSONObject> jsonList = new ArrayList<>();
+
+			for (Object item : rawList) {
+				if (item instanceof Map) {
+					jsonList.add(new JSONObject((Map<?, ?>)item));
+				}
+			}
+
+			return jsonList;
+		}
+
+		return List.of();
+	}
+
+	@PostMapping({"/add", "/delete", "/update", "/standalone"})
+	public ResponseEntity<String> handleEventActions(
+			@AuthenticationPrincipal Jwt jwt, @RequestBody String json)
+		throws JsonProcessingException {
+
+		Map<String, Object> map = _objectMapper.readValue(json, Map.class);
+
+		Map<String, Object> objectEntry = (Map<String, Object>)map.get(
+			"objectEntry");
+
+		String objectActionTriggerKey = map.getOrDefault(
+			"objectActionTriggerKey", "standalone"
+		).toString();
+
+		String objectDefinitionId = map.get(
+			"objectDefinitionId"
+		).toString();
+
+		List<JSONObject> configuration =
+			getConfigurationEntryByObjectDefinitionId(objectDefinitionId);
+
+		JSONObject foundJSONObject = null;
+
+		for (JSONObject itemJSONObject : configuration) {
+			if (Objects.equals(
+					itemJSONObject.optString("type"), objectActionTriggerKey)) {
+
+				foundJSONObject = itemJSONObject;
+
+				break;
+			}
+		}
+
+		if (foundJSONObject != null) {
+			List<String> fields = List.of();
+			List<String> roles = List.of();
+			Map<String, Object> eventMessage = new HashMap<>();
+
+			if (foundJSONObject.optJSONArray("roles") != null) {
+				List<Object> rawRoles = foundJSONObject.getJSONArray(
+					"roles"
+				).toList();
+				roles = new ArrayList<>();
+
+				for (Object role : rawRoles) {
+					roles.add(role.toString());
+				}
+			}
+
+			if (foundJSONObject.optJSONArray("fields") != null) {
+				List<Object> rawFields = foundJSONObject.getJSONArray(
+					"fields"
+				).toList();
+				fields = new ArrayList<>();
+
+				for (Object field : rawFields) {
+					fields.add(field.toString());
+				}
+			}
+
+			List<String> userIds = _sessionManager.getUsersIfInRoles(roles);
+
+			Map<String, Object> values = (Map<String, Object>)objectEntry.get(
+				"values");
+
+			for (String field : fields) {
+				if (objectEntry.get(field) != null) {
+					eventMessage.put(field, objectEntry.get(field));
+				}
+				else if ((values != null) && (values.get(field) != null)) {
+					eventMessage.put(field, values.get(field));
+				}
+			}
+
+			_webSocketHandler.sendMessage(
+				_objectMapper.writeValueAsString(eventMessage),
+				foundJSONObject.optString("name", "unknown"), userIds,
+				foundJSONObject.optBoolean("enableOfflineMessageQueue", false));
+		}
+
+		return ResponseEntity.ok("Ok");
+	}
+
+	@Autowired
+	private LiferayOAuth2AccessTokenManager _liferayOAuth2AccessTokenManager;
+
+	@Value("${com.liferay.lxc.dxp.mainDomain}")
+	private String _lxcDXPMainDomain;
+
+	@Value("${com.liferay.lxc.dxp.server.protocol}")
+	private String _lxcDXPServerProtocol;
+
+	private final ObjectMapper _objectMapper = new ObjectMapper();
+	private final SessionManager _sessionManager;
+	private final WebClient.Builder _webClientBuilder;
+	private final WebSocketHandler _webSocketHandler;
 
 }

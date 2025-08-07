brianchandotcom/liferay-portal

commit 94b43e7278ba77a58f340cb20b315bca9cfb3cfc

LPD-61564 Upgrade process

modules/apps/depot/depot-service/src/main/java/com/liferay/depot/internal/upgrade/registry/DepotServiceUpgradeStepRegistrator.java
@@ -5,6 +5,7 @@
 
 package com.liferay.depot.internal.upgrade.registry;
 
+import com.liferay.depot.constants.DepotConstants;
 import com.liferay.depot.internal.upgrade.v2_2_0.util.DepotEntryPinTable;
 import com.liferay.portal.kernel.upgrade.CTModelUpgradeProcess;
 import com.liferay.portal.kernel.upgrade.UpgradeProcessFactory;
@@ -43,6 +44,18 @@ public class DepotServiceUpgradeStepRegistrator
 				"DepotAppCustomization", "DepotEntry", "DepotEntryGroupRel"));
 
 		registry.register("2.1.0", "2.2.0", DepotEntryPinTable.create());
+
+		registry.register(
+			"2.2.0", "2.3.0",
+			UpgradeProcessFactory.addColumns(
+				"DepotEntry", "type_ INTEGER", "DepotEntryGroupRel",
+				"type_ INTEGER"),
+			UpgradeProcessFactory.runSQL(
+				"update DepotEntry set type_ = " +
+					DepotConstants.TYPE_ASSET_LIBRARY),
+			UpgradeProcessFactory.runSQL(
+				"update DepotEntryGroupRel set type_ = " +
+					DepotConstants.TYPE_ASSET_LIBRARY));
 	}
 
 }

modules/apps/depot/depot-service/bnd.bnd
@@ -1,6 +1,6 @@
 Bundle-Name: Liferay Depot Service
 Bundle-SymbolicName: com.liferay.depot.service
 Bundle-Version: 3.0.73
-Liferay-Require-SchemaVersion: 2.2.0
+Liferay-Require-SchemaVersion: 2.3.0
 Liferay-Service: true
 -dsannotations-options: inherit

commit 775f305b6c40c10d4eacb02fb8203ab81a589803

LPD-61564 Fix upgrade tests in the stable suite caused by 94b43e7278ba77a58f340cb20b315bca9cfb3cfc

modules/apps/depot/depot-service/src/main/java/com/liferay/depot/internal/upgrade/registry/DepotServiceUpgradeStepRegistrator.java
@@ -47,9 +47,9 @@ public class DepotServiceUpgradeStepRegistrator
 
 		registry.register(
 			"2.2.0", "2.3.0",
+			UpgradeProcessFactory.addColumns("DepotEntry", "type_ INTEGER"),
 			UpgradeProcessFactory.addColumns(
-				"DepotEntry", "type_ INTEGER", "DepotEntryGroupRel",
-				"type_ INTEGER"),
+				"DepotEntryGroupRel", "type_ INTEGER"),
 			UpgradeProcessFactory.runSQL(
 				"update DepotEntry set type_ = " +
 					DepotConstants.TYPE_ASSET_LIBRARY),
13f69ccfea1350305716356e3e1e691486d9160e
Mikel Lorza <mikel.lorza@liferay.com>
2025-08-08 12:26:08 +0200

    LPD-59382 Update test

--- modules/apps/site/site-cms-site-initializer-test/src/testIntegration/java/com/liferay/site/cms/site/initializer/internal/display/context/test/ViewContentsSectionDisplayContextTest.java
+++ modules/apps/site/site-cms-site-initializer-test/src/testIntegration/java/com/liferay/site/cms/site/initializer/internal/display/context/test/ViewContentsSectionDisplayContextTest.java
@@ -55,7 +55,7 @@ public class ViewContentsSectionDisplayContextTest
 			getFDSActionDropdownItems();
 
 		Assert.assertEquals(
-			fdsActionDropdownItems.toString(), 9,
+			fdsActionDropdownItems.toString(), 10,
 			fdsActionDropdownItems.size());
 
 		assertFDSActionDropdownItem(
@@ -71,19 +71,22 @@ public class ViewContentsSectionDisplayContextTest
 			fdsActionDropdownItems.get(3), "share", "share", "share", "get",
 			"item");
 		assertFDSActionDropdownItem(
-			fdsActionDropdownItems.get(4), "pencil", "actionLink", "edit",
-			"get", "item");
-		assertFDSActionDropdownItem(
-			fdsActionDropdownItems.get(5), "view", "viewContent", "view", "get",
+			fdsActionDropdownItems.get(4), "view", "viewContent", "view", "get",
 			"item");
 		assertFDSActionDropdownItem(
-			fdsActionDropdownItems.get(6), "date-time", "version-history",
+			fdsActionDropdownItems.get(5), "pencil", "actionLink", "edit",
+			"get", "item");
+		assertFDSActionDropdownItem(
+			fdsActionDropdownItems.get(6), "time", "expire", "expire", "post",
+			"item");
+		assertFDSActionDropdownItem(
+			fdsActionDropdownItems.get(7), "date-time", "version-history",
 			"view-history", "get", "item");
 		assertFDSActionDropdownItem(
-			fdsActionDropdownItems.get(7), "password-policies", "permissions",
+			fdsActionDropdownItems.get(8), "password-policies", "permissions",
 			"permissions", "get", "item");
 		assertFDSActionDropdownItem(
-			fdsActionDropdownItems.get(8), "trash", "delete", "delete",
+			fdsActionDropdownItems.get(9), "trash", "delete", "delete",
 			"delete", "item");
 	}
 

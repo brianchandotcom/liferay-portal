1f0f8338dc731bbbf892787c4d013a8d7af589e1
Brittney Nguyen <brittney.nguyen@liferay.com>
2025-08-12 14:34:07 -0700

    LRCI-6082 Rename

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/test/clazz/group/BatchTestClassGroup.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/test/clazz/group/BatchTestClassGroup.java
@@ -337,8 +337,8 @@ public abstract class BatchTestClassGroup extends BaseTestClassGroup {
 		return topLevelJobName + batchJobSuffix;
 	}
 
-	public Map<String, List<String>> getGlobTestClassMethodsMap() {
-		return _globTestClassMethodsMap;
+	public Map<String, List<String>> getGlobTestClassMethodNamesMap() {
+		return _globTestClassMethodNamesMap;
 	}
 
 	@Override
@@ -675,13 +675,14 @@ public abstract class BatchTestClassGroup extends BaseTestClassGroup {
 				continue;
 			}
 
-			Map<String, List<String>> globTestClassMethodsMap =
-				globJobProperty.getGlobTestClassMethodsMap();
+			Map<String, List<String>> globTestClassMethodNamesMap =
+				globJobProperty.getGlobTestClassMethodNamesMap();
 
-			if ((globTestClassMethodsMap != null) &&
-				!globTestClassMethodsMap.isEmpty()) {
+			if ((globTestClassMethodNamesMap != null) &&
+				!globTestClassMethodNamesMap.isEmpty()) {
 
-				_globTestClassMethodsMap.putAll(globTestClassMethodsMap);
+				_globTestClassMethodNamesMap.putAll(
+					globTestClassMethodNamesMap);
 			}
 
 			for (PathMatcher globPathMatcher : globPathMatchers) {
@@ -1596,7 +1597,7 @@ public abstract class BatchTestClassGroup extends BaseTestClassGroup {
 	private final Map<String, Long> _averageTestOverheadDurations =
 		new HashMap<>();
 	private BatchHistory _batchHistory;
-	private final Map<String, List<String>> _globTestClassMethodsMap =
+	private final Map<String, List<String>> _globTestClassMethodNamesMap =
 		new HashMap<>();
 	private final List<JobProperty> _jobProperties = new ArrayList<>();
 	private final List<SegmentTestClassGroup> _segmentTestClassGroups =

a559730f6a775ec68414364c7cdcc0a3edec6fa5
timscottbell <timothy.bell@liferay.com>
2025-08-19 12:48:31 -0500

    LPD-63041 Setup blue/green rds instances

--- cloud/terraform/aws/dependencies/variables.tf
+++ cloud/terraform/aws/dependencies/variables.tf
@@ -1,14 +1,27 @@
+variable "active_data" {
+	default="blue"
+	validation {
+		condition=contains(["blue", "green"], var.active_data)
+		error_message="The active_db_slot must be either 'blue' or 'green'"
+	}
+}
 variable "cluster_name" {
 	type=string
 }
 variable "cluster_security_group_id" {
 	type=string
 }
+variable "db_restore_snapshot_identifier" {
+	default=null
+}
 variable "deployment_name" {
 	default="liferay-self-hosted"
 }
 variable "deployment_namespace" {
 	default="liferay-system"
+}
+variable "is_restoring" {
+	default=false
 }
 variable "private_subnet_ids" {
 	type=list(string)

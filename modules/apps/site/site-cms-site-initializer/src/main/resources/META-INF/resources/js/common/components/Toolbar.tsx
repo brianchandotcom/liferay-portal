10e0f93083ab878ad2ce225c846781cf27073527
Mikel Lorza <mikel.lorza@liferay.com>
2025-08-22 14:53:56 +0200

    LPD-62894 Add prop className's attributes in order to customize the class name to use in the ClayToolbar

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/common/components/Toolbar.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/common/components/Toolbar.tsx
@@ -3,6 +3,8 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import '../../../css/components/Toolbar.scss';
+
 import ClayToolbar from '@clayui/toolbar';
 import React from 'react';
 
@@ -10,33 +12,51 @@ type Props = {
 	backURL?: string;
 	children?: React.ReactNode;
 	title: string;
+	toolbarClassName?: string;
+	toolbarTitleClassName?: string;
 };
 
-function Toolbar({backURL, children, title}: Props) {
+function Toolbar({
+	backURL,
+	children,
+	title,
+	toolbarClassName,
+	toolbarTitleClassName,
+}: Props) {
+	const clayToolbarClassName = toolbarClassName
+		? toolbarClassName
+		: 'bg-white cms-control-menu px-4';
+
+	const clayToolbarTitleClassName = toolbarTitleClassName
+		? toolbarTitleClassName
+		: 'font-weight-semi-bold m-0 tbar-section text-5 text-dark';
+
 	return (
-		<ClayToolbar className="bg-white cms-control-menu px-4">
-			<ClayToolbar.Nav>
-				{backURL ? (
-					<ClayToolbar.Item>
-						<ClayToolbar.Action
-							aria-label={Liferay.Language.get('back')}
-							href={backURL}
-							symbol="angle-left"
-							title={Liferay.Language.get('back')}
-						/>
+		<ClayToolbar className={clayToolbarClassName}>
+			<div className="container-fluid">
+				<ClayToolbar.Nav>
+					{backURL ? (
+						<ClayToolbar.Item>
+							<ClayToolbar.Action
+								aria-label={Liferay.Language.get('back')}
+								href={backURL}
+								symbol="angle-left"
+								title={Liferay.Language.get('back')}
+							/>
+						</ClayToolbar.Item>
+					) : null}
+
+					<ClayToolbar.Item className="text-left" expand>
+						<ClayToolbar.Section>
+							<h1 className={clayToolbarTitleClassName}>
+								{title}
+							</h1>
+						</ClayToolbar.Section>
 					</ClayToolbar.Item>
-				) : null}
 
-				<ClayToolbar.Item className="text-left" expand>
-					<ClayToolbar.Section>
-						<h1 className="font-weight-semi-bold m-0 tbar-section text-5 text-dark">
-							{title}
-						</h1>
-					</ClayToolbar.Section>
-				</ClayToolbar.Item>
-
-				{children}
-			</ClayToolbar.Nav>
+					{children}
+				</ClayToolbar.Nav>
+			</div>
 		</ClayToolbar>
 	);
 }

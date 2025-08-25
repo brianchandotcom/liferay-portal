148aca8b89f0f8bc39c4bcb16129ddca71d29baa
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2025-08-25 08:15:17 +0200

    LPD-53145 Revert "LPD-61069 WOrdsmith"
    This reverts commit 5425fdf37067606725c4a31332305fc6ba78c1c8.

--- portal-impl/src/com/liferay/portal/upgrade/data/cleanup/NullUnicodeContentDataCleanupPreupgradeProcess.java
+++ portal-impl/src/com/liferay/portal/upgrade/data/cleanup/NullUnicodeContentDataCleanupPreupgradeProcess.java
@@ -67,9 +67,8 @@ public class NullUnicodeContentDataCleanupPreupgradeProcess
 			if (_log.isInfoEnabled() && (rowCount > 0)) {
 				_log.info(
 					StringBundler.concat(
-						"Updated ", rowCount, " row", (rowCount > 1) ? "s" : "",
-						" in ", dbInspector.normalizeName(tableName), " ",
-						rowCount, " because ",
+						"Table ", dbInspector.normalizeName(tableName), ", ",
+						rowCount, " entries updated because ",
 						dbInspector.normalizeName(columnName),
 						" had invalid characters"));
 			}

6e2bc35e4c9fc1561259610383b65ca3aff40339
Rebeca Silva <rebeca.silva@liferay.com>
2025-09-01 16:09:08 -0300

    LPD-63640 Create util functions to format text

---
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/common/utils/getFormattedText.ts
@@ -0,0 +1,14 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+function getFormattedLabel(label: string) {
+	return `<strong>${Liferay.Util.escapeHTML(label)}</strong>`;
+}
+
+function getFormattedLink(className: string, linkText: string, url: string) {
+	return `<strong><a href="${url}" class=${className}><u>${linkText}</u></a></strong>`;
+}
+
+export {getFormattedLabel, getFormattedLink};

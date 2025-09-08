999b4099a82b050642998510166530353cb6b7fe
Brian Chan <brian.chan@liferay.com>
2025-09-08 14:19:03 -0300

    LPD-64010 Rename

--- modules/apps/site/site-cms-site-initializer-test/src/testIntegration/java/com/liferay/site/cms/site/initializer/internal/service/test/ObjectEntryLocalServiceTest.java
+++ modules/apps/site/site-cms-site-initializer-test/src/testIntegration/java/com/liferay/site/cms/site/initializer/internal/service/test/ObjectEntryLocalServiceTest.java
@@ -109,8 +109,8 @@ public class ObjectEntryLocalServiceTest {
 						bundle.getSymbolicName(),
 						"com.liferay.site.initializer.cms")) {
 
-					_setUpProcessedFile(bundle, "01.object.folder");
-					_setUpProcessedFile(bundle, "02.object.definition");
+					_deleteFile(bundle, "01.object.folder");
+					_deleteFile(bundle, "02.object.definition");
 
 					CompletableFuture<Void> completableFuture =
 						_batchEngineUnitProcessor.processBatchEngineUnits(
@@ -245,7 +245,7 @@ public class ObjectEntryLocalServiceTest {
 		return false;
 	}
 
-	private void _setUpProcessedFile(Bundle bundle, String fileName) {
+	private void _deleteFile(Bundle bundle, String fileName) {
 		File file = bundle.getDataFile(
 			".com.liferay.site.initializer.cms.internal.batch." + fileName +
 				".batch.engine.data.json.0.processed");

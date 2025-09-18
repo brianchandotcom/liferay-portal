fa97d63c8d55889acacf5bb323462dbe10619988
Iván Zaera Avellón <ivan.zaera@liferay.com>
2025-09-18 09:26:02 +0200

    LPD-57326 Move HashedFilesRegistry to another package
    As per
    https://github.com/brianchandotcom/liferay-portal/pull/165624#issuecomment-3304293677

--- modules/apps/frontend-js/frontend-js-web/src/main/java/com/liferay/frontend/js/web/internal/frontend/hashed/files/HashedFilesRegistryImpl.java
+++ modules/apps/frontend-js/frontend-js-web/src/main/java/com/liferay/frontend/js/web/internal/frontend/hashed/files/HashedFilesRegistryImpl.java
@@ -3,13 +3,13 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.frontend.js.web.internal.hashed.files;
+package com.liferay.frontend.js.web.internal.frontend.hashed.files;
 
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
 import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.hashed.files.HashedFilesRegistry;
-import com.liferay.portal.kernel.hashed.files.HashedFilesUtil;
+import com.liferay.portal.kernel.frontend.hashed.files.HashedFilesRegistry;
+import com.liferay.portal.kernel.frontend.hashed.files.HashedFilesUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.StringUtil;

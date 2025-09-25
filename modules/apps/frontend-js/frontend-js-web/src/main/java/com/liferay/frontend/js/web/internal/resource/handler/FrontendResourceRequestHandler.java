2326f94f3efbd35e3a083b1a7847506d6780f43b
Iván Zaera Avellón <ivan.zaera@liferay.com>
2025-09-25 09:07:56 +0200

    LPD-57326 Move old and new classes to a new place
    As per
    https://github.com/brianchandotcom/liferay-portal/pull/165874#issuecomment-3329861653

--- modules/apps/frontend-js/frontend-js-web/src/main/java/com/liferay/frontend/js/web/internal/resource/handler/FrontendResourceRequestHandler.java
+++ modules/apps/frontend-js/frontend-js-web/src/main/java/com/liferay/frontend/js/web/internal/resource/handler/FrontendResourceRequestHandler.java
@@ -3,9 +3,9 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.frontend.js.web.internal.frontend.resource.handler;
+package com.liferay.frontend.js.web.internal.resource.handler;
 
-import com.liferay.frontend.js.web.internal.frontend.resource.FrontendResource;
+import com.liferay.frontend.js.web.internal.resource.FrontendResource;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
--- modules/apps/frontend-js/frontend-js-web/src/test/java/com/liferay/frontend/js/web/internal/resource/handler/HashedFileFrontendResourceRequestHandlerTest.java
+++ modules/apps/frontend-js/frontend-js-web/src/test/java/com/liferay/frontend/js/web/internal/resource/handler/HashedFileFrontendResourceRequestHandlerTest.java
@@ -3,9 +3,9 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.frontend.js.web.internal.frontend.resource.handler;
+package com.liferay.frontend.js.web.internal.resource.handler;
 
-import com.liferay.frontend.js.web.internal.frontend.resource.FrontendResource;
+import com.liferay.frontend.js.web.internal.resource.FrontendResource;
 import com.liferay.frontend.js.web.test.util.FrontendJSWebTestUtil;
 import com.liferay.petra.io.StreamUtil;
 import com.liferay.portal.kernel.frontend.hashed.files.HashedFilesRegistry;

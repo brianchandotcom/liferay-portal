402aadab6e22dce12d1e20d9f67070c9046fcc5f
Adriano Interaminense <adriano.interaminense@gmail.com>
2025-09-30 16:54:31 -0300

    LPD-66442 Support connecting sites to a space in the performance tab

--- modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/display/context/BaseSectionDisplayContext.java
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/display/context/BaseSectionDisplayContext.java
@@ -783,6 +783,8 @@ public abstract class BaseSectionDisplayContext {
 		}
 
 		return JSONUtil.put(
+			"externalReferenceCode", group.getExternalReferenceCode()
+		).put(
 			"groupId", group.getGroupId()
 		).put(
 			"name", group.getName(themeDisplay.getLocale())

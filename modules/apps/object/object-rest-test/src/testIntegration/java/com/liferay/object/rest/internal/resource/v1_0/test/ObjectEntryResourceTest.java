b1f402a05709933329ce40d18b64aaaa802a72be
Zákányi Miklós <miklos.zakanyi@liferay.com>
2025-10-01 11:04:37 +0200

    LPD-65423 inline

--- modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/resource/v1_0/test/ObjectEntryResourceTest.java
+++ modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/resource/v1_0/test/ObjectEntryResourceTest.java
@@ -228,6 +228,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Random;
 import java.util.Stack;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
@@ -430,32 +431,6 @@ public class ObjectEntryResourceTest {
 								NAME_STORAGE_DL_FOLDER_PATH
 						).value(
 							StringPool.SLASH + objectDefinitionName
-						).build()),
-					false),
-				ObjectFieldUtil.createObjectField(
-					ObjectFieldConstants.BUSINESS_TYPE_ATTACHMENT,
-					ObjectFieldConstants.DB_TYPE_LONG, true, false, null,
-					_OBJECT_FIELD_NAME_LARGE_ATTACHMENT_USER_COMPUTER_SOURCE,
-					_OBJECT_FIELD_NAME_LARGE_ATTACHMENT_USER_COMPUTER_SOURCE,
-					Arrays.asList(
-						new ObjectFieldSettingBuilder(
-						).name(
-							ObjectFieldSettingConstants.
-								NAME_ACCEPTED_FILE_EXTENSIONS
-						).value(
-							"txt"
-						).build(),
-						new ObjectFieldSettingBuilder(
-						).name(
-							ObjectFieldSettingConstants.NAME_FILE_SOURCE
-						).value(
-							ObjectFieldSettingConstants.VALUE_USER_COMPUTER
-						).build(),
-						new ObjectFieldSettingBuilder(
-						).name(
-							ObjectFieldSettingConstants.NAME_MAX_FILE_SIZE
-						).value(
-							String.valueOf(_MAX_LARGE_FILE_SIZE_VALUE)
 						).build()),
 					false),
 				ObjectFieldUtil.createObjectField(
@@ -9416,29 +9391,77 @@ public class ObjectEntryResourceTest {
 	@Test
 	public void testPostCustomObjectEntryWithLargeAttachmentObjectField()
 		throws Exception {
+		
+		String objectDefinitionName = ObjectDefinitionTestUtil.getRandomName();
+		String attachmentFieldName = "x" + RandomTestUtil.randomString();
 
-		JSONObject jsonObject = HTTPTestUtil.invokeToJSONObject(
-			JSONUtil.put(
-				_OBJECT_FIELD_NAME_LARGE_ATTACHMENT_USER_COMPUTER_SOURCE,
+		ObjectDefinition objectDefinition =
+			ObjectDefinitionTestUtil.publishObjectDefinition(
+				objectDefinitionName,
+				Arrays.asList(
+					ObjectFieldUtil.createObjectField(
+						ObjectFieldConstants.BUSINESS_TYPE_ATTACHMENT,
+						ObjectFieldConstants.DB_TYPE_LONG, true, false, null,
+						attachmentFieldName, attachmentFieldName,
+						Arrays.asList(
+							new ObjectFieldSettingBuilder(
+							).name(
+								ObjectFieldSettingConstants.
+									NAME_ACCEPTED_FILE_EXTENSIONS
+							).value(
+								"txt"
+							).build(),
+							new ObjectFieldSettingBuilder(
+							).name(
+								ObjectFieldSettingConstants.NAME_FILE_SOURCE
+							).value(
+								ObjectFieldSettingConstants.VALUE_USER_COMPUTER
+							).build(),
+							new ObjectFieldSettingBuilder(
+							).name(
+								ObjectFieldSettingConstants.NAME_MAX_FILE_SIZE
+							).value(
+								String.valueOf(100)
+							).build()),
+						false)),
+				ObjectDefinitionConstants.SCOPE_COMPANY);
+		
+		try {
+			byte[] data = new byte[50000000];
+	
+			new Random(
+			).nextBytes(
+				data
+			);
+	
+			JSONObject jsonObject = HTTPTestUtil.invokeToJSONObject(
 				JSONUtil.put(
-					"fileBase64", RandomTestUtil.randomBase64BySize(50000000)
+					attachmentFieldName,
+					JSONUtil.put(
+						"fileBase64",
+						java.util.Base64.getEncoder(
+						).encodeToString(
+							data
+						)
+					).put(
+						"name", StringUtil.randomString() + ".txt"
+					)
+				).toString(),
+				objectDefinition.getRESTContextPath(), Http.Method.POST);
+	
+			_assertAttachmentJSONObject(
+				_dlFileEntryLocalService.getDLFileEntry(
+					_testDLFileEntryModelListener.getLastFileEntryId()),
+				null, jsonObject.getJSONObject(attachmentFieldName),
+				JSONUtil.put(
+					"externalReferenceCode", "L_GLOBAL"
 				).put(
-					"name", StringUtil.randomString() + ".txt"
-				)
-			).toString(),
-			_objectDefinition1.getRESTContextPath(), Http.Method.POST);
-
-		_assertAttachmentJSONObject(
-			_dlFileEntryLocalService.getDLFileEntry(
-				_testDLFileEntryModelListener.getLastFileEntryId()),
-			null,
-			jsonObject.getJSONObject(
-				_OBJECT_FIELD_NAME_LARGE_ATTACHMENT_USER_COMPUTER_SOURCE),
-			JSONUtil.put(
-				"externalReferenceCode", "L_GLOBAL"
-			).put(
-				"type", Scope.Type.SITE.getValue()
-			));
+					"type", Scope.Type.SITE.getValue()
+				));
+		} finally {
+			_objectDefinitionLocalService.deleteObjectDefinition(objectDefinition);
+		}
+		
 	}
 
 	@Test
@@ -19621,8 +19644,6 @@ public class ObjectEntryResourceTest {
 
 	private static final int _MAX_FILE_SIZE_VALUE = 1;
 
-	private static final int _MAX_LARGE_FILE_SIZE_VALUE = 100;
-
 	private static final String _NEW_OBJECT_FIELD_VALUE_1 =
 		RandomTestUtil.randomString();
 
@@ -19670,10 +19691,6 @@ public class ObjectEntryResourceTest {
 
 	private static final String _OBJECT_FIELD_NAME_INTEGER =
 		"x" + RandomTestUtil.randomString();
-
-	private static final String
-		_OBJECT_FIELD_NAME_LARGE_ATTACHMENT_USER_COMPUTER_SOURCE =
-			"x" + RandomTestUtil.randomString();
 
 	private static final String _OBJECT_FIELD_NAME_LOCALIZED_LONG_TEXT =
 		"x" + RandomTestUtil.randomString();

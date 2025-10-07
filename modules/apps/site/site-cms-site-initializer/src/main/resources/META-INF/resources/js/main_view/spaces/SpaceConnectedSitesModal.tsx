af6ec4cbb6bb6a3d8c38153ba02b8abe3cfe84cb
Bruno Carvalho <bruno.carvalho@liferay.com>
2025-10-07 10:10:23 -0300

    LPD-67762 Fix typo

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/spaces/SpaceConnectedSitesModal.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/spaces/SpaceConnectedSitesModal.tsx
@@ -58,7 +58,7 @@ const ConnectedSiteActions = ({
 		showSuccessMessage(
 			sub(
 				Liferay.Language.get(
-					'site-x-disconnected-successfully-from-space'
+					'site-x-disconnected-successfully-to-space'
 				),
 				`<strong>${site.name}</strong>`
 			)
@@ -141,7 +141,7 @@ const SitesSelector = ({
 				showSuccessMessage(
 					sub(
 						Liferay.Language.get(
-							'site-x-connected-successfully-from-space'
+							'site-x-connected-successfully-to-space'
 						),
 						`<strong>${siteSelected.name}</strong>`
 					)

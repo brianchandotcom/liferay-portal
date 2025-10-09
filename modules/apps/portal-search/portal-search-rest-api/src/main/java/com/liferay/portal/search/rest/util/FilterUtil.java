77df6e542e472d4e260f39c45793ec5ae92d844f
Crescenzo Rega <97021604+CrescenzoRegaLF@users.noreply.github.com>
2025-10-09 14:48:02 +0200

    LPD-57834 Moving classes to make them usable in other modules

--- modules/apps/portal-search/portal-search-rest-api/src/main/java/com/liferay/portal/search/rest/util/FilterUtil.java
+++ modules/apps/portal-search/portal-search-rest-api/src/main/java/com/liferay/portal/search/rest/util/FilterUtil.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.search.rest.internal.util;
+package com.liferay.portal.search.rest.util;
 
 import com.liferay.portal.kernel.search.BooleanClause;
 import com.liferay.portal.kernel.search.BooleanClauseOccur;
@@ -16,8 +16,8 @@ import com.liferay.portal.kernel.search.filter.Filter;
 import com.liferay.portal.kernel.search.filter.QueryFilter;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.search.rest.internal.search.filter.BaseFilterVisitor;
-import com.liferay.portal.search.rest.internal.search.query.BaseQueryVisitor;
+import com.liferay.portal.search.rest.search.filter.BaseFilterVisitor;
+import com.liferay.portal.search.rest.search.query.BaseQueryVisitor;
 
 import java.util.Collection;
 import java.util.HashSet;

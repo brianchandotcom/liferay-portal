3588bef3ff2c93f7223b0f9947787be786844d75
Brian Chan <brian.chan@liferay.com>
2025-10-11 06:59:41 -0300

    LPD-58634 SF

--- modules/apps/portal-security/portal-security-permission-impl/src/main/java/com/liferay/portal/security/permission/internal/InlineSQLHelperImpl.java
+++ modules/apps/portal-security/portal-security-permission-impl/src/main/java/com/liferay/portal/security/permission/internal/InlineSQLHelperImpl.java
@@ -93,7 +93,6 @@ public class InlineSQLHelperImpl implements InlineSQLHelper {
 
 		PermissionChecker permissionChecker =
 			PermissionThreadLocal.getPermissionChecker();
-
 		Set<Long> disabledGroupIds = new HashSet<>();
 
 		if (_isSkipReplace(
@@ -290,7 +289,6 @@ public class InlineSQLHelperImpl implements InlineSQLHelper {
 
 		PermissionChecker permissionChecker =
 			PermissionThreadLocal.getPermissionChecker();
-
 		Set<Long> disabledGroupIds = new HashSet<>();
 
 		if ((sql == null) ||

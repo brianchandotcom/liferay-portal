5e95b82ac1b04cc9b82a8f2f2d6234a05c49664b
Alessio Rendina <asilcomando@gmail.com>
2025-10-14 18:14:16 +0200

    LPD-68466 Moved creation buttons

--- modules/apps/commerce/commerce-fragment/commerce-fragment-impl/src/main/resources/META-INF/resources/js/index.ts
+++ modules/apps/commerce/commerce-fragment/commerce-fragment-impl/src/main/resources/META-INF/resources/js/index.ts
@@ -3,9 +3,10 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-//@ts-nocheck
-export {default as PendingAccountOrdersFDSPropsTransformer} from './account_orders_data_set/props_transformers/PendingAccountOrdersFDSPropsTransformer';
-export {default as AccountSelectorButton} from './account_selector_button/AccountSelectorButton';
-export {default as AccountsFDSPropsTransformer} from './accounts_data_set/props_transformers/AccountsFDSPropsTransformer';
-export {default as CreateAccount} from './create_actions/create_account';
-export {default as CreateOrder} from './create_actions/create_order';
+// @ts-nocheck
+
+export {default as PendingAccountOrdersFDSPropsTransformer} from './account_selector/account_orders_data_set/props_transformers/PendingAccountOrdersFDSPropsTransformer';
+export {default as AccountSelectorButton} from './account_selector/account_selector_button';
+export {default as AccountsFDSPropsTransformer} from './account_selector/accounts_data_set/props_transformers/AccountsFDSPropsTransformer';
+export {default as CreateAccountButton} from './account_selector/create_account_button';
+export {default as CreateOrderButton} from './account_selector/create_order_button';

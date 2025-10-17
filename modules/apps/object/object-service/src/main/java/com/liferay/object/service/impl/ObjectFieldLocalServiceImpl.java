46bfdd9a8052255172b30afd159baff9c5fa8551
Jhosseph Gonzalez <jhossephgonzalez@gmail.com>
2025-10-17 10:02:29 -0300

    LPD-43541 remove isEnableLocalization flag from ObjectFieldLocalServiceImpl

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectFieldLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectFieldLocalServiceImpl.java
@@ -1369,9 +1369,7 @@ public class ObjectFieldLocalServiceImpl
 			return objectField;
 		}
 
-		if (objectDefinition.isEnableLocalization() &&
-			objectField.isLocalized()) {
-
+		if (objectField.isLocalized()) {
 			_alterTableDropColumn(
 				objectDefinition.getLocalizationDBTableName(),
 				objectField.getDBColumnName());

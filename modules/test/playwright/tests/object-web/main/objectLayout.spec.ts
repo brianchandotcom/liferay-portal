81348cde6e028157249c382b3d69d7b7686f08e4
Igor Franca <igor.franca@liferay.com>
2025-10-20 19:33:23 -0300

    LPD-68045 Apply FF to playwright tests

--- modules/test/playwright/tests/object-web/main/objectLayout.spec.ts
+++ modules/test/playwright/tests/object-web/main/objectLayout.spec.ts
@@ -13,6 +13,7 @@ import {expect, mergeTests} from '@playwright/test';
 import {apiHelpersTest} from '../../../fixtures/apiHelpersTest';
 import {dataApiHelpersTest} from '../../../fixtures/dataApiHelpersTest';
 import {editObjectDefinitionPagesTest} from '../../../fixtures/editObjectDefinitionPagesTest';
+import {featureFlagsTest} from '../../../fixtures/featureFlagsTest';
 import {loginTest} from '../../../fixtures/loginTest';
 import {objectPagesTest} from '../../../fixtures/objectPagesTest';
 import {getRandomInt} from '../../../utils/getRandomInt';
@@ -26,6 +27,9 @@ export const test = mergeTests(
 	apiHelpersTest,
 	dataApiHelpersTest,
 	editObjectDefinitionPagesTest,
+	featureFlagsTest({
+		'LPD-21926': {enabled: true},
+	}),
 	loginTest(),
 	objectPagesTest
 );

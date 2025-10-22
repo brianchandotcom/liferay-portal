be0cf6b6c5d42614c82274fc4e37704c4ac32de3
Alicia García <alicia.garcia@liferay.com>
2025-10-22 11:00:44 +0200

    LPD-66355 use the erc on the api calls

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/props_transformer/CategoryUsagesFDSPropsTransformer.ts
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/props_transformer/CategoryUsagesFDSPropsTransformer.ts
@@ -8,6 +8,9 @@ import {IInternalRenderer} from '@liferay/frontend-data-set-web';
 import AuthorRenderer from './cell_renderers/AuthorRenderer';
 import SpaceRendererWithCache from './cell_renderers/SpaceRendererWithCache';
 
+const OBJECT_ENTRY_FOLDER_CLASS_NAME =
+	'com.liferay.object.model.ObjectEntryFolder';
+
 export default function CategoryUsagesFDSPropsTransformer({
 	...otherProps
 }: {
@@ -25,7 +28,13 @@ export default function CategoryUsagesFDSPropsTransformer({
 				{
 					component: ({itemData}) =>
 						SpaceRendererWithCache({
-							spaceId: itemData.embedded.scopeId,
+							spaceExternalReferenceCode:
+								itemData?.entryClassName !==
+								OBJECT_ENTRY_FOLDER_CLASS_NAME
+									? itemData.embedded.systemProperties.scope
+											.externalReferenceCode
+									: itemData.embedded.scope
+											.externalReferenceCode,
 						}),
 					name: 'spaceTableCellRenderer',
 					type: 'internal',

1d0602708d2a653de065768179935596222d553c
antonio-ortega <antonio.ortega@liferay.com>
2025-10-23 09:03:40 +0200

    LPD-66079 SF

--- modules/test/playwright/tests/frontend-editor-alloyeditor-web/main/table.spec.ts
+++ modules/test/playwright/tests/frontend-editor-alloyeditor-web/main/table.spec.ts
@@ -35,35 +35,38 @@ test(
 	'AlloyEditor table allows multiple selection',
 	{tag: ['@LPD-66079']},
 	async ({page}) => {
-        const addButton = page.locator('.ae-container').getByRole('button', { name: 'Add' });
+		const addButton = page
+			.locator('.ae-container')
+			.getByRole('button', {name: 'Add'});
 
-        const tableButton = page.locator('.ae-container').getByRole('button', { name: 'Insert Table' });
+		const tableButton = page
+			.locator('.ae-container')
+			.getByRole('button', {name: 'Insert Table'});
 
-        const editorTable = page.locator('.alloy-editor-container table');
+		const editorTable = page.locator('.alloy-editor-container table');
 
 		await test.step('Add a table inside AlloyEditor and open context menu for a cell', async () => {
-            await page.locator('.ae-editable').click();
+			await page.locator('.ae-editable').click();
 
-            await addButton.waitFor({state: 'visible', timeout: 1000});
-			
-            await addButton.click();
+			await addButton.waitFor({state: 'visible', timeout: 1000});
 
-            await tableButton.waitFor({state: 'visible', timeout: 1000});
+			await addButton.click();
 
-            await tableButton.click();
+			await tableButton.waitFor({state: 'visible', timeout: 1000});
+
+			await tableButton.click();
 
 			await page.getByLabel('Confirm').click();
 
-            await expect(editorTable).toBeVisible();
-        });
+			await expect(editorTable).toBeVisible();
+		});
 
+		await test.step('Open table context menu for a cell and assert is tableSelection plugin menu', async () => {
+			editorTable.click({button: 'right'});
 
-        await test.step('Open table context menu for a cell and assert is tableSelection plugin menu', async () => {
-            editorTable.click({ button: 'right' });
+			const tableMenu = page.locator('.cke_menu_panel');
 
-            const tableMenu = page.locator('.cke_menu_panel');
-
-            await expect(tableMenu).toBeVisible();
+			await expect(tableMenu).toBeVisible();
 		});
 	}
 );

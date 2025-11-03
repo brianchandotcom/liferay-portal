3cf053215ac349381da7d9637b56d7f722255df6
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2025-11-03 10:44:00 +0100

    LPD-70380 Add test capabilities to category fragment configuration fields

--- modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/PageElementResourceTest.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/PageElementResourceTest.java
@@ -2283,7 +2283,8 @@ public class PageElementResourceTest extends BasePageElementResourceTestCase {
 					externalReferenceCode,
 					PageElementsTestUtil.
 						getFragmentInstancePageElementDefinition(
-							Collections.emptyMap(), fragmentRenderer))));
+							Collections.emptyMap(), fragmentRenderer,
+							testGroup.getGroupId()))));
 	}
 
 	private void _testPutSitePageSpecificationPageExperiencePageElementWithFragmentPageElementWithConfiguration()

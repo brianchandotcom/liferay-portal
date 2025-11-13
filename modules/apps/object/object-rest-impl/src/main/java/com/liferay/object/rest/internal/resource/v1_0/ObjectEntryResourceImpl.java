417ef11aef5a1ff93b32ffda58ce13beefb8db27
Jhosseph Gonzalez <jhossephgonzalez@gmail.com>
2025-11-13 13:51:50 -0300

    LPD-65252 add dev FF

--- modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/resource/v1_0/ObjectEntryResourceImpl.java
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/resource/v1_0/ObjectEntryResourceImpl.java
@@ -745,7 +745,10 @@ public class ObjectEntryResourceImpl
 			String externalReferenceCode, Comment comment)
 		throws Exception {
 
-		if (!_objectDefinition.isEnableComments()) {
+		if (!_objectDefinition.isEnableComments() ||
+			!FeatureFlagManagerUtil.isEnabled(
+				_objectDefinition.getCompanyId(), "LPD-69419")) {
+
 			throw new UnsupportedOperationException();
 		}
 
@@ -1023,7 +1026,10 @@ public class ObjectEntryResourceImpl
 			String scopeKey, String externalReferenceCode, Comment comment)
 		throws Exception {
 
-		if (!_objectDefinition.isEnableComments()) {
+		if (!_objectDefinition.isEnableComments() ||
+			!FeatureFlagManagerUtil.isEnabled(
+				_objectDefinition.getCompanyId(), "LPD-69419")) {
+
 			throw new UnsupportedOperationException();
 		}
 

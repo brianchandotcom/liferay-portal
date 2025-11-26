1026a1af7d8623fb86bd0a615d41fa96d1cf577f
Akhash R <akhash.ramprakash@liferay.com>
2025-11-26 15:42:03 +0530

    LPD-71300 Use Props to render creation menu

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/spaces/SpaceSummaryHeader.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/spaces/SpaceSummaryHeader.tsx
@@ -3,16 +3,28 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import ClayButton from '@clayui/button';
+import ClayButton, {ClayButtonWithIcon} from '@clayui/button';
+import ClayDropDown from '@clayui/drop-down';
+import ClayIcon from '@clayui/icon';
 import ClayLink from '@clayui/link';
-import React from 'react';
+import React, {useState} from 'react';
 
+import createAssetAction from '../props_transformer/actions/createAssetAction';
+import createFolderAction from '../props_transformer/actions/createFolderAction';
 import manageConnectedSitesAction, {
 	ManageConnectedSitesData,
 } from '../props_transformer/actions/manageConnectedSitesAction';
 import manageMembersAction, {
 	ManageMembersData,
 } from '../props_transformer/actions/manageMembersAction';
+import multipleFilesUploadAction from '../props_transformer/actions/multipleFilesUploadAction';
+import addOnClickToCreationMenuItems from '../props_transformer/utils/addOnClickToCreationMenuItems';
+
+const ACTIONS = {
+	createAsset: createAssetAction,
+	createFolder: createFolderAction,
+	uploadMultipleFiles: multipleFilesUploadAction,
+};
 
 export enum SpaceSummaryHeaderActions {
 	OPEN_MEMBERS_MODAL = 'open-members-modal',
@@ -31,6 +43,7 @@ type SpaceModalPropsType = {
 };
 
 interface SpaceSummaryHeaderProps {
+	creationMenu?: any;
 	label: string;
 	permissions?: SpaceSummaryHeaderPermissions;
 	spaceModalProps?: SpaceModalPropsType;
@@ -39,12 +52,15 @@ interface SpaceSummaryHeaderProps {
 }
 
 export default function SpaceSummaryHeader({
+	creationMenu,
 	label,
 	permissions,
 	spaceModalProps,
 	title,
 	url,
 }: SpaceSummaryHeaderProps) {
+	const [active, setActive] = useState(false);
+
 	const loadData = () => window.location.reload();
 
 	const openMembersModal = (props: SpaceModalPropsType) => {
@@ -88,20 +104,65 @@ export default function SpaceSummaryHeader({
 		<div className="align-items-center d-flex justify-content-between">
 			<h2 className="font-weight-semi-bold m-0 text-4">{title}</h2>
 
-			{url ? (
-				<ClayLink className="text-3 text-weight-semi-bold" href={url}>
-					{label}
-				</ClayLink>
-			) : (
-				<ClayButton
-					className="text-3 text-weight-semi-bold"
-					displayType="link"
-					onClick={getActionCallback}
-					size="sm"
-				>
-					{label}
-				</ClayButton>
-			)}
+			<div className="align-items-center d-flex">
+				{url ? (
+					<ClayLink
+						className="text-3 text-weight-semi-bold"
+						href={url}
+					>
+						{label}
+					</ClayLink>
+				) : (
+					<ClayButton
+						className="text-3 text-weight-semi-bold"
+						displayType="link"
+						onClick={getActionCallback}
+						size="sm"
+					>
+						{label}
+					</ClayButton>
+				)}
+
+				{creationMenu && !!creationMenu.primaryItems.length && (
+					<ClayDropDown
+						active={active}
+						className="ml-2"
+						onActiveChange={setActive}
+						trigger={
+							<ClayButtonWithIcon
+								aria-label={Liferay.Language.get('add-space')}
+								displayType="secondary"
+								small
+								symbol="plus"
+								title={Liferay.Language.get('add-space')}
+							/>
+						}
+					>
+						<ClayDropDown.ItemList>
+							{addOnClickToCreationMenuItems(
+								creationMenu.primaryItems,
+								ACTIONS
+							).map((item: any, index) => (
+								<ClayDropDown.Item
+									key={index}
+									onClick={() => {
+										setActive(false);
+										item.onClick({loadData});
+									}}
+								>
+									{item.icon && (
+										<span className="pr-2">
+											<ClayIcon symbol={item.icon} />
+										</span>
+									)}
+
+									{item.label}
+								</ClayDropDown.Item>
+							))}
+						</ClayDropDown.ItemList>
+					</ClayDropDown>
+				)}
+			</div>
 		</div>
 	);
 }

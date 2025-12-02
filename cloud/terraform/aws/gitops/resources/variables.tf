8fda316dff4a273c198d1e844acdada1c9e102cd
jaredgorski <jared.gorski@liferay.com>
2025-12-02 18:48:05 -0500

    LCD-49013 Implement argocd, split from external-secrets in both platform and resources scripts

--- cloud/terraform/aws/gitops/resources/variables.tf
+++ cloud/terraform/aws/gitops/resources/variables.tf
@@ -2,10 +2,32 @@ variable "argocd_namespace" {
 	default="argocd"
 	type=string
 }
+variable "git_repo_liferay_application_base_path" {
+	default="applications/liferay/base"
+	type=string
+}
+variable "git_repo_liferay_application_environments_pattern" {
+	default="applications/liferay/environments/**/values.yaml"
+	type=string
+}
+variable "git_repo_url" {
+	type=string
+}
 variable "git_token_property" {
 	type=string
 }
 variable "git_username_property" {
+	type=string
+}
+variable "liferay_helm_chart_name" {
+	default="liferay-aws"
+	type=string
+}
+variable "liferay_helm_chart_repo" {
+	default="oci://us-central1-docker.pkg.dev/liferay-artifact-registry/liferay-helm-chart"
+	type=string
+}
+variable "liferay_helm_chart_version" {
 	type=string
 }
 variable "remote_secret_key" {

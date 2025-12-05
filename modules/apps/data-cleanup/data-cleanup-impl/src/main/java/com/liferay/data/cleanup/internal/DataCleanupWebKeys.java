157867077e96d83fc31af28dcab96da88c2cecec
Jorge Avalos <jorge.avalos@liferay.com>
2025-12-05 09:48:33 -0800

    LPD-71334 request attributes always use WebKeys

---
+++ modules/apps/data-cleanup/data-cleanup-impl/src/main/java/com/liferay/data/cleanup/internal/DataCleanupWebKeys.java
@@ -0,0 +1,17 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.data.cleanup.internal;
+
+/**
+ * @author Jorge Avalos
+ */
+public class DataCleanupWebKeys {
+
+	public static final String MODULES_DATA_CLEANUPS = "moduleDataCleanups";
+
+	public static final String SYSTEM_DATA_CLEANUPS = "systemDataCleanups";
+
+}

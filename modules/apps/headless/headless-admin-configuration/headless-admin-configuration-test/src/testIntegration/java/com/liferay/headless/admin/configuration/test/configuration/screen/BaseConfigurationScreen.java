43941862978089d3374e68aea77a085eadb27fd6
Thiago Buarque <thiago.buarque@liferay.com>
2025-12-10 08:28:27 -0300

    LPD-71430 move to 'test' package, this is not a sample

--- modules/apps/headless/headless-admin-configuration/headless-admin-configuration-test/src/testIntegration/java/com/liferay/headless/admin/configuration/test/configuration/screen/BaseConfigurationScreen.java
+++ modules/apps/headless/headless-admin-configuration/headless-admin-configuration-test/src/testIntegration/java/com/liferay/headless/admin/configuration/test/configuration/screen/BaseConfigurationScreen.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.headless.admin.configuration.sample.configuration.screen;
+package com.liferay.headless.admin.configuration.test.configuration.screen;
 
 import com.liferay.configuration.admin.display.ConfigurationScreen;
 import com.liferay.petra.string.StringPool;
--- modules/apps/headless/headless-admin-configuration/headless-admin-configuration-test/src/testIntegration/java/com/liferay/headless/admin/configuration/test/configuration/screen/TestGroupConfigurationScreen.java
+++ modules/apps/headless/headless-admin-configuration/headless-admin-configuration-test/src/testIntegration/java/com/liferay/headless/admin/configuration/test/configuration/screen/TestGroupConfigurationScreen.java
@@ -3,15 +3,16 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.headless.admin.configuration.sample.configuration.screen;
+package com.liferay.headless.admin.configuration.test.configuration.screen;
 
 import com.liferay.configuration.admin.display.ConfigurationScreen;
 import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
-import com.liferay.portal.kernel.util.HashMapDictionary;
 
 import java.io.Serializable;
 
 import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -19,16 +20,16 @@ import org.osgi.service.component.annotations.Component;
  * @author Thiago Buarque
  */
 @Component(service = ConfigurationScreen.class)
-public class TestSystemConfigurationScreen extends BaseConfigurationScreen {
+public class TestGroupConfigurationScreen extends BaseConfigurationScreen {
 
 	@Override
 	public Dictionary<String, Object> exportProperties(Serializable scopePK) {
-		return _properties;
+		return _groupsProperties.get((long)scopePK);
 	}
 
 	@Override
 	public String getScope() {
-		return ExtendedObjectClassDefinition.Scope.SYSTEM.getValue();
+		return ExtendedObjectClassDefinition.Scope.GROUP.getValue();
 	}
 
 	@Override
@@ -36,9 +37,10 @@ public class TestSystemConfigurationScreen extends BaseConfigurationScreen {
 			Dictionary<String, Object> properties, Serializable scopePK)
 		throws Exception {
 
-		_properties = properties;
+		_groupsProperties.put((long)scopePK, properties);
 	}
 
-	private Dictionary<String, Object> _properties = new HashMapDictionary<>();
+	private final Map<Long, Dictionary<String, Object>> _groupsProperties =
+		new HashMap<>();
 
 }

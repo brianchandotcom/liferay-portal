9ae8aeaf8972fb6baf146f1df6cb1ebaad753ca0
balazssk <balazs.safrany-kovalik@liferay.com>
2025-12-11 10:46:56 +0100

    LPD-74082 Implement getEditURL methods, KBComment doesn't have any

--- modules/apps/knowledge-base/knowledge-base-uad/src/main/java/com/liferay/knowledge/base/uad/display/KBArticleUADDisplay.java
+++ modules/apps/knowledge-base/knowledge-base-uad/src/main/java/com/liferay/knowledge/base/uad/display/KBArticleUADDisplay.java
@@ -5,13 +5,40 @@
 
 package com.liferay.knowledge.base.uad.display;
 
+import com.liferay.knowledge.base.model.KBArticle;
+import com.liferay.knowledge.base.util.KnowledgeBaseUtil;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.util.Portal;
 import com.liferay.user.associated.data.display.UADDisplay;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
- * @author Brian Wing Shun Chan
+ * @author Balázs Sáfrány-Kovalik
  */
 @Component(service = UADDisplay.class)
 public class KBArticleUADDisplay extends BaseKBArticleUADDisplay {
+
+	@Override
+	public String getEditURL(
+			KBArticle kbArticle, LiferayPortletRequest liferayPortletRequest,
+			LiferayPortletResponse liferayPortletResponse)
+		throws Exception {
+
+		if (kbArticle.isInTrash()) {
+			return StringPool.BLANK;
+		}
+
+		return KnowledgeBaseUtil.getKBArticleEditURL(
+			liferayPortletRequest, true,
+			_portal.getCurrentURL(liferayPortletRequest),
+			kbArticle.getResourcePrimKey());
+	}
+
+	@Reference
+	private Portal _portal;
+
 }

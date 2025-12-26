15063c420fd1e9778aed47d119c0cb73d080ba12
Sandro Chinea Lorenzo <sandrochinea@gmail.com>
2025-12-26 11:15:49 +0000

    LPD-75159 Revert change

--- modules/test/playwright/tests/layout-page-template-admin-web/main/masterPagesAdmin.spec.ts
+++ modules/test/playwright/tests/layout-page-template-admin-web/main/masterPagesAdmin.spec.ts
@@ -291,7 +291,7 @@ test(
 	{
 		tag: '@LPS-102208',
 	},
-	async ({apiHelpers, masterPagesPage, page, pagesAdminPage, site}) => {
+	async ({apiHelpers, masterPagesPage, page, site}) => {
 
 		// Add master page template
 
@@ -309,7 +309,14 @@ test(
 
 		await masterPagesPage.goto(site.friendlyUrlPath);
 
-		await pagesAdminPage.clickNewButtonAndWaitForBlankTemplate();
+		await clickAndExpectToBeVisible({
+			autoClick: false,
+			target: page.getByRole('menuitem', {name: 'Make a Copy'}),
+			trigger: page
+				.locator('.card-page-item')
+				.filter({hasText: masterName})
+				.getByLabel('More actions'),
+		});
 
 		await hoverAndExpectToBeVisible({
 			autoClick: true,

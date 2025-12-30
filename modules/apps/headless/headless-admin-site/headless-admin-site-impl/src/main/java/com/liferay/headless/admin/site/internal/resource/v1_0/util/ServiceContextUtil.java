4aab7035a37829c53846d7a13b00de99a68cf363
Mikel Lorza <mikel.lorza@liferay.com>
2025-12-30 17:07:50 +0100

    LPD-75317 Adapt siteTemplatePageSpecificationExternalReferenceCode usages

--- modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/util/ServiceContextUtil.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/util/ServiceContextUtil.java
@@ -132,12 +132,12 @@ public class ServiceContextUtil {
 
 	public static void setLayoutSetPrototypeLayoutERC(
 			long groupId, PageSpecification pageSpecification,
-			ServiceContext serviceContext)
+			ServiceContext serviceContext,
+			String siteTemplatePageSpecificationExternalReferenceCode)
 		throws Exception {
 
 		if (Validator.isNull(
-				pageSpecification.
-					getSiteTemplatePageSpecificationExternalReferenceCode())) {
+				siteTemplatePageSpecificationExternalReferenceCode)) {
 
 			return;
 		}
@@ -190,10 +190,6 @@ public class ServiceContextUtil {
 				getLayoutSetPrototypeByUuidAndCompanyId(
 					layoutSet.getLayoutSetPrototypeUuid(),
 					layoutSet.getCompanyId());
-
-		String siteTemplatePageSpecificationExternalReferenceCode =
-			pageSpecification.
-				getSiteTemplatePageSpecificationExternalReferenceCode();
 
 		Layout layoutSetPrototypeLayout =
 			LayoutLocalServiceUtil.fetchLayoutByExternalReferenceCode(

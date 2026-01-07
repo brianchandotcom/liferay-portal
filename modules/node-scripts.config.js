1f3651e179fa7743eeced465555a698b72280da1
Davyson Melo <davyson.melo@liferay.com>
2026-01-07 13:57:04 -0300

    LPD-73437 fix: adjust config files

--- modules/node-scripts.config.js
+++ modules/node-scripts.config.js
@@ -10,12 +10,13 @@
  */
 
 module.exports = {
-	hash: '7a947846579e12dcbba88bf5de33ffb9be638ba99ca1924a1ae9ad926090005b',
+	hash: '4f614d947742aa409c9439f17ed976b647e544e611912b0456689e96a27c171b',
 	imports: {
 		'@liferay/accessibility-menu-web': [],
 		'@liferay/accessibility-settings-state-web': [],
 		'@liferay/address-web': [],
 		'@liferay/ai-creator-openai-web': [],
+		'@liferay/ai-hub-web': [],
 		'@liferay/analytics-reports-js-components-web': [],
 		'@liferay/analytics-settings-web': [],
 		'@liferay/application-list-taglib': [],

e8213b2fe894f99e95dd28fe8c56a96e588a061f
Jorge González <jorge.gonzalez@liferay.com>
2026-01-20 16:47:27 +0100

    LPD-76565 Add Export Details

--- modules/apps/export-import/export-import-web/src/main/resources/META-INF/resources/revamp/js/pages/export/steps/SetupStep.tsx
+++ modules/apps/export-import/export-import-web/src/main/resources/META-INF/resources/revamp/js/pages/export/steps/SetupStep.tsx
@@ -3,14 +3,41 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import ClayForm, {ClayInput} from '@clayui/form';
 import ClayLayout from '@clayui/layout';
+import {sub} from 'frontend-js-web';
 import React from 'react';
+
+import RequiredMark from '../../../components/forms/RequiredMark';
 
 export default function Step() {
 	return (
 		<>
 			<ClayLayout.Sheet>
-				{Liferay.Language.get('details')}
+				<div className="mb-4 sheet-header">
+					<div className="mb-2 sheet-title">
+						{sub(
+							Liferay.Language.get('x-details'),
+							Liferay.Language.get('export')
+						)}
+					</div>
+
+					<div className="sheet-text text-3">
+						{Liferay.Language.get(
+							'provide-a-descriptive-name-for-your-export'
+						)}
+					</div>
+				</div>
+
+				<ClayForm.Group>
+					<label htmlFor="nameInputText">
+						{Liferay.Language.get('name')}
+
+						<RequiredMark />
+					</label>
+
+					<ClayInput id="nameInputText" required type="text" />
+				</ClayForm.Group>
 			</ClayLayout.Sheet>
 
 			<ClayLayout.Sheet>

87fd1996c0ab77ee9316bd897b211685f2bf577a
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2026-01-28 01:06:47 -0800

    LPD-76102 add IndividualSegmentsHelper

---
+++ modules/dxp/apps/segments/segments-asah-connector/src/main/java/com/liferay/segments/asah/connector/helper/IndividualSegmentsHelper.java
@@ -0,0 +1,24 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.segments.asah.connector.helper;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.segments.model.SegmentsEntry;
+
+/**
+ * @author Rachael Koestartyo
+ */
+public interface IndividualSegmentsHelper {
+
+	public SegmentsEntry addSegmentsEntry(
+		long companyId, String id, String name);
+
+	public void updateIndividualSegmentMemberships(
+			String individualPK, Boolean removed, SegmentsEntry segmentsEntry,
+			Long userId)
+		throws PortalException;
+
+}

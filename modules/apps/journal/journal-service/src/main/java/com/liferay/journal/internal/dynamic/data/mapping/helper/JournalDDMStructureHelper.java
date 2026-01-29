5f495815dfbc420ca1ce5538d40083fac31a4353
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2026-01-29 10:01:54 +0100

    LPD-73835 Move to d.d.m.helper package

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/dynamic/data/mapping/helper/JournalDDMStructureHelper.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/dynamic/data/mapping/helper/JournalDDMStructureHelper.java
@@ -3,11 +3,12 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.journal.internal.dynamic.data.mapping.util;
+package com.liferay.journal.internal.dynamic.data.mapping.helper;
 
 import com.liferay.dynamic.data.mapping.model.DDMStructure;
 import com.liferay.dynamic.data.mapping.service.DDMFieldLocalService;
 import com.liferay.dynamic.data.mapping.util.FieldsToDDMFormValuesConverter;
+import com.liferay.journal.internal.dynamic.data.mapping.util.JournalArticleDDMStructureThreadLocal;
 import com.liferay.journal.model.JournalArticle;
 import com.liferay.journal.service.JournalArticleLocalService;
 import com.liferay.journal.util.JournalConverter;

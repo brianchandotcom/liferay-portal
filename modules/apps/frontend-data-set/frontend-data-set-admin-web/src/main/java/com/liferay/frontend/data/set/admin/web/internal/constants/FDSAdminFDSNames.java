dc62f5c7665a14d556cadac611401b964e683262
chestofwonders <miguelarroyoes@hotmail.com>
2026-02-03 11:07:46 +0100

    LPD-71991 Manage User Views admin page

---
+++ modules/apps/frontend-data-set/frontend-data-set-admin-web/src/main/java/com/liferay/frontend/data/set/admin/web/internal/constants/FDSAdminFDSNames.java
@@ -0,0 +1,15 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.frontend.data.set.admin.web.internal.constants;
+
+/**
+ * @author Miguel Arroyo
+ */
+public class FDSAdminFDSNames {
+
+	public static final String USER_VIEWS = "FDS_ADMIN_USER_VIEWS";
+
+}

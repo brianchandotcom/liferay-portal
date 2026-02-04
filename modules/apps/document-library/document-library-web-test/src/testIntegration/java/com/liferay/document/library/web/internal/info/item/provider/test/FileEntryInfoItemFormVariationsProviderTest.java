b565c0d0839f8ee9d3eb25ebd2b1dcf0ffffb69b
Brian Chan <brian.chan@liferay.com>
2026-02-04 14:57:14 +0000

    LPD-75854 Prefer this

--- modules/apps/document-library/document-library-web-test/src/testIntegration/java/com/liferay/document/library/web/internal/info/item/provider/test/FileEntryInfoItemFormVariationsProviderTest.java
+++ modules/apps/document-library/document-library-web-test/src/testIntegration/java/com/liferay/document/library/web/internal/info/item/provider/test/FileEntryInfoItemFormVariationsProviderTest.java
@@ -19,6 +19,7 @@ import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.PropsValues;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.UnicodePropertiesBuilder;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.test.rule.Inject;
@@ -139,7 +140,7 @@ public class FileEntryInfoItemFormVariationsProviderTest {
 			_companyLocalService.updatePreferences(
 				company.getCompanyId(),
 				UnicodePropertiesBuilder.put(
-					PropsKeys.LOCALES, String.join(",", PropsValues.LOCALES)
+					PropsKeys.LOCALES, StringUtil.merge(PropsValues.LOCALES)
 				).build());
 		}
 	}

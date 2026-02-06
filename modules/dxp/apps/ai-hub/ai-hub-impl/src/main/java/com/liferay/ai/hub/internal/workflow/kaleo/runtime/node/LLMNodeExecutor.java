9abdefef70bcf0d66d80fdf07e61b76274a69240
Feliphe Marinho <feliphemarinho18@gmail.com>
2026-02-06 13:36:41 -0300

    LPD-78071 rename to memoryId

--- modules/dxp/apps/ai-hub/ai-hub-impl/src/main/java/com/liferay/ai/hub/internal/workflow/kaleo/runtime/node/LLMNodeExecutor.java
+++ modules/dxp/apps/ai-hub/ai-hub-impl/src/main/java/com/liferay/ai/hub/internal/workflow/kaleo/runtime/node/LLMNodeExecutor.java
@@ -126,7 +126,7 @@ public class LLMNodeExecutor extends BaseNodeExecutor {
 			).onError(
 				throwable -> vertexAiGeminiStreamingChatModel.close()
 			).systemMessageProvider(
-				object -> VariablesUtil.applyInputVariables(
+				memoryId -> VariablesUtil.applyInputVariables(
 					executionContext, "prompt", kaleoNodeSettingValues)
 			).toolProvider(
 				MCPToolProviderUtil.create(

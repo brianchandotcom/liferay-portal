d3197087c1b37549840590c86e52466b7d2ef057
Alejandro Tardín <alejandro.tardin@liferay.com>
2026-02-11 13:24:55 +0100

    LPD-79088 Rewiring private pages to batch

--- modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/SitePageResourceImpl.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/SitePageResourceImpl.java
@@ -210,6 +210,9 @@ public class SitePageResourceImpl
 											"'")),
 							")");
 					}
+				).put(
+					"privatePages",
+					String.valueOf(portletDataContext.isPrivateLayout())
 				).build();
 			}
 
@@ -230,13 +233,7 @@ public class SitePageResourceImpl
 
 			@Override
 			public boolean isActive(PortletDataContext portletDataContext) {
-				if (!portletDataContext.isPrivateLayout() &&
-					FeatureFlagManagerUtil.isEnabled("LPD-35443")) {
-
-					return true;
-				}
-
-				return false;
+				return FeatureFlagManagerUtil.isEnabled("LPD-35443");
 			}
 
 			@Override

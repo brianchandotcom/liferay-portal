06d887220d6f61dc894b2613549209658858883c
Caleb Hall <caleb.hall@liferay.com>
2026-02-12 14:31:14 -0300

    LPD-75718 Rename customFields to ensure lambda references local variable

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-etc-spring-boot/src/main/java/com/liferay/marketplace/pubsub/MarketplaceMessageReceiver.java
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-etc-spring-boot/src/main/java/com/liferay/marketplace/pubsub/MarketplaceMessageReceiver.java
@@ -252,13 +252,13 @@ public class MarketplaceMessageReceiver implements MessageReceiver {
 			};
 		}
 
-		CustomField[] customFields = _getCustomFields(koroneikiAccount);
+		CustomField[] accountCustomFields = _getCustomFields(koroneikiAccount);
 
 		accountResource.postAccount(
 			new Account() {
 				{
 					setAccountUserAccounts(() -> userAccounts);
-					setCustomFields(() -> customFields);
+					setCustomFields(() -> accountCustomFields);
 					setDescription(koroneikiAccount::getDescription);
 					setName(koroneikiAccount::getName);
 				}
@@ -308,13 +308,13 @@ public class MarketplaceMessageReceiver implements MessageReceiver {
 		AccountResource accountResource =
 			_marketplaceService.getAccountResource();
 
-		CustomField[] customFields = _getCustomFields(koroneikiAccount);
+		CustomField[] accountCustomFields = _getCustomFields(koroneikiAccount);
 
 		accountResource.patchAccount(
 			account.getId(),
 			new Account() {
 				{
-					setCustomFields(() -> customFields);
+					setCustomFields(() -> accountCustomFields);
 					setDescription(koroneikiAccount::getDescription);
 					setName(koroneikiAccount::getName);
 				}

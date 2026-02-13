fba8bec64cce46a963e9b2edb6d16d852c12f600
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2026-02-13 13:47:07 +0100

    LPD-75099 Adapt styles

--- modules/apps/application-list/application-list-taglib/src/main/resources/META-INF/resources/js/SideNavigationSearchInput.tsx
+++ modules/apps/application-list/application-list-taglib/src/main/resources/META-INF/resources/js/SideNavigationSearchInput.tsx
@@ -4,7 +4,6 @@
  */
 
 import Form, {ClayInput} from '@clayui/form';
-import ClayIcon from '@clayui/icon';
 import {InternalDispatch, useDebounce, useIsFirstRender} from '@clayui/shared';
 import React, {useEffect, useState} from 'react';
 
@@ -25,24 +24,15 @@ function SideNavigationSearchInput({
 	}, [debouncedQuery, isFirstRender, onChange]);
 
 	return (
-		<Form.Group>
-			<ClayInput.Group>
-				<ClayInput.GroupItem>
-					<ClayInput
-						aria-label={Liferay.Language.get('search')}
-						id="com_liferay_application_list_taglib_side_navigation_search_input"
-						insetBefore
-						onChange={(event) => setQuery(event.target.value)}
-						placeholder={Liferay.Language.get('search')}
-						type="text"
-						value={query}
-					/>
-
-					<ClayInput.GroupInsetItem before>
-						<ClayIcon className="c-ml-3 c-mr-1" symbol="search" />
-					</ClayInput.GroupInsetItem>
-				</ClayInput.GroupItem>
-			</ClayInput.Group>
+		<Form.Group className="c-mx-1 c-px-2">
+			<ClayInput
+				aria-label={Liferay.Language.get('search')}
+				className="c-pl-3"
+				onChange={(event) => setQuery(event.target.value)}
+				placeholder={Liferay.Language.get('search')}
+				type="search"
+				value={query}
+			/>
 		</Form.Group>
 	);
 }

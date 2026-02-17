677d0c3d6f20ae8833ec5657717e78d8d91bf7da
nikki-pru <nikki.prudencio@liferay.com>
2026-02-17 09:28:52 -0800

    LPD-62484 buildService regen

--- modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/persistence/impl/CPDefinitionLocalizationPersistenceImpl.java
+++ modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/persistence/impl/CPDefinitionLocalizationPersistenceImpl.java
@@ -1142,22 +1142,6 @@ public class CPDefinitionLocalizationPersistenceImpl
 			}
 
 			try {
-				cpDefinitionLocalization.setName(
-					SanitizerUtil.sanitize(
-						companyId, groupId, userId,
-						CPDefinitionLocalization.class.getName(),
-						cpDefinitionLocalizationId, ContentTypes.TEXT_PLAIN,
-						Sanitizer.MODE_ALL, cpDefinitionLocalization.getName(),
-						null));
-
-				cpDefinitionLocalization.setShortDescription(
-					SanitizerUtil.sanitize(
-						companyId, groupId, userId,
-						CPDefinitionLocalization.class.getName(),
-						cpDefinitionLocalizationId, ContentTypes.TEXT_HTML,
-						Sanitizer.MODE_ALL,
-						cpDefinitionLocalization.getShortDescription(), null));
-
 				cpDefinitionLocalization.setDescription(
 					SanitizerUtil.sanitize(
 						companyId, groupId, userId,
@@ -1165,14 +1149,6 @@ public class CPDefinitionLocalizationPersistenceImpl
 						cpDefinitionLocalizationId, ContentTypes.TEXT_HTML,
 						Sanitizer.MODE_ALL,
 						cpDefinitionLocalization.getDescription(), null));
-
-				cpDefinitionLocalization.setMetaTitle(
-					SanitizerUtil.sanitize(
-						companyId, groupId, userId,
-						CPDefinitionLocalization.class.getName(),
-						cpDefinitionLocalizationId, ContentTypes.TEXT_HTML,
-						Sanitizer.MODE_ALL,
-						cpDefinitionLocalization.getMetaTitle(), null));
 
 				cpDefinitionLocalization.setMetaDescription(
 					SanitizerUtil.sanitize(
@@ -1189,6 +1165,30 @@ public class CPDefinitionLocalizationPersistenceImpl
 						cpDefinitionLocalizationId, ContentTypes.TEXT_HTML,
 						Sanitizer.MODE_ALL,
 						cpDefinitionLocalization.getMetaKeywords(), null));
+
+				cpDefinitionLocalization.setMetaTitle(
+					SanitizerUtil.sanitize(
+						companyId, groupId, userId,
+						CPDefinitionLocalization.class.getName(),
+						cpDefinitionLocalizationId, ContentTypes.TEXT_HTML,
+						Sanitizer.MODE_ALL,
+						cpDefinitionLocalization.getMetaTitle(), null));
+
+				cpDefinitionLocalization.setName(
+					SanitizerUtil.sanitize(
+						companyId, groupId, userId,
+						CPDefinitionLocalization.class.getName(),
+						cpDefinitionLocalizationId, ContentTypes.TEXT_PLAIN,
+						Sanitizer.MODE_ALL, cpDefinitionLocalization.getName(),
+						null));
+
+				cpDefinitionLocalization.setShortDescription(
+					SanitizerUtil.sanitize(
+						companyId, groupId, userId,
+						CPDefinitionLocalization.class.getName(),
+						cpDefinitionLocalizationId, ContentTypes.TEXT_HTML,
+						Sanitizer.MODE_ALL,
+						cpDefinitionLocalization.getShortDescription(), null));
 			}
 			catch (SanitizerException sanitizerException) {
 				throw new SystemException(sanitizerException);

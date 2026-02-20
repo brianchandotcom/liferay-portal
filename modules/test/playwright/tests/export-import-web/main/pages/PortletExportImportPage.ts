01f4e1cb267ff31e54a31516f3459c685375a6e3
Jaime Leon <jaime.leon@liferay.com>
2026-02-20 14:48:02 +0100

    LPD-78764 Add page to help exporting and exporting from portlets

---
+++ modules/test/playwright/tests/export-import-web/main/pages/PortletExportImportPage.ts
@@ -0,0 +1,50 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {FrameLocator, Page} from '@playwright/test';
+
+import {getTempDir} from '../../../../utils/temp';
+
+export class PortletExportImportPage {
+	readonly frame: FrameLocator;
+	readonly page: Page;
+
+	constructor(page: Page) {
+		this.frame = page.frameLocator('iframe[title="Export \\/ Import"]');
+		this.page = page;
+	}
+
+	async exportLARFile(fileNamePattern: RegExp): Promise<string> {
+		await this.frame.getByRole('button', {name: 'Export'}).click();
+
+		const downloadCell = this.frame
+			.getByRole('cell', {name: fileNamePattern})
+			.first();
+		await downloadCell.waitFor();
+
+		const downloadLink = downloadCell.getByRole('link');
+		const rawText = await downloadLink.innerText();
+
+		const fileName = rawText.split('(')[0].trim();
+		const filePath = `${getTempDir()}/${fileName}`;
+
+		const downloadPromise = this.page.waitForEvent('download');
+		await downloadLink.click();
+		const download = await downloadPromise;
+
+		await download.saveAs(filePath);
+
+		return filePath;
+	}
+
+	async importLARFile(filePath: string): Promise<void> {
+		await this.frame.getByRole('link', {name: 'Import'}).click();
+		await this.frame.locator('input[type="file"]').setInputFiles(filePath);
+		await this.frame.getByRole('button', {name: 'Continue'}).click();
+
+		await this.frame.getByText(/The import LAR file is deleted/).waitFor();
+		await this.frame.getByRole('button', {name: 'Import'}).click();
+	}
+}

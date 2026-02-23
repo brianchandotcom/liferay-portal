edca4c8985057bd144f28b79c1d86ec14e2b3065
CalumR23 <searagan@gmail.com>
2026-02-23 09:43:14 -0800

    LRCI-6878 Switch var order

--- modules/test/jenkins-results-parser/src/main/resources/com/liferay/jenkins/results/parser/dependencies/metrics/test-suite-report/js/main.js
+++ modules/test/jenkins-results-parser/src/main/resources/com/liferay/jenkins/results/parser/dependencies/metrics/test-suite-report/js/main.js
@@ -10,7 +10,7 @@ if ((typeof tableData !== 'undefined') && tableData) {
 	window.onload = function () {
 		triggerEvent(getElementByXpath('//th[contains(.,"Test Suite Name")]'), 'click');
 
-		createBarChartFromTable('Daily Server Duration by Test Suite', 'server-duration-canvas', 'Total Server Duration', tableElement, 'hrs');
+		createBarChartFromTable('Daily Server Duration by Test Suite', 'hrs', 'server-duration-canvas', 'Total Server Duration', tableElement);
 	}
 }
 

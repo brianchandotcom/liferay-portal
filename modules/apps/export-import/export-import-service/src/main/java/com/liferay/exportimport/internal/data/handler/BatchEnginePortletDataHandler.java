44cf1197634b3d44f7de7e8738a6b1618b7de925
Daniel Raposo <daniel.raposo@liferay.com>
2026-02-24 11:38:27 +0100

    LPD-79319 Retrieve system events with all referrerClassNames

--- modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/data/handler/BatchEnginePortletDataHandler.java
+++ modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/data/handler/BatchEnginePortletDataHandler.java
@@ -639,7 +639,9 @@ public class BatchEnginePortletDataHandler extends BasePortletDataHandler {
 				long modelDeletionCount =
 					_exportImportHelper.getModelDeletionCount(
 						portletDataContext,
-						new StagedModelType(modelClassName));
+						new StagedModelType(
+							modelClassName,
+							StagedModelType.REFERRER_CLASS_NAME_ALL));
 
 				manifestSummary.addModelDeletionCount(
 					modelClassName, modelDeletionCount);
@@ -914,7 +916,9 @@ public class BatchEnginePortletDataHandler extends BasePortletDataHandler {
 	private void _updateDeletionSystemEventStagedModelTypes() {
 		setDeletionSystemEventStagedModelTypes(
 			TransformUtil.transformToArray(
-				Arrays.asList(getClassNames()), StagedModelType::new,
+				Arrays.asList(getClassNames()),
+				className -> new StagedModelType(
+					className, StagedModelType.REFERRER_CLASS_NAME_ALL),
 				StagedModelType.class));
 	}
 

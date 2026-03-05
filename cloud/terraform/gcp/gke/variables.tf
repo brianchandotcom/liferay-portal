e36dc5c62691fdb296ecd86219084383c3298b0c
Allen Ziegenfus <allen.ziegenfus@liferay.com>
2026-03-05 17:46:12 -0600

    LCD-50182 SF

--- cloud/terraform/gcp/gke/variables.tf
+++ cloud/terraform/gcp/gke/variables.tf
@@ -1,26 +1,20 @@
 variable "deployment_name" {
 	type=string
 }
-
 variable "pod_cidr" {
 	default="10.1.0.0/16"
 	type=string
 }
-
 variable "project_id" {
 	type=string
 }
-
 variable "region" {
-	default="us-central1"
 	type=string
 }
-
 variable "service_cidr" {
 	default="10.2.0.0/16"
 	type=string
 }
-
 variable "vpc_cidr" {
 	default="10.0.0.0/16"
 	type=string

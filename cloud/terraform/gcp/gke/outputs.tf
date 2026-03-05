4b368f2b4db740b582cde481ba6e8c26566a1a79
Allen Ziegenfus <allen.ziegenfus@liferay.com>
2026-03-05 17:46:12 -0600

    LCD-50182 SF

--- cloud/terraform/gcp/gke/outputs.tf
+++ cloud/terraform/gcp/gke/outputs.tf
@@ -1,15 +1,12 @@
 output "network_id" {
 	value=module.vpc.network_id
 }
-
 output "network_name" {
 	value=module.vpc.network_name
 }
-
 output "subnet_id" {
 	value=module.vpc.subnets_ids[0]
 }
-
 output "subnet_name" {
 	value=module.vpc.subnets_names[0]
 }
--- cloud/terraform/gcp/gke/providers.tf
+++ cloud/terraform/gcp/gke/providers.tf
@@ -2,7 +2,6 @@ provider "google" {
 	project=var.project_id
 	region=var.region
 }
-
 terraform {
 	required_providers {
 		google={

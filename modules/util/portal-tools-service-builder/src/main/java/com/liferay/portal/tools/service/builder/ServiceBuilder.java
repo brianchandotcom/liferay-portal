brianchandotcom/liferay-portal

commit de712ed70287a75b4fc56652d15d72a6d0989e60

LPS-133366 Honor database.in.max.parameters gets if CTPersistenceHelperUtil.isProductionMode is false.

modules/util/portal-tools-service-builder/src/main/resources/com/liferay/portal/tools/service/builder/dependencies/persistence_impl.ftl
@@ -1258,6 +1258,22 @@ public class ${entity.name}PersistenceImpl extends BasePersistenceImpl<${entity.
 					return map;
 				}
 
+				if ((databaseInMaxParameters > 0) && (primaryKeys.size() > databaseInMaxParameters)) {
+					Iterator<Serializable> iterator = primaryKeys.iterator();
+
+					while (iterator.hasNext()) {
+						Set<Serializable> page = new HashSet<>();
+
+						for (int i = 0; (i < databaseInMaxParameters) && iterator.hasNext();i++) {
+							page.add(iterator.next());
+						}
+
+						map.putAll(fetchByPrimaryKeys(page));
+					}
+
+					return map;
+				}
+
 				Set<Serializable> uncachedPrimaryKeys = null;
 
 				for (Serializable primaryKey : primaryKeys) {
@@ -1364,6 +1380,22 @@ public class ${entity.name}PersistenceImpl extends BasePersistenceImpl<${entity.
 
 				if (${entity.variableName} != null) {
 					map.put(primaryKey, ${entity.variableName});
+				}
+
+				return map;
+			}
+
+			if ((databaseInMaxParameters > 0) && (primaryKeys.size() > databaseInMaxParameters)) {
+				Iterator<Serializable> iterator = primaryKeys.iterator();
+
+				while (iterator.hasNext()) {
+					Set<Serializable> page = new HashSet<>();
+
+					for (int i = 0; (i < databaseInMaxParameters) && iterator.hasNext();i++) {
+						page.add(iterator.next());
+					}
+
+					map.putAll(fetchByPrimaryKeys(page));
 				}
 
 				return map;

commit 1b9e29ebbce011b267f88f9697db37ecb1ebfaf2

LPD-82760 Fix regression caused by de712ed70287a75b4fc56652d15d72a6d0989e60 , it broke the compatibility to 700.

modules/util/portal-tools-service-builder/src/main/java/com/liferay/portal/tools/service/builder/ServiceBuilder.java
@@ -1103,6 +1103,12 @@ public class ServiceBuilder {
 				"java.class.name[", key, StringPool.CLOSE_BRACKET));
 	}
 
+	public String getCompatJavaFieldName(String key) {
+		return _compatProperties.getProperty(
+			StringBundler.concat(
+				"java.field.name[", key, StringPool.CLOSE_BRACKET));
+	}
+
 	public String getCreateMappingTableSQL(EntityMapping entityMapping)
 		throws Exception {
 
@@ -3504,6 +3510,9 @@ public class ServiceBuilder {
 
 			context.put("cacheFields", _getCacheFields(modelImplJavaClass));
 
+			context.put(
+				"databaseInMaxParameters",
+				getCompatJavaFieldName("databaseInMaxParameters"));
 			context.put("entity", entity);
 			context.put("persistence", Boolean.TRUE);
 			context.put("referenceEntities", _mergeReferenceEntities(entity));

modules/util/portal-tools-service-builder/src/main/resources/com/liferay/portal/tools/service/builder/dependencies/7_0_0/compatibility.properties
@@ -21,6 +21,8 @@ java.class.name[ProviderType]=aQute.bnd.annotation.ProviderType
 java.class.name[StringBundler]=com.liferay.portal.kernel.util.StringBundler
 java.class.name[UnsyncByteArrayInputStream]=com.liferay.portal.kernel.io.unsync.UnsyncByteArrayInputStream
 
+java.field.name[databaseInMaxParameters]=_databaseInMaxParameters
+
 #
 # The update SQL file should be created and updated manually. Set this to true
 # to update the update SQL file automatically when running Service Builder.

modules/util/portal-tools-service-builder/src/main/resources/com/liferay/portal/tools/service/builder/dependencies/7_1_0/compatibility.properties
@@ -21,6 +21,8 @@ java.class.name[ProviderType]=aQute.bnd.annotation.ProviderType
 java.class.name[StringBundler]=com.liferay.portal.kernel.util.StringBundler
 java.class.name[UnsyncByteArrayInputStream]=com.liferay.petra.io.unsync.UnsyncByteArrayInputStream
 
+java.field.name[databaseInMaxParameters]=databaseInMaxParameters
+
 #
 # The update SQL file should be created and updated manually. Set this to true
 # to update the update SQL file automatically when running Service Builder.

modules/util/portal-tools-service-builder/src/main/resources/com/liferay/portal/tools/service/builder/dependencies/7_2_0/compatibility.properties
@@ -562,6 +562,8 @@ java.class.name[ProviderType]=org.osgi.annotation.versioning.ProviderType
 java.class.name[StringBundler]=com.liferay.petra.string.StringBundler
 java.class.name[UnsyncByteArrayInputStream]=com.liferay.petra.io.unsync.UnsyncByteArrayInputStream
 
+java.field.name[databaseInMaxParameters]=databaseInMaxParameters
+
 #
 # The update SQL file should be created and updated manually. Set this to true
 # to update the update SQL file automatically when running Service Builder.

modules/util/portal-tools-service-builder/src/main/resources/com/liferay/portal/tools/service/builder/dependencies/7_3_0/compatibility.properties
@@ -562,6 +562,8 @@ java.class.name[ProviderType]=org.osgi.annotation.versioning.ProviderType
 java.class.name[StringBundler]=com.liferay.petra.string.StringBundler
 java.class.name[UnsyncByteArrayInputStream]=com.liferay.petra.io.unsync.UnsyncByteArrayInputStream
 
+java.field.name[databaseInMaxParameters]=databaseInMaxParameters
+
 #
 # The update SQL file should be created and updated manually. Set this to true
 # to update the update SQL file automatically when running Service Builder.

modules/util/portal-tools-service-builder/src/main/resources/com/liferay/portal/tools/service/builder/dependencies/7_4_0/compatibility.properties
@@ -562,6 +562,8 @@ java.class.name[ProviderType]=org.osgi.annotation.versioning.ProviderType
 java.class.name[StringBundler]=com.liferay.petra.string.StringBundler
 java.class.name[UnsyncByteArrayInputStream]=com.liferay.petra.io.unsync.UnsyncByteArrayInputStream
 
+java.field.name[databaseInMaxParameters]=databaseInMaxParameters
+
 #
 # The update SQL file should be created and updated manually. Set this to true
 # to update the update SQL file automatically when running Service Builder.

modules/util/portal-tools-service-builder/src/main/resources/com/liferay/portal/tools/service/builder/dependencies/persistence_impl.ftl
@@ -75,6 +75,8 @@ import com.liferay.portal.kernel.configuration.Configuration;
 	import com.liferay.portal.kernel.configuration.Filter;
 	import com.liferay.portal.kernel.dao.db.DBManagerUtil;
 	import com.liferay.portal.kernel.dao.db.DBType;
+
+	import java.lang.reflect.Array;
 </#if>
 
 import com.liferay.portal.kernel.dao.orm.ArgumentsResolver;
@@ -255,6 +257,44 @@ public class ${entity.name}PersistenceImpl extends BasePersistenceImpl<${entity.
 	</#if>
 
 	<#if !serviceBuilder.isVersionGTE_7_1_0()>
+		private static Object _split(Object array, int splitSize) {
+			int length = Array.getLength(array);
+
+			int pageCount = length / splitSize;
+
+			if ((length % splitSize) > 0) {
+				pageCount++;
+			}
+
+			Class<?> clazz = array.getClass();
+
+			Class<?> componentType = clazz.getComponentType();
+
+			Object newArray = Array.newInstance(
+				componentType, pageCount, splitSize);
+
+			if (pageCount == 1) {
+				Array.set(newArray, 0, array);
+
+				return newArray;
+			}
+
+			for (int i = 0; i < pageCount; i++) {
+				int end = Math.min(length, splitSize * (i + 1));
+				int start = splitSize * i;
+
+				int elementLength = end - start;
+
+				Object element = Array.newInstance(componentType, elementLength);
+
+				System.arraycopy(array, start, element, 0, elementLength);
+
+				Array.set(newArray, i, element);
+			}
+
+			return newArray;
+		}
+
 		private int _databaseInMaxParameters;
 	</#if>
 
@@ -1355,39 +1395,21 @@ public class ${entity.name}PersistenceImpl extends BasePersistenceImpl<${entity.
 					return map;
 				}
 
-				<#if serviceBuilder.isVersionGTE_7_1_0()>
-					if ((databaseInMaxParameters > 0) && (primaryKeys.size() > databaseInMaxParameters)) {
-						Iterator<Serializable> iterator = primaryKeys.iterator();
+				if ((${databaseInMaxParameters} > 0) && (primaryKeys.size() > ${databaseInMaxParameters})) {
+					Iterator<Serializable> iterator = primaryKeys.iterator();
 
-						while (iterator.hasNext()) {
-							Set<Serializable> page = new HashSet<>();
+					while (iterator.hasNext()) {
+						Set<Serializable> page = new HashSet<>();
 
-							for (int i = 0; (i < databaseInMaxParameters) && iterator.hasNext(); i++) {
-								page.add(iterator.next());
-							}
-
-							map.putAll(fetchByPrimaryKeys(page));
+						for (int i = 0; (i < ${databaseInMaxParameters}) && iterator.hasNext(); i++) {
+							page.add(iterator.next());
 						}
 
-						return map;
+						map.putAll(fetchByPrimaryKeys(page));
 					}
-				<#else>
-					if ((_databaseInMaxParameters > 0) && (primaryKeys.size() > _databaseInMaxParameters)) {
-						Iterator<Serializable> iterator = primaryKeys.iterator();
 
-						while (iterator.hasNext()) {
-							Set<Serializable> page = new HashSet<>();
-
-							for (int i = 0; (i < _databaseInMaxParameters) && iterator.hasNext(); i++) {
-								page.add(iterator.next());
-							}
-
-							map.putAll(fetchByPrimaryKeys(page));
-						}
-
-						return map;
-					}
-				</#if>
+					return map;
+				}
 
 				Set<Serializable> uncachedPrimaryKeys = null;
 
@@ -1526,13 +1548,13 @@ public class ${entity.name}PersistenceImpl extends BasePersistenceImpl<${entity.
 				return map;
 			}
 
-			if ((databaseInMaxParameters > 0) && (primaryKeys.size() > databaseInMaxParameters)) {
+			if ((${databaseInMaxParameters} > 0) && (primaryKeys.size() > ${databaseInMaxParameters})) {
 				Iterator<Serializable> iterator = primaryKeys.iterator();
 
 				while (iterator.hasNext()) {
 					Set<Serializable> page = new HashSet<>();
 
-					for (int i = 0; (i < databaseInMaxParameters) && iterator.hasNext();i++) {
+					for (int i = 0; (i < ${databaseInMaxParameters}) && iterator.hasNext();i++) {
 						page.add(iterator.next());
 					}
 

modules/util/portal-tools-service-builder/src/main/resources/com/liferay/portal/tools/service/builder/dependencies/persistence_impl_finder_count.ftl
@@ -328,8 +328,8 @@ public int countBy${entityFinder.name}(
 
 		if (count == null) {
 			try {
-				if ((databaseInMaxParameters > 0) && (<#list entityFinderArrayableColsList as arrayablefinderCol>
-							(${arrayablefinderCol.pluralName}.length > databaseInMaxParameters)
+				if ((${databaseInMaxParameters} > 0) && (<#list entityFinderArrayableColsList as arrayablefinderCol>
+							(${arrayablefinderCol.pluralName}.length > ${databaseInMaxParameters})
 
 							<#if arrayablefinderCol_has_next>
 								||
@@ -338,7 +338,7 @@ public int countBy${entityFinder.name}(
 						count = Long.valueOf(0);
 
 						<#list entityFinderArrayableColsList as arrayablefinderCol>
-							${arrayablefinderCol.type}[][] ${arrayablefinderCol.pluralName}Pages = (${arrayablefinderCol.type}[][])ArrayUtil.split(${arrayablefinderCol.pluralName}, databaseInMaxParameters);
+							${arrayablefinderCol.type}[][] ${arrayablefinderCol.pluralName}Pages = (${arrayablefinderCol.type}[][])<#if serviceBuilder.isVersionGTE_7_1_0()>ArrayUtil.split<#else>_split</#if>(${arrayablefinderCol.pluralName}, ${databaseInMaxParameters});
 						</#list>
 
 						<#list entityFinderArrayableColsList as arrayablefinderCol>

modules/util/portal-tools-service-builder/src/main/resources/com/liferay/portal/tools/service/builder/dependencies/persistence_impl_finder_find.ftl
@@ -2110,8 +2110,8 @@ that may or may not be enforced with a unique index at the database level. Case
 
 		if (list == null) {
 			try {
-				if ((start == QueryUtil.ALL_POS) && (end == QueryUtil.ALL_POS) && (databaseInMaxParameters > 0) && (<#list entityFinderArrayableColsList as arrayableentityColumn>
-						(${arrayableentityColumn.pluralName}.length > databaseInMaxParameters)
+				if ((start == QueryUtil.ALL_POS) && (end == QueryUtil.ALL_POS) && (${databaseInMaxParameters} > 0) && (<#list entityFinderArrayableColsList as arrayableentityColumn>
+						(${arrayableentityColumn.pluralName}.length > ${databaseInMaxParameters})
 
 						<#if arrayableentityColumn_has_next>
 							||
@@ -2121,7 +2121,7 @@ that may or may not be enforced with a unique index at the database level. Case
 					list = new ArrayList<${entity.name}>();
 
 					<#list entityFinderArrayableColsList as arrayableentityColumn>
-						${arrayableentityColumn.type}[][] ${arrayableentityColumn.pluralName}Pages = (${arrayableentityColumn.type}[][])ArrayUtil.split(${arrayableentityColumn.pluralName}, databaseInMaxParameters);
+						${arrayableentityColumn.type}[][] ${arrayableentityColumn.pluralName}Pages = (${arrayableentityColumn.type}[][])<#if serviceBuilder.isVersionGTE_7_1_0()>ArrayUtil.split<#else>_split</#if>(${arrayableentityColumn.pluralName}, ${databaseInMaxParameters});
 					</#list>
 
 					<#list entityFinderArrayableColsList as arrayableentityColumn>
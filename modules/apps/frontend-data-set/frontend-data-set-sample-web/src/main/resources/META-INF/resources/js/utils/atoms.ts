35dd03dad4dac62a8de23ba9fe051b612db1e6bd
antonio-ortega <antonio.ortega@liferay.com>
2026-03-16 21:34:42 +0100

    LPD-78124 Fix our samples to avoid misunderstandings

--- modules/apps/frontend-data-set/frontend-data-set-sample-web/src/main/resources/META-INF/resources/js/utils/atoms.ts
+++ modules/apps/frontend-data-set/frontend-data-set-sample-web/src/main/resources/META-INF/resources/js/utils/atoms.ts
@@ -6,14 +6,11 @@
 import {IFDSState} from '@liferay/frontend-data-set-web';
 import {State} from '@liferay/frontend-js-state-web';
 
-const advancedFDSAtom = State.atom<IFDSState>(
-	'com_liferay_frontend_data_set_sample_web_internal_portlet_FDSSamplePortlet-advanced_fdsState',
-	{
-		filters: [],
-		search: {
-			query: '',
-		},
-	}
-);
+const advancedFDSAtom = State.atom<IFDSState>('advancedFDSAtom', {
+	filters: [],
+	search: {
+		query: '',
+	},
+});
 
 export {advancedFDSAtom};

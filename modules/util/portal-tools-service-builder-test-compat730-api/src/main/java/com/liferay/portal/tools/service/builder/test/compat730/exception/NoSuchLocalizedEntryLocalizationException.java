8f6dcf04e067ed06bd6fad3da07ad4e29f94c4de
shuyangzhou <shuyang.zhou@liferay.com>
2026-03-17 16:55:39 -0700

    LPD-82562 Delete all java files to regenerate to clean up renaming leftovers

--- modules/util/portal-tools-service-builder-test-compat730-api/src/main/java/com/liferay/portal/tools/service/builder/test/compat730/exception/NoSuchLocalizedEntryLocalizationException.java
+++ modules/util/portal-tools-service-builder-test-compat730-api/src/main/java/com/liferay/portal/tools/service/builder/test/compat730/exception/NoSuchLocalizedEntryLocalizationException.java
@@ -2,7 +2,6 @@
  * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
-
 package com.liferay.portal.tools.service.builder.test.compat730.exception;
 
 import com.liferay.portal.kernel.exception.NoSuchModelException;
@@ -10,8 +9,7 @@ import com.liferay.portal.kernel.exception.NoSuchModelException;
 /**
  * @author Brian Wing Shun Chan
  */
-public class NoSuchLocalizedEntryLocalizationException
-	extends NoSuchModelException {
+public class NoSuchLocalizedEntryLocalizationException extends NoSuchModelException {
 
 	public NoSuchLocalizedEntryLocalizationException() {
 	}
@@ -20,9 +18,7 @@ public class NoSuchLocalizedEntryLocalizationException
 		super(msg);
 	}
 
-	public NoSuchLocalizedEntryLocalizationException(
-		String msg, Throwable throwable) {
-
+	public NoSuchLocalizedEntryLocalizationException(String msg, Throwable throwable) {
 		super(msg, throwable);
 	}
 
--- modules/util/portal-tools-service-builder-test-compat740-api/src/main/java/com/liferay/portal/tools/service/builder/test/compat740/exception/NoSuchERCVersionedEntryVersionException.java
+++ modules/util/portal-tools-service-builder-test-compat740-api/src/main/java/com/liferay/portal/tools/service/builder/test/compat740/exception/NoSuchERCVersionedEntryVersionException.java
@@ -2,7 +2,6 @@
  * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
-
 package com.liferay.portal.tools.service.builder.test.compat740.exception;
 
 import com.liferay.portal.kernel.exception.NoSuchModelException;
@@ -10,8 +9,7 @@ import com.liferay.portal.kernel.exception.NoSuchModelException;
 /**
  * @author Brian Wing Shun Chan
  */
-public class NoSuchERCVersionedEntryVersionException
-	extends NoSuchModelException {
+public class NoSuchERCVersionedEntryVersionException extends NoSuchModelException {
 
 	public NoSuchERCVersionedEntryVersionException() {
 	}
@@ -20,9 +18,7 @@ public class NoSuchERCVersionedEntryVersionException
 		super(msg);
 	}
 
-	public NoSuchERCVersionedEntryVersionException(
-		String msg, Throwable throwable) {
-
+	public NoSuchERCVersionedEntryVersionException(String msg, Throwable throwable) {
 		super(msg, throwable);
 	}
 

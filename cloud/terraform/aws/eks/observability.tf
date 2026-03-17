45e7de6f78a0f428a99bb8ddde0405f288313063
Brian Greenwald <brian.greenwald@liferay.com>
2026-03-17 11:50:11 -0500

    LCD-50300 Convert from module

--- cloud/terraform/aws/eks/observability.tf
+++ cloud/terraform/aws/eks/observability.tf
@@ -1,9 +1,10 @@
 resource "aws_grafana_workspace" "amg" {
 	account_access_type="CURRENT_ACCOUNT"
 	authentication_providers=["AWS_SSO"]
+	count=var.observability_config.enabled ? 1 : 0
 	name="${var.deployment_name}-amg-workspace"
 	permission_type="SERVICE_MANAGED"
-	role_arn=aws_iam_role.grafana.arn
+	role_arn=aws_iam_role.grafana[0].arn
 }
 resource "aws_iam_role" "grafana" {
 	assume_role_policy=jsonencode(
@@ -19,8 +20,10 @@ resource "aws_iam_role" "grafana" {
 			]
 			Version="2012-10-17"
 		})
+	count=var.observability_config.enabled ? 1 : 0
 	name="grafana-role"
 }
 resource "aws_prometheus_workspace" "amp" {
 	alias="${var.deployment_name}-amp-workspace"
+	count=var.observability_config.enabled ? 1 : 0
 }

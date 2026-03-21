53cde66d4a7f9b8d33ae103ae89ce82c236d9cd3
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2026-03-21 17:37:29 +0100

    LPD-83349 Don't use test utils to make the connection clear
    Using test utils obscure the fact that we're testing the methods in
    the object definition local service.

--- modules/apps/site/site-cms-site-initializer-test/src/testIntegration/java/com/liferay/site/cms/site/initializer/internal/service/test/CMSPermissionsObjectDefinitionLocalServiceWrapperTest.java
+++ modules/apps/site/site-cms-site-initializer-test/src/testIntegration/java/com/liferay/site/cms/site/initializer/internal/service/test/CMSPermissionsObjectDefinitionLocalServiceWrapperTest.java
@@ -13,26 +13,28 @@ import com.liferay.object.constants.ObjectFolderConstants;
 import com.liferay.object.field.util.ObjectFieldUtil;
 import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.model.ObjectFolder;
+import com.liferay.object.service.ObjectDefinitionLocalService;
 import com.liferay.object.service.ObjectFolderLocalService;
-import com.liferay.object.test.util.ObjectDefinitionTestUtil;
 import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.model.role.RoleConstants;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
 import com.liferay.portal.kernel.service.RoleLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
-import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.test.rule.FeatureFlag;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+import com.liferay.portal.vulcan.util.LocalizedMapUtil;
 import com.liferay.site.cms.site.initializer.test.util.CMSTestUtil;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.Assert;
@@ -114,26 +116,37 @@ public class CMSPermissionsObjectDefinitionLocalServiceWrapperTest {
 				role.getRoleId(), ActionKeys.VIEW));
 	}
 
-	private ObjectDefinition _publishObjectDefinition(long objectFolderId)
+	private ObjectDefinition _publishCustomObjectDefinition(long objectFolderId)
 		throws Exception {
 
 		ObjectDefinition objectDefinition =
-			ObjectDefinitionTestUtil.publishObjectDefinition(
-				"A" + RandomTestUtil.randomString(),
-				ListUtil.fromArray(
+			_objectDefinitionLocalService.addCustomObjectDefinition(
+				null, TestPropsValues.getUserId(), objectFolderId, null, false,
+				true, false, true, false, false, false, false, null,
+				LocalizedMapUtil.getLocalizedMap(RandomTestUtil.randomString()),
+				"A" + RandomTestUtil.randomString(), null, null,
+				LocalizedMapUtil.getLocalizedMap(RandomTestUtil.randomString()),
+				true, ObjectDefinitionConstants.SCOPE_DEPOT,
+				ObjectDefinitionConstants.STORAGE_TYPE_DEFAULT,
+				Collections.emptyList(),
+				Collections.singletonList(
 					ObjectFieldUtil.createObjectField(
 						ObjectFieldConstants.BUSINESS_TYPE_TEXT,
 						ObjectFieldConstants.DB_TYPE_STRING,
 						RandomTestUtil.randomString(), "text")),
-				objectFolderId, ObjectDefinitionConstants.SCOPE_DEPOT,
-				TestPropsValues.getUserId());
+				Collections.emptyList(), new ServiceContext());
+
+		objectDefinition =
+			_objectDefinitionLocalService.publishCustomObjectDefinition(
+				TestPropsValues.getUserId(),
+				objectDefinition.getObjectDefinitionId());
 
 		_objectDefinitions.add(objectDefinition);
 
 		return objectDefinition;
 	}
 
-	private ObjectDefinition _publishObjectDefinition(
+	private ObjectDefinition _publishCustomObjectDefinition(
 			String objectFolderExternalReferenceCode)
 		throws Exception {
 
@@ -142,13 +155,13 @@ public class CMSPermissionsObjectDefinitionLocalServiceWrapperTest {
 				objectFolderExternalReferenceCode,
 				TestPropsValues.getCompanyId());
 
-		return _publishObjectDefinition(objectFolder.getObjectFolderId());
+		return _publishCustomObjectDefinition(objectFolder.getObjectFolderId());
 	}
 
 	private void _testPublishCustomObjectDefinitionInContentStructuresFolder()
 		throws Exception {
 
-		ObjectDefinition objectDefinition = _publishObjectDefinition(
+		ObjectDefinition objectDefinition = _publishCustomObjectDefinition(
 			ObjectFolderConstants.EXTERNAL_REFERENCE_CODE_CONTENT_STRUCTURES);
 
 		_assertCMSAdministratorObjectDefinitionPermissions(objectDefinition);
@@ -161,7 +174,7 @@ public class CMSPermissionsObjectDefinitionLocalServiceWrapperTest {
 	private void _testPublishCustomObjectDefinitionInDefaultFolder()
 		throws Exception {
 
-		ObjectDefinition objectDefinition = _publishObjectDefinition(0);
+		ObjectDefinition objectDefinition = _publishCustomObjectDefinition(0);
 
 		long companyId = TestPropsValues.getCompanyId();
 
@@ -191,7 +204,7 @@ public class CMSPermissionsObjectDefinitionLocalServiceWrapperTest {
 	private void _testPublishCustomObjectDefinitionInFileTypesFolder()
 		throws Exception {
 
-		ObjectDefinition objectDefinition = _publishObjectDefinition(
+		ObjectDefinition objectDefinition = _publishCustomObjectDefinition(
 			ObjectFolderConstants.EXTERNAL_REFERENCE_CODE_FILE_TYPES);
 
 		_assertCMSAdministratorObjectDefinitionPermissions(objectDefinition);
@@ -200,6 +213,9 @@ public class CMSPermissionsObjectDefinitionLocalServiceWrapperTest {
 		_assertObjectDefinitionViewPermission(
 			objectDefinition, RoleConstants.USER);
 	}
+
+	@Inject
+	private ObjectDefinitionLocalService _objectDefinitionLocalService;
 
 	@DeleteAfterTestRun
 	private List<ObjectDefinition> _objectDefinitions = new ArrayList<>();

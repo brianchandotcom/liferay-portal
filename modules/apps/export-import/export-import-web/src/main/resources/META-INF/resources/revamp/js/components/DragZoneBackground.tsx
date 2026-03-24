7da69acf239dc20c7d81367cdbb1ff32cc3cd69c
Jaime Leon <jaime.leon@liferay.com>
2026-03-24 16:59:51 +0100

    LPD-77438 Create base DragZone components and styles

---
+++ modules/apps/export-import/export-import-web/src/main/resources/META-INF/resources/revamp/js/components/DragZoneBackground.tsx
@@ -0,0 +1,39 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import ClayButton from '@clayui/button';
+import React from 'react';
+
+export default function DragZoneBackground({maxSize}: {maxSize: number}) {
+	const dragAndDropPath = `${Liferay.ThemeDisplay.getPathContext()}/o/exportimport-web/revamp/images/drag_and_drop.svg`;
+
+	return (
+		<>
+			<img
+				alt={Liferay.Language.get('drag-and-drop-your-file')}
+				src={dragAndDropPath}
+			/>
+
+			<p className="my-2 text-weight-semi-bold">
+				{Liferay.Language.get('drag-and-drop-your-file-or')}
+			</p>
+
+			<ClayButton displayType="secondary">
+				{Liferay.Language.get('select-files')}
+			</ClayButton>
+
+			<p className="my-2">
+				{Liferay.Util.sub(
+					Liferay.Language.get(
+						'only-lar-files-are-allowed-max-file-size-is-x'
+					),
+					Liferay.Util.formatStorage(maxSize, {
+						addSpaceBeforeSuffix: true,
+					})
+				)}
+			</p>
+		</>
+	);
+}

fd34d4653f1de7a6e38f36172aaab57346ebec46
Mario Leandro <mario.leandro@liferay.com>
2026-03-25 09:43:12 -0300

    LPD-79452 Simplify organization and create DesignLibraryService

--- modules/apps/design-library/design-library-web/src/main/resources/META-INF/resources/js/modal/CreateDesignLibraryModal.tsx
+++ modules/apps/design-library/design-library-web/src/main/resources/META-INF/resources/js/modal/CreateDesignLibraryModal.tsx
@@ -11,14 +11,9 @@ import {openToast} from 'frontend-js-components-web';
 import {createPortletURL, fetch, navigate, sub} from 'frontend-js-web';
 import React, {useState} from 'react';
 
-import {
-	FieldText,
-	maxLength,
-	required,
-	validate,
-} from '../common/components/forms';
-
-const FDS_EVENT_UPDATE_DISPLAY = 'fds-update-display';
+import {FieldText, maxLength, required, validate} from '../components/forms';
+import {FDS_EVENT_UPDATE_DISPLAY} from '../constants';
+import DesignLibraryService from '../services/DesignLibraryService';
 
 export default function CreateDesignLibraryModal({
 	dataSetId,
@@ -32,35 +27,6 @@ export default function CreateDesignLibraryModal({
 	redirectURL: string;
 }) {
 	const [nameInputError, setNameInputError] = useState<string>('');
-
-	const createDesignLibrary = async (name: string, description: string) => {
-		const url = '/o/headless-asset-library/v1.0/asset-libraries';
-		const body = {
-			description,
-			name,
-			type: 'DesignLibrary',
-		};
-
-		const response = await fetch(url, {
-			body: JSON.stringify(body),
-			headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json',
-			},
-			method: 'POST',
-		});
-
-		const data = await response.json().catch(() => ({}));
-
-		if (!response.ok) {
-			throw {
-				error: data.title || 'UNKNOWN_ERROR',
-				status: data.status || response.status,
-			};
-		}
-
-		return data as {id: string};
-	};
 
 	const {
 		errors,
@@ -78,10 +44,10 @@ export default function CreateDesignLibraryModal({
 		},
 		onSubmit: async (values) => {
 			try {
-				const data = await createDesignLibrary(
-					values.designLibraryName,
-					values.designLibraryDescription
-				);
+				const data = await DesignLibraryService.create({
+					description: values.designLibraryDescription,
+					name: values.designLibraryName,
+				});
 
 				openToast({
 					message: sub(
@@ -116,10 +82,6 @@ export default function CreateDesignLibraryModal({
 					title: Liferay.Language.get('error'),
 					type: 'danger',
 				});
-
-				throw new Error(
-					`POST request failed to create a new Design Library with name ${values.designLibraryName}`
-				);
 			}
 		},
 		validate: (values) => {
@@ -155,61 +117,59 @@ export default function CreateDesignLibraryModal({
 
 	return (
 		<ClayForm onSubmit={handleSubmit}>
-			<div className="categorization-modal">
-				<ClayModal.Header
-					closeButtonAriaLabel={Liferay.Language.get('close')}
-				>
-					{Liferay.Language.get('new-design-library')}
-				</ClayModal.Header>
+			<ClayModal.Header
+				closeButtonAriaLabel={Liferay.Language.get('close')}
+			>
+				{Liferay.Language.get('new-design-library')}
+			</ClayModal.Header>
 
-				<ClayModal.Body>
-					<FieldText
-						errorMessage={handleNameInputErrorMessage()}
-						label={Liferay.Language.get('name')}
-						name="designLibraryName"
-						onBlur={handleBlur}
-						onChange={(event) => {
-							setNameInputError('');
-							handleChange(event);
-						}}
-						required
-						value={values.designLibraryName}
-					/>
-
-					<FieldText
-						component="textarea"
-						label={Liferay.Language.get('description')}
-						name="designLibraryDescription"
-						onBlur={handleBlur}
-						onChange={(event) => {
-							handleChange(event);
-						}}
-						value={values.designLibraryDescription}
-					/>
-				</ClayModal.Body>
-
-				<ClayModal.Footer
-					last={
-						<ClayButton.Group spaced>
-							<ClayButton
-								displayType="secondary"
-								onClick={onClose}
-								type="button"
-							>
-								{Liferay.Language.get('cancel')}
-							</ClayButton>
-
-							<ClayButton
-								disabled={shouldDisableSaveButton}
-								displayType="primary"
-								type="submit"
-							>
-								{Liferay.Language.get('save')}
-							</ClayButton>
-						</ClayButton.Group>
-					}
+			<ClayModal.Body>
+				<FieldText
+					errorMessage={handleNameInputErrorMessage()}
+					label={Liferay.Language.get('name')}
+					name="designLibraryName"
+					onBlur={handleBlur}
+					onChange={(event) => {
+						setNameInputError('');
+						handleChange(event);
+					}}
+					required
+					value={values.designLibraryName}
 				/>
-			</div>
+
+				<FieldText
+					component="textarea"
+					label={Liferay.Language.get('description')}
+					name="designLibraryDescription"
+					onBlur={handleBlur}
+					onChange={(event) => {
+						handleChange(event);
+					}}
+					value={values.designLibraryDescription}
+				/>
+			</ClayModal.Body>
+
+			<ClayModal.Footer
+				last={
+					<ClayButton.Group spaced>
+						<ClayButton
+							displayType="secondary"
+							onClick={onClose}
+							type="button"
+						>
+							{Liferay.Language.get('cancel')}
+						</ClayButton>
+
+						<ClayButton
+							disabled={shouldDisableSaveButton}
+							displayType="primary"
+							type="submit"
+						>
+							{Liferay.Language.get('save')}
+						</ClayButton>
+					</ClayButton.Group>
+				}
+			/>
 		</ClayForm>
 	);
 }

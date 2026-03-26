f9ef64c77c78812eeaa9f328e9ee5f0df00cdbcf
Jerry Adriano <ajerry550@gmail.com>
2026-03-25 21:17:47 -0300

    Revert "LPD-79062 feature-flag-web: delay operations during startup"
    This reverts commit 4ab78a069010afd73ea6933372a89e7c363b1928.

--- modules/apps/feature-flag/feature-flag-web/src/main/java/com/liferay/feature/flag/web/internal/model/listener/CompanyModelListener.java
+++ modules/apps/feature-flag/feature-flag-web/src/main/java/com/liferay/feature/flag/web/internal/model/listener/CompanyModelListener.java
@@ -3,18 +3,21 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.feature.flag.web.internal.instance.lifecycle;
+package com.liferay.feature.flag.web.internal.model.listener;
 
-import com.liferay.portal.events.StartupHelperUtil;
 import com.liferay.portal.feature.flag.FeatureFlagsBag;
 import com.liferay.portal.feature.flag.FeatureFlagsBagProvider;
-import com.liferay.portal.instance.lifecycle.InitialRequestPortalInstanceLifecycleListener;
-import com.liferay.portal.instance.lifecycle.PortalInstanceLifecycleListener;
+import com.liferay.portal.events.StartupHelperUtil;
+import com.liferay.portal.kernel.exception.ModelListenerException;
 import com.liferay.portal.kernel.feature.flag.FeatureFlag;
 import com.liferay.portal.kernel.feature.flag.FeatureFlagType;
 import com.liferay.portal.kernel.feature.flag.constants.FeatureFlagConstants;
+import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.CompanyConstants;
+import com.liferay.portal.kernel.model.ModelListener;
 import com.liferay.portal.kernel.portlet.PortalPreferences;
+import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.service.PortalPreferencesLocalService;
 import com.liferay.portal.kernel.transaction.TransactionCommitCallbackUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
@@ -23,7 +26,6 @@ import com.liferay.portlet.PortalPreferencesWrapper;
 
 import java.util.List;
 
-import org.osgi.framework.BundleContext;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -32,32 +34,27 @@ import org.osgi.service.component.annotations.Reference;
  * @author Drew Brokke
  * @author Thiago Buarque
  */
-@Component(
-	property = "service.ranking:Integer=100",
-	service = PortalInstanceLifecycleListener.class
-)
-public class
-	ProcessDeprecationFeatureFlagsInitialRequestPortalInstanceLifecycleListener
-		extends InitialRequestPortalInstanceLifecycleListener {
-
-	@Activate
-	@Override
-	protected void activate(BundleContext bundleContext) {
-		super.activate(bundleContext);
-
-		if (StartupHelperUtil.isDBNew()) {
-			_processDeprecationFeatureFlags(CompanyConstants.SYSTEM);
-		}
-	}
+@Component(service = ModelListener.class)
+public class CompanyModelListener extends BaseModelListener<Company> {
 
 	@Override
-	protected void doPortalInstanceRegistered(long companyId) throws Exception {
+	public void onAfterCreate(Company company) throws ModelListenerException {
 		TransactionCommitCallbackUtil.registerCallback(
 			() -> {
-				_processDeprecationFeatureFlags(companyId);
+				_processDeprecationFeatureFlags(company.getCompanyId());
 
 				return null;
 			});
+	}
+
+	@Activate
+	protected void activate() {
+		if (StartupHelperUtil.isDBNew()) {
+			_processDeprecationFeatureFlags(CompanyConstants.SYSTEM);
+		}
+
+		_companyLocalService.forEachCompanyId(
+			this::_processDeprecationFeatureFlags);
 	}
 
 	private PortalPreferences _getPortalPreferences(long companyId) {
@@ -102,6 +99,9 @@ public class
 		_portalPreferencesLocalService.updatePreferences(
 			companyId, PortletKeys.PREFS_OWNER_TYPE_COMPANY, portalPreferences);
 	}
+
+	@Reference
+	private CompanyLocalService _companyLocalService;
 
 	@Reference
 	private FeatureFlagsBagProvider _featureFlagsBagProvider;

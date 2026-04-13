0e3306b89eb2923a43693ef9f33c429d663428c1
Joao Victor <joao.victor@liferay.com>
2026-04-13 17:45:51 -0300

    LPD-86060 feat: create new configuration to store the values of the Vertex AI model

--- modules/dxp/apps/ai-hub/ai-hub-impl/src/main/java/com/liferay/ai/hub/internal/configuration/VertexAIConfiguration.java
+++ modules/dxp/apps/ai-hub/ai-hub-impl/src/main/java/com/liferay/ai/hub/internal/configuration/VertexAIConfiguration.java
@@ -3,33 +3,32 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.ai.hub.cell.configuration;
+package com.liferay.ai.hub.internal.configuration;
 
 import aQute.bnd.annotation.metatype.Meta;
 
 import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
 
 /**
- * @author Feliphe Marinho
- * @author Rafael Praxedes
+ * @author João Victor Alves
  */
 @ExtendedObjectClassDefinition(
 	category = "ai-hub", featureFlagKey = "LPD-62272",
 	scope = ExtendedObjectClassDefinition.Scope.COMPANY
 )
 @Meta.OCD(
-	id = "com.liferay.ai.hub.cell.configuration.AIHubCellConfiguration",
-	localization = "content/Language", name = "ai-hub-cell-configuration-name"
+	id = "com.liferay.ai.hub.internal.configuration.VertexAIConfiguration",
+	localization = "content/Language", name = "vertex-ai-configuration-name"
 )
-public interface AIHubCellConfiguration {
+public interface VertexAIConfiguration {
 
-	@Meta.AD(name = "client-id")
-	public String clientId();
+	@Meta.AD(name = "location")
+	public String location();
 
-	@Meta.AD(name = "client-secret")
-	public String clientSecret();
+	@Meta.AD(name = "model-name")
+	public String modelName();
 
-	@Meta.AD(name = "service-url")
-	public String serviceURL();
+	@Meta.AD(name = "project-id")
+	public String projectId();
 
 }

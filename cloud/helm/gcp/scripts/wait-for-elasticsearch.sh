1c47d0c3ec0c9469b3822fa7cb95bf6dd721fcc6
Allen Ziegenfus <allen.ziegenfus@liferay.com>
2026-04-14 10:12:10 -0500

    LCD-50870 Move script to separate volume mount and add json logging

--- cloud/helm/gcp/scripts/wait-for-elasticsearch.sh
+++ cloud/helm/gcp/scripts/wait-for-elasticsearch.sh
@@ -1,18 +1,36 @@
-#!/bin/bash
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+
+function _log_json {
+	local escaped_message
+	escaped_message=$(echo "${1}" | sed 's/"/\\"/g')
+
+	local script_name
+	script_name=$(basename "${0}")
+
+	local severity="${2:-INFO}"
+
+	local timestamp
+	timestamp=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+
+	printf '{"message": "%s", "script": "%s", "severity": "%s", "timestamp": "%s"}\n' "${escaped_message}" "${script_name}" "${severity}" "${timestamp}"
+}
 
 function main {
-	echo "Waiting for Elasticsearch health to be \"green\" or \"yellow\" at \"${ELASTICSEARCH_URL}/_cluster/health\"."
+	_log_json "Waiting for Elasticsearch health to be \"green\" or \"yellow\" at \"${ELASTICSEARCH_URL}/_cluster/health\"."
 
-	local auth_url="http://${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD}@${ELASTICSEARCH_URL#*//}/_cluster/health"
+	local authenticated_health_url="http://${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD}@${ELASTICSEARCH_URL#*//}/_cluster/health"
 
-	until wget -qO- "${auth_url}" | grep -qE "\"status\":\"(green|yellow)\""
+	until wget -qO- "${authenticated_health_url}" | grep -qE "\"status\":\"(green|yellow)\""
 	do
-		echo "Waiting for ES (current status: \"Red\" or \"Unreachable\")."
+		_log_json "Waiting for Elasticsearch (current status: \"red\" or \"unreachable\")."
 
 		sleep 5
 	done
 
-	echo "Elasticsearch is ready."
+	_log_json "Elasticsearch is ready."
 }
 
 main

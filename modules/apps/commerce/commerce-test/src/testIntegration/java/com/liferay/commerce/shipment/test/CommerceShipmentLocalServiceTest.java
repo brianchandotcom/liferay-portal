104981d50ddfd517c382dae445d587ffcd7693b0
Danny Situ <danny.situ@liferay.com>
2026-04-16 14:01:34 -0700

    LPD-79040 Clean up tests

--- modules/apps/commerce/commerce-test/src/testIntegration/java/com/liferay/commerce/shipment/test/CommerceShipmentLocalServiceTest.java
+++ modules/apps/commerce/commerce-test/src/testIntegration/java/com/liferay/commerce/shipment/test/CommerceShipmentLocalServiceTest.java
@@ -1,5 +1,5 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
@@ -12,33 +12,37 @@ import com.liferay.commerce.currency.test.util.CommerceCurrencyTestUtil;
 import com.liferay.commerce.inventory.model.CommerceInventoryWarehouse;
 import com.liferay.commerce.model.CommerceAddress;
 import com.liferay.commerce.model.CommerceOrder;
+import com.liferay.commerce.model.CommerceOrderItem;
 import com.liferay.commerce.model.CommerceShipment;
-import com.liferay.commerce.model.CommerceShippingFixedOption;
 import com.liferay.commerce.model.CommerceShippingMethod;
 import com.liferay.commerce.product.model.CPInstance;
 import com.liferay.commerce.product.model.CommerceChannel;
+import com.liferay.commerce.product.test.util.CPTestUtil;
 import com.liferay.commerce.service.CommerceOrderLocalService;
+import com.liferay.commerce.service.CommerceShipmentItemLocalService;
 import com.liferay.commerce.service.CommerceShipmentLocalService;
-import com.liferay.commerce.shipment.test.util.CommerceShipmentTestUtil;
+import com.liferay.commerce.shipping.engine.fixed.model.CommerceShippingFixedOption;
 import com.liferay.commerce.test.util.CommerceInventoryTestUtil;
 import com.liferay.commerce.test.util.CommerceTestUtil;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
+
+import java.util.Arrays;
 import java.util.List;
 
-import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
@@ -57,7 +61,7 @@ public class CommerceShipmentLocalServiceTest {
 	public static final AggregateTestRule aggregateTestRule =
 		new AggregateTestRule(
 			new LiferayIntegrationTestRule(),
-			PermissionCheckerMethodTestRule.getPermissionCheckerMethodTestRule());
+			PermissionCheckerMethodTestRule.INSTANCE);
 
 	@Before
 	public void setUp() throws Exception {
@@ -68,50 +72,73 @@ public class CommerceShipmentLocalServiceTest {
 			_group.getCompanyId());
 
 		_commerceChannel = CommerceTestUtil.addCommerceChannel(
-			_user.getUserId(), _group.getGroupId(), _commerceCurrency.getCode());
-	}
-
-	@After
-	public void tearDown() throws Exception {
-		for (CommerceOrder commerceOrder : _commerceOrders) {
-			CommerceTestUtil.deleteCommerceOrder(
-				commerceOrder.getCommerceOrderId());
-		}
-
-		CommerceTestUtil.deleteCommerceChannel(_commerceChannel);
-
-		CommerceCurrencyTestUtil.deleteCommerceCurrency(_commerceCurrency);
-
-		UserTestUtil.deleteUser(_user);
-
-		GroupTestUtil.deleteGroup(_group);
+			_group.getGroupId(), _commerceCurrency.getCode());
 	}
 
 	@Test
-	public void testShipmentsAreNotDuplicatedForEachOrderItem() throws Exception {
+	public void testGetCommerceShipments() throws Exception {
+		CommerceOrder commerceOrder = _addCommerceOrderWithShipments();
+
+		List<CommerceShipment> commerceShipments =
+			_commerceShipmentLocalService.getCommerceShipments(
+				commerceOrder.getCommerceOrderId(), QueryUtil.ALL_POS,
+				QueryUtil.ALL_POS);
+
+		Assert.assertEquals(
+			commerceShipments.toString(), 2, commerceShipments.size());
+	}
+
+	@Test
+	public void testGetCommerceShipmentsCount() throws Exception {
+		CommerceOrder commerceOrder = _addCommerceOrderWithShipments();
+
+		int count = _commerceShipmentLocalService.getCommerceShipmentsCount(
+			commerceOrder.getCommerceOrderId());
+
+		Assert.assertEquals(2, count);
+	}
+
+	@Test
+	public void testGetCommerceShipmentStatusesByCommerceOrderId()
+		throws Exception {
+
+		CommerceOrder commerceOrder = _addCommerceOrderWithShipments();
+
+		int[] statuses =
+			_commerceShipmentLocalService.
+				getCommerceShipmentStatusesByCommerceOrderId(
+					commerceOrder.getCommerceOrderId());
+
+		Assert.assertEquals(Arrays.toString(statuses), 1, statuses.length);
+	}
+
+	private CommerceOrder _addCommerceOrder() throws Exception {
 		CommerceOrder commerceOrder = CommerceTestUtil.addB2CCommerceOrder(
 			_user.getUserId(), _commerceChannel.getGroupId(),
 			_commerceCurrency.getCommerceCurrencyId());
 
-		CommerceInventoryWarehouse commerceInventoryWarehouse =
+		_commerceInventoryWarehouse =
 			CommerceInventoryTestUtil.addCommerceInventoryWarehouse();
 
 		CommerceTestUtil.addWarehouseCommerceChannelRel(
-			commerceInventoryWarehouse.getCommerceInventoryWarehouseId(),
+			_commerceInventoryWarehouse.getCommerceInventoryWarehouseId(),
 			_commerceChannel.getCommerceChannelId());
 
 		for (int i = 0; i < 7; i++) {
-			CPInstance cpInstance = CommerceTestUtil.createCPInstance(
-				_user.getUserId(), _group.getGroupId());
+			CPInstance cpInstance = CPTestUtil.addCPInstanceWithRandomSku(
+				_group.getGroupId());
 
 			CommerceTestUtil.addCommerceOrderItem(
 				commerceOrder.getCommerceOrderId(),
 				cpInstance.getCPInstanceId(), BigDecimal.ONE);
 
 			CommerceInventoryTestUtil.addCommerceInventoryWarehouseItem(
-				_user.getUserId(), commerceInventoryWarehouse,
+				_user.getUserId(), _commerceInventoryWarehouse,
 				BigDecimal.valueOf(100), cpInstance.getSku(), StringPool.BLANK);
 		}
+
+		commerceOrder = _commerceOrderLocalService.getCommerceOrder(
+			commerceOrder.getCommerceOrderId());
 
 		int orderStatusIndex = RandomTestUtil.randomInt(
 			0, CommerceShipmentConstants.ALLOWED_ORDER_STATUSES.length - 1);
@@ -121,20 +148,14 @@ public class CommerceShipmentLocalServiceTest {
 
 		commerceOrder.setOrderStatus(orderStatus);
 
-		commerceOrder = _commerceOrderLocalService.getCommerceOrder(
-			commerceOrder.getCommerceOrderId());
-
-		CommerceAddress billingCommerceAddress =
-			CommerceTestUtil.addUserCommerceAddress(
-				_commerceChannel.getGroupId(), _user.getUserId());
-		CommerceAddress shippingCommerceAddress =
+		CommerceAddress commerceAddress =
 			CommerceTestUtil.addUserCommerceAddress(
 				_commerceChannel.getGroupId(), _user.getUserId());
 
 		commerceOrder.setBillingAddressId(
-			billingCommerceAddress.getCommerceAddressId());
+			commerceAddress.getCommerceAddressId());
 		commerceOrder.setShippingAddressId(
-			shippingCommerceAddress.getCommerceAddressId());
+			commerceAddress.getCommerceAddressId());
 
 		BigDecimal amount = BigDecimal.valueOf(RandomTestUtil.nextDouble());
 
@@ -154,41 +175,64 @@ public class CommerceShipmentLocalServiceTest {
 		commerceOrder.setShippingOptionName(
 			commerceShippingFixedOption.getNameCurrentValue());
 
-		_commerceOrderLocalService.updateCommerceOrder(commerceOrder);
+		commerceOrder = _commerceOrderLocalService.updateCommerceOrder(
+			commerceOrder);
 
-		_commerceOrders.add(commerceOrder);
+		return commerceOrder;
+	}
+
+	private CommerceOrder _addCommerceOrderWithShipments() throws Exception {
+		CommerceOrder commerceOrder = _addCommerceOrder();
+
+		List<CommerceOrderItem> commerceOrderItems =
+			commerceOrder.getCommerceOrderItems();
+
+		_addCommerceShipment(commerceOrder, commerceOrderItems.subList(0, 4));
+		_addCommerceShipment(
+			commerceOrder,
+			commerceOrderItems.subList(4, commerceOrderItems.size()));
+
+		return commerceOrder;
+	}
+
+	private CommerceShipment _addCommerceShipment(
+			CommerceOrder commerceOrder,
+			List<CommerceOrderItem> commerceOrderItems)
+		throws Exception {
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				commerceOrder.getGroupId());
 
 		CommerceShipment commerceShipment =
-			CommerceShipmentTestUtil.createOrderShipment(
-				commerceOrder.getGroupId(), commerceOrder.getCommerceOrderId(),
-				commerceInventoryWarehouse.getCommerceInventoryWarehouseId());
+			_commerceShipmentLocalService.addCommerceShipment(
+				commerceOrder.getCommerceOrderId(), serviceContext);
 
-		List<CommerceShipment> commerceShipments =
-			_commerceShipmentLocalService.getCommerceShipments(
-				commerceOrder.getCommerceOrderId(), QueryUtil.ALL_POS,
-				QueryUtil.ALL_POS);
+		for (CommerceOrderItem commerceOrderItem : commerceOrderItems) {
+			_commerceShipmentItemLocalService.addCommerceShipmentItem(
+				null, commerceShipment.getCommerceShipmentId(),
+				commerceOrderItem.getCommerceOrderItemId(),
+				_commerceInventoryWarehouse.getCommerceInventoryWarehouseId(),
+				commerceOrderItem.getQuantity(), null, true, serviceContext);
+		}
 
-		Assert.assertEquals(1, commerceShipments.size());
-		Assert.assertEquals(
-			commerceShipment.getCommerceShipmentId(),
-			commerceShipments.get(0).getCommerceShipmentId());
-
-		int count = _commerceShipmentLocalService.getCommerceShipmentsCount(
-			commerceOrder.getCommerceOrderId());
-
-		Assert.assertEquals(1, count);
+		return commerceShipment;
 	}
 
 	private CommerceChannel _commerceChannel;
 	private CommerceCurrency _commerceCurrency;
-	private List<CommerceOrder> _commerceOrders = new ArrayList<>();
-	private Group _group;
-	private User _user;
+	private CommerceInventoryWarehouse _commerceInventoryWarehouse;
 
 	@Inject
 	private CommerceOrderLocalService _commerceOrderLocalService;
 
 	@Inject
+	private CommerceShipmentItemLocalService _commerceShipmentItemLocalService;
+
+	@Inject
 	private CommerceShipmentLocalService _commerceShipmentLocalService;
+
+	private Group _group;
+	private User _user;
 
 }

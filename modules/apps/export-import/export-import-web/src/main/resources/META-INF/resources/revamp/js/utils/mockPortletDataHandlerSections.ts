2aa066aa9f95895b5b670cc3503379bd1afba4f4
Jorge González <jorge.gonzalez@liferay.com>
2026-04-21 18:13:23 +0200

    LPD-83052 Split PortletDataHandlerControl into Boolean/Choice union

--- modules/apps/export-import/export-import-web/src/main/resources/META-INF/resources/revamp/js/utils/mockPortletDataHandlerSections.ts
+++ modules/apps/export-import/export-import-web/src/main/resources/META-INF/resources/revamp/js/utils/mockPortletDataHandlerSections.ts
@@ -3,67 +3,64 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-export interface PortletDataControl {
-	controls?: PortletDataControl[];
-	label: string;
-	name: string;
-	options?: {label: string; value: string}[];
-	type: 'boolean' | 'choice';
-}
-
-export interface PortletDataHandler {
-	controls?: PortletDataControl[];
+export interface PortletDataHandlerBoolean {
+	controls?: PortletDataHandlerControl[];
 	label: string;
 	name: string;
 	type: 'boolean';
 }
 
+export interface PortletDataHandlerChoice {
+	choices: {label: string; name: string}[];
+	label: string;
+	name: string;
+	type: 'choice';
+}
+
+export type PortletDataHandlerControl =
+	| PortletDataHandlerBoolean
+	| PortletDataHandlerChoice;
+
+export type PortletDataHandler = Omit<PortletDataHandlerBoolean, 'type'>;
+
 export interface PortletDataHandlerSection {
-	id: string;
-	key: string;
+	label: string;
 	name: string;
 	portletDataHandlers: PortletDataHandler[];
 }
 
 export const mockPortletDataHandlerSections: PortletDataHandlerSection[] = [
 	{
-		id: 'category.site_administration.design',
-		key: 'category.site_administration.design',
-		name: 'Design',
+		label: 'Design',
+		name: 'category.site_administration.design',
 		portletDataHandlers: [
 			{
 				label: 'Theme Settings',
 				name: 'THEME_REFERENCE',
-				type: 'boolean',
 			},
 			{
 				label: 'Logo',
 				name: 'LOGO',
-				type: 'boolean',
 			},
 			{
 				label: 'Fragments',
 				name: 'PORTLET_DATA_com_liferay_fragment_web_portlet_FragmentPortlet',
-				type: 'boolean',
 			},
 		],
 	},
 	{
-		id: 'category.site_administration.build',
-		key: 'category.site_administration.build',
-		name: 'Site Builder',
+		label: 'Site Builder',
+		name: 'category.site_administration.build',
 		portletDataHandlers: [
 			{
 				label: 'Pages',
 				name: 'PORTLET_DATA_com_liferay_layout_admin_web_portlet_GroupPagesPortlet',
-				type: 'boolean',
 			},
 		],
 	},
 	{
-		id: 'category.site_administration.content',
-		key: 'category.site_administration.content',
-		name: 'Content & Data',
+		label: 'Content & Data',
+		name: 'category.site_administration.content',
 		portletDataHandlers: [
 			{
 				controls: [
@@ -75,18 +72,18 @@ export const mockPortletDataHandlerSections: PortletDataHandlerSection[] = [
 					{
 						controls: [
 							{
-								label: 'Referenced Content Behavior',
-								name: '_journal_referenced-content-behavior',
-								options: [
+								choices: [
 									{
 										label: 'Include Always',
-										value: 'include-always',
+										name: 'include-always',
 									},
 									{
 										label: 'Include If Modified',
-										value: 'include-if-modified',
+										name: 'include-if-modified',
 									},
 								],
+								label: 'Referenced Content Behavior',
+								name: '_journal_referenced-content-behavior',
 								type: 'choice',
 							},
 						],
@@ -102,7 +99,6 @@ export const mockPortletDataHandlerSections: PortletDataHandlerSection[] = [
 				],
 				label: 'Web Content',
 				name: 'PORTLET_DATA_com_liferay_journal_web_portlet_JournalPortlet',
-				type: 'boolean',
 			},
 			{
 				controls: [
@@ -126,18 +122,18 @@ export const mockPortletDataHandlerSections: PortletDataHandlerSection[] = [
 							{
 								controls: [
 									{
-										label: 'Referenced Content Behavior',
-										name: '_document_library_referenced-content-behavior',
-										options: [
+										choices: [
 											{
 												label: 'Include Always',
-												value: 'include-always',
+												name: 'include-always',
 											},
 											{
 												label: 'Include If Modified',
-												value: 'include-if-modified',
+												name: 'include-if-modified',
 											},
 										],
+										label: 'Referenced Content Behavior',
+										name: '_document_library_referenced-content-behavior',
 										type: 'choice',
 									},
 								],
@@ -163,7 +159,6 @@ export const mockPortletDataHandlerSections: PortletDataHandlerSection[] = [
 				],
 				label: 'Documents and Media',
 				name: 'PORTLET_DATA_com_liferay_document_library_web_portlet_DLAdminPortlet',
-				type: 'boolean',
 			},
 		],
 	},

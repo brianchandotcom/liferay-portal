03362ad7498d2c35535d233d789a35de6543f6a9
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2026-04-24 15:32:56 +0200

    LPD-86074 Add CMSObjectEntryPortlet
    Note that I'm creating the portlet in one module and the portlet key
    in another one.  This is on purpose:
    1. The portlet is an implementation detail, it is needed just because
    objects uses the asset framework (see later commits). I don't want to
    expose the portlet key in the site cms site initializer module.
    2. Even if I exposed the key in that module, it doesn't make sense for
    the objects module to depend on the CMS module. I chose to duplicate
    the key and keep them decoupled.

---
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/constants/ObjectPortletKeys.java
@@ -0,0 +1,17 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.web.internal.constants;
+
+/**
+ * @author Adolfo Pérez
+ */
+public class ObjectPortletKeys {
+
+	public static final String CMS_OBJECT_ENTRY =
+		"com_liferay_site_cms_site_initializer_internal_portlet_" +
+			"CMSObjectEntryPortlet";
+
+}

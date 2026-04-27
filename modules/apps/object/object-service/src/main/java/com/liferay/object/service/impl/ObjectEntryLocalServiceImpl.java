580998cdc069510afbf4a8e1eeaf42a59c529f28
Marcela Cunha <marcela.cunha@liferay.com>
2026-04-27 10:47:58 -0300

    LPD-70691 Add new PhoneNumber Object Field Type Co-authored-by: Yuri Monteiro <yuri.monteiro@liferay.com>

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -7884,8 +7884,17 @@ public class ObjectEntryLocalServiceImpl
 				defaultLanguageId, existingValues, objectField, partialUpdate,
 				serviceContext, status, validationErrors, values);
 
+			ObjectFieldBusinessType objectFieldBusinessType =
+				_objectFieldBusinessTypeRegistry.getObjectFieldBusinessType(
+					objectField.getBusinessType());
+
 			if (!objectField.isLocalized() &&
 				(values.get(objectField.getName()) != null)) {
+
+				values.put(
+					objectField.getName(),
+					objectFieldBusinessType.processValue(
+						objectField, values.get(objectField.getName())));
 
 				_validateValues(
 					dlFileEntriesMap, existingValues, groupId, guestUser,
@@ -7904,6 +7913,10 @@ public class ObjectEntryLocalServiceImpl
 
 			for (Map.Entry<String, Serializable> entry :
 					localizedValues.entrySet()) {
+
+				entry.setValue(
+					objectFieldBusinessType.processValue(
+						objectField, entry.getValue()));
 
 				_validateValues(
 					dlFileEntriesMap, existingValues, groupId, guestUser,

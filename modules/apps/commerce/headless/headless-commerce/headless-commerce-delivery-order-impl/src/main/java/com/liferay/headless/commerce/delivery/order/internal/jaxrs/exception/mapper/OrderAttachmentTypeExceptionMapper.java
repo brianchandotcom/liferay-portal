491ff1d951ed099490f52eb3a702ad3bf641f700
smottal <117469730+smottal@users.noreply.github.com>
2026-04-28 11:37:36 +0200

    LPD-75148 New exception mappers

---
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-delivery-order-impl/src/main/java/com/liferay/headless/commerce/delivery/order/internal/jaxrs/exception/mapper/OrderAttachmentTypeExceptionMapper.java
@@ -0,0 +1,42 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.headless.commerce.delivery.order.internal.jaxrs.exception.mapper;
+
+import com.liferay.commerce.exception.CommerceOrderAttachmentTypeException;
+import com.liferay.headless.commerce.core.exception.mapper.BaseExceptionMapper;
+
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Stefano Motta
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Headless.Commerce.Delivery.Order)",
+		"osgi.jaxrs.extension=true",
+		"osgi.jaxrs.name=Liferay.Headless.Commerce.Delivery.Order.OrderAttachmentTypeExceptionMapper"
+	},
+	service = ExceptionMapper.class
+)
+@Provider
+public class OrderAttachmentTypeExceptionMapper
+	extends BaseExceptionMapper<CommerceOrderAttachmentTypeException> {
+
+	@Override
+	public String getErrorDescription() {
+		return "Invalid order attachment type";
+	}
+
+	@Override
+	public Response.Status getStatus() {
+		return Response.Status.BAD_REQUEST;
+	}
+
+}

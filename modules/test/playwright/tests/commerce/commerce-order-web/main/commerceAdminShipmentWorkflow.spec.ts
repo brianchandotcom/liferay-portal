b1a386f9ba2250c3f8f19df55f39fab585970769
Crescenzo Rega <97021604+CrescenzoRegaLF@users.noreply.github.com>
2026-04-28 10:30:22 +0200

    LPD-86571 Removes LPD-36105 from the test

--- modules/test/playwright/tests/commerce/commerce-order-web/main/commerceAdminShipmentWorkflow.spec.ts
+++ modules/test/playwright/tests/commerce/commerce-order-web/main/commerceAdminShipmentWorkflow.spec.ts
@@ -7,7 +7,6 @@ import {expect, mergeTests} from '@playwright/test';
 
 import {commercePagesTest} from '../../../../fixtures/commercePagesTest';
 import {dataApiHelpersTest} from '../../../../fixtures/dataApiHelpersTest';
-import {featureFlagsTest} from '../../../../fixtures/featureFlagsTest';
 import {globalMenuPagesTest} from '../../../../fixtures/globalMenuPagesTest';
 import {isolatedSiteTest} from '../../../../fixtures/isolatedSiteTest';
 import {loginTest} from '../../../../fixtures/loginTest';
@@ -18,9 +17,6 @@ import {waitForAlert} from '../../../../utils/waitForAlert';
 export const test = mergeTests(
 	commercePagesTest,
 	dataApiHelpersTest,
-	featureFlagsTest({
-		'LPD-36105': {enabled: true},
-	}),
 	globalMenuPagesTest,
 	isolatedSiteTest,
 	loginTest()

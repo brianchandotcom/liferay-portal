4ff1aed1f43da1ee9adee421269ece976b627fb6
Juanjo Fernandez <juanjo.fernandez@liferay.com>
2026-04-28 09:06:32 +0200

    LPD-81808 Add Share action for FDS snapshots

---
+++ modules/apps/frontend-data-set/frontend-data-set-web/src/main/resources/META-INF/resources/management_bar/controls/shareSnapshotAction.ts
@@ -0,0 +1,86 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {
+	Collaborator,
+	CollaboratorService,
+	PermissionOption,
+	ShareModalContent,
+	openModal,
+	openToast,
+} from 'frontend-js-components-web';
+
+const AUTOCOMPLETE_URL =
+	'/o/search/v1.0/search?emptySearch=true' +
+	'&entryClassNames=com.liferay.portal.kernel.model.User,' +
+	'com.liferay.portal.kernel.model.UserGroup' +
+	'&nestedFields=embedded';
+
+const COLLABORATOR_URL =
+	'/o/data-set-admin/snapshots/{objectEntryId}/collaborators';
+
+const PERMISSION_OPTIONS: PermissionOption[] = [
+	{
+		label: Liferay.Language.get('view'),
+		value: 'VIEW',
+	},
+];
+
+export default async function shareSnapshotAction({
+	itemId,
+	title,
+}: {
+	itemId: number;
+	title: string;
+}) {
+	try {
+		const items = await CollaboratorService.getCollaborators(
+			COLLABORATOR_URL,
+			itemId
+		);
+
+		const initialCollaborators: Collaborator[] = items.reverse().map(
+			({actionIds, dateExpired, id, name, portrait, share, type}) =>
+				({
+					actionIds: actionIds.sort().join(','),
+					dateExpired,
+					share,
+					type,
+					user: {
+						id: id.toString(),
+						image: portrait,
+						name,
+					},
+				}) as Collaborator
+		);
+
+		openModal({
+			contentComponent: ({closeModal}: {closeModal: () => void}) =>
+				ShareModalContent({
+					autocompleteURL: AUTOCOMPLETE_URL,
+					closeModal,
+					collaboratorURL: COLLABORATOR_URL,
+					creator: {
+						contentType: 'UserAccount',
+						id: Liferay.ThemeDisplay.getUserId().toString(),
+						name: Liferay.ThemeDisplay.getUserName(),
+					},
+					initialCollaborators,
+					itemId,
+					permissionOptions: PERMISSION_OPTIONS,
+					title,
+				}),
+			size: 'md',
+		});
+	}
+	catch (error: any) {
+		openToast({
+			message:
+				error.message ||
+				Liferay.Language.get('an-unexpected-error-occurred'),
+			type: 'danger',
+		});
+	}
+}

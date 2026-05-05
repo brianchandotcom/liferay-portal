275c0ef4ac89e3d9fbafaffb12a912209c2a7a54
Jürgen Kappler <jurgen.kappler@liferay.com>
2026-05-05 17:02:38 +0200

    LPD-88656 Wires bulk Duplicate via triggerAssetBulkAction

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/props_transformer/AssetsFDSPropsTransformer.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/props_transformer/AssetsFDSPropsTransformer.tsx
@@ -40,6 +40,7 @@ import deleteAssetEntriesBulkAction, {
 	executeBulkDeleteAction,
 } from './actions/deleteAssetEntriesBulkAction';
 import deleteItemAction from './actions/deleteItemAction';
+import duplicateBulkAction from './actions/duplicateBulkAction';
 import executeResetPermissionObjectBulkSelectionAction from './actions/executeResetPermissionObjectBulkSelectionAction';
 import expireEntriesBulkAction from './actions/expireEntriesBulkAction';
 import exportTranslationBulkAction from './actions/exportTranslationBulkAction';
@@ -730,6 +731,13 @@ export default function AssetsFDSPropsTransformer({
 						additionalProps.parentObjectEntryFolderExternalReferenceCode,
 					selectedData,
 					singleRoleMode: true,
+				});
+			}
+			else if (action?.data.id === 'duplicate') {
+				duplicateBulkAction({
+					apiURL: otherProps.apiURL,
+					dataSetId: otherProps.id,
+					selectedData,
 				});
 			}
 			else if (action?.data.id === 'expire') {

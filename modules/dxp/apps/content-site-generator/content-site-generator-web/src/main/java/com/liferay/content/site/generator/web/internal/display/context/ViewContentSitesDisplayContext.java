0bf1e5657f8c5ae2555771782635abdbf8c906d5
Mylena Monte <mylena.monte@liferay.com>
2026-05-06 14:45:07 -0300

    LPD-87299 feat: create content site generator FDSView

---
+++ modules/dxp/apps/content-site-generator/content-site-generator-web/src/main/java/com/liferay/content/site/generator/web/internal/display/context/ViewContentSitesDisplayContext.java
@@ -0,0 +1,114 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.content.site.generator.web.internal.display.context;
+
+import com.liferay.frontend.data.set.model.FDSActionDropdownItem;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.CreationMenu;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.CreationMenuBuilder;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItem;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Mylena Monte
+ */
+public class ViewContentSitesDisplayContext {
+
+	public ViewContentSitesDisplayContext(
+		HttpServletRequest httpServletRequest,
+		LiferayPortletResponse liferayPortletResponse) {
+
+		_httpServletRequest = httpServletRequest;
+		_liferayPortletResponse = liferayPortletResponse;
+	}
+
+	public String getAPIURL() {
+		return "/o/content-site-generator/runs";
+	}
+
+	public List<DropdownItem> getBulkActionDropdownItems() throws Exception {
+		return List.of(
+			new FDSActionDropdownItem(
+				getAPIURL() + "/{id}", "trash", "delete",
+				LanguageUtil.get(_httpServletRequest, "delete"), "delete",
+				"delete", "async"));
+	}
+
+	public CreationMenu getCreationMenu() throws Exception {
+		return CreationMenuBuilder.addDropdownItem(
+			dropdownItem -> {
+				dropdownItem.setHref("#");
+				dropdownItem.setIcon("stars");
+				dropdownItem.setLabel(
+					LanguageUtil.get(_httpServletRequest, "new-generator"));
+			}
+		).build();
+	}
+
+	public Map<String, Object> getEmptyState() {
+		return HashMapBuilder.<String, Object>put(
+			"description",
+			LanguageUtil.get(
+				_httpServletRequest, "create-a-new-generator-to-get-started")
+		).put(
+			"title",
+			LanguageUtil.get(_httpServletRequest, "no-content-sites-yet")
+		).build();
+	}
+
+	public List<FDSActionDropdownItem> getFDSActionDropdownItems()
+		throws Exception {
+
+		return List.of(
+			new FDSActionDropdownItem(
+				_getViewRunURL(), "view", "view",
+				LanguageUtil.get(_httpServletRequest, "view"), "get", null,
+				null),
+			new FDSActionDropdownItem(
+				getAPIURL() + "/{id}", "trash", "delete",
+				LanguageUtil.get(_httpServletRequest, "delete"), "delete",
+				"delete", "async"));
+	}
+
+	public long getRunId() {
+		long runId = ParamUtil.getLong(_httpServletRequest, "runId");
+
+		if (runId > 0) {
+			return runId;
+		}
+
+		return GetterUtil.getLong(
+			PortalUtil.getOriginalServletRequest(
+				_httpServletRequest
+			).getParameter(
+				"runId"
+			));
+	}
+
+	private String _getViewRunURL() {
+		return PortletURLBuilder.createRenderURL(
+			_liferayPortletResponse
+		).setMVCPath(
+			"/view_refine_step.jsp"
+		).setParameter(
+			"runId", "{id}"
+		).buildString();
+	}
+
+	private final HttpServletRequest _httpServletRequest;
+	private final LiferayPortletResponse _liferayPortletResponse;
+
+}

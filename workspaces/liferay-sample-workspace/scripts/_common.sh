fb808b86a06b45c5c7daded666c4317a4edcee3c
Brian Chan <brian.chan@liferay.com>
2026-05-06 01:14:27 -0700

    LRSD-12299 No human should ever call this directly

--- workspaces/liferay-sample-workspace/scripts/_common.sh
+++ workspaces/liferay-sample-workspace/scripts/_common.sh
@@ -4,23 +4,14 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-cd "$(dirname "${BASH_SOURCE[0]}")"
+function get_gradle_property {
+	local key=${1}
 
-function main {
-	local key=${1:-}
-
-	if [ -z ${key} ]
-	then
-		echo "Usage: ${0} <key>" >&2
-
-		exit 1
-	fi
-
-	local value=$(read_property ${key} ../gradle-local.properties)
+	local value=$(_read_property ${key} ../gradle-local.properties)
 
 	if [ -z ${value} ]
 	then
-		value=$(read_property ${key} ../gradle.properties)
+		value=$(_read_property ${key} ../gradle.properties)
 	fi
 
 	if [ -z ${value} ]
@@ -33,7 +24,7 @@ function main {
 	echo ${value}
 }
 
-function read_property {
+function _read_property {
 	local key=${1}
 	local file=${2}
 
@@ -42,5 +33,3 @@ function read_property {
 		grep "^${key}=" ${file} | cut --delimiter "=" --fields 2- | tr --delete "[:space:]"
 	fi
 }
-
-main "${@}"

ff36fbb90fdd814daea1360c34fc19c63a32c719
Ryan Schuhler <ryan.schuhler@liferay.com>
2026-05-07 10:40:30 -0700

    LRSD-12299 Add more scripts

---
+++ workspaces/liferay-sample-workspace/scripts/extract_license.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+source _common.sh
+
+function main {
+	local force=false
+
+	if [[ ${1:-} == "-f" || ${1:-} == "--force" ]]
+	then
+		force=true
+	fi
+
+	local license_path="../build/docker/deploy/license.xml"
+
+	if [[ -f ${license_path} && ${force} == false ]]
+	then
+		echo "A trial license already exists at ${license_path}."
+
+		exit 0
+	fi
+
+	local license_dir
+
+	license_dir=$(dirname "${license_path}")
+
+	echo "Extracting the trial license from liferay/dxp:latest."
+
+	mkdir --parents "${license_dir}"
+
+	docker run \
+		--entrypoint sh \
+		--rm \
+		--user root \
+		--volume "${license_dir}:/mnt/deploy" \
+		"liferay/dxp:latest" \
+		-c \
+		"cp /opt/liferay/deploy/trial-dxp-license*.xml /mnt/deploy/license.xml"
+}
+
+main "${@}"

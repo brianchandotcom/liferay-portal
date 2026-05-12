cf3e16326ad693a4101ef9a28312a99f562ffcbc
Jaime Leon <jaime.leon@liferay.com>
2026-05-12 16:02:19 +0200

    LPD-89772 Extract CheckboxSheet component

--- modules/apps/export-import/export-import-web/src/main/resources/META-INF/resources/revamp/js/pages/import/steps/DataSelectionStep.tsx
+++ modules/apps/export-import/export-import-web/src/main/resources/META-INF/resources/revamp/js/pages/import/steps/DataSelectionStep.tsx
@@ -3,13 +3,10 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import ClayLayout from '@clayui/layout';
 import React from 'react';
 
-import {
-	FormikFieldCheckbox,
-	FormikFieldContentSelector,
-} from '../../../components/forms/formik';
+import CheckboxSheet from '../../../components/forms/CheckboxSheet';
+import {FormikFieldContentSelector} from '../../../components/forms/formik';
 import {ImportPreview} from '../../../types/exportImportPreview';
 import FileSummary from './FileSummary';
 
@@ -27,38 +24,22 @@ export default function DataSelectionStep({
 			<FileSummary importPreview={importPreview} />
 
 			{importPreview.deletionCount > 0 && (
-				<ClayLayout.Sheet className="mt-4">
-					<ClayLayout.SheetHeader className="mb-1">
-						<div className="mb-2 sheet-title">
-							{Liferay.Language.get('deletions')}
-						</div>
-					</ClayLayout.SheetHeader>
-
-					<FormikFieldCheckbox
-						description={Liferay.Language.get('deletions-help')}
-						label={Liferay.Language.get(
-							'replicate-selected-deletions'
-						)}
-						name="replicateDeletions"
-					/>
-				</ClayLayout.Sheet>
+				<CheckboxSheet
+					description={Liferay.Language.get('deletions-help')}
+					label={Liferay.Language.get('replicate-selected-deletions')}
+					name="replicateDeletions"
+					title={Liferay.Language.get('deletions')}
+				/>
 			)}
 
-			<ClayLayout.Sheet className="mt-4">
-				<ClayLayout.SheetHeader className="mb-1">
-					<div className="mb-2 sheet-title">
-						{Liferay.Language.get('permissions')}
-					</div>
-				</ClayLayout.SheetHeader>
-
-				<FormikFieldCheckbox
-					description={Liferay.Language.get(
-						'export-import-permissions-help'
-					)}
-					label={Liferay.Language.get('import-permissions')}
-					name="importPermissions"
-				/>
-			</ClayLayout.Sheet>
+			<CheckboxSheet
+				description={Liferay.Language.get(
+					'export-import-permissions-help'
+				)}
+				label={Liferay.Language.get('import-permissions')}
+				name="importPermissions"
+				title={Liferay.Language.get('permissions')}
+			/>
 
 			<FormikFieldContentSelector
 				name="contentSelection"

6073f5e5b8e81347cc3bfc479d11cfa341e48e07
Jose Abelenda <jose.abelenda@liferay.com>
2026-05-12 08:58:57 -0300

    Revert "LPD-84552 Rename"
    This reverts commit da8f08eb84ea8a4bd72141e3e8bc11a080f39bb8.

--- workspaces/liferay-aihub-workspace/client-extensions/liferay-aihub-etc-spring-boot/src/main/java/com/liferay/ai/hub/ObjectActionCrawlerRestController.java
+++ workspaces/liferay-aihub-workspace/client-extensions/liferay-aihub-etc-spring-boot/src/main/java/com/liferay/ai/hub/ObjectActionCrawlerRestController.java
@@ -39,7 +39,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author José Abelenda
  */
-@RequestMapping("/object/action/crawler")
+@RequestMapping("/object/action/crawl")
 @RestController
 public class ObjectActionCrawlerRestController extends BaseRestController {
 

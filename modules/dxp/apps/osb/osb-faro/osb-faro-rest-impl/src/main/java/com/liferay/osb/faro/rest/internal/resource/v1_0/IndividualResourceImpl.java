d735c1c2159e27d1cdf1270cb3bf3d47922d93e1
Leslie Wong <8095751+lesliewong92@users.noreply.github.com>
2026-05-13 13:50:53 -0700

    LPD-89839 Add implementation for individuals request

--- modules/dxp/apps/osb/osb-faro/osb-faro-rest-impl/src/main/java/com/liferay/osb/faro/rest/internal/resource/v1_0/IndividualResourceImpl.java
+++ modules/dxp/apps/osb/osb-faro/osb-faro-rest-impl/src/main/java/com/liferay/osb/faro/rest/internal/resource/v1_0/IndividualResourceImpl.java
@@ -5,9 +5,20 @@
 
 package com.liferay.osb.faro.rest.internal.resource.v1_0;
 
+import com.liferay.osb.faro.engine.client.ContactsEngineClient;
+import com.liferay.osb.faro.engine.client.model.Results;
+import com.liferay.osb.faro.rest.dto.v1_0.Individual;
+import com.liferay.osb.faro.rest.internal.dto.v1_0.converter.FaroDTOConverterContext;
+import com.liferay.osb.faro.rest.internal.dto.v1_0.util.FaroPaginationUtil;
 import com.liferay.osb.faro.rest.resource.v1_0.IndividualResource;
+import com.liferay.osb.faro.service.FaroProjectLocalService;
+import com.liferay.portal.kernel.search.Sort;
+import com.liferay.portal.vulcan.dto.converter.DTOConverter;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.pagination.Pagination;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -18,5 +29,63 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = IndividualResource.class
 )
 public class IndividualResourceImpl extends BaseIndividualResourceImpl {
+
+	@Override
+	public Individual getWorkspaceGroupIndividual(
+			Long groupId, String individualId, String channelId)
+		throws Exception {
+
+		return _individualDTOConverter.toDTO(
+			new FaroDTOConverterContext(
+				contextAcceptLanguage.isAcceptAllLanguages(), individualId,
+				contextAcceptLanguage.getPreferredLocale()),
+			_contactsEngineClient.getIndividual(
+				_faroProjectLocalService.getFaroProjectByGroupId(groupId),
+				individualId, channelId));
+	}
+
+	@Override
+	public Page<Individual> getWorkspaceGroupIndividualsPage(
+			Long groupId, String accountId, String channelId,
+			Boolean includeAnonymousUsers, String individualSegmentId,
+			String interestName, Pagination pagination, Sort[] sorts)
+		throws Exception {
+
+		Results<com.liferay.osb.faro.engine.client.model.Individual> results =
+			_contactsEngineClient.getIndividuals(
+				_faroProjectLocalService.getFaroProjectByGroupId(groupId),
+				accountId, channelId, null, individualSegmentId, null,
+				interestName, null, null, null, null,
+				(includeAnonymousUsers != null) && includeAnonymousUsers,
+				FaroPaginationUtil.getCur(pagination),
+				FaroPaginationUtil.getDelta(pagination),
+				FaroPaginationUtil.toOrderByFields(sorts));
+
+		return Page.of(
+			transform(
+				results.getItems(),
+				individual -> _individualDTOConverter.toDTO(
+					new FaroDTOConverterContext(
+						contextAcceptLanguage.isAcceptAllLanguages(),
+						individual.getId(),
+						contextAcceptLanguage.getPreferredLocale()),
+					individual)),
+			pagination, results.getTotal());
+	}
+
+	@Reference
+	private ContactsEngineClient _contactsEngineClient;
+
+	@Reference
+	private FaroProjectLocalService _faroProjectLocalService;
+
+	@Reference(
+		target = "(component.name=com.liferay.osb.faro.rest.internal.dto.v1_0.converter.IndividualDTOConverter)"
+	)
+	private DTOConverter
+		<com.liferay.osb.faro.engine.client.model.Individual, Individual>
+			_individualDTOConverter;
+
 }
+
 // LIFERAY-REST-BUILDER-HASH:1353815917

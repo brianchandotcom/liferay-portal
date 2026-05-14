666f0b65cb35363a947ac2c23bb45a9cddd601e1
Francesco Acciaro <francesco.acciaro@liferay.com>
2026-05-14 09:32:27 +0200

    DG-1775 Replace taxonomy vocabularies API with mock

--- workspaces/liferay-learn-workspace/client-extensions/liferay-learn-etc-cron-1/src/main/java/com/liferay/learn/Main.java
+++ workspaces/liferay-learn-workspace/client-extensions/liferay-learn-etc-cron-1/src/main/java/com/liferay/learn/Main.java
@@ -1428,16 +1428,20 @@ public class Main {
 		Map<String, String> existingTaxonomyCategories = new HashMap<>();
 		Map<String, Long> existingTaxonomyVocabularies = new HashMap<>();
 
-		com.liferay.headless.admin.taxonomy.client.pagination.Page
-			<TaxonomyVocabulary> taxonomyVocabulariesPage =
-				_taxonomyVocabularyResource.getSiteTaxonomyVocabulariesPage(
-					_globalSiteId, null, null, null,
-					com.liferay.headless.admin.taxonomy.client.pagination.
-						Pagination.of(-1, -1),
-					null);
+		List<Long> taxonomyVocabulariesMockIds = new ArrayList<>() {
+			{
+				add(23488662L);
+				add(23488711L);
+				add(23488721L);
+				add(23488728L);
+				add(24938065L);
+			}
+		};
 
-		for (TaxonomyVocabulary taxonomyVocabulary :
-				taxonomyVocabulariesPage.getItems()) {
+		for (Long taxonomyVocabularyId : taxonomyVocabulariesMockIds) {
+			TaxonomyVocabulary taxonomyVocabulary =
+				_taxonomyVocabularyResource.getTaxonomyVocabulary(
+					taxonomyVocabularyId);
 
 			if (StringUtil.equals(
 					taxonomyVocabulary.getExternalReferenceCode(),
@@ -1488,16 +1492,7 @@ public class Main {
 			Long taxonomyVocabularyId = existingTaxonomyVocabularies.get(name);
 
 			if (taxonomyVocabularyId == null) {
-				TaxonomyVocabulary taxonomyVocabulary =
-					new TaxonomyVocabulary();
-
-				taxonomyVocabulary.setName(() -> name);
-
-				taxonomyVocabulary =
-					_taxonomyVocabularyResource.postSiteTaxonomyVocabulary(
-						_globalSiteId, taxonomyVocabulary);
-
-				taxonomyVocabularyId = taxonomyVocabulary.getId();
+				continue;
 			}
 
 			_loadTaxonomyCategories(

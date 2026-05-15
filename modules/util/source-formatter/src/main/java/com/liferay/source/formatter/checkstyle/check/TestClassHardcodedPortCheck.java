c4b40e7601aab783fe96353171dd96bef20e21f3
Alan Huang <alan.huang@liferay.com>
2026-05-15 14:44:15 +0800

    LPD-89060 Add TestClassHardcodedPortCheck

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/check/TestClassHardcodedPortCheck.java
@@ -0,0 +1,104 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.source.formatter.checkstyle.check;
+
+import com.liferay.portal.kernel.util.ArrayUtil;
+
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+import java.util.List;
+
+/**
+ * @author Alan Huang
+ */
+public class TestClassHardcodedPortCheck extends BaseCheck {
+
+	@Override
+	public int[] getDefaultTokens() {
+		return new int[] {TokenTypes.CLASS_DEF};
+	}
+
+	@Override
+	protected void doVisitToken(DetailAST detailAST) {
+		String absolutePath = getAbsolutePath();
+
+		if (!absolutePath.contains("/test/") &&
+			!absolutePath.contains("/testIntegration/")) {
+
+			return;
+		}
+
+		String name = getName(detailAST);
+
+		if (!name.contains("Test")) {
+			return;
+		}
+
+		List<DetailAST> childDetailASTs = getAllChildTokens(
+			detailAST, true, TokenTypes.NUM_INT, TokenTypes.STRING_LITERAL);
+
+		for (DetailAST childDetailAST : childDetailASTs) {
+			_checkHardcodedPort(childDetailAST);
+		}
+	}
+
+	private void _checkHardcodedPort(DetailAST detailAST) {
+		String text = detailAST.getText();
+
+		if (detailAST.getType() == TokenTypes.NUM_INT) {
+			if (!text.equals("8080")) {
+				return;
+			}
+
+			_checkHardcodedPortByMethodName(
+				detailAST, "endpoint", "setServerPort");
+		}
+		else if (detailAST.getType() == TokenTypes.STRING_LITERAL) {
+			text = text.substring(1, text.length() - 1);
+
+			if (!text.contains(":8080")) {
+				return;
+			}
+
+			log(detailAST, _MSG_USE_METHOD);
+		}
+	}
+
+	private void _checkHardcodedPortByMethodName(
+		DetailAST detailAST, String... methodNames) {
+
+		DetailAST parentDetailAST = detailAST.getParent();
+
+		if ((parentDetailAST == null) ||
+			(parentDetailAST.getType() != TokenTypes.EXPR)) {
+
+			return;
+		}
+
+		parentDetailAST = parentDetailAST.getParent();
+
+		if ((parentDetailAST == null) ||
+			(parentDetailAST.getType() != TokenTypes.ELIST)) {
+
+			return;
+		}
+
+		parentDetailAST = parentDetailAST.getParent();
+
+		if ((parentDetailAST == null) ||
+			(parentDetailAST.getType() != TokenTypes.METHOD_CALL) ||
+			!ArrayUtil.contains(methodNames, getMethodName(parentDetailAST))) {
+
+			return;
+		}
+
+		log(detailAST, _MSG_USE_METHOD);
+	}
+
+	private static final String _MSG_USE_METHOD = "mehtod.use";
+
+}

cbc25af9e792b2a616cbc5ff057af06083d626e5
Javier Moral <javier.moral@liferay.com>
2026-05-22 16:19:33 +0200

    LPD-88748 Move FileEntryUtil, URLUtil and ThumbnailURLReferenceUtil to headless-admin-site-api
    A common SPI module should be a leaf in the dependency graph. Following the pattern of headless-delivery-api exporting com.liferay.headless.delivery.dto.v1_0.util (CreatorUtil and others) for reuse from sibling impls. URLUtil moves alongside FileEntryUtil because it was only consumed by FileEntryUtil and by headless-admin-site-impl. With the three utils relocated, headless-common-spi drops its dependencies on headless-admin-site-api becoming a leaf again.

--- modules/apps/headless/headless-admin-site/headless-admin-site-api/src/main/java/com/liferay/headless/admin/site/dto/v1_0/util/URLUtil.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-api/src/main/java/com/liferay/headless/admin/site/dto/v1_0/util/URLUtil.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.headless.common.spi.util;
+package com.liferay.headless.admin.site.dto.v1_0.util;
 
 import com.liferay.exportimport.attachment.ExportImportAttachmentManagerUtil;
 import com.liferay.petra.io.StreamUtil;

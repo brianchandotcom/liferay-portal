1d2fa07e8d7c7aaf20a8428dd1bb215fa8ed3c13
Davyson Melo <davyson.melo@liferay.com>
2026-05-25 09:48:07 -0300

    LPD-91866 refactor: rename chatbot companyLogo schema field to avatar

--- modules/dxp/apps/ai-hub/ai-hub-web/src/main/resources/META-INF/resources/js/chatbot_form/types/Chatbot.ts
+++ modules/dxp/apps/ai-hub/ai-hub-web/src/main/resources/META-INF/resources/js/chatbot_form/types/Chatbot.ts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-export type CompanyLogoUpload = {
+export type AvatarUpload = {
 	fileBase64: string;
 	mimeType?: string;
 	name: string;
@@ -11,8 +11,8 @@ export type CompanyLogoUpload = {
 
 export type Chatbot = {
 	active: boolean;
-	companyLogo?: number | CompanyLogoUpload | null;
-	companyLogoFileName?: string;
+	avatar?: number | AvatarUpload | null;
+	avatarFileName?: string;
 	description: string;
 	externalReferenceCode: string;
 	introMessage_i18n: {[key: string]: string};

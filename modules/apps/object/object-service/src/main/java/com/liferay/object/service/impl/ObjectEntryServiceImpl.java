a2a844944a3448d6f0a738f9e6986a7d035618da
Brian Chan <brian.chan@liferay.com>
2026-05-25 17:34:54 +0800

    LPD-90187 SF

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryServiceImpl.java
@@ -337,7 +337,6 @@ public class ObjectEntryServiceImpl extends ObjectEntryServiceBaseImpl {
 
 		ModelResourcePermission<ObjectEntry> modelResourcePermission =
 			getModelResourcePermission(objectDefinitionId);
-
 		PermissionChecker permissionChecker = getPermissionChecker();
 
 		return TransformUtil.transform(

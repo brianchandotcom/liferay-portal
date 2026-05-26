03e19c58a5921ff4bbe327569e65f1b4da5c8caa
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2026-05-26 18:42:18 +0100

    LPD-91300 Alphabetic order

--- modules/dxp/apps/osb/osb-faro/osb-faro-engine-client/src/main/java/com/liferay/osb/faro/engine/client/internal/ContactsEngineClientImpl.java
+++ modules/dxp/apps/osb/osb-faro/osb-faro-engine-client/src/main/java/com/liferay/osb/faro/engine/client/internal/ContactsEngineClientImpl.java
@@ -3229,13 +3229,13 @@ public class ContactsEngineClientImpl
 
 	@Override
 	public DataSource patchDataSource(
-		FaroProject faroProject, String id, Credentials credentials,
-		long userId, String name, String url, Provider provider, Event event,
-		String status) {
+		Credentials credentials, Event event, FaroProject faroProject,
+		String id, String name, Provider provider, String status, String url,
+		long userId) {
 
 		return _patchDataSource(
-			Rels.DATA_SOURCE, faroProject, id, credentials, userId, name, url,
-			provider, event, status);
+			credentials, event, faroProject, id, name, provider, status,
+			Rels.DATA_SOURCE, url, userId);
 	}
 
 	@Override
@@ -3295,13 +3295,13 @@ public class ContactsEngineClientImpl
 
 	@Override
 	public DataSource reconnectDataSource(
-		FaroProject faroProject, String id, Credentials credentials,
-		long userId, String name, String url, Provider provider, Event event,
-		String status) {
+		Credentials credentials, Event event, FaroProject faroProject,
+		String id, String name, Provider provider, String status, String url,
+		long userId) {
 
 		return _patchDataSource(
-			Rels.DATA_SOURCE_RECONNECT, faroProject, id, credentials, userId,
-			name, url, provider, event, status);
+			credentials, event, faroProject, id, name, provider, status,
+			Rels.DATA_SOURCE_RECONNECT, url, userId);
 	}
 
 	@Override
@@ -3602,9 +3602,9 @@ public class ContactsEngineClientImpl
 	}
 
 	private DataSource _patchDataSource(
-		String rel, FaroProject faroProject, String id, Credentials credentials,
-		long userId, String name, String url, Provider provider, Event event,
-		String status) {
+		Credentials credentials, Event event, FaroProject faroProject,
+		String id, String name, Provider provider, String status, String rel,
+		String url, long userId) {
 
 		Map<String, Object> dataSourcePatch = new HashMap<>();
 

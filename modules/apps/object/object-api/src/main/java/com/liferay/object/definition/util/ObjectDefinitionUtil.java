3c3476d1bdeae06a66395a79ca1dd3f931c3ed49
Fábio Alves <fabio.alves@liferay.com>
2026-05-27 15:35:33 -0300

    LPD-92338 feat: add agent issue report object definition and its artifacts

--- modules/apps/object/object-api/src/main/java/com/liferay/object/definition/util/ObjectDefinitionUtil.java
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/definition/util/ObjectDefinitionUtil.java
@@ -198,6 +198,8 @@ public class ObjectDefinitionUtil {
 		).put(
 			"AIHubModelArmorTemplate", "/ai-hub/model-armor-templates"
 		).put(
+			"AIHubReport", "/ai-hub/reports"
+		).put(
 			"APIApplication", "/headless-builder/applications"
 		).put(
 			"APIEndpoint", "/headless-builder/endpoints"

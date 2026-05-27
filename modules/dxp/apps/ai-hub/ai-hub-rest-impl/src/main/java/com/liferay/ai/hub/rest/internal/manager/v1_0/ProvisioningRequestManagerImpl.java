67402a660fbec8a364d1d5cb6e5c1918ff420792
Feliphe Marinho <feliphemarinho18@gmail.com>
2026-05-27 15:51:28 -0300

    LPD-90670 refactor: rename and simplify

--- modules/dxp/apps/ai-hub/ai-hub-rest-impl/src/main/java/com/liferay/ai/hub/rest/internal/manager/v1_0/ProvisioningRequestManagerImpl.java
+++ modules/dxp/apps/ai-hub/ai-hub-rest-impl/src/main/java/com/liferay/ai/hub/rest/internal/manager/v1_0/ProvisioningRequestManagerImpl.java
@@ -24,6 +24,7 @@ import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.model.ObjectEntry;
 import com.liferay.object.service.ObjectDefinitionLocalService;
 import com.liferay.object.service.ObjectEntryLocalService;
+import com.liferay.petra.function.transform.TransformUtil;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
@@ -34,17 +35,17 @@ import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.service.RoleLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.UserLocalService;
-import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
-import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.vulcan.dto.converter.DTOConverterContext;
 
 import java.io.Serializable;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
-import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 
@@ -64,10 +65,6 @@ public class ProvisioningRequestManagerImpl
 			ProvisioningRequest provisioningRequest)
 		throws Exception {
 
-		AccountEntry aiHubAccountEntry =
-			_accountEntryService.getAccountEntryByExternalReferenceCode(
-				"L_AI_HUB", company.getCompanyId());
-
 		ServiceContext serviceContext = ServiceContextBuilder.create(
 			company.getGroupId(), dtoConverterContext.getHttpServletRequest(),
 			null
@@ -76,29 +73,25 @@ public class ProvisioningRequestManagerImpl
 		serviceContext.setCompanyId(company.getCompanyId());
 		serviceContext.setUserId(dtoConverterContext.getUserId());
 
-		AccountEntry customerAccountEntry = _getOrAddAccountEntry(
+		AccountEntry customerAccountEntry = _addCustomerAccountEntry(
 			provisioningRequest.getAccountEntryExternalReferenceCode(),
 			provisioningRequest.getAccountEntryName(), serviceContext);
 
-		long[] accountEntryIds = {
-			aiHubAccountEntry.getAccountEntryId(),
-			customerAccountEntry.getAccountEntryId()
-		};
+		_addQuotas(customerAccountEntry, serviceContext);
 
-		_addServiceAccountUsers(
-			customerAccountEntry.getAccountEntryId(), accountEntryIds, company,
-			dtoConverterContext.getLocale(), serviceContext);
+		AccountEntry aiHubAccountEntry =
+			_accountEntryService.getAccountEntryByExternalReferenceCode(
+				"L_AI_HUB", company.getCompanyId());
 
-		_addUserAccounts(
-			customerAccountEntry.getAccountEntryId(), accountEntryIds,
+		List<User> users = _addRegularUsers(
+			aiHubAccountEntry, customerAccountEntry,
 			dtoConverterContext.getLocale(), serviceContext,
 			provisioningRequest.getUserAccounts());
 
-		_addOAuth2Application(
-			"ai-hub-oauth2-" + customerAccountEntry.getAccountEntryId(),
-			provisioningRequest, dtoConverterContext.getUser(), serviceContext);
-
-		_addQuotas(customerAccountEntry, serviceContext);
+		_addServiceAccountUsers(
+			aiHubAccountEntry, company, customerAccountEntry,
+			dtoConverterContext.getLocale(), provisioningRequest,
+			serviceContext);
 
 		return new ProvisioningRequest() {
 			{
@@ -107,16 +100,43 @@ public class ProvisioningRequestManagerImpl
 				setAccountEntryId(customerAccountEntry::getAccountEntryId);
 				setAccountEntryName(customerAccountEntry::getName);
 				setLiferayDXPURL(provisioningRequest::getLiferayDXPURL);
-				setUserAccounts(provisioningRequest::getUserAccounts);
+				setUserAccounts(
+					() -> TransformUtil.transformToArray(
+						users, user -> _toUserAccount(user),
+						UserAccount.class));
 			}
 		};
 	}
 
+	private AccountEntry _addCustomerAccountEntry(
+			String externalReferenceCode, String name,
+			ServiceContext serviceContext)
+		throws Exception {
+
+		AccountEntry accountEntry =
+			_accountEntryService.fetchAccountEntryByExternalReferenceCode(
+				externalReferenceCode, serviceContext.getCompanyId());
+
+		if (accountEntry != null) {
+			return accountEntry;
+		}
+
+		return _accountEntryService.addAccountEntry(
+			externalReferenceCode, serviceContext.getUserId(),
+			AccountConstants.PARENT_ACCOUNT_ENTRY_ID_DEFAULT, name, null, null,
+			null, null, null, AccountConstants.ACCOUNT_ENTRY_TYPE_BUSINESS,
+			WorkflowConstants.STATUS_APPROVED, serviceContext);
+	}
+
 	private void _addOAuth2Application(
-			String externalReferenceCode,
+			AccountEntry customerAccountEntry,
 			ProvisioningRequest provisioningRequest, User user,
 			ServiceContext serviceContext)
 		throws Exception {
+
+		String externalReferenceCode =
+			customerAccountEntry.getAccountEntryId() +
+				"-ai-hub-oauth2-application";
 
 		OAuth2Application oAuth2Application =
 			_oAuth2ApplicationLocalService.
@@ -127,19 +147,16 @@ public class ProvisioningRequestManagerImpl
 			return;
 		}
 
-		String portalURL = provisioningRequest.getLiferayDXPURL();
-
 		_oAuth2ApplicationLocalService.addOrUpdateOAuth2Application(
 			externalReferenceCode, user.getUserId(), user.getFullName(),
-			Collections.singletonList(GrantType.CLIENT_CREDENTIALS),
-			"client_secret_post", user.getUserId(),
-			OAuth2SecureRandomGenerator.generateClientId(),
+			List.of(GrantType.CLIENT_CREDENTIALS), "client_secret_post",
+			user.getUserId(), OAuth2SecureRandomGenerator.generateClientId(),
 			ClientProfile.HEADLESS_SERVER.id(),
-			OAuth2SecureRandomGenerator.generateClientSecret(), null,
-			Collections.emptyList(), portalURL, 0, null,
-			provisioningRequest.getAccountEntryName(), null,
-			Collections.singletonList(portalURL), false,
-			Collections.emptyList(), false, serviceContext);
+			OAuth2SecureRandomGenerator.generateClientSecret(), null, List.of(),
+			provisioningRequest.getLiferayDXPURL(), 0, null,
+			provisioningRequest.getAccountEntryName(), null, null, false,
+			Arrays.asList("Liferay.AI.Hub.REST.everything"), false,
+			serviceContext);
 	}
 
 	private void _addQuotaObjectEntry(
@@ -188,24 +205,79 @@ public class ProvisioningRequestManagerImpl
 			objectDefinition, serviceContext);
 	}
 
-	private void _addServiceAccountUsers(
-			long accountEntryId, long[] accountEntryIds, Company company,
-			Locale locale, ServiceContext serviceContext)
+	private List<User> _addRegularUsers(
+			AccountEntry aiHubAccountEntry, AccountEntry customerAccountEntry,
+			Locale locale, ServiceContext serviceContext,
+			UserAccount[] userAccounts)
 		throws Exception {
 
-		User serviceAccountUser = _getOrAddUser(
-			company, locale, accountEntryId + "-service-account",
+		Group group = _groupLocalService.getGroup(
+			serviceContext.getCompanyId(), "AI Hub");
+
+		List<User> users = new ArrayList<>();
+
+		for (UserAccount userAccount : userAccounts) {
+			User user = _addUser(
+				serviceContext.getCompanyId(), locale,
+				userAccount.getScreenName(), userAccount.getEmailAddress(),
+				userAccount.getFirstName(), userAccount.getLastName(),
+				UserConstants.TYPE_REGULAR, serviceContext);
+
+			_userLocalService.addGroupUsers(
+				group.getGroupId(), new long[] {user.getUserId()});
+
+			_associateUserToAccountEntries(
+				aiHubAccountEntry, customerAccountEntry, user);
+
+			users.add(user);
+		}
+
+		return users;
+	}
+
+	private void _addServiceAccountUsers(
+			AccountEntry aiHubAccountEntry, Company company,
+			AccountEntry customerAccountEntry, Locale locale,
+			ProvisioningRequest provisioningRequest,
+			ServiceContext serviceContext)
+		throws Exception {
+
+		long customerAccountEntryId = customerAccountEntry.getAccountEntryId();
+
+		User guestServiceAccountUser = _addUser(
+			company, locale, customerAccountEntryId + "-guest-service-account",
 			serviceContext);
 
 		_accountEntryUserRelLocalService.updateAccountEntryUserRels(
-			accountEntryIds, new long[0], serviceAccountUser.getUserId());
+			new long[] {
+				aiHubAccountEntry.getAccountEntryId(), customerAccountEntryId
+			},
+			new long[0], guestServiceAccountUser.getUserId());
 
-		User guestServiceAccountUser = _getOrAddUser(
-			company, locale, accountEntryId + "-guest-service-account",
+		User serviceAccountUser = _addUser(
+			company, locale, customerAccountEntryId + "-service-account",
 			serviceContext);
 
 		_accountEntryUserRelLocalService.updateAccountEntryUserRels(
-			accountEntryIds, new long[0], guestServiceAccountUser.getUserId());
+			new long[] {
+				aiHubAccountEntry.getAccountEntryId(), customerAccountEntryId
+			},
+			new long[0], serviceAccountUser.getUserId());
+
+		_addOAuth2Application(
+			customerAccountEntry, provisioningRequest, serviceAccountUser,
+			serviceContext);
+	}
+
+	private User _addUser(
+			Company company, Locale locale, String screenName,
+			ServiceContext serviceContext)
+		throws Exception {
+
+		return _addUser(
+			company.getCompanyId(), locale, screenName,
+			screenName + StringPool.AT + company.getMx(), screenName,
+			screenName, UserConstants.TYPE_SERVICE_ACCOUNT, serviceContext);
 	}
 
 	private User _addUser(
@@ -214,111 +286,74 @@ public class ProvisioningRequestManagerImpl
 			ServiceContext serviceContext)
 		throws Exception {
 
-		return _userLocalService.addUser(
-			UserConstants.USER_ID_DEFAULT, companyId, true, null, null, false,
-			screenName, emailAddress, locale, firstName, StringPool.BLANK,
-			lastName, 0, 0, true, Calendar.JANUARY, 1, 1970, StringPool.BLANK,
-			type, null, null, null, null, false, serviceContext);
-	}
+		User user = null;
 
-	private void _addUserAccounts(
-			long accountEntryId, long[] accountEntryIds, Locale locale,
-			ServiceContext serviceContext, UserAccount[] userAccounts)
-		throws Exception {
-
-		if (ArrayUtil.isEmpty(userAccounts)) {
-			return;
+		if (Validator.isNull(screenName)) {
+			user = _userLocalService.fetchUserByEmailAddress(
+				companyId, emailAddress);
 		}
-
-		Role role = _roleLocalService.getRole(
-			serviceContext.getCompanyId(), "AI Hub Agent Manager");
-
-		AccountRole accountRole1 =
-			_accountRoleLocalService.getAccountRoleByRoleId(role.getRoleId());
-
-		long accountRoleId = accountRole1.getAccountRoleId();
-
-		Group group = _groupLocalService.getGroup(
-			serviceContext.getCompanyId(), "AI Hub");
-
-		List<User> users = _userLocalService.getGroupUsers(group.getGroupId());
-
-		for (UserAccount userAccount : userAccounts) {
-			User user = _userLocalService.fetchUserByEmailAddress(
-				serviceContext.getCompanyId(), userAccount.getEmailAddress());
-
-			if (user == null) {
-				user = _addUser(
-					serviceContext.getCompanyId(), locale,
-					userAccount.getScreenName(), userAccount.getEmailAddress(),
-					userAccount.getFirstName(), userAccount.getLastName(),
-					UserConstants.TYPE_REGULAR, serviceContext);
-			}
-
-			if (!users.contains(user)) {
-				_userLocalService.addGroupUsers(
-					group.getGroupId(), new long[] {user.getUserId()});
-			}
-
-			_accountEntryUserRelLocalService.updateAccountEntryUserRels(
-				accountEntryIds, new long[0], user.getUserId());
-
-			List<AccountRole> accountRoles = ListUtil.filter(
-				_accountRoleLocalService.getAccountRoles(
-					accountEntryId, user.getUserId()),
-				accountRole2 ->
-					accountRole2.getAccountRoleId() == accountRoleId);
-
-			if (ListUtil.isNotEmpty(accountRoles)) {
-				continue;
-			}
-
-			_accountRoleLocalService.associateUser(
-				accountEntryId, accountRoleId, user.getUserId());
+		else {
+			user = _userLocalService.fetchUserByScreenName(
+				companyId, screenName);
 		}
-	}
-
-	private AccountEntry _getOrAddAccountEntry(
-			String externalReferenceCode, String name,
-			ServiceContext serviceContext)
-		throws Exception {
-
-		AccountEntry accountEntry =
-			_accountEntryService.fetchAccountEntryByExternalReferenceCode(
-				externalReferenceCode, serviceContext.getCompanyId());
-
-		if (accountEntry != null) {
-			return accountEntry;
-		}
-
-		return _accountEntryService.addAccountEntry(
-			externalReferenceCode, serviceContext.getUserId(),
-			AccountConstants.PARENT_ACCOUNT_ENTRY_ID_DEFAULT, name, null, null,
-			null, null, null, AccountConstants.ACCOUNT_ENTRY_TYPE_BUSINESS,
-			WorkflowConstants.STATUS_APPROVED, serviceContext);
-	}
-
-	private User _getOrAddUser(
-			Company company, Locale locale, String screenName,
-			ServiceContext serviceContext)
-		throws Exception {
-
-		User user = _userLocalService.fetchUserByScreenName(
-			company.getCompanyId(), screenName);
 
 		if (user != null) {
 			return user;
 		}
 
-		user = _addUser(
-			company.getCompanyId(), locale, screenName,
-			screenName + StringPool.AT + company.getMx(), screenName,
-			screenName, UserConstants.TYPE_SERVICE_ACCOUNT, serviceContext);
+		user = _userLocalService.addUser(
+			UserConstants.USER_ID_DEFAULT, companyId, true, null, null, false,
+			screenName, emailAddress, locale, firstName, StringPool.BLANK,
+			lastName, 0, 0, true, Calendar.JANUARY, 1, 1970, StringPool.BLANK,
+			type, null, null, null, null, false, serviceContext);
+
+		if (!user.isServiceAccountUser()) {
+			return user;
+		}
 
 		user.setPasswordReset(false);
 		user.setEmailAddressVerified(true);
 
 		return _userLocalService.updateUser(user);
+	}
+
+	private void _associateUserToAccountEntries(
+			AccountEntry aiHubAccountEntry, AccountEntry customerAccountEntry,
+			User user)
+		throws Exception {
+
+		_accountEntryUserRelLocalService.updateAccountEntryUserRels(
+			new long[] {
+				aiHubAccountEntry.getAccountEntryId(),
+				customerAccountEntry.getAccountEntryId()
+			},
+			new long[0], user.getUserId());
+
+		Role role = _roleLocalService.getRole(
+			aiHubAccountEntry.getCompanyId(), "AI Hub Agent Manager");
+
+		AccountRole accountRole =
+			_accountRoleLocalService.getAccountRoleByRoleId(role.getRoleId());
+
+		if (!_accountRoleLocalService.hasUserAccountRole(
+				customerAccountEntry.getAccountEntryId(),
+				accountRole.getAccountRoleId(), user.getUserId())) {
+
+			_accountRoleLocalService.associateUser(
+				customerAccountEntry.getAccountEntryId(),
+				accountRole.getAccountRoleId(), user.getUserId());
+		}
+	}
+
+	private UserAccount _toUserAccount(User user) {
+		return new UserAccount() {
+			{
+				setEmailAddress(user::getEmailAddress);
+				setFirstName(user::getFirstName);
+				setLastName(user::getLastName);
+				setScreenName(user::getScreenName);
+			}
+		};
 	}
 
 	private static final int _QUOTA_TOKEN_LIMIT = 33333333;

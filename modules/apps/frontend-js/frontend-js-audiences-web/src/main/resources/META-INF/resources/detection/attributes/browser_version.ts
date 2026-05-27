b2d4632b616be7ed4678524b1e3f64e8df56fd3b
Iván Zaera Avellón <ivan.zaera@liferay.com>
2026-05-27 09:15:08 +0200

    LPD-92006 Move cached state to Detection
    Since we have Senna (SPA) in DXP, we cannot store cached page scoped
    stuff in the JS interpreter memory because it would survive navigations,
    so we internalize it to Detection.
    
    See
    https://github.com/liferay-frontend/liferay-portal/pull/5502#discussion_r3304066303.

--- modules/apps/frontend-js/frontend-js-audiences-web/src/main/resources/META-INF/resources/detection/attributes/browser_version.ts
+++ modules/apps/frontend-js/frontend-js-audiences-web/src/main/resources/META-INF/resources/detection/attributes/browser_version.ts
@@ -3,8 +3,8 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {uaParser} from '../util';
+import UAParser from 'ua-parser-js';
 
-export function getBrowserVersion(): string {
+export function getBrowserVersion(uaParser: UAParser): string {
 	return uaParser.getBrowser().version ?? '';
 }

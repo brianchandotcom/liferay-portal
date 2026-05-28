f7925b70e024ed5d711032ff2da252acf1ddff7f
Magdalena Jedraszak <magdalena.jedraszak@liferay.com>
2026-05-28 10:39:20 +0200

    LPD-68626 Refactor

--- modules/test/playwright/tests/export-import-web/main/instance.importReport.spec.ts
+++ modules/test/playwright/tests/export-import-web/main/instance.importReport.spec.ts
@@ -28,125 +28,220 @@ export const test = mergeTests(
 );
 
 async function setupImportReportScenario(apiHelpers: DataApiHelpers) {
-	const objectDefinitionA = await _createObjectDefinition(
-		apiHelpers,
-		'A',
-		'site'
-	);
-	const objectDefinitionB = await _createObjectDefinition(
-		apiHelpers,
-		'B',
-		'site'
-	);
-	const objectDefinitionC = await _createObjectDefinition(
-		apiHelpers,
-		'C',
-		'company'
-	);
-	const objectDefinitionD = await _createObjectDefinition(
-		apiHelpers,
-		'D',
-		'site'
-	);
-	const objectDefinitionZ = await _createObjectDefinition(
-		apiHelpers,
-		'Z',
-		'site'
-	);
+	const [objectDefinition1, objectDefinition2] = await Promise.all([
+		apiHelpers.objectAdmin.postRandomObjectDefinition({
+			scope: 'site',
+			status: {code: 0},
+		}),
+		apiHelpers.objectAdmin.postRandomObjectDefinition({
+			scope: 'site',
+			status: {code: 0},
+		}),
+	]);
 
-	const relationshipName = 'rel' + getRandomInt();
-
-	await _createOneToManyRelationship(
-		apiHelpers,
-		objectDefinitionC,
-		objectDefinitionD,
-		relationshipName
-	);
-
-	await apiHelpers.objectEntry.postObjectEntry(
-		{},
-		`${normalizeRestPath(objectDefinitionA.restContextPath)}/scopes/Guest`
-	);
-
-	await apiHelpers.objectEntry.postObjectEntry(
-		{},
-		`${normalizeRestPath(objectDefinitionB.restContextPath)}/scopes/Guest`
-	);
-
-	await apiHelpers.objectEntry.postObjectEntry(
-		{},
-		`${normalizeRestPath(objectDefinitionZ.restContextPath)}/scopes/Guest`
-	);
-
-	const objectEntryC = await apiHelpers.objectEntry.postObjectEntry(
-		{},
-		normalizeRestPath(objectDefinitionC.restContextPath)
-	);
-
-	await apiHelpers.objectEntry.postObjectEntry(
-		{
-			externalReferenceCode: `ERC-D-${getRandomInt()}`,
-			[`r_${relationshipName}_c_${objectDefinitionC.name[0].toLowerCase() + objectDefinitionC.name.substring(1)}Id`]:
-				objectEntryC.id,
-		},
-		`${normalizeRestPath(objectDefinitionD.restContextPath)}/scopes/Guest`
-	);
-
-	return {
-		objectDefinitionA,
-		objectDefinitionB,
-		objectDefinitionC,
-		objectDefinitionD,
-		objectDefinitionZ,
-		objectEntryC,
-	};
-}
-
-async function _createObjectDefinition(
-	apiHelpers: DataApiHelpers,
-	prefix: string,
-	scope: 'site' | 'company'
-) {
-	const objectDefinition =
+	const objectDefinition3 =
 		await apiHelpers.objectAdmin.postRandomObjectDefinition({
-			objectDefinitionExternalReferenceCode:
-				`${prefix}ObjectDefinition` + getRandomInt(),
-			scope: scope === 'site' ? 'site' : undefined,
 			status: {code: 0},
 		});
 
-	apiHelpers.data.push({id: objectDefinition.id, type: 'objectDefinition'});
+	for (const objectDefinition of [
+		objectDefinition1,
+		objectDefinition2,
+		objectDefinition3,
+	]) {
+		apiHelpers.data.push({
+			id: objectDefinition.id,
+			type: 'objectDefinition',
+		});
+	}
 
-	return objectDefinition;
-}
-
-async function _createOneToManyRelationship(
-	apiHelpers: DataApiHelpers,
-	objectDefinition1: any,
-	objectDefinition2: any,
-	name: string
-) {
 	const objectRelationshipAPIClient = await apiHelpers.buildRestClient(
 		ObjectRelationshipAPI
 	);
 
+	const relationshipName = 'relationship';
+
 	await objectRelationshipAPIClient.postObjectDefinitionByExternalReferenceCodeObjectRelationship(
-		objectDefinition1.externalReferenceCode,
+		objectDefinition3.externalReferenceCode,
 		{
 			deletionType: 'disassociate',
 			label: {en_US: 'Relationship'},
-			name,
+			name: relationshipName,
 			objectDefinitionExternalReferenceCode1:
-				objectDefinition1.externalReferenceCode,
+				objectDefinition3.externalReferenceCode,
 			objectDefinitionExternalReferenceCode2:
 				objectDefinition2.externalReferenceCode,
-			objectDefinitionId1: objectDefinition1.id,
+			objectDefinitionId1: objectDefinition3.id,
 			objectDefinitionId2: objectDefinition2.id,
 			objectDefinitionName2: objectDefinition2.name,
 			type: 'oneToMany',
 		}
 	);
+
+	await apiHelpers.objectEntry.postObjectEntry(
+		{},
+		`${normalizeRestPath(objectDefinition1.restContextPath)}/scopes/Guest`
+	);
+
+	const objectEntry = await apiHelpers.objectEntry.postObjectEntry(
+		{},
+		normalizeRestPath(objectDefinition3.restContextPath)
+	);
+
+	await apiHelpers.objectEntry.postObjectEntry(
+		{
+			[`r_${relationshipName}_c_${objectDefinition3.name[0].toLowerCase() + objectDefinition3.name.substring(1)}Id`]:
+				objectEntry.id,
+		},
+		`${normalizeRestPath(objectDefinition2.restContextPath)}/scopes/Guest`
+	);
+
+	return {
+		objectDefinition1,
+		objectDefinition2,
+		objectDefinition3,
+		objectEntry,
+	};
 }
+
+test(
+	'Can filter, search and sort errors report entries',
+	{tag: '@LPD-68461'},
+	async ({apiHelpers, exportImportPage}) => {
+		const {
+			objectDefinition1,
+			objectDefinition2,
+			objectDefinition3,
+			objectEntry,
+		} = await setupImportReportScenario(apiHelpers);
+
+		await exportImportPage.goToExport();
+
+		const exportFilePath = await exportImportPage.export({
+			portletLabels: [
+				`${objectDefinition1.name} 1 Items`,
+				`${objectDefinition2.name} 1 Items`,
+			],
+		});
+
+		const objectFieldAPIClient =
+			await apiHelpers.buildRestClient(ObjectFieldAPI);
+
+		await objectFieldAPIClient.postObjectDefinitionObjectField(
+			objectDefinition1.id,
+			{
+				DBType: 'String',
+				businessType: 'Text',
+				label: {en_US: 'mandatoryField'},
+				name: 'mandatoryField',
+				required: true,
+			}
+		);
+
+		await apiHelpers.objectEntry.deleteObjectEntry(
+			normalizeRestPath(objectDefinition3.restContextPath),
+			String(objectEntry.id)
+		);
+
+		const site = await apiHelpers.headlessAdminSite.postSite({
+			name: 'TestSite' + getRandomInt(),
+		});
+
+		await exportImportPage.goToImport(site.friendlyUrlPath);
+
+		await exportImportPage.import({
+			filePath: exportFilePath,
+			taskStatus: 'completedWithErrors',
+		});
+
+		const exportName = exportFilePath.slice(
+			exportFilePath.lastIndexOf('/') + 1
+		);
+
+		await exportImportPage.goToImportDetails(exportName);
+
+		// Sort by Entity Type
+
+		await exportImportPage.sortReportBy('Entity Type');
+		let values =
+			await exportImportPage.getReportColumnValues('Entity Type');
+		expect(values).toEqual([...values].sort((a, b) => a.localeCompare(b)));
+
+		await exportImportPage.sortReportBy('Entity Type');
+		values = await exportImportPage.getReportColumnValues('Entity Type');
+		expect(values).toEqual([...values].sort((a, b) => b.localeCompare(a)));
+
+		// Sort by External Reference Code
+
+		await exportImportPage.sortReportBy('External Reference Code');
+		values = await exportImportPage.getReportColumnValues(
+			'External Reference Code'
+		);
+		expect(values).toEqual([...values].sort((a, b) => a.localeCompare(b)));
+
+		await exportImportPage.sortReportBy('External Reference Code');
+		values = await exportImportPage.getReportColumnValues(
+			'External Reference Code'
+		);
+		expect(values).toEqual([...values].sort((a, b) => b.localeCompare(a)));
+
+		// Search by Entity Type name
+
+		await exportImportPage.searchReportEntries(objectDefinition1.name);
+		values = await exportImportPage.getReportColumnValues('Entity Type');
+		expect(values).toEqual([objectDefinition1.name]);
+
+		await exportImportPage.clearReportSearch();
+
+		// Search by External Reference Code
+
+		await exportImportPage.searchReportEntries(
+			objectEntry.externalReferenceCode
+		);
+		values = await exportImportPage.getReportColumnValues(
+			'External Reference Code'
+		);
+		expect(values).toEqual([objectEntry.externalReferenceCode]);
+
+		await exportImportPage.clearReportSearch();
+
+		// Filter by Entity Type
+
+		await exportImportPage.filterReportBy(
+			'Entity Type',
+			objectDefinition1.name
+		);
+		values = await exportImportPage.getReportColumnValues('Entity Type');
+		expect(values).toEqual([objectDefinition1.name]);
+
+		await exportImportPage.removeReportFilter();
+
+		// Filter by External Reference Code
+
+		await exportImportPage.filterReportBy(
+			'External Reference Code',
+			objectEntry.externalReferenceCode
+		);
+		values = await exportImportPage.getReportColumnValues(
+			'External Reference Code'
+		);
+		expect(values).toEqual([objectEntry.externalReferenceCode]);
+
+		await exportImportPage.removeReportFilter();
+
+		// Filter by Type
+
+		await exportImportPage.filterReportBy('Type', 'Empty');
+		values = await exportImportPage.getReportColumnValues('Type');
+		expect(values).toEqual(['Empty']);
+
+		await exportImportPage.excludeReportFilter();
+		values = await exportImportPage.getReportColumnValues('Type');
+		expect(values).toEqual(['Error']);
+
+		await exportImportPage.removeReportFilter();
+	}
+);
 
 test('Can see error report and details', async ({
 	apiHelpers,
@@ -154,29 +249,20 @@ test('Can see error report and details', async ({
 	exportImportPage,
 	globalMenuPage,
 }) => {
-	const objectDefinition =
-		await apiHelpers.objectAdmin.postRandomObjectDefinition({
-			status: {code: 0},
-		});
-
-	apiHelpers.data.push({id: objectDefinition.id, type: 'objectDefinition'});
-
-	const objectEntry = await apiHelpers.objectEntry.postObjectEntry(
-		{externalReferenceCode: '', name: objectDefinition.name},
-		normalizeRestPath(`${objectDefinition.restContextPath}`)
-	);
+	const {objectDefinition3, objectEntry} =
+		await setupImportReportScenario(apiHelpers);
 
 	await globalMenuPage.goToApplications('Export');
 
 	const exportFilePath = await exportImportPage.export({
-		portletLabels: [`${objectDefinition.name} 1 Items`],
+		portletLabels: [`${objectDefinition3.name} 1 Items`],
 	});
 
 	const objectFieldAPIClient =
 		await apiHelpers.buildRestClient(ObjectFieldAPI);
 
 	await objectFieldAPIClient.postObjectDefinitionObjectField(
-		objectDefinition.id,
+		objectDefinition3.id,
 		{
 			DBType: 'String',
 			businessType: 'Text',
@@ -234,185 +320,22 @@ test('Can see error report and details', async ({
 	).toBeVisible();
 });
 
-test(
-	'Can filter, search and sort errors report entries',
-	{tag: '@LPD-68461'},
-	async ({apiHelpers, exportImportPage}) => {
-		const {
-			objectDefinitionA,
-			objectDefinitionB,
-			objectDefinitionC,
-			objectDefinitionD,
-			objectDefinitionZ,
-			objectEntryC,
-		} = await setupImportReportScenario(apiHelpers);
-
-		await exportImportPage.goToExport();
-
-		const exportFilePath = await exportImportPage.export({
-			portletLabels: [
-				`${objectDefinitionA.name} 1 Items`,
-				`${objectDefinitionB.name} 1 Items`,
-				`${objectDefinitionZ.name} 1 Items`,
-				`${objectDefinitionD.name} 1 Items`,
-			],
-		});
-
-		const objectFieldAPIClient =
-			await apiHelpers.buildRestClient(ObjectFieldAPI);
-
-		for (const objectDefinition of [
-			objectDefinitionA,
-			objectDefinitionB,
-			objectDefinitionZ,
-		]) {
-			await objectFieldAPIClient.postObjectDefinitionObjectField(
-				objectDefinition.id,
-				{
-					DBType: 'String',
-					businessType: 'Text',
-					label: {en_US: 'mandatoryField'},
-					name: 'mandatoryField',
-					required: true,
-				}
-			);
-		}
-
-		await apiHelpers.objectEntry.deleteObjectEntry(
-			normalizeRestPath(objectDefinitionC.restContextPath),
-			String(objectEntryC.id)
-		);
-
-		const newSite = await apiHelpers.headlessAdminSite.postSite({
-			name: 'TestSite' + getRandomInt(),
-		});
-
-		await exportImportPage.goToImport(newSite.friendlyUrlPath);
-
-		await exportImportPage.import({
-			filePath: exportFilePath,
-			taskStatus: 'completedWithErrors',
-		});
-
-		const exportName = exportFilePath.slice(
-			exportFilePath.lastIndexOf('/') + 1
-		);
-
-		await exportImportPage.goToImportDetails(exportName);
-
-		// Sort by Entity Type
-
-		await exportImportPage.sortReportBy('Entity Type');
-		let values =
-			await exportImportPage.getReportColumnValues('Entity Type');
-		expect(values).toEqual([...values].sort((a, b) => a.localeCompare(b)));
-
-		await exportImportPage.sortReportBy('Entity Type');
-		values = await exportImportPage.getReportColumnValues('Entity Type');
-		expect(values).toEqual([...values].sort((a, b) => b.localeCompare(a)));
-
-		// Sort by External Reference Code
-
-		await exportImportPage.sortReportBy('External Reference Code');
-		values = await exportImportPage.getReportColumnValues(
-			'External Reference Code'
-		);
-		expect(values).toEqual([...values].sort((a, b) => a.localeCompare(b)));
-
-		await exportImportPage.sortReportBy('External Reference Code');
-		values = await exportImportPage.getReportColumnValues(
-			'External Reference Code'
-		);
-		expect(values).toEqual([...values].sort((a, b) => b.localeCompare(a)));
-
-		// Search by Entity Type name
-
-		await exportImportPage.searchReportEntries(objectDefinitionA.name);
-		values = await exportImportPage.getReportColumnValues('Entity Type');
-		expect(values).toEqual([objectDefinitionA.name]);
-
-		await exportImportPage.clearReportSearch();
-
-		// Search by External Reference Code
-
-		const ercValues = await exportImportPage.getReportColumnValues(
-			'External Reference Code'
-		);
-		const searchERC = ercValues[0];
-
-		await exportImportPage.searchReportEntries(searchERC);
-		values = await exportImportPage.getReportColumnValues(
-			'External Reference Code'
-		);
-		expect(values).toEqual([searchERC]);
-
-		await exportImportPage.clearReportSearch();
-
-		// Filter by Entity Type
-
-		await exportImportPage.filterReportBy(
-			'Entity Type',
-			objectDefinitionA.name
-		);
-		values = await exportImportPage.getReportColumnValues('Entity Type');
-		expect(values).toEqual([objectDefinitionA.name]);
-
-		await exportImportPage.removeReportFilter();
-
-		// Filter by External Reference Code
-
-		await exportImportPage.filterReportBy(
-			'External Reference Code',
-			searchERC
-		);
-		values = await exportImportPage.getReportColumnValues(
-			'External Reference Code'
-		);
-		expect(values).toEqual([searchERC]);
-
-		await exportImportPage.removeReportFilter();
-
-		// Filter by Type
-
-		await exportImportPage.filterReportBy('Type', 'Empty');
-		values = await exportImportPage.getReportColumnValues('Type');
-		expect(values).toEqual(['Empty']);
-
-		await exportImportPage.excludeReportFilter();
-		values = await exportImportPage.getReportColumnValues('Type');
-		expect(values).toEqual(['Error', 'Error', 'Error']);
-
-		await exportImportPage.removeReportFilter();
-	}
-);
-
 test('Report entries actions are not visible for a successful import', async ({
 	apiHelpers,
-	companyExportImportPage,
 	exportImportPage,
-	globalMenuPage,
 }) => {
-	const objectDefinition =
-		await apiHelpers.objectAdmin.postRandomObjectDefinition({
-			status: {code: 0},
-		});
+	const {objectDefinition1} = await setupImportReportScenario(apiHelpers);
 
-	apiHelpers.data.push({id: objectDefinition.id, type: 'objectDefinition'});
-
-	await apiHelpers.objectEntry.postObjectEntry(
-		{externalReferenceCode: '', name: objectDefinition.name},
-		normalizeRestPath(`${objectDefinition.restContextPath}`)
-	);
-
-	await globalMenuPage.goToApplications('Export');
+	await exportImportPage.goToExport();
 
 	const exportFilePath = await exportImportPage.export({
-		portletLabels: [`${objectDefinition.name} 1 Items`],
+		portletLabels: [`${objectDefinition1.name} 1 Items`],
 	});
 
-	await companyExportImportPage.import({
+	await exportImportPage.goToImport();
+
+	await exportImportPage.import({
 		filePath: exportFilePath,
-		includePermissions: true,
 	});
 
 	const exportName = exportFilePath.slice(

2f95e0a9e8e74d7fbfcf04a628d9bb36d4183278
David Truong <david.truong@liferay.com>
2026-06-01 10:31:17 -0500

    LPD-76298 Remove Launch Service Builder entities
    # breaking
    
    ## What modules/apps/launch/launch-api/bnd.bnd
    
    All Java packages previously exported by com.liferay.launch.api - com.liferay.launch.exception, com.liferay.launch.model, com.liferay.launch.service, and com.liferay.launch.service.persistence - have been removed. The bundle now exports only com.liferay.launch.constants.
    
    ## Why
    
    The Launch persistence layer was migrated from Service Builder to Liferay Object Definitions, so the generated SB API surface no longer exists. The Bundle-Version is bumped from 3.0.0 to 4.0.0 to reflect the package removal.
    
    ## Alternatives
    
    Use the headless Objects REST endpoint at /o/launches (and /o/launch-entries for the LaunchEntry data) instead of the previous LaunchSetLocalService and LaunchEntryLocalService APIs.
    
    ----
    
    # breaking
    
    ## What modules/apps/launch/launch-service/bnd.bnd
    
    The Liferay-Service and Liferay-Require-SchemaVersion headers were removed, and the Liferay-Client-Extension-Batch header was added. The bundle no longer ships any Service Builder generated classes or persistence implementations.
    
    ## Why
    
    The Launch persistence layer was migrated from Service Builder to Liferay Object Definitions. The bundle now ships a single batch-engine-data JSON that the BatchEngineBundleTracker discovers via the Liferay-Client-Extension-Batch header on activation. The Bundle-Version is bumped from 1.0.4 to 2.0.0 to reflect the removal of generated classes and the persistence layer.
    
    ## Alternatives
    
    Use the headless Objects REST endpoint at /o/launches instead of the previous LaunchSetLocalService and LaunchEntryLocalService APIs.
    
    ----

---
+++ modules/apps/launch/launch-api/src/main/java/com/liferay/launch/constants/LaunchConstants.java
@@ -0,0 +1,21 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.launch.constants;
+
+/**
+ * @author Dave Truong
+ */
+public class LaunchConstants {
+
+	public static final String OBJECT_DEFINITION_ERC_LAUNCH = "L_LAUNCH";
+
+	public static final String OBJECT_DEFINITION_ERC_LAUNCH_ENTRY =
+		"L_LAUNCH_ENTRY";
+
+	public static final String OBJECT_RELATIONSHIP_ERC_LAUNCH_TO_LAUNCH_ENTRY =
+		"L_LAUNCH_TO_L_LAUNCH_ENTRY";
+
+}

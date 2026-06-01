b2a9779f63960860e759305174599530799572d0
Magdalena Jedraszak <magdalena.jedraszak@liferay.com>
2026-06-01 19:00:46 +0200

    LPD-68626 Use template method to make getExportImportConfigurationId private

--- modules/apps/export-import/export-import-web/src/main/java/com/liferay/exportimport/web/internal/frontend/data/set/filter/BaseImportReportEntrySelectionFDSFilter.java
+++ modules/apps/export-import/export-import-web/src/main/java/com/liferay/exportimport/web/internal/frontend/data/set/filter/BaseImportReportEntrySelectionFDSFilter.java
@@ -7,6 +7,7 @@ package com.liferay.exportimport.web.internal.frontend.data.set.filter;
 
 import com.liferay.frontend.data.set.constants.FDSEntityFieldTypes;
 import com.liferay.frontend.data.set.filter.BaseSelectionFDSFilter;
+import com.liferay.frontend.data.set.filter.SelectionFDSFilterItem;
 import com.liferay.portal.kernel.backgroundtask.BackgroundTask;
 import com.liferay.portal.kernel.backgroundtask.BackgroundTaskManager;
 import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
@@ -20,6 +21,9 @@ import jakarta.servlet.http.HttpServletRequest;
 
 import java.io.Serializable;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import org.osgi.service.component.annotations.Reference;
@@ -35,7 +39,28 @@ public abstract class BaseImportReportEntrySelectionFDSFilter
 		return FDSEntityFieldTypes.STRING;
 	}
 
-	protected long getExportImportConfigurationId() {
+	@Override
+	public List<SelectionFDSFilterItem> getSelectionFDSFilterItems(
+		Locale locale) {
+
+		long exportImportConfigurationId = _getExportImportConfigurationId();
+
+		if (exportImportConfigurationId == 0) {
+			return Collections.emptyList();
+		}
+
+		return doGetSelectionFDSFilterItems(
+			exportImportConfigurationId, locale);
+	}
+
+	protected abstract List<SelectionFDSFilterItem>
+		doGetSelectionFDSFilterItems(
+			long exportImportConfigurationId, Locale locale);
+
+	@Reference
+	protected BackgroundTaskManager backgroundTaskManager;
+
+	private long _getExportImportConfigurationId() {
 		ServiceContext serviceContext =
 			ServiceContextThreadLocal.getServiceContext();
 
@@ -92,8 +117,5 @@ public abstract class BaseImportReportEntrySelectionFDSFilter
 		return GetterUtil.getLong(
 			taskContextMap.get("exportImportConfigurationId"));
 	}
-
-	@Reference
-	protected BackgroundTaskManager backgroundTaskManager;
 
 }

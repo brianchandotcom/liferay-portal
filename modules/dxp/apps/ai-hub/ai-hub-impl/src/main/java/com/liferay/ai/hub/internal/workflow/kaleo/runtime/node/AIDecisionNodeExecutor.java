406ae88b4c73685c6b9e749c837406d8f01af093
Feliphe Marinho <feliphemarinho18@gmail.com>
2026-06-02 18:49:15 -0300

    LPD-92125 feat: audit content retrieval
    Co-authored-by: Pedro Leite <pedro.leite@liferay.com>

--- modules/dxp/apps/ai-hub/ai-hub-impl/src/main/java/com/liferay/ai/hub/internal/workflow/kaleo/runtime/node/AIDecisionNodeExecutor.java
+++ modules/dxp/apps/ai-hub/ai-hub-impl/src/main/java/com/liferay/ai/hub/internal/workflow/kaleo/runtime/node/AIDecisionNodeExecutor.java
@@ -238,7 +238,8 @@ public class AIDecisionNodeExecutor extends BaseNodeExecutor {
 					_fieldConfigBuilderFactory, _highlightBuilderFactory,
 					kaleoNodeSettingValues, serviceContext.getLocale(),
 					_objectEntryManager, _searchEngineAdapter,
-					serviceContext.getUserId(), workflowContext)
+					serviceContext.getUserId(), workflowContext,
+					kaleoInstanceToken.getKaleoInstanceId())
 			).systemMessageProviderFunction(
 				memoryId -> prompt
 			).tools(

704d3e25af9180823c1d3696577bdd3a26e4c096
Iván Zaera Avellón <ivan.zaera@liferay.com>
2026-06-02 10:35:54 +0200

    LPD-92754 Send follow-up to settle on naming and concepts

--- modules/apps/frontend-js/frontend-js-audiences-web/src/main/resources/META-INF/resources/detection/attributes/language.ts
+++ modules/apps/frontend-js/frontend-js-audiences-web/src/main/resources/META-INF/resources/detection/attributes/language.ts
@@ -3,9 +3,6 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-export function getSearchParam(
-	name: string,
-	urlSearchParams: URLSearchParams
-): string {
-	return urlSearchParams.get(name) ?? '';
+export function getLanguage(): string {
+	return navigator.language;
 }

412f290e60027f986de85f2436bb04c9ad7ec24d
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2026-06-02 09:36:20 +0100

    LPD-92872 It should be operator

--- modules/apps/segments/segments-web/src/main/java/com/liferay/segments/web/internal/audiences/AudiencesJSONObjectBuilder.java
+++ modules/apps/segments/segments-web/src/main/java/com/liferay/segments/web/internal/audiences/AudiencesJSONObjectBuilder.java
@@ -51,7 +51,7 @@ public class AudiencesJSONObjectBuilder {
 				"attribute",
 				_attributeNames.getOrDefault(propertyName, propertyName)
 			).put(
-				"operation",
+				"operator",
 				StringUtil.replace(
 					queryJSONObject.getString("operatorName"), '-', '_')
 			).put(

f3d7b137a51954157730c3d3c5600512457570e6
Gabriel Lima <gabriel.lima@liferay.com>
2026-06-03 11:16:33 -0300

    LPD-88085 Add StyleBookTokenSet service logic

--- modules/apps/style-book/style-book-service/src/main/java/com/liferay/style/book/service/impl/StyleBookTokenSetServiceImpl.java
+++ modules/apps/style-book/style-book-service/src/main/java/com/liferay/style/book/service/impl/StyleBookTokenSetServiceImpl.java
@@ -6,12 +6,22 @@
 package com.liferay.style.book.service.impl;
 
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.style.book.constants.StyleBookActionKeys;
+import com.liferay.style.book.constants.StyleBookConstants;
+import com.liferay.style.book.model.StyleBookEntry;
+import com.liferay.style.book.model.StyleBookTokenSet;
+import com.liferay.style.book.service.StyleBookEntryLocalService;
 import com.liferay.style.book.service.base.StyleBookTokenSetServiceBaseImpl;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
- * @author Brian Wing Shun Chan
+ * @author Gabriel Lima
+ * @see    StyleBookTokenSetServiceBaseImpl
  */
 @Component(
 	property = {
@@ -22,4 +32,33 @@ import org.osgi.service.component.annotations.Component;
 )
 public class StyleBookTokenSetServiceImpl
 	extends StyleBookTokenSetServiceBaseImpl {
+
+	@Override
+	public StyleBookTokenSet addStyleBookTokenSet(
+			String externalReferenceCode, String description,
+			String frontendTokenCategoryName, String name,
+			long styleBookEntryId, String themeId,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		StyleBookEntry styleBookEntry =
+			_styleBookEntryLocalService.getStyleBookEntry(styleBookEntryId);
+
+		_portletResourcePermission.check(
+			getPermissionChecker(), styleBookEntry.getGroupId(),
+			StyleBookActionKeys.MANAGE_STYLE_BOOK_ENTRIES);
+
+		return styleBookTokenSetLocalService.addStyleBookTokenSet(
+			externalReferenceCode, description, frontendTokenCategoryName, name,
+			styleBookEntryId, themeId, serviceContext);
+	}
+
+	@Reference(
+		target = "(resource.name=" + StyleBookConstants.RESOURCE_NAME + ")"
+	)
+	private PortletResourcePermission _portletResourcePermission;
+
+	@Reference
+	private StyleBookEntryLocalService _styleBookEntryLocalService;
+
 }

1852fd3ab06e0cdec23b7f96100002f25daad6bc
Leslie Wong <8095751+lesliewong92@users.noreply.github.com>
2026-06-03 17:37:14 -0700

    LPD-92417 Add implementation for performance metric by location and categorization

--- modules/apps/analytics/analytics-cms-rest-impl/src/main/java/com/liferay/analytics/cms/rest/internal/resource/v1_0/PerformanceMetricResourceImpl.java
+++ modules/apps/analytics/analytics-cms-rest-impl/src/main/java/com/liferay/analytics/cms/rest/internal/resource/v1_0/PerformanceMetricResourceImpl.java
@@ -5,9 +5,21 @@
 
 package com.liferay.analytics.cms.rest.internal.resource.v1_0;
 
+import com.liferay.analytics.cms.rest.dto.v1_0.PerformanceMetric;
+import com.liferay.analytics.cms.rest.internal.client.AnalyticsCloudClient;
+import com.liferay.analytics.cms.rest.internal.depot.entry.util.DepotEntryUtil;
 import com.liferay.analytics.cms.rest.resource.v1_0.PerformanceMetricResource;
+import com.liferay.analytics.settings.rest.manager.AnalyticsSettingsManager;
+import com.liferay.portal.kernel.license.util.LicenseManagerUtil;
+import com.liferay.portal.kernel.util.Http;
+import com.liferay.portal.kernel.util.StringUtil;
+
+import jakarta.ws.rs.BadRequestException;
+
+import java.util.Arrays;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -19,4 +31,56 @@ import org.osgi.service.component.annotations.ServiceScope;
 )
 public class PerformanceMetricResourceImpl
 	extends BasePerformanceMetricResourceImpl {
+
+	@Override
+	public PerformanceMetric getPerformanceMetric(
+			Long[] depotEntryIds, String groupBy, String metricType,
+			Integer rangeKey)
+		throws Exception {
+
+		LicenseManagerUtil.checkFreeTier();
+
+		_validateMetricType(metricType);
+
+		Long[] groupIds = DepotEntryUtil.getGroupIds(
+			DepotEntryUtil.getDepotEntries(
+				contextCompany.getCompanyId(), depotEntryIds));
+
+		AnalyticsCloudClient analyticsCloudClient = new AnalyticsCloudClient(
+			_http);
+
+		return analyticsCloudClient.getPerformanceMetric(
+			_analyticsSettingsManager.getAnalyticsConfiguration(
+				contextCompany.getCompanyId()),
+			Arrays.asList(groupIds), metricType, _getPath(groupBy), rangeKey);
+	}
+
+	private String _getPath(String groupBy) {
+		if (StringUtil.equalsIgnoreCase(groupBy, "categories")) {
+			return "/categories";
+		}
+
+		if (StringUtil.equalsIgnoreCase(groupBy, "location")) {
+			return "/geolocation";
+		}
+
+		throw new BadRequestException("Invalid group by: " + groupBy);
+	}
+
+	private void _validateMetricType(String metricType) {
+		if (!StringUtil.equalsIgnoreCase(metricType, "downloadsMetric") &&
+			!StringUtil.equalsIgnoreCase(metricType, "impressionsMetric") &&
+			!StringUtil.equalsIgnoreCase(metricType, "readsMetric") &&
+			!StringUtil.equalsIgnoreCase(metricType, "viewsMetric")) {
+
+			throw new BadRequestException("Invalid metric type: " + metricType);
+		}
+	}
+
+	@Reference
+	private AnalyticsSettingsManager _analyticsSettingsManager;
+
+	@Reference
+	private Http _http;
+
 }

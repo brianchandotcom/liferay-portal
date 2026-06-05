2e900dab1bdd3d1d32a068edae6bb8bee2db599e
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2026-06-05 09:27:23 +0100

    LPD-93510 Add PathName as new criteria for Audiences

--- modules/apps/segments/segments-api/src/main/java/com/liferay/segments/context/Context.java
+++ modules/apps/segments/segments-api/src/main/java/com/liferay/segments/context/Context.java
@@ -35,6 +35,8 @@ public class Context
 
 	public static final String LOCAL_DATE = "localDate";
 
+	public static final String PATHNAME = "pathname";
+
 	public static final String REFERRER_URL = "referrerURL";
 
 	public static final String REQUEST_PARAMETERS = "requestParameters";

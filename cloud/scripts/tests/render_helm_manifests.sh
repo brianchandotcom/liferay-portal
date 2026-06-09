e9fb5275afe67f8098f4a50ccdf2823d5402ece4
Carlos Alvarez <carlos.alvarez@liferay.com>
2026-06-09 15:48:39 -0300

    LCD-51987 Remove orphaned space

--- cloud/scripts/tests/render_helm_manifests.sh
+++ cloud/scripts/tests/render_helm_manifests.sh
@@ -30,7 +30,7 @@ function main {
 			--schema-location default \
 			--schema-location 'https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/{{.Group}}/{{.ResourceKind}}_{{.ResourceAPIVersion}}.json' \
 			--skip ClusterProviderConfig,LiferayInfrastructure \
-	 		--strict \
+			--strict \
 			--summary
 	done
 }

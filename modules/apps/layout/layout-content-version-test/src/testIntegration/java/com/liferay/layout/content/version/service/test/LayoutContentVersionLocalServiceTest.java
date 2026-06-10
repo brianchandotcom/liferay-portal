3474c54664dddbe1baf0f3ff74ebb321a5f630bd
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2026-06-10 08:30:40 +0200

    LPD-90025 Adapt Test Usages

--- modules/apps/layout/layout-content-version-test/src/testIntegration/java/com/liferay/layout/content/version/service/test/LayoutContentVersionLocalServiceTest.java
+++ modules/apps/layout/layout-content-version-test/src/testIntegration/java/com/liferay/layout/content/version/service/test/LayoutContentVersionLocalServiceTest.java
@@ -3,15 +3,15 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.layout.content.versioning.service.test;
+package com.liferay.layout.content.version.service.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
-import com.liferay.layout.content.versioning.exception.LayoutContentVersionExternalReferenceCodeException;
-import com.liferay.layout.content.versioning.exception.LayoutContentVersionNameException;
-import com.liferay.layout.content.versioning.exception.RequiredLayoutContentVersionException;
-import com.liferay.layout.content.versioning.exception.UnsupportedLayoutLayoutContentVersionException;
-import com.liferay.layout.content.versioning.model.LayoutContentVersion;
-import com.liferay.layout.content.versioning.service.LayoutContentVersionLocalService;
+import com.liferay.layout.content.version.exception.LayoutContentVersionExternalReferenceCodeException;
+import com.liferay.layout.content.version.exception.LayoutContentVersionNameException;
+import com.liferay.layout.content.version.exception.RequiredLayoutContentVersionException;
+import com.liferay.layout.content.version.exception.UnsupportedLayoutLayoutContentVersionException;
+import com.liferay.layout.content.version.model.LayoutContentVersion;
+import com.liferay.layout.content.version.service.LayoutContentVersionLocalService;
 import com.liferay.layout.page.template.constants.LayoutPageTemplateEntryTypeConstants;
 import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
 import com.liferay.layout.page.template.test.util.DisplayPageTemplateTestUtil;

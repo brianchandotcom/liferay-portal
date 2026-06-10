68b4b10a690ab668bd28ae8643f744d61c7ba4a6
Nícolas Moura <nicolas.moura@liferay.com>
2026-06-10 20:12:07 -0300

    LPD-70673 Randomize unasserted email literals

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectEntryLocalServiceTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectEntryLocalServiceTest.java
@@ -7584,7 +7584,7 @@ public class ObjectEntryLocalServiceTest {
 		ObjectEntry objectEntry = _addObjectEntry(
 			objectDefinition,
 			HashMapBuilder.<String, Serializable>put(
-				objectFieldName, "user@example.com"
+				objectFieldName, RandomTestUtil.randomString() + "@example.com"
 			).build(),
 			ServiceContextTestUtil.getServiceContext());
 

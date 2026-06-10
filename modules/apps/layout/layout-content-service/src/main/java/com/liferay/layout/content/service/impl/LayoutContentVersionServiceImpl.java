472cc437477c4406653dd29016fcae1f82537854
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2026-06-10 09:49:21 +0200

    LPD-90025 Adapt usages

--- modules/apps/layout/layout-content-service/src/main/java/com/liferay/layout/content/service/impl/LayoutContentVersionServiceImpl.java
+++ modules/apps/layout/layout-content-service/src/main/java/com/liferay/layout/content/service/impl/LayoutContentVersionServiceImpl.java
@@ -3,10 +3,10 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.layout.content.versioning.service.impl;
+package com.liferay.layout.content.service.impl;
 
-import com.liferay.layout.content.versioning.model.LayoutContentVersion;
-import com.liferay.layout.content.versioning.service.base.LayoutContentVersionServiceBaseImpl;
+import com.liferay.layout.content.model.LayoutContentVersion;
+import com.liferay.layout.content.service.base.LayoutContentVersionServiceBaseImpl;
 import com.liferay.portal.aop.AopService;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Layout;

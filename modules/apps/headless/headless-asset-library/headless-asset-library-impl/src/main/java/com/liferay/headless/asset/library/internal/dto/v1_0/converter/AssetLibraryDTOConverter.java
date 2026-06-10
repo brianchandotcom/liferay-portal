8f52bae982febdef45f7ea379ab53348713fbb4e
Victor Cunha <victor.cunha@liferay.com>
2026-06-10 18:46:44 -0300

    Revert "LPD-91035 if value is 0, return the TRASH_ENTRIES_MAX_AGE value by default for the company"
    This reverts commit 009b6211089673f7d7ba64d3148271a7a5e2a7dc.

--- modules/apps/headless/headless-asset-library/headless-asset-library-impl/src/main/java/com/liferay/headless/asset/library/internal/dto/v1_0/converter/AssetLibraryDTOConverter.java
+++ modules/apps/headless/headless-asset-library/headless-asset-library-impl/src/main/java/com/liferay/headless/asset/library/internal/dto/v1_0/converter/AssetLibraryDTOConverter.java
@@ -26,9 +26,6 @@ import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.Portal;
-import com.liferay.portal.kernel.util.PrefsPropsUtil;
-import com.liferay.portal.kernel.util.PropsKeys;
-import com.liferay.portal.kernel.util.PropsValues;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.vulcan.dto.converter.DTOConverter;
@@ -227,20 +224,8 @@ public class AssetLibraryDTOConverter
 					() -> GetterUtil.getBoolean(
 						unicodeProperties.get("trashEnabled"), true));
 				setTrashEntriesMaxAge(
-					() -> {
-						int trashEntriesMaxAge = GetterUtil.getInteger(
-							unicodeProperties.getProperty(
-								"trashEntriesMaxAge"));
-
-						if (trashEntriesMaxAge > 0) {
-							return trashEntriesMaxAge;
-						}
-
-						return PrefsPropsUtil.getInteger(
-							group.getCompanyId(),
-							PropsKeys.TRASH_ENTRIES_MAX_AGE,
-							PropsValues.TRASH_ENTRIES_MAX_AGE);
-					});
+					() -> GetterUtil.getInteger(
+						unicodeProperties.getProperty("trashEntriesMaxAge")));
 				setUseCustomLanguages(
 					() -> !GetterUtil.getBoolean(
 						unicodeProperties.get("inheritLocales")));

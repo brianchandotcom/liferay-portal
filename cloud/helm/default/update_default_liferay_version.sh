f374a34b80f1b6c3a7aeb53903dd0621608d40bb
Carlos Alvarez <carlos.alvarez@liferay.com>
2026-06-11 09:27:24 -0300

    LCD-52136 SF

--- cloud/helm/default/update_default_liferay_version.sh
+++ cloud/helm/default/update_default_liferay_version.sh
@@ -4,26 +4,6 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-function _check_if_image_exists {
-	local tag="${1}"
-
-	local http_code
-
-	http_code=$(curl \
-		--max-time 30 \
-		--output /dev/null \
-		--silent \
-		--write-out '%{http_code}' \
-		"https://hub.docker.com/v2/repositories/liferay/dxp/tags/${tag}")
-
-	if [ "${http_code}" != "200" ]
-	then
-		echo "Image liferay/dxp:${tag} does not exist on Docker Hub."
-
-		exit 1
-	fi
-}
-
 function main {
 	local latest_lts_dxp_version
 
@@ -32,9 +12,9 @@ function main {
 		sed 's/^dxp-//' | \
 		head --lines=1)
 
-	if [ -z "${latest_lts_dxp_version}" ]
+	if [[ -z "${latest_lts_dxp_version}" ]]
 	then
-		echo "Unable to determine the latest LTS DXP version."
+		echo "Unable to determine the latest LTS DXP version" >&2
 
 		exit 1
 	fi
@@ -48,7 +28,28 @@ function main {
 	sed \
 		--in-place \
 		--regexp-extended \
-		"s/^ {4}tag:.*/    tag: ${latest_lts_dxp_version}/" "${script_dir}/values.yaml"
+		"s/^ {4}tag:.*/    tag: ${latest_lts_dxp_version}/" \
+		"${script_dir}/values.yaml"
+}
+
+function _check_if_image_exists {
+	local tag="${1}"
+
+	local http_code
+
+	http_code=$(curl \
+		--max-time 30 \
+		--output /dev/null \
+		--silent \
+		--write-out '%{http_code}' \
+		"https://hub.docker.com/v2/repositories/liferay/dxp/tags/${tag}")
+
+	if [[ "${http_code}" != "200" ]]
+	then
+		echo "Image liferay/dxp:${tag} does not exist on DockerHub" >&2
+
+		exit 1
+	fi
 }
 
 main "${@}"

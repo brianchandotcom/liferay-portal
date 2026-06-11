014d0e65323d3a977c7fe0c4fa4b6d4275e6c482
Crescenzo Rega <97021604+CrescenzoRegaLF@users.noreply.github.com>
2026-06-11 10:40:57 +0200

    LPD-93885  sf

--- modules/node-scripts.config.js
+++ modules/node-scripts.config.js
@@ -10,10 +10,11 @@
  */
 
 module.exports = {
-	hash: '670ceb6206f55f62e32cbfbf81c4c6302fd7ce63c392308cc88a9607356a5406',
+	hash: '5e2a9a821ecfc2da19271d2fbdd2ea2d04953fd43e4d4f672462da49e70258fa',
 	imports: {
 		'@liferay/accessibility-menu-web': [],
 		'@liferay/accessibility-settings-state-web': [],
+		'@liferay/account-validator-vies-web': [],
 		'@liferay/address-web': [],
 		'@liferay/ai-creator-openai-web': [],
 		'@liferay/ai-hub-cell-js-components-web': [],

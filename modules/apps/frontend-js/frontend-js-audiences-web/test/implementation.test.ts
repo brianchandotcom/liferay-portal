5c74e5e906b09a0e28c439fc05358b9cd20c40f9
Iván Zaera Avellón <ivan.zaera@liferay.com>
2026-06-11 12:13:33 +0200

    LPD-93208 Move main entry point to its own folder

--- modules/apps/frontend-js/frontend-js-audiences-web/test/implementation.test.ts
+++ modules/apps/frontend-js/frontend-js-audiences-web/test/implementation.test.ts
@@ -3,8 +3,8 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import * as audiences from '../src/main/resources/META-INF/resources/implementation';
-import {store} from '../src/main/resources/META-INF/resources/store';
+import * as audiences from '../src/main/resources/META-INF/resources/main/implementation';
+import {store} from '../src/main/resources/META-INF/resources/main/store';
 
 describe('implementation', () => {
 	afterEach(async () => {

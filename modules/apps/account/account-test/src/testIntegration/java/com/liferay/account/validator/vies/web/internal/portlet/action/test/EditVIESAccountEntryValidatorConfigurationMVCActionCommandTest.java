9aee22f7ce59933d76ae1a8878cb5e19beb774f9
Crescenzo Rega <97021604+CrescenzoRegaLF@users.noreply.github.com>
2026-06-11 12:31:18 +0200

    LPD-93885 Apply the changes requested by bchan-bot-pr-review

--- modules/apps/account/account-test/src/testIntegration/java/com/liferay/account/validator/vies/web/internal/portlet/action/test/EditVIESAccountEntryValidatorConfigurationMVCActionCommandTest.java
+++ modules/apps/account/account-test/src/testIntegration/java/com/liferay/account/validator/vies/web/internal/portlet/action/test/EditVIESAccountEntryValidatorConfigurationMVCActionCommandTest.java
@@ -29,6 +29,7 @@ import com.liferay.portal.kernel.test.rule.Sync;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringUtil;
@@ -142,7 +143,7 @@ public class EditVIESAccountEntryValidatorConfigurationMVCActionCommandTest {
 						VIESAccountEntryValidatorConfiguration.class.getName(),
 						".scoped)(companyId=", companyId, "))"));
 
-			if ((configurations != null) && (configurations.length > 0)) {
+			if (ArrayUtil.isNotEmpty(configurations)) {
 				return configurations[0];
 			}
 

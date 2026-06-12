188494bca6a383994f7e9f555915c056d9acab1d
Rafael Praxedes <rafael.praxedes@liferay.com>
2026-06-12 14:00:51 -0300

    LPD-88765 Move the consent checker to internal.cookies.consent
    Every concern in the consent management platform module is consent related, so the internal.consent package was ambiguous. Mirror the com.liferay.cookies.consent package of the CookiesConsentChecker interface instead, as its other implementations do.

--- modules/apps/consent-management-platform/consent-management-platform-integration-impl/src/main/java/com/liferay/consent/management/platform/integration/internal/cookies/consent/ConsentManagementPlatformCookiesConsentChecker.java
+++ modules/apps/consent-management-platform/consent-management-platform-integration-impl/src/main/java/com/liferay/consent/management/platform/integration/internal/cookies/consent/ConsentManagementPlatformCookiesConsentChecker.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.consent.management.platform.integration.internal.consent;
+package com.liferay.consent.management.platform.integration.internal.cookies.consent;
 
 import com.liferay.cookies.consent.CookiesConsentChecker;
 import com.liferay.petra.string.StringBundler;

42c234dd16c13be4f3173a6f47acd8aec3b7d110
Rafael Praxedes <rafael.praxedes@liferay.com>
2026-06-12 14:00:51 -0300

    LPD-88765 Move the consent checker to internal.cookies.consent
    Every concern in the consent management platform module is consent related, so the internal.consent package was ambiguous. Mirror the com.liferay.cookies.consent package of the CookiesConsentChecker interface instead, as its other implementations do.

--- modules/apps/consent-management-platform/consent-management-platform-integration-test/src/testIntegration/java/com/liferay/consent/management/platform/integration/internal/cookies/consent/test/ConsentManagementPlatformCookiesConsentCheckerTest.java
+++ modules/apps/consent-management-platform/consent-management-platform-integration-test/src/testIntegration/java/com/liferay/consent/management/platform/integration/internal/cookies/consent/test/ConsentManagementPlatformCookiesConsentCheckerTest.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.consent.management.platform.integration.internal.consent.test;
+package com.liferay.consent.management.platform.integration.internal.cookies.consent.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.cookies.consent.CookiesConsentChecker;
@@ -235,7 +235,7 @@ public class ConsentManagementPlatformCookiesConsentCheckerTest {
 	}
 
 	@Inject(
-		filter = "component.name=com.liferay.consent.management.platform.integration.internal.consent.ConsentManagementPlatformCookiesConsentChecker"
+		filter = "component.name=com.liferay.consent.management.platform.integration.internal.cookies.consent.ConsentManagementPlatformCookiesConsentChecker"
 	)
 	private CookiesConsentChecker _cookiesConsentChecker;
 

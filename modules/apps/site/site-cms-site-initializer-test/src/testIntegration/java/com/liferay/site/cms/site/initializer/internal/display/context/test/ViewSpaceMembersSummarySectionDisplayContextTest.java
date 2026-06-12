eb2f029d3757e06edcf23044f5aab51ab197212a
Mikel Lorza <mikel.lorza@liferay.com>
2026-06-12 12:42:21 +0200

    LPD-94636 Use the depot entry as the members summary test info item
    The test drove the display context by adding an L_CMS_BASIC_WEB_CONTENT object entry in the target depot and setting it as the info item. That object definition is pinned to the "space" domain, so once LPD-88820 started validating that a depot scoped object entry's domain matches the depot subtype, adding it to a TYPE_PROJECT depot was rejected with InvalidGroupIdForDomain.
    
    The display context only needs the group id, which the fragment renderer resolves through InfoItemUtil.getGroupId. That method already accepts a DepotEntry as the info item, so the test now sets the depot entry itself for both the space and project cases. This exercises the same code path without creating an object entry, removing the dependency on the object definition's domain entirely.

--- modules/apps/site/site-cms-site-initializer-test/src/testIntegration/java/com/liferay/site/cms/site/initializer/internal/display/context/test/ViewSpaceMembersSummarySectionDisplayContextTest.java
+++ modules/apps/site/site-cms-site-initializer-test/src/testIntegration/java/com/liferay/site/cms/site/initializer/internal/display/context/test/ViewSpaceMembersSummarySectionDisplayContextTest.java
@@ -11,8 +11,6 @@ import com.liferay.depot.model.DepotEntry;
 import com.liferay.depot.service.DepotEntryLocalService;
 import com.liferay.fragment.renderer.FragmentRenderer;
 import com.liferay.info.constants.InfoDisplayWebKeys;
-import com.liferay.object.rest.test.util.ObjectEntryTestUtil;
-import com.liferay.object.service.ObjectDefinitionLocalService;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.model.Group;
@@ -29,8 +27,6 @@ import com.liferay.portal.test.rule.FeatureFlags;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
-
-import java.io.Serializable;
 
 import java.util.List;
 import java.util.Map;
@@ -67,19 +63,7 @@ public class ViewSpaceMembersSummarySectionDisplayContextTest
 			ServiceContextTestUtil.getServiceContext());
 
 		mockHttpServletRequest.setAttribute(
-			InfoDisplayWebKeys.INFO_ITEM,
-			ObjectEntryTestUtil.addObjectEntry(
-				depotEntry.getGroupId(),
-				_objectDefinitionLocalService.
-					fetchObjectDefinitionByExternalReferenceCode(
-						"L_CMS_BASIC_WEB_CONTENT",
-						TestPropsValues.getCompanyId()),
-				HashMapBuilder.<String, Serializable>put(
-					"title_i18n",
-					HashMapBuilder.put(
-						"en_US", RandomTestUtil.randomString()
-					).build()
-				).build()));
+			InfoDisplayWebKeys.INFO_ITEM, depotEntry);
 
 		_assertHeaderProps(null, depotEntry.getGroup());
 
@@ -89,19 +73,7 @@ public class ViewSpaceMembersSummarySectionDisplayContextTest
 			ServiceContextTestUtil.getServiceContext());
 
 		mockHttpServletRequest.setAttribute(
-			InfoDisplayWebKeys.INFO_ITEM,
-			ObjectEntryTestUtil.addObjectEntry(
-				projectDepotEntry.getGroupId(),
-				_objectDefinitionLocalService.
-					fetchObjectDefinitionByExternalReferenceCode(
-						"L_CMS_BASIC_WEB_CONTENT",
-						TestPropsValues.getCompanyId()),
-				HashMapBuilder.<String, Serializable>put(
-					"title_i18n",
-					HashMapBuilder.put(
-						"en_US", RandomTestUtil.randomString()
-					).build()
-				).build()));
+			InfoDisplayWebKeys.INFO_ITEM, projectDepotEntry);
 
 		_assertHeaderProps(
 			HashMapBuilder.<String, Object>put(
@@ -175,8 +147,5 @@ public class ViewSpaceMembersSummarySectionDisplayContextTest
 		filter = "component.name=com.liferay.site.cms.site.initializer.internal.fragment.renderer.ViewSpaceMembersSummaryJSPSectionFragmentRenderer"
 	)
 	private FragmentRenderer _fragmentRenderer;
-
-	@Inject
-	private ObjectDefinitionLocalService _objectDefinitionLocalService;
 
 }

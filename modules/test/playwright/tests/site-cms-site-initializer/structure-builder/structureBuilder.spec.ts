a3ac83f5f174dc6e7898c7c888ac30e2ba5db2a9
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2026-06-12 10:08:28 +0200

    LPD-94566 Add missing FF

--- modules/test/playwright/tests/site-cms-site-initializer/structure-builder/structureBuilder.spec.ts
+++ modules/test/playwright/tests/site-cms-site-initializer/structure-builder/structureBuilder.spec.ts
@@ -23,6 +23,7 @@ const test = mergeTests(
 	cmsPagesTest,
 	featureFlagsTest({
 		'LPD-17564': {enabled: true},
+		'LPD-70672': {enabled: true},
 		'LPD-83570': {enabled: true},
 	}),
 	loginTest(),

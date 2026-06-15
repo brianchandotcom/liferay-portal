5e9558f4efc09b432207e6d9d0b957e8663fea22
Carlos Correa <carlos.correa@liferay.com>
2026-06-15 11:17:25 +0200

    LPD-87027 Surface feedback when a Site Template sync is triggered
    Wrap the Site Template sync trigger in the portlet action in a try-catch so the user is told what happened instead of the action completing silently. On success an info toast announces that the sync started and a notification will follow when it completes; on failure the error is logged and an error toast is shown. The view JSP renders both messages as toasts and the init JSP imports the helpers it needs.

--- modules/apps/layout/layout-set-prototype-web/src/main/java/com/liferay/layout/set/prototype/web/internal/portlet/LayoutSetPrototypePortlet.java
+++ modules/apps/layout/layout-set-prototype-web/src/main/java/com/liferay/layout/set/prototype/web/internal/portlet/LayoutSetPrototypePortlet.java
@@ -10,8 +10,12 @@ import com.liferay.application.list.constants.ApplicationListWebKeys;
 import com.liferay.application.list.display.context.logic.PanelCategoryHelper;
 import com.liferay.layout.set.prototype.constants.LayoutSetPrototypePortletKeys;
 import com.liferay.layout.set.prototype.helper.LayoutSetPrototypeHelper;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.NoSuchLayoutSetPrototypeException;
 import com.liferay.portal.kernel.exception.RequiredLayoutSetPrototypeException;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.LayoutSetPrototype;
 import com.liferay.portal.kernel.portlet.PortalPreferences;
 import com.liferay.portal.kernel.portlet.PortletPreferencesFactoryUtil;
@@ -21,6 +25,7 @@ import com.liferay.portal.kernel.service.LayoutSetPrototypeService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextFactory;
 import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.servlet.SessionMessages;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.Localization;
@@ -122,8 +127,46 @@ public class LayoutSetPrototypePortlet extends MVCPortlet {
 		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
 			WebKeys.THEME_DISPLAY);
 
-		layoutSetPrototypeHelper.executeLayoutSetPrototypeSync(
-			layoutSetPrototypeId, themeDisplay.getUserId());
+		hideDefaultSuccessMessage(actionRequest);
+
+		try {
+			layoutSetPrototypeHelper.executeLayoutSetPrototypeSync(
+				layoutSetPrototypeId, themeDisplay.getUserId());
+
+			LayoutSetPrototype layoutSetPrototype =
+				layoutSetPrototypeService.fetchLayoutSetPrototype(
+					layoutSetPrototypeId);
+
+			String layoutSetPrototypeName = StringPool.BLANK;
+
+			if (layoutSetPrototype != null) {
+				layoutSetPrototypeName = layoutSetPrototype.getName(
+					themeDisplay.getLocale());
+			}
+
+			SessionMessages.add(
+				actionRequest, "executeLayoutSetPrototypeSyncInfoMessage",
+				LanguageUtil.format(
+					themeDisplay.getLocale(),
+					"the-sync-of-the-site-template-x-started-you-will-" +
+						"receive-a-notification-when-the-process-is-complete",
+					layoutSetPrototypeName));
+		}
+		catch (Exception exception) {
+			_log.error(
+				"Unable to start site template sync for " +
+					layoutSetPrototypeId,
+				exception);
+
+			hideDefaultErrorMessage(actionRequest);
+
+			SessionMessages.add(
+				actionRequest, "executeLayoutSetPrototypeSyncErrorMessage",
+				LanguageUtil.get(
+					themeDisplay.getLocale(),
+					"an-error-has-occurred-and-the-site-template-sync-could-" +
+						"not-be-performed"));
+		}
 	}
 
 	public void updateLayoutSetPrototype(
@@ -258,5 +301,8 @@ public class LayoutSetPrototypePortlet extends MVCPortlet {
 
 	@Reference
 	protected PanelAppRegistry panelAppRegistry;
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		LayoutSetPrototypePortlet.class);
 
 }

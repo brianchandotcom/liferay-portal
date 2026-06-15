3efeec1dadc3549ba7fb338598638702cbe6248d
Victor Galan <victor.galan@liferay.com>
2026-06-15 13:32:06 +0200

    LPD-94453 Add Element Variations editor entry point
    Introduces a Create Variations button in the experience selector that opens a new Element Variations editor route, backed by a render command, display context, and a placeholder React view.

---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/EditElementVariationsDisplayContext.java
@@ -0,0 +1,81 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.layout.content.page.editor.web.internal.display.context;
+
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.Map;
+
+/**
+ * @author Víctor Galán
+ */
+public class EditElementVariationsDisplayContext {
+
+	public EditElementVariationsDisplayContext(
+		HttpServletRequest httpServletRequest) {
+
+		_httpServletRequest = httpServletRequest;
+
+		_themeDisplay = (ThemeDisplay)httpServletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+	}
+
+	public Map<String, Object> getData() {
+		return HashMapBuilder.<String, Object>put(
+			"languageId", _themeDisplay.getLanguageId()
+		).put(
+			"plid", _getPlid()
+		).put(
+			"redirect", getRedirect()
+		).put(
+			"segmentsExperienceId", _getSegmentsExperienceId()
+		).build();
+	}
+
+	public String getRedirect() {
+		if (Validator.isNotNull(_redirect)) {
+			return _redirect;
+		}
+
+		_redirect = ParamUtil.getString(_httpServletRequest, "redirect");
+
+		return _redirect;
+	}
+
+	private long _getPlid() {
+		if (_plid != null) {
+			return _plid;
+		}
+
+		_plid = ParamUtil.getLong(_httpServletRequest, "plid");
+
+		return _plid;
+	}
+
+	private long _getSegmentsExperienceId() {
+		if (_segmentsExperienceId != null) {
+			return _segmentsExperienceId;
+		}
+
+		_segmentsExperienceId = ParamUtil.getLong(
+			_httpServletRequest, "segmentsExperienceId");
+
+		return _segmentsExperienceId;
+	}
+
+	private final HttpServletRequest _httpServletRequest;
+	private Long _plid;
+	private String _redirect;
+	private Long _segmentsExperienceId;
+	private final ThemeDisplay _themeDisplay;
+
+}

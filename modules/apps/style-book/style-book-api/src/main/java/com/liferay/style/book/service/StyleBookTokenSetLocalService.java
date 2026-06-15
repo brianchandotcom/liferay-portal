6de3fa8abf39239d87a233cbf38cfcda1cd87fde
Gabriel Lima <gabriel.lima@liferay.com>
2026-06-15 12:56:32 -0300

    LPD-88085 Auto-generated (style-book-service buildService)

--- modules/apps/style-book/style-book-api/src/main/java/com/liferay/style/book/service/StyleBookTokenSetLocalService.java
+++ modules/apps/style-book/style-book-api/src/main/java/com/liferay/style/book/service/StyleBookTokenSetLocalService.java
@@ -21,6 +21,7 @@ import com.liferay.portal.kernel.search.Indexable;
 import com.liferay.portal.kernel.search.IndexableType;
 import com.liferay.portal.kernel.service.BaseLocalService;
 import com.liferay.portal.kernel.service.PersistedModelLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.change.tracking.CTService;
 import com.liferay.portal.kernel.service.persistence.change.tracking.CTPersistence;
 import com.liferay.portal.kernel.transaction.Isolation;
@@ -60,6 +61,11 @@ public interface StyleBookTokenSetLocalService
 	 *
 	 * Never modify this interface directly. Add custom service methods to <code>com.liferay.style.book.service.impl.StyleBookTokenSetLocalServiceImpl</code> and rerun ServiceBuilder to automatically copy the method declarations to this interface. Consume the style book token set local service via injection or a <code>org.osgi.util.tracker.ServiceTracker</code>. Use {@link StyleBookTokenSetLocalServiceUtil} if injection and service tracking are not available.
 	 */
+	public StyleBookTokenSet addStyleBookTokenSet(
+			String externalReferenceCode, String description,
+			String frontendTokenCategoryName, String frontendTokenDefinitionId,
+			String name, long styleBookEntryId, ServiceContext serviceContext)
+		throws PortalException;
 
 	/**
 	 * Adds the style book token set to the database. Also notifies the appropriate model listeners.
@@ -348,4 +354,4 @@ public interface StyleBookTokenSetLocalService
 		throws E;
 
 }
-// LIFERAY-SERVICE-BUILDER-HASH:1181892511
+// LIFERAY-SERVICE-BUILDER-HASH:1575882806

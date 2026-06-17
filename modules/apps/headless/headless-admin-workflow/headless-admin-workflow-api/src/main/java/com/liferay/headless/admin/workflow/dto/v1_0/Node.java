0cb665c15734315d5806820b6c3f5fa652c9d51f
Feliphe Marinho <feliphemarinho18@gmail.com>
2026-06-17 20:01:36 -0300

    LPD-94528 generated: gw buildRest

--- modules/apps/headless/headless-admin-workflow/headless-admin-workflow-api/src/main/java/com/liferay/headless/admin/workflow/dto/v1_0/Node.java
+++ modules/apps/headless/headless-admin-workflow/headless-admin-workflow-api/src/main/java/com/liferay/headless/admin/workflow/dto/v1_0/Node.java
@@ -403,4 +403,4 @@ public class Node implements Serializable {
 	private Map<String, Serializable> _extendedProperties;
 
 }
-// LIFERAY-REST-BUILDER-HASH:-25574729
+// LIFERAY-REST-BUILDER-HASH:-948667520

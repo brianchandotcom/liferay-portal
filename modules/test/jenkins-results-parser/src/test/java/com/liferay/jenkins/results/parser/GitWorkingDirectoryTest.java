2148842daec240197e04cd83fbbb49206eee9f46
Kenji Heigel <kenji.heigel@liferay.com>
2026-06-16 17:05:03 -0700

    LRCI-7520 Add unit tests for the Shell seam
    Add Shell test helpers (mockShell, stubShellCommand) to the Test base class, plus tests proving the seam: the adapter replays a stubbed ExecutionResult through Process, and git code in GitUtil and GitWorkingDirectory runs with no git on the PATH.

---
+++ modules/test/jenkins-results-parser/src/test/java/com/liferay/jenkins/results/parser/GitWorkingDirectoryTest.java
@@ -0,0 +1,50 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.jenkins.results.parser;
+
+import java.io.File;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+/**
+ * @author Kenji Heigel
+ */
+public class GitWorkingDirectoryTest
+	extends com.liferay.jenkins.results.parser.Test {
+
+	@Test
+	public void testGetLocalGitBranchSHA() throws Exception {
+		File gitRepositoryDir = temporaryFolder.getRoot();
+
+		File gitDir = new File(gitRepositoryDir, ".git");
+
+		gitDir.mkdir();
+
+		Shell shell = mockShell();
+
+		stubShellCommand(
+			shell, "git remote -v",
+			"upstream\tgit@github.com:liferay/liferay-portal.git (fetch)\n" +
+				"upstream\tgit@github.com:liferay/liferay-portal.git (push)\n");
+
+		stubShellCommand(shell, "git rev-parse master", "abcdef1234567890");
+
+		GitWorkingDirectory gitWorkingDirectory =
+			GitWorkingDirectoryFactory.newGitWorkingDirectory(
+				"master", gitRepositoryDir, "liferay-portal");
+
+		Assert.assertEquals(
+			"abcdef1234567890",
+			gitWorkingDirectory.getLocalGitBranchSHA("master"));
+	}
+
+	@Rule
+	public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+}

03dcef3e0aa7fc679ae827259a9d1537e6abbf92
Iván Zaera Avellón <ivan.zaera@liferay.com>
2026-06-18 13:07:19 +0200

    LPD-94735 Wire up audiences definition, element variations and framework
    The dynamic include writes an infinitely cacheable URL to the page
    referenced by a <script> element.
    
    That URL contains all variables and hashes needed so that if its
    contents change, the URL changes. This way it is infinitely cacheable.
    
    The variables are:
    
      - The hash of the "bootstrap.js.tpl" file so that if we change it in the
        future the URL changes.
      - The audiences definition hash.
      - The element variations hash.
      - The plid so that we can send a redirect if any of the hashes is
        obsolete (to make the browser use the new definitions/variations).
      - The configuration value for the enableLog flag since it alters the
        generated JavaScript.

---
+++ modules/apps/frontend-js/frontend-js-audiences-web/src/main/java/com/liferay/frontend/js/audiences/web/internal/servlet/FrontendJSAudiencesWebServlet.java
@@ -0,0 +1,223 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.frontend.js.audiences.web.internal.servlet;
+
+import com.liferay.frontend.js.audiences.AudiencesDefinitionProvider;
+import com.liferay.frontend.js.audiences.ElementVariationsProvider;
+import com.liferay.frontend.js.audiences.web.internal.util.BootstrapJavaScriptUtil;
+import com.liferay.petra.string.CharPool;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.frontend.hashed.files.HashedFilesUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.servlet.HttpHeaders;
+import com.liferay.portal.kernel.util.KeyValuePair;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.StringUtil;
+
+import jakarta.servlet.Servlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import java.util.Objects;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Iván Zaera Avellón
+ */
+@Component(
+	property = {
+		"osgi.http.whiteboard.servlet.name=com.liferay.frontend.js.audiences.web.internal.servlet.FrontendJSAudiencesWebServlet",
+		"osgi.http.whiteboard.servlet.pattern=/audiences/*"
+	},
+	service = Servlet.class
+)
+public class FrontendJSAudiencesWebServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws IOException {
+
+		String[] parts = StringUtil.split(
+			httpServletRequest.getPathInfo(), CharPool.SLASH);
+
+		if (parts.length == 0) {
+			httpServletResponse.setStatus(HttpServletResponse.SC_NOT_FOUND);
+		}
+		else if (parts[parts.length - 1].startsWith("bootstrap.")) {
+			_sendBootstrapJavaScript(
+				httpServletRequest, httpServletResponse, parts);
+		}
+		else if (parts[parts.length - 1].startsWith("definition.")) {
+			_sendAudiencesDefinitionJSON(
+				httpServletRequest, httpServletResponse, parts);
+		}
+		else if (parts[parts.length - 1].startsWith("variations.")) {
+			_sendElementVariationsJavaScript(
+				httpServletRequest, httpServletResponse, parts);
+		}
+		else {
+			httpServletResponse.setStatus(HttpServletResponse.SC_NOT_FOUND);
+		}
+	}
+
+	private Long _parsePlid(String plid) {
+		try {
+			return Long.valueOf(plid);
+		}
+		catch (NumberFormatException numberFormatException) {
+			_log.error(
+				"Unable to parse plid from " + plid, numberFormatException);
+
+			return null;
+		}
+	}
+
+	private void _sendAudiencesDefinitionJSON(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse, String[] parts)
+		throws IOException {
+
+		KeyValuePair keyValuePair =
+			_audiencesDefinitionProvider.getAudiencesDefinition(
+				_portal.getCompanyId(httpServletRequest));
+
+		if (keyValuePair == null) {
+			httpServletResponse.setStatus(HttpServletResponse.SC_NOT_FOUND);
+
+			return;
+		}
+
+		String requestHash = HashedFilesUtil.getHash(parts[parts.length - 1]);
+
+		if (!Objects.equals(keyValuePair.getKey(), requestHash)) {
+			httpServletResponse.sendRedirect(
+				StringBundler.concat(
+					HashedFilesUtil.replaceHash(
+						httpServletRequest.getRequestURI(),
+						keyValuePair.getKey()),
+					StringPool.QUESTION, httpServletRequest.getQueryString()));
+
+			return;
+		}
+
+		httpServletResponse.setContentType("application/json");
+		httpServletResponse.setHeader(
+			HttpHeaders.CACHE_CONTROL, "immutable, max-age=31536000, public");
+
+		PrintWriter printWriter = httpServletResponse.getWriter();
+
+		printWriter.print(keyValuePair.getValue());
+	}
+
+	private void _sendBootstrapJavaScript(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse, String[] parts)
+		throws IOException {
+
+		String hash = BootstrapJavaScriptUtil.getHash();
+
+		String requestHash = HashedFilesUtil.getHash(parts[parts.length - 1]);
+
+		if (!Objects.equals(hash, requestHash)) {
+			httpServletResponse.sendRedirect(
+				StringBundler.concat(
+					HashedFilesUtil.replaceHash(
+						httpServletRequest.getRequestURI(), hash),
+					StringPool.QUESTION, httpServletRequest.getQueryString()));
+
+			return;
+		}
+
+		httpServletResponse.setContentType("application/javascript");
+		httpServletResponse.setHeader(
+			HttpHeaders.CACHE_CONTROL, "immutable, max-age=31536000, public");
+
+		PrintWriter printWriter = httpServletResponse.getWriter();
+
+		Long plid = _parsePlid(httpServletRequest.getParameter("plid"));
+
+		if (plid == null) {
+			httpServletResponse.setStatus(HttpServletResponse.SC_NOT_FOUND);
+
+			return;
+		}
+
+		printWriter.print(
+			BootstrapJavaScriptUtil.getJavaScript(
+				httpServletRequest.getParameter("audiencesDefinitionHash"),
+				httpServletRequest.getParameter("elementVariationsHash"),
+				Boolean.parseBoolean(
+					httpServletRequest.getParameter("enableLog")),
+				plid));
+	}
+
+	private void _sendElementVariationsJavaScript(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse, String[] parts)
+		throws IOException {
+
+		Long plid = _parsePlid(parts[1]);
+
+		if (plid == null) {
+			httpServletResponse.setStatus(HttpServletResponse.SC_NOT_FOUND);
+
+			return;
+		}
+
+		KeyValuePair keyValuePair =
+			_elementVariationsProvider.getElementVariations(plid);
+
+		if (keyValuePair == null) {
+			httpServletResponse.setStatus(HttpServletResponse.SC_NOT_FOUND);
+
+			return;
+		}
+
+		String requestHash = HashedFilesUtil.getHash(parts[parts.length - 1]);
+
+		if (!Objects.equals(keyValuePair.getKey(), requestHash)) {
+			httpServletResponse.sendRedirect(
+				StringBundler.concat(
+					HashedFilesUtil.replaceHash(
+						httpServletRequest.getRequestURI(),
+						keyValuePair.getKey()),
+					StringPool.QUESTION, httpServletRequest.getQueryString()));
+
+			return;
+		}
+
+		httpServletResponse.setContentType("application/javascript");
+		httpServletResponse.setHeader(
+			HttpHeaders.CACHE_CONTROL, "immutable, max-age=31536000, public");
+
+		PrintWriter printWriter = httpServletResponse.getWriter();
+
+		printWriter.print(keyValuePair.getValue());
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		FrontendJSAudiencesWebServlet.class);
+
+	@Reference
+	private AudiencesDefinitionProvider _audiencesDefinitionProvider;
+
+	@Reference
+	private ElementVariationsProvider _elementVariationsProvider;
+
+	@Reference
+	private Portal _portal;
+
+}

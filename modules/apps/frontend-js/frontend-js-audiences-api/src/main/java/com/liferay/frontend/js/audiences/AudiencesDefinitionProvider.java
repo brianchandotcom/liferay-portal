11c85708a19140d0ddb0d0479d9122f361b8067e
Iván Zaera Avellón <ivan.zaera@liferay.com>
2026-06-18 13:00:57 +0200

    LPD-94735 Add frontend-js-audiences-api bundle
    We need to declare two OSGi service interfaces to wire the audiences
    definition and the element variations editors to
    frontend-js-audiences-web.
    
    This is to be able to put all HTTP serving and caching logic in
    frontend-js-audiences-web while the business logic to compose the JSON
    and JavaScript files remains in the proper bundles (the editors).

---
+++ modules/apps/frontend-js/frontend-js-audiences-api/src/main/java/com/liferay/frontend/js/audiences/AudiencesDefinitionProvider.java
@@ -0,0 +1,17 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.frontend.js.audiences;
+
+import com.liferay.portal.kernel.util.KeyValuePair;
+
+/**
+ * @author Iván Zaera Avellón
+ */
+public interface AudiencesDefinitionProvider {
+
+	public KeyValuePair getAudiencesDefinition(long companyId);
+
+}

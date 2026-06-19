f943815396a81a9ec2452e423d1caddd7db594ca
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2026-06-19 09:21:02 +0100

    LPD-95409 Rename to Audiences to match with frontend-js-audiences

---
+++ modules/apps/audiences/audiences-service/src/main/resources/META-INF/sql/indexes.sql
@@ -0,0 +1,2 @@
+create unique index IX_C1DF1009 on AudiencesEntry (companyId, externalReferenceCode[$COLUMN_LENGTH:75$]);
+create index IX_D278B73 on AudiencesEntry (companyId, name[$COLUMN_LENGTH:75$]);

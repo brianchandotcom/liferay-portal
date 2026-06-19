b2dd2f3f6772c71f8b5c6e1106cb8206d1e6ee2f
Jose Luis Navarro <joseluis.navarro@liferay.com>
2026-06-19 10:51:08 +0200

    LPD-91092 Add the data masking engine, interceptor, and listener

---
+++ modules/apps/headless/headless-data-masking/headless-data-masking-impl/src/main/java/com/liferay/headless/data/masking/internal/engine/DataMask.java
@@ -0,0 +1,72 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.headless.data.masking.internal.engine;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Jose Luis Navarro
+ */
+public class DataMask {
+
+	public DataMask(
+		Pattern detectionPattern, String name, Pattern replacementPattern,
+		String replacementValue) {
+
+		_detectionPattern = detectionPattern;
+		_name = name;
+		_replacementPattern = replacementPattern;
+		_replacementValue = replacementValue;
+	}
+
+	public String apply(String text) {
+		Matcher matcher = _detectionPattern.matcher(text);
+
+		StringBuffer sb = new StringBuffer();
+
+		boolean found = false;
+
+		while (matcher.find()) {
+			found = true;
+
+			if (_replacementPattern == null) {
+				matcher.appendReplacement(
+					sb, Matcher.quoteReplacement(_replacementValue));
+			}
+			else {
+				String matchedText = matcher.group();
+
+				Matcher replacementMatcher = _replacementPattern.matcher(
+					matchedText);
+
+				String replaced = replacementMatcher.replaceAll(
+					_replacementValue);
+
+				matcher.appendReplacement(
+					sb, Matcher.quoteReplacement(replaced));
+			}
+		}
+
+		if (!found) {
+			return text;
+		}
+
+		matcher.appendTail(sb);
+
+		return sb.toString();
+	}
+
+	public String getName() {
+		return _name;
+	}
+
+	private final Pattern _detectionPattern;
+	private final String _name;
+	private final Pattern _replacementPattern;
+	private final String _replacementValue;
+
+}

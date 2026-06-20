60267dc07bbf26fbbb7012ddb29a4f5b2e2ebe3c
Victor Galan <victor.galan@liferay.com>
2026-06-20 21:23:41 +0200

    LPD-94453 Add the element variation create and edit form

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/element_variations/ElementVariations.tsx
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/element_variations/ElementVariations.tsx
@@ -7,7 +7,10 @@ import ClayButton from '@clayui/button';
 import {Option, Picker} from '@clayui/core';
 import ClayEmptyState from '@clayui/empty-state';
 import {useId} from 'frontend-js-components-web';
-import React, {useState} from 'react';
+import React, {useReducer, useState} from 'react';
+
+import ElementVariationForm from './ElementVariationForm';
+import {createElementVariation, reducer} from './elementVariationsReducer';
 
 import './ElementVariations.scss';
 
@@ -33,68 +36,135 @@ const ExperienceTrigger = React.forwardRef<HTMLButtonElement, any>(
 	)
 );
 
-export default function ElementVariations({experiences}: Props) {
+export default function ElementVariations({audiences, experiences}: Props) {
 	const experienceId = useId();
+
 	const [experienceKey, setExperienceKey] = useState(
 		experiences[0]?.value ?? ''
+	);
+
+	const [{draftElementVariation, elementVariations}, dispatch] = useReducer(
+		reducer,
+		{draftElementVariation: null, elementVariations: []}
+	);
+
+	const experienceElementVariations = elementVariations.filter(
+		(elementVariation) =>
+			elementVariation.segmentsExperienceERC === experienceKey
 	);
 
 	return (
 		<div className="d-flex element-variations flex-column">
 			<div className="d-flex element-variations__content flex-grow-1">
 				<div className="bg-white border-right d-flex element-variations__sidebar flex-column flex-shrink-0">
-					<div className="border-bottom flex-shrink-0 px-3 py-3">
-						<span className="font-weight-bold">
-							{Liferay.Language.get('element-variations')}
-						</span>
-					</div>
+					{draftElementVariation ? (
+						<ElementVariationForm
+							audiences={audiences}
+							elementVariation={draftElementVariation}
+							key={draftElementVariation.key}
+							onCancel={() =>
+								dispatch({
+									type: 'CANCEL_ELEMENT_VARIATION_DRAFT',
+								})
+							}
+							onChange={(properties) =>
+								dispatch({
+									properties,
+									type: 'UPDATE_ELEMENT_VARIATION_DRAFT',
+								})
+							}
+							onSave={() =>
+								dispatch({type: 'SAVE_ELEMENT_VARIATION_DRAFT'})
+							}
+						/>
+					) : (
+						<>
+							<div className="border-bottom flex-shrink-0 px-3 py-3">
+								<span className="font-weight-bold">
+									{Liferay.Language.get('element-variations')}
+								</span>
+							</div>
 
-					<div className="flex-grow-1">
-						<div className="form-group p-3">
-							<label htmlFor={experienceId}>
-								{Liferay.Language.get('experience')}
-							</label>
+							<div className="flex-grow-1">
+								<div className="form-group p-3">
+									<label htmlFor={experienceId}>
+										{Liferay.Language.get('experience')}
+									</label>
 
-							<Picker
-								aria-label={Liferay.Language.get('experience')}
-								as={ExperienceTrigger}
-								id={experienceId}
-								items={experiences}
-								onSelectionChange={(selection) =>
-									setExperienceKey(String(selection))
-								}
-								selectedKey={experienceKey}
-							>
-								{(item) => (
-									<Option key={item.value}>
-										{item.label}
-									</Option>
+									<Picker
+										aria-label={Liferay.Language.get(
+											'experience'
+										)}
+										as={ExperienceTrigger}
+										id={experienceId}
+										items={experiences}
+										onSelectionChange={(selection) =>
+											setExperienceKey(String(selection))
+										}
+										selectedKey={experienceKey}
+									>
+										{(item) => (
+											<Option key={item.value}>
+												{item.label}
+											</Option>
+										)}
+									</Picker>
+								</div>
+
+								{experienceElementVariations.length ? (
+									<div className="list-group">
+										{experienceElementVariations.map(
+											(elementVariation) => (
+												<button
+													className="list-group-item list-group-item-action text-left"
+													key={elementVariation.key}
+													onClick={() =>
+														dispatch({
+															key: elementVariation.key,
+															type: 'EDIT_ELEMENT_VARIATION',
+														})
+													}
+													type="button"
+												>
+													{elementVariation.name}
+												</button>
+											)
+										)}
+									</div>
+								) : (
+									<ClayEmptyState
+										className="mb-0 px-3"
+										description={Liferay.Language.get(
+											'you-can-create-page-elements-variations-based-on-audiences'
+										)}
+										imgSrc={`${Liferay.ThemeDisplay.getPathThemeImages()}/states/empty_state.svg`}
+										small
+										title={Liferay.Language.get(
+											'no-variations-yet'
+										)}
+									/>
 								)}
-							</Picker>
-						</div>
 
-						<div className="align-items-center d-flex flex-column justify-content-between px-3">
-							<ClayEmptyState
-								className="mb-0"
-								description={Liferay.Language.get(
-									'you-can-create-page-elements-variations-based-on-audiences'
-								)}
-								imgSrc={`${Liferay.ThemeDisplay.getPathThemeImages()}/states/empty_state.svg`}
-								small
-								title={Liferay.Language.get(
-									'no-variations-yet'
-								)}
-							/>
-
-							<ClayButton
-								className="mt-2"
-								displayType="secondary"
-								size="sm"
-							>
-								{Liferay.Language.get('new-variation')}
-							</ClayButton>
-						</div>
-					</div>
+								<div className="d-flex justify-content-center mt-2">
+									<ClayButton
+										displayType="secondary"
+										onClick={() =>
+											dispatch({
+												draftElementVariation:
+													createElementVariation(
+														experienceKey
+													),
+												type: 'CREATE_ELEMENT_VARIATION_DRAFT',
+											})
+										}
+										size="sm"
+									>
+										{Liferay.Language.get('new-variation')}
+									</ClayButton>
+								</div>
+							</div>
+						</>
+					)}
 				</div>
 
 				<iframe

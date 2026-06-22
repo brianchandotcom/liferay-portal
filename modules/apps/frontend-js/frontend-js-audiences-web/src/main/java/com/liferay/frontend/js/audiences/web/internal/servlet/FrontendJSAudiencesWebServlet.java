9af5486f0993b2550ff51013af9f962e1820e4c5
Iván Zaera Avellón <ivan.zaera@liferay.com>
2026-06-22 10:06:43 +0200

    LPD-94735 Rename
    Even though we have parseXXX() methods in liferay-portal codebase,
    according to Claude:
    
    │ A survey of ~18 Liferay HttpServlet subclasses shows request-value extraction helpers                                                                                                                        │
    │ are named _get* (e.g. _getFileEntryId(String uri), _getGroupId(...)) ~95%+ of the                                                                                                                            │
    │ time; _parse* appears essentially never. The four _send* methods already match the                                                                                                                           │
    │ dominant convention and stay as-is.                                                                                                                                                                          │

--- modules/apps/frontend-js/frontend-js-audiences-web/src/main/java/com/liferay/frontend/js/audiences/web/internal/servlet/FrontendJSAudiencesWebServlet.java
+++ modules/apps/frontend-js/frontend-js-audiences-web/src/main/java/com/liferay/frontend/js/audiences/web/internal/servlet/FrontendJSAudiencesWebServlet.java
@@ -75,13 +75,13 @@ public class FrontendJSAudiencesWebServlet extends HttpServlet {
 		}
 	}
 
-	private Long _parsePlid(String plid) {
+	private Long _getPlid(String plid) {
 		try {
 			return Long.valueOf(plid);
 		}
 		catch (NumberFormatException numberFormatException) {
 			_log.error(
-				"Unable to parse plid from " + plid, numberFormatException);
+				"Unable to get plid from " + plid, numberFormatException);
 
 			return null;
 		}
@@ -148,7 +148,7 @@ public class FrontendJSAudiencesWebServlet extends HttpServlet {
 			return;
 		}
 
-		Long plid = _parsePlid(httpServletRequest.getParameter("plid"));
+		Long plid = _getPlid(httpServletRequest.getParameter("plid"));
 
 		if (plid == null) {
 			httpServletResponse.setStatus(HttpServletResponse.SC_NOT_FOUND);
@@ -182,7 +182,7 @@ public class FrontendJSAudiencesWebServlet extends HttpServlet {
 			return;
 		}
 
-		Long plid = _parsePlid(parts[1]);
+		Long plid = _getPlid(parts[1]);
 
 		if (plid == null) {
 			httpServletResponse.setStatus(HttpServletResponse.SC_NOT_FOUND);

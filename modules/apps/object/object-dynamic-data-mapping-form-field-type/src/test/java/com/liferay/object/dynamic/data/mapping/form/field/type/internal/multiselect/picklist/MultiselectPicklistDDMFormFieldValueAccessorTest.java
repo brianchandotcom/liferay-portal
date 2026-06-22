a0d25abb3e49bda81ead0c5484800e7d472ce643
Alan Huang <alan.huang@liferay.com>
2026-06-22 12:39:03 +0800

    LPD-95485 Move package

--- modules/apps/object/object-dynamic-data-mapping-form-field-type/src/test/java/com/liferay/object/dynamic/data/mapping/form/field/type/internal/multiselect/picklist/MultiselectPicklistDDMFormFieldValueAccessorTest.java
+++ modules/apps/object/object-dynamic-data-mapping-form-field-type/src/test/java/com/liferay/object/dynamic/data/mapping/form/field/type/internal/multiselect/picklist/MultiselectPicklistDDMFormFieldValueAccessorTest.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.object.dynamic.data.mapping.form.field.type.internal.multi.select.picklist;
+package com.liferay.object.dynamic.data.mapping.form.field.type.internal.multiselect.picklist;
 
 import com.liferay.dynamic.data.mapping.model.UnlocalizedValue;
 import com.liferay.dynamic.data.mapping.test.util.DDMFormValuesTestUtil;

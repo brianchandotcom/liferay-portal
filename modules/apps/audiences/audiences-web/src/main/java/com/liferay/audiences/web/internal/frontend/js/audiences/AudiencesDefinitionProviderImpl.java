cd8beddf314036f75837653f8369c6fd0191f567
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2026-06-22 16:16:34 +0100

    LPD-95807 Implements AudiencesDefinitionProvider for audiences

--- modules/apps/audiences/audiences-web/src/main/java/com/liferay/audiences/web/internal/frontend/js/audiences/AudiencesDefinitionProviderImpl.java
+++ modules/apps/audiences/audiences-web/src/main/java/com/liferay/audiences/web/internal/frontend/js/audiences/AudiencesDefinitionProviderImpl.java
@@ -5,10 +5,21 @@
 
 package com.liferay.audiences.web.internal.frontend.js.audiences;
 
+import com.liferay.audiences.model.AudiencesEntry;
+import com.liferay.audiences.service.AudiencesEntryLocalService;
 import com.liferay.frontend.js.audiences.AudiencesDefinition;
 import com.liferay.frontend.js.audiences.AudiencesDefinitionProvider;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
+import com.liferay.portal.kernel.frontend.hashed.files.HashedFilesUtil;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONUtil;
+
+import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Eudaldo Alonso
@@ -19,7 +30,31 @@ public class AudiencesDefinitionProviderImpl
 
 	@Override
 	public AudiencesDefinition getAudiencesDefinition(long companyId) {
-		return null;
+		if (!FeatureFlagManagerUtil.isEnabled(companyId, "LPD-93951")) {
+			return null;
+		}
+
+		JSONArray audiencesJSONArray = _jsonFactory.createJSONArray();
+
+		List<AudiencesEntry> audiencesEntries =
+			_audiencesEntryLocalService.getAudiencesEntries(
+				companyId, QueryUtil.ALL_POS, QueryUtil.ALL_POS, null);
+
+		for (AudiencesEntry audiencesEntry : audiencesEntries) {
+			audiencesJSONArray.put(audiencesEntry);
+		}
+
+		String json = JSONUtil.put(
+			"audiences", audiencesJSONArray
+		).toString();
+
+		return new AudiencesDefinition(HashedFilesUtil.computeHash(json), json);
 	}
+
+	@Reference
+	private AudiencesEntryLocalService _audiencesEntryLocalService;
+
+	@Reference
+	private JSONFactory _jsonFactory;
 
 }

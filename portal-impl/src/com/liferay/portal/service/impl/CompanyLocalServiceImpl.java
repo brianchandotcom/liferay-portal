e7a63a84cb0e10e645dac25e228033e985e56823
IstvanD <istvan.dezsi@liferay.com>
2026-06-23 14:16:54 +0200

    LPD-82361 Move secondary owned Company columns to CompanyInfo

--- portal-impl/src/com/liferay/portal/service/impl/CompanyLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/CompanyLocalServiceImpl.java
@@ -408,6 +408,9 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 
 							company.setName(name);
 
+							_companyInfoPersistence.update(
+								company.getCompanyInfo());
+
 							company = companyPersistence.update(company);
 						}
 
@@ -645,6 +648,8 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 						company.setWebId(webId);
 						company.setName(name);
 						company.setNew(true);
+
+						_companyInfoPersistence.update(company.getCompanyInfo());
 
 						company = companyPersistence.update(company);
 
@@ -1252,6 +1257,8 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 		company.setType(type);
 		company.setSize(size);
 
+		_companyInfoPersistence.update(company.getCompanyInfo());
+
 		companyPersistence.update(company);
 
 		// Virtual host
@@ -1349,6 +1356,8 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 
 		company.setIndexNameNext(indexNameNext);
 
+		_companyInfoPersistence.update(company.getCompanyInfo());
+
 		return companyPersistence.update(company);
 	}
 
@@ -1361,6 +1370,8 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 
 		company.setIndexNameCurrent(indexNameCurrent);
 		company.setIndexNameNext(indexNameNext);
+
+		_companyInfoPersistence.update(company.getCompanyInfo());
 
 		return companyPersistence.update(company);
 	}
@@ -1581,6 +1592,8 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 			logoId = counterLocalService.increment();
 
 			company.setLogoId(logoId);
+
+			_companyInfoPersistence.update(company.getCompanyInfo());
 
 			company = companyPersistence.update(company);
 		}

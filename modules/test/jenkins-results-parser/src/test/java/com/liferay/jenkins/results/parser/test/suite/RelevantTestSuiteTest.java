c4bd8b182ae8c915bdbe07e316d4a8bb7db3ae0c
Brian Chan <brian.chan@liferay.com>
2026-06-26 08:49:01 +0800

    LRCI-7685 Sort/As used

--- modules/test/jenkins-results-parser/src/test/java/com/liferay/jenkins/results/parser/test/suite/RelevantTestSuiteTest.java
+++ modules/test/jenkins-results-parser/src/test/java/com/liferay/jenkins/results/parser/test/suite/RelevantTestSuiteTest.java
@@ -163,18 +163,18 @@ public class RelevantTestSuiteTest extends BaseRelevantRuleTestCase {
 
 	@Test
 	public void testStableRuleBatchBypassesWhitelist() {
-		RelevantTestSuite relevantTestSuite = new RelevantTestSuite(
-			getPortalAcceptancePullRequestJob());
-
-		relevantTestSuite.setModifiedFiles(
-			Arrays.asList(new File(getBaseDir(), "stable/trigger.txt")));
-
 		RelevantRuleEngine relevantRuleEngine =
 			RelevantRuleEngine.getInstance();
 
 		relevantRuleEngine.setBaseDir(getBaseDir());
 
 		Set<String> batchNames = new HashSet<>();
+
+		RelevantTestSuite relevantTestSuite = new RelevantTestSuite(
+			getPortalAcceptancePullRequestJob());
+
+		relevantTestSuite.setModifiedFiles(
+			Arrays.asList(new File(getBaseDir(), "stable/trigger.txt")));
 
 		for (TestBatch testBatch : relevantTestSuite.getTestBatches(false)) {
 			batchNames.add(testBatch.getName());

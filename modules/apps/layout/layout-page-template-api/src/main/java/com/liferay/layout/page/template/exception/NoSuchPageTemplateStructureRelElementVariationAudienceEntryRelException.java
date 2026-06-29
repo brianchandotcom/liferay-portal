437b4ff69f153b862ff6dbfb090af393f3f64b87
Victor Galan <victor.galan@liferay.com>
2026-06-30 01:57:40 +0200

    LPD-96412 SF

--- modules/apps/layout/layout-page-template-api/src/main/java/com/liferay/layout/page/template/exception/NoSuchPageTemplateStructureRelElementVariationAudienceEntryRelException.java
+++ modules/apps/layout/layout-page-template-api/src/main/java/com/liferay/layout/page/template/exception/NoSuchPageTemplateStructureRelElementVariationAudienceEntryRelException.java
@@ -2,6 +2,7 @@
  * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
+
 package com.liferay.layout.page.template.exception;
 
 import com.liferay.portal.kernel.exception.NoSuchModelException;
@@ -9,20 +10,28 @@ import com.liferay.portal.kernel.exception.NoSuchModelException;
 /**
  * @author Brian Wing Shun Chan
  */
-public class NoSuchPageTemplateStructureRelElementVariationAudienceEntryRelException extends NoSuchModelException {
+public class
+	NoSuchPageTemplateStructureRelElementVariationAudienceEntryRelException
+		extends NoSuchModelException {
 
 	public NoSuchPageTemplateStructureRelElementVariationAudienceEntryRelException() {
 	}
 
-	public NoSuchPageTemplateStructureRelElementVariationAudienceEntryRelException(String msg) {
+	public NoSuchPageTemplateStructureRelElementVariationAudienceEntryRelException(
+		String msg) {
+
 		super(msg);
 	}
 
-	public NoSuchPageTemplateStructureRelElementVariationAudienceEntryRelException(String msg, Throwable throwable) {
+	public NoSuchPageTemplateStructureRelElementVariationAudienceEntryRelException(
+		String msg, Throwable throwable) {
+
 		super(msg, throwable);
 	}
 
-	public NoSuchPageTemplateStructureRelElementVariationAudienceEntryRelException(Throwable throwable) {
+	public NoSuchPageTemplateStructureRelElementVariationAudienceEntryRelException(
+		Throwable throwable) {
+
 		super(throwable);
 	}
 

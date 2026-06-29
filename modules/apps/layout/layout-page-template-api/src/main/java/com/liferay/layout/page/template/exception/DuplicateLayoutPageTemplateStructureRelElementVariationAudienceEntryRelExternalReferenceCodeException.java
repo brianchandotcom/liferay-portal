92a875c68a960029cd5d510a225eb73dab16b4d3
Victor Galan <victor.galan@liferay.com>
2026-06-30 01:57:40 +0200

    LPD-96412 SF

--- modules/apps/layout/layout-page-template-api/src/main/java/com/liferay/layout/page/template/exception/DuplicateLayoutPageTemplateStructureRelElementVariationAudienceEntryRelExternalReferenceCodeException.java
+++ modules/apps/layout/layout-page-template-api/src/main/java/com/liferay/layout/page/template/exception/DuplicateLayoutPageTemplateStructureRelElementVariationAudienceEntryRelExternalReferenceCodeException.java
@@ -2,6 +2,7 @@
  * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
+
 package com.liferay.layout.page.template.exception;
 
 import com.liferay.portal.kernel.exception.DuplicateExternalReferenceCodeException;
@@ -9,20 +10,28 @@ import com.liferay.portal.kernel.exception.DuplicateExternalReferenceCodeExcepti
 /**
  * @author Brian Wing Shun Chan
  */
-public class DuplicateLayoutPageTemplateStructureRelElementVariationAudienceEntryRelExternalReferenceCodeException extends DuplicateExternalReferenceCodeException {
+public class
+	DuplicateLayoutPageTemplateStructureRelElementVariationAudienceEntryRelExternalReferenceCodeException
+		extends DuplicateExternalReferenceCodeException {
 
 	public DuplicateLayoutPageTemplateStructureRelElementVariationAudienceEntryRelExternalReferenceCodeException() {
 	}
 
-	public DuplicateLayoutPageTemplateStructureRelElementVariationAudienceEntryRelExternalReferenceCodeException(String msg) {
+	public DuplicateLayoutPageTemplateStructureRelElementVariationAudienceEntryRelExternalReferenceCodeException(
+		String msg) {
+
 		super(msg);
 	}
 
-	public DuplicateLayoutPageTemplateStructureRelElementVariationAudienceEntryRelExternalReferenceCodeException(String msg, Throwable throwable) {
+	public DuplicateLayoutPageTemplateStructureRelElementVariationAudienceEntryRelExternalReferenceCodeException(
+		String msg, Throwable throwable) {
+
 		super(msg, throwable);
 	}
 
-	public DuplicateLayoutPageTemplateStructureRelElementVariationAudienceEntryRelExternalReferenceCodeException(Throwable throwable) {
+	public DuplicateLayoutPageTemplateStructureRelElementVariationAudienceEntryRelExternalReferenceCodeException(
+		Throwable throwable) {
+
 		super(throwable);
 	}
 

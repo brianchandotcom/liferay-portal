51214d8185ab3af1ceb0b90c9e8f5f929d7c5350
Javier Moral <javier.moral@liferay.com>
2026-07-13 01:51:35 +0200

    LPD-95007 Replace bare UnsupportedOperationException in the shared page utilities

---
+++ modules/apps/segments/segments-api/src/main/java/com/liferay/segments/exception/DuplicateSegmentsExperienceKeyException.java
@@ -0,0 +1,29 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.segments.exception;
+
+import com.liferay.portal.kernel.exception.PortalException;
+
+/**
+ * @author Javier Moral
+ */
+public class DuplicateSegmentsExperienceKeyException extends PortalException {
+
+	public DuplicateSegmentsExperienceKeyException(
+		String segmentsExperienceKey) {
+
+		super("Duplicate segments experience key " + segmentsExperienceKey);
+
+		_segmentsExperienceKey = segmentsExperienceKey;
+	}
+
+	public String getSegmentsExperienceKey() {
+		return _segmentsExperienceKey;
+	}
+
+	private final String _segmentsExperienceKey;
+
+}

44111c463c32d8313b7a759ea4706f30968e7d6a
brookedalton <brooke.dalton@liferay.com>
2026-07-21 10:08:16 -0700

    LPD-91213 Update messages to match JSON object labels

--- workspaces/liferay-seostudio-workspace/client-extensions/liferay-seostudio-etc-crawler/src/main/java/com/liferay/seo/studio/service/DetectorService.java
+++ workspaces/liferay-seostudio-workspace/client-extensions/liferay-seostudio-etc-crawler/src/main/java/com/liferay/seo/studio/service/DetectorService.java
@@ -38,7 +38,7 @@ public class DetectorService {
 
 		if (domain == null) {
 			return new DetectorResult(
-				"Unable to get a domain for SEO Studio domain ID " +
+				"Unable to get a domain for SEO Studio Domain ID " +
 					seoStudioDomainId,
 				SEOStudioScanConstants.STATE_FAILED);
 		}
@@ -48,7 +48,7 @@ public class DetectorService {
 
 		if (ListUtil.isEmpty(crawlHits)) {
 			return new DetectorResult(
-				"Unable to get crawl hits for SEO Studio domain ID " +
+				"Unable to get crawl hits for SEO Studio Domain ID " +
 					seoStudioDomainId,
 				SEOStudioScanConstants.STATE_FAILED);
 		}

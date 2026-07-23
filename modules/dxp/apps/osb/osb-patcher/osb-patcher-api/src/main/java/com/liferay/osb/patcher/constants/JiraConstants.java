eb6a5c085b996c26253409c20dda8e475372c462
Michael Prigge <michael.prigge@liferay.com>
2026-07-23 13:47:19 -0600

    LPD-94656 Expand LPE tickets to their related LPD tickets via Jira

---
+++ modules/dxp/apps/osb/osb-patcher/osb-patcher-api/src/main/java/com/liferay/osb/patcher/constants/JiraConstants.java
@@ -0,0 +1,33 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.osb.patcher.constants;
+
+/**
+ * @author Pedro Malta
+ */
+public class JiraConstants {
+
+	public static final String FIELD_FIELDS = "fields";
+
+	public static final String FIELD_INWARD_ISSUE = "inwardIssue";
+
+	public static final String FIELD_ISSUE_LINKS = "issuelinks";
+
+	public static final String FIELD_KEY = "key";
+
+	public static final String FIELD_NAME = "name";
+
+	public static final String FIELD_OUTWARD_ISSUE = "outwardIssue";
+
+	public static final String FIELD_TYPE = "type";
+
+	public static final String LINK_TYPE_RELATIONSHIP = "Relationship";
+
+	public static final String LPD_KEY_PREFIX = "LPD-";
+
+	public static final String LPE_KEY_PREFIX = "LPE-";
+
+}

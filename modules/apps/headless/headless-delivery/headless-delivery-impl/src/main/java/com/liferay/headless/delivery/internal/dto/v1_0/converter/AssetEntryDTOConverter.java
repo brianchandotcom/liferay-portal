be7112c9253bf57c1541574fbd8fe641864ecfeb
Gabriel Lima <gabriel.lima@liferay.com>
2026-07-24 11:31:55 -0300

    LPD-99377 Compute viewableByGuest from the guest role's view permission

--- modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/dto/v1_0/converter/AssetEntryDTOConverter.java
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/dto/v1_0/converter/AssetEntryDTOConverter.java
@@ -13,11 +13,18 @@ import com.liferay.asset.kernel.model.ClassTypeReader;
 import com.liferay.headless.delivery.dto.v1_0.AssetEntry;
 import com.liferay.headless.delivery.dto.v1_0.util.CreatorUtil;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.ResourceConstants;
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.model.role.RoleConstants;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
+import com.liferay.portal.kernel.service.RoleLocalService;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.vulcan.dto.converter.DTOConverter;
 import com.liferay.portal.vulcan.dto.converter.DTOConverterContext;
+import com.liferay.portal.vulcan.fields.NestedFieldsSupplier;
 
 import java.util.Locale;
 
@@ -106,8 +113,27 @@ public class AssetEntryDTOConverter
 						return assetRenderer.getStatus();
 					});
 				setTitle(() -> serviceBuilderAssetEntry.getTitle(locale));
+				setViewableByGuest(
+					() -> NestedFieldsSupplier.supply(
+						"viewableByGuest",
+						nestedField -> _hasGuestViewPermission(
+							serviceBuilderAssetEntry)));
 			}
 		};
+	}
+
+	private boolean _hasGuestViewPermission(
+			com.liferay.asset.kernel.model.AssetEntry assetEntry)
+		throws Exception {
+
+		Role role = _roleLocalService.getRole(
+			assetEntry.getCompanyId(), RoleConstants.GUEST);
+
+		return _resourcePermissionLocalService.hasResourcePermission(
+			assetEntry.getCompanyId(), assetEntry.getClassName(),
+			ResourceConstants.SCOPE_INDIVIDUAL,
+			String.valueOf(assetEntry.getClassPK()), role.getRoleId(),
+			ActionKeys.VIEW);
 	}
 
 	@Reference
@@ -115,6 +141,12 @@ public class AssetEntryDTOConverter
 
 	@Reference
 	private Portal _portal;
+
+	@Reference
+	private ResourcePermissionLocalService _resourcePermissionLocalService;
+
+	@Reference
+	private RoleLocalService _roleLocalService;
 
 	@Reference
 	private UserLocalService _userLocalService;

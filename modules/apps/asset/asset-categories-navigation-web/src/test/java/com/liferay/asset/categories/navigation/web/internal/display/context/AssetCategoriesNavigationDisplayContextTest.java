71841d62201834e80f16f6124842aa4c3de16b98
Akhash R <akhash.ramprakash@liferay.com>
2026-07-31 09:12:58 +0530

    LPD-98572 Test the vocabulary order reconstruction when reading
    Cover the display context with the ordered format, asserting a selection that alternates between the global, current, and parent sites reads back in the configured order, that a vocabulary that no longer resolves keeps the survivors in their configured relative order, that misaligned preferences fall back to rendering every stored vocabulary in the external-then-local order, and that preferences saved before the ordered format keep that same order.

---
+++ modules/apps/asset/asset-categories-navigation-web/src/test/java/com/liferay/asset/categories/navigation/web/internal/display/context/AssetCategoriesNavigationDisplayContextTest.java
@@ -0,0 +1,373 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.asset.categories.navigation.web.internal.display.context;
+
+import com.liferay.asset.categories.navigation.web.internal.configuration.AssetCategoriesNavigationPortletInstanceConfiguration;
+import com.liferay.asset.kernel.model.AssetVocabulary;
+import com.liferay.asset.kernel.service.AssetVocabularyLocalServiceUtil;
+import com.liferay.asset.kernel.service.AssetVocabularyServiceUtil;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.configuration.module.configuration.ConfigurationProviderUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.upgrade.MockPortletPreferences;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
+
+import jakarta.portlet.PortletPreferences;
+import jakarta.portlet.RenderRequest;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+import org.springframework.mock.web.MockHttpServletRequest;
+
+/**
+ * @author Akhash Ramprakash
+ */
+public class AssetCategoriesNavigationDisplayContextTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	@Before
+	public void setUp() {
+		_companyId = RandomTestUtil.randomLong();
+
+		_setUpConfigurationProviderUtil();
+		_setUpThemeDisplay();
+
+		_companyGroup = _mockGroup();
+		_parentGroup = _mockGroup();
+	}
+
+	@After
+	public void tearDown() {
+		_assetVocabularyLocalServiceUtilMockedStatic.close();
+		_assetVocabularyServiceUtilMockedStatic.close();
+		_configurationProviderUtilMockedStatic.close();
+		_groupLocalServiceUtilMockedStatic.close();
+	}
+
+	@Test
+	public void testGetAssetVocabularyIds() throws Exception {
+		_testGetAssetVocabularyIdsWithLegacyPortletPreferences();
+		_testGetAssetVocabularyIdsWithMisalignedPortletPreferences();
+		_testGetAssetVocabularyIdsWithOrderedPortletPreferences();
+		_testGetAssetVocabularyIdsWithUnresolvableAssetVocabulary();
+	}
+
+	private AssetCategoriesNavigationDisplayContext _createDisplayContext(
+			PortletPreferences portletPreferences)
+		throws Exception {
+
+		MockHttpServletRequest mockHttpServletRequest =
+			new MockHttpServletRequest();
+
+		mockHttpServletRequest.setAttribute(
+			WebKeys.THEME_DISPLAY, _themeDisplay);
+
+		RenderRequest renderRequest = Mockito.mock(RenderRequest.class);
+
+		Mockito.when(
+			renderRequest.getPreferences()
+		).thenReturn(
+			portletPreferences
+		);
+
+		return new AssetCategoriesNavigationDisplayContext(
+			mockHttpServletRequest, renderRequest);
+	}
+
+	private AssetVocabulary _mockAssetVocabulary(long groupId) {
+		AssetVocabulary assetVocabulary = Mockito.mock(AssetVocabulary.class);
+
+		String externalReferenceCode = RandomTestUtil.randomString();
+		long vocabularyId = RandomTestUtil.randomLong();
+
+		Mockito.when(
+			assetVocabulary.getExternalReferenceCode()
+		).thenReturn(
+			externalReferenceCode
+		);
+
+		Mockito.when(
+			assetVocabulary.getVocabularyId()
+		).thenReturn(
+			vocabularyId
+		);
+
+		_assetVocabularyLocalServiceUtilMockedStatic.when(
+			() ->
+				AssetVocabularyLocalServiceUtil.
+					fetchAssetVocabularyByExternalReferenceCode(
+						externalReferenceCode, groupId)
+		).thenReturn(
+			assetVocabulary
+		);
+
+		_assetVocabularyServiceUtilMockedStatic.when(
+			() -> AssetVocabularyServiceUtil.fetchVocabulary(vocabularyId)
+		).thenReturn(
+			assetVocabulary
+		);
+
+		return assetVocabulary;
+	}
+
+	private Group _mockGroup() {
+		Group group = Mockito.mock(Group.class);
+
+		String externalReferenceCode = RandomTestUtil.randomString();
+		long groupId = RandomTestUtil.randomLong();
+
+		Mockito.when(
+			group.getExternalReferenceCode()
+		).thenReturn(
+			externalReferenceCode
+		);
+
+		Mockito.when(
+			group.getGroupId()
+		).thenReturn(
+			groupId
+		);
+
+		_groupLocalServiceUtilMockedStatic.when(
+			() -> GroupLocalServiceUtil.fetchGroupByExternalReferenceCode(
+				externalReferenceCode, _companyId)
+		).thenReturn(
+			group
+		);
+
+		return group;
+	}
+
+	private void _setUpConfigurationProviderUtil() {
+		AssetCategoriesNavigationPortletInstanceConfiguration
+			assetCategoriesNavigationPortletInstanceConfiguration =
+				Mockito.mock(
+					AssetCategoriesNavigationPortletInstanceConfiguration.
+						class);
+
+		Mockito.when(
+			assetCategoriesNavigationPortletInstanceConfiguration.
+				allAssetVocabularies()
+		).thenReturn(
+			false
+		);
+
+		_configurationProviderUtilMockedStatic.when(
+			() -> ConfigurationProviderUtil.getPortletInstanceConfiguration(
+				Mockito.eq(
+					AssetCategoriesNavigationPortletInstanceConfiguration.
+						class),
+				Mockito.any(ThemeDisplay.class))
+		).thenReturn(
+			assetCategoriesNavigationPortletInstanceConfiguration
+		);
+	}
+
+	private void _setUpThemeDisplay() {
+		_scopeGroupId = RandomTestUtil.randomLong();
+
+		_themeDisplay = Mockito.mock(ThemeDisplay.class);
+
+		Mockito.when(
+			_themeDisplay.getCompanyId()
+		).thenReturn(
+			_companyId
+		);
+
+		Mockito.when(
+			_themeDisplay.getScopeGroupId()
+		).thenReturn(
+			_scopeGroupId
+		);
+	}
+
+	private void _testGetAssetVocabularyIdsWithLegacyPortletPreferences()
+		throws Exception {
+
+		AssetVocabulary companyAssetVocabulary1 = _mockAssetVocabulary(
+			_companyGroup.getGroupId());
+		AssetVocabulary companyAssetVocabulary2 = _mockAssetVocabulary(
+			_companyGroup.getGroupId());
+		AssetVocabulary scopeAssetVocabulary = _mockAssetVocabulary(
+			_scopeGroupId);
+
+		PortletPreferences portletPreferences = new MockPortletPreferences();
+
+		portletPreferences.setValues(
+			"assetVocabularyExternalReferenceCodes",
+			scopeAssetVocabulary.getExternalReferenceCode());
+		portletPreferences.setValues(
+			"assetVocabularyExternalReferenceCodes_" +
+				_companyGroup.getExternalReferenceCode(),
+			companyAssetVocabulary1.getExternalReferenceCode(),
+			companyAssetVocabulary2.getExternalReferenceCode());
+		portletPreferences.setValues(
+			"assetVocabularyGroupExternalReferenceCodes",
+			_companyGroup.getExternalReferenceCode());
+
+		AssetCategoriesNavigationDisplayContext
+			assetCategoriesNavigationDisplayContext = _createDisplayContext(
+				portletPreferences);
+
+		Assert.assertArrayEquals(
+			new long[] {
+				companyAssetVocabulary1.getVocabularyId(),
+				companyAssetVocabulary2.getVocabularyId(),
+				scopeAssetVocabulary.getVocabularyId()
+			},
+			assetCategoriesNavigationDisplayContext.getAssetVocabularyIds());
+	}
+
+	private void _testGetAssetVocabularyIdsWithMisalignedPortletPreferences()
+		throws Exception {
+
+		AssetVocabulary companyAssetVocabulary = _mockAssetVocabulary(
+			_companyGroup.getGroupId());
+		AssetVocabulary scopeAssetVocabulary = _mockAssetVocabulary(
+			_scopeGroupId);
+
+		PortletPreferences portletPreferences = new MockPortletPreferences();
+
+		portletPreferences.setValues(
+			"assetVocabularyExternalReferenceCodes",
+			scopeAssetVocabulary.getExternalReferenceCode());
+		portletPreferences.setValues(
+			"assetVocabularyExternalReferenceCodes_" +
+				_companyGroup.getExternalReferenceCode(),
+			companyAssetVocabulary.getExternalReferenceCode());
+		portletPreferences.setValues(
+			"assetVocabularyGroupExternalReferenceCodes",
+			_companyGroup.getExternalReferenceCode(),
+			_companyGroup.getExternalReferenceCode(), StringPool.BLANK);
+
+		AssetCategoriesNavigationDisplayContext
+			assetCategoriesNavigationDisplayContext = _createDisplayContext(
+				portletPreferences);
+
+		Assert.assertArrayEquals(
+			new long[] {
+				companyAssetVocabulary.getVocabularyId(),
+				scopeAssetVocabulary.getVocabularyId()
+			},
+			assetCategoriesNavigationDisplayContext.getAssetVocabularyIds());
+	}
+
+	private void _testGetAssetVocabularyIdsWithOrderedPortletPreferences()
+		throws Exception {
+
+		AssetVocabulary companyAssetVocabulary1 = _mockAssetVocabulary(
+			_companyGroup.getGroupId());
+		AssetVocabulary companyAssetVocabulary2 = _mockAssetVocabulary(
+			_companyGroup.getGroupId());
+		AssetVocabulary parentAssetVocabulary = _mockAssetVocabulary(
+			_parentGroup.getGroupId());
+		AssetVocabulary scopeAssetVocabulary = _mockAssetVocabulary(
+			_scopeGroupId);
+
+		PortletPreferences portletPreferences = new MockPortletPreferences();
+
+		portletPreferences.setValues(
+			"assetVocabularyExternalReferenceCodes",
+			scopeAssetVocabulary.getExternalReferenceCode());
+		portletPreferences.setValues(
+			"assetVocabularyExternalReferenceCodes_" +
+				_companyGroup.getExternalReferenceCode(),
+			companyAssetVocabulary1.getExternalReferenceCode(),
+			companyAssetVocabulary2.getExternalReferenceCode());
+		portletPreferences.setValues(
+			"assetVocabularyExternalReferenceCodes_" +
+				_parentGroup.getExternalReferenceCode(),
+			parentAssetVocabulary.getExternalReferenceCode());
+		portletPreferences.setValues(
+			"assetVocabularyGroupExternalReferenceCodes",
+			_companyGroup.getExternalReferenceCode(), StringPool.BLANK,
+			_parentGroup.getExternalReferenceCode(),
+			_companyGroup.getExternalReferenceCode());
+
+		AssetCategoriesNavigationDisplayContext
+			assetCategoriesNavigationDisplayContext = _createDisplayContext(
+				portletPreferences);
+
+		Assert.assertArrayEquals(
+			new long[] {
+				companyAssetVocabulary1.getVocabularyId(),
+				scopeAssetVocabulary.getVocabularyId(),
+				parentAssetVocabulary.getVocabularyId(),
+				companyAssetVocabulary2.getVocabularyId()
+			},
+			assetCategoriesNavigationDisplayContext.getAssetVocabularyIds());
+	}
+
+	private void _testGetAssetVocabularyIdsWithUnresolvableAssetVocabulary()
+		throws Exception {
+
+		AssetVocabulary companyAssetVocabulary = _mockAssetVocabulary(
+			_companyGroup.getGroupId());
+		AssetVocabulary scopeAssetVocabulary = _mockAssetVocabulary(
+			_scopeGroupId);
+
+		PortletPreferences portletPreferences = new MockPortletPreferences();
+
+		portletPreferences.setValues(
+			"assetVocabularyExternalReferenceCodes",
+			scopeAssetVocabulary.getExternalReferenceCode());
+		portletPreferences.setValues(
+			"assetVocabularyExternalReferenceCodes_" +
+				_companyGroup.getExternalReferenceCode(),
+			RandomTestUtil.randomString(),
+			companyAssetVocabulary.getExternalReferenceCode());
+		portletPreferences.setValues(
+			"assetVocabularyGroupExternalReferenceCodes",
+			_companyGroup.getExternalReferenceCode(), StringPool.BLANK,
+			_companyGroup.getExternalReferenceCode());
+
+		AssetCategoriesNavigationDisplayContext
+			assetCategoriesNavigationDisplayContext = _createDisplayContext(
+				portletPreferences);
+
+		Assert.assertArrayEquals(
+			new long[] {
+				scopeAssetVocabulary.getVocabularyId(),
+				companyAssetVocabulary.getVocabularyId()
+			},
+			assetCategoriesNavigationDisplayContext.getAssetVocabularyIds());
+	}
+
+	private final MockedStatic<AssetVocabularyLocalServiceUtil>
+		_assetVocabularyLocalServiceUtilMockedStatic = Mockito.mockStatic(
+			AssetVocabularyLocalServiceUtil.class);
+	private final MockedStatic<AssetVocabularyServiceUtil>
+		_assetVocabularyServiceUtilMockedStatic = Mockito.mockStatic(
+			AssetVocabularyServiceUtil.class);
+	private Group _companyGroup;
+	private long _companyId;
+	private final MockedStatic<ConfigurationProviderUtil>
+		_configurationProviderUtilMockedStatic = Mockito.mockStatic(
+			ConfigurationProviderUtil.class);
+	private final MockedStatic<GroupLocalServiceUtil>
+		_groupLocalServiceUtilMockedStatic = Mockito.mockStatic(
+			GroupLocalServiceUtil.class);
+	private Group _parentGroup;
+	private long _scopeGroupId;
+	private ThemeDisplay _themeDisplay;
+
+}

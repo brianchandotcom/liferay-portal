62f418ee6c19839e9f330d63bf89e16700604290
Alessio Rendina <alessio.rendina@liferay.com>
2026-08-04 11:48:43 +0200

    LPD-93762 Support empty model stubs for CPOptionValue

--- modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPOptionValueLocalServiceImpl.java
+++ modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPOptionValueLocalServiceImpl.java
@@ -13,6 +13,7 @@ import com.liferay.commerce.product.model.CPOptionValue;
 import com.liferay.commerce.product.service.base.CPOptionValueLocalServiceBaseImpl;
 import com.liferay.commerce.product.service.persistence.CPOptionPersistence;
 import com.liferay.expando.kernel.service.ExpandoRowLocalService;
+import com.liferay.exportimport.kernel.empty.model.EmptyModelManager;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.aop.AopService;
@@ -51,6 +52,7 @@ import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import java.io.Serializable;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -93,9 +95,13 @@ public class CPOptionValueLocalServiceImpl
 
 		User user = _userLocalService.getUser(serviceContext.getUserId());
 
+		CPOption cpOption = _cpOptionPersistence.findByPrimaryKey(cpOptionId);
+
 		key = _friendlyURLNormalizer.normalize(key);
 
-		_validateKey(_cpOptionPersistence.findByPrimaryKey(cpOptionId), 0, key);
+		if (!_emptyModelManager.isEmptyModel()) {
+			_validateKey(cpOption, 0, key);
+		}
 
 		_validateName(nameMap);
 
@@ -112,7 +118,14 @@ public class CPOptionValueLocalServiceImpl
 		cpOptionValue.setNameMap(nameMap);
 		cpOptionValue.setPriority(priority);
 		cpOptionValue.setKey(key);
-		cpOptionValue.setStatus(WorkflowConstants.STATUS_APPROVED);
+
+		if (_emptyModelManager.isEmptyModel()) {
+			cpOptionValue.setStatus(WorkflowConstants.STATUS_EMPTY);
+		}
+		else {
+			cpOptionValue.setStatus(WorkflowConstants.STATUS_APPROVED);
+		}
+
 		cpOptionValue.setExpandoBridgeAttributes(serviceContext);
 
 		cpOptionValue = cpOptionValuePersistence.update(cpOptionValue);
@@ -214,6 +227,31 @@ public class CPOptionValueLocalServiceImpl
 		return cpOptionValuePersistence.countByCPOptionId(cpOptionId);
 	}
 
+	@Indexable(type = IndexableType.REINDEX)
+	@Override
+	public CPOptionValue getOrAddEmptyCPOptionValue(
+			String externalReferenceCode, long cpOptionId, long companyId,
+			long userId)
+		throws PortalException {
+
+		ServiceContext serviceContext = new ServiceContext();
+
+		serviceContext.setCompanyId(companyId);
+		serviceContext.setUserId(userId);
+
+		return _emptyModelManager.getOrAddEmptyModel(
+			CPOptionValue.class, companyId,
+			() -> cpOptionValueLocalService.addCPOptionValue(
+				externalReferenceCode, cpOptionId,
+				Collections.singletonMap(
+					LocaleUtil.getSiteDefault(), externalReferenceCode),
+				0, externalReferenceCode, serviceContext),
+			externalReferenceCode,
+			this::fetchCPOptionValueByExternalReferenceCode,
+			this::getCPOptionValueByExternalReferenceCode,
+			CPOptionValue.class.getName());
+	}
+
 	@Override
 	public Hits search(SearchContext searchContext) {
 		try {
@@ -274,6 +312,16 @@ public class CPOptionValueLocalServiceImpl
 		cpOptionValue.setNameMap(nameMap);
 		cpOptionValue.setPriority(priority);
 		cpOptionValue.setKey(key);
+
+		if (cpOptionValue.getStatus() == WorkflowConstants.STATUS_EMPTY) {
+			cpOptionValue.setStatus(
+				_emptyModelManager.solveEmptyModel(
+					cpOptionValue.getExternalReferenceCode(),
+					cpOptionValue.getModelClassName(),
+					cpOptionValue.getCompanyId(), 0, cpOptionValue.getStatus(),
+					() -> WorkflowConstants.STATUS_APPROVED));
+		}
+
 		cpOptionValue.setExpandoBridgeAttributes(serviceContext);
 
 		cpOptionValue = cpOptionValuePersistence.update(cpOptionValue);
@@ -502,6 +550,9 @@ public class CPOptionValueLocalServiceImpl
 
 	@Reference
 	private CPOptionPersistence _cpOptionPersistence;
+
+	@Reference
+	private EmptyModelManager _emptyModelManager;
 
 	@Reference
 	private ExpandoRowLocalService _expandoRowLocalService;

d1efc68e3a94d8e4ee5da6f4bf83a8765d0b3a7e
Luis Ortiz <luis.ortiz@liferay.com>
2026-08-14 15:45:20 +0200

    LPD-93373 Add the virtual instance in a background task

---
+++ modules/apps/portal-instances/portal-instances-web/src/main/java/com/liferay/portal/instances/web/internal/constants/PortalInstancesBackgroundTaskConstants.java
@@ -0,0 +1,43 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.instances.web.internal.constants;
+
+/**
+ * @author Luis Ortiz
+ */
+public class PortalInstancesBackgroundTaskConstants {
+
+	public static final String ACTIVE = "active";
+
+	public static final String DEFAULT_ADMIN_EMAIL_ADDRESS =
+		"defaultAdminEmailAddress";
+
+	public static final String DEFAULT_ADMIN_FIRST_NAME =
+		"defaultAdminFirstName";
+
+	public static final String DEFAULT_ADMIN_LAST_NAME = "defaultAdminLastName";
+
+	public static final String DEFAULT_ADMIN_MIDDLE_NAME =
+		"defaultAdminMiddleName";
+
+	public static final String DEFAULT_ADMIN_PASSWORD = "defaultAdminPassword";
+
+	public static final String DEFAULT_ADMIN_SCREEN_NAME =
+		"defaultAdminScreenName";
+
+	public static final String MAX_USERS = "maxUsers";
+
+	public static final String MX = "mx";
+
+	public static final String NAME_ADD_VIRTUAL_INSTANCE = "AddVirtualInstance";
+
+	public static final String SITE_INITIALIZER_KEY = "siteInitializerKey";
+
+	public static final String VIRTUAL_HOSTNAME = "virtualHostname";
+
+	public static final String WEB_ID = "webId";
+
+}

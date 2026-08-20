brianchandotcom/liferay-portal

commit 30b91df21d6dbdb4af4acc8da56724da0484a714

LPS-145182 LearnMessageUtil

modules/apps/learn/learn-api/bnd.bnd
@@ -1,0 +1,4 @@
+Bundle-Name: Liferay Learn API
+Bundle-SymbolicName: com.liferay.learn.api
+Bundle-Version: 1.0.0
+Export-Package: com.liferay.learn

modules/apps/learn/learn-api/build.gradle
@@ -1,0 +1,5 @@
+dependencies {
+	compileOnly group: "com.liferay.portal", name: "com.liferay.portal.impl", version: "default"
+	compileOnly group: "com.liferay.portal", name: "com.liferay.portal.kernel", version: "default"
+	compileOnly project(":core:petra:petra-string")
+}

modules/apps/learn/learn-taglib/build.gradle
@@ -1,9 +1,8 @@
 dependencies {
-	compileOnly group: "com.liferay.portal", name: "com.liferay.portal.impl", version: "default"
 	compileOnly group: "com.liferay.portal", name: "com.liferay.portal.kernel", version: "default"
 	compileOnly group: "com.liferay.portal", name: "com.liferay.util.taglib", version: "default"
 	compileOnly group: "javax.portlet", name: "portlet-api", version: "3.0.1"
 	compileOnly group: "javax.servlet.jsp", name: "javax.servlet.jsp-api", version: "2.3.1"
 	compileOnly group: "org.apache.felix", name: "org.apache.felix.http.servlet-api", version: "1.1.2"
-	compileOnly project(":core:petra:petra-string")
+	compileOnly project(":apps:learn:learn-api")
 }

modules/apps/learn/learn-api/src/main/java/com/liferay/learn/LearnMessage.java
@@ -1,0 +1,75 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.learn;
+
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONObject;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class LearnMessage {
+
+	public LearnMessage(JSONObject jsonObject, String key, String languageId) {
+		if (jsonObject.length() == 0) {
+			return;
+		}
+
+		JSONObject keyJSONObject = jsonObject.getJSONObject(key);
+
+		if (keyJSONObject == null) {
+			return;
+		}
+
+		JSONObject languageIdJSONObject = keyJSONObject.getJSONObject(
+			languageId);
+
+		if (languageIdJSONObject == null) {
+			if (languageId.equals("en_US")) {
+				return;
+			}
+
+			languageIdJSONObject = keyJSONObject.getJSONObject("en_US");
+
+			if (languageIdJSONObject == null) {
+				return;
+			}
+		}
+
+		_message = languageIdJSONObject.getString("message");
+		_url = languageIdJSONObject.getString("url");
+
+		_html = StringBundler.concat(
+			"<a href=\"", _url, "\" target=\"_blank\">", _message, "</a>");
+	}
+
+	public String getHTML() {
+		return _html;
+	}
+
+	public String getMessage() {
+		return _message;
+	}
+
+	public String getURL() {
+		return _url;
+	}
+
+	private String _html = StringPool.BLANK;
+	private String _message = StringPool.BLANK;
+	private String _url = StringPool.BLANK;
+
+}

modules/apps/learn/learn-api/src/main/java/com/liferay/learn/LearnMessageUtil.java
@@ -1,0 +1,104 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.learn;
+
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.Http;
+import com.liferay.portal.kernel.util.HttpUtil;
+import com.liferay.portal.kernel.webcache.WebCacheItem;
+import com.liferay.portal.kernel.webcache.WebCachePoolUtil;
+import com.liferay.portal.util.PropsValues;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class LearnMessageUtil {
+
+	public static JSONObject getJSONObject(String resource) {
+		return (JSONObject)WebCachePoolUtil.get(
+			JSONObjectWebCacheItem.class.getName() + StringPool.POUND +
+				resource,
+			new JSONObjectWebCacheItem(resource));
+	}
+
+	public static LearnMessage getLearnMessage(
+		String key, String languageId, String resource) {
+
+		JSONObject jsonObject = getJSONObject(resource);
+
+		return new LearnMessage(jsonObject, key, languageId);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		LearnMessageUtil.class);
+
+	private static class JSONObjectWebCacheItem implements WebCacheItem {
+
+		public JSONObjectWebCacheItem(String resource) {
+			_resource = resource;
+		}
+
+		@Override
+		public JSONObject convert(String key) {
+			try {
+				if (!PropsValues.LEARN_RESOURCES_ENABLED) {
+					return JSONFactoryUtil.createJSONObject();
+				}
+
+				StringBundler sb = new StringBundler(5);
+
+				sb.append(Http.HTTPS_WITH_SLASH);
+
+				if (!PropsValues.LEARN_RESOURCES_CDN_ENABLED) {
+					sb.append("s3.amazonaws.com/");
+				}
+
+				sb.append("learn-resources.liferay.com/");
+				sb.append(_resource);
+				sb.append(".json");
+
+				String url = sb.toString();
+
+				if (_log.isDebugEnabled()) {
+					_log.debug("Reading " + url);
+				}
+
+				return JSONFactoryUtil.createJSONObject(
+					HttpUtil.URLtoString(url));
+			}
+			catch (Exception exception) {
+				if (_log.isDebugEnabled()) {
+					_log.debug(exception, exception);
+				}
+
+				return JSONFactoryUtil.createJSONObject();
+			}
+		}
+
+		@Override
+		public long getRefreshTime() {
+			return PropsValues.LEARN_RESOURCES_REFRESH_TIME;
+		}
+
+		private final String _resource;
+
+	}
+
+}

modules/apps/learn/learn-api/src/main/resources/com/liferay/learn/packageinfo
@@ -1,0 +1,1 @@
+version 1.0.0

modules/apps/learn/learn-taglib/src/main/java/com/liferay/learn/taglib/internal/web/cache/JSONObjectWebCacheItem.java
@@ -1,91 +1,0 @@
-/**
- * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- */
-
-package com.liferay.learn.taglib.internal.web.cache;
-
-import com.liferay.petra.string.StringBundler;
-import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.json.JSONFactoryUtil;
-import com.liferay.portal.kernel.json.JSONObject;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.util.Http;
-import com.liferay.portal.kernel.util.HttpUtil;
-import com.liferay.portal.kernel.webcache.WebCacheItem;
-import com.liferay.portal.kernel.webcache.WebCachePoolUtil;
-import com.liferay.portal.util.PropsValues;
-
-/**
- * @author Brian Wing Shun Chan
- */
-public class JSONObjectWebCacheItem implements WebCacheItem {
-
-	public static JSONObject get(String resource) {
-		return (JSONObject)WebCachePoolUtil.get(
-			JSONObjectWebCacheItem.class.getName() + StringPool.POUND +
-				resource,
-			new JSONObjectWebCacheItem(resource));
-	}
-
-	public JSONObjectWebCacheItem(String resource) {
-		_resource = resource;
-	}
-
-	@Override
-	public JSONObject convert(String key) {
-		try {
-			if (!PropsValues.LEARN_RESOURCES_ENABLED) {
-				return JSONFactoryUtil.createJSONObject();
-			}
-
-			StringBundler sb = new StringBundler(5);
-
-			sb.append(Http.HTTPS_WITH_SLASH);
-
-			if (!PropsValues.LEARN_RESOURCES_CDN_ENABLED) {
-				sb.append("s3.amazonaws.com/");
-			}
-
-			sb.append("learn-resources.liferay.com/");
-			sb.append(_resource);
-			sb.append(".json");
-
-			String url = sb.toString();
-
-			if (_log.isDebugEnabled()) {
-				_log.debug("Reading " + url);
-			}
-
-			return JSONFactoryUtil.createJSONObject(HttpUtil.URLtoString(url));
-		}
-		catch (Exception exception) {
-			if (_log.isDebugEnabled()) {
-				_log.debug(exception, exception);
-			}
-
-			return JSONFactoryUtil.createJSONObject();
-		}
-	}
-
-	@Override
-	public long getRefreshTime() {
-		return PropsValues.LEARN_RESOURCES_REFRESH_TIME;
-	}
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		JSONObjectWebCacheItem.class);
-
-	private final String _resource;
-
-}

modules/apps/learn/learn-taglib/src/main/java/com/liferay/learn/taglib/servlet/taglib/JSONTag.java
@@ -14,7 +14,7 @@
 
 package com.liferay.learn.taglib.servlet.taglib;
 
-import com.liferay.learn.taglib.internal.web.cache.JSONObjectWebCacheItem;
+import com.liferay.learn.LearnMessageUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.taglib.util.IncludeTag;
@@ -36,7 +36,7 @@ public class JSONTag extends IncludeTag {
 
 	@Override
 	public int processEndTag() throws Exception {
-		JSONObject jsonObject = JSONObjectWebCacheItem.get(_resource);
+		JSONObject jsonObject = LearnMessageUtil.getJSONObject(_resource);
 
 		if (Validator.isNotNull(_var)) {
 			pageContext.setAttribute(_var, jsonObject.toString());

commit 4ebb79e18121002c622166bac49216ba28145eef

LPD-103271 Guard the learn resources global server with its own test
Every learn message in the product renders from the resource files the global server at s3.amazonaws.com/learn-resources.liferay.com serves, and the server answering 403 makes learn links silently vanish portal wide: the fetch failure is swallowed into an empty result and cached, nothing is logged above debug level, and the only CI signal was UI tests failing on the missing link as if the features around it broke. Guard the server itself with a dedicated test that asserts the fetch answers 200 with usable JSON and surfaces the server's own error body when it does not, so an outage reads as what it is instead of as unrelated UI failures.

The test lives in the learn module, so it runs where learn changes run and stays out of unrelated suites.

Root cause: the learn message machinery never had liveness coverage of the global server it depends on; 30b91df21d6db (LPS-145182) introduced the fetch with its failure swallowed into an empty object at debug level, so no later test could see the server down except through rendering side effects.

modules/apps/learn/learn-test/bnd.bnd
@@ -1,0 +1,3 @@
+Bundle-Name: Liferay Learn Test
+Bundle-SymbolicName: com.liferay.learn.test
+Bundle-Version: 1.0.0

modules/apps/learn/learn-test/build.gradle
@@ -1,0 +1,4 @@
+dependencies {
+	testIntegrationImplementation project(":apps:learn:learn-api")
+	testIntegrationImplementation project(":test:arquillian-extension-junit-bridge")
+}

modules/apps/learn/learn-test/src/testIntegration/java/com/liferay/learn/test/LearnResourcesTest.java
@@ -1,0 +1,65 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.learn.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.learn.LearnMessageUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.util.Http;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Shuyang Zhou
+ */
+@RunWith(Arquillian.class)
+public class LearnResourcesTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Test
+	public void testGlobalServerServesLearnResources() throws Exception {
+
+		// Every learn message in the product renders from the resource files
+		// this global server serves, so when it stops serving them, learn
+		// links silently vanish portal wide. Guard the server itself here
+		// instead of leaving its outages to surface as unrelated UI test
+		// failures.
+
+		Http.Options options = new Http.Options();
+
+		options.setLocation(
+			"https://s3.amazonaws.com/learn-resources.liferay.com" +
+				"/marketplace-store-web.json");
+
+		String body = _http.URLtoString(options);
+
+		Http.Response response = options.getResponse();
+
+		Assert.assertEquals(body, 200, response.getResponseCode());
+
+		JSONObject jsonObject = LearnMessageUtil.getJSONObject(
+			"marketplace-store-web");
+
+		Assert.assertTrue(
+			jsonObject.toString(),
+			jsonObject.keys(
+			).hasNext());
+	}
+
+	@Inject
+	private Http _http;
+
+}
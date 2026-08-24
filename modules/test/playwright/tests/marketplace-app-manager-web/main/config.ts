54d695fc2107f30190b7fe20d29960251c05d802
Luis Ortiz <luis.ortiz@liferay.com>
2026-08-24 09:32:31 +0200

    LPD-93373 Raise the timeouts the asynchronous add needs

--- modules/test/playwright/tests/marketplace-app-manager-web/main/config.ts
+++ modules/test/playwright/tests/marketplace-app-manager-web/main/config.ts
@@ -6,6 +6,7 @@
 export const config = {
 	name: 'marketplace-app-manager-web.main',
 	testDir: 'tests/marketplace-app-manager-web/main',
+	timeout: 300 * 1000,
 	use: {
 		testIdAttribute: 'data-qa-id',
 	},

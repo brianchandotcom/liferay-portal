olafk/liferay-portal-old

commit ec5224e69b93b75d6c39d27d09eef113af93157e

LPD-37883 CPConfiguration service layer

modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CommerceCatalogLocalServiceImpl.java
@@ -14,12 +14,14 @@ import com.liferay.commerce.product.constants.CommerceCatalogConstants;
 import com.liferay.commerce.product.exception.CommerceCatalogProductsException;
 import com.liferay.commerce.product.exception.CommerceCatalogSystemException;
 import com.liferay.commerce.product.model.CommerceCatalog;
+import com.liferay.commerce.product.service.CPConfigurationListLocalService;
 import com.liferay.commerce.product.service.CPDefinitionLocalService;
 import com.liferay.commerce.product.service.base.CommerceCatalogLocalServiceBaseImpl;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.aop.AopService;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.GroupConstants;
@@ -44,12 +46,16 @@ import com.liferay.portal.kernel.service.ResourceLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.systemevent.SystemEvent;
+import com.liferay.portal.kernel.util.CalendarFactoryUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.uuid.PortalUUIDUtil;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
@@ -109,6 +115,29 @@ public class CommerceCatalogLocalServiceImpl
 
 		_resourceLocalService.addModelResources(
 			commerceCatalog, serviceContext);
+
+		// CPConfigurationList
+
+		Date date = new Date();
+
+		Calendar calendar = CalendarFactoryUtil.getCalendar(date.getTime());
+
+		int displayDateHour = calendar.get(Calendar.HOUR);
+
+		if (calendar.get(Calendar.AM_PM) == Calendar.PM) {
+			displayDateHour += 12;
+		}
+
+		_cpConfigurationListLocalService.addCPConfigurationList(
+			null, commerceCatalog.getGroupId(), user.getUserId(), 0, true,
+			_language.format(
+				LocaleUtil.fromLanguageId(
+					commerceCatalog.getCatalogDefaultLanguageId()),
+				"master-configuration-x", commerceCatalog.getName(), false),
+			0D, calendar.get(Calendar.MONTH),
+			calendar.get(Calendar.DAY_OF_MONTH), calendar.get(Calendar.YEAR),
+			displayDateHour, calendar.get(Calendar.MINUTE), 0, 0, 0, 0, 0,
+			true);
 
 		return commerceCatalog;
 	}
@@ -483,10 +512,16 @@ public class CommerceCatalogLocalServiceImpl
 	private CompanyLocalService _companyLocalService;
 
 	@Reference
+	private CPConfigurationListLocalService _cpConfigurationListLocalService;
+
+	@Reference
 	private CPDefinitionLocalService _cpDefinitionLocalService;
 
 	@Reference
 	private GroupLocalService _groupLocalService;
+
+	@Reference
+	private Language _language;
 
 	@Reference
 	private ResourceLocalService _resourceLocalService;

modules/apps/commerce/commerce-product-service/service.xml
@@ -412,7 +412,7 @@
 			<finder-column arrayable-operator="OR" name="groupId" />
 			<finder-column name="companyId" />
 		</finder>
-		<finder name="G_MasterCPConfigurationList" return-type="CPConfigurationList">
+		<finder name="G_M" return-type="CPConfigurationList">
 			<finder-column name="groupId" />
 			<finder-column name="masterCPConfigurationList" />
 		</finder>
@@ -1400,6 +1400,8 @@
 		<exception>CPAttachmentFileEntryName</exception>
 		<exception>CPAttachmentFileEntryProtocol</exception>
 		<exception>CPAttachmentFileEntrySize</exception>
+		<exception>CPConfigurationListCannotDelete</exception>
+		<exception>CPConfigurationListParentCPConfigurationListGroupId</exception>
 		<exception>CPDefinitionDeliveryMaxSubscriptionCycles</exception>
 		<exception>CPDefinitionDeliverySubscriptionLength</exception>
 		<exception>CPDefinitionDisplayDate</exception>
@@ -1459,6 +1461,7 @@
 		<exception>DuplicateCommerceChannelAccountEntryRel</exception>
 		<exception>DuplicateCommerceChannelRel</exception>
 		<exception>DuplicateCPAttachmentFileEntry</exception>
+		<exception>DuplicateCPConfigurationList</exception>
 		<exception>DuplicateCPDefinitionOptionRelKey</exception>
 		<exception>DuplicateCPDefinitionOptionValueRelKey</exception>
 		<exception>DuplicateCPInstance</exception>

modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPConfigurationEntryLocalServiceImpl.java
@@ -5,10 +5,21 @@
 
 package com.liferay.commerce.product.service.impl;
 
+import com.liferay.commerce.product.model.CPConfigurationEntry;
 import com.liferay.commerce.product.service.base.CPConfigurationEntryLocalServiceBaseImpl;
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.search.Indexable;
+import com.liferay.portal.kernel.search.IndexableType;
+import com.liferay.portal.kernel.service.UserLocalService;
+
+import java.math.BigDecimal;
+
+import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Marco Leo
@@ -19,4 +30,71 @@ import org.osgi.service.component.annotations.Component;
 )
 public class CPConfigurationEntryLocalServiceImpl
 	extends CPConfigurationEntryLocalServiceBaseImpl {
+
+	@Indexable(type = IndexableType.REINDEX)
+	@Override
+	public CPConfigurationEntry addCPConfigurationEntry(
+			String externalReferenceCode, long userId,
+			long cpConfigurationListId, long classNameId, long classPK,
+			String cpDefinitionInventoryEngine, String lowStockActivity,
+			boolean displayAvailability, boolean displayStockQuantity,
+			BigDecimal minStockQuantity, boolean backOrders,
+			BigDecimal minOrderQuantity, BigDecimal maxOrderQuantity,
+			String allowedOrderQuantities, BigDecimal multipleOrderQuantity)
+		throws PortalException {
+
+		User user = _userLocalService.getUser(userId);
+
+		long cpConfigurationEntryId = counterLocalService.increment();
+
+		CPConfigurationEntry cpConfigurationEntry =
+			cpConfigurationEntryPersistence.create(cpConfigurationEntryId);
+
+		cpConfigurationEntry.setExternalReferenceCode(externalReferenceCode);
+		cpConfigurationEntry.setCompanyId(user.getCompanyId());
+		cpConfigurationEntry.setUserId(user.getUserId());
+		cpConfigurationEntry.setUserName(user.getFullName());
+		cpConfigurationEntry.setClassNameId(classNameId);
+		cpConfigurationEntry.setClassPK(classPK);
+		cpConfigurationEntry.setCPConfigurationListId(cpConfigurationListId);
+		cpConfigurationEntry.setAllowedOrderQuantities(allowedOrderQuantities);
+		cpConfigurationEntry.setBackOrders(backOrders);
+		cpConfigurationEntry.setCPDefinitionInventoryEngine(
+			cpDefinitionInventoryEngine);
+		cpConfigurationEntry.setDisplayAvailability(displayAvailability);
+		cpConfigurationEntry.setDisplayStockQuantity(displayStockQuantity);
+		cpConfigurationEntry.setLowStockActivity(lowStockActivity);
+		cpConfigurationEntry.setMaxOrderQuantity(maxOrderQuantity);
+		cpConfigurationEntry.setMinOrderQuantity(minOrderQuantity);
+		cpConfigurationEntry.setMinStockQuantity(minStockQuantity);
+		cpConfigurationEntry.setMultipleOrderQuantity(multipleOrderQuantity);
+
+		return cpConfigurationEntryPersistence.update(cpConfigurationEntry);
+	}
+
+	@Override
+	public void deleteCPConfigurationEntries(long cpConfigurationListId) {
+		List<CPConfigurationEntry> cpConfigurationEntries =
+			cpConfigurationEntryLocalService.getCPConfigurationEntries(
+				cpConfigurationListId);
+
+		for (CPConfigurationEntry cpConfigurationEntry :
+				cpConfigurationEntries) {
+
+			cpConfigurationEntryLocalService.deleteCPConfigurationEntry(
+				cpConfigurationEntry);
+		}
+	}
+
+	@Override
+	public List<CPConfigurationEntry> getCPConfigurationEntries(
+		long cpConfigurationListId) {
+
+		return cpConfigurationEntryPersistence.findByCPConfigurationListId(
+			cpConfigurationListId);
+	}
+
+	@Reference
+	private UserLocalService _userLocalService;
+
 }

modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPConfigurationListLocalServiceImpl.java
@@ -5,10 +5,30 @@
 
 package com.liferay.commerce.product.service.impl;
 
+import com.liferay.commerce.price.list.exception.CommercePriceListDisplayDateException;
+import com.liferay.commerce.price.list.exception.CommercePriceListExpirationDateException;
+import com.liferay.commerce.product.exception.CPConfigurationListCannotDeleteException;
+import com.liferay.commerce.product.exception.CPConfigurationListParentCPConfigurationListGroupIdException;
+import com.liferay.commerce.product.exception.DuplicateCPConfigurationListException;
+import com.liferay.commerce.product.exception.NoSuchCPConfigurationListException;
+import com.liferay.commerce.product.model.CPConfigurationList;
+import com.liferay.commerce.product.service.CPConfigurationEntryLocalService;
 import com.liferay.commerce.product.service.base.CPConfigurationListLocalServiceBaseImpl;
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.SystemEventConstants;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.search.Indexable;
+import com.liferay.portal.kernel.search.IndexableType;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.systemevent.SystemEvent;
+import com.liferay.portal.kernel.util.Portal;
+
+import java.util.Date;
+import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Marco Leo
@@ -19,4 +39,178 @@ import org.osgi.service.component.annotations.Component;
 )
 public class CPConfigurationListLocalServiceImpl
 	extends CPConfigurationListLocalServiceBaseImpl {
+
+	@Override
+	public CPConfigurationList addCPConfigurationList(
+			String externalReferenceCode, long groupId, long userId,
+			long parentCPConfigurationListId, boolean masterCPConfigurationList,
+			String name, double priority, int displayDateMonth,
+			int displayDateDay, int displayDateYear, int displayDateHour,
+			int displayDateMinute, int expirationDateMonth,
+			int expirationDateDay, int expirationDateYear,
+			int expirationDateHour, int expirationDateMinute,
+			boolean neverExpire)
+		throws PortalException {
+
+		User user = _userLocalService.getUser(userId);
+
+		_validate(
+			groupId, 0, masterCPConfigurationList, parentCPConfigurationListId);
+
+		Date expirationDate = null;
+
+		Date displayDate = _portal.getDate(
+			displayDateMonth, displayDateDay, displayDateYear, displayDateHour,
+			displayDateMinute, user.getTimeZone(),
+			CommercePriceListDisplayDateException.class);
+
+		if (!neverExpire) {
+			expirationDate = _portal.getDate(
+				expirationDateMonth, expirationDateDay, expirationDateYear,
+				expirationDateHour, expirationDateMinute, user.getTimeZone(),
+				CommercePriceListExpirationDateException.class);
+		}
+
+		long cpConfigurationListId = counterLocalService.increment();
+
+		CPConfigurationList cpConfigurationList =
+			cpConfigurationListPersistence.create(cpConfigurationListId);
+
+		cpConfigurationList.setExternalReferenceCode(externalReferenceCode);
+		cpConfigurationList.setGroupId(groupId);
+		cpConfigurationList.setCompanyId(user.getCompanyId());
+		cpConfigurationList.setUserId(user.getUserId());
+		cpConfigurationList.setUserName(user.getFullName());
+		cpConfigurationList.setParentCPConfigurationListId(
+			parentCPConfigurationListId);
+		cpConfigurationList.setMasterCPConfigurationList(
+			masterCPConfigurationList);
+		cpConfigurationList.setName(name);
+		cpConfigurationList.setPriority(priority);
+		cpConfigurationList.setDisplayDate(displayDate);
+		cpConfigurationList.setExpirationDate(expirationDate);
+
+		return cpConfigurationListPersistence.update(cpConfigurationList);
+	}
+
+	@Indexable(type = IndexableType.DELETE)
+	@Override
+	@SystemEvent(type = SystemEventConstants.TYPE_DELETE)
+	public CPConfigurationList deleteCPConfigurationList(
+			CPConfigurationList cpConfigurationList)
+		throws PortalException {
+
+		if (cpConfigurationList.isMasterCPConfigurationList()) {
+			throw new CPConfigurationListCannotDeleteException();
+		}
+
+		cpConfigurationListPersistence.remove(cpConfigurationList);
+
+		_cpConfigurationEntryLocalService.deleteCPConfigurationEntries(
+			cpConfigurationList.getCPConfigurationListId());
+
+		return cpConfigurationList;
+	}
+
+	@Override
+	public CPConfigurationList deleteCPConfigurationList(
+			long cpConfigurationListId)
+		throws PortalException {
+
+		CPConfigurationList cpConfigurationList =
+			cpConfigurationListPersistence.findByPrimaryKey(
+				cpConfigurationListId);
+
+		return cpConfigurationListLocalService.deleteCPConfigurationList(
+			cpConfigurationList);
+	}
+
+	@Override
+	public void deleteCPConfigurationLists(long companyId)
+		throws PortalException {
+
+		List<CPConfigurationList> cpConfigurationLists =
+			cpConfigurationListPersistence.findByCompanyId(companyId);
+
+		for (CPConfigurationList cpConfigurationList : cpConfigurationLists) {
+			cpConfigurationListLocalService.forceDeleteCPConfigurationList(
+				cpConfigurationList);
+		}
+	}
+
+	@Indexable(type = IndexableType.DELETE)
+	@Override
+	@SystemEvent(type = SystemEventConstants.TYPE_DELETE)
+	public CPConfigurationList forceDeleteCPConfigurationList(
+			CPConfigurationList cpConfigurationList)
+		throws PortalException {
+
+		cpConfigurationListPersistence.remove(cpConfigurationList);
+
+		_cpConfigurationEntryLocalService.deleteCPConfigurationEntries(
+			cpConfigurationList.getCPConfigurationListId());
+
+		return cpConfigurationList;
+	}
+
+	@Override
+	public List<CPConfigurationList> getCPConfigurationLists(
+		long groupId, long companyId) {
+
+		return cpConfigurationListPersistence.findByG_C(groupId, companyId);
+	}
+
+	@Override
+	public CPConfigurationList getMasterCPConfigurationList(long groupId)
+		throws NoSuchCPConfigurationListException {
+
+		return cpConfigurationListPersistence.findByG_MasterCPConfigurationList(
+			groupId, true);
+	}
+
+	private void _validate(
+			long groupId, long cpConfigurationListId,
+			boolean masterConfigurationList, long parentCPConfigurationListId)
+		throws PortalException {
+
+		if (masterConfigurationList) {
+			CPConfigurationList cpConfigurationList =
+				cpConfigurationListPersistence.
+					fetchByG_MasterCPConfigurationList(
+						groupId, masterConfigurationList);
+
+			if ((cpConfigurationList != null) &&
+				(cpConfigurationList.getCPConfigurationListId() !=
+					cpConfigurationListId)) {
+
+				throw new DuplicateCPConfigurationListException();
+			}
+		}
+
+		if (parentCPConfigurationListId > 0) {
+			if (parentCPConfigurationListId == cpConfigurationListId) {
+				throw new CPConfigurationListParentCPConfigurationListGroupIdException();
+			}
+
+			CPConfigurationList cpConfigurationList =
+				cpConfigurationListLocalService.fetchCPConfigurationList(
+					parentCPConfigurationListId);
+
+			if ((cpConfigurationList != null) &&
+				(cpConfigurationList.getGroupId() != groupId)) {
+
+				throw new CPConfigurationListParentCPConfigurationListGroupIdException();
+			}
+		}
+	}
+
+	@Reference
+	private CPConfigurationEntryLocalService _cpConfigurationEntryLocalService;
+
+	@Reference
+	private Portal _portal;
+
+	@Reference
+	private UserLocalService _userLocalService;
+
 }

modules/apps/commerce/commerce-product-test/src/testIntegration/java/com/liferay/commerce/product/service/test/CPConfigurationEntryLocalServiceTest.java
@@ -1,0 +1,146 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.commerce.product.service.test;
+
+import com.liferay.account.constants.AccountConstants;
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.commerce.product.model.CPConfigurationEntry;
+import com.liferay.commerce.product.model.CPConfigurationList;
+import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.product.model.CommerceCatalog;
+import com.liferay.commerce.product.service.CPConfigurationEntryLocalService;
+import com.liferay.commerce.product.service.CPConfigurationListLocalService;
+import com.liferay.commerce.product.service.CommerceCatalogService;
+import com.liferay.commerce.product.test.util.CPTestUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+
+import java.math.BigDecimal;
+
+import org.frutilla.FrutillaRule;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Andrea Sbarra
+ */
+@RunWith(Arquillian.class)
+public class CPConfigurationEntryLocalServiceTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+
+		_user = UserTestUtil.addUser();
+
+		_serviceContext = ServiceContextTestUtil.getServiceContext(
+			_group.getGroupId(), _user.getUserId());
+
+		_commerceCatalog = _commerceCatalogService.addCommerceCatalog(
+			RandomTestUtil.randomString(),
+			AccountConstants.ACCOUNT_ENTRY_ID_DEFAULT,
+			RandomTestUtil.randomString(), "USD", "en_US",
+			ServiceContextTestUtil.getServiceContext(
+				TestPropsValues.getCompanyId(), _user.getGroupId(),
+				_user.getUserId()));
+
+		_cpDefinition = CPTestUtil.addCPDefinition(
+			_commerceCatalog.getGroupId());
+
+		_cpConfigurationList =
+			_cpConfigurationListLocalService.getMasterCPConfigurationList(
+				_commerceCatalog.getGroupId());
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		_cpConfigurationListLocalService.deleteCPConfigurationLists(
+			_serviceContext.getCompanyId());
+	}
+
+	@Test
+	public void testAddCPConfigurationList() throws Exception {
+		frutillaRule.scenario(
+			"Add Product Configuration Entry"
+		).given(
+			"There is a Commerce Catalog and its master configuration"
+		).when(
+			"A Configuration Entry is added"
+		).then(
+			"The Configuration Entry is created"
+		);
+
+		String externalReferenceCode = RandomTestUtil.randomString();
+
+		CPConfigurationEntry cpConfigurationEntry =
+			_cpConfigurationEntryLocalService.addCPConfigurationEntry(
+				externalReferenceCode, _user.getUserId(),
+				_cpConfigurationList.getCPConfigurationListId(),
+				_portal.getClassNameId(CPDefinition.class),
+				_cpDefinition.getCPDefinitionId(), "cpde", "lowstoc", true,
+				true, BigDecimal.ONE, true, BigDecimal.ONE, BigDecimal.TEN,
+				"123", BigDecimal.ONE);
+
+		Assert.assertNotNull(cpConfigurationEntry);
+		Assert.assertEquals(
+			externalReferenceCode,
+			cpConfigurationEntry.getExternalReferenceCode());
+	}
+
+	@Rule
+	public final FrutillaRule frutillaRule = new FrutillaRule();
+
+	private static User _user;
+
+	private CommerceCatalog _commerceCatalog;
+
+	@Inject
+	private CommerceCatalogService _commerceCatalogService;
+
+	@Inject
+	private CPConfigurationEntryLocalService _cpConfigurationEntryLocalService;
+
+	private CPConfigurationList _cpConfigurationList;
+
+	@Inject
+	private CPConfigurationListLocalService _cpConfigurationListLocalService;
+
+	private CPDefinition _cpDefinition;
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+	@Inject
+	private Portal _portal;
+
+	private ServiceContext _serviceContext;
+
+}

brianchandotcom/liferay-portal

commit 7e12071a630f066e70af5741721983cfa3f964ab

LPD-103738 Delete the master configuration list with its catalog
Adding a commerce catalog creates the master configuration list on the catalog's group, but deleting the catalog removed only the catalog row, its group and its resources, leaving the master list and its entries behind with a dead group. Their own deletes refuse without the force flag, so nothing else can ever clean them up: every catalog deletion leaked them, and the test data guard's leftover sweep, the only place loud enough to notice, died on the protection guards. Delete the group's configuration lists through the force path the company deletion already uses, so the delete undoes what the add created.

Root cause: born broken in ec5224e69b93b75d6c39d27d09eef113af93157e (LPD-37883), which added the master configuration list creation to addCommerceCatalog and the force-only deletion guard without giving deleteCommerceCatalog the matching cleanup.

modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CommerceCatalogLocalServiceImpl.java
@@ -201,6 +201,16 @@ public class CommerceCatalogLocalServiceImpl
 
 		commerceCatalogPersistence.remove(commerceCatalog);
 
+		// Commerce product configuration lists
+
+		for (CPConfigurationList cpConfigurationList :
+				_cpConfigurationListLocalService.getCPConfigurationLists(
+					groupId, commerceCatalog.getCompanyId())) {
+
+			_cpConfigurationListLocalService.deleteCPConfigurationList(
+				cpConfigurationList, true);
+		}
+
 		// Group
 
 		_groupLocalService.deleteGroup(groupId);
93dad22dbced1cec77f7afaf98c47e3d73982d56
Christian Moura <christian.moura@liferay.com>
2026-08-31 09:12:21 -0300

    LPD-97721 Resolve and propagate the request ID in the audit filter

--- portal-impl/test/unit/com/liferay/portal/kernel/audit/AuditMessageTest.java
+++ portal-impl/test/unit/com/liferay/portal/kernel/audit/AuditMessageTest.java
@@ -31,6 +31,34 @@ public class AuditMessageTest {
 		LiferayUnitTestRule.INSTANCE;
 
 	@Test
+	public void testCaptureRequestIdFromAuditRequestThreadLocal()
+		throws Exception {
+
+		AuditRequestThreadLocal auditRequestThreadLocal =
+			AuditRequestThreadLocal.getAuditThreadLocal();
+
+		String requestId = RandomTestUtil.randomString();
+
+		auditRequestThreadLocal.setRequestIdGenerated(true);
+		auditRequestThreadLocal.setRequestId(requestId);
+
+		try {
+			AuditMessage auditMessage = new AuditMessage(
+				RandomTestUtil.randomLong(), RandomTestUtil.randomLong(),
+				RandomTestUtil.randomLong(), RandomTestUtil.randomString(),
+				RandomTestUtil.nextDate(), JSONFactoryUtil.createJSONObject(),
+				RandomTestUtil.randomString(), RandomTestUtil.randomString(),
+				RandomTestUtil.randomString(), RandomTestUtil.randomString());
+
+			Assert.assertEquals(requestId, auditMessage.getRequestId());
+			Assert.assertTrue(auditMessage.isRequestIdGenerated());
+		}
+		finally {
+			AuditRequestThreadLocal.removeAuditThreadLocal();
+		}
+	}
+
+	@Test
 	public void testToJSONObject() throws Exception {
 		long groupId = RandomTestUtil.randomLong();
 		Date timestampDate = RandomTestUtil.nextDate();

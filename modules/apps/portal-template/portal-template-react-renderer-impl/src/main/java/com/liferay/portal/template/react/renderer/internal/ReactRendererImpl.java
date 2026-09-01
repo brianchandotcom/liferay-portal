brianchandotcom/liferay-portal

commit 71fd3bae52079967787d52d23d76fe520be40dd5

LPS-95524 Adds ReactRenderer to render React components in a standalone way from Java

modules/apps/portal-template/portal-template-react-renderer-impl/src/main/java/com/liferay/portal/template/react/renderer/internal/ReactRendererImpl.java
@@ -1,0 +1,72 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.template.react.renderer.internal;
+
+import com.liferay.portal.kernel.template.TemplateException;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.template.react.renderer.ComponentDescriptor;
+import com.liferay.portal.template.react.renderer.ReactRenderer;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import java.util.Map;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Chema Balsas
+ */
+@Component(service = ReactRenderer.class)
+public class ReactRendererImpl implements ReactRenderer {
+
+	public void renderReact(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse,
+			ComponentDescriptor componentDescriptor, Map<String, ?> data)
+		throws IOException, TemplateException {
+
+		renderReact(
+			httpServletRequest, httpServletResponse.getWriter(),
+			componentDescriptor, data);
+	}
+
+	public void renderReact(
+			HttpServletRequest httpServletRequest, Writer writer,
+			ComponentDescriptor componentDescriptor, Map<String, ?> data)
+		throws IOException, TemplateException {
+
+		ReactRendererHelper reactRendererHelper = new ReactRendererHelper(
+			httpServletRequest, _servletContext, componentDescriptor, data,
+			_portal);
+
+		reactRendererHelper.renderReact(writer);
+	}
+
+	@Reference
+	private Portal _portal;
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.portal.template.react.renderer.impl)",
+		unbind = "-"
+	)
+	private ServletContext _servletContext;
+
+}

modules/apps/portal-template/portal-template-react-renderer-api/bnd.bnd
@@ -1,0 +1,4 @@
+Bundle-Name: Liferay Portal Template React Renderer API
+Bundle-SymbolicName: com.liferay.portal.template.react.renderer.api
+Bundle-Version: 1.0.0
+Export-Package: com.liferay.portal.template.react.renderer

modules/apps/portal-template/portal-template-react-renderer-api/build.gradle
@@ -1,0 +1,4 @@
+dependencies {
+	compileOnly group: "com.liferay.portal", name: "com.liferay.portal.kernel", version: "default"
+	compileOnly group: "org.apache.felix", name: "org.apache.felix.http.servlet-api", version: "1.1.2"
+}

modules/apps/portal-template/portal-template-react-renderer-api/src/main/java/com/liferay/portal/template/react/renderer/ComponentDescriptor.java
@@ -1,0 +1,75 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.template.react.renderer;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Chema Balsas
+ */
+public class ComponentDescriptor {
+
+	public ComponentDescriptor(String module) {
+		this(module, null, null);
+	}
+
+	public ComponentDescriptor(String module, String componentId) {
+		this(module, componentId, null);
+	}
+
+	public ComponentDescriptor(
+		String module, String componentId, Collection<String> dependencies) {
+
+		this(module, componentId, dependencies, false);
+	}
+
+	public ComponentDescriptor(
+		String module, String componentId, Collection<String> dependencies,
+		boolean positionInLine) {
+
+		_module = module;
+
+		if (dependencies != null) {
+			_dependencies.addAll(dependencies);
+		}
+
+		_componentId = componentId;
+		_positionInLine = positionInLine;
+	}
+
+	public String getComponentId() {
+		return _componentId;
+	}
+
+	public Set<String> getDependencies() {
+		return _dependencies;
+	}
+
+	public String getModule() {
+		return _module;
+	}
+
+	public boolean isPositionInLine() {
+		return _positionInLine;
+	}
+
+	private String _componentId;
+	private Set<String> _dependencies = new HashSet<>();
+	private String _module;
+	private boolean _positionInLine;
+
+}

modules/apps/portal-template/portal-template-react-renderer-api/src/main/java/com/liferay/portal/template/react/renderer/ReactRenderer.java
@@ -1,0 +1,43 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.template.react.renderer;
+
+import com.liferay.portal.kernel.template.TemplateException;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Chema Balsas
+ */
+public interface ReactRenderer {
+
+	public void renderReact(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse,
+			ComponentDescriptor componentDescriptor, Map<String, ?> data)
+		throws IOException, TemplateException;
+
+	public void renderReact(
+			HttpServletRequest httpServletRequest, Writer writer,
+			ComponentDescriptor componentDescriptor, Map<String, ?> data)
+		throws IOException, TemplateException;
+
+}

modules/apps/portal-template/portal-template-react-renderer-api/src/main/resources/com/liferay/portal/template/react/renderer/packageinfo
@@ -1,0 +1,1 @@
+version 1.0.0

modules/apps/portal-template/portal-template-react-renderer-impl/bnd.bnd
@@ -1,0 +1,4 @@
+Bundle-Name: Liferay Portal Template React Renderer Implementation
+Bundle-SymbolicName: com.liferay.portal.template.react.renderer.impl
+Bundle-Version: 1.0.0
+Web-ContextPath: /portal-template-react-renderer-impl

modules/apps/portal-template/portal-template-react-renderer-impl/build.gradle
@@ -1,0 +1,9 @@
+dependencies {
+	compileOnly group: "com.liferay.portal", name: "com.liferay.portal.kernel", version: "default"
+	compileOnly group: "javax.portlet", name: "portlet-api", version: "3.0.1"
+	compileOnly group: "org.apache.felix", name: "org.apache.felix.http.servlet-api", version: "1.1.2"
+	compileOnly group: "org.osgi", name: "org.osgi.service.component.annotations", version: "1.3.0"
+	compileOnly project(":apps:frontend-js:frontend-js-loader-modules-extender-api")
+	compileOnly project(":apps:portal-template:portal-template-react-renderer-api")
+	compileOnly project(":core:petra:petra-string")
+}

commit 654c31e0b4885346f98cab60686bd5c160321ed8

LPD-104207 Give every React component placeholder a unique id
StringUtil.randomId() returns four random lowercase letters with no uniqueness guarantee. Drawing from the 456976 available ids, a page carrying the six to eight <react:component> placeholders a typical admin screen emits collides on the birthday bound roughly once per 16,000 to 30,000 renders.

render.es.js resolves the placeholder with getElementById and renders into its parentElement, so two placeholders sharing an id both resolve to whichever one comes first in document order. When both render calls are emitted into the same script block they also run in the same tick, and root.render is asynchronous under React 18 unless __reactDOMFlushSync is set, so neither call has removed its placeholder by the time the other resolves. Both then create a React root on the same container and the later one silently destroys the earlier component's tree. The symptom is a blank component body with no console, network, or server error to point at it.

Observed twice on ci:test:object: the object Fields screen blanked on duplicate id ifpy, shared by Fields and ExpressionBuilderModal, and the Objects definition list blanked on duplicate id renm, shared by ViewObjectDefinitions and the portlet-options DropdownMenu.

A monotonic counter cannot collide, so the ambiguity is removed at the point where the id is minted rather than compensated for downstream.

Root cause: born broken in 71fd3bae52079967787d52d23d76fe520be40dd5 (LPS-95524 Adds ReactRenderer to render React components in a standalone way from Java), which introduced both the randomId placeholder id and the getElementById().parentElement render target in the same commit.

modules/apps/portal-template/portal-template-react-renderer-impl/src/main/java/com/liferay/portal/template/react/renderer/internal/ReactRendererImpl.java
@@ -10,7 +10,6 @@ import com.liferay.frontend.js.loader.modules.extender.npm.NPMResolvedPackageNam
 import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Portal;
-import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.template.react.renderer.ComponentDescriptor;
 import com.liferay.portal.template.react.renderer.ReactRenderer;
 import com.liferay.portal.url.builder.AbsolutePortalURLBuilderFactory;
@@ -23,6 +22,7 @@ import java.io.Writer;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -39,7 +39,7 @@ public class ReactRendererImpl implements ReactRenderer {
 			HttpServletRequest httpServletRequest, Writer writer)
 		throws IOException {
 
-		String placeholderId = StringUtil.randomId();
+		String placeholderId = "reactComponent" + _counter.incrementAndGet();
 
 		_renderPlaceholder(writer, placeholderId);
 
@@ -122,6 +122,8 @@ public class ReactRendererImpl implements ReactRenderer {
 		writer.append(placeholderId);
 		writer.append("\"></div>");
 	}
+
+	private static final AtomicLong _counter = new AtomicLong();
 
 	@Reference
 	private AbsolutePortalURLBuilderFactory _absolutePortalURLBuilderFactory;
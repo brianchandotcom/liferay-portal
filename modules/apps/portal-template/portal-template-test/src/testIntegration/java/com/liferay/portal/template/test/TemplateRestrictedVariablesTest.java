85832207cb3142f04c2ec37267b0ff4926138984
Tina Tian <tina.tian@liferay.com>
2026-09-21 14:12:33 -0700

    LPD-105461 Add integration test to cover the restricted Portlet model in FreeMarker templates
    The restricted object wrapper matches with isAssignableFrom, so adding the Portlet interface to the restricted classes has to deny every implementation wherever it is reached. The test asserts that against a PortletImpl, and also asserts that an unrestricted template still reads the same property, which is what catches a restriction that reaches further than intended. The portlet carries the test company because the wrapper checks company ownership on a BaseModel before it checks the restricted classes.

--- modules/apps/portal-template/portal-template-test/src/testIntegration/java/com/liferay/portal/template/test/TemplateRestrictedVariablesTest.java
+++ modules/apps/portal-template/portal-template-test/src/testIntegration/java/com/liferay/portal/template/test/TemplateRestrictedVariablesTest.java
@@ -6,11 +6,17 @@
 package com.liferay.portal.template.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.petra.io.unsync.UnsyncStringWriter;
+import com.liferay.portal.kernel.model.Portlet;
 import com.liferay.portal.kernel.template.StringTemplateResource;
 import com.liferay.portal.kernel.template.Template;
+import com.liferay.portal.kernel.template.TemplateConstants;
+import com.liferay.portal.kernel.template.TemplateException;
 import com.liferay.portal.kernel.template.TemplateManager;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.model.impl.PortletImpl;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
 import java.util.Collection;
@@ -29,6 +35,7 @@ import org.osgi.framework.ServiceReference;
 /**
  * @author Dante Wang
  * @author Debora Buriti
+ * @author Tina Tian
  */
 @RunWith(Arquillian.class)
 public class TemplateRestrictedVariablesTest {
@@ -78,6 +85,91 @@ public class TemplateRestrictedVariablesTest {
 						restrictedVariable + " accessible in " + name,
 						template.containsKey(restrictedVariable));
 				}
+			}
+			finally {
+				bundleContext.ungetService(serviceReference);
+			}
+		}
+	}
+
+	@Test
+	public void testRestrictedPortletModel() throws Exception {
+		Bundle bundle = FrameworkUtil.getBundle(
+			TemplateRestrictedVariablesTest.class);
+
+		BundleContext bundleContext = bundle.getBundleContext();
+
+		Collection<ServiceReference<TemplateManager>> serviceReferences =
+			bundleContext.getServiceReferences(
+				TemplateManager.class,
+				"(language.type=" + TemplateConstants.LANG_TYPE_FTL + ")");
+
+		Assert.assertFalse(serviceReferences.isEmpty());
+
+		Portlet portlet = new PortletImpl(
+			TestPropsValues.getCompanyId(), RandomTestUtil.randomString());
+
+		for (ServiceReference<TemplateManager> serviceReference :
+				serviceReferences) {
+
+			TemplateManager templateManager = bundleContext.getService(
+				serviceReference);
+
+			try {
+				Template template = templateManager.getTemplate(
+					new StringTemplateResource(
+						RandomTestUtil.randomString(), "${portlet}"),
+					true);
+
+				template.put("portlet", portlet);
+
+				UnsyncStringWriter unsyncStringWriter =
+					new UnsyncStringWriter();
+
+				template.processTemplate(unsyncStringWriter);
+
+				Assert.assertEquals(
+					"Denied access to the toString method in class " +
+						portlet.getClass(),
+					unsyncStringWriter.toString());
+
+				template = templateManager.getTemplate(
+					new StringTemplateResource(
+						RandomTestUtil.randomString(), "${portlet.portletId}"),
+					true);
+
+				template.put("portlet", portlet);
+
+				try {
+					template.processTemplate(new UnsyncStringWriter());
+
+					Assert.fail();
+				}
+				catch (TemplateException templateException) {
+					Throwable throwable = templateException.getCause();
+
+					String message = throwable.getMessage();
+
+					Assert.assertTrue(
+						message,
+						message.contains(
+							"Denied access to method or field portletId of " +
+								portlet.getClass()));
+				}
+
+				template = templateManager.getTemplate(
+					new StringTemplateResource(
+						RandomTestUtil.randomString(), "${portlet.portletId}"),
+					false);
+
+				template.put("portlet", portlet);
+
+				unsyncStringWriter = new UnsyncStringWriter();
+
+				template.processTemplate(unsyncStringWriter);
+
+				Assert.assertEquals(
+					portlet.getPortletId(), unsyncStringWriter.toString());
 			}
 			finally {
 				bundleContext.ungetService(serviceReference);
